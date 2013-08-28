b62a42aa6a453602cd267ae76213d4a27ba1718c
Max Lynch <max@drifty.com>
2013-08-28 11:27:10 -0500

    Working on example app

--- example/cordova/iOS/www/js/app.js
+++ example/cordova/iOS/www/js/app.js
@@ -1,9 +1,20 @@
-var chat = angular.module('Chat', []);
+var chat = angular.module('Chat', ['ngTouch']);
+
+chat.controller('MenuCtrl', function($scope) {
+});
 
 chat.controller('RoomsCtrl', function($scope) {
   $scope.rooms = [
     { name: 'All', key: 'all' },
     { name: 'Marketing', key: 'marketing' }
   ];
+  
+  $scope.openPanel = function() {
+    $scope.isPanelShowing = true;
+  };
+
+  $scope.showAlert = function() {
+    alert("WHAT");
+  }
 });
 
