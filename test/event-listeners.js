5f8b74aa8e4b132ea88b0f048b730c65134b80af
Adam Bradley <adamdbradley@gmail.com>
2013-08-29 09:27:05 -0500

    renamed to ionic

--- test/event-listeners.js
+++ test/event-listeners.js
@@ -1,4 +1,4 @@
-(function(window, document, framework) {
+(function(window, document, ion) {
 
   // this file should not be apart of the build
   // its just just for testing that the correct
@@ -6,37 +6,37 @@
   // times, and so we don't have to hardcode/remove
   // console calls throughout the code
   
-  framework.on('ready', function(){
+  ion.on('ready', function(){
     console.log('ready');
   });
 
-  framework.on('initalized', function(){
+  ion.on('initalized', function(){
     console.log('initalized');
   });
 
-  framework.on('pageinit', function(e){
+  ion.on('pageinit', function(e){
     console.log('pageinit:', e.detail);
   });
 
-  framework.on('pageinitfailed', function(){
+  ion.on('pageinitfailed', function(){
     console.log('pageinitfailed');
   });
 
-  framework.on('pageloaded', function(e){
+  ion.on('pageloaded', function(e){
     console.log('pageloaded,', e.detail.data.url, ", Title:", e.detail.data.title);
   });
 
-  framework.on('pagecreate', function(e){
+  ion.on('pagecreate', function(e){
     console.log('pagecreate,', e.detail.url);
   });
 
-  framework.on('pageview', function(){
+  ion.on('pageview', function(){
     console.log('pageview');
   });
 
-  framework.on('pageremove', function(){
+  ion.on('pageremove', function(){
     console.log('pageremove');
   });
 
 
-})(this, document, FM = this.FM || {});
+})(this, document, ion = ion.FM || {});
