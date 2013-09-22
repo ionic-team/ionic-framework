7ea252254bf8691992733fad4ec7a815145cee9f
Max Lynch <max@codiqa.com>
2013-09-22 13:27:36 -0500

    No more errors on side menu ctrl

--- hacking/sideMenuAngular.js
+++ hacking/sideMenuAngular.js
@@ -6,7 +6,48 @@ angular.module('ionic.ui', [])
   angular.extend(this, SideMenuController.prototype);
 
   SideMenuController.call(this, {
+    left: {
+      width: 270,
+      pushDown: function() {
+      },
+      bringUp: function() {
+      }
+    },
+    right: {
+      width: 270,
+      pushDown: function() {
+      },
+      bringUp: function() {
+      }
+    },
+    content: {
+      onDrag: function(e) {},
+      endDrag: function(e) {},
+      getTranslateX: function() {
+        /*
+        var r = /translate3d\((-?.+)px/;
+        var d = r.exec(this.el.style.webkitTransform);
+
+        if(d && d.length > 0) {
+          return parseFloat(d[1]);
+        }
+        */
+        return $scope.contentTranslateX || 0;
+      },
+      setTranslateX: function(amount) {
+        //this.el.style.webkitTransform = 'translate3d(' + amount + 'px, 0, 0)';
+        $scope.contentTranslateX = amount;
+      },
+      enableAnimation: function() {
+        //this.el.classList.add(this.animateClass);
+      },
+      disableAnimation: function() {
+        //this.el.classList.remove(this.animateClass);
+      }
+    }
   });
+
+  $scope.contentTranslateX = 0;
 })
 
 .directive('sideMenuController', function() {
@@ -23,8 +64,8 @@ angular.module('ionic.ui', [])
   return {
     restrict: 'CA',
     require: '^sideMenuController',
-    compile: function(element, attr, transclude, sideMenuCtrl) {
-      return function($scope, $element, $attr) {
+    compile: function(element, attr, transclude) {
+      return function($scope, $element, $attr, sideMenuCtrl) {
         window.ionic.onGesture('drag', function(e) {
           sideMenuCtrl._handleDrag(e);
         }, $element[0]);
@@ -33,6 +74,9 @@ angular.module('ionic.ui', [])
           sideMenuCtrl._endDrag(e);
         }, $element[0]);
 
+        $scope.$watch('contentTranslateX', function(value) {
+          console.log('Translate X changing', value);
+        });
       };
     }
   }
