c7aa843fd9bd5fafd078671c672709460e55009f
Max Lynch <max@drifty.com>
2013-09-24 15:11:31 -0500

    Basic animation stuff

--- js/animate.js
+++ js/animate.js
@@ -1,15 +1,43 @@
 (function(ionic) {
   ionic.Animator = {
-    animate: function(element, fn) {
-      var endFunc = function() {
-        console.log('Animation finished for element', element);
-        element.removeEventListener('webkitTransitionEnd', endFunc);
-        element.removeEventListener('transitionEnd', endFunc);
-      };
-      element.addEventListener('webkitTransitionEnd', endFunc);
-      element.addEventListener('transitionEnd', endFunc);
+    animate: function(element, className, fn) {
+      return {
+        leave: function() {
+          var endFunc = function() {
+            console.log('Animation finished for element', element);
 
-      element.classList.add('enter');
+            element.classList.remove('leave');
+            element.classList.remove('leave-active');
+
+            element.removeEventListener('webkitTransitionEnd', endFunc);
+            element.removeEventListener('transitionEnd', endFunc);
+          };
+          element.addEventListener('webkitTransitionEnd', endFunc);
+          element.addEventListener('transitionEnd', endFunc);
+
+          element.classList.add('leave');
+          element.classList.add('leave-active');
+          return this;
+        },
+        enter: function() {
+          var endFunc = function() {
+            console.log('Animation finished for element', element);
+
+            element.classList.remove('enter');
+            element.classList.remove('enter-active');
+
+            element.removeEventListener('webkitTransitionEnd', endFunc);
+            element.removeEventListener('transitionEnd', endFunc);
+          };
+          element.addEventListener('webkitTransitionEnd', endFunc);
+          element.addEventListener('transitionEnd', endFunc);
+
+          element.classList.add('enter');
+          element.classList.add('enter-active');
+
+          return this;
+        }
+      };
     }
   };
 })(ionic = window.ionic || {});
