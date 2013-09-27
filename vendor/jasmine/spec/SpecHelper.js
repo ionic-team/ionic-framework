2abb446b8fd7d4b21ed7242426a0e48168660fc5
Max Lynch <max@drifty.com>
2013-09-27 15:21:43 -0500

    Removed font-awesome, jquery, etc.

--- vendor/jasmine/spec/SpecHelper.js
+++
@@ -1,9 +0,0 @@
-beforeEach(function() {
-  this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
-    }
-  });
-});
