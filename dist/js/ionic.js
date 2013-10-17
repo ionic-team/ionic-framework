02cf3872a1fab7d663474ddaf860e93547565d1a
Adam Bradley <adamdbradley@gmail.com>
2013-10-17 11:46:48 -0500

    removed div.track from toggle

--- dist/js/ionic.js
+++ dist/js/ionic.js
@@ -2875,20 +2875,8 @@ ionic.views.TabBar.prototype = {
   ionic.views.Toggle = function(opts) {
     this.el = opts.el;
     this.checkbox = opts.checkbox;
-    this.track = opts.track;
     this.handle = opts.handle;
     this.openPercent = -1;
-
-    /*
-    // remember that this element, and all its children are apart of a component
-    // and assign the component instance to each element so the lookups
-    // only has to go through this process just once
-    this.el.isComponent = true;
-    this.track.component = this;
-    this.track.isComponent = true;
-    this.handle.component = this;
-    this.handle.isComponent = true;
-    */
   };
 
   ionic.views.Toggle.prototype = {
@@ -2898,8 +2886,8 @@ ionic.views.TabBar.prototype = {
     },
 
     drag: function(e) {
-      var slidePageLeft = this.track.offsetLeft + (this.handle.offsetWidth / 2);
-      var slidePageRight = this.track.offsetLeft + this.track.offsetWidth - (this.handle.offsetWidth / 2);
+      var slidePageLeft = this.checkbox.offsetLeft + (this.handle.offsetWidth / 2);
+      var slidePageRight = this.checkbox.offsetLeft + this.checkbox.offsetWidth - (this.handle.offsetWidth / 2);
 
       if(e.pageX >= slidePageRight - 4) {
         this.val(true);
@@ -2920,7 +2908,7 @@ ionic.views.TabBar.prototype = {
         } else if(openPercent === 100) {
           this.val(true);
         } else {
-          var openPixel = Math.round( (openPercent / 100) * this.track.offsetWidth - (this.handle.offsetWidth) );
+          var openPixel = Math.round( (openPercent / 100) * this.checkbox.offsetWidth - (this.handle.offsetWidth) );
           openPixel = (openPixel < 1 ? 0 : openPixel);
           this.handle.style.webkitTransform = 'translate3d(' + openPixel + 'px,0,0)';
         }
--- js/ext/angular/src/directive/ionicToggle.js
+++ js/ext/angular/src/directive/ionicToggle.js
@@ -8,28 +8,21 @@ angular.module('ionic.ui.toggle', [])
     replace: true,
     require: '?ngModel',
     scope: true,
-    template: '<div class="toggle">' +
-              ' <input type="checkbox">'+
-              ' <div class="track">' +
-              '   <div class="handle"></div>' +
-              ' </div>' +
-              '</div>',
+    template: '<div class="toggle"><input type="checkbox"><div class="handle"></div></div>',
 
     link: function($scope, $element, $attr, ngModel) {
-      var checkbox, track, handle;
+      var checkbox, handle;
 
       if(!ngModel) { return; }
 
       checkbox = $element.children().eq(0);
-      track = $element.children().eq(1);
       handle = track.children().eq(0);
 
-      if(!checkbox.length || !track.length || !handle.length) { return; }
+      if(!checkbox.length || !handle.length) { return; }
 
       $scope.toggle = new ionic.views.Toggle({ 
         el: $element[0],
         checkbox: checkbox[0],
-        track: track[0],
         handle: handle[0]
       });
 
