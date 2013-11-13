12d461a65ef6342c53945b07e1951eaaf03f5b9d
Max Lynch <max@drifty.com>
2013-11-13 10:18:34 -0600

    Added action sheet thing back

--- js/ext/angular/src/directive/ionicActionSheet.js
+++ js/ext/angular/src/directive/ionicActionSheet.js
@@ -3,26 +3,46 @@
 
 angular.module('ionic.ui.actionSheet', [])
 
-.directive('actionSheet', function() {
+.directive('actionSheet', function($document) {
   return {
     restrict: 'E',
     scope: true,
     replace: true,
     link: function($scope, $element){
+      var keyUp = function(e) {
+        if(e.which == 27) {
+          $scope.cancel();
+          $scope.$apply();
+        }
+      };
+
+      var backdropClick = function(e) {
+        if(e.target == $element[0]) {
+          $scope.cancel();
+          $scope.$apply();
+        }
+      };
       $scope.$on('$destroy', function() {
         $element.remove();
+        $document.unbind('keyup', keyUp);
+        $element.unbind('click', backdropClick);
       });
+
+      $document.bind('keyup', keyUp);
+      $element.bind('click', backdropClick);
     },
-    template: '<div class="action-sheet slide-in-up">' +
-                '<div class="action-sheet-group">' +
-                  '<div class="action-sheet-title" ng-if="titleText">{{titleText}}</div>' +
-                  '<button class="button" ng-click="buttonClicked($index)" ng-repeat="button in buttons">{{button.text}}</button>' +
-                '</div>' +
-                '<div class="action-sheet-group" ng-if="destructiveText">' +
-                  '<button class="button destructive" ng-click="destructiveButtonClicked()">{{destructiveText}}</button>' +
-                '</div>' +
-                '<div class="action-sheet-group" ng-if="cancelText">' +
-                  '<button class="button" ng-click="cancel()">{{cancelText}}</button>' +
+    template: '<div class="action-sheet-backdrop">' +
+                '<div class="action-sheet action-sheet-up">' +
+                  '<div class="action-sheet-group">' +
+                    '<div class="action-sheet-title" ng-if="titleText">{{titleText}}</div>' +
+                    '<button class="button" ng-click="buttonClicked($index)" ng-repeat="button in buttons">{{button.text}}</button>' +
+                  '</div>' +
+                  '<div class="action-sheet-group" ng-if="destructiveText">' +
+                    '<button class="button destructive" ng-click="destructiveButtonClicked()">{{destructiveText}}</button>' +
+                  '</div>' +
+                  '<div class="action-sheet-group" ng-if="cancelText">' +
+                    '<button class="button" ng-click="cancel()">{{cancelText}}</button>' +
+                  '</div>' +
                 '</div>' +
               '</div>'
   };
