a5a323d85caed272c03a66f0150b93b06ae5d8c3
Max Lynch <max@codiqa.com>
2013-11-23 08:15:17 -0600

    Fixed bad polyfill for #181

--- js/utils/events.js
+++ js/utils/events.js
@@ -13,25 +13,26 @@
 (function(ionic) {
 
   // Custom event polyfill
-
   if(!window.CustomEvent) {
-    var CustomEvent;
+    (function() {
+      var CustomEvent;
 
-    CustomEvent = function(event, params) {
-      var evt;
-      params = params || {
-        bubbles: false,
-        cancelable: false,
-        detail: undefined
+      CustomEvent = function(event, params) {
+        var evt;
+        params = params || {
+          bubbles: false,
+          cancelable: false,
+          detail: undefined
+        };
+        evt = document.createEvent("CustomEvent");
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
       };
-      evt = document.createEvent("CustomEvent");
-      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-      return evt;
-    };
 
-    CustomEvent.prototype = window.Event.prototype;
+      CustomEvent.prototype = window.Event.prototype;
 
-    window.CustomEvent = CustomEvent;
+      window.CustomEvent = CustomEvent;
+    })();
   }
 
   ionic.EventController = {
