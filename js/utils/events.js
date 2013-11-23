963e0e3ae6b7392d0289f4779396fe6b99287ea3
Max Lynch <max@codiqa.com>
2013-11-23 08:07:41 -0600

    Fixed #181 - CustomEvent polyfill

--- js/utils/events.js
+++ js/utils/events.js
@@ -11,6 +11,29 @@
  */
 
 (function(ionic) {
+
+  // Custom event polyfill
+
+  if(!window.CustomEvent) {
+    var CustomEvent;
+
+    CustomEvent = function(event, params) {
+      var evt;
+      params = params || {
+        bubbles: false,
+        cancelable: false,
+        detail: undefined
+      };
+      evt = document.createEvent("CustomEvent");
+      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+      return evt;
+    };
+
+    CustomEvent.prototype = window.Event.prototype;
+
+    window.CustomEvent = CustomEvent;
+  }
+
   ionic.EventController = {
     VIRTUALIZED_EVENTS: ['tap', 'swipe', 'swiperight', 'swipeleft', 'drag', 'hold', 'release'],
 
