7eb5c2582df69392e7534df6272a99a4a3fbf344
Ulrich Sossou <sorich87@gmail.com>
2013-11-23 18:53:28 +0100

    remove unused _this variable

--- js/ext/angular/src/directive/ionicVirtRepeat.js
+++ js/ext/angular/src/directive/ionicVirtRepeat.js
@@ -12,7 +12,6 @@ angular.module('ionic.ui.virtRepeat', [])
     compile: function(element, attr, transclude) {
       return function($scope, $element, $attr, ctrls) {
         var virtualList = ctrls[1];
-        var _this = this;
 
         virtualList.listView.renderViewport = function(high, low, start, end) {
         }
