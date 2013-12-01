ff9f331d8e14a2cd4ea3acc3be4572a1c69fc0bc
Adam Bradley <adamdbradley@gmail.com>
2013-11-30 19:31:24 -0600

    links to compatibility and blog

--- dist/js/ionic-angular.js
+++ dist/js/ionic-angular.js
@@ -5,7 +5,6 @@
  * Ionic - a powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
- *
  * By @maxlynch, @helloimben, @adamdbradley <3
  *
  * Licensed under the MIT license. Please see LICENSE for more information.
