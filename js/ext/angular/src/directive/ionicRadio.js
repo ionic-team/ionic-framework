ad12ddaa831c64d97e85a10aac296b3c2d769764
Max Lynch <max@codiqa.com>
2013-12-05 11:46:01 -0600

    Initial work

--- js/ext/angular/src/directive/ionicRadio.js
+++ js/ext/angular/src/directive/ionicRadio.js
@@ -55,6 +55,55 @@ angular.module('ionic.ui.radio', [])
       }
     }
   };
+})
+
+// The radio button is a radio powered element with only
+// one possible selection in a set of options.
+.directive('radioButtons', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    require: '?ngModel',
+    scope: {
+      value: '@'
+    },
+    transclude: true,
+    template: '<div class="button-bar button-bar-inline" ng-transclude></div>',
+
+    link: function($scope, $element, $attr, ngModel) {
+      var radio;
+
+      if(!ngModel) { return; }
+
+      radio = $element.children().eq(0);
+
+      if(!radio.length) { return; }
+
+      $scope.tapHandler = function(e) {
+        radio[0].checked = true;
+        ngModel.$setViewValue($scope.$eval($attr.ngValue));
+        e.alreadyHandled = true;
+      };
+
+      var clickHandler = function(e) {
+        ngModel.$setViewValue($scope.$eval($attr.ngValue));
+      };
+
+      if(ngModel) {
+        //$element.bind('tap', tapHandler);
+        $element.bind('click', clickHandler);
+
+        ngModel.$render = function() {
+          var val = $scope.$eval($attr.ngValue);
+          if(val === ngModel.$viewValue) {
+            radio.attr('checked', 'checked');
+          } else {
+            radio.removeAttr('checked');
+          }
+        };
+      }
+    }
+  };
 });
 
 })(window.ionic);
