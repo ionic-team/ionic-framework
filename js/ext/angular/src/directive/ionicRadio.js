4e75b1b5919d2f2884b7144d0e4a7a9d7ac29afa
Max Lynch <max@drifty.com>
2013-12-05 12:48:20 -0600

    yay

--- js/ext/angular/src/directive/ionicRadio.js
+++ js/ext/angular/src/directive/ionicRadio.js
@@ -73,37 +73,55 @@ angular.module('ionic.ui.radio', [])
     link: function($scope, $element, $attr, ngModel) {
       var radio;
 
-      if(!ngModel) { return; }
-
-      radio = $element.children().eq(0);
-
-      if(!radio.length) { return; }
-
-      $scope.tapHandler = function(e) {
-        radio[0].checked = true;
-        ngModel.$setViewValue($scope.$eval($attr.ngValue));
-        e.alreadyHandled = true;
-      };
-
-      var clickHandler = function(e) {
-        ngModel.$setViewValue($scope.$eval($attr.ngValue));
-      };
 
       if(ngModel) {
         //$element.bind('tap', tapHandler);
-        $element.bind('click', clickHandler);
 
         ngModel.$render = function() {
           var val = $scope.$eval($attr.ngValue);
           if(val === ngModel.$viewValue) {
-            radio.attr('checked', 'checked');
           } else {
-            radio.removeAttr('checked');
           }
         };
+        $scope.$parent.$on('button.toggled', function(e, element) {
+          var c, children = $element.children();
+          for(var i = 0; i < children.length; i++) {
+            c = children[i];
+            if(c != element[0]) {
+              c.classList.remove('active');
+            }
+          }
+        });
       }
     }
   };
+})
+
+.directive('button', function() {
+  return {
+    restrict: 'E',
+    require: '?^ngModel',
+    link: function($scope, $element, $attr, ngModel) {
+      if(!ngModel) { return; }
+
+      var setIt = function() {
+        $element.addClass('active');
+        ngModel.$setViewValue($scope.$eval($attr.ngValue));
+        $scope.$emit('button.toggled', $element);
+      };
+
+      $scope.tapHandler = function(e) {
+        setIt();
+        e.alreadyHandled = true;
+      };
+
+      var clickHandler = function(e) {
+        setIt();
+      };
+        
+      $element.bind('click', clickHandler);
+    }
+  }
 });
 
 })(window.ionic);
