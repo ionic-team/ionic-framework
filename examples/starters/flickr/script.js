c3f7d86d2d106c66454a5b0efe7502ddb5b04b10
Max Lynch <max@drifty.com>
2013-12-09 16:57:19 -0600

    Flickr example push up search

--- examples/starters/flickr/script.js
+++ examples/starters/flickr/script.js
@@ -31,4 +31,26 @@ angular.module('myApp', ['ionic', 'ngResource'])
     doSearch($scope.query);
   }
 
-});
+})
+
+.directive('pushSearch', function() {
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+      var amt, st, header;
+
+      $element.bind('scroll', function(e) {
+        if(!header) {
+          header = document.getElementById('search-bar');
+        }
+        st = e.detail.scrollTop;
+        if(st < 0) {
+          header.style.webkitTransform = 'translate3d(0, 0px, 0)';
+        } else {
+          header.style.webkitTransform = 'translate3d(0, ' + -st + 'px, 0)';
+        }
+      });
+    }
+  }
+})
+
