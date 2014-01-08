cfdefd452e40963ed29146561acbbd017c73a411
Adam Bradley <adamdbradley@gmail.com>
2014-01-07 19:50:39 -0600

    update w/ ui-router

--- examples/starters/list/js/app.js
+++ examples/starters/list/js/app.js
@@ -3,34 +3,29 @@
 // the 2nd parameter is an array or 'requires'
 // 'listExample.services' is found in services.js
 // 'listExample.controllers' is found in controllers.js
-angular.module('listExample', ['ionic', 'ngRoute', 'ngAnimate', 'listExample.services', 'listExample.controllers'])
+angular.module('listExample', ['ionic', 'listExample.services', 'listExample.controllers'])
 
-.config(function ($compileProvider){
-  // Needed for routing to work
-  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-})
-
-.config(function($routeProvider, $locationProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
   // Set up the initial routes that our app will respond to.
   // These are then tied up to our nav router which animates and
   // updates a navigation bar
-  $routeProvider.when('/', {
-    templateUrl: '/index.html',
-    controller: 'MovieIndexCtrl'
-  });
+  $stateProvider
+    .state('movieIndex', {
+      url: '/',
+      templateUrl: '/index.html',
+      controller: 'MovieIndexCtrl'
+    })
 
-  // if the url matches something like /movie/88 then this route
-  // will fire off the MovieDetailCtrl (controllers.js)
-  $routeProvider.when('/movie/:movieId', {
-    templateUrl: '/movie.html',
-    controller: 'MovieDetailCtrl'
-  });
+    // if the url matches something like /movie/88 then this route
+    // will fire off the MovieDetailCtrl (controllers.js)
+    .state('movieDetail', {
+      url: '/movie/:movieId',
+      templateUrl: '/movie.html',
+      controller: 'MovieDetailCtrl'
+    });
 
   // if none of the above routes are met, use this fallback
-  $routeProvider.otherwise({
-    redirectTo: '/'
-  });
+  $urlRouterProvider.otherwise('/');
 
 });
-
