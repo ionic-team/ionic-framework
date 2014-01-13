724c2b8ec4117e30afaa9d425cadeda5470dbe8d
Justin Noel <github@calendee.com>
2014-01-13 06:22:20 -0600

    Applied Optional To activeSlide Directive
    Per PR https://github.com/driftyco/ionic/pull/404
    
    Signed-off-by: Justin Noel <github@calendee.com>

--- js/ext/angular/src/directive/ionicSlideBox.js
+++ js/ext/angular/src/directive/ionicSlideBox.js
@@ -24,7 +24,7 @@ angular.module('ionic.ui.slideBox', [])
       showPager: '@',
       disableScroll: '@',
       onSlideChanged: '&',
-      activeSlide: '='
+      activeSlide: '=?'
     },
     controller: ['$scope', '$element', function($scope, $element) {
       var _this = this;
