8a160077cccffadea7a4edea9e3e77d0b7d05721
Adam Bradley <adamdbradley@gmail.com>
2014-01-30 12:11:47 -0600

    fix toggle on desktop

--- js/ext/angular/src/directive/ionicToggle.js
+++ js/ext/angular/src/directive/ionicToggle.js
@@ -22,8 +22,8 @@ angular.module('ionic.ui.toggle', [])
                 '<div class="disable-pointer-events" ng-transclude></div>' +
                 '<label class="toggle">' +
                   '<input type="checkbox" ng-model="ngModel" ng-value="ngValue" ng-change="ngChange()">' +
-                  '<div class="track">' +
-                    '<div class="handle"></div>' +
+                  '<div class="track disable-pointer-events">' +
+                    '<div class="handle disable-pointer-events"></div>' +
                   '</div>' +
                 '</label>' +
               '</div>',
