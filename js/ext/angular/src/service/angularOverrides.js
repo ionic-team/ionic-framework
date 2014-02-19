0421b7b450626017a1ebc9478808d5d30d062b7e
Adam Bradley <adamdbradley@gmail.com>
2014-02-18 21:54:15 -0600

    Minimal classList polyfill added

--- js/ext/angular/src/service/angularOverrides.js
+++ js/ext/angular/src/service/angularOverrides.js
@@ -1,26 +1,9 @@
 
 angular.element.prototype.addClass = function(cssClasses) {
-  var x, y, cssClass, el, splitClasses, existingClasses;
   if (cssClasses && cssClasses != 'ng-scope' && cssClasses != 'ng-isolate-scope') {
-    for(x=0; x<this.length; x++) {
-      el = this[x];
-      if(el.setAttribute) {
-
-        if(cssClasses.indexOf(' ') < 0) {
-          el.classList.add(cssClasses);
-        } else {
-          existingClasses = (' ' + (el.getAttribute('class') || '') + ' ')
-            .replace(/[\n\t]/g, " ");
-          splitClasses = cssClasses.split(' ');
-
-          for (y=0; y<splitClasses.length; y++) {
-            cssClass = splitClasses[y].trim();
-            if (existingClasses.indexOf(' ' + cssClass + ' ') === -1) {
-              existingClasses += cssClass + ' ';
-            }
-          }
-          el.setAttribute('class', existingClasses.trim());
-        }
+    for(var x=0; x<this.length; x++) {
+      if(this[x].setAttribute) {
+        this[x].classList.add.apply(this[x].classList, cssClasses.split(/\s+/));
       }
     }
   }
@@ -28,25 +11,10 @@ angular.element.prototype.addClass = function(cssClasses) {
 };
 
 angular.element.prototype.removeClass = function(cssClasses) {
-  var x, y, splitClasses, cssClass, el;
   if (cssClasses) {
-    for(x=0; x<this.length; x++) {
-      el = this[x];
-      if(el.getAttribute) {
-        if(cssClasses.indexOf(' ') < 0) {
-          el.classList.remove(cssClasses);
-        } else {
-          splitClasses = cssClasses.split(' ');
-
-          for (y=0; y<splitClasses.length; y++) {
-            cssClass = splitClasses[y];
-            el.setAttribute('class', (
-                (" " + (el.getAttribute('class') || '') + " ")
-                .replace(/[\n\t]/g, " ")
-                .replace(" " + cssClass.trim() + " ", " ")).trim()
-            );
-          }
-        }
+    for(var x=0; x<this.length; x++) {
+      if(this[x].setAttribute) {
+        this[x].classList.remove.apply(this[x].classList, cssClasses.split(/\s+/));
       }
     }
   }
--- js/utils/poly.js
+++ js/utils/poly.js
@@ -17,6 +17,50 @@
     }
   })();
 
+  // classList polyfill for them older Androids
+  // https://gist.github.com/devongovett/1381839
+  if (!("classList" in document.documentElement) && Object.defineProperty && typeof HTMLElement !== 'undefined') {
+    Object.defineProperty(HTMLElement.prototype, 'classList', {
+      get: function() {
+        var self = this;
+        function update(fn) {
+          return function() {
+            var x, classes = self.className.split(/\s+/);
+
+            for(x=0; x<arguments.length; x++) {
+              fn(classes, classes.indexOf(arguments[x]), arguments[x]);
+            }
+            
+            self.className = classes.join(" ");
+          }
+        }
+
+        return {                    
+          add: update(function(classes, index, value) {
+            ~index || classes.push(value);
+          }),
+
+          remove: update(function(classes, index) {
+            ~index && classes.splice(index, 1);
+          }),
+
+          toggle: update(function(classes, index, value) {
+            ~index ? classes.splice(index, 1) : classes.push(value);
+          }),
+
+          contains: function(value) {
+            return !!~self.className.split(/\s+/).indexOf(value);
+          },
+
+          item: function(i) {
+            return self.className.split(/\s+/)[i] || null;
+          }
+        };
+
+      }
+    });
+  }
+
   // polyfill use to simulate native "tap"
   ionic.tapElement = function(target, e) {
     // simulate a normal click by running the element's click method then focus on it
