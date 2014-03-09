42560a918be03cecc5f6648bfbd16d67ddc9ad7d
Andy Joslin <andytjoslin@gmail.com>
2014-03-08 20:56:42 -0700

    docs(ionNavAnimation): write docs

--- js/ext/angular/src/directive/ionicNavAnimation.js
+++ js/ext/angular/src/directive/ionicNavAnimation.js
@@ -1,4 +1,29 @@
 angular.module('ionic.ui.navAnimation', [])
+/**
+ * @ngdoc directive
+ * @name ionNavAnimation
+ * @module ionic
+ * @restrict A
+ *
+ * @description
+ * When used under an {@link ionic.directive:ionNavView ionNavView} and on an `<a>` element, 
+ * allows you to set the animation all clicks on that link within the navView use.
+ *
+ * @usage
+ * ```html
+ * <ion-nav-view>
+ *   <ion-view>
+ *     <ion-content>
+ *       <a href="#/some-page" ion-nav-animation="slide-in-up">
+ *         Click me and #/some-page will transition in with the slide-in-up animation!
+ *       </a>
+ *     </ion-content>
+ *   </ion-view>
+ * </ion-nav-view>
+ * ```
+ *
+ * @param {string} ion-nav-animation The animation to make the parent ionNavView change pages with when clicking this element.
+ */
 .directive('ionNavAnimation', function() {
   return {
     restrict: 'A',
