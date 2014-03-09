2a686dd97d6f233d6769387bc61f0e84ec32bf4d
Andy Joslin <andytjoslin@gmail.com>
2014-03-08 20:56:35 -0700

    docs(ionCheckbox): write docs

--- js/ext/angular/src/directive/ionicCheckbox.js
+++ js/ext/angular/src/directive/ionicCheckbox.js
@@ -3,7 +3,21 @@
 
 angular.module('ionic.ui.checkbox', [])
 
-
+/**
+ * @ngdoc directive
+ * @name ionCheckbox
+ * @module ionic
+ * @restrict E
+ * @description
+ * No different than the HTML checkbox input, except it's styled differently.
+ *
+ * Behaves like any [AngularJS checkbox](http://docs.angularjs.org/api/ng/input/input[checkbox]).
+ *
+ * @usage
+ * ```html
+ * <ion-checkbox ng-model="isChecked">Checkbox Label</ion-checkbox>
+ * ```
+ */
 .directive('ionCheckbox', function() {
   return {
     restrict: 'E',
