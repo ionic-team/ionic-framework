700997c608af33dadbe0d31995d7ff44c05bd402
Max Lynch <max@drifty.com>
2014-03-11 15:59:25 -0500

    Temp refactor

--- js/ext/angular/src/directive/ionicPopup.js
+++ js/ext/angular/src/directive/ionicPopup.js
@@ -3,25 +3,49 @@
 
 angular.module('ionic.ui.popup', [])
 
-.directive('ionPopup', function() {
+.directive('ionPopupBackdrop', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="popup-backdrop"></div>'
+  }
+})
+
+.directive('ionPopup', ['$ionicBind', function($ionicBind) {
   return {
     restrict: 'E',
     replace: true,
     transclude: true,
-    link: function($scope, $element){
+    link: function($scope, $element, $attr) {
+      $ionicBind($scope, $attr, {
+        title: '@',
+        buttons: '=',
+        $onButtonTap: '&',
+        $onClose: '&'
+      });
+
+      $scope.onButtonTap = function(button, event) {
+        console.log('Button tapped', button, event);
+        var close = button.onTap && button.onTap(event);
+        if(close === true) {
+          console.log('Closing!');
+          $scope.$onClose(button, event);
+          //self.remove();
+        }
+        $scope.$onButtonTap(button, event);
+      }
     },
-    template: '<div class="popup-backdrop active">' +
-                '<div class="popup">' +
+    template:   '<div class="popup">' +
                   '<div class="popup-head">' +
                     '<h3 class="popup-title">{{title}}</h3>' +
                   '</div>' +
                   '<div class="popup-body" ng-transclude>' +
                   '</div>' +
                   '<div class="popup-buttons row">' +
+                    '<button ng-repeat="button in buttons" ng-click="onButtonTap(button, $event)" class="button button-clear col" ng-class="button.type || \'button-positive\'" ng-bind-html="button.text"></button>' +
                   '</div>' +
-                '</div>' +
-              '</div>'
+                '</div>'
   };
-});
+}]);
 
 })();
--- js/ext/angular/src/service/ionicPopup.js
+++ js/ext/angular/src/service/ionicPopup.js
@@ -1,23 +1,123 @@
+(function(ionic) {
+'use strict';
+
 angular.module('ionic.service.popup', ['ionic.service.templateLoad'])
 
+.factory('$ionicPopup', ['$rootScope', '$q', '$document', '$compile', '$timeout', '$ionicTemplateLoader',
+  function($rootScope, $q, $document, $compile, $timeout, $ionicTemplateLoader) {
 
-.factory('$ionicPopup', ['$rootScope', '$q', '$document', '$compile', '$ionicTemplateLoader', function($rootScope, $q, $document, $compile, $ionicTemplateLoader) {
+
+  var positionPopup = function(popup) {
+    popup.el.style.marginLeft = (-popup.el.offsetWidth) / 2 + 'px';
+    popup.el.style.marginTop = (-popup.el.offsetHeight) / 2 + 'px';
+  };
+
+  var hideBody = function(popup) {
+    var bodyEl = popup.el.querySelector('.popup-body');
+    if(bodyEl && bodyEl.innerHTML.trim() == '') {
+      bodyEl.style.display = 'none';
+    }
+  };
+
+  var showSinglePopup = function(popup, opts) {
+    var _this = this;
+
+    ionic.requestAnimationFrame(function() {
+      hideBody(popup);
+      positionPopup(popup);
+      popup.el.classList.remove('popup-hidden');
+      popup.el.classList.add('popup-showing');
+      popup.el.classList.add('active');
+    });
+  };
+
+  var hideSinglePopup = function(popup) {
+    ionic.requestAnimationFrame(function() {
+      popup.el.classList.remove('active');
+      popup.el.classList.add('popup-hidden');
+    });
+  };
+
+  var removePopup = function(popup) {
+    var popups;
+
+    // Force a reflow so the animation will actually run
+    this.el.offsetWidth;
+
+    this.el.classList.remove('active');
+
+    this.el.remove();
+  };
+
+
+  /**
+   * Popup stack and directive
+   */
+
+  var popupStack = [];
+
+  var showBackdrop = function() {
+    var el = $compile('<ion-popup-backdrop></ion-popup-backdrop>')($rootScope.$new(true));
+    $document[0].body.appendChild(el[0]);
+  };
+
+  // Push the new popup onto the stack with the given data and scope.
+  // If this is the first one in the stack, show the backdrop, otherwise don't.
+  var pushAndShow = function(popup, data, q) {
+    var lastPopup = popupStack[popupStack.length-1];
+
+    popupStack.push(popup);
+
+    // If this is the first popup, show the backdrop
+    if(popupStack.length == 1) {
+      showBackdrop();
+    }
+
+    // TODO: Use the q field to resolve/reject based on popup response
+
+    // If we have an existing popup, add a delay between hiding and showing it
+    if(lastPopup) {
+      hideSinglePopup(lastPopup);
+      $timeout(function() {
+        showSinglePopup(popup);
+      }, 50);
+    } else {
+      // Otherwise, immediately show it
+      showSinglePopup(popup);
+    }
+
+  };
+
+  var popAndRemove = function(popup) {
+  };
 
   // Append the element to the screen, create the popup view,
   // and add the popup to the scope
   var constructPopupOnScope = function(element, scope) {
-    $document[0].body.appendChild(element[0]);
-
-    var popup = new ionic.views.Popup({el: element[0] });
+    var popup = {
+      el: element[0],
+      scope: scope
+    };
 
     scope.popup = popup;
 
     return popup;
   }
 
+  var buildPopupTemplate = function(opts, content) {
+    return '<ion-popup title="' + opts.title + '" buttons="buttons" on-button-tap="onButtonTap(button, event)" on-close="onClose(button, event)">' 
+        + (content || '') + 
+      '</ion-popup>';
+  };
+
+
+  // Given an options object, build a new popup window and return a promise
+  // which will contain the constructed popup at a later date. Perhaps at a later
+  // year even. At this point, it's hard to say.
   var createPopup = function(opts) {
     var q = $q.defer();
 
+    // Create some defaults
     var defaults = {
       title: '',
       animation: 'fade-in',
@@ -25,33 +125,49 @@ angular.module('ionic.service.popup', ['ionic.service.templateLoad'])
 
     opts = angular.extend(defaults, opts);
 
+    // Create a new scope, and bind some of the options stuff to that scope
     var scope = opts.scope && opts.scope.$new() || $rootScope.$new(true);
     angular.extend(scope, opts);
 
     // Check if we need to load a template for the content of the popup
     if(opts.templateUrl) {
+
+      // Load the template externally
       $ionicTemplateLoader.load(opts.templateUrl).then(function(templateString) {
-        var element = $compile('<ion-popup>' + templateString + '</ion-popup>')(scope);
+
+        var popupTemplate = buildPopupTemplate(opts, templateString);
+        var element = $compile(popupTemplate)(scope);
+        $document[0].body.appendChild(element[0]);
         q.resolve(constructPopupOnScope(element, scope));
+
       }, function(err) {
+        // Error building the popup
         q.reject(err);
       });
 
     } else {
       // Compile the template
-      var element = $compile('<ion-popup></ion-popup>')(scope);
+      var popupTemplate = buildPopupTemplate(opts, opts.content);
+      var element = $compile(popupTemplate)(scope);
+      $document[0].body.appendChild(element[0]);
       q.resolve(constructPopupOnScope(element, scope));
     }
 
     return q.promise;
   };
 
+
+
+  // Public API
   return {
     showPopup: function(data) {
       var q = $q.defer();
 
-      createPopup(data).then(function(popup) {
-        popup.show(data);
+      createPopup(data).then(function(popup, scope) {
+
+        // We constructed the popup, push it on the stack and show it
+        pushAndShow(popup, data, scope, q);
+
       }, function(err) {
         console.error('Unable to load popup:', err);
       });
@@ -80,3 +196,5 @@ angular.module('ionic.service.popup', ['ionic.service.templateLoad'])
     }
   };
 }]);
+
+})(ionic);
