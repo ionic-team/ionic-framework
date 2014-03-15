baa04cde4d7292d07ec2bfae949e008aeaafae1b
Adam Bradley <adambradley25@gmail.com>
2014-03-15 01:12:56 -0500

    feat(active): Removing use of :active in favor of .active for more control of active state
    Using the :active pseudo works fine for desktop, but mobile is a
    completely different beast, especially with the quirks of each
    platform. By intentionally not using any :active selectors and manually
    adding/removing a .active class, it gives us a precise control on how
    the active state works for ALL platforms. Additionally, this places
    less selectors in the css, and reduces the possibility of unnecessary
    repaints. Currently this method of using .active instead of :active is
    being applied to .button and .item elements.

--- js/utils/tap.js
+++ js/utils/tap.js
@@ -175,6 +175,13 @@
       var tap = isRecentTap(e);
       if(tap) delete tapCoordinates[tap.id];
     }, REMOVE_PREVENT_DELAY);
+
+    setTimeout(function(){
+      for(var hitKey in hitElements) {
+        hitElements[hitKey] && hitElements[hitKey].classList.remove('active');
+        delete hitElements[hitKey];
+      }
+    }, 150);
   }
 
   function stopEvent(e){
@@ -197,6 +204,20 @@
 
   function recordStartCoordinates(e) {
     startCoordinates = getCoordinates(e);
+
+    var x, ele = e.target;
+    for(x=0; x<5; x++) {
+      if(!ele || ele.tagName === 'LABEL') break;
+      if( ele.classList.contains('item') || ele.classList.contains('button') ) {
+        hitElements[hitCounts] = ele;
+        hitCounts = (hitCounts > 24 ? 0 : hitCounts + 1);
+        ionic.requestAnimationFrame(function(){
+          ele.classList.add('active');
+        });
+        break;
+      }
+      ele = ele.parentElement;
+    }
   }
 
   var tapCoordinates = {}; // used to remember coordinates to ignore if they happen again quickly
@@ -204,6 +225,8 @@
   var CLICK_PREVENT_DURATION = 1500; // max milliseconds ghostclicks in the same area should be prevented
   var REMOVE_PREVENT_DELAY = 375; // delay after a touchend/mouseup before removing the ghostclick prevent
   var HIT_RADIUS = 15;
+  var hitElements = {};
+  var hitCounts = 0;
 
   // set global click handler and check if the event should stop or not
   document.addEventListener('click', preventGhostClick, true);
