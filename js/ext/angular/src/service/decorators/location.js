fa7d3a651a18415d4e7ee8845825a9f1ed99dbf5
PatrickJS <github@gdi2290.com>
2014-03-21 21:12:33 -0700

    chore($LocationDecorator): out of global scope
    Closes #867

--- js/ext/angular/src/service/decorators/location.js
+++ js/ext/angular/src/service/decorators/location.js
@@ -4,25 +4,25 @@ angular.module('ionic.decorator.location', [])
  * @private
  */
 .config(['$provide', function($provide) {
+  function $LocationDecorator($location, $timeout) {
+
+    $location.__hash = $location.hash;
+    //Fix: when window.location.hash is set, the scrollable area
+    //found nearest to body's scrollTop is set to scroll to an element
+    //with that ID.
+    $location.hash = function(value) {
+      if (angular.isDefined(value)) {
+        $timeout(function() {
+          var scroll = document.querySelector('.scroll-content');
+          if (scroll)
+            scroll.scrollTop = 0;
+        }, 0, false);
+      }
+      return $location.__hash(value);
+    };
+
+    return $location;
+  }
+  
   $provide.decorator('$location', ['$delegate', '$timeout', $LocationDecorator]);
 }]);
-
-function $LocationDecorator($location, $timeout) {
-
-  $location.__hash = $location.hash;
-  //Fix: when window.location.hash is set, the scrollable area
-  //found nearest to body's scrollTop is set to scroll to an element
-  //with that ID.
-  $location.hash = function(value) {
-    if (angular.isDefined(value)) {
-      $timeout(function() {
-        var scroll = document.querySelector('.scroll-content');
-        if (scroll)
-          scroll.scrollTop = 0;
-      }, 0, false);
-    }
-    return $location.__hash(value);
-  };
-
-  return $location;
-}
