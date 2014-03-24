dccf7b41823228c52a3b0d08e0da8b8604c9012d
Adam Bradley <adamdbradley@gmail.com>
2014-03-24 13:11:01 -0500

    rename .hide-footer to .footer-hide

--- js/utils/keyboard.js
+++ js/utils/keyboard.js
@@ -24,12 +24,12 @@ function androidKeyboardFix() {
     //If the height changes, and it's less than before, we have a keyboard open
     } else if (rememberedDeviceHeight !== window.innerHeight &&
                window.innerHeight < rememberedDeviceHeight) {
-      document.body.classList.add('hide-footer');
+      document.body.classList.add('footer-hide');
       //Wait for next frame so document.activeElement is set
       ionic.requestAnimationFrame(handleKeyboardChange);
     } else {
       //Otherwise we have a keyboard close or a *really* weird resize
-      document.body.classList.remove('hide-footer');
+      document.body.classList.remove('footer-hide');
     }
 
     function handleKeyboardChange() {
