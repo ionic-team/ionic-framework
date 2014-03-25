eb44123587e5b356bd918baba467f68b8186b692
Andy Joslin <andytjoslin@gmail.com>
2014-03-25 09:54:17 -0600

    chore(travis): make ionic-bower include ionic.bundle.js

--- scripts/bower/publish.sh
+++ scripts/bower/publish.sh
@@ -38,8 +38,6 @@ function run {
 
   # Angular dependencies are managed by bower, don't include them
   rm -rf $BOWER_DIR/js/angular*
-  # Remove bundle, dependencies are again managed by bower!
-  rm -rf $BOWER_DIR/js/ionic.bundle.*
   rm -rf $BOWER_DIR/version.json # unneeded
 
   echo "-- Copying bower.json from project_dir and renaming main files"
