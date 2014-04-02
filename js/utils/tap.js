f2d52ea9877dae03b55e8a89ae79d2a31aca4f65
Adam Bradley <adambradley25@gmail.com>
2014-04-02 16:04:01 -0500

    set focus() only if it wasn't scrolled, selection and type=email throws errors

--- js/utils/tap.js
+++ js/utils/tap.js
@@ -22,9 +22,8 @@
     ele.dispatchEvent(clickEvent);
 
     if(ele.tagName === 'INPUT' || ele.tagName === 'TEXTAREA') {
-      if(ele.selectionStart === 0 && ele.selectionEnd === 0 && !isScrolledSinceStart(e)) {
+      if(!isScrolledSinceStart(e)) {
         ele.focus();
-        ele.setSelectionRange && ele.setSelectionRange(ele.value.length, ele.value.length);
       }
       e.preventDefault();
     } else {
