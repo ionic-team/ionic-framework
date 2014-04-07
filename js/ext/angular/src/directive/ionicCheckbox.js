dfbb3765527fe475896e23d822035e07b5f13d5c
Adam Bradley <adambradley25@gmail.com>
2014-04-07 14:03:48 -0500

    rename .checkbox-handle to .checkbox-icon

--- js/ext/angular/src/directive/ionicCheckbox.js
+++ js/ext/angular/src/directive/ionicCheckbox.js
@@ -34,9 +34,9 @@ angular.module('ionic.ui.checkbox', [])
     transclude: true,
 
     template: '<label class="item item-checkbox">' +
-                '<div class="checkbox checkbox-input-hidden">' +
+                '<div class="checkbox checkbox-input-hidden disable-pointer-events">' +
                   '<input type="checkbox" ng-model="ngModel" ng-value="ngValue" ng-change="ngChange()">' +
-                  '<div class="checkbox-handle"></div>' +
+                  '<i class="checkbox-icon"></i>' +
                 '</div>' +
                 '<div class="item-content disable-pointer-events" ng-transclude></div>' +
               '</label>',
