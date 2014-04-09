41c02f10ed4c09830fd4eeb5ca0d2d73625024c0
Andy Joslin <andytjoslin@gmail.com>
2014-04-09 09:46:07 -0600

    chore(build): fix update-angular script

--- scripts/update-angular.sh
+++ scripts/update-angular.sh
@@ -4,16 +4,23 @@ ARG_DEFS=(
   "--version=(.*)"
 )
 function run {
+  PROJECT_DIR=$SCRIPT_DIR/../
 
-  cd $SCRIPT_DIR/..//config/lib/js/angular/
+  cd $SCRIPT_DIR/../config/lib/js/angular/
   rm -rf *.js
-  wget http://code.angularjs.org/$VERSION/angular{,-animate,-mocks,-resource,-scenario,-animate.min,-resource.min,.min,-sanitize,-sanitize.min}.js
 
-  cd $SCRIPT_DIR/../
+  wget http://code.angularjs.org/$VERSION/angular{,-sanitize,-animate,-resource}{.min.js,.js}
 
-  replaceJsonProp "bower.json" "angular" $VERSION
-  replaceJsonProp "bower.json" "angular-animate" $VERSION
-  replaceJsonProp "bower.json" "angular-sanitize" $VERSION
+  # # no min versions of mocks and scenario
+  wget http://code.angularjs.org/$VERSION/angular{-scenario,-mocks}.js
+
+  cd $PROJECT_DIR
+
+  echo "Setting bower.json angular versions to ~$VERSION"
+
+  replaceJsonProp "bower.json" "angular" "~$VERSION"
+  replaceJsonProp "bower.json" "angular-animate" "~$VERSION"
+  replaceJsonProp "bower.json" "angular-sanitize" "~$VERSION"
 }
 
 source $(dirname $0)/utils.inc
