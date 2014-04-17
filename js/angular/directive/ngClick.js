c8304754060ca4fc09771499efc96cf9d7123a09
Adam Bradley <adamdbradley@gmail.com>
2014-04-17 09:20:17 -0500

    bring back $ionicNgClick, but without having to use ionic tap

--- js/angular/directive/ngClick.js
+++ js/angular/directive/ngClick.js
@@ -1,7 +1,37 @@
-// Similar to Angular's ngTouch, however it uses Ionic's tap detection
-// and click simulation. ngClick
-
 IonicModule
+
+.config(['$provide', function($provide) {
+  $provide.decorator('ngClickDirective', ['$delegate', function($delegate) {
+    // drop the default ngClick directive
+    $delegate.shift();
+    return $delegate;
+  }]);
+}])
+
+/**
+ * @private
+ */
+.factory('$ionicNgClick', ['$parse', function($parse) {
+  return function(scope, element, clickExpr) {
+    var clickHandler = $parse(clickExpr);
+
+    element.on('click', function(event) {
+      scope.$apply(function() {
+        clickHandler(scope, {$event: (event)});
+      });
+    });
+
+    // Hack for iOS Safari's benefit. It goes searching for onclick handlers and is liable to click
+    // something else nearby.
+    element.onclick = function(event) { };
+  };
+}])
+
+.directive('ngClick', ['$ionicNgClick', function($ionicNgClick) {
+  return function(scope, element, attr) {
+    $ionicNgClick(scope, element, attr.ngClick);
+  };
+}])
 
 .directive('ionStopEvent', function () {
   function stopEvent(e) {
