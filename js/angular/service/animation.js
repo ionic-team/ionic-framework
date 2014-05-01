a1a16dfb4241167bab273b2e304d3a70d1149f32
Max Lynch <max@drifty.com>
2014-05-01 08:15:48 -0500

    Initial commit

---
+++ js/angular/service/animation.js
@@ -0,0 +1,26 @@
+/**
+ * @ngdoc service
+ * @name $ionicAnimation
+ * @module ionic
+ * @description
+ *
+ * A powerful animation and transition system for Ionic apps.
+ *
+ * @usage
+ *
+ * ```js
+ * angular.module('mySuperApp', ['ionic'])
+ * .controller(function($scope, $ionicAnimation) {
+ * });
+ * ```
+ *
+ */
+IonicModule
+.factory('$ionicAnimation', [
+  '$rootScope',
+  '$document',
+  '$compile',
+  '$timeout',
+  '$interval',
+function($rootScope, $document, $compile, $timeout, $interval) {
+});
