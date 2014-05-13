c3fa63955839a83c2614e1d7503010742c6c1fb6
Khashayar Hajian <me@khashayar.me>
2014-05-13 10:18:14 +0200

    Fixes the doc example
    menu-toggle value should be an angular expression.

--- js/angular/directive/menuToggle.js
+++ js/angular/directive/menuToggle.js
@@ -14,7 +14,7 @@
  * ```html
  * <ion-view>
  *   <ion-nav-buttons side="left">
- *    <button menu-toggle="left" class="button button-icon icon ion-navicon"></button>
+ *    <button menu-toggle="'left'" class="button button-icon icon ion-navicon"></button>
  *   </ion-nav-buttons>
  *  ...
  * </ion-view>
