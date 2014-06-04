6697e66f173ab99b1c9b52d14dd7bfbfe6597bdc
Tim Lancina <tim@drifty.com>
2014-06-04 10:41:46 -0500

    fix keyboard-attach docs

--- js/angular/directive/keyboardAttach.js
+++ js/angular/directive/keyboardAttach.js
@@ -13,7 +13,7 @@
  * - This directive requires the
  * [Ionic Keyboard Plugin](https://github.com/driftyco/ionic-plugins-keyboard).
  * - On Android not in fullscreen mode, i.e. you have
- *   `<preference name="Fullscreen" value="true" />` in your `config.xml` file,
+ *   `<preference name="Fullscreen" value="false" />` or no preference in your `config.xml` file,
  *   this directive is unnecessary since it is the default behavior.
  * - On iOS, if there is an input in your footer, you will need to set
  *   `cordova.plugins.Keyboard.disableScroll(true)`.
