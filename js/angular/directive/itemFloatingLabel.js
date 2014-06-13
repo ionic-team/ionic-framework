731cf726a9906543ca8a6e0f744586f42dd116f0
Adam Bradley <adamdbradley@gmail.com>
2014-06-13 09:36:14 -0500

    test(floatingLabel): snapshot tests

--- js/angular/directive/itemFloatingLabel.js
+++ js/angular/directive/itemFloatingLabel.js
@@ -11,11 +11,9 @@ IonicModule
       if ( !input || !inputLabel ) return;
 
       var onInput = function() {
-        var hasInput = inputLabel.classList.contains('has-input');
-        if ( input.value && !hasInput ) {
+        if ( input.value ) {
           inputLabel.classList.add('has-input');
-        }
-        else if ( !input.value && hasInput ) {
+        } else {
           inputLabel.classList.remove('has-input');
         }
       };
@@ -26,8 +24,6 @@ IonicModule
       if ( ngModelCtrl ) {
         ngModelCtrl.$render = function() {
           input.value = ngModelCtrl.$viewValue || '';
-          if ( ngModelCtrl.$viewValue ) input.value = ngModelCtrl.$viewValue;
-          else input.value = '';
           onInput();
         };
       }
