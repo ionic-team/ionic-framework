b4b94073d5b26698be4a45b9fa4dbeb5a2cdcad4
Andrew Joslin <andytjoslin@gmail.com>
2014-06-17 13:13:35 +0000

    fix(gestureDirectives): fix problem with event being passed in

--- js/angular/directive/gesture.js
+++ js/angular/directive/gesture.js
@@ -239,26 +239,22 @@ function gestureDirective(directiveName) {
   return ['$ionicGesture', '$parse', function($ionicGesture, $parse) {
     var eventType = directiveName.substr(2).toLowerCase();
 
-    return {
-      restrict: 'A',
-      compile: function($element, attr) {
-        var fn = $parse( attr[directiveName] );
+    return function(scope, element, attr) {
+      var fn = $parse( attr[directiveName] );
 
-        return function(scope, element, attr) {
-
-          var listener = function(ev) {
-            scope.$apply(function() {
-              fn(scope, {$event:event});
-            });
-          };
-
-          var gesture = $ionicGesture.on(eventType, listener, $element);
-
-          scope.$on('$destroy', function() {
-            $ionicGesture.off(gesture, eventType, listener);
+      var listener = function(ev) {
+        scope.$apply(function() {
+          fn(scope, {
+            $event: ev
           });
-        };
-      }
+        });
+      };
+
+      var gesture = $ionicGesture.on(eventType, listener, element);
+
+      scope.$on('$destroy', function() {
+        $ionicGesture.off(gesture, eventType, listener);
+      });
     };
   }];
 }
