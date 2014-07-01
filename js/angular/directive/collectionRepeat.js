1b51231e8f991ca103d8c6d4f0390a47d04f72f0
Perry Govier <phoenix_fire_7@hotmail.com>
2014-07-01 10:12:57 -0500

    docs(collectionRepeat): Noting that the dimensions must be known ahead of item creation. Closes #1707

--- js/angular/directive/collectionRepeat.js
+++ js/angular/directive/collectionRepeat.js
@@ -88,7 +88,7 @@
  *   </div>
  * </ion-content>
  * ```
- * Percentage of total visible list dimensions. This example shows a 3 by 3 matrix that fits on the screen (3 rows and 3 colums)
+ * Percentage of total visible list dimensions. This example shows a 3 by 3 matrix that fits on the screen (3 rows and 3 colums). Note that dimensions are used in the creation of the element and therefore a measurement of the item cannnot be used as an input dimension. 
  * ```css
  * .my-image-item img {
  *   height: 33%;
