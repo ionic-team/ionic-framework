3dea57d8fb45c36442897fee5177bbde5460d4b1
Andrew <andytjoslin@gmail.com>
2014-07-10 15:42:55 -0600

    docs(navBackButton): clarify custom back button action

--- js/angular/directive/navBackButton.js
+++ js/angular/directive/navBackButton.js
@@ -29,7 +29,7 @@
  * ```html
  * <ion-nav-bar ng-controller="MyCtrl">
  *   <ion-nav-back-button class="button-clear"
- *     ng-click="canGoBack && goBack()">
+ *     ng-click="doSomethingCool()">
  *     <i class="ion-arrow-left-c"></i> Back
  *   </ion-nav-back-button>
  * </ion-nav-bar>
