e40873f46509dfd9ebdbf7f79b4d2468b6f8c2e3
Mauro Bianchi <bianchimro@gmail.com>
2014-08-21 23:42:07 +0200

    fixed class name in setButtonShown method, ionList directive

--- js/angular/directive/list.js
+++ js/angular/directive/list.js
@@ -168,7 +168,7 @@ function($animate, $timeout) {
           function setButtonShown(el, shown) {
             shown() && el.addClass('visible') || el.removeClass('active');
             ionic.requestAnimationFrame(function() {
-              shown() && el.addClass('active') || el.removeClass('invisible');
+              shown() && el.addClass('active') || el.removeClass('visible');
             });
           }
         }
