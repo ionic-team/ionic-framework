087db6ed66f905bfd3e6f710106c51ba0f180e99
Casey Garland <caseyg1204@gmail.com>
2014-09-03 09:50:10 -0400

    add expect statements for default modal values

--- test/unit/views/modalView.unit.js
+++ test/unit/views/modalView.unit.js
@@ -1,13 +1,19 @@
 describe('Modal View', function() {
   var h;
   beforeEach(function() {
-    h = document.createElement('modal');
-    h.innerHTML = '<a class="button">Click</a><h1 class="title">What what what</h1>';
+    m = document.createElement('modal');
+    m.innerHTML = '<a class="button">Click</a><h1 class="title">What what what</h1>';
   });
 
   it('Should init', function() {
-    var bar = new ionic.views.Modal({
-      el: h
+    var mod = new ionic.views.Modal({
+      el: m
     });
+    expect(mod.focusFirstInput).toBe(false);
+    expect(mod.unfocusOnHide).toBe(true);
+    expect(mod.focusFirstDelay).toBe(600);
+    expect(mod.backdropClickToClose).toBe(true);
+    expect(mod.hardwareBackButtonClose).toBe(true);
+
   });
 });
