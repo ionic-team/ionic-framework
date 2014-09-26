f0af0858413b90c3f34649a9c8089bd4b7fd7ef5
Adam Bradley <adambradley25@gmail.com>
2014-09-26 08:15:21 -0500

    refactor(clickBlock): ensure click block fallback cancels

--- js/angular/service/clickBlock.js
+++ js/angular/service/clickBlock.js
@@ -4,21 +4,33 @@ IonicModule
   '$ionicBody',
   '$timeout',
 function($document, $ionicBody, $timeout) {
+  var fallbackTimer;
+
   var cb = $document[0].createElement('div');
   cb.className = 'click-block';
+
   return {
     show: function() {
+      // cancel the fallback timer
+      $timeout.cancel( fallbackTimer );
+
       if(cb.parentElement) {
         cb.classList.remove('hide');
       } else {
         $ionicBody.append(cb);
       }
-      $timeout(function(){
+      fallbackTimer = $timeout(function(){
         cb.classList.add('hide');
       }, 500);
     },
     hide: function() {
-      cb.classList.add('hide');
+      // cancel the fallback timer
+      $timeout.cancel( fallbackTimer );
+
+      // should be a minimum time it should hide
+      ionic.requestAnimationFrame(function(){
+        cb.classList.add('hide');
+      });
     }
   };
 }]);
