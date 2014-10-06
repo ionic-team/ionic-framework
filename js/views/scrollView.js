84f18a1c6e1255e2ab9eff1f7d6fb92bac924391
Perry Govier <govierp@gmail.com>
2014-10-06 14:56:35 -0500

    fix(content): don't resize content on keyboard open if it's inside a modal. Fixes #2212

--- js/views/scrollView.js
+++ js/views/scrollView.js
@@ -629,7 +629,8 @@ ionic.views.Scroll = ionic.views.View.inherit({
       if( !self.isScrolledIntoView ) {
         // shrink scrollview so we can actually scroll if the input is hidden
         // if it isn't shrink so we can scroll to inputs under the keyboard
-        if (ionic.Platform.isIOS() || ionic.Platform.isFullScreen){
+        if((ionic.Platform.isIOS() || ionic.Platform.isFullScreen) && !container.parentNode.classList.contains('modal')){
+
           // if there are things below the scroll view account for them and
           // subtract them from the keyboard height when resizing
           scrollBottomOffsetToTop = container.getBoundingClientRect().bottom;
