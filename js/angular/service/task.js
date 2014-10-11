32962fa76f9a3ff432076a82dd91032dfecc331c
Max Lynch <max@drifty.com>
2014-10-11 16:44:40 -0700

    More refined oeprations

--- js/angular/service/task.js
+++ js/angular/service/task.js
@@ -7,14 +7,16 @@
  */
 IonicModule
 .factory('$ionicTask', ['$window', '$q', function($window, $q) {
-  return function(scriptPath, onMessage) {
+  return function(scriptPath, onMessage, onError, onComplete) {
     var worker = new window.Worker(scriptPath);
     var q = $q.defer();
 
     q.promise.then(function(resolveMessage) {
+      onComplete && onComplete(resolveMessage);
     }, function(err) {
+      onError && onError(err);
     }, function(msg) {
-      onMessage(msg);
+      onMessage && onMessage(msg);
     });
 
     worker.addEventListener('message', function(e) {
@@ -25,6 +27,22 @@ IonicModule
       post: function(data) {
         worker.postMessage(data);
         return q.promise;
+      },
+
+      /**
+       * Post a close message to the worker thread. The worker
+       * thread must understand this message and call self.close()
+       *
+       * @param msg the custom message to send, otherwise the message { 'op': 'close' } will be sent.
+       */
+      close: function(data) {
+        worker.postMessage(data || {
+          'op': 'close'
+        });
+        q.resolve();
+      },
+      terminate: function() {
+        worker.terminate();
       }
     }
   }
