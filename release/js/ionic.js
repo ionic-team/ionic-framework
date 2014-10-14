34097188f442094744ed99beac279e4ee3b7c6d0
Mirko Menegazzo <mirko.menegazzo@gmail.com>
2014-10-14 14:27:09 +0100

    Removed changes from ionic.js

--- release/js/ionic.js
+++ release/js/ionic.js
@@ -2958,11 +2958,7 @@ ionic.DomUtil.ready(function(){
 
           // in XX milliseconds, set the queued elements to active
           if(e.type === 'touchstart') {
-            if (ele.hasAttribute('data-instantActivate')) {
-			 self._activateTimeout = setTimeout(activateElements, 1);
-			} else {
 			 self._activateTimeout = setTimeout(activateElements, 80);
-			}
           } else {
             ionic.requestAnimationFrame(activateElements);
           }
