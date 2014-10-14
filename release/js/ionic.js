86110845939b2a5fe24dde355b4db22a0b46b330
Mirko Menegazzo <mirko.menegazzo@gmail.com>
2014-10-14 10:58:36 +0100

    Added custom attribute data-instantActivate to remove 80ms activation delay when an element is touched (down to 1ms)

--- release/js/ionic.js
+++ release/js/ionic.js
@@ -2958,7 +2958,11 @@ ionic.DomUtil.ready(function(){
 
           // in XX milliseconds, set the queued elements to active
           if(e.type === 'touchstart') {
-            self._activateTimeout = setTimeout(activateElements, 80);
+            if (ele.hasAttribute('data-instantActivate')) {
+			 self._activateTimeout = setTimeout(activateElements, 1);
+			} else {
+			 self._activateTimeout = setTimeout(activateElements, 80);
+			}
           } else {
             ionic.requestAnimationFrame(activateElements);
           }
