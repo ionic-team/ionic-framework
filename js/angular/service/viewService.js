be28ac8f908ff52ca60a004649d13b142e20ae6e
Adam Bradley <adambradley25@gmail.com>
2014-11-16 22:14:15 -0600

    chore($ionicViewService): deprecate

---
+++ js/angular/service/viewService.js
@@ -0,0 +1,32 @@
+/**
+ * @private
+ * DEPRECATED
+ */
+IonicModule
+.factory('$ionicViewService', ['$ionicHistory', '$log', function($ionicHistory, $log) {
+
+  function warn(oldMethod, newMethod) {
+    $log.warn('$ionicViewService' + oldMethod + ' is deprecated, please use $ionicHistory' + newMethod + ' instead: http://ionicframework.com/docs/nightly/api/service/$ionicHistory/');
+  }
+
+  warn('', '');
+
+  var methodsMap = {
+    getCurrentView: 'currentView',
+    getBackView: 'backView',
+    getForwardView: 'forwardView',
+    getCurrentStateName: 'currentStateName',
+    nextViewOptions: 'nextViewOptions',
+    clearHistory: 'clearHistory'
+  };
+
+  forEach(methodsMap, function(newMethod, oldMethod){
+    methodsMap[oldMethod] = function() {
+      warn('.' + oldMethod, '.' + newMethod);
+      return $ionicHistory[newMethod].apply(this, arguments);
+    };
+  });
+
+  return methodsMap;
+
+}]);
