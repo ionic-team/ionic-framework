422ac6acff9a15d68339c26500cb88742d9d2ef7
Thiery Laverdure <thiery@uptrending.com>
2014-11-19 22:30:02 -0500

    Update menuToggle.js
    Matched parent element to description in usage.

--- js/angular/directive/menuToggle.js
+++ js/angular/directive/menuToggle.js
@@ -12,12 +12,11 @@
  * would open the given side menu, and tapping it again would close it.
  *
  * ```html
- * <ion-view>
+ * <ion-nav-bar>
  *   <ion-nav-buttons side="left">
  *    <button menu-toggle="left" class="button button-icon icon ion-navicon"></button>
  *   </ion-nav-buttons>
- *  ...
- * </ion-view>
+ * </ion-nav-bar>
  * ```
  */
 IonicModule
