096784984f312126a4546217ec4f140d6afd6aa9
Adam Bradley <adambradley25@gmail.com>
2014-12-03 08:03:30 -0600

    fix(clickBlock): cancel pending show if already hidden

--- js/angular/service/clickBlock.js
+++ js/angular/service/clickBlock.js
@@ -6,29 +6,32 @@ IonicModule
 function($document, $ionicBody, $timeout) {
   var fallbackTimer, isAttached;
   var CSS_HIDE = 'click-block-hide';
+  var pendingShow;
 
   var cb = $document[0].createElement('div');
   cb.className = 'click-block';
 
   return {
     show: function(autoExpire) {
+      pendingShow = true;
       // cancel the fallback timer
       $timeout.cancel(fallbackTimer);
 
       ionic.requestAnimationFrame(function() {
-        if (isAttached) {
-          cb.classList.remove(CSS_HIDE);
-        } else {
-          $ionicBody.append(cb);
-          isAttached = true;
+        if (pendingShow) {
+          if (isAttached) {
+            cb.classList.remove(CSS_HIDE);
+          } else {
+            $ionicBody.append(cb);
+            isAttached = true;
+          }
         }
       });
 
-      fallbackTimer = $timeout(function() {
-        cb.classList.add(CSS_HIDE);
-      }, autoExpire || 300);
+      fallbackTimer = $timeout(this.hide, autoExpire || 310);
     },
     hide: function() {
+      pendingShow = false;
       $timeout.cancel(fallbackTimer);
       cb.classList.add(CSS_HIDE);
     }
