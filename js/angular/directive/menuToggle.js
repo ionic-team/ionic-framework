a7a21fb939634bbfff8c591ac8561ac2463d4e70
Perry Govier <perrygovier@users.noreply.github.com>
2014-12-17 14:11:42 -0600

    docs(menuToggle): reorder paragraphs

--- js/angular/directive/menuToggle.js
+++ js/angular/directive/menuToggle.js
@@ -6,16 +6,6 @@
  *
  * @description
  * Toggle a side menu on the given side.
- * 
- * *Note*: By default, the menu toggle button will only appear on a root
- * level side-menu page. Navigating in to child views will hide the menu-
- * toggle button. They can be made visible on child pages by setting the
- * enable-menu-with-back-views attribute of the {@link ionic.directive:ionSideMenus} 
- * directive to true. 
- * 
- * ```html
- * <ion-side-menus enable-menu-with-back-views="true">
- * ```
  *
  * @usage
  * Below is an example of a link within a nav bar. Tapping this button
@@ -27,6 +17,16 @@
  *    <button menu-toggle="left" class="button button-icon icon ion-navicon"></button>
  *   </ion-nav-buttons>
  * </ion-nav-bar>
+ * ```
+ * 
+ *  **Note**: By default, the menu toggle button will only appear on a root
+ * level side-menu page. Navigating in to child views will hide the menu-
+ * toggle button. They can be made visible on child pages by setting the
+ * enable-menu-with-back-views attribute of the {@link ionic.directive:ionSideMenus} 
+ * directive to true. 
+ * 
+ * ```html
+ * <ion-side-menus enable-menu-with-back-views="true">
  * ```
  */
 IonicModule
