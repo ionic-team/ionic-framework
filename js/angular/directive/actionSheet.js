61a8353c76f69a0aef976c4ec49373eae046b1aa
Adam Bradley <adambradley25@gmail.com>
2014-12-31 13:38:17 -0600

    refactor(actionsheet): improve styles/layout

--- js/angular/directive/actionSheet.js
+++ js/angular/directive/actionSheet.js
@@ -33,14 +33,12 @@ IonicModule
     template: '<div class="action-sheet-backdrop">' +
                 '<div class="action-sheet-wrapper">' +
                   '<div class="action-sheet">' +
-                    '<div class="action-sheet-group">' +
+                    '<div class="action-sheet-group action-sheet-options">' +
                       '<div class="action-sheet-title" ng-if="titleText" ng-bind-html="titleText"></div>' +
-                      '<button class="button" ng-click="buttonClicked($index)" ng-repeat="button in buttons" ng-bind-html="button.text"></button>' +
+                      '<button class="button action-sheet-option" ng-click="buttonClicked($index)" ng-repeat="button in buttons" ng-bind-html="button.text"></button>' +
+                      '<button class="button destructive action-sheet-destructive" ng-if="destructiveText" ng-click="destructiveButtonClicked()" ng-bind-html="destructiveText"></button>' +
                     '</div>' +
-                    '<div class="action-sheet-group" ng-if="destructiveText">' +
-                      '<button class="button destructive" ng-click="destructiveButtonClicked()" ng-bind-html="destructiveText"></button>' +
-                    '</div>' +
-                    '<div class="action-sheet-group" ng-if="cancelText">' +
+                    '<div class="action-sheet-group action-sheet-cancel" ng-if="cancelText">' +
                       '<button class="button" ng-click="cancel()" ng-bind-html="cancelText"></button>' +
                     '</div>' +
                   '</div>' +
