ee48cbdbbf5a15a8ce44683fff73ac8388d48a15
Stian Jensen <me@stianj.com>
2015-01-20 00:16:39 +0100

    Bind collection-repeat resize on view enter/leave
    Fixes driftyco/ionic#2935

--- js/angular/directive/collectionRepeat.js
+++ js/angular/directive/collectionRepeat.js
@@ -261,7 +261,13 @@ function($collectionRepeatManager, $collectionDataSource, $parse) {
       }
 
       scrollCtrl.$element.on('scroll.resize', rerenderOnResize);
-      ionic.on('resize', rerenderOnResize, window);
+      $scope.$on('$ionicView.enter', function() {
+        ionic.on('resize', rerenderOnResize, window);
+      });
+      $scope.$on('$ionicView.leave', function() {
+        ionic.off('resize', rerenderOnResize, window);
+      });
+
       var deregisterViewListener;
       if (navViewCtrl) {
         deregisterViewListener = navViewCtrl.scope.$on('$ionicView.afterEnter', viewEnter);
@@ -270,7 +276,6 @@ function($collectionRepeatManager, $collectionDataSource, $parse) {
       $scope.$on('$destroy', function() {
         collectionRepeatManager.destroy();
         dataSource.destroy();
-        ionic.off('resize', rerenderOnResize, window);
         (deregisterViewListener || angular.noop)();
       });
     }
