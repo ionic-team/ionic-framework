ea912e80f0eef33474b1224d5a37e8cda1c5d8c0
Andrew <andrewtjoslin@gmail.com>
2015-02-09 09:43:11 -0700

    docs(ionSlideBox): document how show-pager attribute can be dynamic

--- js/angular/directive/slideBox.js
+++ js/angular/directive/slideBox.js
@@ -30,7 +30,7 @@
  * @param {boolean=} does-continue Whether the slide box should loop.
  * @param {boolean=} auto-play Whether the slide box should automatically slide. Default true if does-continue is true.
  * @param {number=} slide-interval How many milliseconds to wait to change slides (if does-continue is true). Defaults to 4000.
- * @param {boolean=} show-pager Whether a pager should be shown for this slide box.
+ * @param {boolean=} show-pager Whether a pager should be shown for this slide box. Accepts expressions via `show-pager="{{shouldShow()}}"`.
  * @param {expression=} pager-click Expression to call when a pager is clicked (if show-pager is true). Is passed the 'index' variable.
  * @param {expression=} on-slide-changed Expression called whenever the slide is changed.  Is passed an '$index' variable.
  * @param {expression=} active-slide Model to bind the current slide to.
