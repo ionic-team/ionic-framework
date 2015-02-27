c6d70315c1725def25861a27e404d66493c7eeb7
Joshua Hutt <joshua@huttj.com>
2015-02-27 11:58:35 -0800

    docs($ionicPlatform) List back button priorities
    Added the list of build-in hooks on the hardware back button and explained the connection between the existing actions and actions registered through `$ionicPlatform.registerBackButtonAction`. Also added an example describing how to selectively override existing actions.

--- js/angular/service/platform.js
+++ js/angular/service/platform.js
@@ -61,6 +61,19 @@ IonicModule
          * close the actionsheet, but it should not also go back a page view
          * or close a modal which may be open.
          *
+         * The priorities for the existing back button hooks are as follows:
+         *   Return to previous view = 100
+         *   Close side menu = 150
+         *   Dismiss modal = 200
+         *   Close action sheet = 300
+         *   Dismiss popup = 400
+         *   Dismiss loading overlay = 500
+         * 
+         * Your back button action will override each of the above actions
+         * whose priority is less than the priority you provide. For example,
+         * an action assigned a priority of 101 will override the 'return to
+         * previous view' action, but not any of the other actions.
+         * 
          * @param {function} callback Called when the back button is pressed,
          * if this listener is the highest priority.
          * @param {number} priority Only the highest priority will execute.
