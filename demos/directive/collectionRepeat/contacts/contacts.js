580a9558eb1eb197cc9400e9ac7493101e3bd525
Andrew <andrewtjoslin@gmail.com>
2015-03-05 09:20:16 -0700

    demo(collectionRepeat): update ng-app name

--- demos/directive/collectionRepeat/contacts/contacts.js
+++ demos/directive/collectionRepeat/contacts/contacts.js
@@ -3,7 +3,7 @@ name: contacts
 component: collectionRepeat
 ---
 
-angular.module('contactsApp', ['ionic'])
+angular.module('contacts', ['ionic'])
 
 .filter('ionLetterDividers', function($parse) {
   // Reuse letter objects for angular digest, so it can assign an id
