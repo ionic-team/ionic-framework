f33967c4f5474d87bcc1d45480edfce674746204
Andrew <andrewtjoslin@gmail.com>
2015-03-13 10:42:55 -0600

    amend(collectionRepeat): resize if width or height changes

--- js/angular/directive/collectionRepeat.js
+++ js/angular/directive/collectionRepeat.js
@@ -187,16 +187,19 @@ function CollectionRepeatDirective($ionicCollectionManager, $parse, $window, $$r
         dataLength: 0,
         width: 0,
         height: 0,
+        // A resize triggers a refresh only if we have data, the scrollView has size,
+        // and the size has changed.
         resizeRequiresRefresh: function(newWidth, newHeight) {
-          var requiresRefresh = self.dataLength &&
-            newWidth && newWidth !== self.width &&
-            newHeight && newHeight !== self.height;
+          var requiresRefresh = self.dataLength && newWidth && newHeight &&
+            (newWidth !== self.width || newHeight !== self.height);
 
           self.width = newWidth;
           self.height = newHeight;
 
           return !!requiresRefresh;
         },
+        // A change in data only triggers a refresh if the data has length, or if the data's
+        // length is less than before.
         dataChangeRequiresRefresh: function(newData) {
           var requiresRefresh = newData.length > 0 || newData.length < self.dataLength;
 
