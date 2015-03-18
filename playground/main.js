47cb9c877fa579826f4c591da7aad98d452d8a73
Andrew <andrewtjoslin@gmail.com>
2015-03-17 19:27:14 -0500

    app(): you don't need ./ionic anymore, just ionic

--- playground/main.js
+++ playground/main.js
@@ -1,6 +1,6 @@
 import {bootstrap} from 'angular2/core';
 import {Component, Template} from 'angular2/angular2';
-import {Tabbar} from './ionic/components/tabbar/tabbar';
+import {Tabbar} from 'ionic/components/tabbar/tabbar';
 
 @Component({ selector: 'playground-main' })
 @Template({ 
