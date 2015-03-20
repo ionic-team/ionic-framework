aa5b1535a84206bcb80d72289a35419d1ba2158b
Adam Bradley <adambradley25@gmail.com>
2015-03-20 12:55:08 -0500

    codes

--- src/components/view/view.js
+++ src/components/view/view.js
@@ -9,17 +9,30 @@ import {Ion} from '../ion';
 })
 @Template({
   inline: `
-    <div class="container">
-      <div class="bar">
-        <div class="bar-accessories">
-          <div class="bar-accessory accessory-primary" style="background:red">
+      <div class="bar bar-android2">
+        <div class="bar-items">
+          <div class="back-button">
+            <div class="back-button-icon">&lt;</div>
+            <div class="back-button-text">
+              <div class="back-default">Back</div>
+              <div class="back-title"></div>
+            </div>
+          </div>
+          <div class="title">
+            <div class="inner-title">
+              Create hybrid mobile apps with the web technologies you love. Free and open source, Ionic offers a library of mobile-optimized HTML, CSS and JS components, gestures, and tools for building highly interactive apps. Built with Sass and optimized for AngularJS.
+            </div>
+          </div>
+          <div class="bar-primary-item" style="background:red">
             PRIMARY
           </div>
-          <div class="bar-accessory accessory-secondary" style="background:green">
+          <div class="spacer"></div>
+          <div class="bar-secondary-item" style="background:green">
             SECONDARY
           </div>
         </div>
       </div>
+    <div class="container">
       <content></content>
     </div>`
 })
