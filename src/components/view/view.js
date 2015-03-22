c142885145a3d84493449e94c5569ba0bed2aeed
Adam Bradley <adambradley25@gmail.com>
2015-03-21 20:57:49 -0500

    view

---
+++ src/components/view/view.js
@@ -0,0 +1,24 @@
+import {NgElement, Component, Template} from 'angular2/angular2'
+import {ToolBar} from '../toolbar/toolbar'
+import {Ion} from '../ion'
+
+@Component({
+  selector: 'ion-view',
+  bind: {
+    title: 'view-title'
+  }
+})
+@Template({
+  inline: `
+    <ion-toolbar>
+    </ion-toolbar>
+    <div class="container">
+      <content></content>
+    </div>`,
+  directives: [ToolBar]
+})
+export class View extends Ion {
+  constructor(@NgElement() ele:NgElement) {
+    ele.domElement.classList.add('view')
+  }
+}
