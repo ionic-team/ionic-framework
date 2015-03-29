fa07c5175bd613ce36c654120ce74880bf53e942
Max Lynch <ihasmax@gmail.com>
2015-03-29 10:58:20 -0600

    logging util

--- src/util.js
+++ src/util.js
@@ -46,3 +46,21 @@ export let array = {
     });
   }
 };
+
+export class Log {
+  static log(...args) {
+    console.log.apply(console, args)
+  }
+  static info(...args) {
+    console.info.apply(console, args)
+  }
+  static dir(...args) {
+    console.dir.apply(console, args)
+  }
+  static warn(...args) {
+    console.warn.apply(console, args)
+  }
+  static error(...args) {
+    console.error.apply(console, args)
+  }
+}
