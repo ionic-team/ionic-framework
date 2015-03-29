cfb09b5ca6c4b3b4d86481227782d1f72772fb05
Max Lynch <max@drifty.com>
2015-03-29 09:09:15 -0600

    Create router.js

---
+++ mockup/router.js
@@ -0,0 +1,6 @@
+/**
+ * This is a mockup of how the new router might work.
+ *
+ * The goal of this is to help shape the new Angular router, or
+ * figure out if we need to do our own implementation.
+ */
