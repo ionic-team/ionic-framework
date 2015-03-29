3f5030b31a5bc10524a60ca1b539253dfee417e2
Max Lynch <max@drifty.com>
2015-03-29 09:16:07 -0600

    Update router.js

--- mockup/router.js
+++ mockup/router.js
@@ -3,6 +3,17 @@
  *
  * The goal of this is to help shape the new Angular router, or
  * figure out if we need to do our own implementation.
+   
+   Some design decisions we need to figure out and agree on:
+    
+    * Deep linking. In v1 we did a complicated nested view linking, where
+      a route with a URL like /contacts/name would navigate in each sub 
+      view, reconstructing a whole navigation context.
+      
+      I'm not convinced this is the right approach for v2. Instead, we might
+      want to make it more explicit, where certain URLs can trigger a handler
+      where navigation can be reconstructed by the developer, but with the
+      default behavior of routes being navigated to in the root nav.
  */
 
 // Decorators
