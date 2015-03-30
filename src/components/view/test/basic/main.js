33b43ee3407ab8a1b4904e7553826a26a92007cf
Adam Bradley <adambradley25@gmail.com>
2015-03-30 15:19:33 -0500

    blank e2e tests

--- src/components/view/test/basic/main.js
+++ src/components/view/test/basic/main.js
@@ -1,38 +1,17 @@
-import {Component, Template, bootstrap} from 'angular2/angular2';
+import {bootstrap} from 'angular2/core';
+import {Component, Template} from 'angular2/angular2';
 import {View} from 'ionic2/components/view/view';
 import {Content} from 'ionic2/components/content/content';
-import {ToolBar} from 'ionic2/components/toolbar/toolbar';
 
-@Component({
-  selector: '[ion-app]' 
-})
+@Component({ selector: '[ion-app]' })
 @Template({
-  inline: `
-<ion-view view-title="!attr Title!">
-
-  <ion-view-title>
-    !ele Title!
-  </ion-view-title>
-
-  <ion-nav-items side="primary">
-    <button>p1</button>
-    <button>p2</button>
-    <button>p3</button>
-    <button>p4</button>
-  </ion-nav-items>
-
-  <ion-nav-items side="secondary">
-    <button>s1</button>
-    <button>s2</button>
-  </ion-nav-items>
-
-  <ion-content>
-    CONTENT!!
-  </ion-content>
-
-</ion-view>
-  `,
-  directives: [View, Content, ToolBar]
+  url: 'main.html',
+  directives: [View, Content]
 })
-class MyApp {}
-bootstrap(MyApp);
+class IonicApp {
+  constructor() {
+    console.log('IonicApp Start')
+  }
+}
+
+bootstrap(IonicApp)
