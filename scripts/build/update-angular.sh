d1be210f9f0b87ab988f21dc0be9d17c00fd73c2
Andrew <andrewtjoslin@gmail.com>
2015-03-30 12:12:13 -0600

    chore(build): make update-angular script actually update angular

--- scripts/build/update-angular.sh
+++ scripts/build/update-angular.sh
@@ -7,6 +7,9 @@ cd $(dirname $0)/../../
 $(test -e $NG_FOLDER) || (git clone git@github.com:angular/angular $NG_FOLDER)
 
 cd $NG_FOLDER
+echo "Pulling from master.."
+git pull origin master
+
 echo "Running npm install in angular2..."
 npm install
 
--- src/components/nav-view/test/basic/main.js
+++ src/components/nav-view/test/basic/main.js
@@ -1,19 +1,40 @@
-import {bootstrap} from 'angular2/core';
-import {Component, Template, NgElement, Parent} from 'angular2/angular2';
-import {NavView} from 'ionic2/components/nav-view/nav-view';
-import {View} from 'ionic2/components/view/view';
-import {Log} from 'ionic2/util';
+import {bootstrap} from 'angular2/core'
+import {Component, Template, NgElement, Parent} from 'angular2/angular2'
+import {TemplateResolver} from 'angular2/src/core/compiler/template_resolver'
+import {Compiler} from 'angular2/src/core/compiler/compiler'
+import {ElementInjector} from 'angular2/src/core/compiler/element_injector'
+import {EventManager} from 'angular2/src/core/events/event_manager'
+import {NavView} from 'ionic2/components/nav-view/nav-view'
+import {View} from 'ionic2/components/view/view'
+import {Log} from 'ionic2/util'
+
+@Component({
+  selector: 'my-page'
+})
+@Template({
+  inline: 'Hello, inside page!'
+})
+export class InsidePage {
+}
 
 @Component({
   selector: 'my-page',
+  services: [TemplateResolver, Compiler]
 })
 @Template({
   inline: "<div><h2>I'm a page!</h2><content></content></div>"
 })
 export class MyPage extends View {
-  constructor(@NgElement() ele:NgElement, @Parent() nav: NavView) {
-    Log.log('Page constructed!', ele, nav)
-    super(ele, nav)
+  constructor(
+    @NgElement() ele:NgElement,
+    compiler: Compiler,
+    hostElementInjector: ElementInjector,
+    eventManager: EventManager
+  ) {
+    super(ele)
+    compiler.compile(InsidePage).then(function(protoView) {
+      debugger;
+    })
   }
 }
 
