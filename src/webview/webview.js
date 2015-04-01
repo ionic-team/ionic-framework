2614138c613bcdc603b9b373363c5a4de45636ea
Adam Bradley <adambradley25@gmail.com>
2015-04-01 11:15:12 -0500

    stub methods

---
+++ src/webview/webview.js
@@ -0,0 +1,22 @@
+
+// platformReady: Cordova said it's ready
+
+// domReady: DOM is ready
+
+// ready: if cordova, it's the same as platformReady, if browser, same as dom ready
+
+// windowLoad: All scripts have been loaded, window ready
+
+// fullScreen
+
+// showStatusBar
+
+// exitApp
+
+// is: is('ios') type of check
+
+// platformName:  returns ios
+
+// platformVersion: returns 8.2
+
+// webview: returns cordova, trigger.io, browser
