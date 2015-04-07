83ec010d2fb23f3576a36f1cdd4c179015e4779e
Adam Bradley <adambradley25@gmail.com>
2015-04-07 10:04:49 -0500

    .tab-active

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -12,8 +12,8 @@ import {IonicComponent} from 'ionic2/config/component'
     <div class="toolbar tab-bar toolbar-ios toolbar-bottom">
       <div class="tab-bar-container">
         <a *for="#tab of tabs"
-          class="tab-bar-item tab-active"
-          [class.active]="tab.isSelected"
+          class="tab-bar-item"
+          [class.tab-active]="tab.isSelected"
           (^click)="onClickTabItem($event, tab)">
             <icon class="tab-bar-item-icon ion-home"></icon>
             <span class="tab-bar-item-text">{{tab.title}}</span>
