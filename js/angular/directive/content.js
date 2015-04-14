10552634d973959f685abb54c18b03a546ff11cf
Andrew <andrewtjoslin@gmail.com>
2015-04-14 10:53:27 -0600

    fix(content): make on-scroll-complete pass (scrollLeft, scrollTop) locals
    Closes #2464.

--- js/angular/directive/content.js
+++ js/angular/directive/content.js
@@ -35,7 +35,7 @@
  * @param {string=} start-x Initial horizontal scroll position. Default 0.
  * @param {string=} start-y Initial vertical scroll position. Default 0.
  * @param {expression=} on-scroll Expression to evaluate when the content is scrolled.
- * @param {expression=} on-scroll-complete Expression to evaluate when a scroll action completes.
+ * @param {expression=} on-scroll-complete Expression to evaluate when a scroll action completes. Has access to 'scrollLeft' and 'scrollTop' locals.
  * @param {boolean=} has-bouncing Whether to allow scrolling to bounce past the edges
  * of the content.  Defaults to true on iOS, false on Android.
  * @param {number=} scroll-event-interval Number of milliseconds between each firing of the 'on-scroll' expression. Default 10.
@@ -138,20 +138,22 @@ function($timeout, $controller, $ionicBind, $ionicConfig) {
               scrollingX: $scope.direction.indexOf('x') >= 0,
               scrollingY: $scope.direction.indexOf('y') >= 0,
               scrollEventInterval: parseInt($scope.scrollEventInterval, 10) || 10,
-              scrollingComplete: function() {
-                $scope.$onScrollComplete({
-                  scrollTop: this.__scrollTop,
-                  scrollLeft: this.__scrollLeft
-                });
-              }
+              scrollingComplete: onScrollComplete
             };
           }
 
           // init scroll controller with appropriate options
-          $controller('$ionicScroll', {
+          var scrollCtrl = $controller('$ionicScroll', {
             $scope: $scope,
             scrollViewOptions: scrollViewOptions
           });
+
+          function onScrollComplete() {
+            $scope.$onScrollComplete({
+              scrollTop: scrollCtrl.scrollView.__scrollTop,
+              scrollLeft: scrollCtrl.scrollView.__scrollLeft
+            });
+          }
 
           $scope.$on('$destroy', function() {
             if (scrollViewOptions) {
