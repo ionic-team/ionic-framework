19296c853d9b2dae159586fc28ce50da97b61e70
perry <perry@drifty.com>
2015-04-23 15:57:43 -0500

    fix(navigation): Whitelist Cordova Windows Phone style protocols

--- js/angular/service/ionicConfig.js
+++ js/angular/service/ionicConfig.js
@@ -614,4 +614,11 @@ IonicModule
   provider.$get = function() {
     return provider;
   };
+})
+// Fix for URLs in Cordova apps on Windows Phone
+// http://blogs.msdn.com/b/msdn_answers/archive/2015/02/10/
+// running-cordova-apps-on-windows-and-windows-phone-8-1-using-ionic-angularjs-and-other-frameworks.aspx
+.config(function($compileProvider) {
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|ghttps?|ms-appx|x-wmapp0):/);z
+  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|ms-appx|x-wmapp0):|data:image\//);
 });
