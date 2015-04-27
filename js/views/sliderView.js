08956b29b0ccf93203681a83f6ae6c5c9c28375f
perry <perry@drifty.com>
2015-04-27 12:40:56 -0500

    fix(slidebox): prevent read only property error on assigning of length. Closes #3589

--- js/views/sliderView.js
+++ js/views/sliderView.js
@@ -551,7 +551,7 @@ ionic.views.Slider = ionic.views.View.inherit({
       element.style.left = '';
 
       // reset slides so no refs are held on to
-      slides && (slides.length = 0);
+      slides && (slides.length = []);
 
       // removed event listeners
       if (browser.addEventListener) {
