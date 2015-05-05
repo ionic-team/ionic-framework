296985976d1f440af04acc88b34eca3c57d34b56
Max Lynch <max@drifty.com>
2015-05-05 11:27:33 -0500

    Form basic test

---
+++ ionic/components/form/test/basic/main.js
@@ -0,0 +1,20 @@
+import {Component, View, bootstrap} from 'angular2/angular2'
+import {FormBuilder, Validators, FormDirectives, ControlGroup} from 'angular2/forms';
+import {Button, Form, Input, Content} from 'ionic/ionic';
+
+@Component({ selector: '[ion-app]' })
+@View({
+  templateUrl: 'main.html',
+  directives: [FormDirectives, Button, Input, Content]
+})
+class IonicApp {
+  constructor() {
+    var fb = new FormBuilder();
+    this.form = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+}
+
+bootstrap(IonicApp)
