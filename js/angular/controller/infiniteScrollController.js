7d8ceb2bc42f873b1d15c95307c3bc868417d5a8
Javier Castro <javiercastro@nexion.com.ar>
2015-05-05 11:08:11 -0300

    fix(scrolling): throws exceptions in some use cases (#3574)

--- js/angular/controller/infiniteScrollController.js
+++ js/angular/controller/infiniteScrollController.js
@@ -43,10 +43,12 @@ function($scope, $attrs, $element, $timeout) {
       $element[0].classList.remove('active');
     });
     $timeout(function() {
-      if (self.jsScrolling) self.scrollView.resize();
-      // only check bounds again immediately if the page isn't cached (scroll el has height)
-      if (self.scrollView.__container && self.scrollView.__container.offsetHeight > 0) {
-        self.checkBounds();
+      if (self.jsScrolling) {
+        self.scrollView.resize();
+        // only check bounds again immediately if the page isn't cached (scroll el has height)
+        if (self.scrollView.__container && self.scrollView.__container.offsetHeight > 0) {
+          self.checkBounds();
+        }
       }
     }, 30, false);
     self.isLoading = false;
