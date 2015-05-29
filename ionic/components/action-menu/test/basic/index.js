0274236f15a2cf15dccfaee636dede650ca9e65b
Max Lynch <ihasmax@gmail.com>
2015-05-29 18:28:18 -0500

    Fixed demo

--- ionic/components/action-menu/test/basic/index.js
+++ ionic/components/action-menu/test/basic/index.js
@@ -2,12 +2,14 @@ import {bootstrap} from 'angular2/angular2'
 import {Component} from 'angular2/src/core/annotations_impl/annotations';
 import {View} from 'angular2/src/core/annotations_impl/view';
 
+import {Button, Content} from 'ionic/ionic';
 import {ActionMenu} from 'ionic/components/action-menu/action-menu';
 
 
 @Component({ selector: 'ion-app' })
 @View({
   templateUrl: 'main.html',
+  directives: [Button, Content]
 })
 class IonicApp {
   constructor() {
