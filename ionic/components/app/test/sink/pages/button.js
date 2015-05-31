61dde4ee793d72916b925c480e29a6596d4682a0
Max Lynch <ihasmax@gmail.com>
2015-05-30 23:57:40 -0500

    Nicer button demo

--- ionic/components/app/test/sink/pages/button.js
+++ ionic/components/app/test/sink/pages/button.js
@@ -24,63 +24,12 @@ import {NavbarTemplate, Navbar, NavController, Button, Content} from 'ionic/ioni
     </p>
     <p>
       <div>
-        <a class="button" href="#">a (default)</a>
-        <button class="button">button (default)</button>
-        <button class="button hover">hover</button>
-        <button class="button activated">activated</button>
-      </div>
-
-      <div>
-        <a class="button button-primary" href="#">a.primary</a>
-        <button class="button button-primary">button.primary</button>
-        <button class="button button-primary hover">hover</button>
-        <button class="button button-primary activated">activated</button>
-      </div>
-
-      <div>
-        <a class="button button-secondary" href="#">a.secondary</a>
-        <button class="button button-secondary">button.secondary</button>
-        <button class="button button-secondary hover">hover</button>
-        <button class="button button-secondary activated">activated</button>
-      </div>
-
-      <div>
-        <a class="button button-danger" href="#">a.danger</a>
-        <button class="button button-danger">button.danger</button>
-        <button class="button button-danger hover">hover</button>
-        <button class="button button-danger activated">activated</button>
-      </div>
-
-      <div>
-        <a class="button button-light" href="#">a.light</a>
-        <button class="button button-light">button.light</button>
-        <button class="button button-light hover">hover</button>
-        <button class="button button-light activated">activated</button>
-      </div>
-
-      <div>
-        <a class="button button-stable" href="#">a.stable</a>
-        <button class="button button-stable">button.stable</button>
-        <button class="button button-stable hover">hover</button>
-        <button class="button button-stable activated">activated</button>
-      </div>
-
-      <div>
-        <a class="button button-dark" href="#">a.dark</a>
-        <button class="button button-dark">button.dark</button>
-        <button class="button button-dark hover">hover</button>
-        <button class="button button-dark activated">activated</button>
-      </div>
-
-      <h2>With Properties</h2>
-
-      <div>
-        <button primary>button.primary</button>
-        <button secondary>button.secondary</button>
-        <button stable>button.stable</button>
-        <button light>button.light</button>
-        <button dark>button.dark</button>
-        <button danger>button.danger</button>
+        <button primary>Primary</button>
+        <button secondary>Secondary</button>
+        <button stable>Stable</button>
+        <button light>Light</button>
+        <button dark>Dark</button>
+        <button danger>Danger</button>
       </div>
     </p>
 
