ef19fabc5a980deb72d3e50f4eec9c34c9ed2d80
Max Lynch <ihasmax@gmail.com>
2015-06-01 21:30:36 -0500

    Form and aside flex

--- ionic/components/app/test/sink/pages/form.js
+++ ionic/components/app/test/sink/pages/form.js
@@ -3,7 +3,9 @@ import {Ancestor} from 'angular2/src/core/annotations_impl/visibility';
 import {Component, Directive} from 'angular2/src/core/annotations_impl/annotations';
 import {View} from 'angular2/src/core/annotations_impl/view';
 
-import {List, Item, ActionMenu, Modal, ModalRef,
+import {FormBuilder, Validators, formDirectives, ControlGroup} from 'angular2/forms';
+
+import {List, Item, Input, ActionMenu, Modal, ModalRef,
   NavbarTemplate, Navbar, NavController, Button, Content} from 'ionic/ionic';
 
 @Component({
@@ -23,11 +25,31 @@ import {List, Item, ActionMenu, Modal, ModalRef,
       Ionic comes with a variety of useful from controls, like
       text inputs, text areas, toggle switches, and sliders.
     </p>
+    <form (^submit)="doSubmit($event)" [control-group]="form">
+      <ion-input>
+        <input control="email" type="email" placeholder="Your email">
+      </ion-input>
+      <ion-input>
+        <input control="password" type="password" placeholder="Your password">
+      </ion-input>
+      <button ion-button primary block type="submit">Submit</button>
+    </form>
   </ion-content>
   `,
-  directives: [NavbarTemplate, Navbar, Content, List, Item]
+  directives: [formDirectives, NavbarTemplate, Navbar, Content, List, Item, Input, Button]
 })
 export class FormPage {
   constructor() {
+    var fb = new FormBuilder()
+
+    this.form = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  doSubmit(event) {
+    console.log('Submitted:', this.form.value);
+    event.preventDefault();
   }
 }
