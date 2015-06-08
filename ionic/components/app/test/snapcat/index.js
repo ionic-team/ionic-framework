7a46b2ff966f88997386f50701a8029a76882e01
Max Lynch <max@drifty.com>
2015-06-08 18:09:40 -0500

    Derpleton

--- ionic/components/app/test/snapcat/index.js
+++ ionic/components/app/test/snapcat/index.js
@@ -1,16 +1,27 @@
 import {bootstrap} from 'angular2/angular2'
 import {Component, Directive} from 'angular2/src/core/annotations_impl/annotations';
 import {View} from 'angular2/src/core/annotations_impl/view';
+import {FormBuilder, Control, ControlGroup, Validators, formDirectives} from 'angular2/forms';
 
-import {Nav, Slides, Slide, Content, Button, List, Item} from 'ionic/ionic';
+import {Nav, Segment, SegmentButton, Slides, Slide, Content, Button, List, Item} from 'ionic/ionic';
 
 @Component({ selector: 'ion-app' })
 @View({
   templateUrl: 'main.html',
-  directives: [Nav, Slides, Slide, Content, Button, List, Item]
+  directives: [formDirectives, Nav, Segment, SegmentButton, Slides, Slide, Content, Button, List, Item]
 })
 export class IonicApp {
   constructor() {
+
+    this.filterForm = new ControlGroup({
+      filterControl: new Control("")
+    });
+    /*
+    var fb = new FormBuilder();
+    this.form = fb.group({
+      filter: ['new']
+    });
+    */
   }
 }
 
