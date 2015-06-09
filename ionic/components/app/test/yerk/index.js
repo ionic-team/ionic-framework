8d467734c60a8adf25b38f533a68378b20bc6428
Max Lynch <ihasmax@gmail.com>
2015-06-08 22:16:14 -0500

    Yerk Yakr

---
+++ ionic/components/app/test/yerk/index.js
@@ -0,0 +1,23 @@
+import {bootstrap} from 'angular2/angular2'
+import {Component, Directive} from 'angular2/src/core/annotations_impl/annotations';
+import {View} from 'angular2/src/core/annotations_impl/view';
+
+import {Tabs, Tab, Content} from 'ionic/ionic'
+import {Engine} from 'ionic/engine/engine'
+import * as util from 'ionic/util'
+
+
+@Component({ selector: 'ion-app' })
+@View({
+  templateUrl: 'main.html',
+  directives: [Tabs, Tab, Content]
+})
+class IonicApp {
+  constructor() {
+    console.log('IonicApp Start');
+  }
+}
+
+export function main() {
+  bootstrap(IonicApp);
+}
