1e5d6ca74d96a7c724ea74700a301fb2056f0c40
Tim Lancina <tim@drifty.com>
2015-06-29 14:20:22 -0500

    less bundle, more fundle

--- config.js
+++ config.js
@@ -1,28 +1,9 @@
 System.config({
   "baseURL": "/",
-  "transpiler": "traceur",
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
     "npm:*": "jspm_packages/npm/*.js",
-    "ionic/*": "dist/js/ionic.bundle.js",
-    "angular2/*": "scripts/resources/angular2.dev.js"
-  },
-  "bundles": {
-    "dist/js/ionic.bundle": [
-      "ionic"
-    ],
-    "scripts/resources/angular2.dev": [
-      "angular2"
-    ]
+    "ionic/*": "ionic/*"
   }
 });
-
-System.config({
-  "map": {
-    "babel": "npm:babel-core@5.6.5",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
-  }
-});
-
