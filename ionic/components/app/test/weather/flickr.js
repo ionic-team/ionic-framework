7f2ae39f8818aafb51c80d913bbe8bab4480bb87
Max Lynch <max@drifty.com>
2015-07-01 17:59:31 -0500

    Weather demo

---
+++ ionic/components/app/test/weather/flickr.js
@@ -0,0 +1,35 @@
+import {Http} from 'ionic/net/http';
+
+let FLICKR_API_KEY = '504fd7414f6275eb5b657ddbfba80a2c';
+
+let baseUrl = 'https://api.flickr.com/services/rest/';
+
+export class Flickr {
+  constructor() {
+
+    /*
+    var flickrSearch = $resource(baseUrl, {
+      method: 'flickr.groups.pools.getPhotos',
+      group_id: '1463451@N25',
+      safe_search: 1,
+      jsoncallback: 'JSON_CALLBACK',
+      api_key: FLICKR_API_KEY,
+      format: 'json'
+    }, {
+      get: {
+        method: 'JSONP'
+      }
+    });
+    */
+  }
+
+  static search(tags, lat, lng) {
+    return new Promise((resolve, reject) => {
+      Http.get(baseUrl + '?method=flickr.groups.pools.getPhotos&group_id=1463451@N25&safe_search=1&api_key=' + FLICKR_API_KEY + '&format=json&tags=' + tags + '&lat=' + lat + '&lng=' + lng).then((val) => {
+        resolve(val);
+      }, (err) => {
+        reject(httpResponse);
+      })
+    })
+  }
+}
---
+++ ionic/components/app/test/weather/index.js
@@ -0,0 +1,123 @@
+import {Component, Directive} from 'angular2/src/core/annotations_impl/annotations';
+import {FormBuilder, Control, ControlGroup, Validators, formDirectives} from 'angular2/forms';
+
+import {IonicView, Animation, Modal, NavController, NavParams, IonicComponent} from 'ionic/ionic';
+
+import {Geo} from './geo';
+import {Weather} from './weather';
+import {Flickr} from './flickr';
+
+console.log('Imported', Geo, Weather, Flickr);
+
+@Component({
+  selector: 'ion-app',
+  appInjector: [Modal]
+})
+@IonicView({
+  templateUrl: 'main.html'
+})
+class WeatherApp {
+  constructor(Modal: Modal) {
+    this.Modal = Modal;
+
+    this.currentLocationString = 'Madison, WI';
+    this.current = {
+      local_tz_short: 'CDT'
+    };
+
+    this.activeBgImageIndex = 0;
+
+    /*
+    $ionicPlatform.ready(function() {
+      // Hide the status bar
+      if(window.StatusBar) {
+        StatusBar.hide();
+      }
+    });
+    */
+
+  }
+
+  onInit() {
+    this.refreshData();
+  }
+
+  showSettings() {
+    this.Modal.show(SettingsModal).then((settingsModal) => {
+      this.settingsModal = settingsModal;
+    });
+  }
+
+  getBackgroundImage(lat, lng, locString) {
+    Flickr.search(locString, lat, lng).then((resp) => {
+      let photos = resp.photos;
+      if(photos.photo.length) {
+        this.bgImages = photos.photo;
+        this.cycleBgImages();
+      }
+    }, (error) => {
+      console.error('Unable to get Flickr images', error);
+    });
+  }
+
+  getCurrent(lat, lng, locString) {
+    Weather.getAtLocation(lat, lng).then((resp) => {
+      this.current = resp.data;
+      console.log('GOT CURRENT', this.current);
+    }, (error) => {
+      alert('Unable to get current conditions');
+      console.error(error);
+    });
+  }
+
+  cycleBgImages() {
+    setTimeout(() => {
+      if(this.bgImages) {
+        this.activeBgImage = this.bgImages[this.activeBgImageIndex++ % this.bgImages.length];
+      }
+    });
+  }
+
+  refreshData() {
+    Geo.getLocation().then((position) => {
+      let lat = position.coords.latitude;
+      let lng = position.coords.longitude;
+
+      Geo.reverseGeocode(lat, lng).then((locString) => {
+        this.currentLocationString = locString;
+        this.getBackgroundImage(lat, lng, locString);
+      });
+
+      this.getCurrent(lat, lng);
+    }, (error) => {
+      alert('Unable to get current location: ' + error);
+    });
+  }
+}
+
+/*
+.controller('SettingsCtrl', function($scope, Settings) {
+  $scope.settings = Settings.getSettings();
+
+  // Watch deeply for settings changes, and save them
+  // if necessary
+  $scope.$watch('settings', function(v) {
+    Settings.save();
+  }, true);
+
+  $scope.closeSettings = function() {
+    $scope.modal.hide();
+  };
+
+});
+*/
+
+@IonicComponent(Modal)
+@IonicView({
+  template: '<ion-view id="settings-modal"><ion-content padding><button primary (click)="close()">Close</button></ion-content></ion-view>'
+})
+export class SettingsModal extends Modal {}
+
+export function main(ionicBootstrap) {
+  ionicBootstrap(WeatherApp);
+}
