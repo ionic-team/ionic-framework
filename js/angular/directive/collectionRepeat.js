6cf21de07497bdce831a571597bdf21d40b8b009
Alex Luke <alex@alexluke.me>
2015-07-14 14:06:18 -0700

    Fix documentation markup for collection-repeat

--- js/angular/directive/collectionRepeat.js
+++ js/angular/directive/collectionRepeat.js
@@ -56,7 +56,7 @@
  *   <ion-scroll direction="x" class="available-scroller">
  *     <div class="photo" collection-repeat="photo in main.photos"
  *        item-height="250" item-width="photo.width + 30">
- *        <img ng-src="{{photo.src}}">
+ *        <img ng-src="{% raw %}{{photo.src}}{% endraw %}">
  *     </div>
  *   </ion-scroll>
  * </ion-content>
