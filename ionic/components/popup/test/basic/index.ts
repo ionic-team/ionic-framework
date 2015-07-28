39b1d208ef0bce4df7e32f051228de1c9c82f5bb
Adam Bradley <adambradley25@gmail.com>
2015-07-28 15:45:26 -0500

    popup snapshot

--- ionic/components/popup/test/basic/index.ts
+++ ionic/components/popup/test/basic/index.ts
@@ -1,35 +1,46 @@
-import {App, IonicView, IonicApp, IonicConfig, Platform} from 'ionic/ionic';
-import {Popup} from 'ionic/ionic';
+import {App, Popup} from 'ionic/ionic';
 
 
 @App({
   templateUrl: 'main.html'
 })
-class MyAppCmp {
+class E2EApp {
 
-  constructor(popup: Popup, app: IonicApp, ionicConfig: IonicConfig) {
+  constructor(popup: Popup) {
     this.popup = popup;
-
+    this.alertOpen = false;
+    this.promptOpen = false;
+    this.promptResult = '';
+    this.confirmOpen = false;
+    this.confirmResult = '';
   }
 
   doAlert() {
-    this.popup.alert('What!?').then(() => {
-    }, () => {
+    this.alertOpen = true;
+    this.popup.alert('Alert').then(() => {
+      this.alertOpen = false;
     });
   }
 
   doPrompt() {
-    this.popup.prompt('What is your name?').then((name, event) => {
-      console.log('Got response', name);
+    this.promptOpen = true;
+    this.popup.prompt('What is your name?').then((name) => {
+      this.promptResult = name;
+      this.promptOpen = false;
     }, () => {
       console.error('Prompt closed');
+      this.promptOpen = false;
     });
   }
 
   doConfirm() {
-    this.popup.confirm('Are you sure?').then((yes, event) => {
-      console.log('CONFIRMED', yes);
+    this.confirmOpen = true;
+    this.popup.confirm('Are you sure?').then((result, ev) => {
+      console.log('CONFIRMED', result);
+      this.confirmResult = result;
+      this.confirmOpen = false;
     }, () => {
+      this.confirmOpen = false;
       console.error('NOT CONFIRMED');
     });
   }
