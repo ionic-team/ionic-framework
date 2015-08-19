d83e99de43b0330d990c315631e93fcaa271d052
Tim Lancina <tim@ionic.io>
2015-08-19 13:42:32 -0500

    add angular2 dependency

--- scripts/build/config.js
+++ scripts/build/config.js
@@ -15,7 +15,7 @@ module.exports = {
     'node_modules/traceur/bin/traceur-runtime.js',
     'node_modules/systemjs/node_modules/es6-module-loader/dist/es6-module-loader.js',
     'node_modules/systemjs/dist/system.js',
-    'node_modules/angular2-bundle/angular2.dev.js',
+    'node_modules/angular2/bundles/angular2.dev.js',
     'dist/js/ionic.js',
     'node_modules/web-animations-js/web-animations.min.js'
   ],
