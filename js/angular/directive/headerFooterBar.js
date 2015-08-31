985b83e67a291f3d28cfccd69da6be2abd155480
Delian Asparouhov <me@delian.io>
2015-08-31 16:26:12 -0700

    fix(ionFooterBar): Run align on footer

--- js/angular/directive/headerFooterBar.js
+++ js/angular/directive/headerFooterBar.js
@@ -164,6 +164,12 @@ function headerFooterBarDirective(isHeader) {
             $scope.$watch('$hasTabs', function(val) {
               $element.toggleClass('has-tabs', !!val);
             });
+            ctrl.align();
+            $scope.$on('$ionicFooter.align', function() {
+              ionic.requestAnimationFrame(function() {
+                ctrl.align();
+              });
+            });
           }
         }
       }
