70076f350c853e5f3ff2d7a2a8221674d1ba35a4
Tim Lancina <tim@ionic.io>
2015-09-03 14:57:14 -0500

    fix api index version

--- scripts/docs/processors/index-page.js
+++ scripts/docs/processors/index-page.js
@@ -5,15 +5,19 @@ module.exports = function indexPage(renderDocsProcessor) {
     $runAfter: ['adding-extra-docs'],
     $runBefore: ['extra-docs-added'],
     $process: function(docs) {
-      var currentVersion = renderDocsProcessor.extraData.version.current.name;
+      var versionData = renderDocsProcessor.extraData.version;
+      var currentVersion = versionData.current.name;
+      var latestVersion = versionData.latest.name;
 
-      docs.push({
-        docType: 'index-page',
-        id: 'index-page',
-        currentVersion: currentVersion,
-        template: 'api_index.template.html',
-        outputPath: 'docs/' + currentVersion + '/api/index.md'
-      });
+      if (currentVersion == latestVersion) {
+        docs.push({
+          docType: 'index-page',
+          id: 'index-page',
+          currentVersion: currentVersion,
+          template: 'api_index.template.html',
+          outputPath: 'docs/api/index.md'
+        });
+      }
     }
   }
 };
