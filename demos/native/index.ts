50a4549e8e81dab1d96965918cfee031ebba18e6
Tim Lancina <tim@ionic.io>
2015-09-09 15:49:06 -0500

    chore(demos): move app tests to demos

---
+++ demos/native/index.ts
@@ -0,0 +1,33 @@
+import {Component} from 'angular2/angular2';
+import {Control, ControlGroup} from 'angular2/forms';
+
+import {App, Http} from 'ionic/ionic';
+
+import {Camera, Geolocation, Vibration, Battery, Device} from 'ionic/ionic';
+
+import {CameraPage} from 'pages/camera';
+import {BatteryPage} from 'pages/battery';
+import {ContactsPage} from 'pages/contacts';
+import {DevicePage} from 'pages/device';
+import {DeviceMotionPage} from 'pages/device-motion';
+import {GeolocationPage} from 'pages/geolocation';
+import {VibrationPage} from 'pages/vibration';
+
+@App({
+  templateUrl: 'main.html'
+})
+class IonicApp {
+  constructor() {
+    this.firstPage = CameraPage;
+    console.log('First page', CameraPage);
+    this.plugins = [
+      {title: 'Camera', page: CameraPage},
+      {title: 'Device', page: DevicePage},
+      {title: 'Device Motion', page: DeviceMotionPage},
+      {title: 'Geolocation', page: GeolocationPage},
+      {title: 'Contacts', page: ContactsPage},
+      {title: 'Battery', page: BatteryPage},
+      {title: 'Vibration', page: VibrationPage},
+    ]
+  }
+}
