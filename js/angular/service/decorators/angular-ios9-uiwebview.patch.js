0269056a691f200f8784eafcfe50f37e350f9367
Perry Govier <perrygovier@users.noreply.github.com>
2015-09-18 11:53:32 -0500

    amend(iOS 9) use strict mode for the patch

--- js/angular/service/decorators/angular-ios9-uiwebview.patch.js
+++ js/angular/service/decorators/angular-ios9-uiwebview.patch.js
@@ -36,7 +36,8 @@
 
 angular.module('ngIOS9UIWebViewPatch', ['ng']).config(['$provide', function($provide) {
   $provide.decorator('$browser', ['$delegate', '$window', function($delegate, $window) {
-
+    'use strict';
+    
     if (isIOS9UIWebView($window.navigator.userAgent)) {
       return applyIOS9Shim($delegate);
     }
