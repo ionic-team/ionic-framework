335a10605b3671d9176483c6e21538840c0fb383
Max Lynch <max@drifty.com>
2015-09-28 14:30:58 -0500

    VIBRATE

--- ionic/platform/vibration/vibration.ts
+++ ionic/platform/vibration/vibration.ts
@@ -3,6 +3,14 @@ import * as Rx from 'rx';
 import * as util from 'ionic/util';
 import {NativePlugin} from '../plugin';
 
+/**
+* Vibrate the device. Uses the HTMl5 Vibration API or the `cordova-plugin-vibration` plugin (preferred)
+*
+* @usage
+* ```js
+* Vibration.vibrate();
+* ```
+*/
 @NativePlugin({
   name: 'Vibration',
   platforms: ['ios', 'android', 'web'],
@@ -11,6 +19,11 @@ import {NativePlugin} from '../plugin';
   }
 })
 export class Vibration {
+  /**
+   * Vibrate the device. Note: iOS does not support the pattern parameter.
+   *
+   * @param pattern the vibration pattern in ms to use [1000,1000,1000] (vibrate three times, one second each)
+   */
   static vibrate(pattern) {
     if(!navigator.vibrate) {
       this.pluginWarn();
