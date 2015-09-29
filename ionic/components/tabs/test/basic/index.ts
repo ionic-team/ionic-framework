f6331a688fa467a8d9bc4e336dfcd652d9f61169
Adam Bradley <adambradley25@gmail.com>
2015-09-29 16:41:30 -0500

    fix(nabber): iOS nabber updates

--- ionic/components/tabs/test/basic/index.ts
+++ ionic/components/tabs/test/basic/index.ts
@@ -1,7 +1,74 @@
-import {App} from 'ionic/ionic';
+  import {App, IonicView, NavController} from 'ionic/ionic';
 
+//
+// Tab 1
+//
+@IonicView({
+  template: `
+    <ion-navbar *navbar>
+      <ion-title>Heart</ion-title>
+    </ion-navbar>
+    <ion-content class="padding">
+      <h2>Tab 1</h2>
+    </ion-content>
+    `
+})
+class Tab1 {
+  constructor(nav: NavController) {
+    this.nav = nav;
+  }
+}
+
+//
+// Tab 2
+//
+@IonicView({
+  template: `
+    <ion-navbar *navbar>
+      <ion-title>Star</ion-title>
+    </ion-navbar>
+    <ion-content class="padding">
+      <h2>Tab 2</h2>
+    </ion-content>
+    `
+})
+class Tab2 {
+  constructor(nav: NavController) {
+    this.nav = nav;
+  }
+}
+
+//
+// Tab 3
+//
+@IonicView({
+  template: `
+    <ion-navbar *navbar>
+      <ion-title>Stopwatch</ion-title>
+    </ion-navbar>
+    <ion-content class="padding">
+      <h2>Tab 3</h2>
+    </ion-content>
+    `
+})
+class Tab3 {
+  constructor(nav: NavController) {
+    this.nav = nav;
+  }
+}
 
 @App({
-  templateUrl: 'main.html'
+  template:  `
+    <ion-tabs>
+  <ion-tab tab-title="Heart" tab-icon="heart" [root]="root1"></ion-tab>
+  <ion-tab tab-title="Star" tab-icon="star" [root]="root2"></ion-tab>
+  <ion-tab tab-title="Stopwatch" tab-icon="stopwatch" [root]="root3"></ion-tab>
+</ion-tabs>`
 })
-class E2EApp {}
+export class TabsPage {
+  constructor() {
+    this.root1 = Tab1;
+    this.root2 = Tab2;
+    this.root3 = Tab3;
+  }
+}
