b6ae19cfb921424979ae11c97010b65581dc7011
Adam Bradley <adambradley25@gmail.com>
2015-09-29 19:45:45 -0500

    fix(item): allow clicks from within item content
    Closes #215

--- ionic/components/item/test/buttons/index.ts
+++ ionic/components/item/test/buttons/index.ts
@@ -4,4 +4,10 @@ import {App} from 'ionic/ionic';
 @App({
   templateUrl: 'main.html'
 })
-class E2EApp {}
+class E2EApp {
+
+  testClick(ev) {
+    console.log('CLICK!', ev.target.tagName, ev.target.textContent.trim());
+  }
+
+}
