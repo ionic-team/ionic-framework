0cef1afe2023ecd67572fa80a7a74edae0ff119f
Brandy Carney <brandyscarney@gmail.com>
2015-10-05 17:03:04 -0400

    refactor(search-bar): Added iOS styling and another test page
    References #247

---
+++ ionic/components/search-bar/test/floating/index.ts
@@ -0,0 +1,21 @@
+import {NgControl} from 'angular2/angular2';
+import {FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/forms';
+
+import {App} from 'ionic/ionic';
+import {SearchPipe} from 'ionic/components/search-bar/search-bar';
+
+@App({
+  templateUrl: 'main.html',
+  bindings: [NgControl, FormBuilder],
+  directives: [FORM_DIRECTIVES]
+})
+class IonicApp {
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      searchQuery: ['', Validators.required]
+    });
+    this.toolbarForm = fb.group({
+      toolbarSearchQuery: ['', Validators.required]
+    })
+  }
+}
