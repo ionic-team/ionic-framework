5d6a367fa37514de5bc4991caf33747241688505
Tim Lancina <tim@ionic.io>
2015-10-06 16:20:03 -0500

    IdRef API docs
    Closes #208.

--- ionic/components/app/id.ts
+++ ionic/components/app/id.ts
@@ -6,6 +6,27 @@ import {IonicApp} from './app';
  * IdRef is an easy way to identify unique components in an app and access them
  * no matter where in the UI heirarchy you are. For example, this makes toggling
  * a global side menu feasible from any place in the application.
+ *
+ * See the [Menu section](http://localhost:4000/docs/v2/components/#menus) of
+ * the Component docs for an example of how Menus rely on ID's.
+ *
+ * To give any component an ID, simply set its `id` property:
+ * ```html
+ * <ion-checkbox id="myCheckbox"></ion-checkbox>
+ * ```
+ *
+ * To get a reference to the registered component, inject the [IonicApp](../app/IonicApp/)
+ * service:
+ * ```ts
+ * constructor(app: IonicApp) {
+ *   var checkbox = app.getComponent("myCheckbox");
+ *   if (checkbox.checked) console.log('checkbox is checked');
+ * }
+ * ```
+ *
+ * *NOTE:* It is not recommended to use ID's across Pages, as there is often no
+ * guarantee that the registered component has not been destroyed if its Page
+ * has been navigated away from.
  */
 @Directive({
   selector: '[id]',
