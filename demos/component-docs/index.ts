760cfcf9ef3f78f844a894bb02d7471a58b36451
Drew Rygh <drew.rygh@gmail.com>
2015-10-06 08:40:25 -0500

    docs(demos): tabs, nav, button demos

--- demos/component-docs/index.ts
+++ demos/component-docs/index.ts
@@ -1,30 +1,54 @@
-import {App, IonicApp, ActionSheet, Animation, NavController, NavParams} from 'ionic/ionic';
-import {IonicView, IonicConfig, Events} from 'ionic/ionic';
+import {App, IonicApp, ActionSheet, NavController, NavParams} from 'ionic/ionic';
+import {Modal, IonicView, IonicConfig, Events} from 'ionic/ionic';
 import {NgZone} from 'angular2/angular2';
 import {NavigationDetailsPage} from 'navigation';
+import {TabsPage} from 'tabs';
+
 import * as helpers from 'helpers';
+
+
+@IonicView({
+  template: '<div>My Modal</div>'
+})
+class DemoModal extends Modal {
+  constructor() {
+    super();
+  }
+}
 
 
 @IonicView({
   templateUrl: 'main.html'
 })
-class MainPage {
+export class MainPage {
 
-  constructor(app: IonicApp, nav: NavController, actionSheet: ActionSheet, zone: NgZone, params: NavParams, events: Events) {
+  component: any = { title: 'Action Sheets' };
+
+  constructor(app: IonicApp,
+              nav: NavController,
+              actionSheet: ActionSheet,
+              zone: NgZone,
+              params: NavParams,
+              modal: Modal,
+              events: Events)
+  {
     this.params = params;
     this.nav = nav;
+    this.modal = modal;
     this.actionSheet = actionSheet;
-
     this.navDetailsPage = NavigationDetailsPage;
-    this.component = { title: 'Action Sheets' };
 
-    this.setupAnimations();
+    if (params.data.location) { this.component.title = params.data.location; }
+    else if (window.location.hash) { this.component.title = window.location.hash; }
 
     window.addEventListener('message', (e) => {
       zone.run(() => {
         if (e.data) {
           var data = JSON.parse(e.data);
           this.component.title = helpers.toTitleCase(data.hash.replace('-', ' '));
+          if (this.component.title === 'Tabs') {
+            this.nav.setRoot(TabsPage);
+          }
         }
       });
     });
@@ -70,26 +94,16 @@ class MainPage {
     this.nav.push(NavigationDetailsPage, {name: item});
   }
 
-  // **************************
-  // Animations
-  // **************************
-  setupAnimations() {
-    this.animation = new Animation();
-    this.animation
-      .duration(2000)
 
-    var ionitronSpin = new Animation(document.querySelector('#ionitron'));
-    ionitronSpin
-      .from('transform', 'rotate(0deg)')
-      .to('transform', 'rotate(360deg)')
-
-    this.animation.add(ionitronSpin);
-  }
-  play() {
-    this.animation.play();
-  }
-  pause() {
-    this.animation.pause();
+  // **************************
+  // Modal
+  // **************************
+  openModal() {
+    this.modal.open(DemoModal, {
+      enterAnimation: 'my-fade-in',
+      leaveAnimation: 'my-fade-out',
+      handle: 'my-awesome-modal'
+    });
   }
 
 }
