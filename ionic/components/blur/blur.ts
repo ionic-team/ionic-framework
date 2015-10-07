19aa828846162f339e84553af461a9e93361a887
Max Lynch <max@drifty.com>
2015-10-07 09:31:04 -0500

    :derp: console logs

--- ionic/components/blur/blur.ts
+++ ionic/components/blur/blur.ts
@@ -5,7 +5,6 @@ import {Directive, Renderer, ElementRef} from 'angular2/angular2';
 })
 export class Blur {
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
-    console.log('Blur renderer', renderer);
     renderer.setElementStyle(elementRef, '-webkit-backdrop-filter', 'blur(10px)');
   }
 }
