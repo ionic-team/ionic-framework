e9a7eabf9b86ce07ea90b10a5ab7490eb4c88a4c
jbavari <josh@drifty.com>
2015-10-09 11:22:43 -0600

    Update demo for grouping and item sticky elements

--- ionic/components/item/test/groups/index.ts
+++ ionic/components/item/test/groups/index.ts
@@ -1,31 +1,42 @@
-import {Host} from 'angular2/angular2';
-import {App} from 'ionic/ionic';
+import {ElementRef, Host} from 'angular2/angular2';
 import {Component, View, NgIf, NgFor} from 'angular2/angular2';
-import {Content, Icon, Item, ItemGroup, ItemGroupTitle, ItemSliding, List, ListHeader, NavController} from 'ionic/ionic';
-
-
-console.log('content', Content)
+import {App, Content, Icon, Item, ItemGroup, ItemGroupTitle, ItemSliding, List, ListHeader, NavController} from 'ionic/ionic';
 
 @Component({
   properties: ['data'],
-  selector: 'component-list'
-})
-@View({
-  directives: [ComponentList, Item, ItemGroup, ItemGroupTitle, List, NgIf, NgFor],
-  templateUrl: 'list.html'
+  selector: 'component-list',
+  directives: [ComponentList, Item, ItemGroup, ItemGroupTitle, ItemSliding, List, NgIf, NgFor],
+  templateUrl: 'component-list.html'
 })
 class ComponentList {
-  constructor(@Host() content:Content) {
+  constructor(elementRef: ElementRef, @Host() content:Content) {
     console.log('Component List init', content);
   }
 }
 
 @App({
-  directives: [ComponentList, Item, ItemGroup, ItemGroupTitle, List, NgIf, NgFor],
+  directives: [Content, ComponentList, Item, ItemGroup, ItemGroupTitle, ItemSliding, List, NgIf, NgFor],
   templateUrl: 'main.html'
 })
 class E2EApp {
   constructor() {
-    this.data = ['1'];
+    let session = {
+      "name": "Breakfast",
+      "location": "Main hallway"
+    };
+
+    this.data = {
+      "day": "05/17/2016",
+      "sessions": [
+        {
+          "name": "Breakfast",
+          "location": "Main hallway"
+        }
+      ]
+    };
+
+    for(let i = 0; i < 50; i++) {
+      this.data.sessions.push(session);
+    }
   }
 }
