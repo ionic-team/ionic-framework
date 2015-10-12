33ac1cc13bcee81b8bcd6110296447fc4ff7d2ce
Max Lynch <max@drifty.com>
2015-10-12 14:56:40 -0500

    SearchBar

---
+++ ionic/components/search-bar/test/model/index.ts
@@ -0,0 +1,43 @@
+import {NgControl, FORM_DIRECTIVES, FormBuilder, Validators, Control, ControlGroup} from 'angular2/angular2';
+
+import {App} from 'ionic/ionic';
+import {SearchPipe} from 'ionic/components/search-bar/search-bar';
+
+
+function randomTitle() {
+  var items = ['Soylent', 'Pizza', 'Pumpkin', 'Apple', 'Bologna', 'Turkey', 'Kabob', 'Salad', 'Fruit bowl', 'Fish Tacos', 'Chimichongas', 'Meatloaf'];
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+@App({
+  templateUrl: 'main.html',
+  bindings: [NgControl],
+  directives: [FORM_DIRECTIVES]
+})
+class IonicApp {
+  constructor() {
+    var fb = new FormBuilder();
+
+    this.searchQuery = '';
+
+    this.items = []
+    for(let i = 0; i < 100; i++) {
+      this.items.push({
+        title: randomTitle()
+      })
+    }
+  }
+
+  getItems() {
+    var q = this.searchQuery;
+    if(q.trim() == '') {
+      return this.items;
+    }
+    return this.items.filter((v) => {
+      if(v.title.toLowerCase().indexOf(q.toLowerCase()) >= 0) {
+        return true;
+      }
+      return false;
+    })
+  }
+}
