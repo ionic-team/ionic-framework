e8e6f557458a5454456a23aa67edf63ec36b3ac3
Drew Rygh <drew.rygh@gmail.com>
2015-10-25 23:49:56 -0500

    docs(demos): add scrollbar check helper function

--- demos/component-docs/helpers.ts
+++ demos/component-docs/helpers.ts
@@ -64,6 +64,38 @@ export function toTitleCase(str) {
   return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+export function hasScrollbar() {
+    console.log(window);
+  // The Modern solution
+  if (typeof window.innerWidth === 'number')
+    return window.innerWidth > document.documentElement.clientWidth
+
+  // rootElem for quirksmode
+  var rootElem = document.documentElement || document.body
+
+  // Check overflow style property on body for fauxscrollbars
+  var overflowStyle
+
+  if (typeof rootElem.currentStyle !== 'undefined')
+    overflowStyle = rootElem.currentStyle.overflow
+
+  overflowStyle = overflowStyle || window.getComputedStyle(rootElem, '').overflow
+
+    // Also need to check the Y axis overflow
+  var overflowYStyle
+
+  if (typeof rootElem.currentStyle !== 'undefined')
+    overflowYStyle = rootElem.currentStyle.overflowY
+
+  overflowYStyle = overflowYStyle || window.getComputedStyle(rootElem, '').overflowY
+
+  var contentOverflows = rootElem.scrollHeight > rootElem.clientHeight
+  var overflowShown    = /^(visible|auto)$/.test(overflowStyle) || /^(visible|auto)$/.test(overflowYStyle)
+  var alwaysShowScroll = overflowStyle === 'scroll' || overflowYStyle === 'scroll'
+
+  return (contentOverflows && overflowShown) || (alwaysShowScroll)
+}
+
 export function getPageFor(hash) {
     return {
         'action-sheets': ActionSheetPage,
