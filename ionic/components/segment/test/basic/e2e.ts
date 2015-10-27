d8f643d340c33aeca223f3fabc4bcbd95ac1dc3f
Brandy Carney <brandyscarney@gmail.com>
2015-10-27 15:16:37 -0400

    test(segment): added e2e tests for segment
    Cleaned up some of the UI for the tests

---
+++ ionic/components/segment/test/basic/e2e.ts
@@ -0,0 +1,12 @@
+
+it('friends should be selected', function() {
+  element(by.css('.e2eSegmentFriends')).click();
+});
+
+it('standard should be selected', function() {
+  element(by.css('.e2eSegmentStandard')).click();
+});
+
+it('model c should be selected', function() {
+  element(by.css('.e2eSegmentModelC')).click();
+});
