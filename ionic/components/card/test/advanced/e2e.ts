4584829b860ad6890de1ac7d52104632a4341cff
Brandy Carney <brandyscarney@gmail.com>
2015-10-28 15:31:06 -0400

    test(card): added missing tests for card
    for some reason the iOS clicks aren’t showing in the screenshots.
    references #369

--- ionic/components/card/test/advanced/e2e.ts
+++ ionic/components/card/test/advanced/e2e.ts
@@ -1,1 +1,8 @@
 
+it('share should be activated', function() {
+  element(by.css('.e2eAdvancedCardShareButton')).click();
+});
+
+it('favorite should be activated', function() {
+  element(by.css('.e2eAdvancedCardFavoriteButton')).click();
+});
--- ionic/components/card/test/basic/e2e.ts
+++ ionic/components/card/test/basic/e2e.ts
@@ -1,1 +1,12 @@
 
+it('view should be activated', function() {
+  element(by.css('.e2eBasicCardViewButton')).click();
+});
+
+it('link 1 should be activated', function() {
+  element(by.css('.e2eBasicCardLink1')).click();
+});
+
+it('button 1 should be activated', function() {
+  element(by.css('.e2eBasicCardButton1')).click();
+});
