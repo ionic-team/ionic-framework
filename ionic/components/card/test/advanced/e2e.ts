7dd7043064770910015d088f6d498b871ac8c705
Brandy Carney <brandyscarney@gmail.com>
2015-10-29 18:59:16 -0400

    test(card): removing card tests in favor of applying the activated class
    references #369

--- ionic/components/card/test/advanced/e2e.ts
+++ ionic/components/card/test/advanced/e2e.ts
@@ -1,8 +1,1 @@
 
-it('share should be activated', function() {
-  element(by.css('.e2eAdvancedCardShareButton')).click();
-});
-
-it('favorite should be activated', function() {
-  element(by.css('.e2eAdvancedCardFavoriteButton')).click();
-});
