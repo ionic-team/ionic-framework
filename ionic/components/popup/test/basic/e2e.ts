744920949e73fed0ef728b0f129ea23ac619ace7
Brandy Carney <brandyscarney@gmail.com>
2015-10-29 13:02:21 -0400

    test(popup): switched the order of the tests to test for subtitle being passed
    references #342

--- ionic/components/popup/test/basic/e2e.ts
+++ ionic/components/popup/test/basic/e2e.ts
@@ -8,24 +8,20 @@ it('should close alert', function() {
   element(by.css('.popup-buttons button:last-of-type')).click();
 });
 
+it('should open confirm', function() {
+  element(by.css('.e2eOpenConfirm')).click();
+});
+
+it('should close confirm', function() {
+  element(by.css('.popup-buttons button:last-of-type')).click();
+});
 
 it('should open prompt', function() {
   element(by.css('.e2eOpenPrompt')).click();
 });
 
-
 it('should close prompt', function() {
   var inputEle = element(by.css('.popup-body input'));
   inputEle.sendKeys('prompt text');
-  element(by.css('.popup-buttons button:last-of-type')).click();
-});
-
-
-it('should open confirm', function() {
-  element(by.css('.e2eOpenConfirm')).click();
-});
-
-
-it('should close confirm', function() {
   element(by.css('.popup-buttons button:last-of-type')).click();
 });
