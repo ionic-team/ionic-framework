30ef9e165801d91b56a5b8cd8c4a14d7a1b02c21
Max Lynch <max@drifty.com>
2015-10-29 11:53:11 -0500

    fix(camera): expose constants. Fixes #302

--- ionic/platform/camera/camera.ts
+++ ionic/platform/camera/camera.ts
@@ -28,6 +28,75 @@ import {NativePlugin} from '../plugin';
   }
 })
 export class Camera {
+
+  /**
+   * @enum {number}
+   */
+  static DestinationType = {
+    /** Return base64 encoded string */
+    DATA_URL: 0,
+    /** Return file uri (content://media/external/images/media/2 for Android) */
+    FILE_URI: 1,
+    /** Return native uri (eg. asset-library://... for iOS) */
+    NATIVE_URI: 2
+  }
+
+  /**
+   * @enum {number}
+   */
+  static EncodingType = {
+    /** Return JPEG encoded image */
+    JPEG: 0,
+    /** Return PNG encoded image */
+    PNG: 1
+  }
+
+  /**
+   * @enum {number}
+   */
+  static MediaType = {
+    /** Allow selection of still pictures only. DEFAULT. Will return format specified via DestinationType */
+    PICTURE: 0,
+    /** Allow selection of video only, ONLY RETURNS URL */
+    VIDEO: 1,
+    /** Allow selection from all media types */
+    ALLMEDIA : 2
+  }
+
+  /**
+   * @enum {number}
+   */
+  static PictureSourceType = {
+    /** Choose image from picture library (same as SAVEDPHOTOALBUM for Android) */
+    PHOTOLIBRARY : 0,
+    /** Take picture from camera */
+    CAMERA : 1,
+    /** Choose image from picture library (same as PHOTOLIBRARY for Android) */
+    SAVEDPHOTOALBUM : 2
+  }
+
+  /**
+   * Matches iOS UIPopoverArrowDirection constants to specify arrow location on popover.
+   * @enum {number}
+   */
+  static PopoverArrowDirection = {
+      ARROW_UP : 1,
+      ARROW_DOWN : 2,
+      ARROW_LEFT : 4,
+      ARROW_RIGHT : 8,
+      ARROW_ANY : 15
+  }
+
+  /**
+   * @enum {number}
+   */
+  static Direction = {
+      /** Use the back-facing camera */
+      BACK: 0,
+      /** Use the front-facing camera */
+      FRONT: 1
+  }
+
   /**
    * Let the user take a photo or capture video.
    *
