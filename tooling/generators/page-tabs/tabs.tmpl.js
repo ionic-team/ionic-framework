c0a14a682f24c999490d1ce6359ccb04d476fb3d
jbavari <josh@drifty.com>
2015-10-30 16:50:55 -0600

    Fix(generate): Adjust spacing in tabs template js and html

--- tooling/generators/page-tabs/tabs.tmpl.js
+++ tooling/generators/page-tabs/tabs.tmpl.js
@@ -1,17 +1,14 @@
 import {NavController, Page} from 'ionic/ionic';
 
-<% _.forEach(tabs, function(tab) { %>
-import {<%= tab.javascriptClassName %>} from '../<%= tab.fileAndClassName %>/<%= tab.fileAndClassName %>';
+<% _.forEach(tabs, function(tab) { %>import {<%= tab.javascriptClassName %>} from '../<%= tab.fileAndClassName %>/<%= tab.fileAndClassName %>';
 <% }); %>
-
 @Page({
   templateUrl: 'app/<%= fileAndClassName %>/<%= fileAndClassName %>.html'
 })
 export class <%= javascriptClassName %> {
   constructor(nav: NavController) {
     // set the root pages for each tab
-    <% _.forEach(tabs, function(tab) { %>
-    this.<%= tab.javascriptClassName %> = <%= tab.javascriptClassName %>;
+    <% _.forEach(tabs, function(tab) { %>this.<%= tab.javascriptClassName %> = <%= tab.javascriptClassName %>;
     <% }); %>
   }
 
