f721020dc09cb6340133d9a8e23ff13fe0990fda
Adam Bradley <adambradley25@gmail.com>
2015-10-29 22:29:44 -0500

    chore(snapshot): no css animations

--- scripts/snapshot/snapshot.config.js
+++ scripts/snapshot/snapshot.config.js
@@ -11,7 +11,7 @@ exports.config = {
   specs: 'dist/e2e/**/*e2e.js',
   //specs: 'dist/e2e/search-bar/**/*e2e.js',
 
-  sleepBetweenSpecs: 300,
+  sleepBetweenSpecs: 250,
 
   platformDefauls: {
     browser: 'chrome',
