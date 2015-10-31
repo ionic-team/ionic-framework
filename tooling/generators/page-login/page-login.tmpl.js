f413a9002a653eb83cc71e726ab6d2d099b9b0f8
jbavari <josh@drifty.com>
2015-10-30 23:31:25 -0600

    Feature(generators): Add generators for login page and signup page

---
+++ tooling/generators/page-login/page-login.tmpl.js
@@ -0,0 +1,32 @@
+import {FormBuilder, Validators} from 'angular2/angular2';
+import {Log} from 'ionic/util'
+import {Page, NavController} from 'ionic/ionic'
+
+@Page({
+  templateUrl: 'app/<%= fileAndClassName %>/<%= fileAndClassName %>.html'
+})
+class <%= javascriptClassName %> {
+  constructor(nav: NavController ) {
+
+    this.nav = nav
+    Log.log('LOGIN PAGE', this)
+
+    var fb = new FormBuilder()
+
+    this.loginForm = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+  }
+
+  doLogin(event) {
+    Log.log('Doing login')
+    event.preventDefault();
+    console.log(this.loginForm.value);
+  }
+
+  doSignup(event) {
+    this.nav.push(SignupPage)
+  }
+}
---
+++ tooling/generators/page-signup/page-signup.tmpl.js
@@ -0,0 +1,34 @@
+import {FormBuilder, Validators} from 'angular2/angular2';
+import {Log} from 'ionic/util'
+import {Page, NavController} from 'ionic/ionic'
+
+
+@Page({
+  templateUrl: 'app/<%= fileAndClassName %>/<%= fileAndClassName %>.html'
+})
+export class <%= javascriptClassName %> {
+  constructor(nav: NavController) {
+    this.nav = nav
+
+    Log.log('SIGNUP PAGE')
+
+    var fb = new FormBuilder()
+
+    this.signupForm = fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  doLogin(event) {
+    this.nav.pop()
+  }
+  doSignup(event) {
+    Log.log('Doing signup')
+    event.preventDefault();
+    console.log(this.signupForm.value);
+
+    this.nav.push(AppPage)
+  }
+}
