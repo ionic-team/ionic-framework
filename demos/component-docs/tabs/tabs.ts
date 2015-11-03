4759193752651f70c1e0f7648faa9648d07b3004
Drew Rygh <drew.rygh@gmail.com>
2015-11-03 11:28:49 -0600

    docs(demos): inline tabs example

--- demos/component-docs/tabs/tabs.ts
+++ demos/component-docs/tabs/tabs.ts
@@ -3,9 +3,16 @@ import {Page, ViewController} from 'ionic/ionic';
 import {forwardRef} from 'angular2/angular2';
 import * as helpers from '../helpers';
 
+var TABS_DEMO_TMPL = '' +
+'<ion-navbar *navbar hide-back-button class="android-attr">' +
+'<ion-title>Tabs</ion-title>' +
+'</ion-navbar>' +
+'<ion-content>' +
+'</ion-content>';
+
 @Page({
-  template: 'Hello 1',
-  directives: [forwardRef(() => helpers.AndroidAttribute)]
+  template: TABS_DEMO_TMPL,
+  directives: [forwardRef(() => helpers.AndroidAttribute)],
 })
 class TabOneCtrl {
   constructor(nav: NavController, view: ViewController) {
@@ -15,11 +22,40 @@ class TabOneCtrl {
 }
 
 @Page({
-  templateUrl: 'tabs/tabs.html',
-  directives: [forwardRef(() => helpers.AndroidAttribute)]
+  template: TABS_DEMO_TMPL,
+  directives: [forwardRef(() => helpers.AndroidAttribute)],
+})
+class TabTwoCtrl {
+  constructor(nav: NavController, view: ViewController) {
+    this.nav = nav;
+    this.view = view;
+  }
+}
+
+@Page({
+  template: TABS_DEMO_TMPL,
+  directives: [forwardRef(() => helpers.AndroidAttribute)],
+})
+class TabThreeCtrl {
+  constructor(nav: NavController, view: ViewController) {
+    this.nav = nav;
+    this.view = view;
+  }
+}
+
+@Page({
+  template: 
+    '<ion-tabs>' +
+      '<ion-tab tab-title="Food" tab-icon="pizza" [root]="tabOne"></ion-tab>' +
+      '<ion-tab tab-title="Drinks" tab-icon="beer" [root]="tabTwo"></ion-tab>' +
+      '<ion-tab tab-title="Hours" tab-icon="clock" [root]="tabThree"></ion-tab>' +
+    '</ion-tabs>',
 })
 export class TabsPage {
   constructor(nav: NavController, params: NavParams) {
     this.nav = nav;
+    this.tabOne = TabOneCtrl;
+    this.tabTwo = TabTwoCtrl;
+    this.tabThree = TabThreeCtrl;
   }
 }
