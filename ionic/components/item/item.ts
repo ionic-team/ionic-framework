a800834cc504cae678e59e114b0466bb2788f386
Drew Rygh <drew.rygh@gmail.com>
2015-11-12 17:52:53 -0600

    docs(item): add use cases of detail-push

--- ionic/components/item/item.ts
+++ ionic/components/item/item.ts
@@ -10,6 +10,11 @@ import {Component} from 'angular2/angular2';
  * - Use `<button ion-item>` for something that can be clicked/tapped. Typically this element will also have a `(click)` handler.
  * - Use `<a ion-item>` for when the item needs to contain a `href`.
  *
+ * By default, `<button ion-item>` and `<a ion-item>` will receive a right arrow icon on iOS to signal that tapping the item will reveal more information.
+ * To hide this icon, add the `detail-none` attribute to the item (eg: `<button ion-item detail-none>`). To add the icon when it is not displayed by default, 
+ * add the `detail-push` attribute (eg: `<ion-item detail-push>`).
+ *
+ * 
  * @usage
  * ```html
  * <ion-list>
