24d823ab5fbe9f104e90c67e9ba339678775e074
Drew Rygh <drew.rygh@gmail.com>
2015-11-12 14:20:36 -0600

    docs(demos): add search demo

--- demos/component-docs/inputs/inputs.ts
+++ demos/component-docs/inputs/inputs.ts
@@ -1,6 +1,7 @@
 export * from './checkbox/pages';
 export * from './radio/pages';
 export * from './range/pages';
+export * from './search/pages';
 export * from './segment/pages';
 export * from './select/pages';
 export * from './switch/pages';
