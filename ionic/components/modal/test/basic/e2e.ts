d2374fd37e46892c30fb68ea32a7f2ae91648630
Brandy Carney <brandyscarney@gmail.com>
2015-11-12 20:07:13 -0500

    fix(toolbar): fixed the toolbar spacing for iOS with platform-cordova
    Added some tests for modals with a toolbar, only target the first
    toolbar in a menu or modal so the user can have multiple toolbars
    without skewing them. References #469

--- ionic/components/modal/test/basic/e2e.ts
+++ ionic/components/modal/test/basic/e2e.ts
@@ -1,8 +1,16 @@
 
-it('should open action sheet', function() {
+it('should open modal', function() {
   element(by.css('.e2eOpenModal')).click();
 });
 
 it('should close with close button click', function() {
   element(by.css('.e2eCloseMenu')).click();
+});
+
+it('should open toolbar modal', function() {
+  element(by.css('.e2eOpenToolbarModal')).click();
+});
+
+it('should close toolbar modal', function() {
+  element(by.css('.e2eCloseToolbarModal')).click();
 });
