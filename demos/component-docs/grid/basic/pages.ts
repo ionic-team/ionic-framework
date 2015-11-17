cfa15b47edeeb94baa097806b37960e98dd030de
Drew Rygh <drew.rygh@gmail.com>
2015-11-17 11:19:20 -0600

    docs(demos): add grid example
    Closes #536

---
+++ demos/component-docs/grid/basic/pages.ts
@@ -0,0 +1,13 @@
+import {Page} from 'ionic/ionic';
+import {forwardRef} from 'angular2/angular2';
+import {AndroidAttribute} from '../../helpers';
+
+@Page({
+    templateUrl: 'grid/basic/template.html',
+    directives: [forwardRef(() => AndroidAttribute)]
+})
+export class BasicPage {
+    constructor() {
+
+    }
+}
