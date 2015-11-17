c7e90967bdbf7b2f1360b8280c97712eee6b785b
perry <perry@ionic.io>
2015-11-17 15:21:58 -0600

    chore(CI): getting rid of paralellism in tests too

--- scripts/circle/test.sh
+++ scripts/circle/test.sh
@@ -21,40 +21,44 @@ function run {
   node_modules/.bin/gulp demos --demo-version=nightly
   TEST_ID=$CIRCLE_SHA1-$CIRCLE_BUILD_NUM
 
-  case $INDEX in
-  0)
-    [ -z $IS_MASTER ] || node_modules/.bin/gulp ddescribe-iit
-    node_modules/.bin/gulp eslint
-    node_modules/.bin/gulp karma --browsers=PhantomJS --reporters=dots,coverage
-    ;;
-  1)
-    # node_modules/.bin/gulp snapshot-sauce \
-    #   --browser="chrome" \
-    #   --params.width="400" \
-    #   --params.height="800" \
-    #   --params.test_id=$TEST_ID
-    #   --params.platform_id="chrome_desktop_small" \
-    #   --params.platform_index=$INDEX \
-    #   --params.platform_count=$TOTAL
-    ;;
-  2)
-    # node_modules/.bin/gulp snapshot-sauce \
-    #   --browser="safari" \
-    #   --platform="OS X 10.9"
-    #   --params.width="400" \
-    #   --params.height="800" \
-    #   --params.test_id=$TEST_ID \
-    #   --params.platform_id="safari_desktop_small" \
-    #   --params.platform_index=$INDEX \
-    #   --params.platform_count=$TOTAL
-    ;;
-  3)
-    ;;
-  4)
-    ;;
-  5)
-    ;;
-  esac
+  [ -z $IS_MASTER ] || node_modules/.bin/gulp ddescribe-iit
+  node_modules/.bin/gulp eslint
+  node_modules/.bin/gulp karma --browsers=PhantomJS --reporters=dots,coverage
+
+  # case $INDEX in
+  # 0)
+  #   [ -z $IS_MASTER ] || node_modules/.bin/gulp ddescribe-iit
+  #   node_modules/.bin/gulp eslint
+  #   node_modules/.bin/gulp karma --browsers=PhantomJS --reporters=dots,coverage
+  #   ;;
+  # 1)
+  #   # node_modules/.bin/gulp snapshot-sauce \
+  #   #   --browser="chrome" \
+  #   #   --params.width="400" \
+  #   #   --params.height="800" \
+  #   #   --params.test_id=$TEST_ID
+  #   #   --params.platform_id="chrome_desktop_small" \
+  #   #   --params.platform_index=$INDEX \
+  #   #   --params.platform_count=$TOTAL
+  #   ;;
+  # 2)
+  #   # node_modules/.bin/gulp snapshot-sauce \
+  #   #   --browser="safari" \
+  #   #   --platform="OS X 10.9"
+  #   #   --params.width="400" \
+  #   #   --params.height="800" \
+  #   #   --params.test_id=$TEST_ID \
+  #   #   --params.platform_id="safari_desktop_small" \
+  #   #   --params.platform_index=$INDEX \
+  #   #   --params.platform_count=$TOTAL
+  #   ;;
+  # 3)
+  #   ;;
+  # 4)
+  #   ;;
+  # 5)
+  #   ;;
+  # esac
 }
 
 source $(dirname $0)/../utils.inc
