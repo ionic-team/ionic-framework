160cc70332f2abe4db3d181fc448b44729203245
Drew Rygh <drew.rygh@gmail.com>
2015-12-02 13:32:59 -0600

    docs(api): do not generate files for private classes
    Closes #684

--- scripts/docs/dgeni-config.js
+++ scripts/docs/dgeni-config.js
@@ -19,6 +19,8 @@ module.exports = function(currentVersion){
 .processor(require('./processors/index-page'))
 .processor(require('./processors/jekyll'))
 .processor(require('./processors/remove-private-members'))
+.processor(require('./processors/hide-private-api'))
+
 
 // for debugging docs
 // .processor(function test(){
