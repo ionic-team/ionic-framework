bee68c1badb2fc7dafbdc33cf297b0f80fd3a660
Tim Lancina <tim@ionic.io>
2015-12-02 14:18:26 -0600

    Split decorators into separate files
    Resolves circular ref with ConfigComponent

---
+++ ionic/config/decorators/config-component.ts
@@ -0,0 +1,39 @@
+import {Component} from 'angular2/angular2'
+import {pascalCaseToDashCase} from '../../util/util';
+
+
+/**
+ * @private
+ */
+export function ConfigComponent(config) {
+  return function(cls) {
+    var annotations = Reflect.getMetadata('annotations', cls) || [];
+    annotations.push(new Component(appendConfig(cls, config)));
+    Reflect.defineMetadata('annotations', annotations, cls);
+    return cls;
+  }
+}
+
+/**
+ * @private
+ */
+function appendConfig(cls, config) {
+  config.host = config.host || {};
+
+  cls.defaultInputs = config.defaultInputs || {};
+
+  config.inputs = config.inputs || [];
+
+  for (let prop in cls.defaultInputs) {
+    // add the property to the component "inputs"
+    config.inputs.push(prop);
+
+    // set the component "hostProperties", so the instance's
+    // input value will be used to set the element's attribute
+    config.host['[attr.' + pascalCaseToDashCase(prop) + ']'] = prop;
+  }
+
+  cls.delegates = config.delegates;
+
+  return config;
+}
