6eb1ef5fb540fa36cfb4a1d55f8e7249fb9d28d8
Adam Bradley <adambradley25@gmail.com>
2015-12-03 15:31:48 -0600

    dynamically add link element for testing

--- ionic/config/bootstrap.ts
+++ ionic/config/bootstrap.ts
@@ -98,7 +98,13 @@ function setupDom(window, document, config, platform, clickBlock, featureDetect)
 
   // set the mode class name
   // ios/md
-  bodyEle.classList.add(config.get('mode'));
+  let mode = config.get('mode');
+  bodyEle.classList.add(mode);
+
+  let linkEle = document.createElement('link');
+  linkEle.setAttribute('href', '../../../css/ionic.' + mode + '.css');
+  linkEle.setAttribute('ref', 'stylesheet');
+  document.head.appendChild(linkEle);
 
   // touch devices should not use :hover CSS pseudo
   // enable :hover CSS when the "hoverCSS" setting is not false
