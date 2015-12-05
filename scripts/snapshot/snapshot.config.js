5ae94e655984391d5d10d5470578228e1645f835
Adam Bradley <adambradley25@gmail.com>
2015-12-05 11:19:09 -0600

    mode font styling

--- scripts/snapshot/snapshot.config.js
+++ scripts/snapshot/snapshot.config.js
@@ -8,8 +8,8 @@ exports.config = {
   domain: 'ionic-snapshot-go.appspot.com',
   //domain: 'localhost:8080',
 
-  specs: 'dist/e2e/**/*e2e.js',
-  //specs: 'dist/e2e/searchbar/**/*e2e.js',
+  //specs: 'dist/e2e/**/*e2e.js',
+  specs: 'dist/e2e/action-sheet/**/*e2e.js',
 
   sleepBetweenSpecs: 350,
 
