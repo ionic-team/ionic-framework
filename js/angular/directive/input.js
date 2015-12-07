10110194130a855f583bca2e9eee02082e3eae12
Max Lynch <ihasmax@gmail.com>
2015-12-07 16:46:35 -0600

    <ion-label> focus

--- js/angular/directive/input.js
+++ js/angular/directive/input.js
@@ -13,6 +13,11 @@
 *   <ion-input>
 *     <input type="text" placeholder="First Name">
 *   <ion-input>
+*
+*   <ion-input>
+*     <ion-label>Username</ion-label>
+*     <input type="text">
+*   </ion-input>
 * </ion-list>
 * ```
 */
@@ -29,6 +34,64 @@ IonicModule
 
       this.input = $element[0].querySelector('input,textarea');
     }]
+  };
+}]);
+
+/**
+* @ngdoc directive
+* @name ionLabel
+* @parent ionic.directive:ionList
+* @module ionic
+* @restrict E
+*
+* New in Ionic 1.2. It is strongly recommended that you use `<ion-label>` in place
+* of any `<label>` elements for maximum cross-browser support and performance.
+*
+* Creates a label for a form input.
+*
+* @usage
+*
+* ```html
+* <ion-list>
+*   <ion-input>
+*     <ion-label>Username</ion-label>
+*     <input type="text">
+*   </ion-input>
+* </ion-list>
+* ```
+*/
+IonicModule
+.directive('ionLabel', ['$timeout', function($timeout) {
+  return {
+    restrict: 'E',
+    require: '?^ionInput',
+    compile: function($element, $attrs) {
+
+      return function link($scope, $element, $attrs, ionInputCtrl) {
+        var element = $element[0];
+
+        $element.addClass('input-label');
+
+        $element.attr('aria-label', $element.text());
+        var id = element.id || '_label-' + ++labelIds;
+
+        if (!element.id) {
+          $element.attr('id', id);
+        }
+
+        if (ionInputCtrl && ionInputCtrl.input) {
+          ionInputCtrl.input.setAttribute('aria-labelledby', id);
+
+          $element.on('click', function(e) {
+            console.log('CLICK');
+            $timeout(function() {
+              ionInputCtrl.input.focus();
+            })
+          })
+
+        }
+      }
+    }
   };
 }]);
 
@@ -55,33 +118,7 @@ IonicModule
         if (ionInputCtrl && ionInputCtrl.input) {
           ionInputCtrl.input.setAttribute('aria-labelledby', id);
         }
-      }
-    }
-  };
-}]);
 
-IonicModule
-.directive('ionLabel', [function() {
-  return {
-    restrict: 'E',
-    require: '?^ionInput',
-    compile: function($element, $attrs) {
-
-      return function link($scope, $element, $attrs, ionInputCtrl) {
-        var element = $element[0];
-
-        $element.addClass('input-label');
-
-        $element.attr('aria-label', $element.text());
-        var id = element.id || '_label-' + ++labelIds;
-
-        if (!element.id) {
-          $element.attr('id', id);
-        }
-
-        if (ionInputCtrl && ionInputCtrl.input) {
-          ionInputCtrl.input.setAttribute('aria-labelledby', id);
-        }
       }
     }
   };
