a27a9a2a397d16cc560265ff6e1256849a90f67e
Drew Rygh <drew.rygh@gmail.com>
2015-12-07 20:51:20 -0600

    docs(demos/modal): modal api demo

--- demos/modal/index.ts
+++ demos/modal/index.ts
@@ -3,35 +3,32 @@ import {Modal, ActionSheet, NavController, NavParams, Animation} from 'ionic/ion
 
 
 @App({
+  templateUrl: 'app.html'
+})
+class ApiDemoApp {
+
+  constructor() {
+    this.rootPage = ModalFirstPage;
+  }
+}
+
+@Page({
   templateUrl: 'main.html'
 })
-class MyAppCmp {
-
-  constructor(modal: Modal, app: IonicApp, config: Config, platform: Platform) {
+export class ModalFirstPage {
+  constructor(modal: Modal) {
     this.modal = modal;
-
-    console.log('platforms', platform.platforms());
-    console.log('mode', config.get('mode'));
-
-    console.log('core', platform.is('core'))
-    console.log('cordova', platform.is('cordova'))
-    console.log('mobile', platform.is('mobile'))
-    console.log('ipad', platform.is('ipad'))
-    console.log('iphone', platform.is('iphone'))
-    console.log('phablet', platform.is('phablet'))
-    console.log('tablet', platform.is('tablet'))
-    console.log('ios', platform.is('ios'))
-    console.log('android', platform.is('android'))
-    console.log('windows phone', platform.is('windowsphone'))
-
-    platform.ready().then(() => {
-      console.log('platform.ready')
-    });
-
+    this.myParam = '';
   }
 
-  openModal() {
-    this.modal.open(ContactModal, {
+  openBasicModal() {
+    this.modal.open(ModalContentPage);
+  }
+  openModalWithParams() {
+    this.modal.open(ModalContentPage, { 'myParam': this.myParam });
+  }
+  openCustomAnimationModal() {
+    this.modal.open(ModalContentPage, {}, {
       enterAnimation: 'my-fade-in',
       leaveAnimation: 'my-fade-out',
       handle: 'my-awesome-modal'
@@ -40,154 +37,76 @@ class MyAppCmp {
 }
 
 @Page({
-  template: '<ion-nav [root]="rootView"></ion-nav>'
+    templateUrl: "modal-content.html"
 })
-export class ContactModal {
-  constructor() {
-    console.log('ContactModal constructor')
-    this.rootView = ModalFirstPage;
-  }
-  onViewLoaded() {
-    console.log('ContactModal onViewLoaded');
-  }
-  onViewWillEnter() {
-    console.log('ContactModal onViewWillEnter');
-  }
-  onViewDidEnter() {
-    console.log('ContactModal onViewDidEnter');
-  }
-  onViewWillLeave() {
-    console.log('ContactModal onViewWillLeave');
-  }
-  onViewDidLeave() {
-    console.log('ContactModal onViewDidLeave');
-  }
-  onViewWillUnload() {
-    console.log('ContactModal onViewWillUnload');
-  }
-  onViewDidUnload() {
-    console.log('ContactModal onViewDidUnload');
-  }
-}
+export class ModalContentPage {
+    constructor(
+        nav: NavController,
+        modal: Modal,
+        actionSheet: ActionSheet,
+        params: NavParams
+    ) {
+        this.nav = nav;
+        this.modal = modal;
+        this.actionSheet = actionSheet;
+        this.myParam = params.get('myParam');
+    }
 
-
-@Page({
-  template: `
-    <ion-navbar *navbar><ion-title>First Page Header</ion-title><ion-nav-items primary><button (click)="closeModal()">Close</button></ion-nav-items></ion-navbar>
-    <ion-content padding>
-      <p>
-        <button (click)="push()">Push (Go to 2nd)</button>
-      </p>
-      <p>
-        <button (click)="openActionSheet()">Open Action Sheet</button>
-      </p>
-      <p>
-        <button (click)="closeByHandleModal()">Close By Handle</button>
-      </p>
-      <f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f>
-      <f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f>
-    </ion-content>
-  `
-})
-export class ModalFirstPage {
-  constructor(
-    nav: NavController,
-    modal: Modal,
-    actionSheet: ActionSheet
-  ) {
-    this.nav = nav;
-    this.modal = modal;
-    this.actionSheet = actionSheet;
-  }
-
-  push() {
-    this.nav.push(ModalSecondPage, { id: 8675309, myData: [1,2,3,4] }, { animation: 'ios' });
-  }
-
-  closeModal() {
-    let modal = this.modal.get();
-    modal.close();
-  }
-
-  closeByHandleModal() {
-    debugger;
-    let modal = this.modal.get('my-awesome-modal');
-    modal.close();
-  }
-
-  openActionSheet() {
-    this.actionSheet.open({
-      buttons: [
-        { text: 'Share This' },
-        { text: 'Move' }
-      ],
-      destructiveText: 'Delete',
-      titleText: 'Modify your album',
-      cancelText: 'Cancel',
-      cancel: function() {
-        console.log('Canceled');
-      },
-      destructiveButtonClicked: () => {
-        console.log('Destructive clicked');
-      },
-      buttonClicked: function(index) {
-        console.log('Button clicked', index);
-        if(index == 1) { return false; }
-        return true;
+    closeModal() {
+      let modal = this.modal.get();
+      if (modal) {
+        modal.close();
       }
-    }).then(actionSheetRef => {
-      this.actionSheetRef = actionSheetRef;
-    });
-  }
-}
+    }
 
-
-@Page({
-  template: `
-    <ion-navbar *navbar><ion-title>Second Page Header</ion-title></ion-navbar>
-    <ion-content padding>
-      <p>
-        <button (click)="nav.pop()">Pop (Go back to 1st)</button>
-      </p>
-      <f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f>
-      <f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f><f></f>
-    </ion-content>
-  `
-})
-export class ModalSecondPage {
-  constructor(
-    nav: NavController,
-    params: NavParams
-  ) {
-    this.nav = nav;
-    this.params = params;
-
-    console.log('Second page params:', params);
-  }
-
+    openActionSheet() {
+        this.actionSheet.open({
+            buttons: [
+                { text: 'Share This' },
+                { text: 'Move' }
+            ],
+            destructiveText: 'Delete',
+            titleText: 'Modify your album',
+            cancelText: 'Cancel',
+            cancel: function() {
+                console.log('Canceled');
+            },
+            destructiveButtonClicked: () => {
+                console.log('Destructive clicked');
+            },
+            buttonClicked: function(index) {
+                console.log('Button clicked', index);
+                if (index == 1) { return false; }
+                return true;
+            }
+        }).then(actionSheetRef => {
+            this.actionSheetRef = actionSheetRef;
+        });
+    }
 }
 
 
 class FadeIn extends Animation {
-  constructor(element) {
-    super(element);
+  constructor(enteringView, leavingView) {
+    super(enteringView.pageRef());
     this
       .easing('ease')
-      .duration(450)
-      .fadeIn();
+      .duration(1000)
+      .fromTo('translateY', '0%', '0%')
+      .fadeIn()
+      .before.addClass('show-page');
   }
 }
-
 Animation.register('my-fade-in', FadeIn);
 
 class FadeOut extends Animation {
-  constructor(element) {
-    super(element);
+  constructor(enteringView, leavingView) {
+    super(leavingView.pageRef());
     this
       .easing('ease')
-      .duration(250)
-      .fadeOut();
+      .duration(500)
+      .fadeOut()
+      .before.addClass('show-page');
   }
 }
-
 Animation.register('my-fade-out', FadeOut);
