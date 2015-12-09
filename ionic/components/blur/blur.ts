4995d80bc9cb60827ed137ac9adda93d865681f0
Drew Rygh <drew.rygh@gmail.com>
2015-12-09 15:59:40 -0600

    docs(blur): description

--- ionic/components/blur/blur.ts
+++ ionic/components/blur/blur.ts
@@ -1,5 +1,12 @@
 import {Directive, Renderer, ElementRef} from 'angular2/angular2';
 
+
+/**
+ * The blur attribute applies the CSS blur attribute to an element. If the CSS attribute is not supported,
+ * it will fall back to applying a semi-transparent background color to the element.
+ *
+ * @demo /docs/v2/demos/blur/
+ */
 @Directive({
   selector: '[ion-blur]'
 })
