87006dc5bea24716d181d54ccc18ca9d62d02901
Adam Bradley <adambradley25@gmail.com>
2015-12-10 21:44:55 -0600

    fix(tabs): navbar flicker

--- ionic/animations/ios-transition.ts
+++ ionic/animations/ios-transition.ts
@@ -50,6 +50,7 @@ class IOSTransition extends Animation {
     if (enteringHasNavbar) {
       // entering page has a navbar
       let enteringNavBar = new Animation(enteringView.navbarRef());
+      enteringNavBar.before.addClass('show-navbar');
       this.add(enteringNavBar);
 
       let enteringTitle = new Animation(enteringView.titleRef());
