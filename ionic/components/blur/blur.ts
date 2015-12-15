ade88a92ed0ba197c0c502736e3feeb115c95a5f
mhartington <mikehartington@gmail.com>
2015-12-15 11:47:49 -0500

    docs(blur, config): api clean up

--- ionic/components/blur/blur.ts
+++ ionic/components/blur/blur.ts
@@ -2,8 +2,17 @@ import {Directive, Renderer, ElementRef} from 'angular2/core';
 
 
 /**
+ * @name Blur
+ * @description
  * The blur attribute applies the CSS blur attribute to an element. If the CSS attribute is not supported,
  * it will fall back to applying a semi-transparent background color to the element.
+ *
+ * @usage
+ * ```html
+ * <ion-card blur>
+ *    This card will blur the content behind it.
+ * </ion-card>
+ * ```
  *
  * @demo /docs/v2/demos/blur/
  */
--- ionic/config/config.ts
+++ ionic/config/config.ts
@@ -60,46 +60,27 @@ import {isObject, isDefined, isFunction, isArray, extend} from '../util/util';
  *
  * A config value can come from anywhere and be anything, but there are a default set of values.
  *
- * ``` javascript
- * // iOS
- * activator: 'highlight',
- * actionSheetEnter: 'action-sheet-slide-in',
- * actionSheetLeave: 'action-sheet-slide-out',
- * actionSheetCancelIcon: '',
- * actionSheetDestructiveIcon: '',
- * backButtonText: 'Back',
- * backButtonIcon: 'ion-ios-arrow-back',
- * iconMode: 'ios',
- * menuType: 'reveal',
- * modalEnter: 'modal-slide-in',
- * modalLeave: 'modal-slide-out',
- * pageTransition: 'ios-transition',
- * pageTransitionDelay: 16,
- * popupEnter: 'popup-pop-in',
- * popupLeave: 'popup-pop-out',
- * tabbarPlacement: 'bottom',
-
- * // MD
- * activator: 'ripple',
- * actionSheetEnter: 'action-sheet-md-slide-in',
- * actionSheetLeave: 'action-sheet-md-slide-out',
- * actionSheetCancelIcon: 'ion-md-close',
- * actionSheetDestructiveIcon: 'ion-md-trash',
- * backButtonText: '',
- * backButtonIcon: 'ion-md-arrow-back',
- * iconMode: 'md',
- * menuType: 'overlay',
- * modalEnter: 'modal-md-slide-in',
- * modalLeave: 'modal-md-slide-out',
- * pageTransition: 'md-transition',
- * pageTransitionDelay: 120,
- * popupEnter: 'popup-md-pop-in',
- * popupLeave: 'popup-md-pop-out',
- * tabbarHighlight: true,
- * tabbarPlacement: 'top',
- * tabSubPages: true,
- * ```
  *
+ * | Config property            | Default iOS Value      | Default MD Value          |
+ * |----------------------------|------------------------|---------------------------|
+ * | activator                  | highlight              | ripple                    |
+ * | actionSheetEnter           | action-sheet-slide-in  | action-sheet-md-slide-in  |
+ * | actionSheetLeave           | action-sheet-slide-out | action-sheet-md-slide-out |
+ * | actionSheetCancelIcon      |                        | ion-md-close              |
+ * | actionSheetDestructiveIcon |                        | ion-md-trash              |
+ * | backButtonText             | Back                   |                           |
+ * | backButtonIcon             | ion-ios-arrow-back     | ion-md-arrow-back         |
+ * | iconMode                   | ios                    | md                        |
+ * | menuType                   | reveal                 | overlay                   |
+ * | modalEnter                 | modal-slide-in         | modal-md-slide-in         |
+ * | modalLeave                 | modal-slide-out        | modal-md-slide-out        |
+ * | pageTransition             | ios-transition         | md-transition             |
+ * | pageTransitionDelay        | 16                     | 120                       |
+ * | popupEnter                 | popup-pop-in           | popup-md-pop-in           |
+ * | popupLeave                 | popup-pop-out          | popup-md-pop-out          |
+ * | tabbarPlacement            | bottom                 | true                      |
+ * | tabbarHighlight            |                        | top                       |
+ * | tabSubPage                 |                        | true                      |
  *
 **/
 export class Config {
