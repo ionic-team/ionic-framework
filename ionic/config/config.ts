5ec73a6bf3d5547a79291969d33412d0f2adabb5
mhartington <mikehartington@gmail.com>
2015-12-16 14:52:34 -0500

    docs(config): fix config docs

--- ionic/config/config.ts
+++ ionic/config/config.ts
@@ -63,45 +63,6 @@ import {isObject, isDefined, isFunction, isArray, extend} from '../util/util';
  *
  * A config value can come from anywhere and be anything, but there are a default set of values.
  *
- * ``` javascript
- * // iOS
- * activator: 'highlight',
- * actionSheetEnter: 'action-sheet-slide-in',
- * actionSheetLeave: 'action-sheet-slide-out',
- * actionSheetCancelIcon: '',
- * actionSheetDestructiveIcon: '',
- * backButtonText: 'Back',
- * backButtonIcon: 'ion-ios-arrow-back',
- * iconMode: 'ios',
- * menuType: 'reveal',
- * modalEnter: 'modal-slide-in',
- * modalLeave: 'modal-slide-out',
- * pageTransition: 'ios-transition',
- * pageTransitionDelay: 16,
- * popupEnter: 'popup-pop-in',
- * popupLeave: 'popup-pop-out',
- * tabbarPlacement: 'bottom',
-
- * // MD
- * activator: 'ripple',
- * actionSheetEnter: 'action-sheet-md-slide-in',
- * actionSheetLeave: 'action-sheet-md-slide-out',
- * actionSheetCancelIcon: 'ion-md-close',
- * actionSheetDestructiveIcon: 'ion-md-trash',
- * backButtonText: '',
- * backButtonIcon: 'ion-md-arrow-back',
- * iconMode: 'md',
- * menuType: 'overlay',
- * modalEnter: 'modal-md-slide-in',
- * modalLeave: 'modal-md-slide-out',
- * pageTransition: 'md-transition',
- * pageTransitionDelay: 120,
- * popupEnter: 'popup-md-pop-in',
- * popupLeave: 'popup-md-pop-out',
- * tabbarHighlight: true,
- * tabbarPlacement: 'top',
- * tabSubPages: true,
- * ```
  *
  * | Config property            | Default iOS Value      | Default MD Value          |
  * |----------------------------|------------------------|---------------------------|
