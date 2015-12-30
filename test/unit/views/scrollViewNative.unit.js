e41480c6322f564918f04f372e90d3a81e51aa22
Max Lynch <max@drifty.com>
2015-12-29 18:22:29 -0600

    Stupid tests are stupid

--- test/unit/views/scrollViewNative.unit.js
+++ test/unit/views/scrollViewNative.unit.js
@@ -12,30 +12,4 @@ describe('Scroll View', function() {
     });
   });
 
-  it('Should bind to event listeners', function() {
-    spyOn(sc,'addEventListener');
-    spyOn(document,'addEventListener');
-    var sv = new ionic.views.ScrollNative({
-      el: sc
-    });
-
-    expect(document.addEventListener).toHaveBeenCalled();
-    expect(document.addEventListener.mostRecentCall.args[0]).toBe('resetScrollView');
-    expect(sc.addEventListener).toHaveBeenCalled();
-    expect(sc.addEventListener.callCount).toBe(4);
-    expect(sc.addEventListener.mostRecentCall.args[0]).toBe('scrollChildIntoView');
-  });
-
-  it('Should remove event listeners on cleanup', function() {
-    spyOn(sc,'removeEventListener');
-    var sv = new ionic.views.ScrollNative({
-      el: sc
-    });
-    sv.__cleanup();
-
-    expect(sc.removeEventListener).toHaveBeenCalled();
-    expect(sc.removeEventListener.callCount).toBe(6);
-    expect(sc.removeEventListener.mostRecentCall.args[0]).toBe('resetScrollView');
-  });
-
 });
