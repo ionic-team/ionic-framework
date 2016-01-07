10e0558cd1ff55c5f08f974b51c2cf8a10243d4e
Tim Lancina <tim@ionic.io>
2016-01-07 14:59:25 -0600

    chore(generators): update page generator

--- tooling/generators/page/index.js
+++ tooling/generators/page/index.js
@@ -1,36 +1,12 @@
-/*
-  ionic g page about
-  what should happen:
-    create directories if not existing: /www, /www/app, /www/app/about
-    create files (about.html, about.scss, about.js) in /www/app/about
-*/
-var fs = require('fs'),
-    Generator = module.exports,
-    Generate = require('../../generate'),
-    path = require('path'),
-    Q = require('q');
-/*
-  Run: generate a page template from the name and save
-  it in the desired app directory
-    @options
-      name: Page name
-      appDirectory: App directory of where to save file
-*/
-Generator.run = function run(options) {
-  Generate.createScaffoldDirectories({appDirectory: options.appDirectory, componentDirectory: 'pages', fileName: options.fileName});
 
-  // Generate.defaultTemplates(options)
-  options.rootDirectory = options.rootDirectory || path.join('app', 'pages');
-  var savePath = path.join(options.appDirectory, options.rootDirectory, options.fileName);
+var path = require('path'),
+    Generator = require('../../generator');
 
-  var templates = Generate.loadGeneratorTemplates(__dirname);
+module.exports = PageGenerator;
 
-  templates.forEach(function(template) {
-    options.templatePath = template.file;
-    var renderedTemplate = Generate.renderTemplateFromFile(options);
-    var saveFilePath = path.join(savePath, [options.fileName, template.type].join(''));
-    // console.log('renderedTemplate', renderedTemplate, 'saving to', saveFilePath);
-    console.log('√ Create'.blue, path.relative(options.appDirectory, saveFilePath));
-    fs.writeFileSync(saveFilePath, renderedTemplate);
-  });
-};
+function PageGenerator(options) {
+  Generator.call(this, options);
+  this.directory = path.join('app', 'pages');
+}
+
+PageGenerator.prototype = Object.create(Generator.prototype);
--- tooling/generators/page/page.tmpl.js
+++ tooling/generators/page/page.tmpl.js
@@ -1,7 +1,13 @@
 import {Page, NavController} from 'ionic/ionic';
 
+/*
+  Generated class for the <%= jsClassName %> page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
 @Page({
-  templateUrl: 'app/<%= fileName %>/<%= fileName %>.html',
+  templateUrl: '<%= directory %>/<%= fileName %>/<%= fileName %>.html',
 })
 export class <%= jsClassName %> {
   constructor(nav: NavController) {
