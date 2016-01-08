f7af052330e7b581359f05be2373102c4ed96ebf
Tim Lancina <tim@ionic.io>
2016-01-07 19:26:14 -0600

    fix(generators): replace tabs code lost by atom crashing

--- tooling/generators/tabs/tabs.tmpl.js
+++ tooling/generators/tabs/tabs.tmpl.js
@@ -1,3 +1,14 @@
 import {NavController, Page} from 'ionic/ionic';
+<% _.forEach(tabs, function(tab) { %>import {<%= tab.jsClassName %>} from '../<%= tab.fileName %>/<%= tab.fileName %>';
+<% }); %>
 
-
+@Page({
+  templateUrl: 'build/<%= directory %>/<%= fileName %>/<%= fileName %>.html'
+})
+export class <%= jsClassName %> {
+  constructor(nav: NavController) {
+    // set the root pages for each tab
+    <% _.forEach(tabs, function(tab, i) { %>this.tab<%= ++i %>Root = <%= tab.jsClassName %>;
+    <% }); %>
+  }
+}
