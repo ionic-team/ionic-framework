3f4948b99d489742a228e5e5b2b52886dea0ade8
Tim Lancina <tim@ionic.io>
2016-01-19 16:37:51 -0600

    fix(animation): fix type error
    Closes https://github.com/driftyco/ionic/issues/5042.

--- ionic/animations/animation.ts
+++ ionic/animations/animation.ts
@@ -209,15 +209,15 @@ export class Animation {
 
   get before() {
     return {
-      addClass: (className) => {
+      addClass: (className): Animation => {
         this._bfAdd.push(className);
         return this;
       },
-      removeClass: (className) => {
+      removeClass: (className): Animation => {
         this._bfRmv.push(className);
         return this;
       },
-      setStyles: (styles) => {
+      setStyles: (styles): Animation => {
         this._bfSty = styles;
         return this;
       }
@@ -226,11 +226,11 @@ export class Animation {
 
   get after() {
     return {
-      addClass: (className) => {
+      addClass: (className: string): Animation => {
         this._afAdd.push(className);
         return this;
       },
-      removeClass: (className) => {
+      removeClass: (className: string): Animation => {
         this._afRmv.push(className);
         return this;
       }
