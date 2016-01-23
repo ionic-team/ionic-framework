8fc159e2e8d20117f7a9805a55cb74cc946fd7f1
Brandy Carney <brandyscarney@gmail.com>
2016-01-23 14:31:48 -0500

    test(tabs): add badge test for a dynamic value
    References #5007

--- ionic/components/tabs/test/badges/index.ts
+++ ionic/components/tabs/test/badges/index.ts
@@ -4,6 +4,10 @@ import {App} from 'ionic/ionic';
 @App({
   templateUrl: 'main.html'
 })
-class E2EApp {}
+class E2EApp {
+  myBadge:number = 55;
+
+  constructor() {}
+}
 
 document.body.innerHTML += '<link href="styles.css" rel="stylesheet">'
