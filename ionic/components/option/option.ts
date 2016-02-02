d44fb880a908e5d4fe16598da06672720afe5e08
mhartington <mikehartington@gmail.com>
2016-02-02 11:47:16 -0500

    docs(option): update docs

--- ionic/components/option/option.ts
+++ ionic/components/option/option.ts
@@ -4,6 +4,12 @@ import {isDefined, isTrueProperty} from '../../util/util';
 
 /**
  * @name Option
+ * @description
+ * `ion-option` is a child component of `ion-select`. Similar to the native option element, `ion-option` can take a value and a checked property.
+ *
+ * @property [value] - the value of the option
+ * @property [checked] - whether or not the option is already checked and selected
+ *
  */
 @Directive({
   selector: 'ion-option'
@@ -12,10 +18,16 @@ export class Option {
   private _checked: any = false;
   private _value;
 
+  /**
+   * @private
+   */
   @Output() select: EventEmitter<any> = new EventEmitter();
 
   constructor(private _elementRef: ElementRef) {}
 
+  /**
+   * @private
+   */
   @Input()
   get checked() {
     return this._checked;
@@ -25,6 +37,9 @@ export class Option {
     this._checked = isTrueProperty(val);
   }
 
+  /**
+   * @private
+   */
   @Input()
   get value() {
     if (isDefined(this._value)) {
@@ -37,6 +52,9 @@ export class Option {
     this._value = val;
   }
 
+  /**
+   * @private
+   */
   get text() {
     return this._elementRef.nativeElement.textContent;
   }
