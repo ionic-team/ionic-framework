7d8e567c652658971c7026ee099c066b1a1ea191
Drew Rygh <drew.rygh@gmail.com>
2016-02-09 18:37:01 -0600

    docs(id): fix broken link

--- ionic/components/app/id.ts
+++ ionic/components/app/id.ts
@@ -18,7 +18,7 @@ import {IonicApp} from './app';
  * <ion-checkbox id="myCheckbox"></ion-checkbox>
  * ```
  *
- * To get a reference to the registered component, inject the [IonicApp](../app/IonicApp/)
+ * To get a reference to the registered component, inject the [IonicApp](../IonicApp/)
  * service:
  * ```ts
  * constructor(app: IonicApp) {
