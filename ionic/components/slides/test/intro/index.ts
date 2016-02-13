ebeb940105229459e88540d2e62370cd494d28c2
Brandy Carney <brandyscarney@gmail.com>
2016-02-13 14:03:33 -0500

    test(slides): add navigation to the intro test

--- ionic/components/slides/test/intro/index.ts
+++ ionic/components/slides/test/intro/index.ts
@@ -1,11 +1,44 @@
-import {App} from '../../../../../ionic/ionic';
+import {App, Page, NavController} from 'ionic/ionic';
 
 
 @App({
-  templateUrl: 'main.html'
+  template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 class MyApp {
+  constructor() {
+    this.rootPage = IntroPage;
+  }
+}
+
+@Page({
+  templateUrl: 'main.html'
+})
+class IntroPage {
+  constructor(nav: NavController) {
+    this.nav = nav;
+  }
+
   onSlideChanged(slider) {
     console.log('Slide changed', slider);
   }
+
+  skip() {
+    this.nav.push(MainPage);
+  }
+}
+
+@Page({
+  template: `
+  <ion-navbar *navbar>
+    <ion-title>Slides</ion-title>
+  </ion-navbar>
+
+  <ion-content padding>
+    <h1>Another Page</h1>
+  </ion-content>
+
+  `
+})
+class MainPage {
+
 }
