87364db977a4b9c3c87f9df88de565e0091c9a79
Adam Bradley <adambradley25@gmail.com>
2016-02-18 14:49:20 -0600

    chore(demos): update toggle demo

--- demos/toggle/index.ts
+++ demos/toggle/index.ts
@@ -3,4 +3,23 @@ import {App} from 'ionic/ionic';
 @App({
   templateUrl: 'main.html'
 })
-class ApiDemoApp {}
+class ApiDemoApp {
+  data;
+
+  constructor() {
+    this.data = {
+      frodo: true,
+      sam: false,
+      eowyn: true,
+      legolas: true,
+      gimli: false,
+      saruman: true,
+      gandalf: true,
+      arwen: false,
+      boromir: false,
+      gollum: true,
+      galadriel: false
+    };
+  }
+
+}
