af0d84c7117ab9604a8c7e3f6b9e3cf2586121c5
Drew Rygh <drew.rygh@gmail.com>
2016-02-19 11:05:08 -0600

    fix(deploy): make sure production mode is set to true

--- scripts/docs/deploy.sh
+++ scripts/docs/deploy.sh
@@ -30,7 +30,7 @@ function run {
   cp tmp/sass.json $SITE_DIR/docs/v2/theming/overriding-ionic-variables/
 
   #compile API Demos
-  gulp demos --production
+  gulp demos --production=true
 
   # CD in to the site dir to commit updated docs
   cd $SITE_DIR
