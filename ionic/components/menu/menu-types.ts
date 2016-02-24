96d35ffbe7b3e65f51afe2ec039fdd2909a9bb7c
mhartington <mikehartington@gmail.com>
2016-02-24 14:12:06 -0500

    docs(menuTypes): make private from dgeni

--- ionic/components/menu/menu-types.ts
+++ ionic/components/menu/menu-types.ts
@@ -3,11 +3,11 @@ import {Animation} from '../../animations/animation';
 
 
 /**
+ * @private
  * Menu Type
  * Base class which is extended by the various types. Each
  * type will provide their own animations for open and close
  * and registers itself with Menu.
- * @private
  */
 export class MenuType {
   ani: Animation = new Animation();
@@ -56,6 +56,7 @@ export class MenuType {
 
 
 /**
+ * @private
  * Menu Reveal Type
  * The content slides over to reveal the menu underneath.
  * The menu itself, which is under the content, does not move.
@@ -79,6 +80,7 @@ MenuController.registerType('reveal', MenuRevealType);
 
 
 /**
+ * @private
  * Menu Push Type
  * The content slides over to reveal the menu underneath.
  * The menu itself also slides over to reveal its bad self.
@@ -116,6 +118,7 @@ MenuController.registerType('push', MenuPushType);
 
 
 /**
+ * @private
  * Menu Overlay Type
  * The menu slides over the content. The content
  * itself, which is under the menu, does not move.
