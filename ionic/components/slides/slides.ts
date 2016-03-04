f33a4530cd5409064e45023c747c665f67e25678
Brandy Carney <brandyscarney@gmail.com>
2016-03-04 10:58:16 -0500

    docs(slides): remove inputs from sildes
    fixes #5714

--- ionic/components/slides/slides.ts
+++ ionic/components/slides/slides.ts
@@ -22,7 +22,7 @@ import {Scroll} from '../scroll/scroll';
  * ```ts
  * @Page({
  *  template: `
- *     <ion-slides pager (change)="onSlideChanged($event)" (move)="onSlideMove($event)" loop="true" autoplay="true">
+ *     <ion-slides pager (change)="onSlideChanged($event)" (move)="onSlideMove($event)">
  *      <ion-slide>
  *        <h3>Thank you for choosing the Awesome App!</h3>
  *        <p>
