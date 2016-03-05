64726b8609c719ee5fcdadd05999b7b56fa3836a
Roman Rosluk <roma@redbox.by>
2016-03-05 17:53:38 +0100

    Side menu close with backview fix

--- js/angular/controller/sideMenuController.js
+++ js/angular/controller/sideMenuController.js
@@ -401,7 +401,7 @@ function($scope, $attrs, $ionicSideMenuDelegate, $ionicPlatform, $ionicBody, $io
 
     var backView = $ionicHistory.backView();
 
-    if (backView && (enableMenuWithBackViews || enableMenuSwipeWithBackViews) && self.content.element.offsetWidth > 45) {
+    if (backView && !self.isOpen() && (enableMenuWithBackViews || enableMenuSwipeWithBackViews) && self.content.element.offsetWidth > 45) {
       dragIsWithinBounds = startX > 45 && self.content.element.offsetWidth - 45 > startX;
     } else {
       dragIsWithinBounds = !shouldOnlyAllowEdgeDrag ||
