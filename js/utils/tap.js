cd59c62db264d9dd43d401f0910ba0ba02c2bd78
Tomas Bedrich <ja@tbedrich.cz>
2016-03-06 10:25:38 +0100

    Removed iOS preventDefault on tapTouchStart

--- js/utils/tap.js
+++ js/utils/tap.js
@@ -422,20 +422,6 @@ function tapTouchStart(e) {
 
   tapEventListener(tapTouchMoveListener);
   ionic.activator.start(e);
-
-  if (ionic.Platform.isIOS() && ionic.tap.isLabelWithTextInput(e.target)) {
-    // if the tapped element is a label, which has a child input
-    // then preventDefault so iOS doesn't ugly auto scroll to the input
-    // but do not prevent default on Android or else you cannot move the text caret
-    // and do not prevent default on Android or else no virtual keyboard shows up
-
-    var textInput = tapTargetElement(tapContainingElement(e.target));
-    if (textInput !== tapActiveEle) {
-      // don't preventDefault on an already focused input or else iOS's text caret isn't usable
-      //console.log('Would prevent default here');
-      e.preventDefault();
-    }
-  }
 }
 
 function tapTouchEnd(e) {
