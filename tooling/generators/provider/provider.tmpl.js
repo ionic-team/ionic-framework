44803d3da79a272aea94f6bf0e2d7af03eed87e9
andrewmcgivery <andrew@mcgivery.com>
2016-03-27 17:51:26 -0400

    @Inject to parameters()
    Changed injection of Http to use parameters() instead of @Inject

--- tooling/generators/provider/provider.tmpl.js
+++ tooling/generators/provider/provider.tmpl.js
@@ -1,4 +1,4 @@
-import {Injectable, Inject} from 'angular2/core';
+import {Injectable} from 'angular2/core';
 import {Http} from 'angular2/http';
 
 /*
@@ -9,7 +9,11 @@ import {Http} from 'angular2/http';
 */
 @Injectable()
 export class <%= jsClassName %> {
-  constructor(@Inject(Http) http) {
+  static get parameters(){
+    return [[Http]]
+  }  
+
+  constructor(http) {
     this.http = http;
     this.data = null;
   }
