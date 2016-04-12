932ab42eb1dd73dc1fdd3ed32b2cb36a57d24ae1
Brandy Carney <brandyscarney@gmail.com>
2016-04-11 22:05:05 -0400

    docs(menu): remove basic menu usage doc link

--- ionic/components/menu/menu-controller.ts
+++ ionic/components/menu/menu-controller.ts
@@ -5,9 +5,6 @@ import {MenuType} from './menu-types';
 /**
  * @name Menu
  * @description
- * _For basic Menu usage, see the [Menu section](../../../../components/#menus)
- * of the Component docs._
- *
  * Menu is a side-menu interface that can be dragged and toggled to open or close.
  * An Ionic app can have numerous menus, all of which can be controlled within
  * template HTML, or programmatically.
