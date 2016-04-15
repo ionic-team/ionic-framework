4897f02c110ce790807512f0f637690fa256b62c
Brandy Carney <brandyscarney@gmail.com>
2016-04-15 16:51:21 -0400

    docs(demos): update platform demo to include windows
    add styling to the code blocks to differentiate them more visually

--- demos/platform/index.ts
+++ demos/platform/index.ts
@@ -4,9 +4,15 @@ import {App, Platform} from 'ionic-angular';
   templateUrl: 'main.html'
 })
 class ApiDemoApp {
+  isIos: boolean;
+  isAndroid: boolean;
+  isWindows: boolean;
+  userAgent: string;
+
   constructor(platform: Platform) {
     this.isIos = platform.is('ios');
     this.isAndroid = platform.is('android');
+    this.isWindows = platform.is('windows');
     this.userAgent = platform.userAgent();
   }
 }
