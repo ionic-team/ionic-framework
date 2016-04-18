8b55812ec086b3469fad4d7d077fa0006d1f9a81
Brandy Carney <brandyscarney@gmail.com>
2016-04-18 17:32:48 -0400

    docs(demos): remove user agent from platform demo

--- demos/platform/index.ts
+++ demos/platform/index.ts
@@ -7,12 +7,10 @@ class ApiDemoApp {
   isIos: boolean;
   isAndroid: boolean;
   isWindows: boolean;
-  userAgent: string;
 
   constructor(platform: Platform) {
     this.isIos = platform.is('ios');
     this.isAndroid = platform.is('android');
     this.isWindows = platform.is('windows');
-    this.userAgent = platform.userAgent();
   }
 }
