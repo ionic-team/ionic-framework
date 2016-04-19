864da7652e194eee3bb63caeaf9fcbdb31555c13
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-04-18 23:01:05 -0500

    Revert "fix(scrolling): removing faulty logic that executes on propagated scroll events"
    This reverts commit 7c6fc9263965f24326c3fe31e0db969c442249c0.

--- js/views/listView.js
+++ js/views/listView.js
@@ -566,10 +566,9 @@
     _handleEndDrag: function(e) {
       var self = this;
 
-      /*if (self.scrollView) {
+      if (self.scrollView) {
         self.isScrollFreeze = self.scrollView.freeze(false);
       }
-      */
 
       self._didDragUpOrDown = false;
 
