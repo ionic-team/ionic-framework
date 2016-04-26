8abc52997fde031be4f8e58a9a77b437a7bc5688
Brandy Carney <brandyscarney@gmail.com>
2016-04-26 19:21:04 -0400

    test(button): add e2e test for dynamic button styles and colors
    references #6202

---
+++ ionic/components/button/test/dynamic/index.ts
@@ -0,0 +1,49 @@
+import {App, IonicApp} from 'ionic-angular';
+
+
+@App({
+  templateUrl: 'main.html'
+})
+class E2EApp {
+  isDestructive: boolean;
+  isSecondary: boolean;
+  isCustom: boolean;
+  isOutline: boolean;
+  isClear: boolean;
+  isClicked: boolean;
+  myColor1: string;
+  myColor2: string;
+  multiColor: Array<string>;
+
+  constructor() {
+		this.reset();
+	}
+
+	unify() {
+	  this.isDestructive = false;
+	  this.isSecondary = false;
+	  this.isCustom = false;
+	  this.isOutline = false;
+	  this.isClear = false;
+	  this.isClicked = false;
+	  this.myColor1 = 'primary';
+	  this.myColor2 = 'primary';
+	  this.multiColor = ['primary'];
+	}
+
+	reset() {
+	  this.isDestructive = true;
+	  this.isSecondary = true;
+	  this.isCustom = true;
+	  this.isOutline = true;
+	  this.isClear = true;
+	  this.isClicked = false;
+	  this.myColor1 = 'custom1';
+	  this.myColor2 = 'custom2';
+	  this.multiColor = ['primary','secondary'];
+	}
+
+	toggle() {
+	  this.isClicked = !this.isClicked;
+	}
+}
