aed4fad1b77f46c2dd05f96de87ae9391303f9ef
Brandy Carney <brandyscarney@gmail.com>
2016-05-20 16:26:02 -0400

    chore(snapshot): fix snapshot failing tests and increase time between screenshots

--- scripts/snapshot/snapshot.config.js
+++ scripts/snapshot/snapshot.config.js
@@ -11,7 +11,7 @@ exports.config = {
   specs: 'dist/e2e/**/*e2e.js',
   // specs: 'dist/e2e/button/**/*e2e.js',
 
-  sleepBetweenSpecs: 400,
+  sleepBetweenSpecs: 600,
 
   platformDefauls: {
     browser: 'chrome',
