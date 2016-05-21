702a88225ba6f769ae669a194b9d0c276468a216
Brandy Carney <brandyscarney@gmail.com>
2016-05-21 12:52:17 -0400

    fix(input): fix material design success/error highlighting on inputs
    references #6040
    references #5052

--- src/components/input/test/form-inputs/index.ts
+++ src/components/input/test/form-inputs/index.ts
@@ -1,30 +1,44 @@
 import {App} from '../../../../../src';
-import {FormBuilder, Validators} from '@angular/common';
+import {FormBuilder, Validators, Control} from '@angular/common';
 
 
 @App({
   templateUrl: 'main.html'
 })
 class E2EApp {
+  loginForm: any;
+
+  login = {
+    email: 'help@ionic.io',
+    username: 'admin'
+  };
+
+  user = {
+    username: 'asdf',
+    password: '82'
+  };
+
+  submitted: boolean = false;
+  isTextAreaDisabled: boolean;
+
   constructor(fb: FormBuilder) {
     this.loginForm = fb.group({
-      email: ["", Validators.required],
+      email: ["", Validators.compose([
+        Validators.required,
+        this.emailValidator
+      ])],
       username: [""],
       password: ["", Validators.required],
       comments: ["", Validators.required]
     });
+  }
 
-    this.login = {
-      email: 'help@ionic.io',
-      username: 'admin'
-    };
+  emailValidator(control) {
+    var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-    this.user = {
-      username: 'asdf',
-      password: '82'
-    };
-
-    this.submitted = false;
+    if (!EMAIL_REGEXP.test(control.value)) {
+      return {invalidEmail: true};
+    }
   }
 
   submit(ev, value) {
