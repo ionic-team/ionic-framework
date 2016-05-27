24b7b8f356bd02e1cab228a7c2dedbc83e4dd9b0
Ankush Aggarwal <ankushagg93@gmail.com>
2016-05-26 18:06:56 -0700

    docs(scroll): correct typo (#6673)

--- src/components/scroll/scroll.ts
+++ src/components/scroll/scroll.ts
@@ -10,7 +10,7 @@ import * as util from '../../util';
 /**
  * @name Scroll
  * @description
- * Scroll is a non-flexboxed scroll area that can scroll horizontally or vertically. `ion-Scroll` Can be used in places were you may not need a full page scroller, but a highly customized one, such as image scubber or comment scroller.
+ * Scroll is a non-flexboxed scroll area that can scroll horizontally or vertically. `ion-Scroll` Can be used in places where you may not need a full page scroller, but a highly customized one, such as image scubber or comment scroller.
  * @usage
  * ```html
  * <ion-scroll scrollX="true">
