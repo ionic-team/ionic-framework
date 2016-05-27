2c6e11b10dc53790fd033b1619503adf08a8e87e
Adam Bradley <adambradley25@gmail.com>
2016-05-27 13:20:36 -0500

    feat(range): create ion-range input

---
+++ src/components/range/test/basic/index.ts
@@ -0,0 +1,22 @@
+import {App, Page} from '../../../../../src';
+
+
+@Page({
+  templateUrl: 'page1.html'
+})
+class Page1 {
+  singleValue: number;
+  singleValue2: number = 150;
+  singleValue3: number = 64;
+  singleValue4: number = 1300;
+  dualValue: any;
+  dualValue2 = {lower: 33, upper: 60};
+}
+
+
+@App({
+  templateUrl: 'main.html'
+})
+class E2EApp {
+  rootPage = Page1;
+}
