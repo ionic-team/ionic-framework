f6fe7d7b6ac237d22a02cd42010154dee6b6d470
Antoine Motet <antoine.motet@epitech.eu>
2016-06-13 15:44:55 +0200

    docs(nav-bar): remove unnecessary tags

--- js/angular/directive/navBar.js
+++ js/angular/directive/navBar.js
@@ -42,7 +42,6 @@
  * @param {boolean=} no-tap-scroll By default, the navbar will scroll the content
  * to the top when tapped.  Set no-tap-scroll to true to disable this behavior.
  *
- * </table><br/>
  */
 IonicModule
 .directive('ionNavBar', function() {
