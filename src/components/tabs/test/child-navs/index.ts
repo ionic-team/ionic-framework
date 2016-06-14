b20473e0386841e5b987ca342ecdb28cceba9d31
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-06-14 14:44:18 -0500

    test(tabs): added multiple nested children test
    added multiple nested children test

---
+++ src/components/tabs/test/child-navs/index.ts
@@ -0,0 +1,140 @@
+import {Component} from '@angular/core';
+import {ionicBootstrap, NavController, App, Alert, Modal, ViewController, Tab, Tabs} from '../../../../../src';
+
+
+//
+// Tab 1
+//
+@Component({
+  template: `
+  <ion-navbar *navbar>
+    <ion-title>
+      Tab 1
+    </ion-title>
+  </ion-navbar>
+
+  <ion-content class="home">
+    <ion-nav [root]="root"></ion-nav>
+  </ion-content>
+    `
+})
+export class Tab1 {
+  root = SecondPage;
+}
+
+//
+// Tab 2
+//
+@Component({
+  template: `
+  <ion-navbar *navbar>
+    <ion-title>
+      Tab 2
+    </ion-title>
+  </ion-navbar>
+
+  <ion-content class="home">
+    <ion-nav [root]="root"></ion-nav>
+  </ion-content>
+    `
+})
+export class Tab2 {
+  root = SecondPage;
+}
+
+//
+// Tab 3
+//
+@Component({
+  template: `
+  <ion-navbar *navbar>
+    <ion-title>
+      Tab 3
+    </ion-title>
+  </ion-navbar>
+
+  <ion-content class="home">
+    <ion-nav [root]="root"></ion-nav>
+  </ion-content>
+    `
+})
+export class Tab3 {
+  root = SecondPage;
+}
+
+@Component({
+  template: `
+  <ion-content class="home">
+    <div>SecondPage Cmp</div>
+    <ion-nav [root]="root"></ion-nav>
+  </ion-content>
+  `
+})
+class SecondPage{
+  root = ThirdPage;
+}
+
+@Component({
+  template: `
+  <ion-content class="home">
+    <div>ThirdPage Cmp</div>
+    <ion-nav [root]="root"></ion-nav>
+  </ion-content>
+  `
+})
+class ThirdPage{
+  root = FourthPage;
+}
+
+@Component({
+  template: `
+  <ion-navbar *navbar>
+    <ion-title>
+      Fourth Page Comp
+    </ion-title>
+  </ion-navbar>
+  <ion-content>
+    <ion-list>
+      <ion-item *ngFor="let item of items">
+        {{item}}
+      </ion-item>
+    </ion-list>
+  </ion-content>
+  `
+})
+class FourthPage{
+  private items: string[];
+
+  ionViewWillEnter(){
+    let items: string[] = [];
+    for ( let i = 0 ; i < 500; i++ ){
+      items.push(`Item ${(i + 1)}`);
+    }
+    this.items = items;
+  }
+}
+
+
+@Component({
+  template: `
+    <ion-tabs #content>
+      <ion-tab tabTitle="Tab 1" tabIcon="star" [root]="root1"></ion-tab>
+      <ion-tab tabTitle="Tab 2" tabIcon="globe" [root]="root2"></ion-tab>
+      <ion-tab tabTitle="Tab 3" tabIcon="stopwatch" [root]="root3"></ion-tab>
+    </ion-tabs>
+  `
+})
+export class TabsPage {
+  root1 = Tab1;
+  root2 = Tab2;
+  root3 = Tab3;
+}
+
+@Component({
+  template: `<ion-nav [root]="root"></ion-nav>`
+})
+export class E2EApp {
+  root = TabsPage;
+}
+
+ionicBootstrap(E2EApp);
