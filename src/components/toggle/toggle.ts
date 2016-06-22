4aa322d33b706bdae2dfdb14fed7faa67ca92c72
Manu Mtz.-Almeida <manu.valladolid@gmail.com>
2016-06-22 18:32:28 +0200

    fix(toggle): host listeners are not longer needed

--- src/components/toggle/toggle.ts
+++ src/components/toggle/toggle.ts
@@ -64,13 +64,6 @@ const TOGGLE_VALUE_ACCESSOR = new Provider(
             '[attr.aria-checked]="_checked" ' +
             '[attr.aria-labelledby]="_labelId" ' +
             '[attr.aria-disabled]="_disabled" ' +
-            '(touchstart)=pointerDown($event) ' +
-            '(touchmove)=pointerMove($event) ' +
-            '(mousemove)=pointerMove($event) ' +
-            '(mousedown)=pointerDown($event) ' +
-            '(touchend)=pointerUp($event) ' +
-            '(mouseup)=pointerUp($event) ' +
-            '(mouseout)=pointerUp($event) ' +
             'class="item-cover">' +
     '</button>',
   host: {
