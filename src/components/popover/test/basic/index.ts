55bc32dafe2f16f6cdcd1bf4b727a52405fc6099
Brandy Carney <brandyscarney@gmail.com>
2016-07-08 14:25:21 -0400

    fix(popover): remove min-height from ios, add sass variables
    sass variables added for min-width and min-height.
    
    closes #7215

--- src/components/popover/test/basic/index.ts
+++ src/components/popover/test/basic/index.ts
@@ -139,8 +139,6 @@ class PopoverRadioPage {
       <ion-list-header>Ionic</ion-list-header>
       <button ion-item (click)="close()">Learn Ionic</button>
       <button ion-item (click)="close()">Documentation</button>
-      <button ion-item (click)="close()">Showcase</button>
-      <button ion-item (click)="close()">GitHub Repo</button>
     </ion-list>
   `
 })
