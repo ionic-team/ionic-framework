d92c8ebc44f7fc11e9587f7d3d1975b8fa03e930
matthew bishop <matthew.g.bishop@accenture.com>
2016-07-12 10:25:51 +0000

    make sure rapidUpdate doesnt fire before slider is ready

--- src/components/slides/slides.ts
+++ src/components/slides/slides.ts
@@ -396,6 +396,8 @@ export class Slides extends Ion {
     setTimeout(() => {
       var swiper = new Swiper(this.getNativeElement().children[0], options);
       this.slider = swiper;
+
+      this.rapidUpdate();
     }, 300);
 
     /*
@@ -866,8 +868,6 @@ export class Slide {
   ) {
     this.ele = elementRef.nativeElement;
     this.ele.classList.add('swiper-slide');
-
-    slides.rapidUpdate();
   }
 
   ngOnDestroy() {
