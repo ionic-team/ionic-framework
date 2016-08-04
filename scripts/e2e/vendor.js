a1b84f3cb596e628dc5001c9ad8043df1b03c130
Adam Bradley <adambradley25@gmail.com>
2016-08-04 09:49:46 -0500

    chore(): remove router dep

--- scripts/e2e/vendor.js
+++ scripts/e2e/vendor.js
@@ -1,7 +1,6 @@
 // Angular 2
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
-import '@angular/router';
 import '@angular/common';
 import '@angular/core';
 import '@angular/http';
