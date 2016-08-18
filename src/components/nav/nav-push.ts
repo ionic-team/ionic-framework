6fb848cc4ae727cd0d9eb338efeb0ec73fe222e7
Justin Willis <jgw9617@gmail.com>
2016-08-18 09:26:44 -0500

    fix(docs): get rid of reference to @Page

--- src/components/nav/nav-push.ts
+++ src/components/nav/nav-push.ts
@@ -23,7 +23,7 @@ import { noop } from '../../util/util';
  *
  * Where `pushPage` and `params` are specified in your component,
  * and `pushPage` contains a reference to a
- * [@Page component](../../../config/Page/):
+ * component you would like to push:
  *
  * ```ts
  * import { LoginPage } from './login';
