8f270f4f44c1fb02be1de35f37b8683d1215006b
Francis Clavette <clavette.francis@gmail.com>
2016-08-20 22:31:38 -0400

    docs(item): fix typo in sliding item docs
    ionSiwpe -> ionSwipe

--- src/components/item/item-sliding.ts
+++ src/components/item/item-sliding.ts
@@ -19,7 +19,7 @@ export const enum ItemSideFlags {
  * @name ItemOptions
  * @description
  * The option buttons for an `ion-item-sliding`. These buttons can be placed either on the left or right side.
- * You can combind the `(ionSiwpe)` event plus the `expandable` directive to create a full swipe action for the item.
+ * You can combind the `(ionSwipe)` event plus the `expandable` directive to create a full swipe action for the item.
  *
  * @usage
  *
