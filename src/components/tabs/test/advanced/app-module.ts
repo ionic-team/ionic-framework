fb5e9f4be9e30d38ad68d75c8ccd20baa74ba80a
Adam Bradley <adambradley25@gmail.com>
2016-08-31 14:21:54 -0500

    test: temp remove DeepLink decorator

--- src/components/tabs/test/advanced/app-module.ts
+++ src/components/tabs/test/advanced/app-module.ts
@@ -1,8 +1,8 @@
 import { Component, NgModule, ViewChild } from '@angular/core';
-import { DeepLink, DeepLinkConfig, IonicApp, IonicModule, App, NavController, NavParams, ModalController, ViewController, Tabs, Tab } from '../../../..';
+import { /*DeepLink,*/ DeepLinkConfig, IonicApp, IonicModule, App, NavController, NavParams, ModalController, ViewController, Tabs, Tab } from '../../../..';
 
 
-@DeepLink({ name: 'sign-in' })
+// @DeepLink({ name: 'sign-in' })
 @Component({
   templateUrl: './signIn.html'
 })
@@ -26,7 +26,7 @@ export class ChatPage {
 }
 
 
-@DeepLink({ name: 'tabs' })
+// @DeepLink({ name: 'tabs' })
 @Component({
   templateUrl: './tabs.html'
 })
@@ -78,7 +78,7 @@ export class TabsPage {
 //
 // tab 1
 //
-@DeepLink({ name: 'tab1-page1' })
+// @DeepLink({ name: 'tab1-page1' })
 @Component({
   templateUrl: './tab1page1.html'
 })
@@ -126,7 +126,7 @@ export class Tab1Page1 {
 }
 
 
-@DeepLink({ name: 'tab1-page2' })
+// @DeepLink({ name: 'tab1-page2' })
 @Component({
   templateUrl: './tab1page2.html'
 })
@@ -154,7 +154,7 @@ export class Tab1Page2 {
 }
 
 
-@DeepLink({ name: 'tab1-page3' })
+// @DeepLink({ name: 'tab1-page3' })
 @Component({
   templateUrl: './tab1page3.html'
 })
@@ -187,7 +187,7 @@ export class Tab1Page3 {
 //
 // tab 2
 //
-@DeepLink({ name: 'tab2-page1' })
+// @DeepLink({ name: 'tab2-page1' })
 @Component({
   templateUrl: './tab2page1.html'
 })
@@ -215,7 +215,7 @@ export class Tab2Page1 {
 }
 
 
-@DeepLink({ name: 'tab2-page2' })
+// @DeepLink({ name: 'tab2-page2' })
 @Component({
   templateUrl: './tab2page2.html'
 })
@@ -243,10 +243,10 @@ export class Tab2Page2 {
 }
 
 
-@DeepLink({
-  name: 'tab2-page3',
-  defaultHistory: []
-})
+// @DeepLink({
+//   name: 'tab2-page3',
+//   defaultHistory: []
+// })
 @Component({
   templateUrl: './tab2page3.html'
 })
@@ -279,7 +279,7 @@ export class Tab2Page3 {
 //
 // tab 3
 //
-@DeepLink({ name: 'tab3-page1' })
+// @DeepLink({ name: 'tab3-page1' })
 @Component({
   templateUrl: './tab3page1.html'
 })
