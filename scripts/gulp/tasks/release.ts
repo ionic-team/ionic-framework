c896f39d42cbcb01589faca80be5396a7118ecaa
Adam Bradley <adambradley25@gmail.com>
2016-09-03 22:27:35 -0500

    style: fix indentation

--- scripts/gulp/tasks/release.ts
+++ scripts/gulp/tasks/release.ts
@@ -1,37 +1,38 @@
-import {dest, src, task} from 'gulp';
-import { SRC_ROOT, DIST_BUILD_ROOT, PROJECT_ROOT } from '../constants';
+import { dest, src, task } from 'gulp';
+import { DIST_BUILD_ROOT, SRC_ROOT, PROJECT_ROOT } from '../constants';
 import { compileSass, copyFonts } from '../util';
 
+
 task('nightly', (done: Function) => {
-    const runSequence = require('run-sequence');
-    runSequence('release.nightlyPackage', 'release.publishNightly', done);
+  const runSequence = require('run-sequence');
+  runSequence('release.nightlyPackage', 'release.publishNightly', done);
 });
 
 task('release.prepareNightly', (done: Function) => {
-    const runSequence = require('run-sequence');
-    runSequence(/*'release.pullLatest', 'validate', */'release.copyTools', 'release.copyNpmInfo', 'release.preparePackageJsonTemplate', 'release.nightlyPackageJson', done);
+  const runSequence = require('run-sequence');
+  runSequence(/*'release.pullLatest', 'validate', */'release.copyTools', 'release.copyNpmInfo', 'release.preparePackageJsonTemplate', 'release.nightlyPackageJson', done);
 });
 
 task('release.nightlyPackage', (done: Function) => {
-    const runSequence = require('run-sequence');
-    runSequence('clean', /*'release.prepareNightly',*/ 'compile.release', 'release.prepareNightly', 'release.compileSass', 'release.fonts', 'release.scss', done);
+  const runSequence = require('run-sequence');
+  runSequence('clean', /*'release.prepareNightly',*/ 'compile.release', 'release.prepareNightly', 'release.compileSass', 'release.fonts', 'release.scss', done);
 });
 
 task('release.publishNightly', (done: Function) => {
-    var spawn = require('child_process').spawn;
+  const spawn = require('child_process').spawn;
 
-    var npmCmd = spawn('npm', ['publish', '--tag=nightly', DIST_BUILD_ROOT]);
-    npmCmd.stdout.on('data', function (data) {
-        console.log(data.toString());
-    });
+  const npmCmd = spawn('npm', ['publish', '--tag=nightly', DIST_BUILD_ROOT]);
+  npmCmd.stdout.on('data', function (data) {
+    console.log(data.toString());
+  });
 
-    npmCmd.stderr.on('data', function (data) {
-        console.log('npm err: ' + data.toString());
-    });
+  npmCmd.stderr.on('data', function (data) {
+    console.log('npm err: ' + data.toString());
+  });
 
-    npmCmd.on('close', function() {
-        done();
-    });
+  npmCmd.on('close', function() {
+    done();
+  });
 });
 
 task('release.compileSass', () => {
@@ -43,84 +44,83 @@ task('release.fonts', () => {
 });
 
 task('release.scss', () => {
-    return src([`${SRC_ROOT}/**/*.scss`, `!${SRC_ROOT}/components/*/test/**/*`, `!${SRC_ROOT}/util/test/*`]).pipe(dest(`${DIST_BUILD_ROOT}`));
+  return src([`${SRC_ROOT}/**/*.scss`, `!${SRC_ROOT}/components/*/test/**/*`, `!${SRC_ROOT}/util/test/*`]).pipe(dest(`${DIST_BUILD_ROOT}`));
 });
 
 task('release.pullLatest', (done: Function) => {
-    const gulpGit = require('gulp-git');
-    gulpGit.pull('origin', ['master'], err => {
-        done(err);
-    });
+  const gulpGit = require('gulp-git');
+  gulpGit.pull('origin', ['master'], err => {
+    done(err);
+  });
 });
 
 task('release.prepareChangelog', () => {
-    const changelog = require('gulp-conventional-changelog');
-    return gulp.src(`${PROJECT_ROOT}/CHANGELOG.md`)
-    .pipe(changelog({
-      preset: 'angular'
-    }))
-    .pipe(gulp.dest(`${PROJECT_ROOT}`));
+  const changelog = require('gulp-conventional-changelog');
+  return gulp.src(`${PROJECT_ROOT}/CHANGELOG.md`)
+         .pipe(changelog({
+           preset: 'angular'
+         }))
+         .pipe(gulp.dest(`${PROJECT_ROOT}`));
 });
 
 
 task('release.prepareRootPackageJson', () => {
-    const semver = require('semver');
-    const fs = require('fs');
+  const semver = require('semver');
+  const fs = require('fs');
 
-    let packageJSON = require('./package.json');
-    packageJSON.version = semver.inc(packageJSON.version, 'prerelease', 'beta');
-    fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, 2));
+  let packageJSON = require('./package.json');
+  packageJSON.version = semver.inc(packageJSON.version, 'prerelease', 'beta');
+  fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, 2));
 });
 
 task('release.copyTools', () => {
-    return src([`${PROJECT_ROOT}/tooling/**/*`]).pipe(dest(`${DIST_BUILD_ROOT}/tooling`));
+  return src([`${PROJECT_ROOT}/tooling/**/*`]).pipe(dest(`${DIST_BUILD_ROOT}/tooling`));
 });
 
 task('release.copyNpmInfo', () => {
-    return src([`${PROJECT_ROOT}/scripts/npm/.npmignore`, `${PROJECT_ROOT}/scripts/npm/README.md`]).pipe(dest(DIST_BUILD_ROOT));
+  return src([`${PROJECT_ROOT}/scripts/npm/.npmignore`, `${PROJECT_ROOT}/scripts/npm/README.md`]).pipe(dest(DIST_BUILD_ROOT));
 });
 
 task('release.preparePackageJsonTemplate', () => {
-    const fs = require('fs');
-    let templatePackageJSON = require(`${PROJECT_ROOT}/scripts/npm/package.json`);
-    const sourcePackageJSON = require(`${PROJECT_ROOT}/package.json`);
-    const sourceDependencies = sourcePackageJSON.dependencies;
-    // copy source package.json data to template
-    templatePackageJSON.version = sourcePackageJSON.version;
-    templatePackageJSON.description = sourcePackageJSON.description;
-    templatePackageJSON.keywords = sourcePackageJSON.keywords;
+  const fs = require('fs');
+  let templatePackageJSON = require(`${PROJECT_ROOT}/scripts/npm/package.json`);
+  const sourcePackageJSON = require(`${PROJECT_ROOT}/package.json`);
+  const sourceDependencies = sourcePackageJSON.dependencies;
+  // copy source package.json data to template
+  templatePackageJSON.version = sourcePackageJSON.version;
+  templatePackageJSON.description = sourcePackageJSON.description;
+  templatePackageJSON.keywords = sourcePackageJSON.keywords;
 
-    // copy source dependencies versions to the template's peerDependencies
-    // only copy dependencies that show up as peerDependencies in the template
-    for (let dependency in sourceDependencies) {
-        if (dependency in templatePackageJSON.peerDependencies) {
-            templatePackageJSON.peerDependencies[dependency] = sourceDependencies[dependency];
-        }
+  // copy source dependencies versions to the template's peerDependencies
+  // only copy dependencies that show up as peerDependencies in the template
+  for (let dependency in sourceDependencies) {
+    if (dependency in templatePackageJSON.peerDependencies) {
+      templatePackageJSON.peerDependencies[dependency] = sourceDependencies[dependency];
     }
+  }
 
-    fs.writeFileSync(`${DIST_BUILD_ROOT}` + '/package.json', JSON.stringify(templatePackageJSON, null, 2));
+  fs.writeFileSync(`${DIST_BUILD_ROOT}` + '/package.json', JSON.stringify(templatePackageJSON, null, 2));
 });
 
 task('release.nightlyPackageJson', () => {
-    const fs = require('fs');
-    let packageJson: any = require(`${DIST_BUILD_ROOT}/package.json`);
+  const fs = require('fs');
+  const packageJson: any = require(`${DIST_BUILD_ROOT}/package.json`);
 
-    // Generate a unique id formatted from current timestamp
-    function createTimestamp() {
-        // YYYYMMDDHHMM
-        var d = new Date();
-        return d.getUTCFullYear() + // YYYY
-            ('0' + (d.getUTCMonth() + 1)).slice(-2) + // MM
-            ('0' + (d.getUTCDate())).slice(-2) + // DD
-            ('0' + (d.getUTCHours())).slice(-2) + // HH
-            ('0' + (d.getUTCMinutes())).slice(-2); // MM
-    }
+  // Generate a unique id formatted from current timestamp
+  function createTimestamp() {
+    // YYYYMMDDHHMM
+    var d = new Date();
+    return d.getUTCFullYear() + // YYYY
+           ('0' + (d.getUTCMonth() + 1)).slice(-2) + // MM
+           ('0' + (d.getUTCDate())).slice(-2) + // DD
+           ('0' + (d.getUTCHours())).slice(-2) + // HH
+           ('0' + (d.getUTCMinutes())).slice(-2); // MM
+  }
 
-
-    packageJson.version = packageJson.version.split('-')
-    .slice(0, 2)
-    .concat(createTimestamp())
-    .join('-');
+  packageJson.version = packageJson.version.split('-')
+                                   .slice(0, 2)
+                                   .concat(createTimestamp())
+                                   .join('-');
 
   fs.writeFileSync(`${DIST_BUILD_ROOT}/package.json`, JSON.stringify(packageJson, null, 2));
 });
