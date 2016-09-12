b69a4da2b424f2880e91333c5f99664028e35e39
Justin Willis <jgw9617@gmail.com>
2016-09-12 16:58:13 -0500

    fix(): remove base option

--- scripts/gulp/tasks/polyfills.ts
+++ scripts/gulp/tasks/polyfills.ts
@@ -7,7 +7,7 @@ task('polyfills', (done) => {
         'node_modules/zone.js/proxy.min.js',
         'node_modules/core-js/es6/index.js',
         'node_modules/core-js/es7/reflect.js'
-    ], {base: 'node_modules/'})
+    ])
     .pipe(concat('polyfills.js'))
     .pipe(dest('dist/ionic-angular/polyfills/'), done);
 });
