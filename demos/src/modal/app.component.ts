1158a96288004f34ead58fa1a933aa3977a74347
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-09-16 11:09:09 -0500

    chore(build): all demos build in ngc w/ 2.0.0
    all demos build in ngc w/ 2.0.0

--- demos/src/modal/app.component.ts
+++ demos/src/modal/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Animation, ModalController, NavParams, ViewController } from 'ionic-angular';
+import { ModalController, NavParams, ViewController } from 'ionic-angular';
 
 
 @Component({
@@ -17,12 +17,6 @@ export class ModalFirstPage {
   }
   openModalWithParams() {
     let myModal = this.modalCtrl.create(ModalContentPage, { 'myParam': this.myParam });
-    myModal.present();
-  }
-  openCustomAnimationModal() {
-    let myModal = this.modalCtrl.create(ModalContentPage, {
-      animation: 'my-fade-in',
-    });
     myModal.present();
   }
 }
@@ -53,28 +47,3 @@ export class ModalContentPage {
 export class ApiDemoApp {
   root = ModalFirstPage;
 }
-
-export class FadeIn extends Animation {
-  constructor(enteringView: ViewController, leavingView: ViewController) {
-    super(enteringView.pageRef());
-    this
-      .easing('ease')
-      .duration(1000)
-      .fromTo('translateY', '0%', '0%')
-      .fromTo('opacity', 0, 1)
-      .beforeAddClass('show-page');
-  }
-}
-Animation.register('my-fade-in', FadeIn);
-
-export class FadeOut extends Animation {
-  constructor(enteringView: ViewController, leavingView: ViewController) {
-    super(leavingView.pageRef());
-    this
-      .easing('ease')
-      .duration(500)
-      .fromTo('opacity', 1, 0)
-      .beforeAddClass('show-page');
-  }
-}
-Animation.register('my-fade-out', FadeOut);
