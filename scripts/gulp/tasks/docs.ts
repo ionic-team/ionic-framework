65ba554b388fac0b7c5362c994032d46ffb43ed8
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-09-21 14:25:43 -0500

    chore(build): changed docs task name
    changed docs task name

--- scripts/gulp/tasks/docs.ts
+++ scripts/gulp/tasks/docs.ts
@@ -11,8 +11,9 @@ import { argv } from 'yargs';
 
 import { DEMOS_ROOT } from '../constants';
 
+task('docs', ['docs.dgeni', 'docs.copyDemos', 'docs.sassVariables']);
 
-task('docs', () => {
+task('docs.dgeni', () => {
   const docVersion = argv['doc-version'] || 'nightly';
   const initialVersionBuild = argv['initial-build'] || false;
   if (docVersion !== 'nightly' && ! valid(docVersion)) {
