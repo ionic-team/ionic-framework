248d0c1a9c13ae23787e03b8b324717a903bfc06
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-09-27 16:48:27 -0500

    chore(e2e): add css back to e2e tests where it was missing
    add css back to e2e tests where it was missing

--- src/components/tabs/test/colors/app-module.ts
+++ src/components/tabs/test/colors/app-module.ts
@@ -8,7 +8,25 @@ export class E2EPage {}
 
 @Component({
   templateUrl: 'main.html',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  styles: [
+    `
+      ion-tabs {
+        margin-bottom: 20px;
+      }
+    `,
+
+    `
+      ion-tabs,
+      ion-tabs ion-tabbar {
+        position: relative;
+        top: auto;
+        height: auto;
+        visibility: visible;
+        opacity: 1;
+      }
+    `
+  ]
 })
 export class E2EApp {
   root = E2EPage;
