417997ddc4865a43ce6aca51906badb3180a2b9f
Max Lynch <max@drifty.com>
2016-10-20 15:50:07 -0500

    fix(select): don't simulate click. Fixes #8181

--- js/utils/tap.js
+++ js/utils/tap.js
@@ -486,6 +486,10 @@ function tapIgnoreEvent(e) {
     return true;
   }
 
+  if(e.target.tagName == 'SELECT') {
+    return true;
+  }
+
   if (ionic.scroll.isScrolling && ionic.tap.containsOrIsTextInput(e.target)) {
     e.preventDefault();
     return true;
