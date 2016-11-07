bc5cd7f03f68b53fa606224877660d1ccf009a03
mhartington <mikehartington@gmail.com>
2016-11-07 14:52:05 -0500

    docs(input): update input docs

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -120,7 +120,7 @@ export class TextInput extends InputBase {
   }
 
   /**
-   * @private
+   * @input {string} The text value of the input
    */
   @Input()
   get value() {
@@ -142,7 +142,7 @@ export class TextInput extends InputBase {
   }
 
   /**
-   * @private
+   * @input {bool} If the input should be disabled or not
    */
   @Input()
   get disabled() {
@@ -305,12 +305,12 @@ export class TextArea extends InputBase {
   }
 
   /**
-   * @private
+   * @input {string} The placeholder for the textarea
    */
   @Input() placeholder: string = '';
 
   /**
-   * @private
+   * @input {string} The value of the textarea
    */
   @Input()
   get value() {
@@ -332,7 +332,7 @@ export class TextArea extends InputBase {
   }
 
   /**
-   * @private
+   * @input {bool} Wheather the textara should be disabled or not
    */
   @Input()
   get disabled() {
@@ -367,12 +367,12 @@ export class TextArea extends InputBase {
   }
 
   /**
-   * @private
+   * @output {event} Expression to call when the textarea no longer has focus
    */
   @Output() blur: EventEmitter<Event> = new EventEmitter<Event>();
 
   /**
-   * @private
+   * @output {event} Expression to call when the textarea has focus
    */
   @Output() focus: EventEmitter<Event> = new EventEmitter<Event>();
 
