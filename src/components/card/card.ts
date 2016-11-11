ee3decc31d69c0d4f66e077c66a4cacb8592a17d
Brandy Carney <brandyscarney@users.noreply.github.com>
2016-11-10 20:43:22 -0500

    fix(colors): override element styling and tweak colors for card and item
    * refactor(card): add color/mode classes to header, title, content
    
    - Remove native HTML element styling inside of cards (headings,
    paragraph)
    - Move the default color for the HTML elements to the content
    - Generate colors for the card itself, the header, the content, and the
    title using the color input
    - Add e2e test for card colors
    
    References #8330
    
    * refactor(item): remove paragraph styles, update activated colors
    
    - Adds test for item colors
    - Removes the color from native paragraph elements allowing colors to
    be passed
    
    Closes #9081
    
    * refactor(item): bring back paragraph color and override in the item loop
    
    * refactor(card): add back heading and paragraph colors and override in color loop

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -43,7 +43,31 @@ export class Card extends Ion {
 @Directive({
   selector: 'ion-card-content'
 })
-export class CardContent {}
+export class CardContent extends Ion {
+
+  /**
+   * @input {string} The predefined color to use. For example: `"primary"`, `"secondary"`, `"danger"`.
+   */
+  @Input()
+  set color(val: string) {
+    this._setColor('card-content', val);
+  }
+
+  /**
+   * @input {string} The mode to apply to this component.
+   */
+  @Input()
+  set mode(val: string) {
+    this._setMode('card-content', val);
+  }
+
+  constructor(config: Config, elementRef: ElementRef, renderer: Renderer) {
+    super(config, elementRef, renderer);
+
+    this.mode = config.get('mode');
+  }
+
+}
 
 
 /**
@@ -52,7 +76,31 @@ export class CardContent {}
 @Directive({
   selector: 'ion-card-header'
 })
-export class CardHeader {}
+export class CardHeader extends Ion {
+
+  /**
+   * @input {string} The predefined color to use. For example: `"primary"`, `"secondary"`, `"danger"`.
+   */
+  @Input()
+  set color(val: string) {
+    this._setColor('card-header', val);
+  }
+
+  /**
+   * @input {string} The mode to apply to this component.
+   */
+  @Input()
+  set mode(val: string) {
+    this._setMode('card-header', val);
+  }
+
+  constructor(config: Config, elementRef: ElementRef, renderer: Renderer) {
+    super(config, elementRef, renderer);
+
+    this.mode = config.get('mode');
+  }
+
+}
 
 /**
  * @private
@@ -60,4 +108,28 @@ export class CardHeader {}
 @Directive({
   selector: 'ion-card-title'
 })
-export class CardTitle {}
+export class CardTitle extends Ion {
+
+  /**
+   * @input {string} The predefined color to use. For example: `"primary"`, `"secondary"`, `"danger"`.
+   */
+  @Input()
+  set color(val: string) {
+    this._setColor('card-title', val);
+  }
+
+  /**
+   * @input {string} The mode to apply to this component.
+   */
+  @Input()
+  set mode(val: string) {
+    this._setMode('card-title', val);
+  }
+
+  constructor(config: Config, elementRef: ElementRef, renderer: Renderer) {
+    super(config, elementRef, renderer);
+
+    this.mode = config.get('mode');
+  }
+
+}
