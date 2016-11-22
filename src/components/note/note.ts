bf00803737e547c78409f5322a98e57c6a2e4fc1
Brandy Carney <brandyscarney@gmail.com>
2016-11-22 12:45:40 -0500

    fix(note): separate note from item so its styles will alway be applied
    this was discussed with Ben previously, fixes #9173

--- src/components/note/note.ts
+++ src/components/note/note.ts
@@ -1,5 +1,7 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer } from '@angular/core';
 
+import { Config } from '../../config/config';
+import { Ion } from '../ion';
 
 /**
   * @private
@@ -7,4 +9,26 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'ion-note'
 })
-export class Note {}
+export class Note extends Ion {
+
+  /**
+   * @input {string} The predefined color to use. For example: `"primary"`, `"secondary"`, `"danger"`.
+   */
+  @Input()
+  set color(val: string) {
+    this._setColor(val);
+  }
+
+  /**
+   * @input {string} The mode to apply to this component.
+   */
+  @Input()
+  set mode(val: string) {
+    this._setMode(val);
+  }
+
+  constructor(config: Config, elementRef: ElementRef, renderer: Renderer) {
+    super(config, elementRef, renderer, 'note');
+  }
+
+}
