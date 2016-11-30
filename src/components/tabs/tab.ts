3fd9a205d7c23dcd92b5b12b53a0fd1a712c54f8
Diego <dscafati@yahoo.com.ar>
2016-11-30 00:29:08 -0300

    docs(tabs): fix closing tag

--- src/components/tabs/tab.ts
+++ src/components/tabs/tab.ts
@@ -35,7 +35,7 @@ import { ViewController } from '../../navigation/view-controller';
  *
  * ```html
  * <ion-tabs>
- *  <ion-tab [root]="chatRoot" tabTitle="Chat" tabIcon="chat"><ion-tab>
+ *  <ion-tab [root]="chatRoot" tabTitle="Chat" tabIcon="chat"></ion-tab>
  * </ion-tabs>
  * ```
  *
