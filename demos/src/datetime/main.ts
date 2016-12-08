f078bf3eac9c7bc4eb09b0b9571bdaec9e0d6b81
Brandy Carney <brandyscarney@gmail.com>
2016-12-08 16:29:17 -0500

    chore(demos): remove the main.ts files from each demo

--- demos/src/datetime/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './app.module.ngfactory';
-
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
