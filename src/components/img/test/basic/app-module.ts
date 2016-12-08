8417c51655087e154096c703a92d5803a0d1f547
Brandy Carney <brandyscarney@gmail.com>
2016-12-08 11:08:43 -0500

    test(img): add tests for the img component

--- src/components/img/test/basic/app-module.ts
+++ src/components/img/test/basic/app-module.ts
@@ -1,11 +1,38 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from '../../../..';
 
+
+@Component({
+  selector: '<my-img>',
+  template: '<ion-img [width]="width" [height]="height" [src]="src"></ion-img>'
+})
+export class MyImg {
+  @Input() width: any;
+  @Input() height: any;
+  @Input() src: any;
+}
 
 @Component({
   templateUrl: 'main.html'
 })
 export class E2EPage {
+  images = [
+    {
+      width: '100',
+      height: '44',
+      src: 'http://localhost:8000/dist/e2e/img/img/batmobile.jpg'
+    },
+    {
+      width: '100',
+      height: '75',
+      src: 'http://localhost:8000/dist/e2e/img/img/knight-rider.jpg'
+    },
+    {
+      width: '100',
+      height: '68',
+      src: 'http://localhost:8000/dist/e2e/img/img/general-lee.jpg'
+    }
+  ];
 
 }
 
@@ -21,7 +48,8 @@ export class E2EApp {
 @NgModule({
   declarations: [
     E2EApp,
-    E2EPage
+    E2EPage,
+    MyImg
   ],
   imports: [
     IonicModule.forRoot(E2EApp)
