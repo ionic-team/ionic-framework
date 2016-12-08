ec17ef554a897d9de08064cd2243745dafad88cc
Manu Mtz.-Almeida <manu.valladolid@gmail.com>
2016-12-08 22:52:05 +0100

    fix(item): tappable does not make ion-label pointer-event:none

--- src/components/item/test/buttons/app-module.ts
+++ src/components/item/test/buttons/app-module.ts
@@ -9,6 +9,10 @@ export class E2EPage {
   testClick(ev: any) {
     console.log('CLICK!', ev.target.tagName, ev.target.textContent.trim());
   }
+
+  testClickOutsize(ev: any) {
+    console.log('CLICK OUTSIDE!', ev.target.tagName, ev.target.textContent.trim());
+  }
 }
 
 @Component({
