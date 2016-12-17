cc72a7868470e0c1ca82aaaca16f9ca5b4f853ef
Alex Muramoto <amuramoto@apache.org>
2016-12-16 17:20:22 -0800

    docs(avatar): add usage example and edit doc

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -4,9 +4,21 @@ import { Directive } from '@angular/core';
   * @name Avatar
   * @module ionic
   * @description
-  * An Avatar is a component that creates a circular image for an item.
-  * Avatar's can be place on the left or right side of an item with the `item-left` or `item-right` directive.
-  * @see {@link /docs/v2/components/#avatar-list Avatar Component Docs}
+  * The Avatar component creates a circular image. Avatar's must be nested inside `ion-item`, and
+  * can be positioned on the left or right side of an item with the `item-left` or `item-right` directive.
+  *
+  * @usage
+  * ```
+  * <ion-item>
+  *   <ion-avatar item-left>
+  *     <img src="img/avatar-cher.png">
+  *   </ion-avatar>
+  * </ion-item>
+  * ```
+  *
+  * @demo /docs/v2/demos/src/avatar/
+  * @see {@link /docs/v2/components/#list Avatar Component Docs}
+  * @see {@link ../item/Item Item API Docs}
  */
 @Directive({
   selector: 'ion-avatar'
