7e667cd4c8fc9110f226013f9f18ff2ffc7603cb
Adam Bradley <adambradley25@gmail.com>
2016-12-20 12:50:26 -0600

    create range-knob module

---
+++ src/components/range/range-knob.ts
@@ -0,0 +1,99 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { clamp, isNumber, isPresent, isString, isTrueProperty } from '../../util/util';
+import { Range } from './range';
+
+/**
+ * @private
+ */
+@Component({
+  selector: '.range-knob-handle',
+  template:
+    '<div class="range-pin" *ngIf="range.pin">{{_val}}</div>' +
+    '<div class="range-knob"></div>',
+  host: {
+    '[class.range-knob-pressed]': 'pressed',
+    '[class.range-knob-min]': '_val===range.min',
+    '[class.range-knob-max]': '_val===range.max',
+    '[style.left]': '_x',
+    '[attr.aria-valuenow]': '_val',
+    '[attr.aria-valuemin]': 'range.min',
+    '[attr.aria-valuemax]': 'range.max',
+    'role': 'slider',
+    'tabindex': '0'
+  }
+})
+export class RangeKnob implements OnInit {
+  _ratio: number;
+  _val: number;
+  _x: string;
+  _upper: boolean = false;
+  pressed: boolean;
+
+  @Input()
+  get upper(): boolean {
+    return this._upper;
+  }
+  set upper(val: boolean) {
+    this._upper = isTrueProperty(val);
+  }
+
+  constructor(public range: Range) { }
+
+  get ratio(): number {
+    return this._ratio;
+  }
+  set ratio(ratio: number) {
+    this._ratio = clamp(0, ratio, 1);
+    this._val = this.range.ratioToValue(this._ratio);
+
+    if (this.range.snaps) {
+      this._ratio = this.range.valueToRatio(this._val);
+    }
+  }
+
+  get value(): number {
+    return this._val;
+  }
+  set value(val: number) {
+    if (isString(val)) {
+      val = Math.round(val);
+    }
+    if (isNumber(val) && !isNaN(val)) {
+      this._ratio = this.range.valueToRatio(val);
+      this._val = this.range.ratioToValue(this._ratio);
+    }
+  }
+
+  position() {
+    this._x = `${this._ratio * 100}%`;
+  }
+
+  ngOnInit() {
+    if (isPresent(this.range.value)) {
+      // we already have a value
+      if (this.range.dualKnobs) {
+        // we have a value and there are two knobs
+        if (this._upper) {
+          // this is the upper knob
+          this.value = this.range.value.upper;
+
+        } else {
+          // this is the lower knob
+          this.value = this.range.value.lower;
+        }
+
+      } else {
+        // we have a value and there is only one knob
+        this.value = this.range.value;
+      }
+
+    } else {
+      // we do not have a value so set defaults
+      this.ratio = ((this.range.dualKnobs && this.upper) ? 1 : 0);
+    }
+
+    this.position();
+  }
+
+}
--- src/components/range/range.ts
+++ src/components/range/range.ts
@@ -1,7 +1,7 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, forwardRef, Input, Inject, OnDestroy, OnInit, Optional, Output, QueryList, Renderer, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, forwardRef, Input, OnDestroy, Optional, Output, QueryList, Renderer, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-import { clamp, isNumber, isPresent, isString, isTrueProperty } from '../../util/util';
+import { clamp, isPresent, isTrueProperty } from '../../util/util';
 import { Config } from '../../config/config';
 import { DomController } from '../../platform/dom-controller';
 import { Form } from '../../util/form';
@@ -12,6 +12,7 @@ import { Platform } from '../../platform/platform';
 import { PointerCoordinates, pointerCoord } from '../../util/dom';
 import { TimeoutDebouncer } from '../../util/debouncer';
 import { UIEventManager } from '../../gestures/ui-event-manager';
+import { RangeKnob } from './range-knob';
 
 
 export const RANGE_VALUE_ACCESSOR: any = {
@@ -20,100 +21,7 @@ export const RANGE_VALUE_ACCESSOR: any = {
   multi: true
 };
 
-/**
- * @private
- */
-@Component({
-  selector: '.range-knob-handle',
-  template:
-    '<div class="range-pin" *ngIf="range.pin">{{_val}}</div>' +
-    '<div class="range-knob"></div>',
-  host: {
-    '[class.range-knob-pressed]': 'pressed',
-    '[class.range-knob-min]': '_val===range.min',
-    '[class.range-knob-max]': '_val===range.max',
-    '[style.left]': '_x',
-    '[attr.aria-valuenow]': '_val',
-    '[attr.aria-valuemin]': 'range.min',
-    '[attr.aria-valuemax]': 'range.max',
-    'role': 'slider',
-    'tabindex': '0'
-  }
-})
-export class RangeKnob implements OnInit {
-  _ratio: number;
-  _val: number;
-  _x: string;
-  _upper: boolean = false;
-  pressed: boolean;
 
-  @Input()
-  get upper(): boolean {
-    return this._upper;
-  }
-  set upper(val: boolean) {
-    this._upper = isTrueProperty(val);
-  }
-
-  constructor( @Inject(forwardRef(() => Range)) public range: Range) { }
-
-  get ratio(): number {
-    return this._ratio;
-  }
-  set ratio(ratio: number) {
-    this._ratio = clamp(0, ratio, 1);
-    this._val = this.range.ratioToValue(this._ratio);
-
-    if (this.range.snaps) {
-      this._ratio = this.range.valueToRatio(this._val);
-    }
-  }
-
-  get value(): number {
-    return this._val;
-  }
-  set value(val: number) {
-    if (isString(val)) {
-      val = Math.round(val);
-    }
-    if (isNumber(val) && !isNaN(val)) {
-      this._ratio = this.range.valueToRatio(val);
-      this._val = this.range.ratioToValue(this._ratio);
-    }
-  }
-
-  position() {
-    this._x = `${this._ratio * 100}%`;
-  }
-
-  ngOnInit() {
-    if (isPresent(this.range.value)) {
-      // we already have a value
-      if (this.range.dualKnobs) {
-        // we have a value and there are two knobs
-        if (this._upper) {
-          // this is the upper knob
-          this.value = this.range.value.upper;
-
-        } else {
-          // this is the lower knob
-          this.value = this.range.value.lower;
-        }
-
-      } else {
-        // we have a value and there is only one knob
-        this.value = this.range.value;
-      }
-
-    } else {
-      // we do not have a value so set defaults
-      this.ratio = ((this.range.dualKnobs && this.upper) ? 1 : 0);
-    }
-
-    this.position();
-  }
-
-}
 
 
 /**
