1c992177328338ca39e61c6dc2b2e3cb8fe47925
Brandy Carney <brandyscarney@gmail.com>
2016-12-29 15:50:48 -0500

    chore(demos): add watch task to the new prod task

--- scripts/gulp/tasks/demos.scripts.ts
+++ scripts/gulp/tasks/demos.scripts.ts
@@ -1,14 +1,15 @@
 import { spawnSync } from 'child_process';
-import { readdirSync, readFileSync, stat, statSync } from 'fs';
+import { accessSync, F_OK, readdirSync, readFileSync, stat, statSync } from 'fs';
 import { dirname, join } from 'path';
 
-import { dest, src, task } from 'gulp';
+import { dest, src, start, task } from 'gulp';
 import * as gulpif from 'gulp-if';
+import * as watch from 'gulp-watch';
 import * as runSequence from 'run-sequence';
 import { obj } from 'through2';
 import * as VinylFile from 'vinyl';
 
-import { DEMOS_SRC_ROOT, DIST_DEMOS_ROOT, DEMOS_NAME, ES5, ES_2015, SCRIPTS_ROOT } from '../constants';
+import { DEMOS_SRC_ROOT, DIST_DEMOS_ROOT, DIST_NAME, DEMOS_NAME, ES5, ES_2015, LOCAL_SERVER_PORT, SCRIPTS_ROOT } from '../constants';
 import { createTempTsConfig, getFolderInfo } from '../util';
 
 task('demos.prod', demosBuild);
@@ -138,4 +139,79 @@ function getFolders(dir) {
     .filter(function(file) {
       return statSync(join(dir, file)).isDirectory();
     });
+}
+
+task('demos.watchProd', (done: Function) => {
+  const folderInfo = getFolderInfo();
+  let demoTestPath = DEMOS_SRC_ROOT;
+
+  if (folderInfo.componentName && folderInfo.componentTest) {
+    demoTestPath = join(DEMOS_SRC_ROOT, folderInfo.componentName, 'app.module.ts');
+  }
+
+  try {
+    accessSync(demoTestPath, F_OK);
+  } catch (e) {
+    done(new Error(`Could not find demos test: ${demoTestPath}`));
+    return;
+  }
+
+  if (demosComponentsExists(folderInfo)) {
+    // already generated the demos directory
+    demosWatch(folderInfo.componentName, folderInfo.componentTest);
+
+  } else {
+    // generate the demos directory
+    console.log('Generate demo builds first...');
+    demosBuild(() => {
+      demosWatch(folderInfo.componentName, folderInfo.componentTest);
+    });
+  }
+});
+
+function demosWatch(componentName: string, componentTest: string) {
+  // If any tests change within components then run demos.resources.
+  watch([
+    'demos/src/**/*'
+  ],
+    function (file) {
+      console.log('start demos.resources - ' + JSON.stringify(file.history, null, 2));
+      start('demos.copyAndCompile');
+    });
+
+  // If any src files change except for tests then transpile only the source ionic files
+  watch([
+    'src/**/*.ts',
+    '!src/components/*/test/**/*',
+    '!src/util/test/*'
+  ],
+    function (file) {
+      console.log('start demos.ngcSource - ' + JSON.stringify(file.history, null, 2));
+      start('demos.copyAndCompile');
+    });
+
+  // If any scss files change then recompile all sass
+  watch(['src/**/*.scss'], (file) => {
+    console.log('start sass - ' + JSON.stringify(file.history, null, 2));
+    start('demos.sass');
+  });
+
+  console.log(`http://localhost:${LOCAL_SERVER_PORT}/${DIST_NAME}/${DEMOS_NAME}/${componentName}`);
+
+  start('demos.serve');
+}
+
+function demosComponentsExists(folderInfo: any): boolean {
+  let componentPath = DIST_DEMOS_ROOT;
+
+  if (folderInfo.componentName && folderInfo.componentTest) {
+    componentPath += `/${folderInfo.componentName}/build`;
+  }
+
+  try {
+    accessSync(componentPath, F_OK);
+  } catch (e) {
+    return false;
+  }
+  return true;
 }
