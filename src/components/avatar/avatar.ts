dbfc79b63a2bafec32aa6931cc257e12de4ff3db
Felix Gellner <flink@flinkblog.de>
2017-01-11 22:22:35 +0100

    docs(avatar): fix typo (#9965)
    place to placed

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -5,7 +5,7 @@ import { Directive } from '@angular/core';
   * @module ionic
   * @description
   * An Avatar is a component that creates a circular image for an item.
-  * Avatar's can be place on the left or right side of an item with the `item-left` or `item-right` directive.
+  * Avatar's can be placed on the left or right side of an item with the `item-left` or `item-right` directive.
   * @see {@link /docs/v2/components/#avatar-list Avatar Component Docs}
  */
 @Directive({
