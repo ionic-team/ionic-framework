68f3c6c0c841e61dab7daf7cc86ebdaf05276ca4
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-03-02 14:55:10 -0600

    refactor(nav): restructure nav component to separate modules, support async component loading
    restructure nav component to separate modules, support async component loading

--- src/components/nav/nav-pop.ts
+++ src/components/nav/nav-pop.ts
@@ -1,8 +1,6 @@
-import { AfterContentInit, Directive, HostListener, Optional } from '@angular/core';
+import { Directive, HostListener, Optional } from '@angular/core';
 
-import { DeepLinker } from '../../navigation/deep-linker';
 import { NavController } from '../../navigation/nav-controller';
-import { ViewController } from '../../navigation/view-controller';
 
 
 /**
@@ -50,41 +48,6 @@ export class NavPop {
     }
 
     return true;
-  }
-
-}
-
-
-/**
- * @private
- */
-@Directive({
-  selector: 'a[navPop]',
-  host: {
-    '[attr.href]': '_href'
-  }
-})
-export class NavPopAnchor implements AfterContentInit {
-
-  _href: string;
-
-  constructor(
-    @Optional() public host: NavPop,
-    public linker: DeepLinker,
-    @Optional() public viewCtrl: ViewController) {}
-
-  updateHref() {
-    if (this.host && this.viewCtrl) {
-      const previousView = this.host._nav.getPrevious(this.viewCtrl);
-      this._href = (previousView && this.linker.createUrl(this.host._nav, this.viewCtrl.component, this.viewCtrl.data)) || '#';
-
-    } else {
-      this._href = '#';
-    }
-  }
-
-  ngAfterContentInit() {
-    this.updateHref();
   }
 
 }
