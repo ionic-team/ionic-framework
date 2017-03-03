656ea666329f48d106ab2f1050f21102f3ba2aca
Daniel Imhoff <dwieeb@gmail.com>
2017-03-03 17:07:19 -0600

    test(e2e): radio/basic

---
+++ src/components/radio/test/basic/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule } from '../../../../..';
+
+import { E2EApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    E2EApp,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(E2EApp, {}, {
+      links: [
+        { loadChildren: '../pages/main/main.module#E2EPageModule', name: 'E2EPage' },
+      ]
+    })
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    E2EApp,
+  ]
+})
+export class AppModule {}
