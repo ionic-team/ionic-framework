19ec0caebb5893a8bed00a0bfa5e5fbec4457517
Brandy Carney <brandyscarney@gmail.com>
2017-03-03 11:25:30 -0500

    style(lint): fix linter errors

--- src/components/list/test/header-scenarios/app/app.component.ts
+++ src/components/list/test/header-scenarios/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { E2EPage } from '../pages/main/main'
+import { E2EPage } from '../pages/main/main';
 
 @Component({
   template: '<ion-nav [root]="root"></ion-nav>'
--- src/components/list/test/header-scenarios/app/app.module.ts
+++ src/components/list/test/header-scenarios/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule } from '../../../../..';
 
