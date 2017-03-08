7dfe061a7c0394e53777809b671b2c03fbef6714
Brandy Carney <brandyscarney@gmail.com>
2017-03-08 10:41:05 -0500

    test(split-pane): fix ionic import path and structure

--- src/components/split-pane/test/nested/app.module.ts
+++ src/components/split-pane/test/nested/app.module.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule } from '@angular/core';
-import { IonicApp, IonicModule, NavController } from '../../../../../ionic-angular';
+import { IonicApp, IonicModule, NavController } from '../../../..';
 
 
 @Component({
--- src/components/split-pane/test/tabs/app.module.ts
+++ src/components/split-pane/test/tabs/app.module.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule } from '@angular/core';
-import { IonicApp, IonicModule, NavController, MenuController } from '../../../../../ionic-angular';
+import { IonicApp, IonicModule, NavController, MenuController } from '../../../..';
 
 
 @Component({
