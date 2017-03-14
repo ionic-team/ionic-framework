e5f781279890fa49c4270e5e1a9513249db7b9ea
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-03-14 16:06:51 -0500

    test(modal): fix broken unit tests

--- src/components/modal/test/modal.spec.ts
+++ src/components/modal/test/modal.spec.ts
@@ -1,7 +1,6 @@
 import { mockApp, mockConfig, mockDeepLinker } from '../../../util/mock-providers';
 import { Component } from '@angular/core';
 import { ModalController } from '../modal-controller';
-import { ModalCmp } from '../modal-component';
 
 describe('Modal', () => {
 
@@ -10,7 +9,7 @@ describe('Modal', () => {
     it('should have the correct properties on modal view controller proxy instance', () => {
       let modalCtrl = new ModalController(mockApp(), mockConfig(), mockDeepLinker());
       let modalViewControllerProxy = modalCtrl.create(ComponentToPresent);
-      expect(modalViewControllerProxy._component).toEqual(ModalCmp);
+      expect(modalViewControllerProxy._component).toEqual(ComponentToPresent);
     });
   });
 
