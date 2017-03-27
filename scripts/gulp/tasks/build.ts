54acc74fdbe27a4ea99eb776c3abcfef7dcb6769
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-03-27 16:42:01 -0500

    refactor(packaging):  points to es5 code with es2015 import/header,  points to umd cod
     points to es5 code with es2015 import/header,  points to umd code,  points to
    pure es2015 code

--- scripts/gulp/tasks/build.ts
+++ scripts/gulp/tasks/build.ts
@@ -1,5 +1,5 @@
 import { task } from 'gulp';
-import { DIST_BUILD_ROOT, DIST_BUILD_ESM_ROOT, DIST_BUILD_UMD_ROOT, ES5, ES_2015, PROJECT_ROOT, UMD_MODULE } from '../constants';
+import { DIST_BUILD_ROOT, DIST_BUILD_ES2015_ROOT, DIST_BUILD_UMD_ROOT, ES5, ES_2015, PROJECT_ROOT, UMD_MODULE } from '../constants';
 import { copySourceToDest, createTempTsConfig, deleteFiles, runNgc, runTsc } from '../util';
 
 
@@ -45,29 +45,10 @@ export function buildIonicAngularUmdTsc(excludeSpec: boolean, stripDebug: boolea
 
 
 export function buildIonicAngularEsm(stripDebug: boolean, done: Function) {
-  const stream = copySourceToDest(DIST_BUILD_ESM_ROOT, true, true, stripDebug);
-  stream.on('end', () => {
-    // the source files are copied, copy over a tsconfig from
-    createTempTsConfig(['./**/*.ts'], ES_2015, ES_2015, `${PROJECT_ROOT}/tsconfig.json`, `${DIST_BUILD_ESM_ROOT}/tsconfig.json`);
-    runNgc(`${DIST_BUILD_ESM_ROOT}/tsconfig.json`, (err) => {
-      if (err) {
-        done(err);
-        return;
-      }
-      // clean up any .ts files that remain as well as ngc metadata
-      deleteFiles([`${DIST_BUILD_ESM_ROOT}/**/*.ts`,
-                  `${DIST_BUILD_ESM_ROOT}/node_modules`,
-                  `${DIST_BUILD_ESM_ROOT}/tsconfig.json`,
-                  `!${DIST_BUILD_ESM_ROOT}/**/*.d.ts`], done);
-    });
-  });
-}
-
-export function buildIonicPureEs6(stripDebug: boolean, done: Function) {
   const stream = copySourceToDest(DIST_BUILD_ROOT, true, true, stripDebug);
   stream.on('end', () => {
     // the source files are copied, copy over a tsconfig from
-    createTempTsConfig(['./**/*.ts'], ES_2015, ES_2015, `${PROJECT_ROOT}/tsconfig.json`, `${DIST_BUILD_ROOT}/tsconfig.json`);
+    createTempTsConfig(['./**/*.ts'], ES5, ES_2015, `${PROJECT_ROOT}/tsconfig.json`, `${DIST_BUILD_ROOT}/tsconfig.json`);
     runNgc(`${DIST_BUILD_ROOT}/tsconfig.json`, (err) => {
       if (err) {
         done(err);
@@ -78,6 +59,25 @@ export function buildIonicPureEs6(stripDebug: boolean, done: Function) {
                   `${DIST_BUILD_ROOT}/node_modules`,
                   `${DIST_BUILD_ROOT}/tsconfig.json`,
                   `!${DIST_BUILD_ROOT}/**/*.d.ts`], done);
+    });
+  });
+}
+
+export function buildIonicPureEs6(stripDebug: boolean, done: Function) {
+  const stream = copySourceToDest(DIST_BUILD_ES2015_ROOT, true, true, stripDebug);
+  stream.on('end', () => {
+    // the source files are copied, copy over a tsconfig from
+    createTempTsConfig(['./**/*.ts'], ES_2015, ES_2015, `${PROJECT_ROOT}/tsconfig.json`, `${DIST_BUILD_ES2015_ROOT}/tsconfig.json`);
+    runNgc(`${DIST_BUILD_ES2015_ROOT}/tsconfig.json`, (err) => {
+      if (err) {
+        done(err);
+        return;
+      }
+      // clean up any .ts files that remain as well as ngc metadata
+      deleteFiles([`${DIST_BUILD_ES2015_ROOT}/**/*.ts`,
+                  `${DIST_BUILD_ES2015_ROOT}/node_modules`,
+                  `${DIST_BUILD_ES2015_ROOT}/tsconfig.json`,
+                  `!${DIST_BUILD_ES2015_ROOT}/**/*.d.ts`], done);
     });
   });
 }
