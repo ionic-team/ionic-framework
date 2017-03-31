150f076022f18aebb10fa13eaaa1cf69e6507a3f
Lukas Jakob <lu.jakob@googlemail.com>
2017-03-31 15:09:14 +0200

    Make position top relative to scrollable content instead of viewport.

--- src/components/img/img.ts
+++ src/components/img/img.ts
@@ -111,6 +111,10 @@ export class Img implements OnDestroy {
   /** @internal */
   _rect: any;
   /** @internal */
+  _top: number;
+  /** @internal */
+  _bottom: number;
+  /** @internal */
   _w: string = '';
   /** @internal */
   _h: string = '';
@@ -258,16 +262,14 @@ export class Img implements OnDestroy {
    * @hidden
    */
   get top(): number {
-    const bounds = this._getBounds();
-    return bounds && bounds.top || 0;
+    return this._top || 0;
   }
 
   /**
    * @hidden
    */
   get bottom(): number {
-    const bounds = this._getBounds();
-    return bounds && bounds.bottom || 0;
+    return this._bottom || 0;
   }
 
   private _getBounds() {
@@ -280,6 +282,9 @@ export class Img implements OnDestroy {
       // we don't have bounds from virtual scroll
       // so let's do the raw DOM lookup w/ getBoundingClientRect
       this._rect = (<HTMLElement>this._elementRef.nativeElement).getBoundingClientRect();
+
+      this._top = this._rect.top + this._content._scrollContent.nativeElement.scrollTop;
+      this._bottom = this._rect.bottom + this._content._scrollContent.nativeElement.scrollTop;
       console.debug(`img, ${this._src}, read, ${this._rect.top} - ${this._rect.bottom}`);
     }
     return this._rect;
