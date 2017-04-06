df269bf7fba79ed75f564775814dff48c68c8f69
Musa Haidari <musa.haidari@gmail.com>
2017-04-07 03:14:01 +0430

    docs(platform): add missing parameter updateDocument in the doc (#11075)

--- src/platform/platform.ts
+++ src/platform/platform.ts
@@ -351,6 +351,7 @@ export class Platform {
    * the language needs to be dynamically changed per user/session.
    * [W3C: Declaring language in HTML](http://www.w3.org/International/questions/qa-html-language-declarations)
    * @param {string} language  Examples: `en-US`, `en-GB`, `ar`, `de`, `zh`, `es-MX`
+   * @param {boolean} updateDocument  Specifies whether the `lang` attribute of `<html>` should be updated
    */
   setLang(language: string, updateDocument: boolean) {
     this._lang = language;
