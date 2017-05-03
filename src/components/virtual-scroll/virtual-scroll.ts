5595b8c084cfe2a7b37c52d6cf01d303789890da
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2017-05-03 16:03:46 +0300

    fix(VirtualScroll): initialise differ with trackByFn

--- src/components/virtual-scroll/virtual-scroll.ts
+++ src/components/virtual-scroll/virtual-scroll.ts
@@ -234,6 +234,7 @@ export class VirtualScroll implements DoCheck, AfterContentInit, OnDestroy {
   };
   _queue: number = SCROLL_QUEUE_NO_CHANGES;
   _recordSize: number = 0;
+  _virtualTrackBy: TrackByFn;
 
   @ContentChild(VirtualItem) _itmTmp: VirtualItem;
   @ContentChild(VirtualHeader) _hdrTmp: VirtualHeader;
@@ -368,7 +369,13 @@ export class VirtualScroll implements DoCheck, AfterContentInit, OnDestroy {
   /**
    * @input {function} Same as `ngForTrackBy` which can be used on `ngFor`.
    */
-  @Input() virtualTrackBy: TrackByFn;
+  @Input() set virtualTrackBy(val: TrackByFn) {
+    if (!isPresent(val)) return;
+    this._virtualTrackBy = val;
+    if (isPresent(this._records)) {
+      this._differ = this._iterableDiffers.find(this._records).create(this.virtualTrackBy);
+    }
+  };
 
 
   constructor(
