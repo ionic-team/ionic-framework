3139c97571e87e1482c425cdbb9f33e5ee52ece8
Brandy Carney <brandyscarney@gmail.com>
2017-05-10 13:35:21 -0400

    test(content): update content no-bounce e2e test

---
+++ src/components/content/test/no-bounce/pages/root-page/root-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { IonicPage } from '../../../../../../';
+
+@IonicPage()
+@Component({
+  templateUrl: 'root-page.html'
+})
+export class RootPage {}
