86deb1d5cccc345ba5fb5033d2897cd936e1b9e1
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-06-08 20:02:46 -0400

    fix(nav): add boolean to promise resolution on guard hooks style(nav): fix lints on lifecycle hooks

--- src/navigation/lifecycle-hooks/view-can-leave.ts
+++ src/navigation/lifecycle-hooks/view-can-leave.ts
@@ -8,5 +8,5 @@ export interface ViewCanLeave {
      * @description
      * Runs before the view can leave. This can be used as a sort of "guard" in authenticated views where you need to check permissions before the view can leave
      */
-    ionViewCanLeave(): boolean | Promise <void> ;
+    ionViewCanLeave(): boolean | Promise <boolean|void> ;
 }
