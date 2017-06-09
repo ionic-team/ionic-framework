63b0f0aaf637899789e75107e26743cbc148c9bf
Brandy Carney <brandyscarney@gmail.com>
2017-06-09 18:11:40 -0400

    style(lint): fix lint errors and add !default flags

--- .scss-linters/default_rule.rb
+++ .scss-linters/default_rule.rb
@@ -1,7 +1,19 @@
 module SCSSLint
-  # Reports the use of !important in properties.
+  # Reports the use of !default at the end of variable declarations.
   class Linter::DefaultRule < Linter
     include LinterRegistry
+
+    def visit_each(node)
+      return true
+    end
+
+    def visit_function(node)
+      return true
+    end
+
+    def visit_mixin(node)
+      return true
+    end
 
     def visit_variable(node)
       return if source_from_range(node.source_range).include?('!default')
