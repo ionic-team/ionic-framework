a99610c776a5dc405b9f4684b6d64c3e48136d4c
Brandy Carney <brandyscarney@gmail.com>
2017-06-12 13:43:35 -0400

    chore(e2e): update e2e task

--- scripts/gulp/tasks/e2e.dev.ts
+++ scripts/gulp/tasks/e2e.dev.ts
@@ -21,7 +21,7 @@ task('e2e.watch', ['e2e.prepare', 'core.watch'], (done: Function) => {
 });
 
 function serveTest(folderInfo: any) {
-  const testOrDemoName = folderInfo.componentName + '/' + folderInfo.componentTest;
+  const testOrDemoName = join(folderInfo.componentName, folderInfo.componentTest);
   const ionicAngularDir = join(PROJECT_ROOT, 'src');
   const coreCompilerFilePath = join(PROJECT_ROOT, '..', 'ionic-core', 'dist', 'compiler');
   const coreDir = join(PROJECT_ROOT, '..', 'ionic-core', 'dist', 'compiled-ionic-angular');
@@ -49,5 +49,5 @@ function serveTest(folderInfo: any) {
   const appNgModulePath = join(dirname(appEntryPoint), 'app.module.ts');
   const distDir = join(distTestRoot, 'www');
 
-  return runAppScriptsServe(join(folderInfo.componentName, folderInfo.componentTest), appEntryPoint, appNgModulePath, ionicAngularDir, distDir, pathToWriteFile, ionicAngularDir, sassConfigPath, copyConfigPath, null);
+  return runAppScriptsServe(testOrDemoName, appEntryPoint, appNgModulePath, ionicAngularDir, distDir, pathToWriteFile, ionicAngularDir, coreCompilerFilePath, coreDir, sassConfigPath, copyConfigPath, null);
 }
