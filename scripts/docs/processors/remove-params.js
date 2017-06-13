2dbde00925aa5fc18b566ecdeb5049f8804e470d
mhartington <mikehartington@gmail.com>
2017-06-13 15:04:03 -0400

    docs(): remove constructor params from docs

---
+++ scripts/docs/processors/remove-params.js
@@ -0,0 +1,28 @@
+var console = require('better-console');
+module.exports = function removeParams() {
+  return {
+    name: 'remove-params',
+    description: 'Prevent class params from being rendered',
+    $runAfter: ['paths-computed'],
+    $runBefore: ['rendering-docs'],
+    $process: function(docs) {
+      docs.forEach(function(doc) {
+        if (doc.constructorDoc) {
+          const constructorDoc = doc.constructorDoc;
+          if (doc.members) {
+            doc.members = doc.members.filter(function(method) {
+              constructorDoc.parameters.forEach(function(param) {
+                if (param.indexOf(method.name) > -1) {
+                  return true;
+                } else {
+                  return false;
+                }
+              });
+            });
+          }
+        }
+      });
+      return docs;
+    }
+  };
+};
