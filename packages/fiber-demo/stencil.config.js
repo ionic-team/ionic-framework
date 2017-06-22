984bc7025faa780b782a92ddbf9578dbf8aeccb6
Josh Thomas <jthoms1@gmail.com>
2017-06-22 12:03:59 -0500

    chore(): updates to allow fiber demo to work.

--- packages/fiber-demo/stencil.config.js
+++ packages/fiber-demo/stencil.config.js
@@ -2,20 +2,6 @@ exports.config = {
   src: 'src',
   dest: 'dist',
   bundles: [
-    { components: ['ion-app', 'ion-content', 'ion-footer', 'ion-header', 'ion-navbar', 'ion-page', 'ion-title', 'ion-toolbar'] },
-    { components: ['ion-avatar', 'ion-thumbnail'] },
-    { components: ['ion-badge'] },
-    { components: ['ion-button', 'ion-buttons', 'ion-icon'] },
-    { components: ['ion-card', 'ion-card-content', 'ion-card-header', 'ion-card-title'] },
-    { components: ['ion-gesture', 'ion-scroll'], priority: 'low' },
-    { components: ['ion-item', 'ion-item-divider', 'ion-label', 'ion-list', 'ion-list-header', 'ion-skeleton-text'] },
-    { components: ['ion-loading', 'ion-loading-controller'] },
-    { components: ['ion-menu'], priority: 'low' },
-    { components: ['ion-modal', 'ion-modal-controller'] },
-    { components: ['ion-slides', 'ion-slide'] },
-    { components: ['ion-spinner'] },
-    { components: ['ion-toggle'] },
-    { components: ['fiber-demo', 'fiber-triangle', 'fiber-dot'] },
-    { components: ['news-list', 'news-container', 'comments-page', 'comments-list'] }
+    { components: ['fiber-demo', 'fiber-triangle', 'fiber-dot'] }
   ]
 };
