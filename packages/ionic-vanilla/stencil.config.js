77665fca7f71904bb1e129819a14ce3a409331d9
Adam Bradley <adambradley25@gmail.com>
2017-06-30 14:13:19 -0500

    chore(): update vanilla config

--- packages/ionic-vanilla/stencil.config.js
+++ packages/ionic-vanilla/stencil.config.js
@@ -1,6 +1,8 @@
 exports.config = {
+  namespace: 'IonicVanilla',
   dest: 'dist',
   collections: [
     '@ionic/core'
-  ]
+  ],
+  preamble: '(C) Ionic Vanilla https://ionicframework.com - MIT License'
 };
