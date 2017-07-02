024ee2e4fee7ee3d1bd5b3f7816ff0f4c0506092
Abdel El-Medny <abdel@springtree.nl>
2017-07-02 22:44:03 +0200

    feat(progress): inlines template

--- src/components/progress/progress.ts
+++ src/components/progress/progress.ts
@@ -1,20 +1,32 @@
-import { Component, Input, ViewEncapsulation, HostBinding } from '@angular/core';
+import { Component, Input, ViewEncapsulation, ElementRef, Renderer } from '@angular/core';
+import { Ion } from '../ion';
+import { Config } from '../../config/config';
+export type progressIndicator = 'determinate' | 'indeterminate' | 'buffer' | 'query';
 
 @Component({
   selector: 'ion-progress',
-  templateUrl : './progress.html',
-  encapsulation: ViewEncapsulation.None
+  template : `
+    <div class="progress" [style.width.%]="value"></div>
+    <div class="animatable"></div>
+    <div class="buffer-circles"></div>
+    <div class="buffer" [style.width.%]="buffer"></div>
+  `,
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class]': 'indicator'
+  }
 })
-export class Progress {
+export class Progress extends Ion {
 
-  constructor( ) { }
+  constructor(config: Config, elementRef: ElementRef, renderer: Renderer) {
+    super(config, elementRef, renderer, 'progress');
+   }
 
   /**
    * @input {'determinate' | 'indeterminate' | 'buffer' | 'query'} The current mode of the progress indicator
    */
   @Input()
-  @HostBinding('class')
-  indicator: 'determinate' | 'indeterminate' | 'buffer' | 'query' = 'determinate';
+  indicator: progressIndicator =  'determinate';
   /**
    * @input {number} The value of the progress indicator, as a percentage (out of 100)
    */
