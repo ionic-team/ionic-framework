db9e9d9159f3faad417e48d1c1d66eea3626fb9c
Brandy Carney <brandy@ionic.io>
2017-07-12 16:04:28 -0400

    feat(fab): add fab container and buttons

---
+++ packages/core/src/components/fab/fab-list.tsx
@@ -0,0 +1,63 @@
+import { Component, h, PropDidChange, State, VNodeData } from '@stencil/core';
+
+import { HostElement } from '../../utils/interfaces';
+
+
+/**
+  * @name FabList
+  * @description
+  * `ion-fab-list` is a container for multiple FAB buttons. They are components of `ion-fab` and allow you to specificy the buttons position, left, right, top, bottom.
+  * @usage
+  *
+  * ```html
+  *  <ion-fab bottom right>
+  *    <button ion-fab>Share</button>
+  *    <ion-fab-list side="top">
+  *      <button ion-fab>Facebook</button>
+  *      <button ion-fab>Twitter</button>
+  *      <button ion-fab>Youtube</button>
+  *    </ion-fab-list>
+  *    <ion-fab-list side="left">
+  *      <button ion-fab>Vimeo</button>
+  *    </ion-fab-list>
+  *  </ion-fab>
+  * ```
+  * @module ionic
+  *
+  * @demo /docs/demos/src/fab/
+  * @see {@link /docs/components#fab Fab Component Docs}
+ */
+@Component({
+  tag: 'ion-fab-list',
+})
+export class FabList {
+  $el: HTMLElement;
+
+  @State() activated: boolean = false;
+
+  @PropDidChange('activated')
+  activatedChange(activated: boolean) {
+    const fabs = this.$el.querySelectorAll('ion-fab-button') as NodeListOf<HostElement>;
+
+    // if showing the fabs add a timeout, else show immediately
+    var timeout = activated ? 30 : 0;
+    for (var i = 0; i < fabs.length; i++) {
+      const fab = fabs[i].$instance;
+      setTimeout(() => fab.show = activated, i * timeout);
+    }
+  }
+
+  hostData(): VNodeData {
+    return {
+      class: {
+        'fab-list-active': this.activated
+      }
+    };
+  }
+
+  render() {
+    return (
+      <slot></slot>
+    );
+  }
+}
---
+++ packages/core/src/components/fab/fab.tsx
@@ -0,0 +1,201 @@
+import { Component, CssClassObject, h, Method, Prop, State } from '@stencil/core';
+
+import { createThemedClasses } from '../../utils/theme';
+import { HostElement } from '../../utils/interfaces';
+
+
+/**
+  * @name FabButton
+  * @module ionic
+  *
+  * @description
+  * FABs (Floating Action Buttons) are standard material design components. They are shaped as a circle that represents a promoted action. When pressed, it may contain more related actions.
+  * FABs as its name suggests are floating over the content in a fixed position. This is not achieved exclusively with `<button ion-fab>Button</button>` but it has to wrapped with the `<ion-fab>` component, like this:
+  *
+  * ```html
+  * <ion-content>
+  *  <!-- Real floating action button, fixed. It will not scroll with the content -->
+  *  <ion-fab>
+  *    <button ion-fab>Button</button>
+  *  </ion-fab>
+  *
+  *  <!-- Button shaped as a circle that just like a normal button scrolls with the content -->
+  *  <button ion-fab>Button</button>
+  * </ion-content>
+  *
+  * ```
+  *
+  * In case the button is not wrapped with `<ion-fab>`, the fab button will behave like a normal button, scrolling with the content.
+  *
+  * See [ion-fab] to learn more information about how to position the fab button.
+  *
+  * @property [mini] - Makes a fab button with a reduced size.
+  *
+  * @usage
+  *
+  * ```html
+  *
+  * <!-- Colors -->
+  * <ion-fab>
+  *   <button ion-fab color="primary">Button</button>
+  * </ion-fab>
+  *
+  * <!-- Mini -->
+  * <ion-fab>
+  *   <button ion-fab mini>Small</button>
+  * </ion-fab>
+  * ```
+  *
+  * @demo /docs/demos/src/fab/
+  * @see {@link /docs/components#fabs FAB Component Docs}
+ */
+@Component({
+  tag: 'ion-fab-button',
+  styleUrls: {
+    ios: 'fab.ios.scss',
+    md: 'fab.md.scss',
+    wp: 'fab.wp.scss'
+  }
+})
+export class FabButton {
+  $el: HTMLElement;
+  mode: string;
+  color: string;
+
+  @State() activated: boolean = false;
+
+  @State() closeActivated: boolean = false;
+
+  @State() show: boolean = false;
+
+  @State() inContainer: boolean = false;
+
+  @State() inList: boolean = false;
+
+  @Prop() href: string;
+
+  /**
+   * @Prop {boolean} If true, sets the button into a disabled state.
+   */
+  @Prop() disabled: boolean = false;
+
+  ionViewDidLoad() {
+    const parentNode = this.$el.parentNode.nodeName;
+
+    this.inList = (parentNode === 'ION-FAB-LIST');
+    this.inContainer = (parentNode === 'ION-FAB');
+  }
+
+  clickedFab() {
+    if (this.inContainer) {
+      const activated = !this.activated;
+      this.setActiveLists(activated);
+    }
+  }
+
+  /**
+   * @hidden
+   */
+  setActiveLists(activated: boolean) {
+    const lists = this.$el.parentElement.querySelectorAll('ion-fab-list') as NodeListOf<HostElement>;
+
+    if (lists.length > 0) {
+      this.activated = activated;
+    }
+
+    for (var i = 0; i < lists.length; i++) {
+      const list = lists[i].$instance;
+      list.activated = activated;
+    }
+  }
+
+  /**
+   * Close an active FAB list container
+   */
+  @Method()
+  close() {
+    this.setActiveLists(false);
+  }
+
+  /**
+   * @hidden
+   * Get the element classes to add to the child element
+   */
+  getElementClassList() {
+    let classList = [].concat(
+      this.$el.className.length ? this.$el.className.split(' ') : []
+    );
+
+    return classList;
+  }
+
+  /**
+   * @hidden
+   * Get the classes for fab buttons in lists
+   */
+  getFabListClassList() {
+    if (!this.inList) {
+      return [];
+    }
+    return [
+      `fab-in-list`,
+      `fab-${this.mode}-in-list`
+    ];
+  }
+
+  /**
+   * @hidden
+   * Get the close active class for fab buttons
+   */
+  getFabActiveClassList() {
+    if (!this.activated) {
+      return [];
+    }
+    return [
+      `fab-close-active`
+    ];
+  }
+
+  /**
+   * @hidden
+   * Get the show class for fab buttons
+   */
+  getFabShowClassList() {
+    if (!this.show) {
+      return [];
+    }
+    return [
+      `show`
+    ];
+  }
+
+  render() {
+    const themedClasses = createThemedClasses(this.mode, this.color, 'fab');
+
+    var fabClasses: CssClassObject = []
+      .concat(
+        this.getElementClassList(),
+        this.getFabListClassList(),
+        this.getFabActiveClassList(),
+        this.getFabShowClassList()
+      )
+      .reduce((prevValue, cssClass) => {
+        prevValue[cssClass] = true;
+        return prevValue;
+      }, {});
+
+    const TagType = this.href ? 'a' : 'button';
+
+    fabClasses = Object.assign(fabClasses, themedClasses);
+
+    return (
+      <TagType class={fabClasses} onclick={this.clickedFab.bind(this)} disabled={this.disabled}>
+        <ion-icon name="close" class="fab-close-icon"></ion-icon>
+        <span class='button-inner'>
+          <slot></slot>
+        </span>
+        <div class='button-effect'></div>
+      </TagType>
+    );
+  }
+}
