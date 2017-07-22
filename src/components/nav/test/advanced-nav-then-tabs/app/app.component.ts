e73b2317279c5281c590e18688762ae596ec777f
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-07-21 23:46:47 -0500

    update test

--- src/components/nav/test/advanced-nav-then-tabs/app/app.component.ts
+++ src/components/nav/test/advanced-nav-then-tabs/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   template: `
 <ion-split-pane>
-  <ion-nav [root]="rootOne"></ion-nav>
-  <ion-nav [root]="rootTwo" main #content></ion-nav>
+  <ion-nav [root]="rootOne" name="left"></ion-nav>
+  <ion-nav [root]="rootTwo" main #content name="right"></ion-nav>
 
 </ion-split-pane>
   `
