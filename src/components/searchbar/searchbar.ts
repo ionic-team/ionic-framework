96524a8c80884824fce13bd07445cca44fdd90b4
Jesús Botella <jesus.botella@gmail.com>
2017-08-04 12:31:20 +0200

    feat(searchbar): reflect disabled attr in input

--- src/components/searchbar/searchbar.ts
+++ src/components/searchbar/searchbar.ts
@@ -40,6 +40,7 @@ import { Platform } from '../../platform/platform';
         '[attr.autocomplete]="_autocomplete" ' +
         '[attr.autocorrect]="_autocorrect" ' +
         '[attr.spellcheck]="_spellcheck">' +
+        '[disabled]="_disabled" ' +
       '<button ion-button clear class="searchbar-clear-icon" [mode]="_mode" (click)="clearInput($event)" (mousedown)="clearInput($event)" type="button"></button>' +
     '</div>' +
     '<button ion-button #cancelButton mode="ios" [tabindex]="_isActive ? 1 : -1" clear (click)="cancelSearchbar($event)" (mousedown)="cancelSearchbar($event)" class="searchbar-ios-cancel" type="button">{{cancelButtonText}}</button>',
