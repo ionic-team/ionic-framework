60b4bec10015a1cc6d84597a07ce59aa4549e8c0
Brandy Carney <brandy@ionic.io>
2017-08-10 17:50:17 -0400

    fix(list-header): add proper styles for list header

--- packages/core/src/components/list-header/list-header.tsx
+++ packages/core/src/components/list-header/list-header.tsx
@@ -3,6 +3,11 @@ import { Component } from '@stencil/core';
 
 @Component({
   tag: 'ion-list-header',
+  styleUrls: {
+    ios: 'list-header.ios.scss',
+    md: 'list-header.md.scss',
+    wp: 'list-header.wp.scss'
+  },
   host: {
     theme: 'list-header'
   }
