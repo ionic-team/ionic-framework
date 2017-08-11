96b7c7b02602613175ce4f577eb2d822de325df8
Brian Soumakian <bsoumakian@gmail.com>
2017-08-10 23:39:06 -0700

    set default value for secondary ID

--- src/components/tabs/tabs.ts
+++ src/components/tabs/tabs.ts
@@ -643,6 +643,8 @@ export class Tabs extends Ion implements AfterViewInit, RootNode, ITabs, Navigat
    * @private
    */
   _getSelectedTabIndex(secondaryId: string, fallbackIndex: number = 0): number {
+    secondaryId = secondaryId || '';
+
     // we found a segment which probably represents which tab to select
     const indexMatch = secondaryId.match(/tab-(\d+)/);
     if (indexMatch) {
