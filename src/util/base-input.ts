fc62a569120a24a9763e8ac7979d277df4691867
Rodolfo Silva <contato@rodolfosilva.com>
2017-08-31 20:33:31 -0300

    Remove call of onTouched at onChange event

--- src/util/base-input.ts
+++ src/util/base-input.ts
@@ -254,7 +254,6 @@ export class BaseInput<T> extends Ion implements CommonInput<T> {
    */
   private onChange() {
     this._onChanged && this._onChanged(this._inputNgModelEvent());
-    this._onTouched && this._onTouched();
   }
 
   /**
