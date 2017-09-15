3b89fe85d466946264ab226fa4a53f601ed9ee46
iwantwin <developer@iwantwin.nl>
2017-09-15 16:44:04 +0200

    Register multiple callbacks to modals
    This changes allows for registering multiple listeners for onDidDismiss and onWillDismiss. It is backwards compatible, does not require any changes to registering. Although, there may be a change in some behaviour: if you register multiple callbacks for onDidDismiss or onWillDismiss, it would have been overwritten in the past (only last register will execute) and now it will execute all registered listeners.
    
    This is useful for situations where you would want to build a provider (for example 'modalmanager') that serves a modal to another provider, where both providers want to register to onDidDismiss or onWillDismiss.

--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -14,12 +14,42 @@ import { ModalImpl } from './modal-impl';
 export class Modal extends OverlayProxy {
 
   public isOverlay: boolean = true;
+  private _onDidDismissQueue: Array<(data: any, role: string) => void>;
+  private _onWillDismissQueue: Array<() => void>;
 
   constructor(app: App, component: any, private data: any, private opts: ModalOptions = {}, config: Config, deepLinker: DeepLinker) {
     super(app, component, config, deepLinker);
+
+    super.onDidDismiss((data: any, role: string) => {
+      for (let it = 0; it < this._onDidDismissQueue.length; it++) {
+        let didDismissCallback = this._onDidDismissQueue[it];
+        didDismissCallback(data, role);
+      }
+    });
+
+    super.onWillDismiss(() => {
+      for (let it = 0; it < this._onWillDismissQueue.length; it++) {
+        let willDismissCallback = this._onWillDismissQueue[it];
+        willDismissCallback();
+      }
+    });
   }
 
   getImplementation(): Overlay {
     return new ModalImpl(this._app, this._component, this.data, this.opts, this._config);
+  }
+
+  onDidDismiss (callback: (data: any, role: string) => void ) {
+    if (typeof(this._onDidDismissQueue) === 'undefined') {
+      this._onDidDismissQueue = [];
+    }
+    this._onDidDismissQueue.push(callback);
+  }
+
+  onWillDismiss (callback: () => void ) {
+    if (typeof(this._onWillDismissQueue) === 'undefined') {
+      this._onWillDismissQueue = [];
+    }
+    this._onWillDismissQueue.push(callback);
   }
 }
