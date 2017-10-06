9bfd286dfa3b7e4fb55b9b2b058784ec700dbf69
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-10-06 00:02:12 -0500

    chore(dependencies): update app-scripts version to run latest nightly with deepLinkDir fix

--- src/components/nav/test/simple-tabs/app/app.component.ts
+++ src/components/nav/test/simple-tabs/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   template: `
   <ion-tabs name="simple">
     <ion-tab tabIcon="heart" [root]="tab1" tabTitle="Taco Burrito Enchilada"></ion-tab>
-    <ion-tab tabIcon="star" [root]="tab2"></ion-tab>
+    <ion-tab tabIcon="star" [root]="tab2" tabTitle="Tab 2" tabUrlPath="ABC123"></ion-tab>
   </ion-tabs>
   `
 })
