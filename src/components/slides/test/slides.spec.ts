5b1fbffafbfc80b9955bcfe996fcce4e5bc54e72
Simon Schell <simon.schell@provinzial.com>
2017-10-26 15:24:37 +0200

    test(slides): pagination basic and nested slides

--- src/components/slides/test/slides.spec.ts
+++ src/components/slides/test/slides.spec.ts
@@ -1,7 +1,7 @@
 
 import {Slides} from '../slides';
 import {
-  MockElementRef, mockConfig, mockPlatform, mockRenderer, mockView, mockZone
+  MockElement, MockElementRef, mockConfig, mockPlatform, mockRenderer, mockView, mockZone
 } from '../../../util/mock-providers';
 import {CLS} from '../swiper/swiper-utils';
 
@@ -18,16 +18,39 @@ describe('Slides', () => {
     const slides = mockSlides(container);
     expect(slides.length()).toBe(2);
   });
+
+  describe('Pager', () => {
+    it('should include bullets equal to the number of slides', () => {
+      const container = createSlideContainer();
+      // As the grid for pagination uses clientWidth for calculation we need the container to be rendered
+      document.body.appendChild(container);
+      const slides = mockSlides(container);
+      expect(slides._paginationContainer.children.length).toBe(2);
+    });
+    it('should add the bullets to the correct pagination-container in case of nested slides', () => {
+      const container = createSlideContainer();
+      const nestedContainer = createSlideContainer();
+      container.children[0].children[0].appendChild(nestedContainer);
+      // As the grid for pagination uses clientWidth for calculation we need the container to be rendered
+      document.body.appendChild(container);
+      const slides = mockSlides(container);
+      expect(slides._paginationContainer.children.length).toBe(2);
+      expect(slides._paginationContainer).not.toBe(nestedContainer.children[1]);
+      expect(slides._paginationContainer).toBe(container.children[1]);
+    });
+  });
 });
 
 export function mockSlides(firstChild: HTMLElement): Slides {
   const platform = mockPlatform();
   const config = mockConfig(null, '/', platform);
   const zone = mockZone();
-  const elementRef = new MockElementRef(document.createElement('ion-slides'));
+  const mockSlides = new MockElement();
+  const elementRef = new MockElementRef(mockSlides);
   const renderer = mockRenderer();
   const view = mockView();
-  elementRef.nativeElement.appendChild(firstChild);
+  elementRef.nativeElement.children = [firstChild];
+  mockSlides.nodeName = 'ion-slides';
 
   const slides = new Slides(config, platform, zone, view, elementRef, renderer);
   view.readReady.emit();
@@ -40,13 +63,14 @@ export function createSlideContainer(): HTMLElement {
   const slide1 = document.createElement('ion-slide');
   const slide2 = document.createElement('ion-slide');
   const pagination = document.createElement('div');
+  container.className = 'swiper-container';
   pagination.className = 'swiper-pagination';
   wrapper.className = CLS.wrapper;
   slide1.className = CLS.slide;
   slide2.className = CLS.slide;
-  container.appendChild(wrapper);
   wrapper.appendChild(slide1);
   wrapper.appendChild(slide2);
+  container.appendChild(wrapper);
   container.appendChild(pagination);
   return container;
 }
