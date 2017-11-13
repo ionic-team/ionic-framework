12399b057bf58e793ced10e59ee85a725e2852bb
Cam Wiegert <cam@ionic.io>
2017-11-13 16:48:54 -0600

    fix(button) update toolbar e2e deps

--- packages/core/src/components/button/test/toolbar/e2e.js
+++ packages/core/src/components/button/test/toolbar/e2e.js
@@ -1,11 +1,8 @@
-'use strict';
-
-const register = require('../../../../scripts/e2e-test-runner').register;
-const E2ETestPage = require('../../../../scripts/E2ETestPage');
+const { register, Page } = require('../../../../../scripts/e2e');
 
 describe('button: toolbar', () => {
   register('navigates', (driver) => {
-    const page = new E2ETestPage(driver, 'http://localhost:3333/src/components/button/test/toolbar');
+    const page = new Page(driver, 'http://localhost:3333/src/components/button/test/toolbar');
     return page.navigate();
   });
 });
