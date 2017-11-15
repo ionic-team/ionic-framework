418db9437446c6429af17c8239c3af621652ca26
Cam Wiegert <cam@ionic.io>
2017-11-15 14:20:47 -0600

    feat(e2e-test) add present method to Page class

--- packages/core/scripts/e2e/e2e-test-page.js
+++ packages/core/scripts/e2e/e2e-test-page.js
@@ -13,4 +13,11 @@ module.exports = class E2ETestPage {
     this.driver.wait(until.elementLocated(By.css('.hydrated')));
     return this.driver.wait(until.elementIsVisible(this.driver.findElement(By.css('.hydrated'))));
   }
+
+  present(clickTarget, options) {
+    this.navigate();
+    this.driver.findElement(By.css(clickTarget)).click();
+    this.driver.wait(until.elementLocated(By.css(options.waitFor)));
+    return this.driver.wait(until.elementIsVisible(this.driver.findElement(By.css(options.waitFor))));
+  }
 }
