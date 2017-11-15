98302fe9384e230d5c8b4eea0475731972a4a1ca
Ken Sodemann <ken@ionic.io>
2017-11-14 22:05:55 -0600

    feat(demo): load ionic/core

--- packages/angular/demo/src/app/app.module.ts
+++ packages/angular/demo/src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {}
