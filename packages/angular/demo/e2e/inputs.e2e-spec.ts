eb548536148db4a07381faefcd32a01635957062
Ken Sodemann <ken@ionic.io>
2017-11-16 13:34:42 -0600

    feat(demo): expand the e2e testing

--- packages/angular/demo/e2e/inputs.e2e-spec.ts
+++ packages/angular/demo/e2e/inputs.e2e-spec.ts
@@ -12,11 +12,65 @@ describe('Demo Inputs Page', () => {
     expect(page.getTitleText()).toEqual('Ionic Core Inputs Demo');
   });
 
-  describe('input one', () => {
+  describe('text input', () => {
     it('should display the starting text', () => {
       page.navigateTo();
       const el = page.getIonicTextInput();
       expect(el.getAttribute('value')).toEqual('This is the Ionic Text Input');
+    });
+
+    it('should reflect back the entered data', () => {
+      page.navigateTo();
+      const el = page.getIonicTextInputEditable();
+      el.clear();
+      el.sendKeys('I am new text');
+      expect(page.getIonicTextInputOutputText()).toEqual('I am new text');
+    });
+  });
+
+  describe('textarea input', () => {
+    it('should display the starting text', () => {
+      page.navigateTo();
+      const el = page.getIonicTextareaInput();
+      expect(el.getAttribute('value')).toEqual('This is the Ionic Textarea Input');
+    });
+
+    it('should reflect back the entered data', () => {
+      page.navigateTo();
+      const el = page.getIonicTextareaInputEditable();
+      el.clear();
+      el.sendKeys('I am new text');
+      expect(page.getIonicTextareaInputOutputText()).toEqual('I am new text');
+    });
+  });
+
+  describe('checkbox input', () => {
+    it('should be set the initial value', () => {
+      page.navigateTo();
+      const el = page.getIonicCheckbox();
+      expect(el.getAttribute('checked')).toEqual('true');
+    });
+
+    it('should reflect toggling the value', () => {
+      page.navigateTo();
+      const el = page.getIonicCheckbox();
+      el.click();
+      expect(page.getIonicCheckboxOutputText()).toEqual('false');
+    });
+  });
+
+  describe('toggle input', () => {
+    it('should be set the initial value', () => {
+      page.navigateTo();
+      const el = page.getIonicToggle();
+      expect(el.getAttribute('checked')).toBeNull();
+    });
+
+    it('should reflect toggling the value', () => {
+      page.navigateTo();
+      const el = page.getIonicToggle();
+      el.click();
+      expect(page.getIonicToggleOutputText()).toEqual('true');
     });
   });
 });
--- packages/angular/demo/e2e/inputs.po.ts
+++ packages/angular/demo/e2e/inputs.po.ts
@@ -5,23 +5,49 @@ export class InputsPage {
     return browser.get('/inputs');
   }
 
-  getTitleText() {
-    return element(by.css('.title')).getText();
-  }
-
-  getIonicTextInput() {
-    return element(by.id('ionTextInput'));
-  }
-
-  getIonicTextInputOutputText() {
-    return element(by.id('ionTextInputOutput')).getText();
-  }
-
   getIonicCheckbox() {
     return element(by.id('ionCheckbox'));
   }
 
   getIonicCheckboxOutputText() {
     return element(by.id('ionCheckboxOutput')).getText();
+  }
+
+  getIonicToggle() {
+    return element(by.id('ionToggle'));
+  }
+
+  getIonicToggleOutputText() {
+    return element(by.id('ionToggleOutput')).getText();
+  }
+
+  getTitleText() {
+    return element(by.css('.title')).getText();
+  }
+
+  getIonicTextareaInput() {
+    return element(by.id('ionTextareaInput'));
+  }
+
+  getIonicTextareaInputEditable() {
+    const parent = this.getIonicTextareaInput();
+    return parent.all(by.css('textarea')).first();
+  }
+
+  getIonicTextareaInputOutputText() {
+    return element(by.id('ionTextareaInputOutput')).getText();
+  }
+
+  getIonicTextInput() {
+    return element(by.id('ionTextInput'));
+  }
+
+  getIonicTextInputEditable() {
+    const parent = this.getIonicTextInput();
+    return parent.all(by.css('input')).first();
+  }
+
+  getIonicTextInputOutputText() {
+    return element(by.id('ionTextInputOutput')).getText();
   }
 }
