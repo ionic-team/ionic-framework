63d5c9bac0d193f2038154c6683095cba54f584e
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-11-21 14:19:02 -0600

    chore(build): get package build process in place

---
+++ packages/angular/src/index.ts
@@ -0,0 +1,5 @@
+export * from './directives/ion-nav';
+export * from './module';
+export * from './providers/alert-controller';
+export * from './providers/app';
+export * from './providers/nav-controller';
