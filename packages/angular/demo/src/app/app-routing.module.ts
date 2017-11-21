833f384eb6456a08bfc3f6dfadd020dd6f628cb2
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-11-21 12:42:43 -0600

    refactor(): remove ionic module from angular project temporarily

--- packages/angular/demo/src/app/app-routing.module.ts
+++ packages/angular/demo/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'basic-inputs', loadChildren: 'app/basic-inputs-page/basic-inputs-page.module#BasicInputsPageModule' },
   { path: 'group-inputs', loadChildren: 'app/group-inputs-page/group-inputs-page.module#GroupInputsPageModule' },
   { path: 'home', loadChildren: 'app/home-page/home-page.module#HomePageModule' },
-  { path: 'alert', loadChildren: 'app/alert/alert.module#AlertModule' }
+  // { path: 'alert', loadChildren: 'app/alert/alert.module#AlertModule' }
 ];
 
 @NgModule({
