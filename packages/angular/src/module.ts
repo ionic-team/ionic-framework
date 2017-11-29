a09e10e5689427d9c9bf3d5bb0ef45fbbb50e47f
Ken Sodemann <ken@ionic.io>
2017-11-29 10:05:11 -0600

    WIP - remove notes as that stuff belongs in app importing this

--- packages/angular/src/module.ts
+++ packages/angular/src/module.ts
@@ -1,4 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { IonBooleanValueAccessor } from './directives/ion-boolean-value-accessor';
@@ -24,12 +23,6 @@ import { AlertController } from './providers/alert-controller';
     IonSelectValueAccessor,
     IonTextValueAccessor
   ],
-  // NOTE: Not sure if we should be including this here or not, but...
-  //  - If not included, ElementRef (and probably Renderer2) are not provided (needed by value accessors)
-  //  - Even if included fixing above issue, NgZone (needed by ion-nav) is not provided for some reason
-  // imports: [
-  //   BrowserModule
-  // ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ],
