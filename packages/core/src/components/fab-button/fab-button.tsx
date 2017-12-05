a2b88c5cb8384846d3545dfc81fd5047d60db26b
Brandy Carney <brandyscarney@users.noreply.github.com>
2017-12-05 15:28:58 -0500

    refactor(fab): rename files and styles for the fab components (#13585)
    * refactor(fab): rename files to match tag names
    
    * refactor(fab): fix class names and separate styles
    
    * chore(config): bundle fab button separately

---
+++ packages/core/src/components/fab-button/fab-button.tsx
@@ -0,0 +1,149 @@
+import { Component, CssClassMap, Element, Prop, State } from '@stencil/core';
+import { createThemedClasses, getElementClassObject } from '../../utils/theme';
+
+
+@Component({
+  tag: 'ion-fab-button',
+  styleUrls: {
+    ios: 'fab-button.ios.scss',
+    md: 'fab-button.md.scss'
+  }
+})
+export class FabButton {
+  @Element() private el: HTMLElement;
+
+  /**
+   * @input {string} The color to use from your Sass `$colors` map.
+   * Default options are: `"primary"`, `"secondary"`, `"danger"`, `"light"`, and `"dark"`.
+   * For more information, see [Theming your App](/docs/theming/theming-your-app).
+   */
+  @Prop() color: string;
+
+  /**
+   * @input {string} The mode determines which platform styles to use.
+   * Possible values are: `"ios"` or `"md"`.
+   * For more information, see [Platform Styles](/docs/theming/platform-specific-styles).
+   */
+  @Prop() mode: 'ios' | 'md';
+
+  /**
+   * @input {string} Contains a URL or a URL fragment that the hyperlink points to.
+   * If this property is set, an anchor tag will be rendered.
+   */
+  @Prop() href: string;
+
+  /**
+   * @input {boolean} If true, adds transparency to the fab.
+   * Only affects `ios` mode. Defaults to `false`.
+   */
+  @Prop() translucent: boolean = false;
+
+  @Prop() activated: boolean = false;
+  @Prop() toggleActive: Function = () => {};
+
+  @Prop() show: boolean = false;
+
+  @State() private inContainer: boolean = false;
+  @State() private inList: boolean = false;
+
+  /**
+   * @input {boolean} If true, sets the button into a disabled state.
+   */
+  @Prop() disabled: boolean = false;
+
+  componentDidLoad() {
+    const parentNode = this.el.parentNode.nodeName;
+
+    this.inList = (parentNode === 'ION-FAB-LIST');
+    this.inContainer = (parentNode === 'ION-FAB');
+  }
+
+
+  clickedFab() {
+    if (this.inContainer) {
+      this.toggleActive();
+    }
+  }
+
+  /**
+   * @hidden
+   * Get the classes for fab buttons in lists
+   */
+  getFabListClassList() {
+    if (!this.inList) {
+      return [];
+    }
+    let listClasses = [
+      `fab-button-in-list`,
+      `fab-button-${this.mode}-in-list`
+    ];
+
+    if (this.translucent) {
+      listClasses.push(`fab-button-translucent-${this.mode}-in-list`);
+    }
+
+    return listClasses;
+  }
+
+  /**
+   * @hidden
+   * Get the close active class for fab buttons
+   */
+  getFabActiveClassList() {
+    if (!this.activated) {
+      return [];
+    }
+    return [
+      `fab-button-close-active`
+    ];
+  }
+
+  /**
+   * @hidden
+   * Get the show class for fab buttons
+   */
+  getFabShowClassList() {
+    if (!this.show) {
+      return [];
+    }
+    return [
+      `fab-button-show`
+    ];
+  }
+
+  render() {
+    const themedClasses = createThemedClasses(this.mode, this.color, 'fab-button');
+    const translucentClasses = this.translucent ? createThemedClasses(this.mode, this.color, 'fab-button-translucent') : {};
+    const hostClasses = getElementClassObject(this.el.classList);
+
+    const elementClasses: CssClassMap = []
+      .concat(
+        this.getFabListClassList(),
+        this.getFabActiveClassList(),
+        this.getFabShowClassList()
+      )
+      .reduce((prevValue, cssClass) => {
+        prevValue[cssClass] = true;
+        return prevValue;
+      }, {});
+
+    const TagType = this.href ? 'a' : 'button';
+
+    const fabClasses = {
+      ...themedClasses,
+      ...translucentClasses,
+      ...hostClasses,
+      ...elementClasses
+    };
+
+    return (
+      <TagType class={fabClasses} onClick={this.clickedFab.bind(this)} disabled={this.disabled}>
+        <ion-icon name='close' class='fab-button-close-icon'></ion-icon>
+        <span class='button-inner'>
+          <slot></slot>
+        </span>
+        <div class='button-effect'></div>
+      </TagType>
+    );
+  }
+}
---
+++ packages/core/src/components/fab-button/readme.md
@@ -0,0 +1,126 @@
+# ion-fab-button
+
+Floating Action Buttons (FABs) represent the primary action in an application. By default, they have a circular shape. When pressed, the button may open more related actions. As the name suggests, FABs generally float over the content in a fixed position. This is not achieved exclusively by using an `<ion-fab-button>FAB</ion-fab-button>`. They need to be wrapped with an `<ion-fab>` component in order to be fixed over the content:
+
+```html
+<ion-content>
+  <!-- Fixed Floating Action Button that does not scroll with the content -->
+  <ion-fab>
+    <ion-fab-button>Button</ion-fab-button>
+  </ion-fab>
+
+  <!-- Default Floating Action Button that scrolls with the content.-->
+  <ion-fab-button>Button</ion-fab-button>
+</ion-content>
+```
+
+If the FAB button is not wrapped with `<ion-fab>`, it will scroll with the content. FAB buttons have a default size, a mini size and can accept different colors:
+
+```html
+<ion-content>
+  <ion-fab-button>Default</ion-fab-button>
+
+  <!-- Mini -->
+  <ion-fab-button mini>Mini</ion-fab-button>
+
+  <!-- Colors -->
+  <ion-fab-button color="primary">Primary</ion-fab-button>
+  <ion-fab-button color="secondary">Secondary</ion-fab-button>
+  <ion-fab-button color="danger">Danger</ion-fab-button>
+  <ion-fab-button color="light">Light</ion-fab-button>
+  <ion-fab-button color="dark">Dark</ion-fab-button>
+</ion-content>
+```
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+#### activated
+
+boolean
+
+
+#### color
+
+string
+
+
+#### disabled
+
+boolean
+
+
+#### href
+
+string
+
+
+#### mode
+
+any
+
+
+#### show
+
+boolean
+
+
+#### toggleActive
+
+any
+
+
+#### translucent
+
+boolean
+
+
+## Attributes
+
+#### activated
+
+boolean
+
+
+#### color
+
+string
+
+
+#### disabled
+
+boolean
+
+
+#### href
+
+string
+
+
+#### mode
+
+any
+
+
+#### show
+
+boolean
+
+
+#### toggleActive
+
+any
+
+
+#### translucent
+
+boolean
+
+
+
+----------------------------------------------
+
+*Built by [StencilJS](https://stenciljs.com/)*
