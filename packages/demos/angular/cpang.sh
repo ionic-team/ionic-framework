03ec0d23b15834a82dc99d3eb8fc7e31dcec17b0
Ken Sodemann <ken@ionic.io>
2017-12-07 17:43:25 -0600

    chore(): add scripts to copy @ionic/core and @ionic/angular

---
+++ packages/demos/angular/cpang.sh
@@ -0,0 +1,2 @@
+rm -rf node_modules/\@ionic/angular/dist
+cp -r ../../core/dist/ node_modules/\@ionic/angular/dist
---
+++ packages/demos/angular/cpcore.sh
@@ -0,0 +1,2 @@
+rm -rf node_modules/\@ionic/core/dist
+cp -r ../../core/dist/ node_modules/\@ionic/core/dist
