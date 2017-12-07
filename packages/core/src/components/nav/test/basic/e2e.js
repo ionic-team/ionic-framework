2d1147a3ac930c044b52dd5ef13effee90dbff13
Cam Wiegert <cam@ionic.io>
2017-12-07 15:42:38 -0600

    feat(e2e) parameterize navigate method

--- packages/core/src/components/nav/test/basic/e2e.js
+++ packages/core/src/components/nav/test/basic/e2e.js
@@ -2,6 +2,6 @@ const { register, navigate } = require('../../../../../scripts/e2e');
 
 describe('nav/basic', () => {
 
-  register('should init', navigate('http://localhost:3333/src/components/nav/test/basic'));
+  register('should init', navigate('http://localhost:3333/src/components/nav/test/basic', 'ion-nav'));
 
 });
