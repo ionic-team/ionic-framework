8c2e6b1c25afaf6f0cdf76e2aef0ff6a91e9d758
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-12-21 15:50:44 -0600

    chore(demos): initial commit of conf app

---
+++ packages/demos/conference-app/angular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule, } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
+})
+export class AppModule { }
