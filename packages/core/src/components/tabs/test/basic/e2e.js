00e11bd4d993997a5c70eb753555c23c0f3ec134
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-12-21 15:31:26 -0600

    test(tabs): add e2e tests for basic scenario with pages, and vanilla scenario with divs

--- packages/core/src/components/tabs/test/basic/e2e.js
+++ packages/core/src/components/tabs/test/basic/e2e.js
@@ -15,5 +15,40 @@ platforms.forEach(platform => {
       const page = new E2ETestPage(driver, platform);
       return page.navigate();
     });
+
+    register('should check each tab', async (driver, testContext) => {
+      testContext.timeout(60000);
+      const page = new E2ETestPage(driver, platform);
+
+      await waitForTransition(300);
+
+      const tabTwoButton = await waitAndGetElementById(driver, 'tab-t-0-1');
+      tabTwoButton.click();
+      await waitForTransition(600);
+
+      const tabThreeButton = await waitAndGetElementById(driver, 'tab-t-0-2');
+      tabThreeButton.click();
+      await waitForTransition(600);
+    });
   });
 });
+
+async function waitAndGetElement(driver, selector) {
+  driver.wait(until.elementLocated(By.css(selector)));
+  const element = driver.findElement(By.css(selector));
+  await driver.wait(until.elementIsVisible(driver.findElement(By.css(selector))));
+  return element;
+}
+
+async function waitAndGetElementById(driver, selector) {
+  driver.wait(until.elementLocated(By.id(selector)));
+  const element = driver.findElement(By.id(selector));
+  await driver.wait(until.elementIsVisible(driver.findElement(By.id(selector))));
+  return element;
+}
+
+function waitForTransition(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration);
+  })
+}
