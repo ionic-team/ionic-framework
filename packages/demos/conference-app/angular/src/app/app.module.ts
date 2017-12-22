f5392297d21c71a76950a9c2993908f1d25b6a4e
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-12-22 14:51:38 -0600

    chore(demos): making progress on the conf app

--- packages/demos/conference-app/angular/src/app/app.module.ts
+++ packages/demos/conference-app/angular/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule, } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
+import { IonicAngularModule } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { AppComponent } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { PopoverPage } from '../pages/about-popover/about-popover';
+import { MapPage } from '../pages/map/map';
+import { TabsPage } from '../pages/tabs-page/tabs-page';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AboutPage,
+    AppComponent,
+    MapPage,
+    PopoverPage,
+    TabsPage
+  ],
+  entryComponents: [
+    AboutPage,
+    MapPage,
+    PopoverPage,
+    TabsPage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IonicAngularModule.forRoot(),
+    IonicStorageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ packages/demos/conference-app/angular/src/pages/about/about.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+import { PopoverPage } from '../about-popover/about-popover';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+  styleUrls: ['./about.scss']
+})
+export class AboutPage {
+  conferenceDate = '2047-05-17';
+
+  constructor(public popoverCtrl: PopoverController) { }
+
+  presentPopover(event: Event) {
+    const popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({ ev: event });
+  }
+}
