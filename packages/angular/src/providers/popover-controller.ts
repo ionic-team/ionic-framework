89bce36615208101fb920aeda8b3892d6110d17d
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-01-03 16:23:16 -0600

    refactor(angular): optionally wrap the user's template in

--- packages/angular/src/providers/popover-controller.ts
+++ packages/angular/src/providers/popover-controller.ts
@@ -31,11 +31,11 @@ export class PopoverController implements FrameworkDelegate {
 
   attachViewToDom(elementOrContainerToMountTo: HTMLElement, elementOrComponentToMount: Type<any>, _propsOrDataObj?: any, classesToAdd?: string[]): Promise<AngularMountingData> {
 
-    return this.angularComponentMounter.attachViewToDom(elementOrContainerToMountTo, null, elementOrComponentToMount, this.componentResolveFactory, this.injector, _propsOrDataObj, classesToAdd);
+    return this.angularComponentMounter.attachViewToDom(elementOrContainerToMountTo, null, elementOrComponentToMount, this.componentResolveFactory, this.injector, _propsOrDataObj, classesToAdd, false);
   }
 
-  removeViewFromDom(_parentElement: HTMLElement, childElement: HTMLElement) {
-    return this.angularComponentMounter.removeViewFromDom(childElement);
+  removeViewFromDom(parentElement: HTMLElement, childElement: HTMLElement) {
+    return this.angularComponentMounter.removeViewFromDom(parentElement, childElement);
   }
 }
 
