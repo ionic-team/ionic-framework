fd0aa4546195999b4eb7d475a787c6077226456a
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-01-09 16:02:57 -0600

    refactor(popover): remove ion-page from template

--- packages/demos/conference-app/angular/src/pages/about-popover/about-popover.ts
+++ packages/demos/conference-app/angular/src/pages/about-popover/about-popover.ts
@@ -6,25 +6,23 @@ import { App } from '@ionic/angular';
 
 @Component({
   template: `
-  <ion-page>
-    <ion-list>
-      <ion-item (click)="close('http://ionicframework.com/docs/v2/getting-started')">
-        <ion-label>Learn Ionic</ion-label>
-      </ion-item>
-      <ion-item (click)="close('http://ionicframework.com/docs/v2')">
-        <ion-label>Documentation</ion-label>
-      </ion-item>
-      <ion-item (click)="close('http://showcase.ionicframework.com')">
-        <ion-label>Showcase</ion-label>
-      </ion-item>
-      <ion-item (click)="close('https://github.com/ionic-team/ionic')">
-        <ion-label>GitHub Repo</ion-label>
-      </ion-item>
-      <ion-item (click)="support()">
-        <ion-label>Support</ion-label>
-      </ion-item>
-    </ion-list>
-  </ion-page>
+  <ion-list>
+    <ion-item (click)="close('http://ionicframework.com/docs/v2/getting-started')">
+      <ion-label>Learn Ionic</ion-label>
+    </ion-item>
+    <ion-item (click)="close('http://ionicframework.com/docs/v2')">
+      <ion-label>Documentation</ion-label>
+    </ion-item>
+    <ion-item (click)="close('http://showcase.ionicframework.com')">
+      <ion-label>Showcase</ion-label>
+    </ion-item>
+    <ion-item (click)="close('https://github.com/ionic-team/ionic')">
+      <ion-label>GitHub Repo</ion-label>
+    </ion-item>
+    <ion-item (click)="support()">
+      <ion-label>Support</ion-label>
+    </ion-item>
+  </ion-list>
   `
 })
 export class PopoverPage {
