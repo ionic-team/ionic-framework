9d9a08fa7a57994b9ed40f47e73d4b42c44ac85b
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-02-21 09:26:18 -0600

    updates

--- packages/demos/conference-app/angular/src/app/app.module.ts
+++ packages/demos/conference-app/angular/src/app/app.module.ts
@@ -3,77 +3,34 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { IonicAngularModule } from '@ionic/angular';
+import { IonicAngularModule, IonicRouterModule } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { PopoverPage } from '../pages/about-popover/about-popover';
-import { AccountPage } from '../pages/account/account';
-import { LoginPage } from '../pages/login/login';
-import { MapPage } from '../pages/map/map';
-import { SchedulePage } from '../pages/schedule/schedule';
-import { ScheduleFilterPage } from '../pages/schedule-filter/schedule-filter';
-import { SessionDetailPage } from '../pages/session-detail/session-detail';
-import { SignupPage } from '../pages/signup/signup';
-import { SpeakerDetailPage } from '../pages/speaker-detail/speaker-detail';
-import { SpeakerListPage } from '../pages/speaker-list/speaker-list';
-import { SupportPage } from '../pages/support/support';
-import { TabsPage } from '../pages/tabs-page/tabs-page';
-import { TutorialPage } from '../pages/tutorial/tutorial';
-
-import { ConferenceData } from '../providers/conference-data';
-import { UserData } from '../providers/user-data';
+import { ConferenceData } from './providers/conference-data';
+import { UserData } from './providers/user-data';
 
 @NgModule({
   declarations: [
-    AboutPage,
-    AccountPage,
     AppComponent,
-    LoginPage,
-    MapPage,
-    PopoverPage,
-    SchedulePage,
-    ScheduleFilterPage,
-    SessionDetailPage,
-    SignupPage,
-    SpeakerDetailPage,
-    SpeakerListPage,
-    SupportPage,
-    TabsPage,
-    TutorialPage
-  ],
-  entryComponents: [
-    AboutPage,
-    AccountPage,
-    LoginPage,
-    MapPage,
-    PopoverPage,
-    SchedulePage,
-    ScheduleFilterPage,
-    SessionDetailPage,
-    SignupPage,
-    SpeakerDetailPage,
-    SpeakerListPage,
-    SupportPage,
-    TabsPage,
-    TutorialPage
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    FormsModule,
     HttpModule,
     IonicAngularModule.forRoot(),
-    IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot(),
+    IonicRouterModule.forRoot()
   ],
   providers: [
-    ConferenceData,
+    // ConferenceData,
     InAppBrowser,
     SplashScreen,
-    UserData
+    // UserData
   ],
   bootstrap: [AppComponent],
   schemas: [
