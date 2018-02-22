9ef4cbb3a73e883dc6cde7271ecb35c800d95d2f
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-02-21 20:28:16 -0600

    removed bloat to make it easier to see issue

--- packages/demos/conference-app/angular/src/app/app.component.ts
+++ packages/demos/conference-app/angular/src/app/app.component.ts
@@ -23,6 +23,7 @@ import { UserData } from './providers/user-data';
 export class AppComponent implements OnInit {
 
   loggedIn = false;
+
   constructor(
     private events: Events,
     private router: Router,
@@ -31,45 +32,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.checkLoginStatus();
-    // this.listenForLoginEvents();
-    /*setInterval(() => {
-      this.loggedIn = !this.loggedIn;
-    }, 2000);
-    */
+
   }
 
   checkLoginStatus() {
-
-    // synchronous works
-    // this.loggedIn = true;
-
-
-    // from resolved promise works
-    /*return Promise.resolve().then(() => {
-      this.loggedIn = true;
-    });
-    */
-
-    // new promise w/ set-timeout works
-    /*return new Promise((resolve) => {
-      this.loggedIn = true;
-      setTimeout(() => {
-        resolve();
-      }, 0);
-    });
-
-    */
-
-    // with set-timeout does not work
-    /*return this.userData.isLoggedIn().then((loggedIn) => {
-      setTimeout(() => {
-        this.loggedIn = true;
-      }, 0);
-    });
-    */
-
-
-    // vanilla promise resolve does not work
+// vanilla promise resolve does not work
     return this.userData.isLoggedIn().then((loggedIn) => {
       this.loggedIn = true;
       // this.loggedIn = loggedIn;
@@ -77,44 +44,5 @@ export class AppComponent implements OnInit {
     });
   }
 
-  listenForLoginEvents() {
-    /*this.events.subscribe('user:login', () => {
-      this.loggedIn = true;
-    });
-
-    this.events.subscribe('user:signup', () => {
-      this.loggedIn = true;
-    });
-
-    this.events.subscribe('user:logout', () => {
-      this.loggedIn = false;
-    });
-    */
-  }
-
-  selectTab(index: number) {
-    const tabs = document.querySelector('ion-tabs');
-    if (tabs) {
-      return tabs.componentOnReady().then(() => {
-        return tabs.select(index);
-      });
-    }
-  }
-
-  login() {
-    this.router.navigateByUrl('/login');
-  }
-
-  signup() {
-    this.router.navigateByUrl('/sign-up');
-  }
-
-  openSupport() {
-    this.router.navigateByUrl('/support');
-  }
-
-  openTutorial() {
-    alert('todo tutorial');
-  }
 }
 
