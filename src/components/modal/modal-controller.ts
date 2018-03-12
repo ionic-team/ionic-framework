433139e990eda2f9ae9408bbf99e6acfbc358219
Brandy Carney <brandyscarney@users.noreply.github.com>
2018-03-12 12:26:55 -0400

    docs(modal): update indentation and add bracket

--- src/components/modal/modal-controller.ts
+++ src/components/modal/modal-controller.ts
@@ -77,43 +77,43 @@ import { DeepLinker } from '../../navigation/deep-linker';
  * @Component(...)
  * class HomePage {
  *
- *  constructor(public modalCtrl: ModalController) {
+ *   constructor(public modalCtrl: ModalController) {
  *
- *  }
+ *   }
  *
- *  presentContactModal() {
- *    let contactModal = this.modalCtrl.create(ContactUs);
- *    contactModal.present();
- *  }
+ *   presentContactModal() {
+ *     let contactModal = this.modalCtrl.create(ContactUs);
+ *     contactModal.present();
+ *   }
  *
- *  presentProfileModal() {
- *    let profileModal = this.modalCtrl.create(Profile, { userId: 8675309 });
+ *   presentProfileModal() {
+ *     let profileModal = this.modalCtrl.create(Profile, { userId: 8675309 });
  *    
- *    //fires after dismiss animation finishes
- *    profileModal.onDidDismiss(data => {
- *      console.log(data);
- *    });
+ *     // fires after dismiss animation finishes
+ *     profileModal.onDidDismiss(data => {
+ *       console.log(data);
+ *     });
  *    
- *    //fires before dismiss animation begins
- *    profileModal.onWillDismiss(data => {
- *      console.log(data);
- *    });
- *    profileModal.present();
- *  }
+ *     // fires before dismiss animation begins
+ *     profileModal.onWillDismiss(data => {
+ *       console.log(data);
+ *     });
  *
+ *     profileModal.present();
+ *   }
+ * }
  *
  * @Component(...)
  * class Profile {
  *
- *  constructor(public viewCtrl: ViewController) {
+ *   constructor(public viewCtrl: ViewController) {
  *
- *  }
+ *   }
  *
- *  dismiss() {
- *    let data = { 'foo': 'bar' };
- *    this.viewCtrl.dismiss(data);
- *  }
- *
+ *   dismiss() {
+ *     let data = { 'foo': 'bar' };
+ *     this.viewCtrl.dismiss(data);
+ *   }
  * }
  * ```
  *
