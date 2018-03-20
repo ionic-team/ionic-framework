65e2ea751dcf600fa165f98a71b4f36b7d1aca9a
mhartington <mikehartington@gmail.com>
2018-03-20 17:21:00 -0400

    fix(): refactor back-button and menu-button

--- core/src/components/menu-button/menu-button.tsx
+++ core/src/components/menu-button/menu-button.tsx
@@ -13,10 +13,14 @@ import { Config } from '../../index';
 })
 export class MenuButton {
 
+<<<<<<< HEAD
   private custom = true;
 
   @Prop({ context: 'config' }) config: Config;
 
+=======
+  private mode: string;
+>>>>>>> fix(): refactor back-button and menu-button
   /**
    * Optional property that maps to a Menu's `menuId` prop. Can also be `left` or `right` for the menu side. This is used to find the correct menu to toggle
    */
@@ -29,19 +33,15 @@ export class MenuButton {
 
   @Element() el: HTMLElement;
 
-  componentWillLoad() {
-    this.custom = this.el.childElementCount > 0;
-  }
 
   render() {
     const menuIcon = this.config.get('menuIcon', 'menu');
     return (
       <ion-menu-toggle menu={this.menu} autoHide={this.autoHide}>
-        <ion-button>
-          {this.custom
-            ? <slot/>
-            : <ion-icon slot='icon-only' name={menuIcon}/>}
-        </ion-button>
+        <button class='menu-button-inner'>
+          <ion-icon name={menuIcon}/>
+          { this.mode === 'md' && <ion-ripple-effect/> }
+        </button>
       </ion-menu-toggle>
     );
   }
