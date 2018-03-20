769282935b929303db30c72e853da996109dc1b8
Brandy Carney <brandy@ionic.io>
2018-03-20 14:06:08 -0400

    test(e2e): update e2e tests to pass

--- core/src/components/menu/test/standalone/e2e.js
+++ core/src/components/menu/test/standalone/e2e.js
@@ -13,7 +13,7 @@ platforms.forEach(platform => {
   describe('menu/standalone', () => {
     register('should init', driver => {
       const page = new E2ETestPage(driver, platform);
-      return page.navigate('ion-page');
+      return page.navigate('.ion-page');
     });
   });
 });
--- core/src/components/modal/test/basic/e2e.js
+++ core/src/components/modal/test/basic/e2e.js
@@ -7,13 +7,6 @@ class E2ETestPage extends Page {
   constructor(driver, platform) {
     super(driver, `http://localhost:3333/src/components/modal/test/basic?ionicplatform=${platform}`);
   }
-
-  async present(buttonId) {
-    await this.navigate('#presentModal');
-    this.driver.findElement(By.id(buttonId)).click();
-    await this.driver.wait(until.elementLocated(By.css('.modal-wrapper')));
-    return await this.driver.wait(until.elementIsVisible(this.driver.findElement(By.css('.modal-wrapper'))));
-  }
 }
 
 platforms.forEach(platform => {
