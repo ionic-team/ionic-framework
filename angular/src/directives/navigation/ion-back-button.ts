1f78390c845edaa0649d664fb1f3ef2ef54ad5e7
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-04-03 13:34:24 +0200

    fix(angular): back-button

--- angular/src/directives/navigation/ion-back-button.ts
+++ angular/src/directives/navigation/ion-back-button.ts
@@ -1,12 +1,16 @@
-import { Directive, HostListener, Optional } from '@angular/core';
+import { Directive, HostListener, Input, Optional } from '@angular/core';
 import { IonRouterOutlet } from './ion-router-outlet';
+import { Router } from '@angular/router';
 
 @Directive({
   selector: 'ion-back-button'
 })
 export class IonBackButton {
 
+  @Input() defaultHref: string;
+
   constructor(
+    @Optional() private router: Router,
     @Optional() private routerOutlet: IonRouterOutlet,
   ) {}
 
@@ -14,6 +18,8 @@ export class IonBackButton {
   onClick() {
     if (this.routerOutlet && this.routerOutlet.canGoBack()) {
       this.routerOutlet.pop();
+    } else if (this.router && this.defaultHref != null) {
+      this.router.navigateByUrl(this.defaultHref);
     }
   }
 }
