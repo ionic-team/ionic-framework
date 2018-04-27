ce74e9b12ec288aa7bdc1730b3415609c77568c3
Brandy Carney <brandy@ionic.io>
2018-04-27 11:48:23 -0400

    fix(toast): only use constant and env if supported

--- core/src/components/toast/animations/ios.enter.ts
+++ core/src/components/toast/animations/ios.enter.ts
@@ -10,9 +10,20 @@ export default function iosEnterAnimation(Animation: Animation, baseEl: HTMLElem
   const wrapperEle = baseEl.querySelector('.toast-wrapper') as HTMLElement;
   wrapperAnimation.addElement(wrapperEle);
 
+  let variable;
+
+  if (CSS.supports('bottom', 'env(safe-area-inset-bottom)')) {
+    variable = 'env';
+  } else if (CSS.supports('bottom', 'constant(safe-area-inset-bottom)')) {
+    variable = 'constant';
+  }
+
+  const bottom = variable ? 'calc(-10px - ' + variable + '(safe-area-inset-bottom))' : '-10px';
+  const top = variable ? 'calc(' + variable + '(safe-area-inset-top) + 10px)' : '10px';
+
   switch (position) {
     case 'top':
-      wrapperAnimation.fromTo('translateY', '-100%', 'calc(env(safe-area-inset-top) + 10px)');
+      wrapperAnimation.fromTo('translateY', '-100%', top);
       break;
     case 'middle':
       const topPosition = Math.floor(
@@ -22,7 +33,7 @@ export default function iosEnterAnimation(Animation: Animation, baseEl: HTMLElem
       wrapperAnimation.fromTo('opacity', 0.01, 1);
       break;
     default:
-      wrapperAnimation.fromTo('translateY', '100%', 'calc(-10px - env(safe-area-inset-bottom))');
+      wrapperAnimation.fromTo('translateY', '100%', bottom);
       break;
   }
   return Promise.resolve(baseAnimation
