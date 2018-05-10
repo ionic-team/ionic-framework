f7bcb680a3f008239fafc5841180a6d6070432c6
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-05-10 16:30:20 +0200

    fix(angular): unexport some es2017 js

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,9 +1,7 @@
 // Util functions
 export * from './utils/helpers';
 export * from './utils/haptic';
-export * from './utils/overlays';
 export * from './utils/framework-delegate';
 export * from './utils/platform';
-export * from './utils/transition';
 export * from './utils/config';
 
