6d5606c1d38be131324f88041bd5075d99bb73f0
Brandy Carney <brandy@ionic.io>
2018-06-08 16:46:59 -0400

    fix(avatar): add theme back

--- core/src/components/avatar/avatar.tsx
+++ core/src/components/avatar/avatar.tsx
@@ -6,6 +6,9 @@ import { Component } from '@stencil/core';
   styleUrls: {
     ios: 'avatar.ios.scss',
     md: 'avatar.md.scss'
+  },
+  host: {
+    theme: 'avatar'
   }
 })
 export class Avatar {}
