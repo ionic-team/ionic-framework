ba99744f148c11628a5eb37788be161d331e2635
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-06-26 19:27:19 +0200

    wip

--- core/src/components/back-button/back-button.tsx
+++ core/src/components/back-button/back-button.tsx
@@ -63,6 +63,7 @@ export class BackButton {
     return {
       class: {
         ...createColorClasses(this.color),
+        'button': true,
         'show-back-button': showBackButton
       },
       'tappable': '',
