2c37a5a7b279da9f82b377d54b27ba2a8391eaed
Brandy Carney <brandy@ionic.io>
2018-07-24 16:27:17 -0400

    docs(previews): update preview tests to have translucent white toolbars
    to make ben happy

--- core/src/components/note/note.tsx
+++ core/src/components/note/note.tsx
@@ -14,7 +14,7 @@ export class Note {
   /**
    * The color to use from your application's color palette.
    * Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`.
-   * For more information on theming colors, see [theming](/docs/theming/basics).
+   * For more information on colors, see [theming](/docs/theming/basics).
    */
   @Prop() color?: Color;
 
