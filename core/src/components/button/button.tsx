a08b0f950318cadd8e64f5fb493777d7c7826217
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-08-24 23:58:49 +0200

    chore(): fix linter issues

--- core/src/components/button/button.tsx
+++ core/src/components/button/button.tsx
@@ -183,7 +183,7 @@ export class Button {
             <slot></slot>
             <slot name="end"></slot>
           </span>
-         { this.mode === 'md' && <ion-ripple-effect  /> }
+         { this.mode === 'md' && <ion-ripple-effect /> }
       </TagType>
     );
   }
