dc82675ccbd3a1f2d642aaa5d119c54bf2aaa9b8
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-08-25 10:18:32 +0200

    fix(app): listen statusTap event

--- core/src/utils/status-tap.ts
+++ core/src/utils/status-tap.ts
@@ -1,19 +1,22 @@
 import { QueueApi } from '@stencil/core';
 
 export function startStatusTap(win: Window, queue: QueueApi) {
-  queue.read(async () => {
-    const width = win.innerWidth;
-    const height = win.innerWidth;
-    const el = win.document.elementFromPoint(width / 2, height / 2);
-    if (!el) {
-      return;
-    }
-    const contentEl = el.closest('ion-content');
-    if (contentEl) {
-      await contentEl.componentOnReady();
-      queue.write(() => {
-        contentEl.scrollToTop(300);
-      });
-    }
+  win.addEventListener('statusTap', () => {
+    queue.read(() => {
+      const width = win.innerWidth;
+      const height = win.innerHeight;
+      const el = win.document.elementFromPoint(width / 2, height / 2);
+      if (!el) {
+        return;
+      }
+      const contentEl = el.closest('ion-content');
+      if (contentEl) {
+        contentEl.componentOnReady().then(() => {
+          queue.write(() => {
+            contentEl.scrollToTop(300);
+          });
+        });
+      }
+    });
   });
 }
