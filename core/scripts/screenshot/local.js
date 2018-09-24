315fb70995906036faee78cfe5b20b4f142fd912
Daniel Imhoff <dwieeb@gmail.com>
2018-09-24 17:16:24 -0500

    chore(screenshot): download missing images from public url

--- core/scripts/screenshot/local.js
+++ core/scripts/screenshot/local.js
@@ -1,9 +1,35 @@
 const ScreenshotConnector = require('@stencil/core/screenshot/screenshot-connector');
 const fs = require('fs');
 const path = require('path');
-
+const https = require('https');
 
 class LocalScreenshotConnector extends ScreenshotConnector {
+
+  downloadImage(image) {
+    return new Promise((resolve, reject) => {
+      const req = https.request({
+        method: 'GET',
+        hostname: 'screenshot.ionicframework.com',
+        path: `/images/${image}`,
+      });
+
+      req.on('response', res => {
+        if (res.statusCode !== 200) {
+          return reject(new Error(`Bad Status Code: ${res.statusCode}`));
+        }
+
+        const f = fs.createWriteStream(path.join(this.imagesDir, image));
+        f.on('error', reject);
+
+        res.on('error', reject);
+        res.on('end', resolve);
+        res.pipe(f);
+      });
+
+      req.on('error', reject);
+      req.end();
+    });
+  }
 
   async pullMasterImages() {
     const timespan = this.logger.createTimeSpan(`pull master screenshot images started`);
@@ -20,10 +46,18 @@ class LocalScreenshotConnector extends ScreenshotConnector {
       return true
     });
 
-    missingImages.forEach(missingImage => {
-      const url = missingImage;
-      this.logger.info(`downloading: ${url}`);
-    });
+    if (missingImages.length > 0) {
+      await Promise.all(missingImages.map(async image => {
+        this.logger.debug(`downloading: ${image}`);
+
+        try {
+          await this.downloadImage(image);
+        } catch (e) {
+          this.logger.error(`Error with ${image}: ${e}`);
+          throw e;
+        }
+      }));
+    }
 
     timespan.finish(`pull master screenshot images finished`);
   }
