45b43ff96341bf3a563f7c370e0a5d9626e323e7
mhartington <mikehartington@gmail.com>
2018-09-25 14:08:23 -0400

    fix(): add safe area cutouts

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -4021,7 +4021,7 @@ export namespace Components {
     * The text to display on the ok button. Default: `OK`.
     */
     'okText': string;
-    'open': (ev?: UIEvent | undefined) => Promise<HTMLIonActionSheetElement | HTMLIonAlertElement | HTMLIonPopoverElement>;
+    'open': (ev?: UIEvent | undefined) => Promise<HTMLIonPopoverElement | HTMLIonActionSheetElement | HTMLIonAlertElement>;
     /**
     * The text to display when the select is empty.
     */
