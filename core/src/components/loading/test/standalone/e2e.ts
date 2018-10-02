05f3d92af4d27a564ab90b5e9a3b19d927874bf4
Cam Wiegert <cam@camwiegert.com>
2018-10-02 10:39:49 -0500

    test(loading): migrate e2e tests
    references #15803

---
+++ core/src/components/loading/test/standalone/e2e.ts
@@ -0,0 +1,14 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+it('loading: standalone', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/loading/test/standalone?ionic:animated=false'
+  });
+
+  await page.click('#basic');
+  const loading = await page.find('ion-loading');
+  expect(loading).not.toBeNull();
+
+  const compare = await page.compareScreenshot();
+  expect(compare).toMatchScreenshot();
+});
