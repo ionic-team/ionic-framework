7bde3442914146acfec897e1dd6973396d878dc6
Brandy Carney <brandy@ionic.io>
2018-10-10 11:35:00 -0500

    test(card-header): use testing query instead of animated

--- core/src/components/card-header/test/basic/e2e.ts
+++ core/src/components/card-header/test/basic/e2e.ts
@@ -2,7 +2,7 @@ import { newE2EPage } from '@stencil/core/testing';
 
 it('card-header: basic', async () => {
   const page = await newE2EPage({
-    url: '/src/components/card-header/test/basic?ionic:animated=false'
+    url: '/src/components/card-header/test/basic?ionic:_testing=true'
   });
 
   const compare = await page.compareScreenshot();
