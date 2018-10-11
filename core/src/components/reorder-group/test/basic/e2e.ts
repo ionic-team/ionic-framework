0e3ade60f6e80c2befd9ee9de36201765e39047c
Adam Bradley <adambradley25@gmail.com>
2018-10-11 16:04:02 -0500

    test(e2e): fix reorder-group

--- core/src/components/reorder-group/test/basic/e2e.ts
+++ core/src/components/reorder-group/test/basic/e2e.ts
@@ -2,7 +2,7 @@ import { newE2EPage } from '@stencil/core/testing';
 
 it('reorder: basic', async () => {
   const page = await newE2EPage({
-    url: '/src/components/reorder/test/basic?ionic:_testing=true'
+    url: '/src/components/reorder-group/test/basic?ionic:_testing=true'
   });
 
   const compare = await page.compareScreenshot();
--- core/src/components/reorder-group/test/standalone/e2e.ts
+++ core/src/components/reorder-group/test/standalone/e2e.ts
@@ -2,7 +2,7 @@ import { newE2EPage } from '@stencil/core/testing';
 
 it('reorder: standalone', async () => {
   const page = await newE2EPage({
-    url: '/src/components/reorder/test/standalone?ionic:_testing=true'
+    url: '/src/components/reorder-group/test/standalone?ionic:_testing=true'
   });
 
   const compare = await page.compareScreenshot();
