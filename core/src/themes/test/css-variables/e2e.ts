3235f9e29878a18d1ec2a8f8eac9ba5790f71762
Brandy Carney <brandy@ionic.io>
2018-10-18 16:48:45 -0400

    fix(css): rename/remove global css variables
    ## Removed Global CSS Variables
    
    The following global CSS variables have been removed for the reasons
    listed.
    
    | Variable Name                     | Reason
                  |
    | ----------------------------------|
    ------------------------------------------------|
    | `--ion-toolbar-color-inactive`    | Unused
                  |
    
    ## Renamed Global CSS Variables
    
    The following global CSS variables have been renamed for the reasons
    listed.
    
    | Old Variable Name                        | New Variable Name
            | Reason
                    |
    | -----------------------------------------|
    -----------------------------------|
    ------------------------------------------------------------------------
    ------|
    | `--ion-toolbar-text-color`               | `--ion-toolbar-color`
            | Variable is not limited to text color
                    |
    | `--ion-toolbar-color-active`             |
    `--ion-toolbar-color-activated`    | Consistency with our component
    variables                                      |
    | `--ion-tabbar-text-color`                | `--ion-tabbar-color`
            | Variable is not limited to text color
                    |
    | `--ion-tabbar-text-color-active`         |
    `--ion-tabbar-color-activated`     | Consistency with our component
    variables                                      |
    | `--ion-tabbar-background-color`          | `--ion-tabbar-background`
            | Applies to the background property
                    |
    | `--ion-tabbar-background-color-focused`  |
    `--ion-tabbar-background-focused`  | Applies to the background property
                                               |
    | `--ion-item-background-color`            | `--ion-item-background`
            | Applies to the background property
                    |
    | `--ion-item-background-color-active`     |
    `--ion-item-background-activated`  | Applies to the background property
    / Consistency with our component variables |
    | `--ion-item-text-color`                  | `--ion-item-color`
            | Variable is not limited to text color
                    |

--- core/src/themes/test/css-variables/e2e.ts
+++ core/src/themes/test/css-variables/e2e.ts
@@ -1,6 +1,6 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-it('badge: standalone', async () => {
+it('themes: css-variables', async () => {
   const page = await newE2EPage({
     url: '/src/themes/test/css-variables?ionic:_testing=true'
   });
