96b8a33ee857dbed879b6b56ad62d738e4e65016
Adam Bradley <adambradley25@gmail.com>
2018-10-19 10:12:34 -0500

    mor logz

--- core/src/components/select/test/single-value/e2e.ts
+++ core/src/components/select/test/single-value/e2e.ts
@@ -5,15 +5,25 @@ it('select: single-value', async () => {
     url: '/src/components/select/test/single-value?ionic:_testing=true'
   });
 
+  console.log(1)
+
   let compare = await page.compareScreenshot();
   expect(compare).toMatchScreenshot();
+
+  console.log(2)
 
   const select = await page.find('#gender');
   await select.click();
 
+  console.log(3)
+
   const alert = await page.find('ion-alert');
   await alert.waitForVisible();
 
+  console.log(4)
+
   compare = await page.compareScreenshot('should open gender single select');
   expect(compare).toMatchScreenshot();
+
+  console.log(5)
 });
