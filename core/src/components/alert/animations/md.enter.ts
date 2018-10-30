c036b2d9a0c21475efe4f8acc167cc41a8775418
Cam Wiegert <cam@camwiegert.com>
2018-10-30 11:19:18 -0500

    fix(alert): match MD spec
    This updates our alert to match the updated Material Design
    spec, including enter/leave animations.

--- core/src/components/alert/animations/md.enter.ts
+++ core/src/components/alert/animations/md.enter.ts
@@ -14,12 +14,12 @@ export function mdEnterAnimation(AnimationC: Animation, baseEl: HTMLElement): Pr
 
   backdropAnimation.fromTo('opacity', 0.01, 0.5);
 
-  wrapperAnimation.fromTo('opacity', 0.01, 1).fromTo('scale', 1.1, 1);
+  wrapperAnimation.fromTo('opacity', 0.01, 1).fromTo('scale', 0.9, 1);
 
   return Promise.resolve(baseAnimation
     .addElement(baseEl)
     .easing('ease-in-out')
-    .duration(200)
+    .duration(150)
     .add(backdropAnimation)
     .add(wrapperAnimation));
 }
