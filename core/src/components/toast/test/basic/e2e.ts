cee89a3a12a09e64e60ae180114c4827f9acfad5
Brandy Carney <brandy@ionic.io>
2018-11-14 11:04:02 -0500

    test(toast): update e2e test to include other positions

--- core/src/components/toast/test/basic/e2e.ts
+++ core/src/components/toast/test/basic/e2e.ts
@@ -5,21 +5,64 @@ test('toast: basic', async () => {
     url: '/src/components/toast/test/basic?ionic:_testing=true'
   });
 
-  const button = await page.find('#showBottomToast');
+  // Show bottom toast
+  let button = await page.find('#showBottomToast');
   await button.click();
 
   let toast = await page.find('ion-toast');
   await toast.waitForVisible();
   await page.waitFor(250);
 
-  let compare = await page.compareScreenshot();
+  let compare = await page.compareScreenshot(`bottom toast`);
   expect(compare).toMatchScreenshot();
 
   await toast.callMethod('dismiss');
   await toast.waitForNotVisible();
   await page.waitFor(250);
 
-  compare = await page.compareScreenshot('dismissed');
+  compare = await page.compareScreenshot('dismissed bottom toast');
+  expect(compare).toMatchScreenshot();
+
+  toast = await page.find('ion-toast');
+  expect(toast).toBeNull();
+
+  // Show middle toast
+  button = await page.find('#showMiddleToast');
+  await button.click();
+
+  toast = await page.find('ion-toast');
+  await toast.waitForVisible();
+  await page.waitFor(250);
+
+  compare = await page.compareScreenshot(`middle toast`);
+  expect(compare).toMatchScreenshot();
+
+  await toast.callMethod('dismiss');
+  await toast.waitForNotVisible();
+  await page.waitFor(250);
+
+  compare = await page.compareScreenshot('dismissed middle toast');
+  expect(compare).toMatchScreenshot();
+
+  toast = await page.find('ion-toast');
+  expect(toast).toBeNull();
+
+  // Show top toast
+  button = await page.find('#showTopToast');
+  await button.click();
+
+  toast = await page.find('ion-toast');
+  await toast.waitForVisible();
+  await page.waitFor(250);
+
+  compare = await page.compareScreenshot(`top toast`);
+  expect(compare).toMatchScreenshot();
+
+  await toast.callMethod('dismiss');
+  await toast.waitForNotVisible();
+  await page.waitFor(250);
+
+  compare = await page.compareScreenshot('dismissed top toast');
   expect(compare).toMatchScreenshot();
 
   toast = await page.find('ion-toast');
