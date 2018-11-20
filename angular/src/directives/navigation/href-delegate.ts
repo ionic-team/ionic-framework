6e48f543b1d0392fd1d14a6a7e2aeedb4a47aaf2
Levy <levy@codeandmore.pl>
2018-11-21 00:25:22 +0100

    fix angular navigation when routerLink used

--- angular/src/directives/navigation/href-delegate.ts
+++ angular/src/directives/navigation/href-delegate.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, HostListener, Input, Optional } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterLinkWithHref } from '@angular/router';
 import { NavController, NavIntent } from '../../providers/nav-controller';
 
 export type RouterDirection =  'forward' | 'back' | 'root' | 'auto';
@@ -12,11 +12,6 @@ export class HrefDelegate {
   @Input() routerDirection: RouterDirection = 'forward';
 
   @Input()
-  set routerLink(_: any) {
-    this.elementRef.nativeElement.button = true;
-  }
-
-  @Input()
   set href(value: string) {
     this.elementRef.nativeElement.href = value;
   }
@@ -26,17 +21,36 @@ export class HrefDelegate {
 
   constructor(
     @Optional() private router: Router,
+    @Optional() private routerLink: RouterLink,
+    @Optional() private routerLinkWithHref: RouterLinkWithHref,
     private navCtrl: NavController,
     private elementRef: ElementRef
-  ) {}
+  ) {
 
-  @HostListener('click', ['$event'])
-  onClick(ev: Event) {
-    const url = this.href;
-    if (this.router && url != null && url[0] !== '#' && url.indexOf('://') === -1) {
+    if (routerLink || routerLinkWithHref) {
+      this.elementRef.nativeElement.button = true;
+    }
+  }
+
+  @HostListener('click', ['$event', '$event.button', '$event.ctrlKey', '$event.metaKey', '$event.shiftKey'])
+  onClick(ev: Event, button: number, ctrlKey: boolean, metaKey: boolean, shiftKey: boolean) {
+    
+    let url: string | null = this.href;
+    if (!this.router || (url && (url[0] === '#' || url.indexOf('://') > -1))) {
+      url = null;
+    }
+    
+    if (this.routerLink || this.routerLinkWithHref || url) {
       ev.preventDefault();
       this.navCtrl.setIntent(textToIntent(this.routerDirection));
-      this.router.navigateByUrl(url);
+
+      if (this.routerLink) {
+        this.routerLink.onClick();
+      } else if (this.routerLinkWithHref) {
+        this.routerLinkWithHref.onClick(button, ctrlKey, metaKey, shiftKey);
+      } else if (url != null) {
+        this.router.navigateByUrl(url);
+      }
     }
   }
 }
