80508e8f4f93ae5693ed2711525a08221e18d057
Brandy Carney <brandy@ionic.io>
2018-11-20 17:16:43 -0500

    fix(tab-bar): override bakground and color directly
    references #14853

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -4052,7 +4052,7 @@ export namespace Components {
     /**
     * Opens the select overlay, it could be an alert, action-sheet or popover, based in `ion-select` settings.
     */
-    'open': (ev?: UIEvent | undefined) => Promise<HTMLIonActionSheetElement | HTMLIonAlertElement | HTMLIonPopoverElement | undefined>;
+    'open': (ev?: UIEvent | undefined) => Promise<HTMLIonAlertElement | HTMLIonPopoverElement | HTMLIonActionSheetElement | undefined>;
     /**
     * The text to display when the select is empty.
     */
