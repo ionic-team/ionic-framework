a67f1fe3b979222944d432ac26871e7a533025d5
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-12-07 20:13:07 +0100

    update tests

--- angular/test/test-app/src/app/tabs-tab2/tabs-tab2.component.ts
+++ angular/test/test-app/src/app/tabs-tab2/tabs-tab2.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs-tab2',
   templateUrl: './tabs-tab2.component.html',
-  styleUrls: ['./tabs-tab2.component.css']
 })
 export class TabsTab2Component { }
