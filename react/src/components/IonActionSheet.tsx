99bdd1f016870edae2de490964be4907ab94aff6
Josh Thomas <jthoms1@gmail.com>
2018-12-20 01:39:40 -0600

    feat(react): create initial portal implementation for overlay ctrls (#16830)

--- react/src/components/IonActionSheet.tsx
+++ react/src/components/IonActionSheet.tsx
@@ -1,8 +1,8 @@
 import { Components } from '@ionic/core';
-import { createControllerComponent } from './createControllerComponent';
+import { createOverlayComponent } from './createOverlayComponent';
 import { Omit } from './types';
 
 export type ActionSheetOptions = Omit<Components.IonActionSheetAttributes, 'overlayIndex'>;
 
-const IonActionSheet = createControllerComponent<ActionSheetOptions, HTMLIonActionSheetElement, HTMLIonActionSheetControllerElement>('ion-action-sheet', 'ion-action-sheet-controller')
+const IonActionSheet = createOverlayComponent<ActionSheetOptions, HTMLIonActionSheetElement, HTMLIonActionSheetControllerElement>('ion-action-sheet', 'ion-action-sheet-controller')
 export default IonActionSheet;
