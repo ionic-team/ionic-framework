a3c94b7c0164a44587ff26d7b69b18d747e1ab24
kplhub <kplhub@users.noreply.github.com>
2018-12-31 17:24:44 +0100

    feat(radio-group): add missing implementation for property allowEmptySelection (#16880)
    fixes #16841

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -3285,6 +3285,10 @@ export namespace Components {
     */
     'onIonBlur'?: (event: CustomEvent<void>) => void;
     /**
+    * Emitted when checked radio button is selected.
+    */
+    'onIonDeselect'?: (event: CustomEvent<RadioChangeEventDetail>) => void;
+    /**
     * Emitted when the radio button has focus.
     */
     'onIonFocus'?: (event: CustomEvent<void>) => void;
