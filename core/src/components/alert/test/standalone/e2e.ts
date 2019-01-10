82457d874ba006a634e48703dcfcde423d1b1230
Brandy Carney <brandyscarney@users.noreply.github.com>
2019-01-10 18:13:31 -0500

    fix(action-sheet): remove the height shift on press and update iOS design (#16862)
    This changes the border into a linear gradient background image which allows us to avoid the height decrease on press and have the same border as the background color when pressed.
    
    - merges all action sheet tests to the basic directory (except standalone, translucent), references #16715
    - removes the height shift on press, fixes #16790
    - fixes the cancel button on iOS so it is `#ffffff` by default
    - gets the look of the action sheet closer to native for iOS
    - only applies the translucent and backdrop filter if it is supported by the browser (chrome will not show transparent anymore)
    - updates documentation to describe when translucent will show up
    - adds documentation on how to use screenshot tests

--- core/src/components/alert/test/standalone/e2e.ts
+++ core/src/components/alert/test/standalone/e2e.ts
@@ -1,32 +1,55 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-test('alert: standalone', async () => {
+async function openAlert(selector: string) {
   const page = await newE2EPage({
     url: '/src/components/alert/test/standalone?ionic:_testing=true'
   });
 
-  const alerts = [
-    ['#basic'],
-    ['#longMessage', 'long message'],
-    ['#multipleButtons', 'multiple buttons'],
-    ['#noMessage', 'no message'],
-    ['#confirm', 'confirm'],
-    ['#prompt', 'prompt'],
-    ['#radio', 'radio'],
-    ['#checkbox', 'checkbox']
-  ];
+  await page.click(selector);
 
-  for (const [buttonSelector, message] of alerts) {
-    await page.click(buttonSelector);
+  let alert = await page.find('ion-alert');
+  expect(alert).not.toBe(null);
+  await alert.waitForVisible();
+  await page.waitFor(250);
 
-    const alert = await page.find('ion-alert');
-    expect(alert).not.toBe(null);
-    await alert.waitForVisible();
-    await page.waitFor(250);
+  const compare = await page.compareScreenshot();
+  expect(compare).toMatchScreenshot();
 
-    const compare = await page.compareScreenshot(message);
-    expect(compare).toMatchScreenshot();
-    await alert.callMethod('dismiss');
-  }
+  await alert.callMethod('dismiss');
+  await alert.waitForNotVisible();
 
+  alert = await page.find('ion-alert');
+  expect(alert).toBe(null);
+}
+
+test(`alert: standalone`, async () => {
+  await openAlert('#basic');
+});
+
+test(`alert: standalone, long message`, async () => {
+  await openAlert('#longMessage');
+});
+
+test(`alert: standalone, multiple buttons`, async () => {
+  await openAlert('#multipleButtons');
+});
+
+test(`alert: standalone, no message`, async () => {
+  await openAlert('#noMessage');
+});
+
+test(`alert: standalone, confirm`, async () => {
+  await openAlert('#confirm');
+});
+
+test(`alert: standalone, prompt`, async () => {
+  await openAlert('#prompt');
+});
+
+test(`alert: standalone, radio`, async () => {
+  await openAlert('#radio');
+});
+
+test(`alert: standalone, checkbox`, async () => {
+  await openAlert('#checkbox');
 });
