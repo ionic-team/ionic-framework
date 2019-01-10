27a47091950c675ff03b491b9cdb5eec0d917fb0
Manu MA <manu.valladolid@gmail.com>
2019-01-10 12:59:29 +0100

    fix(angular): ViewChild() fix (#17037)
    fixes #17034

--- angular/test/test-app/src/app/app.module.ts
+++ angular/test/test-app/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { NestedOutletComponent } from './nested-outlet/nested-outlet.component';
 import { NestedOutletPageComponent } from './nested-outlet-page/nested-outlet-page.component';
 import { NestedOutletPage2Component } from './nested-outlet-page2/nested-outlet-page2.component';
 import { NavComponent } from './nav/nav.component';
+import { ViewChildComponent } from './view-child/view-child.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { NavComponent } from './nav/nav.component';
     NestedOutletComponent,
     NestedOutletPageComponent,
     NestedOutletPage2Component,
-    NavComponent
+    NavComponent,
+    ViewChildComponent
   ],
   imports: [
     BrowserModule,
