d5279f0d4e14551c38eb3eb3c9bddd40fbf2352b
Brandy Carney <brandy@ionic.io>
2019-01-16 12:22:13 -0500

    test(alert): separate out the rtl tests

--- core/src/components/alert/test/basic/e2e.ts
+++ core/src/components/alert/test/basic/e2e.ts
@@ -1,8 +1,11 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-async function openAlert(selector: string) {
+async function openAlert(selector: string, rtl = false) {
+  const pageUrl = rtl
+   ? '/src/components/alert/test/basic?ionic:_testing=true&rtl=true'
+   : '/src/components/alert/test/basic?ionic:_testing=true';
   const page = await newE2EPage({
-    url: '/src/components/alert/test/basic?ionic:_testing=true'
+    url: pageUrl
   });
 
   await page.click(selector);
@@ -12,29 +15,9 @@ async function openAlert(selector: string) {
   await alert.waitForVisible();
   await page.waitFor(250);
 
-  const compare = await page.compareScreenshot();
-  expect(compare).toMatchScreenshot();
-
-  await alert.callMethod('dismiss');
-  await alert.waitForNotVisible();
-
-  alert = await page.find('ion-alert');
-  expect(alert).toBe(null);
-}
-
-async function openRTLAlert(selector: string) {
-  const page = await newE2EPage({
-    url: '/src/components/alert/test/basic?ionic:_testing=true&rtl=true'
-  });
-
-  await page.click(selector);
-
-  let alert = await page.find('ion-alert');
-  expect(alert).not.toBe(null);
-  await alert.waitForVisible();
-  await page.waitFor(250);
-
-  const compare = await page.compareScreenshot('rtl');
+  const compare = rtl
+    ? await page.compareScreenshot('rtl')
+    : await page.compareScreenshot();
   expect(compare).toMatchScreenshot();
 
   await alert.callMethod('dismiss');
@@ -46,40 +29,67 @@ async function openRTLAlert(selector: string) {
 
 test(`alert: basic`, async () => {
   await openAlert('#basic');
-  await openRTLAlert('#basic');
 });
 
 test(`alert: basic, long message`, async () => {
   await openAlert('#longMessage');
-  await openRTLAlert('#longMessage');
 });
 
 test(`alert: basic, multiple buttons`, async () => {
   await openAlert('#multipleButtons');
-  await openRTLAlert('#multipleButtons');
 });
 
 test(`alert: basic, no message`, async () => {
   await openAlert('#noMessage');
-  await openRTLAlert('#noMessage');
 });
 
 test(`alert: basic, confirm`, async () => {
   await openAlert('#confirm');
-  await openRTLAlert('#confirm');
 });
 
 test(`alert: basic, prompt`, async () => {
   await openAlert('#prompt');
-  await openRTLAlert('#prompt');
 });
 
 test(`alert: basic, radio`, async () => {
   await openAlert('#radio');
-  await openRTLAlert('#radio');
 });
 
 test(`alert: basic, checkbox`, async () => {
   await openAlert('#checkbox');
-  await openRTLAlert('#checkbox');
+});
+
+// Right to Left tests
+// ------------------------------------------------------
+
+test(`alert: basic`, async () => {
+  await openAlert('#basic', true);
+});
+
+test(`alert: basic, long message`, async () => {
+  await openAlert('#longMessage', true);
+});
+
+test(`alert: basic, multiple buttons`, async () => {
+  await openAlert('#multipleButtons', true);
+});
+
+test(`alert: basic, no message`, async () => {
+  await openAlert('#noMessage', true);
+});
+
+test(`alert: basic, confirm`, async () => {
+  await openAlert('#confirm', true);
+});
+
+test(`alert: basic, prompt`, async () => {
+  await openAlert('#prompt', true);
+});
+
+test(`alert: basic, radio`, async () => {
+  await openAlert('#radio', true);
+});
+
+test(`alert: basic, checkbox`, async () => {
+  await openAlert('#checkbox', true);
 });
