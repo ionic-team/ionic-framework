263a7310e106a83043b3587a86c3104d96d38b15
Abdelaziz <abennouna@gmail.com>
2019-01-18 19:45:56 +0100

    feat(tab-bar): add RTL test

--- core/src/components/tab-bar/test/preview/e2e.ts
+++ core/src/components/tab-bar/test/preview/e2e.ts
@@ -5,6 +5,15 @@ test('tab-bar: preview', async () => {
     url: '/src/components/tab-bar/test/preview?ionic:_testing=true'
   });
 
-  const compare = await page.compareScreenshot();
+  const compare = await page.compareScreenshot('tab-bar: preview');
+  expect(compare).toMatchScreenshot();
+});
+
+test('tab-bar: preview-rtl', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/tab-bar/test/preview?ionic:_testing=true&rtl=true'
+  });
+
+  const compare = await page.compareScreenshot('tab-bar: preview-rtl');
   expect(compare).toMatchScreenshot();
 });
