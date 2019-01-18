a517e6ca86c54ae4c12a575fd1f87eddd295f263
Abdelaziz <abennouna@gmail.com>
2019-01-18 16:04:19 +0100

    fix(progress-bar): invert progress directions in normal / reverse

--- core/src/components/progress-bar/test/basic/e2e.ts
+++ core/src/components/progress-bar/test/basic/e2e.ts
@@ -5,6 +5,15 @@ test('progress-bar: basic', async () => {
     url: '/src/components/progress-bar/test/basic?ionic:_testing=true'
   });
 
-  const compare = await page.compareScreenshot();
+  const compare = await page.compareScreenshot('progress-bar: basic');
+  expect(compare).toMatchScreenshot();
+});
+
+test('progress-bar: basic-rtl', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/progress-bar/test/basic?ionic:_testing=true&rtl=true'
+  });
+
+  const compare = await page.compareScreenshot('progress-bar: basic-rtl');
   expect(compare).toMatchScreenshot();
 });
