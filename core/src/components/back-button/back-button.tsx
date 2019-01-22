ce5bfa895c6b7c13fb4dd03147dd2e156e3a8061
Brandy Carney <brandyscarney@users.noreply.github.com>
2019-01-22 13:37:39 -0500

    fix(icon): update ionicons to flip for rtl (#17196)
    - updates to latest ionicons 4.55 and adds rtl test
    - includes e2e test for RTL icons
    
    references #17012

--- core/src/components/back-button/back-button.tsx
+++ core/src/components/back-button/back-button.tsx
@@ -72,12 +72,8 @@ export class BackButton implements ComponentInterface {
 
   render() {
     const defaultBackButtonText = this.mode === 'ios' ? 'Back' : null;
+    const backButtonIcon = this.icon != null ? this.icon : this.config.get('backButtonIcon', 'arrow-back');
     const backButtonText = this.text != null ? this.text : this.config.get('backButtonText', defaultBackButtonText);
-
-    let backButtonIcon = this.icon != null ? this.icon : this.config.get('backButtonIcon', 'arrow-back');
-    if (backButtonIcon === 'arrow-back' && document.dir === 'rtl') {
-      backButtonIcon = 'arrow-forward';
-    }
 
     return (
       <button
