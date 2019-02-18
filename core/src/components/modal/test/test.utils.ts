5825fa3be77d8837b647c4944765e3134c2cc40a
Liam DeBeasi <liamdebeasi@icloud.com>
2019-02-18 15:19:16 -0500

    test(modal): DRY up tests, add RTL tests

---
+++ core/src/components/modal/test/test.utils.ts
@@ -0,0 +1,47 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+export function cleanScreenshotName(screenshotName: string): string {
+  return screenshotName
+    .replace(/([-])/g, ' ')
+    .replace(/[^0-9a-zA-Z\s]/gi, '')
+    .toLowerCase();
+}
+
+export async function testModal(type: string, selector: string, rtl = false, screenshotName: string = cleanScreenshotName(selector)) {
+  try {
+    let pageUrl = `/src/components/modal/test/${type}?ionic:_testing=true`;
+    if (rtl) {
+      pageUrl = `${pageUrl}&rtl=true`;
+      screenshotName = `${screenshotName} rtl`;
+    }
+
+    const page = await newE2EPage({
+      url: pageUrl
+    });
+
+    const screenShotCompares = [];
+
+    await page.click(selector);
+    let popover = await page.find('ion-modal');
+    await popover.waitForVisible();
+    await page.waitFor(250);
+
+    screenShotCompares.push(await page.compareScreenshot(screenshotName));
+
+    await popover.callMethod('dismiss');
+    await popover.waitForNotVisible();
+    await page.waitFor(250);
+
+    screenShotCompares.push(await page.compareScreenshot(`dismiss ${screenshotName}`));
+
+    popover = await page.find('ion-modal');
+    expect(popover).toBeNull();
+
+    for (const screenShotCompare of screenShotCompares) {
+      expect(screenShotCompare).toMatchScreenshot();
+    }
+
+  } catch (err) {
+    throw err;
+  }
+}
