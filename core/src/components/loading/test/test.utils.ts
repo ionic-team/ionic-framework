b4cd512b584f115cdc6f4a1938215ccd8a5d9b75
Liam DeBeasi <liamdebeasi@icloud.com>
2019-02-18 16:18:38 -0500

    test(loading): DRY up tests, add rtl tests, add utils file

---
+++ core/src/components/loading/test/test.utils.ts
@@ -0,0 +1,54 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+export function cleanScreenshotName(screenshotName: string): string {
+  return screenshotName
+    .replace(/([-])/g, ' ')
+    .replace(/[^0-9a-zA-Z\s]/gi, '')
+    .toLowerCase();
+}
+
+export async function testLoading(
+  type: string,
+  selector: string,
+  rtl: boolean = false,
+  screenshotName: string = cleanScreenshotName(selector)
+) {
+  try {
+    let pageUrl = `/src/components/loading/test/${type}?ionic:_testing=true`;
+    if (rtl) {
+      pageUrl = `${pageUrl}&rtl=true`;
+      screenshotName = `${screenshotName} rtl`;
+    }
+
+    const page = await newE2EPage({
+      url: pageUrl
+    });
+
+    const screenShotCompares = [];
+
+    await page.click(selector);
+    let loading = await page.find('ion-loading');
+    expect(loading).not.toBeNull();
+  
+    await loading.waitForVisible();
+    await page.waitFor(500);
+
+    screenShotCompares.push(await page.compareScreenshot(screenshotName));
+
+    await loading.callMethod('dismiss');
+    await loading.waitForNotVisible();
+    await page.waitFor(250);
+
+    screenShotCompares.push(await page.compareScreenshot(`dismiss ${screenshotName}`));
+
+    loading = await page.find('ion-loading');
+    expect(loading).toBeNull();
+
+    for (const screenShotCompare of screenShotCompares) {
+      expect(screenShotCompare).toMatchScreenshot();
+    }
+
+  } catch (err) {
+    throw err;
+  }
+}
