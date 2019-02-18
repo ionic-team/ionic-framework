52b0d831fae3bdda8159f9975e552187ea56eab4
Liam DeBeasi <liamdebeasi@icloud.com>
2019-02-18 13:15:31 -0500

    test(toast): DRY up tests, add expanded tests, add rtl tests

--- core/src/components/toast/test/basic/e2e.ts
+++ core/src/components/toast/test/basic/e2e.ts
@@ -1,70 +1,133 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-test('toast: basic', async () => {
-  const page = await newE2EPage({
-    url: '/src/components/toast/test/basic?ionic:_testing=true'
-  });
+async function testToast(selector: string, screenshotName: string, dismissedScreenshotName: string, rtl = false) {
+  try {
+    let pageUrl = '/src/components/toast/test/basic?ionic:_testing=true';
+    if (rtl) { pageUrl = `${pageUrl}&rtl=true`; }
 
-  // Show bottom toast
-  let button = await page.find('#showBottomToast');
-  await button.click();
+    const page = await newE2EPage({
+      url: pageUrl
+    });
 
-  let toast = await page.find('ion-toast');
-  await toast.waitForVisible();
-  await page.waitFor(250);
+    const screenShotCompares = [];
 
-  let compare = await page.compareScreenshot(`bottom toast`);
-  expect(compare).toMatchScreenshot();
+    const button = await page.find(selector);
+    await button.click();
 
-  await toast.callMethod('dismiss');
-  await toast.waitForNotVisible();
-  await page.waitFor(250);
+    let toast = await page.find('ion-toast');
+    await toast.waitForVisible();
+    await page.waitFor(250);
 
-  compare = await page.compareScreenshot('dismissed bottom toast');
-  expect(compare).toMatchScreenshot();
+    screenShotCompares.push(await page.compareScreenshot(screenshotName));
 
-  toast = await page.find('ion-toast');
-  expect(toast).toBeNull();
+    await toast.callMethod('dismiss');
+    await toast.waitForNotVisible();
+    await page.waitFor(250);
 
-  // Show middle toast
-  button = await page.find('#showMiddleToast');
-  await button.click();
+    screenShotCompares.push(await page.compareScreenshot(dismissedScreenshotName));
 
-  toast = await page.find('ion-toast');
-  await toast.waitForVisible();
-  await page.waitFor(250);
+    toast = await page.find('ion-toast');
+    expect(toast).toBeNull();
 
-  compare = await page.compareScreenshot(`middle toast`);
-  expect(compare).toMatchScreenshot();
+    for (const screenShotCompare of screenShotCompares) {
+      expect(screenShotCompare).toMatchScreenshot();
+    }
 
-  await toast.callMethod('dismiss');
-  await toast.waitForNotVisible();
-  await page.waitFor(250);
+  } catch (err) {
+    throw err;
+  }
+}
 
-  compare = await page.compareScreenshot('dismissed middle toast');
-  expect(compare).toMatchScreenshot();
+test('toast: bottom', async () => {
+  const screenshotName = 'bottom toast';
+  await testToast('#showBottomToast', screenshotName, `dismissed ${screenshotName}`);
+});
 
-  toast = await page.find('ion-toast');
-  expect(toast).toBeNull();
+test('toast: middle', async () => {
+  const screenshotName = 'middle toast';
+  await testToast('#showMiddleToast', screenshotName, `dismissed ${screenshotName}`);
+});
 
-  // Show top toast
-  button = await page.find('#showTopToast');
-  await button.click();
+test('toast: top', async () => {
+  const screenshotName = 'bottom toast';
+  await testToast('#showTopToast', screenshotName, `dismissed ${screenshotName}`);
+});
 
-  toast = await page.find('ion-toast');
-  await toast.waitForVisible();
-  await page.waitFor(250);
+test('toast: two lines', async () => {
+  const screenshotName = 'two line';
+  await testToast('#twoLineToast', screenshotName, `dismissed ${screenshotName}`);
+});
 
-  compare = await page.compareScreenshot(`top toast`);
-  expect(compare).toMatchScreenshot();
+test('toast: close button', async () => {
+  const screenshotName = 'close button';
+  await testToast('#closeButtonToast', screenshotName, `dismissed ${screenshotName}`);
+});
 
-  await toast.callMethod('dismiss');
-  await toast.waitForNotVisible();
-  await page.waitFor(250);
+test('toast: custom close text', async () => {
+  const screenshotName = 'custom close text';
+  await testToast('#customCloseTextToast', screenshotName, `dismissed ${screenshotName}`);
+});
 
-  compare = await page.compareScreenshot('dismissed top toast');
-  expect(compare).toMatchScreenshot();
+test('toast: translucent', async () => {
+  const screenshotName = 'translucent toast';
+  await testToast('#translucentToast', screenshotName, `dismissed ${screenshotName}`);
+});
 
-  toast = await page.find('ion-toast');
-  expect(toast).toBeNull();
+test('toast: color', async () => {
+  const screenshotName = 'color toast';
+  await testToast('#colorToast', screenshotName, `dismissed ${screenshotName}`);
+});
+
+test('toast: custom class', async () => {
+  const screenshotName = 'custom class toast';
+  await testToast('#customClassToast', screenshotName, `dismissed ${screenshotName}`);
+});
+
+/**
+ * RTL Tests
+ */
+
+test('toast:rtl: bottom', async () => {
+  const screenshotName = 'bottom toast rtl';
+  await testToast('#showBottomToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: middle', async () => {
+  const screenshotName = 'middle toast rtl';
+  await testToast('#showMiddleToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: top', async () => {
+  const screenshotName = 'top toast rtl';
+  await testToast('#showTopToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: two lines', async () => {
+  const screenshotName = 'two line';
+  await testToast('#twoLineToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: close button', async () => {
+  const screenshotName = 'close button';
+  await testToast('#closeButtonToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: custom close text', async () => {
+  const screenshotName = 'custom close text';
+  await testToast('#customCloseTextToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: translucent', async () => {
+  const screenshotName = 'translucent toast';
+  await testToast('#translucentToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: color', async () => {
+  const screenshotName = 'color toast';
+  await testToast('#colorToast', screenshotName, `dismissed ${screenshotName}`, true);
+});
+
+test('toast:rtl: custom class', async () => {
+  const screenshotName = 'custom class toast';
+  await testToast('#customClassToast', screenshotName, `dismissed ${screenshotName}`, true);
 });
