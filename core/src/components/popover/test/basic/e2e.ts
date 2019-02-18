5e76b4c38b6fbec6dfad1c41cd851974e745ca9e
Liam DeBeasi <liamdebeasi@icloud.com>
2019-02-18 14:46:45 -0500

    test(popover): add more screenshots, add rtl tests

--- core/src/components/popover/test/basic/e2e.ts
+++ core/src/components/popover/test/basic/e2e.ts
@@ -1,29 +1,91 @@
 import { newE2EPage } from '@stencil/core/testing';
 
+function cleanScreenshotName(screenshotName: string): string {
+  return screenshotName
+    .replace(/([-])/g, ' ')
+    .replace(/[^0-9a-zA-Z\s]/gi, '')
+    .toLowerCase();
+}
+
+async function testPopover(selector: string, rtl = false, screenshotName: string = cleanScreenshotName(selector)) {
+  try {
+    let pageUrl = '/src/components/popover/test/basic?ionic:_testing=true';
+    if (rtl) {
+      pageUrl = `${pageUrl}&rtl=true`;
+      screenshotName = `${screenshotName} rtl`;
+    }
+
+    const page = await newE2EPage({
+      url: pageUrl
+    });
+
+    const screenShotCompares = [];
+
+    await page.click(selector);
+    let popover = await page.find('ion-popover');
+    await popover.waitForVisible();
+    await page.waitFor(250);
+
+    screenShotCompares.push(await page.compareScreenshot(screenshotName));
+
+    await popover.callMethod('dismiss');
+    await popover.waitForNotVisible();
+    await page.waitFor(250);
+
+    screenShotCompares.push(await page.compareScreenshot(`dismiss ${screenshotName}`));
+
+    popover = await page.find('ion-popover');
+    expect(popover).toBeNull();
+
+    for (const screenShotCompare of screenShotCompares) {
+      expect(screenShotCompare).toMatchScreenshot();
+    }
+
+  } catch (err) {
+    throw err;
+  }
+}
+
 test('popover: basic', async () => {
-  const page = await newE2EPage({
-    url: '/src/components/popover/test/basic?ionic:_testing=true'
-  });
-
-  await page.click('.e2eShowPopover');
-  const popover = await page.find('ion-popover');
-  await popover.waitForVisible();
-  await page.waitFor(250);
-
-  const compare = await page.compareScreenshot();
-  expect(compare).toMatchScreenshot();
+  await testPopover('#basic-popover');
 });
 
-test('popover: basic-rtl', async () => {
-  const page = await newE2EPage({
-    url: '/src/components/popover/test/basic?ionic:_testing=true&rtl=true'
-  });
+test('popover: translucent', async () => {
+  await testPopover('#translucent-popover');
+});
 
-  await page.click('.e2eShowPopover');
-  const popover = await page.find('ion-popover');
-  await popover.waitForVisible();
-  await page.waitFor(250);
+test('popover: long list', async () => {
+  await testPopover('#long-list-popover');
+});
 
-  const compare = await page.compareScreenshot();
-  expect(compare).toMatchScreenshot();
+test('popover: no event', async () => {
+  await testPopover('#no-event-popover');
+});
+
+test('popover: custom class', async () => {
+  await testPopover('#custom-class-popover');
+});
+
+/**
+ * RTL Tests
+ */
+
+test('popover:rtl: basic', async () => {
+  await testPopover('#basic-popover', true);
+});
+
+test('popover:rtl: translucent', async () => {
+  await testPopover('#translucent-popover', true);
+});
+
+test('popover:rtl: long list', async () => {
+  await testPopover('#long-list-popover', true);
+});
+
+test('popover:rtl: no event', async () => {
+  await testPopover('#no-event-popover', true);
+});
+
+test('popover:rtl: custom class', async () => {
+  await testPopover('#custom-class-popover', true);
 });
