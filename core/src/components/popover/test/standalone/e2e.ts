f07ea4f7dbe4b64c92b02a6faa3b966c3705ca90
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2019-03-01 10:41:55 -0500

    test(): Take more screenshots, DRY up code, add more tests (#17534)
    * test(toast): DRY up tests, add expanded tests, add rtl tests
    
    * test(popover): add more screenshots, add rtl tests
    
    * clean up tests a bit more
    
    * test(modal): DRY up tests, add RTL tests
    
    * add missing type to util function
    
    * add utils file, update standlone test
    
    * update standalone tests, add utils file
    
    * test(menu): DRY up tests, add rtl tests
    
    * test(loading): DRY up tests, add rtl tests, add utils file
    
    * lint files
    
    * test(alert): add more screenshots, dry up tests
    
    * fix potential race condition
    
    * remove artificial delay
    
    * lint files
    
    * lint files
    
    * remove artificial delays
    
    * remove artificial delay
    
    * add initial test updates, need to figure out alert from action sheet
    
    * test(): dry up tests some more, add generic utils file
    
    * lint files
    
    * add hook
    
    * fix typescript error

--- core/src/components/popover/test/standalone/e2e.ts
+++ core/src/components/popover/test/standalone/e2e.ts
@@ -1,15 +1,7 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { testPopover } from '../test.utils';
+
+const DIRECTORY = 'standalone';
 
 test('popover: standalone', async () => {
-  const page = await newE2EPage({
-    url: '/src/components/popover/test/standalone?ionic:_testing=true'
-  });
-
-  await page.click('#basic');
-  const alert = await page.find('ion-popover');
-  await alert.waitForVisible();
-  await page.waitFor(250);
-
-  const compare = await page.compareScreenshot();
-  expect(compare).toMatchScreenshot();
+  await testPopover(DIRECTORY, '#basic-popover');
 });
