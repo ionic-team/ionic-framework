6498999e209a91381aabda52710a311a29126ec7
Brandy Carney <brandy@ionic.io>
2019-03-11 13:09:27 -0400

    test(item-sliding): update standalone test to take more SS

--- core/src/components/item-sliding/test/standalone/e2e.ts
+++ core/src/components/item-sliding/test/standalone/e2e.ts
@@ -5,6 +5,39 @@ test('item-sliding: standalone', async () => {
     url: '/src/components/item-sliding/test/standalone?ionic:_testing=true'
   });
 
-  const compare = await page.compareScreenshot();
-  expect(compare).toMatchScreenshot();
+  const compares = [];
+
+  compares.push(await page.compareScreenshot());
+
+  // Grab item with start icons in option
+  let item = await page.find('#start');
+  await openItem(item, page);
+
+  compares.push(await page.compareScreenshot());
+
+  // Grab item with top icons in option
+  item = await page.find('#top');
+  await openItem(item, page);
+
+  compares.push(await page.compareScreenshot());
+
+  for (const compare of compares) {
+    expect(compare).toMatchScreenshot();
+  }
 });
+
+async function openItem(item: any, page: any) {
+  try {
+    // Simulate a drag
+    const boundingBox = await item.boundingBox();
+    const centerX = parseFloat(boundingBox.x + boundingBox.width / 2);
+    const centerY = parseFloat(boundingBox.y + boundingBox.height / 2);
+
+    await page.mouse.move(centerX, centerY);
+    await page.mouse.down();
+    await page.mouse.move(0, centerY);
+    await page.mouse.up();
+  } catch (err) {
+    throw err;
+  }
+}
