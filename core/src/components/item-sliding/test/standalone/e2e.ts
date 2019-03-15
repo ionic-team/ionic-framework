b3010cd2adbe14b177cdbca51e8e1256cf75d935
Brandy Carney <brandy@ionic.io>
2019-03-15 10:32:02 -0400

    test(item-sliding): update standalone

--- core/src/components/item-sliding/test/standalone/e2e.ts
+++ core/src/components/item-sliding/test/standalone/e2e.ts
@@ -9,33 +9,29 @@ test('item-sliding: standalone', async () => {
 
   compares.push(await page.compareScreenshot());
 
-  // Grab item with start icons in option
-  let item = await page.find('#start');
-  await openItem(item, page);
+  // Pass item with start icons in option
+  await openItem('#start', page);
+  compares.push(await page.compareScreenshot(`start icons open`));
 
-  compares.push(await page.compareScreenshot());
+  // Pass item with top icons in option
+  await openItem('#top', page);
+  compares.push(await page.compareScreenshot(`top icons open`));
 
-  // Grab item with top icons in option
-  item = await page.find('#top');
-  await openItem(item, page);
-
-  compares.push(await page.compareScreenshot());
-
-  // Grab item with anchor option
-  item = await page.find('#anchor');
-  await openItem(item, page);
-
-  compares.push(await page.compareScreenshot());
+  // Pass item with anchor option
+  await openItem('#anchor', page);
+  compares.push(await page.compareScreenshot(`anchor option`));
 
   for (const compare of compares) {
     expect(compare).toMatchScreenshot();
   }
 });
 
-async function openItem(item: any, page: any) {
+async function openItem(id: string, page: any) {
   try {
+    const slidingItem = await page.$(id);
+
     // Simulate a drag
-    const boundingBox = await item.boundingBox();
+    const boundingBox = await slidingItem.boundingBox();
     const centerX = parseFloat(boundingBox.x + boundingBox.width / 2);
     const centerY = parseFloat(boundingBox.y + boundingBox.height / 2);
 
@@ -43,6 +39,10 @@ async function openItem(item: any, page: any) {
     await page.mouse.down();
     await page.mouse.move(0, centerY);
     await page.mouse.up();
+
+    const slidingEl = await page.find(id);
+    const open = await slidingEl.callMethod('getOpenAmount');
+    console.log('open amount is', open);
   } catch (err) {
     throw err;
   }
