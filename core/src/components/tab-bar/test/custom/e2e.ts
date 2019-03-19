da305e010dbfa80f6b5c55f54b5d96bc435c1b36
Liam DeBeasi <liamdebeasi@icloud.com>
2019-03-19 11:50:05 -0400

    change timeout to waitForVisible

--- core/src/components/tab-bar/test/custom/e2e.ts
+++ core/src/components/tab-bar/test/custom/e2e.ts
@@ -7,13 +7,15 @@ test('tab-bar: custom', async () => {
 
   const screenshotCompares = [];
 
+  const tabBar = await page.find('ion-tab-bar');
+  await tabBar.waitForVisible();
+
   screenshotCompares.push(await page.compareScreenshot('tab-bar: custom default'));
 
-  await page.waitFor(150);
   await page.keyboard.press('Tab');
 
   screenshotCompares.push(await page.compareScreenshot('tab-bar: custom tabbed'));
-
+  await page.waitFor(10000);
   for (const screenshotCompare of screenshotCompares) {
     expect(screenshotCompare).toMatchScreenshot();
   }
