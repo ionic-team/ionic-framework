aee122000fc427bf6d22b205b48975467fa4fd64
Brandy Carney <brandy@ionic.io>
2019-04-10 15:46:37 -0400

    style(toast): remove destructive role from buttons

--- core/src/components/toast/toast-interface.ts
+++ core/src/components/toast/toast-interface.ts
@@ -24,7 +24,7 @@ export interface ToastButton {
   text?: string;
   icon?: string;
   side?: 'start' | 'end';
-  role?: 'cancel' | 'destructive' | string;
+  role?: 'cancel' | string;
   cssClass?: string | string[];
   handler?: () => boolean | void | Promise<boolean>;
 }
