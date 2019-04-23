55880a741b78aa0ab96baa5594f3fd49a0f4b622
Liam DeBeasi <liamdebeasi@icloud.com>
2019-04-23 13:08:35 -0400

    wrap sanitize in try catch, add case for numbers

--- core/src/utils/sanitization/index.ts
+++ core/src/utils/sanitization/index.ts
@@ -4,31 +4,35 @@
  */
 
 export const sanitizeDOMString = (untrustedString: string | undefined): string | undefined => {
-  if (untrustedString === undefined) { return untrustedString; }
+  try {
+    if (typeof untrustedString !== 'string') { return untrustedString; }
 
-  const whitelistedAttributes = ['class', 'id', 'href', 'src'];
-  const range = document.createRange();
-  const documentFragment = range.createContextualFragment(untrustedString);
+    const whitelistedAttributes = ['class', 'id', 'href', 'src'];
+    const range = document.createRange();
+    const documentFragment = range.createContextualFragment(untrustedString);
 
-  for (const childEl of (documentFragment.children as any)) {
-    for (const attributeName of childEl.getAttributeNames()) {
+    for (const childEl of (documentFragment.children as any)) {
+      for (const attributeName of childEl.getAttributeNames()) {
 
-      // remove non-whitelisted attribs
-      if (!whitelistedAttributes.includes(attributeName.toLowerCase())) {
-        childEl.removeAttribute(attributeName);
-        continue;
-      }
+        // remove non-whitelisted attribs
+        if (!whitelistedAttributes.includes(attributeName.toLowerCase())) {
+          childEl.removeAttribute(attributeName);
+          continue;
+        }
 
-      // clean up any whitelisted attribs
-      // that attempt to do any JS funny-business
-      const attributeValue = childEl.getAttribute(attributeName);
+        // clean up any whitelisted attribs
+        // that attempt to do any JS funny-business
+        const attributeValue = childEl.getAttribute(attributeName);
 
-      /* tslint:disable-next-line */
-      if (attributeValue != null && attributeValue.toLowerCase().includes('javascript:')) {
-        childEl.removeAttribute(attributeName);
+        /* tslint:disable-next-line */
+        if (attributeValue != null && attributeValue.toLowerCase().includes('javascript:')) {
+          childEl.removeAttribute(attributeName);
+        }
       }
     }
-  }
 
-  return new XMLSerializer().serializeToString(documentFragment);
+    return new XMLSerializer().serializeToString(documentFragment);
+  } catch (err) {
+    throw err;
+  }
 };
