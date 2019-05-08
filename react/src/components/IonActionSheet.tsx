7bb6a8e8b1fe51a5aa92f930df8916a79b5721f2
Ely Lucas <ely@ionic.io>
2019-05-08 14:53:33 -0600

    fix(react): Support for adding css classes via `className` in Ionic React components (#18231)
    * fix(react): adding classname to react props
    
    * fix(react): updating rtl to latest to fix ts error
    
    * fix(react): changes to support className

--- react/src/components/IonActionSheet.tsx
+++ react/src/components/IonActionSheet.tsx
@@ -1,7 +1,8 @@
 import { Components } from '@ionic/core';
 import { createOverlayComponent } from './createOverlayComponent';
+import { ReactProps } from './ReactProps';
 
 export type ActionSheetOptions = Components.IonActionSheetAttributes;
 
-const IonActionSheet = createOverlayComponent<ActionSheetOptions, HTMLIonActionSheetElement, HTMLIonActionSheetControllerElement>('ion-action-sheet', 'ion-action-sheet-controller')
+const IonActionSheet = createOverlayComponent<ActionSheetOptions & ReactProps, HTMLIonActionSheetElement, HTMLIonActionSheetControllerElement>('ion-action-sheet', 'ion-action-sheet-controller')
 export default IonActionSheet;
