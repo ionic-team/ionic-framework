55dda6e4c830142e3dc5fd21fbd346b17cf8155e
David Strauß <david.strauss@ventocom.at>
2019-05-22 17:44:50 +0200

    fix lint issues

--- core/src/components/item/item.tsx
+++ core/src/components/item/item.tsx
@@ -1,7 +1,7 @@
 import { Component, ComponentInterface, Element, Listen, Prop, State } from '@stencil/core';
+
 import { Color, CssClassMap, Mode, RouterDirection, StyleEventDetail } from '../../interface';
 import { createColorClasses, hostContext, openURL } from '../../utils/theme';
-
 
 /**
  * @slot - Content is placed between the named slots if provided without a slot.
