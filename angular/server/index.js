aa62afd72aae240130ae437030315be951e7503f
Adam Bradley <adambradley25@gmail.com>
2019-06-25 10:19:38 -0500

    wip

---
+++ angular/server/index.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { hydrateIonicComponents, IonicServerModule } from './ionic-server-module';
---
+++ angular/server/ionic-server-module.d.ts
@@ -0,0 +1,3 @@
+export declare class IonicServerModule {
+}
+export declare function hydrateIonicComponents(doc: any, appId: any): () => Promise<void>;
