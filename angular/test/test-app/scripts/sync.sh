00a955fcd67661b8bae719751ed6079ce06481ce
Adam Bradley <adambradley25@gmail.com>
2019-06-26 11:31:08 -0500

    @ionic/angular/server

--- angular/test/test-app/scripts/sync.sh
+++ angular/test/test-app/scripts/sync.sh
@@ -1,12 +1,16 @@
 # Copy angular dist
 rm -rf node_modules/@ionic/angular/dist
+rm -rf node_modules/@ionic/angular/server
 cp -a ../../dist node_modules/@ionic/angular/dist
+cp -a ../../server node_modules/@ionic/angular/server
 cp -a ../../package.json node_modules/@ionic/angular/package.json
 
 # Copy core dist
 rm -rf node_modules/@ionic/core/dist
+rm -rf node_modules/@ionic/core/hydrate
 rm -rf node_modules/@ionic/core/loader
 cp -a ../../../core/dist node_modules/@ionic/core/dist
+cp -a ../../../core/hydrate node_modules/@ionic/core/hydrate
 cp -a ../../../core/loader node_modules/@ionic/core/loader
 cp -a ../../../core/package.json node_modules/@ionic/core/package.json
 
--- angular/test/test-app/src/app/app.server.module.ts
+++ angular/test/test-app/src/app/app.server.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
-// import { IonicServerModule } from '@ionic/angular/server';
+import { IonicServerModule } from '@ionic/angular/server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,7 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
-    // IonicServerModule
+    IonicServerModule
   ],
   bootstrap: [AppComponent],
 })
