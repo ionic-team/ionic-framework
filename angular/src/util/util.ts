c5991ef6ef134488e11ce44f8fb53531d3b56696
Adam Bradley <adambradley25@gmail.com>
2019-06-27 12:58:05 -0500

    arrow fns

--- angular/src/util/util.ts
+++ angular/src/util/util.ts
@@ -1,16 +1,16 @@
 import { HTMLStencilElement } from '../types/interfaces';
 
-export function proxyMethod(ctrlName: string, doc: Document, methodName: string, ...args: any[]) {
+export const proxyMethod = (ctrlName: string, doc: Document, methodName: string, ...args: any[]) => {
   const controller = ensureElementInBody(ctrlName, doc);
   return controller.componentOnReady()
     .then(() => (controller as any)[methodName].apply(controller, args));
-}
+};
 
-export function ensureElementInBody(elementName: string, doc: Document) {
+export const ensureElementInBody = (elementName: string, doc: Document) => {
   let element = doc.querySelector(elementName);
   if (!element) {
     element = doc.createElement(elementName);
     doc.body.appendChild(element);
   }
   return element as HTMLStencilElement;
-}
+};
