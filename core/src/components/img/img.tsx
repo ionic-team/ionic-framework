2624feda5eeff4dcc004630288226f5e41bfa129
Rafael <coding@rafael.kr>
2019-07-03 18:22:29 +0200

    Update img.tsx

--- core/src/components/img/img.tsx
+++ core/src/components/img/img.tsx
@@ -100,10 +100,10 @@ export class Img implements ComponentInterface {
   }
 
   render() {
-    if(!this.loadSrc) {
-      return
+    if (typeof this.loadSrc !== 'string') {
+      return;
     }
-    
+
     return (
       <img
         src={this.loadSrc}
