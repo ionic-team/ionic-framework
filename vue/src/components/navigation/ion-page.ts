470615eb0537958a21ee4df9be5f6ad0677d39b9
Mike Hartington <mikehartington@gmail.com>
2019-07-08 20:26:39 -0400

    chore(): add page component

--- vue/src/components/navigation/ion-page.ts
+++ vue/src/components/navigation/ion-page.ts
@@ -4,6 +4,6 @@ export default {
   name: 'IonPage',
   functional: true,
   render(h: CreateElement, { children }: RenderContext) {
-    return h('div', { class: { 'ion-page':true } }, children);
+    return h('div', { class: { 'ion-page': true } }, children);
   }
 };
