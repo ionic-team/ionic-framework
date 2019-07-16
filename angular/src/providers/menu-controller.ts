092120dab32201df82ecaf3aab5337539be8fcb7
Ken Sodemann <ken@ionic.io>
2019-07-16 13:05:33 -0500

    Update angular/src/providers/menu-controller.ts
    Co-Authored-By: Manu MA <manu.valladolid@gmail.com>

--- angular/src/providers/menu-controller.ts
+++ angular/src/providers/menu-controller.ts
@@ -61,7 +61,8 @@ export class MenuController {
    * @return Returns the instance of the menu, which is useful for chaining.
    */
   swipeEnable(shouldEnable: boolean, menuId?: string): Promise<HTMLIonMenuElement> {
-    return proxyMethod(CTRL, this.doc, 'swipeGesture', shouldEnable, menuId);
+    console.warn('MenuController.swipeEnable is deprecated. Use MenuController.swipeGestute() instead');
+    return this.swipeGestute(shouldEnable, menuId);
   }
 
   /**
