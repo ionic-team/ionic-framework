a869ca0bba48e664af12e6645ae5434a60890693
Amy Marsh <19311766+amymarsh4work@users.noreply.github.com>
2019-07-24 17:35:50 -0400

    (fix): define top-level ARIA landmark regions to improve accessibility (#18672)
    references #18671

--- core/src/components/footer/footer.tsx
+++ core/src/components/footer/footer.tsx
@@ -27,6 +27,7 @@ export class Footer implements ComponentInterface {
   hostData() {
     const mode = getIonMode(this);
     return {
+      role: 'contentinfo',
       class: {
         [mode]: true,
 
