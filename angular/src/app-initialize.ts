230d1349aef8c2a1a017e77e2fa8515f131cfd76
Adam Bradley <adambradley25@gmail.com>
2019-07-30 16:16:22 -0500

    wip

--- angular/src/app-initialize.ts
+++ angular/src/app-initialize.ts
@@ -1,7 +1,6 @@
 import { NgZone } from '@angular/core';
 import { applyPolyfills, defineCustomElements } from '@ionic/core/loader';
 
-import './hack';
 import { Config } from './providers/config';
 import { IonicWindow } from './types/interfaces';
 import { raf } from './util/util';
