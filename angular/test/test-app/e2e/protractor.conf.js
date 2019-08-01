0da191a9bdcc6ffbedaaa5b5b1e61af72a603264
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-08-01 22:00:44 +0200

    run all tests again

--- angular/test/test-app/e2e/protractor.conf.js
+++ angular/test/test-app/e2e/protractor.conf.js
@@ -6,7 +6,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './**/navigation.e2e-spec.ts'
+    './**/*.e2e-spec.ts'
   ],
   capabilities: {
     browserName: 'chrome',
