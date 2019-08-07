fcd360ff91a4aa7b73b9038ed1399afee0e4237d
Ely Lucas <ely@meta-tek.net>
2019-08-07 17:24:07 -0600

    fix(): fixing react build due to move to packages

--- packages/react/scripts/copy.js
+++ packages/react/scripts/copy.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 
 function copyCSS() {
-  const src = path.join(__dirname, '..', '..', 'core', 'css');
+  const src = path.join(__dirname, '..', '..', '..', 'core', 'css');
   const dst = path.join(__dirname, '..', 'css');
 
   fs.removeSync(dst);
