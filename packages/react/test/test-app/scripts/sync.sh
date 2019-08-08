87326de02037ce0cd9b89211f6f0b394c44473c2
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-08-08 13:50:39 +0200

    chore(): add automated testing using cypress

--- packages/react/test/test-app/scripts/sync.sh
+++ packages/react/test/test-app/scripts/sync.sh
@@ -6,10 +6,10 @@ cp -a ../../package.json node_modules/@ionic/react/package.json
 # Copy core dist
 rm -rf node_modules/@ionic/core/dist
 rm -rf node_modules/@ionic/core/loader
-cp -a ../../../core/dist node_modules/@ionic/core/dist
-cp -a ../../../core/loader node_modules/@ionic/core/loader
-cp -a ../../../core/package.json node_modules/@ionic/core/package.json
+cp -a ../../../../core/dist node_modules/@ionic/core/dist
+cp -a ../../../../core/loader node_modules/@ionic/core/loader
+cp -a ../../../../core/package.json node_modules/@ionic/core/package.json
 
 # Copy ionicons
 rm -rf node_modules/ionicons
-cp -a ../../../core/node_modules/ionicons node_modules/ionicons
+cp -a ../../../../core/node_modules/ionicons node_modules/ionicons
