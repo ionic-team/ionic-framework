30ca46ab127b3cf5a0d05c54519be29dd6b4d4ab
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2019-08-12 10:05:04 -0400

    feat(animation): add animation utility (#18918)
    * Add new keyframes proof of concept
    
    * update esm import
    
    * add base before and after methods, add tests
    
    * add base before and after hooks
    
    * update clean up methods, add tests
    
    * add web animations support, change to arrow functions
    
    * remove console logs
    
    * add from, to, fromTo, and other properties
    
    * add more tests, fix onFinish functionality, being testing with nav transitions
    
    * add progress methods, use force linear
    
    * run linter
    
    * Add playSync
    
    * integrate animations with framework components
    
    * onFinish now supports multiple callbacks
    
    * change const to let
    
    * testing reverse
    
    * add support for both animation utilities
    
    * bug fix
    
    * export createAnimation, a few tweaks
    
    * add base tests
    
    * fix issue with onFinish being called out of order. added tests
    
    * fix race conditions in tests
    
    * clean up
    
    * fix bug where onFinish not calling for empty elements array,  update test
    
    * clean up
    
    * fix treeshaking, remove old comments
    
    * remove old tests
    
    * Add test for animationbuilder backwards compat
    
    * update typings for menu controller
    
    * mock web animations in tests
    
    * run build
    
    * fix type errors
    
    * sync with master
    
    * use requestAnimationFrame instead of writeTask
    
    * fix flaky tests, fix menu
    
    * fix ordering
    
    * update webdriver
    
    * fix wrong version
    
    * Revert "fix wrong version"
    
    This reverts commit be91296e9701399f8d784b08d09a3c475ca15df7.
    
    Revert chromedriver update
    
    * Revert "update webdriver"
    
    This reverts commit e49bc9d76e335a0af5828725065399bd6795fa37.
    
    Revert chromedriver update
    
    * expose raw animation object, add tests
    
    * add stylesheet recycling
    
    * finalize before and after hook tests
    
    * a few styling changes
    
    * fix lint warnings
    
    * get rid of old code
    
    * Fix progressStep overflow bug
    
    * disable reuse stylesheet
    
    * small updates
    
    * fix old animation create
    
    * setStyleProperty helper
    
    * reuse keyframe styles
    
    * keyframes
    
    * fix css animation issue with display: none, add tests
    
    * add comment
    
    * fix issue with progress animations and css animations
    
    * clean up
    
    * clean up pt2
    
    * fix tests
    
    * fix linter
    
    * add fill for overlays
    
    * fix swipe to go back
    
    * clean up css animations when done
    
    * fix edge cases with css animations
    
    * fix menu open and close
    
    * add reset function
    
    * clean up reset fn
    
    * Fix issue where animation always being reset
    
    * allow updating animations on the fly
    
    * add clear onfinish method
    
    * fix linter
    
    * add callback options, expand force direction
    
    * ensure opts is defined
    
    * fix css animations open and close for menus
    
    * remove test
    
    * add extra check
    
    * clean up
    
    * fix css anim bug swipe to go back
    
    * fix pause
    
    * setup alt animation to avoid flickering
    
    * clean up
    
    * reset flags on destroy
    
    * add ability to change duration on progressEnd
    
    * fix flicker on duration change for css animations
    
    * fix ios transition
    
    * remove unneeded recursion
    
    * increase durability of updating css animations on the fly
    
    * fix gesture anim
    
    * fix web anim as well. more work for cleanup
    
    * simplify progressEnd for css animations
    
    * fix swipe to go back race condition
    
    * clean up
    
    * Add todo
    
    * fix one more bug

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -25,12 +25,14 @@ import {
   HeaderFn,
   HeaderHeightFn,
   InputChangeEventDetail,
+  IonicAnimation,
   ItemHeightFn,
   ItemRenderFn,
   ItemReorderEventDetail,
   LoadingOptions,
   MenuChangeEventDetail,
   MenuControllerI,
+  MenuI,
   ModalOptions,
   NavComponent,
   NavOptions,
@@ -1425,7 +1427,7 @@ export namespace Components {
     * @param name The name of the animation to register.
     * @param animation The animation function to register.
     */
-    'registerAnimation': (name: string, animation: AnimationBuilder) => Promise<void>;
+    'registerAnimation': (name: string, animation: AnimationBuilder | ((menu: MenuI) => IonicAnimation)) => Promise<void>;
     /**
     * Enable or disable the ability to swipe open the menu.
     * @param enable If `true`, the menu swipe gesture should be enabled.
