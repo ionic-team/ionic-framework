caecabac6f770a00b560c69abfee41a43328d7e3
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-08-16 13:59:54 +0200

    menu angular

--- core/src/index.ts
+++ core/src/index.ts
@@ -7,3 +7,4 @@ export * from './utils/config';
 export * from './components/nav/constants';
 export { isPlatform, Platforms, getPlatforms } from './utils/platform';
 export { alertController, actionSheetController, modalController, loadingController, pickerController, popoverController, toastController } from './utils/overlays';
+export { menuController } from './utils/menu-controller';
