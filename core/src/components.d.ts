35fd2faeefa983232d37c38724a65ee29d9010cd
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2019-08-20 10:03:18 -0400

    refactor(toast): remove showCloseButton and closeButtonText (#18957)
    * Remove close button
    
    * update tests
    
    * update tests
    
    * add build

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -2707,10 +2707,6 @@ export namespace Components {
     */
     'buttons'?: (ToastButton | string)[];
     /**
-    * @deprecated Use `buttons` instead. Text to display in the close button.
-    */
-    'closeButtonText'?: string;
-    /**
     * The color to use from your application's color palette. Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`. For more information on colors, see [theming](/docs/theming/basics).
     */
     'color'?: Color;
@@ -2769,10 +2765,6 @@ export namespace Components {
     * Present the toast overlay after it has been created.
     */
     'present': () => Promise<void>;
-    /**
-    * @deprecated Use `buttons` instead. If `true`, the close button will be displayed.
-    */
-    'showCloseButton': boolean;
     /**
     * If `true`, the toast will be translucent. Only applies when the mode is `"ios"` and the device supports [`backdrop-filter`](https://developer.mozilla.org/en-US/docs/Web/CSS/backdrop-filter#Browser_compatibility).
     */
@@ -6023,10 +6015,6 @@ declare namespace LocalJSX {
     */
     'buttons'?: (ToastButton | string)[];
     /**
-    * @deprecated Use `buttons` instead. Text to display in the close button.
-    */
-    'closeButtonText'?: string;
-    /**
     * The color to use from your application's color palette. Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`. For more information on colors, see [theming](/docs/theming/basics).
     */
     'color'?: Color;
@@ -6082,10 +6070,6 @@ declare namespace LocalJSX {
     * The position of the toast on the screen.
     */
     'position'?: 'top' | 'bottom' | 'middle';
-    /**
-    * @deprecated Use `buttons` instead. If `true`, the close button will be displayed.
-    */
-    'showCloseButton'?: boolean;
     /**
     * If `true`, the toast will be translucent. Only applies when the mode is `"ios"` and the device supports [`backdrop-filter`](https://developer.mozilla.org/en-US/docs/Web/CSS/backdrop-filter#Browser_compatibility).
     */
