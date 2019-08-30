b9d3e45285011d5d4cb9a350cc46c3b2c7aa3c81
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2019-08-30 16:43:17 +0200

    fix test

--- core/src/components/virtual-scroll/test/basic/e2e.ts
+++ core/src/components/virtual-scroll/test/basic/e2e.ts
@@ -4,7 +4,6 @@ test('virtual-scroll: basic', async () => {
   const page = await newE2EPage({
     url: '/src/components/virtual-scroll/test/basic?ionic:_testing=true'
   });
-
   await page.waitFor(300);
 
   const compare = await page.compareScreenshot();
