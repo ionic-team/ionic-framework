86a12174f6f9d5ba3b53c97c79193e8e58a1c69d
Liam DeBeasi <liamdebeasi@icloud.com>
2019-09-05 12:47:57 -0400

    add e2e

---
+++ core/src/components/toolbar/test/title/e2e.ts
@@ -0,0 +1,10 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+test('toolbar: title', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/toolbar/test/title?ionic:_testing=true'
+  });
+
+  const compare = await page.compareScreenshot();
+  expect(compare).toMatchScreenshot();
+});
