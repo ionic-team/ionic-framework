8944e4214bf166a14683e225383e77e35fc86545
Brandy Carney <brandyscarney@users.noreply.github.com>
2019-09-27 17:28:46 -0400

    refactor(scss): remove scss (#19292)
    BREAKING CHANGES
    
    Removes `scss` files from the distributed files. Please use CSS variables for theming instead.

--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -76,10 +76,7 @@ export const config: Config = {
     },
     {
       type: 'dist',
-      esmLoaderPath: '../loader',
-      copy: [
-        { src: '**/*.scss' }
-      ]
+      esmLoaderPath: '../loader'
     },
     // {
     //   type: 'experimental-dist-module',
