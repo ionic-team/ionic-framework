4fc5c7b379c398bdd59006f078e6cf2b8cd1f4ae
Brandy Carney <brandy@ionic.io>
2019-10-11 16:36:23 -0400

    test(protractor): increase timeout to fix failing tests

--- angular/test/test-app/e2e/protractor.conf.js
+++ angular/test/test-app/e2e/protractor.conf.js
@@ -23,7 +23,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 50000,
     print: function() {}
   },
   onPrepare() {
