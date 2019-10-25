6814d1fdafd7a1fee78a7232bee67190a908e847
Liam DeBeasi <liamdebeasi@icloud.com>
2019-10-25 14:05:06 -0400

    fix css vars

--- core/src/components/split-pane/test/basic/e2e.ts
+++ core/src/components/split-pane/test/basic/e2e.ts
@@ -5,6 +5,28 @@ test('split-pane: basic', async () => {
     url: '/src/components/split-pane/test/basic?ionic:_testing=true'
   });
 
-  const compare = await page.compareScreenshot();
-  expect(compare).toMatchScreenshot();
+  const screenshotCompares = [];
+  const MIN_WIDTH = '#side-min-width';
+  const MAX_WIDTH = '#side-max-width';
+  const WIDTH = '#side-width';
+
+  screenshotCompares.push(await page.compareScreenshot());
+
+  await page.click(MIN_WIDTH);
+
+  screenshotCompares.push(await page.compareScreenshot());
+
+  await page.click(MIN_WIDTH);
+  await page.click(MAX_WIDTH);
+
+  screenshotCompares.push(await page.compareScreenshot());
+
+  await page.click(MAX_WIDTH);
+  await page.click(WIDTH);
+
+  screenshotCompares.push(await page.compareScreenshot());
+
+  for (const screenshotCompare of screenshotCompares) {
+    expect(screenshotCompare).toMatchScreenshot();
+  }
 });
