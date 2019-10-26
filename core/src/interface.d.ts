264a8fab52238745e40ceae2a16b5e108fdb9c2a
tawfiek <tawfiek.108@gmail.com>
2019-10-26 15:15:37 +0200

    refactor: removing textarea from TextFieldTypes and add it into alert type

--- core/src/interface.d.ts
+++ core/src/interface.d.ts
@@ -35,7 +35,7 @@ export * from './utils/overlays-interface';
 export * from './global/config';
 export { Gesture, GestureDetail } from './utils/gesture';
 
-export type TextFieldTypes = 'date' | 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'url' | 'time' | 'textarea';
+export type TextFieldTypes = 'date' | 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'url' | 'time';
 export type Side = 'start' | 'end';
 export type PredefinedColors = 'primary' | 'secondary' | 'tertiary' | 'success' | 'warning' | 'danger' | 'light' | 'medium' | 'dark';
 export type Color = PredefinedColors | string;
