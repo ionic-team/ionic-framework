afa511a07976b46eca614eda784c3c239d483c0d
Brandy Carney <brandy@ionic.io>
2019-10-29 10:45:16 -0400

    fix(segment): use proper gesture enable fn

--- core/src/components/segment/segment.tsx
+++ core/src/components/segment/segment.tsx
@@ -68,7 +68,7 @@ export class Segment implements ComponentInterface {
   @Watch('disabled')
   disabledChanged() {
     if (this.gesture) {
-      this.gesture.setDisabled(this.disabled);
+      this.gesture.enable(!this.disabled);
     }
   }
 
