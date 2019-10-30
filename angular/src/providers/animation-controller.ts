c937da5868329c1947b5dd6aa174af387e5c6bd4
Liam DeBeasi <liamdebeasi@icloud.com>
2019-10-30 12:21:36 -0400

    run linter

--- angular/src/providers/animation-controller.ts
+++ angular/src/providers/animation-controller.ts
@@ -11,18 +11,18 @@ export class AnimationController {
   create(animationId?: string): Animation {
     return createAnimation(animationId);
   }
-  
+
   /**
    * EXPERIMENTAL
    *
    * Given a progression and a cubic bezier function,
-   * this utility returns the time value(s) at which the 
+   * this utility returns the time value(s) at which the
    * cubic bezier reaches the given time progression.
-   * 
+   *
    * If the cubic bezier never reaches the progression
    * the result will be an empty array.
    *
-   * This is most useful for switching between easing curves 
+   * This is most useful for switching between easing curves
    * when doing a gesture animation (i.e. going from linear easing
    * during a drag, to another easing when `progressEnd` is called)
    */
