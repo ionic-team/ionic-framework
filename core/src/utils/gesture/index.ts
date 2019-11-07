2e66ddf5052feb0de0123d069acbdc3906a6692e
Liam DeBeasi <liamdebeasi@icloud.com>
2019-11-07 09:50:26 -0500

    reset gesture when disabling

--- core/src/utils/gesture/index.ts
+++ core/src/utils/gesture/index.ts
@@ -203,8 +203,12 @@ export const createGesture = (config: GestureConfig): Gesture => {
 
   return {
     enable(enable = true) {
-      if (!enable && hasCapturedPan) {
-        pointerUp(undefined);
+      if (!enable) {
+        reset();
+
+        if (hasCapturedPan) {
+          pointerUp(undefined);
+        }
       }
       pointerEvents.enable(enable);
     },
