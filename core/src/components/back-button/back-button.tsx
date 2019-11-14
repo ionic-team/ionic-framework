b494c25dbd20f02eeefde5ce61a7236264e6d68e
Liam DeBeasi <liamdebeasi@icloud.com>
2019-11-14 13:39:38 -0500

    Add shadow parts

--- core/src/components/back-button/back-button.tsx
+++ core/src/components/back-button/back-button.tsx
@@ -106,10 +106,10 @@ export class BackButton implements ComponentInterface, ButtonInterface {
           'show-back-button': showBackButton
         }}
       >
-        <button type={type} disabled={disabled} class="button-native">
+        <button type={type} disabled={disabled} class="button-native" part="button-native">
           <span class="button-inner">
-            {backButtonIcon && <ion-icon icon={backButtonIcon} lazy={false}></ion-icon>}
-            {backButtonText && <span class="button-text">{backButtonText}</span>}
+            {backButtonIcon && <ion-icon icon={backButtonIcon} lazy={false} part="icon"></ion-icon>}
+            {backButtonText && <span class="button-text" part="text">{backButtonText}</span>}
           </span>
           {mode === 'md' && <ion-ripple-effect type={this.rippleType}></ion-ripple-effect>}
         </button>
