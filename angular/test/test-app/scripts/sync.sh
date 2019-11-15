302164b35069a7f90ac7e526875639028796d2cd
Mike Hartington <mikehartington@gmail.com>
2019-11-15 12:09:34 -0500

    fix(): fix sync script

--- angular/test/test-app/scripts/sync.sh
+++ angular/test/test-app/scripts/sync.sh
@@ -1,12 +1,10 @@
 # Copy angular dist
 rm -rf node_modules/@ionic/angular
-mkdir node_modules/@ionic/angular
-cp -a ../../dist/ node_modules/@ionic/angular
+cp -a ../../dist node_modules/@ionic/angular
 
 # Copy angular server
 rm -rf node_modules/@ionic/angular-server
-mkdir node_modules/@ionic/angular-server
-cp -a ../../../packages/angular-server/dist/ node_modules/@ionic/angular-server
+cp -a ../../../packages/angular-server/dist node_modules/@ionic/angular-server
 
 # # Copy core dist
 rm -rf node_modules/@ionic/core
