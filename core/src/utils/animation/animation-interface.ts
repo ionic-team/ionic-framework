a10c52959a3eddaf5f739a85f627aa3bc3ceed2f
Liam DeBeasi <liamdebeasi@icloud.com>
2019-11-21 10:33:27 -0500

    animation org updates

--- core/src/utils/animation/animation-interface.ts
+++ core/src/utils/animation/animation-interface.ts
@@ -220,18 +220,20 @@ export interface Animation {
 }
 
 export type AnimationLifecycle = (currentStep: 0 | 1, animation: Animation) => void;
+export type AnimationKeyFrames = [AnimationKeyFrameEdge, AnimationKeyFrameEdge] | AnimationKeyFrame[];
+export type AnimationStyles = Record<string, any>;
 
 export interface AnimationCallbackOptions {
   oneTimeCallback: boolean;
 }
 
-export type AnimationKeyFrames = AnimationKeyFrame[];
-
 export interface AnimationKeyFrame extends AnimationStyles {
   offset: number;
 }
 
-export type AnimationStyles = Record<string, any>;
+export interface AnimationKeyFrameEdge extends AnimationStyles {
+  offset?: number;
+}
 
 export interface AnimationPlayOptions {
   sync: boolean;
