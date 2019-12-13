ce2bb90e36f9ea2f57bef8dbc128855d08bed7d8
Mike Hartington <mikehartington@gmail.com>
2019-12-13 12:12:21 -0500

    chore(): updates

--- angular/src/directives/navigation/nav-delegate.ts
+++ angular/src/directives/navigation/nav-delegate.ts
@@ -1,7 +1,12 @@
 import { ComponentFactoryResolver, Directive, ElementRef, Injector, ViewContainerRef } from '@angular/core';
 
 import { AngularDelegate } from '../../providers/angular-delegate';
+import { ProxyCmp, proxyOutputs } from '../proxies-utils';
 
+@ProxyCmp({
+  inputs: ['animated', 'animation', 'root', 'rootParams', 'swipeGesture'],
+  methods: ['push', 'insert', 'insertPages', 'pop', 'popTo', 'popToRoot', 'removeIndex', 'setRoot', 'setPages', 'getActive', 'getByIndex', 'canGoBack', 'getPrevious']
+})
 @Directive({
   selector: 'ion-nav'
 })
@@ -16,5 +21,6 @@ export class NavDelegate {
   ) {
     this.el = ref.nativeElement;
     ref.nativeElement.delegate = angularDelegate.create(resolver, injector, location);
+    proxyOutputs(this, this.el, ['ionNavDidChange' , 'ionNavWillChange' ])
   }
 }
