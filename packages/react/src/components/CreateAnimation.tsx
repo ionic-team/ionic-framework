430cf98dcd22afdc5dbeb672f07d8d6c870e8470
Liam DeBeasi <liamdebeasi@icloud.com>
2020-01-22 12:56:57 -0500

    clean up

--- packages/react/src/components/CreateAnimation.tsx
+++ packages/react/src/components/CreateAnimation.tsx
@@ -1,8 +1,10 @@
 import { Animation, AnimationCallbackOptions, AnimationDirection, AnimationFill, AnimationKeyFrames, AnimationLifecycle, createAnimation } from '@ionic/core';
 import React from 'react';
 
-type LifecycleCallback = { callback: AnimationLifecycle, opts?: AnimationCallbackOptions } | { callback: AnimationLifecycle, opts?: AnimationCallbackOptions }[];
-type HookCallback = () => void | (() => void)[];
+type LifecycleCallback = (callback: AnimationLifecycle, opts?: AnimationCallbackOptions) => void;
+type HookCallback = () => void;
+interface PartialPropertyValue { property: string; value: any; }
+interface PropertyValue { property: string; fromValue: any; toValue: any; }
 
 export interface CreateAnimationProps {
   delay?: number;
@@ -29,6 +31,9 @@ export interface CreateAnimationProps {
   onFinish?: LifecycleCallback;
 
   keyframes?: AnimationKeyFrames;
+  from?: PartialPropertyValue;
+  to?: PartialPropertyValue;
+  fromTo?: PropertyValue;
 
   play: boolean;
 }
@@ -42,130 +47,67 @@ export interface CreateAnimationProps {
  * progressStart
  * progressStep
  * progressEnd
- * from
- * to
- * fromTo
  * addAnimation
- * fix onFinish types (they aren't correct)
  */
 
  /**
   * QUESTIONS
-  * How do I call the getter methods? (i.e. getKeyframes())
   * How do I update props on the fly? (i.e. toggle play/pause)
   * Is there a better way to setup the animation
   * other than individually checking each prop?
   * Should the before/after hooks accept arrays of
   * callback in the core method?
+  * How do I accept multiples elements even if they aren't immediately nested?
   */
 
 export class CreateAnimation extends React.Component<CreateAnimationProps> {
-  ref: React.RefObject<HTMLElement> = React.createRef();
+  private nodes: Map<number, HTMLElement> = new Map();
   animation?: Animation;
 
   updateAnimation() {
     if (!this.animation) { return; }
 
-    if (this.ref.current) {
-      this.animation.addElement(this.ref.current);
+    console.log(this);
+    const animation = this.animation!;
+
+    if (this.nodes.size > 0) {
+      animation.addElement(Array.from(this.nodes.values()));
     }
 
-    if (this.props.iterations) {
-      this.animation.iterations(this.props.iterations);
+    const props = this.props;
+    for (const key in props) {
+      if (props.hasOwnProperty(key) && !['play', 'from', 'to', 'fromTo', 'onFinish'].includes(key)) {
+        (animation as any)[key]((props as any)[key]);
+      }
     }
 
-    if (this.props.fill) {
-      this.animation.fill(this.props.fill);
+    const fromValues = this.props.from;
+    if (fromValues) {
+      const values = (Array.isArray(fromValues)) ? fromValues : [fromValues];
+      values.forEach(val => animation.from(val.property, val.value));
     }
 
-    if (this.props.direction) {
-      this.animation.direction(this.props.direction);
+    const toValues = this.props.to;
+    if (toValues) {
+      const values = (Array.isArray(toValues)) ? toValues : [toValues];
+      values.forEach(val => animation.to(val.property, val.value));
     }
 
-    if (this.props.duration) {
-      this.animation.duration(this.props.duration);
+    const fromToValues = this.props.fromTo;
+    if (fromToValues) {
+      const values = (Array.isArray(fromToValues)) ? fromToValues : [fromToValues];
+      values.forEach(val => animation.fromTo(val.property, val.fromValue, val.toValue));
     }
 
-    if (this.props.easing) {
-      this.animation.easing(this.props.easing);
-    }
-
-    if (this.props.delay) {
-      this.animation.delay(this.props.delay);
-    }
-
-    if (this.props.keyframes) {
-      this.animation.keyframes(this.props.keyframes);
+    const onFinishValues = this.props.onFinish;
+    if (onFinishValues) {
+      const values = (Array.isArray(onFinishValues)) ? onFinishValues : [onFinishValues];
+      values.forEach(val => animation.onFinish(val.callback, val.opts));
     }
 
     if (this.props.play) {
-      this.animation.play();
+      animation.play();
     }
-
-    const afterAddRead = this.props.afterAddRead;
-    if (afterAddRead) {
-      this.setCallbacks(afterAddRead, this.animation.afterAddRead);
-    }
-
-    const afterAddWrite = this.props.afterAddWrite;
-    if (afterAddWrite) {
-      this.setCallbacks(afterAddWrite, this.animation.afterAddWrite);
-    }
-
-    const afterClearStyles = this.props.afterClearStyles;
-    if (afterClearStyles) {
-      this.setCallbacks(afterClearStyles, this.animation.afterClearStyles);
-    }
-
-    const afterStyles = this.props.afterStyles;
-    if (afterStyles) {
-      this.setCallbacks(afterStyles, this.animation.afterStyles);
-    }
-
-    const afterAddClass = this.props.afterAddClass;
-    if (afterAddClass) {
-      this.setCallbacks(afterAddClass, this.animation.afterAddClass);
-    }
-
-    const afterRemoveClass = this.props.afterRemoveClass;
-    if (afterRemoveClass) {
-      this.setCallbacks(afterRemoveClass, this.animation.afterRemoveClass);
-    }
-
-    const beforeAddRead = this.props.beforeAddRead;
-    if (beforeAddRead) {
-      this.setCallbacks(beforeAddRead, this.animation.beforeAddRead);
-    }
-
-    const beforeAddWrite = this.props.beforeAddWrite;
-    if (beforeAddWrite) {
-      this.setCallbacks(beforeAddWrite, this.animation.beforeAddWrite);
-    }
-
-    const beforeClearStyles = this.props.beforeClearStyles;
-    if (beforeClearStyles) {
-      this.setCallbacks(beforeClearStyles, this.animation.beforeClearStyles);
-    }
-
-    const beforeStyles = this.props.beforeStyles;
-    if (beforeStyles) {
-      this.setCallbacks(beforeStyles, this.animation.beforeStyles);
-    }
-
-    const beforeAddClass = this.props.beforeAddClass;
-    if (beforeAddClass) {
-      this.setCallbacks(beforeAddClass, this.animation.beforeAddClass);
-    }
-
-    const beforeRemoveClass = this.props.beforeRemoveClass;
-    if (beforeRemoveClass) {
-      this.setCallbacks(beforeRemoveClass, this.animation.beforeRemoveClass);
-    }
-  }
-
-  setCallbacks(prop: any, method: any) {
-    const fns = (Array.isArray(prop)) ? prop : [prop];
-    fns.forEach(fn => method(fn));
   }
 
   componentDidMount() {
@@ -177,8 +119,9 @@ export class CreateAnimation extends React.Component<CreateAnimationProps> {
     const { children } = this.props;
     return (
       <>
-        {React.cloneElement(children as any, { ref: this.ref })}
+        {React.Children.map(children, ((child, id) => React.cloneElement(child as any, { ref: (el: any) => this.nodes.set(id, el) })))}
       </>
     );
   }
+
 }
