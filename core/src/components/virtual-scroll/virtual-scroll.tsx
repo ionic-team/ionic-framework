f374465bc5a0a01c8cd946ce96e1d3f6f3f7add3
Elvis <elvis.graholskis@cloudbrixx.de>
2020-03-02 11:43:49 +0100

    Fixing typing issues

--- core/src/components/virtual-scroll/virtual-scroll.tsx
+++ core/src/components/virtual-scroll/virtual-scroll.tsx
@@ -443,7 +443,7 @@ export class VirtualScroll implements ComponentInterface {
   }
 }
 
-const VirtualProxy: FunctionalComponent<{dom: VirtualNode[], minIHeight: number | undefined}> = ({ dom, minimumItemHeight }, children, utils) => {
+const VirtualProxy: FunctionalComponent<{dom: VirtualNode[], minIHeight: MinimumItemHeightFn | undefined}> = ({ dom, minimumItemHeight }, children, utils) => {
   return utils.map(children, (child, i) => {
     const node = dom[i];
     const vattrs = child.vattrs || {};
