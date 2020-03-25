ae7fe543fe71c57a2b6ce7082a9279f60344af35
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2020-03-25 13:36:54 -0400

    fix(modal): respect card-style modal spec for iPadOS (#20750)
    fixes #20700

--- core/src/components/modal/animations/ios.leave.ts
+++ core/src/components/modal/animations/ios.leave.ts
@@ -10,13 +10,12 @@ export const iosLeaveAnimation = (
   presentingEl?: HTMLElement,
   duration = 500
 ): Animation => {
-
   const backdropAnimation = createAnimation()
     .addElement(baseEl.querySelector('ion-backdrop')!)
     .fromTo('opacity', 'var(--backdrop-opacity)', 0.0);
 
   const wrapperAnimation = createAnimation()
-    .addElement(baseEl.querySelector('.modal-wrapper')!)
+    .addElement(baseEl.querySelectorAll('.modal-wrapper, .modal-shadow')!)
     .beforeStyles({ 'opacity': 1 })
     .fromTo('transform', 'translateY(0vh)', 'translateY(100vh)');
 
@@ -24,15 +23,13 @@ export const iosLeaveAnimation = (
     .addElement(baseEl)
     .easing('cubic-bezier(0.32,0.72,0,1)')
     .duration(duration)
-    .addAnimation([backdropAnimation, wrapperAnimation]);
+    .addAnimation(wrapperAnimation);
 
   if (presentingEl) {
-    const transformOffset = (!CSS.supports('width', 'max(0px, 1px)')) ? '30px' : 'max(30px, var(--ion-safe-area-top))';
-    const modalTransform = (presentingEl.tagName === 'ION-MODAL' && (presentingEl as HTMLIonModalElement).presentingElement !== undefined) ? '-10px' : transformOffset;
-    const bodyEl = document.body;
-    const currentPresentingScale = SwipeToCloseDefaults.MIN_PRESENTING_SCALE;
+    const isMobile = window.innerWidth < 768;
+    const hasCardModal = (presentingEl.tagName === 'ION-MODAL' && (presentingEl as HTMLIonModalElement).presentingElement !== undefined);
+
     const presentingAnimation = createAnimation()
-      .addElement(presentingEl)
       .beforeClearStyles(['transform'])
       .afterClearStyles(['transform'])
       .onFinish(currentStep => {
@@ -45,13 +42,58 @@ export const iosLeaveAnimation = (
         if (numModals <= 1) {
           bodyEl.style.setProperty('background-color', '');
         }
-      })
-      .keyframes([
-        { offset: 0, filter: 'contrast(0.85)', transform: `translateY(${modalTransform}) scale(${currentPresentingScale})`, borderRadius: '10px 10px 0 0' },
-        { offset: 1, filter: 'contrast(1)', transform: 'translateY(0px) scale(1)', borderRadius: '0px' }
-      ]);
+      });
 
-    baseAnimation.addAnimation(presentingAnimation);
+    const bodyEl = document.body;
+
+    if (isMobile) {
+      const transformOffset = (!CSS.supports('width', 'max(0px, 1px)')) ? '30px' : 'max(30px, var(--ion-safe-area-top))';
+      const modalTransform = hasCardModal ? '-10px' : transformOffset;
+      const toPresentingScale = SwipeToCloseDefaults.MIN_PRESENTING_SCALE;
+      const finalTransform = `translateY(${modalTransform}) scale(${toPresentingScale})`;
+
+      presentingAnimation
+        .addElement(presentingEl)
+        .keyframes([
+          { offset: 0, filter: 'contrast(0.85)', transform: finalTransform, borderRadius: '10px 10px 0 0' },
+          { offset: 1, filter: 'contrast(1)', transform: 'translateY(0px) scale(1)', borderRadius: '0px' }
+        ]);
+
+      baseAnimation.addAnimation(presentingAnimation);
+    } else {
+      baseAnimation.addAnimation(backdropAnimation);
+
+      if (!hasCardModal) {
+        wrapperAnimation.fromTo('opacity', '1', '0');
+      } else {
+        const toPresentingScale = (hasCardModal) ? SwipeToCloseDefaults.MIN_PRESENTING_SCALE : 1;
+        const finalTransform = `translateY(-10px) scale(${toPresentingScale})`;
+
+        presentingAnimation
+          .addElement(presentingEl.querySelector('.modal-wrapper')!)
+          .afterStyles({
+            'transform': 'translate3d(0, 0, 0)'
+          })
+          .keyframes([
+            { offset: 0, filter: 'contrast(0.85)', transform: finalTransform },
+            { offset: 1, filter: 'contrast(1)', transform: 'translateY(0) scale(1)' }
+          ]);
+
+        const shadowAnimation = createAnimation()
+          .addElement(presentingEl.querySelector('.modal-shadow')!)
+          .afterStyles({
+            'transform': 'translateY(0) scale(1)'
+          })
+          .keyframes([
+            { offset: 0, opacity: '0', transform: finalTransform },
+            { offset: 1, opacity: '1', transform: 'translateY(0) scale(1)' }
+          ]);
+
+        baseAnimation.addAnimation([presentingAnimation, shadowAnimation]);
+      }
+    }
+  } else {
+    baseAnimation.addAnimation(backdropAnimation);
   }
 
   return baseAnimation;
