2b0ebcdbe6b255cf1736ce846897c94761ce244a
HansKrywaa <hanskrywaa@web.de>
2020-04-02 21:48:11 +0200

    resolve merge conflict

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -390,6 +390,10 @@ export namespace Components {
          */
         "color"?: Color;
         /**
+          * If `true`, the user cannot interact with the chip.
+         */
+        "disabled": boolean;
+        /**
           * The mode determines which platform styles to use.
          */
         "mode"?: "ios" | "md";
@@ -3629,6 +3633,10 @@ declare namespace LocalJSX {
           * The color to use from your application's color palette. Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`. For more information on colors, see [theming](/docs/theming/basics).
          */
         "color"?: Color;
+        /**
+          * If `true`, the user cannot interact with the chip.
+         */
+        "disabled"?: boolean;
         /**
           * The mode determines which platform styles to use.
          */
