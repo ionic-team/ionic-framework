75012d00b918eb4936930ad1ff73217c3c3f757d
Liam DeBeasi <liamdebeasi@icloud.com>
2020-04-22 19:46:55 +0000

    build

--- core/src/components/item/item.tsx
+++ core/src/components/item/item.tsx
@@ -12,7 +12,7 @@ import { createColorClasses, hostContext, openURL } from '../../utils/theme';
  * @slot start - Content is placed to the left of the item text in LTR, and to the right in RTL.
  * @slot end - Content is placed to the right of the item text in LTR, and to the left in RTL.
  *
- * @TODOpart detail-icon - The chevron icon for the item. Only applies when `detail="true"`.
+ * @part detail-icon - The chevron icon for the item. Only applies when `detail="true"`.
  */
 @Component({
   tag: 'ion-item',
