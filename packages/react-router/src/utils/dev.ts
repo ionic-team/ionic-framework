645277a1687ec7c0dfbb0158101518df8c2fb026
Ely Lucas <ely@meta-tek.net>
2020-04-23 09:33:47 -0600

    removing unneeded utils

--- packages/react-router/src/utils/dev.ts
+++
@@ -1,9 +0,0 @@
-export const isDevMode = () => {
-  return process && process.env && process.env.NODE_ENV === 'development';
-};
-
-export const deprecationWarning = (message: string) => {
-  if (isDevMode()) {
-    console.warn(message);
-  }
-};
