14c226ce75958da14b66821028d6f3a6852d695c
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2020-04-27 11:31:32 -0400

    fix(back-button): screen readers correctly announce the back button text (#21053)
    fixes #21043

--- core/src/components/back-button/back-button.tsx
+++ core/src/components/back-button/back-button.tsx
@@ -126,10 +126,10 @@ export class BackButton implements ComponentInterface, ButtonInterface {
           'show-back-button': showBackButton
         }}
       >
-        <button type={type} disabled={disabled} class="button-native" part="button">
+        <button type={type} disabled={disabled} class="button-native" part="button" aria-label={backButtonText || 'back'}>
           <span class="button-inner">
-            {backButtonIcon && <ion-icon icon={backButtonIcon} lazy={false} part="icon"></ion-icon>}
-            {backButtonText && <span class="button-text" part="text">{backButtonText}</span>}
+            {backButtonIcon && <ion-icon icon={backButtonIcon} aria-hidden="true" lazy={false} part="icon"></ion-icon>}
+            {backButtonText && <span aria-hidden="true" class="button-text" part="text">{backButtonText}</span>}
           </span>
           {mode === 'md' && <ion-ripple-effect type={this.rippleType}></ion-ripple-effect>}
         </button>
