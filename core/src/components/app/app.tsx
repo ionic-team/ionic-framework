07d11a1ac53404509131ab1db75c0a3798fe737a
Liam DeBeasi <liamdebeasi@icloud.com>
2020-04-30 14:46:30 -0400

    add window check

--- core/src/components/app/app.tsx
+++ core/src/components/app/app.tsx
@@ -28,7 +28,7 @@ export class App implements ComponentInterface {
         if (config.getBoolean('hardwareBackButton', isHybrid)) {
           import('../../utils/hardware-back-button').then(module => module.startHardwareBackButton());
         }
-        if (typeof window !== 'undefined') {
+        if (typeof (window as any) !== 'undefined') {
           import('../../utils/keyboard').then(module => module.startKeyboardAssist(window));
         }
         import('../../utils/focus-visible').then(module => module.startFocusVisible());
