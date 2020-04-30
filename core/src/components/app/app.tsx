711bf588d4794722d9f2812f98ceb4a6d984286c
Liam DeBeasi <liamdebeasi@icloud.com>
2020-04-30 14:45:48 -0400

    add window check

--- core/src/components/app/app.tsx
+++ core/src/components/app/app.tsx
@@ -28,8 +28,9 @@ export class App implements ComponentInterface {
         if (config.getBoolean('hardwareBackButton', isHybrid)) {
           import('../../utils/hardware-back-button').then(module => module.startHardwareBackButton());
         }
-
-        import('../../utils/keyboard').then(module => module.startKeyboardAssist(window));
+        if (typeof window !== 'undefined') {
+          import('../../utils/keyboard').then(module => module.startKeyboardAssist(window));
+        }
         import('../../utils/focus-visible').then(module => module.startFocusVisible());
       });
     }
