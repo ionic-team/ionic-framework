ae5f1ddff0c0799167a154abbb418b2ad3434d47
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2020-04-30 16:07:44 -0400

    feat(app): keyboard open and close events (#18478)

--- angular/src/providers/platform.ts
+++ angular/src/providers/platform.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable, NgZone } from '@angular/core';
-import { BackButtonEventDetail, Platforms, getPlatforms, isPlatform } from '@ionic/core';
+import { BackButtonEventDetail, KeyboardEventDetail, Platforms, getPlatforms, isPlatform } from '@ionic/core';
 import { Subject, Subscription } from 'rxjs';
 
 export interface BackButtonEmitter extends Subject<BackButtonEventDetail> {
@@ -19,6 +19,18 @@ export class Platform {
    * @hidden
    */
   backButton: BackButtonEmitter = new Subject<BackButtonEventDetail>() as any;
+
+  /**
+   * The keyboardDidShow event emits when the
+   * on-screen keyboard is presented.
+   */
+  keyboardDidShow = new Subject<KeyboardEventDetail>() as any;
+
+  /**
+   * The keyboardDidHide event emits when the
+   * on-screen keyboard is hidden.
+   */
+  keyboardDidHide = new Subject<void>();
 
   /**
    * The pause event emits when the native platform puts the application
@@ -55,6 +67,8 @@ export class Platform {
       proxyEvent(this.resume, doc, 'resume');
       proxyEvent(this.backButton, doc, 'ionBackButton');
       proxyEvent(this.resize, this.win, 'resize');
+      proxyEvent(this.keyboardDidShow, this.win, 'ionKeyboardDidShow');
+      proxyEvent(this.keyboardDidHide, this.win, 'ionKeyboardDidHide');
 
       let readyResolve: (value: string) => void;
       this._readyPromise = new Promise(res => { readyResolve = res; });
