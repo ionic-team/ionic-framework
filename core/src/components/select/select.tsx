bcf0fa17a97a10b1813b3cecda32ba908cb76482
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2020-04-30 12:10:08 -0400

    docs(select): direct old controller links to component pages (#21167)

--- core/src/components/select/select.tsx
+++ core/src/components/select/select.tsx
@@ -78,9 +78,9 @@ export class Select implements ComponentInterface {
 
   /**
    * Any additional options that the `alert`, `action-sheet` or `popover` interface
-   * can take. See the [AlertController API docs](../../alert/AlertController/#create), the
-   * [ActionSheetController API docs](../../action-sheet/ActionSheetController/#create) and the
-   * [PopoverController API docs](../../popover/PopoverController/#create) for the
+   * can take. See the [ion-alert docs](../alert), the
+   * [ion-action-sheet docs](../action-sheet) and the
+   * [ion-popover docs](../popover) for the
    * create options for each interface.
    *
    * Note: `interfaceOptions` will not override `inputs` or `buttons` with the `alert` interface.
