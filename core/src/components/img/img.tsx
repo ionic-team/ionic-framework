5a030d950d0e927c818e8138f5526272f1c63c5c
Evgeniy <osben90@gmail.com>
2020-05-23 11:54:08 +0300

    fix IntersectionObserver
    ref link https://github.com/w3c/IntersectionObserver/issues/296
    bug in chrome 55
    p.s. Sorry for not issuing

--- core/src/components/img/img.tsx
+++ core/src/components/img/img.tsx
@@ -53,7 +53,9 @@ export class Img implements ComponentInterface {
     if (this.src === undefined) {
       return;
     }
-    if ('IntersectionObserver' in window) {
+    if ('IntersectionObserver' in window &&
+      'IntersectionObserverEntry' in window &&
+      'isIntersecting' in window.IntersectionObserverEntry.prototype) {
       this.removeIO();
       this.io = new IntersectionObserver(data => {
         // because there will only ever be one instance
