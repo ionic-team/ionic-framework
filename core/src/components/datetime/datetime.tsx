e6d4556a614d40f2534395a0fe1e68dcc0e9ac09
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2020-06-02 10:06:04 -0400

    Update core/src/components/datetime/datetime.tsx

--- core/src/components/datetime/datetime.tsx
+++ core/src/components/datetime/datetime.tsx
@@ -179,7 +179,7 @@ export class Datetime implements ComponentInterface {
   /**
    * Short abbreviated day of the week names. This can be used to provide
    * locale names for each day in the week. Defaults to English.
-   * Default: `['Sun','Mon','Tue','Wed','Thu','Fri','Sat']`
+   * Defaults to: `['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']`
    */
   @Prop() dayShortNames?: string[] | string;
 
