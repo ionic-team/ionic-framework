c8a9d61b7b2e0d48c07a3744d0a3e08cc63f2e03
Wis <im@wis.am>
2020-06-02 20:34:10 +0300

    make doc arg optional so tsc completes and test passes

--- packages/react/src/components/utils/attachProps.ts
+++ packages/react/src/components/utils/attachProps.ts
@@ -61,7 +61,7 @@ export const getClassName = (classList: DOMTokenList, newProps: any, oldProps: a
  * Checks if an event is supported in the current execution environment.
  * @license Modernizr 3.0.0pre (Custom Build) | MIT
  */
-export const isCoveredByReact = (eventNameSuffix: string, doc: Document) => {
+export const isCoveredByReact = (eventNameSuffix: string, doc?: Document) => {
   if (typeof document === "undefined") {
     return true;
   }
