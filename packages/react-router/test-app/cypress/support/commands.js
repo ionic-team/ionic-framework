3b12f7db4a113ed96f453dc576f10a9b7f27f822
Ely Lucas <ely@meta-tek.net>
2020-06-11 10:41:05 -0600

    update to test-app

--- packages/react-router/test-app/cypress/support/commands.js
+++ packages/react-router/test-app/cypress/support/commands.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
