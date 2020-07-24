167c23abfd86093b8191dcde8038a1c569c358e9
Liam DeBeasi <liamdebeasi@icloud.com>
2020-07-24 18:13:35 -0400

    add additional exports

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,2 +1,48 @@
+import { addIcons } from 'ionicons';
+import { arrowBackSharp, caretBackSharp, chevronBack, chevronForward, close, closeCircle, closeSharp, menuOutline, menuSharp, reorderThreeOutline, reorderTwoSharp, searchOutline, searchSharp } from 'ionicons/icons';
+
 export * from './proxies';
 export * from './ionic-vue';
+
+export {
+  // Overlay Controllers
+  alertController,
+  actionSheetController,
+  menuController, modalController,
+  loadingController,
+  pickerController,
+  popoverController,
+  toastController,
+
+  // Security
+  IonicSafeString,
+
+  // Platform
+  isPlatform, Platforms, getPlatforms,
+
+  // Gesture
+  createGesture,
+
+  // Animations
+  createAnimation,
+  iosTransitionAnimation,
+  mdTransitionAnimation,
+  getTimeGivenProgression,
+} from '@ionic/core';
+
+// Icons that are used by internal components
+addIcons({
+  'arrow-back-sharp': arrowBackSharp,
+  'caret-back-sharp': caretBackSharp,
+  'chevron-back': chevronBack,
+  'chevron-forward': chevronForward,
+  'close': close,
+  'close-circle': closeCircle,
+  'close-sharp': closeSharp,
+  'menu-outline': menuOutline,
+  'menu-sharp': menuSharp,
+  'reorder-two-sharp': reorderTwoSharp,
+  'reorder-three-outline': reorderThreeOutline,
+  'search-outline': searchOutline,
+  'search-sharp': searchSharp,
+});
