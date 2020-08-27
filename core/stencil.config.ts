7e84f371ced879f7acb5382ccb4da76e949f14e4
Brandy Carney <brandy@ionic.io>
2020-08-27 11:21:15 -0400

    chore(puppeteer): no sandbox

--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -131,6 +131,10 @@ export const config: Config = {
   },
   testing: {
     allowableMismatchedPixels: 200,
+    browserArgs: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ],
     pixelmatchThreshold: 0.05,
     waitBeforeScreenshot: 20,
     emulate: [
