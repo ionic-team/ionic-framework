a61903dfff714e7c8631434b1d5a0d95cc00d124
Brandy Carney <brandy@ionic.io>
2020-08-27 10:26:45 -0400

    style(lint): fix lint errors, add comments

--- angular/test/test-app/e2e/protractor-ci.conf.js
+++ angular/test/test-app/e2e/protractor-ci.conf.js
@@ -1,4 +1,7 @@
 
+// Protractor CI configuration file, see link for more information
+// https://angular.io/guide/testing#configure-cli-for-ci-testing-in-chrome
+
 const config = require('./protractor.conf').config;
 
 config.capabilities = {
