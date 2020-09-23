1fbddfa57793b5a958ed694ff2a59a0bc2221298
Liam DeBeasi <liamdebeasi@icloud.com>
2020-09-23 16:12:23 -0400

    fix

--- packages/vue/src/controllers.ts
+++ packages/vue/src/controllers.ts
@@ -6,19 +6,17 @@ import { VueDelegate } from './framework-delegate';
 
 const oldModalCreate = modalController.create.bind(modalController);
 modalController.create = (options) => {
-  const delegate = (options.component instanceof HTMLElement) ? undefined : VueDelegate();
   return oldModalCreate({
     ...options,
-    delegate
+    delegate: VueDelegate()
   });
 }
 
 const oldPopoverCreate = popoverController.create.bind(popoverController);
 popoverController.create = (options) => {
-  const delegate = (options.component instanceof HTMLElement) ? undefined : VueDelegate();
   return oldPopoverCreate({
     ...options,
-    delegate
+    delegate: VueDelegate()
   });
 }
 
