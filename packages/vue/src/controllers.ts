add942051837787815d7e21772fc00e45e2408e9
Liam DeBeasi <liamdebeasi@icloud.com>
2020-09-23 14:50:05 -0400

    do not create dleegate with el

--- packages/vue/src/controllers.ts
+++ packages/vue/src/controllers.ts
@@ -6,17 +6,19 @@ import { VueDelegate } from './framework-delegate';
 
 const oldModalCreate = modalController.create.bind(modalController);
 modalController.create = (options) => {
+  const delegate = (options.component instanceof HTMLElement) ? undefined : VueDelegate();
   return oldModalCreate({
     ...options,
-    delegate: VueDelegate()
+    delegate
   });
 }
 
 const oldPopoverCreate = popoverController.create.bind(popoverController);
 popoverController.create = (options) => {
+  const delegate = (options.component instanceof HTMLElement) ? undefined : VueDelegate();
   return oldPopoverCreate({
     ...options,
-    delegate: VueDelegate()
+    delegate
   });
 }
 
