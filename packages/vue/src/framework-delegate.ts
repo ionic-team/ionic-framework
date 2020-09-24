ea7d7b006780dc0890e71a0a2938806d9773183a
Liam DeBeasi <liamdebeasi@icloud.com>
2020-09-24 14:47:04 +0000

    do not add opts

--- packages/vue/src/framework-delegate.ts
+++ packages/vue/src/framework-delegate.ts
@@ -1,8 +1,8 @@
 import { createVNode, render } from 'vue';
 
 export const VueDelegate = () => {
-  const attachViewToDom = (parentElement: HTMLElement, component: any, opts?: any, classes?: string[]) => {
-    const vueInstance = createVNode(component, opts);
+  const attachViewToDom = (parentElement: HTMLElement, component: any, _: any, classes?: string[]) => {
+    const vueInstance = createVNode(component);
 
     const div = document.createElement('div');
     classes && div.classList.add(...classes);
