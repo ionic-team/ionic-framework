37896eb7628867be335bbe5471e61a3f690836bf
Liam DeBeasi <liamdebeasi@icloud.com>
2020-10-06 19:07:01 +0000

    add tests

---
+++ packages/vue/test-app/src/views/NavigationChild.vue
@@ -0,0 +1,63 @@
+<template>
+  <ion-page data-pageid="navigationchild">
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons>
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title>Navigation Child</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Navigation Child</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div class="ion-padding">
+        Navigation Child Page
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: 'NavigationChild',
+  components: {
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  },
+  setup() {
+    const router = useRouter();
+    const setRouteParams = () => {
+      router.push({
+        query: {
+          search: 'liamwashere'
+        }
+      });
+    }
+
+    return { setRouteParams }
+  }
+});
+</script>
