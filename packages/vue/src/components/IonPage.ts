84cd18b1b221408aa658f921827c834361ccc561
Liam DeBeasi <liamdebeasi@icloud.com>
2020-10-14 18:56:55 +0000

    improve hmr experience

--- packages/vue/src/components/IonPage.ts
+++ packages/vue/src/components/IonPage.ts
@@ -3,7 +3,11 @@ import { h, defineComponent } from 'vue';
 export const IonPage = defineComponent({
   name: 'IonPage',
   props: {
-    isInOutlet: { type: Boolean, default: false }
+    isInOutlet: { type: Boolean, default: false },
+    registerIonPage: { type: Function, default: () => {} }
+  },
+  mounted() {
+    this.$props.registerIonPage(this.$refs.ionPage);
   },
   setup(props, { attrs, slots }) {
     const hidePageClass = (props.isInOutlet) ? 'ion-page-invisible' : '';
--- packages/vue/test-app/src/views/TabsSecondary.vue
+++ packages/vue/test-app/src/views/TabsSecondary.vue
@@ -3,17 +3,17 @@
     <ion-content>
       <ion-tabs id="tabs">
         <ion-tab-bar slot="bottom">
-          <ion-tab-button tab="tab1" href="/tabs-secondary/tab1">
+          <ion-tab-button tab="tab1-secondary" href="/tabs-secondary/tab1">
             <ion-icon :icon="triangle" />
             <ion-label>Tab 1</ion-label>
           </ion-tab-button>
 
-          <ion-tab-button tab="tab2" href="/tabs-secondary/tab2">
+          <ion-tab-button tab="tab2-secondary" href="/tabs-secondary/tab2">
             <ion-icon :icon="ellipse" />
             <ion-label>Tab 2</ion-label>
           </ion-tab-button>
 
-          <ion-tab-button tab="tab3" href="/tabs-secondary/tab3">
+          <ion-tab-button tab="tab3-secondary" href="/tabs-secondary/tab3">
             <ion-icon :icon="square" />
             <ion-label>Tab 3</ion-label>
           </ion-tab-button>
