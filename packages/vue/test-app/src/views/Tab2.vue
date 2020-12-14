7928eaefd675e0f787ac4cacf62c61ecb7cbff8b
Liam DeBeasi <liamdebeasi@icloud.com>
2020-12-14 22:43:54 +0000

    indent

--- packages/vue/test-app/src/views/Tab2.vue
+++ packages/vue/test-app/src/views/Tab2.vue
@@ -1,22 +1,22 @@
 <template>
-<ion-page data-pageid="tab2">
-  <ion-header>
-    <ion-toolbar>
-      <ion-title>Tab 2</ion-title>
-    </ion-toolbar>
-  </ion-header>
-  <ion-content :fullscreen="true">
-    <ion-header collapse="condense">
+  <ion-page data-pageid="tab2">
+    <ion-header>
       <ion-toolbar>
-        <ion-title size="large">Tab 2</ion-title>
+        <ion-title>Tab 2</ion-title>
       </ion-toolbar>
     </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
 
-    <ion-item button router-link="/routing" id="routing">
-      <ion-label>Go to /routing</ion-label>
-    </ion-item>
-  </ion-content>
-</ion-page>
+      <ion-item button router-link="/routing" id="routing">
+        <ion-label>Go to /routing</ion-label>
+      </ion-item>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
