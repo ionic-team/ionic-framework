556b695ecaf69408934b4710ebc741c5b7b26f4b
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-12-17 17:02:51 -0600

    chore: enable dist-hydrate-script

--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -134,9 +134,9 @@ export const config: Config = {
       type: 'docs-json',
       file: '../docs/core.json'
     },
-    // {
-    //   type: 'dist-hydrate-script'
-    // },
+    {
+      type: 'dist-hydrate-script'
+    },
     apiSpecGenerator({
       file: 'api.txt'
     }) as any,
