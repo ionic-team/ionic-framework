30c087c9bff119aa1781cc47184159c3baebb51b
Liam DeBeasi <liamdebeasi@icloud.com>
2020-12-17 16:44:47 -0500

    fix

--- packages/vue/test-app/src/views/Tabs.vue
+++ packages/vue/test-app/src/views/Tabs.vue
@@ -17,11 +17,6 @@
             <ion-icon :icon="square" />
             <ion-label>Tab 3</ion-label>
           </ion-tab-button>
-
-          <ion-tab-button tab="tab4" href="/tabs/tab4">
-            <ion-icon :icon="cog" />
-            <ion-label>Tab 4</ion-label>
-          </ion-tab-button>
         </ion-tab-bar>
       </ion-tabs>
     </ion-content>
@@ -30,14 +25,13 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonContent, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { cog, ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
   components: { IonContent, IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      cog,
       ellipse,
       square,
       triangle,
