9d44979ac54ff1bca9696bbddd2620929a7a57d8
Liam DeBeasi <liamdebeasi@icloud.com>
2021-02-18 10:07:16 -0500

    run guards on initial load

--- core/src/components/router/router.tsx
+++ core/src/components/router/router.tsx
@@ -61,7 +61,17 @@ export class Router implements ComponentInterface {
     await waitUntilNavNode();
     console.debug('[ion-router] found nav');
 
-    await this.onRoutesChanged();
+    const canProceed = await this.runGuards(this.getPath());
+    if (canProceed !== true) {
+      if (typeof canProceed === 'object') {
+        const path = parsePath(canProceed.redirect);
+        const queryString = canProceed.redirect.split('?')[1];
+        this.setPath(path, ROUTER_INTENT_NONE, queryString);
+        await this.writeNavStateRoot(path, ROUTER_INTENT_NONE);
+      }
+    } else {
+      await this.onRoutesChanged();
+    }
   }
 
   componentDidLoad() {
