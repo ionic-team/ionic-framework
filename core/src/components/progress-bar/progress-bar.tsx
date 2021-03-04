d91d66541b5b268668f958f64c54dd8a1be23043
Brandy Carney <brandyscarney@users.noreply.github.com>
2021-03-04 10:53:26 -0500

    Update core/src/components/progress-bar/progress-bar.tsx
    Co-authored-by: Liam DeBeasi <liamdebeasi@users.noreply.github.com>

--- core/src/components/progress-bar/progress-bar.tsx
+++ core/src/components/progress-bar/progress-bar.tsx
@@ -11,7 +11,7 @@ import { createColorClasses } from '../../utils/theme';
  *
  * @part progress - The progress bar that shows the current value when `type` is `"determinate"` and slides back and forth when `type` is `"indeterminate"`.
  * @part stream - The animated circles that appear while buffering. This only shows when `buffer` is set and `type` is `"determinate"`.
- * @part track - The track bar is behind the progress bar, if the `buffer` property is set and `type` is `"determinate"` the track will be the
+ * @part track - The track bar behind the progress bar. If the `buffer` property is set and `type` is `"determinate"` the track will be the
  * width of the `buffer` value.
  */
 @Component({
