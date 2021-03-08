969b9659e38715d268a89c2a5f90fb0306355866
Marcelo Brandao <marcelo.barbosa@medgrupo.com.br>
2021-03-08 15:20:16 -0300

    Ajuste botoes

--- core/src/components/button/button.tsx
+++ core/src/components/button/button.tsx
@@ -223,25 +223,23 @@ export class Button implements ComponentInterface, AnchorInterface, ButtonInterf
           'ion-focusable': true,
         })}
       >
-        <div class="center">
-          <TagType
-            {...attrs}
-            class="button-native"
-            part="native"
-            disabled={disabled}
-            onFocus={this.onFocus}
-            onBlur={this.onBlur}
-            {...inheritedAttributes}
-          >
-            <span class="button-inner">
-              <slot name="icon-only"></slot>
-              <slot name="start"></slot>
-              <slot></slot>
-              <slot name="end"></slot>
-            </span>
-            {mode === 'md' && <ion-ripple-effect type={this.rippleType}></ion-ripple-effect>}
-          </TagType>
-        </div>
+        <TagType
+          {...attrs}
+          class="button-native"
+          part="native"
+          disabled={disabled}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          {...inheritedAttributes}
+        >
+          <span class="button-inner">
+            <slot name="icon-only"></slot>
+            <slot name="start"></slot>
+            <slot></slot>
+            <slot name="end"></slot>
+          </span>
+          {mode === 'md' && <ion-ripple-effect type={this.rippleType}></ion-ripple-effect>}
+        </TagType>
       </Host>
     );
   }
