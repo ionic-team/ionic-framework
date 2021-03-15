f7bfa2959eadd2b4c0dd9459e72a5fbeb1fd1528
alexbleggi <alexbleggi@gmail.com>
2021-03-15 12:01:11 -0300

    Implementação storybook

--- core/.storybook/main.js
+++ core/.storybook/main.js
@@ -6,6 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-addon-designs"
+    '@storybook/addon-a11y',
+    'storybook-addon-themes',
+    'storybook-addon-designs'
   ]
 }
