8584d91acf47562c8e9901f18bb81c42f076eb91
alexbleggi <alexbleggi@gmail.com>
2021-03-15 11:25:21 -0300

    Implementação storybook

--- core/.storybook/preview.js
+++ core/.storybook/preview.js
@@ -1,4 +1,16 @@
 import { defineCustomElements} from '../loader/index.js';
+import '../css/core.css';
+import '../css/display.css';
+import '../css/flex-utils.css';
+import '../css/float-elements.css';
+import '../css/global.bundle.css';
+import '../css/ionic.bundle.css';
+import '../css/normalize.css';
+import '../css/padding.css';
+import '../css/structure.css';
+import '../css/text-alignment.css';
+import '../css/text-transformation.css';
+import '../css/typography.css';
 
 defineCustomElements();
 
