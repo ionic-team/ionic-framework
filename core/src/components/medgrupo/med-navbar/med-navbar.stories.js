9fe76615656007a485d9f4ea63c65416c5a3a075
alexbleggi <alexbleggi@gmail.com>
2021-03-19 16:26:47 -0300

    Implementação story

--- core/src/components/medgrupo/med-navbar/med-navbar.stories.js
+++ core/src/components/medgrupo/med-navbar/med-navbar.stories.js
@@ -1,12 +1,13 @@
 import { html } from 'lit-html';
 import { withDesign } from 'storybook-addon-designs';
+import imageFile from '../../../../';
 
 export default {
   title: 'Components/med-navbar',
   decorators: [withDesign],
 };
 
-const Template = () => {
+const TemplateDefault = () => {
   return html`
   <ion-app>
     <med-navbar></med-navbar>
@@ -15,7 +16,7 @@ const Template = () => {
     `
   }
 
-export const Default = Template.bind({});
+export const Default = TemplateDefault.bind({});
 Default.parameters = {
   design: {
     type: 'figma',
@@ -23,17 +24,20 @@ Default.parameters = {
   },
 }
 
-const Template = () => {
+const TemplateIconLeft = () => {
   return html`
   <ion-app>
-    <med-navbar></med-navbar>
+    <med-navbar>
+      <img slot="left" src='../.storybook/assets/left-icon.svg'>
+      <img slot="right" src='../../../../.storybook/assets/right-icon.svg'>
+    </med-navbar>
     <ion-content></ion-content>
   </ion-app>
     `
   }
 
-export const Default = Template.bind({});
-Default.parameters = {
+export const IconLeft = TemplateIconLeft.bind({});
+IconLeft.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/file/2j9jNt3PmQXpzD3IQJkyZe/Componentes?node-id=729%3A189',
