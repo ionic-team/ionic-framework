14c999da8335f9b467337c4f5766c4a1be4ba9be
Liam DeBeasi <liamdebeasi@icloud.com>
2021-03-24 23:45:56 +0000

    account for vue 3.0.6 bug fix in overlays as well

--- packages/vue/src/utils.ts
+++ packages/vue/src/utils.ts
@@ -57,3 +57,5 @@ export const getConfig = (): CoreConfig | null => {
   }
   return null;
 };
+
+export const needsKebabCase = (version: string) => !['3.0.0', '3.0.1', '3.0.2', '3.0.3', '3.0.4', '3.0.5'].includes(version);
