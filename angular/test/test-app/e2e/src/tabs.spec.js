41fe4cf6743df2dd2d7289b15787283dce305ac6
Brandy Carney <brandy@ionic.io>
2021-03-26 13:52:14 -0400

    TODO revert: make a test fail

--- angular/test/test-app/e2e/src/tabs.spec.js
+++ angular/test/test-app/e2e/src/tabs.spec.js
@@ -16,7 +16,7 @@ describe('Tabs', () => {
 
       cy.get('#tab-button-contact').click();
       tab = testTabTitle('Tab 2 - Page 1');
-      tab.find('.segment-changed').should('have.text', 'false');
+      tab.find('.segment-changed').should('have.text', 'true');
     });
 
     it('should simulate stack + double tab click', () => {
