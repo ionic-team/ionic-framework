f3d055fea96203a4a1361a9da591c138af9eb5ca
willmartindev <contact@willmartin.dev>
2021-04-20 11:28:52 -0400

    remove hard-coded label, fix whitespace

--- core/src/components/datetime/test/a11y/datetime.spec.ts
+++ core/src/components/datetime/test/a11y/datetime.spec.ts
@@ -4,26 +4,27 @@ import { Item } from '../../../item/item';
 import { Label } from '../../../label/label';
 
 describe('Datetime a11y', () => {
-    it('does not set a default aria-labelledby when there is not a neighboring ion-label', async () => {
-        const page = await newSpecPage({
-            components: [Datetime, Item, Label],
-            html: `<ion-datetime></ion-datetime>`
-        })
+  it('does not set a default aria-labelledby when there is not a neighboring ion-label', async () => {
+    const page = await newSpecPage({
+      components: [Datetime, Item, Label],
+      html: `<ion-datetime></ion-datetime>`
+    })
 
-        const ariaLabelledBy = page.root.getAttribute('aria-labelledby');
-        expect(ariaLabelledBy).toBe(null);
-    });
+    const ariaLabelledBy = page.root.getAttribute('aria-labelledby');
+    expect(ariaLabelledBy).toBe(null);
+  });
 
-    it('set a default aria-labelledby when a neighboring ion-label exists', async () => {
-        const page = await newSpecPage({
-            components: [Datetime, Item, Label],
-            html: `<ion-item>
-                <ion-label>A11y Test</ion-label>
-                <ion-datetime></ion-datetime>
-            </ion-item>`
-        })
+  it('set a default aria-labelledby when a neighboring ion-label exists', async () => {
+    const page = await newSpecPage({
+      components: [Datetime, Item, Label],
+      html: `<ion-item>
+        <ion-label>A11y Test</ion-label>
+        <ion-datetime></ion-datetime>
+      </ion-item>`
+    })
 
-        const ariaLabelledBy = page.body.querySelector('ion-datetime').getAttribute('aria-labelledby');
-        expect(ariaLabelledBy).toBe('ion-dt-1-lbl');
-    });
+    const label = page.body.querySelector('ion-label');
+    const ariaLabelledBy = page.body.querySelector('ion-datetime').getAttribute('aria-labelledby');
+    expect(ariaLabelledBy).toBe(label.id);
+  });
 });
--- core/src/components/input/test/a11y/input.spec.ts
+++ core/src/components/input/test/a11y/input.spec.ts
@@ -4,26 +4,27 @@ import { Item } from '../../../item/item';
 import { Label } from '../../../label/label';
 
 describe('Input a11y', () => {
-    it('does not set a default aria-labelledby when there is not a neighboring ion-label', async () => {
-        const page = await newSpecPage({
-            components: [Input, Item, Label],
-            html: `<ion-input></ion-input>`
-        })
+  it('does not set a default aria-labelledby when there is not a neighboring ion-label', async () => {
+    const page = await newSpecPage({
+      components: [Input, Item, Label],
+      html: `<ion-input></ion-input>`
+    })
+ 
+    const ariaLabelledBy = page.body.querySelector('ion-input > input').getAttribute('aria-labelledby');
+    expect(ariaLabelledBy).toBe(null);
+  });
 
-        const ariaLabelledBy = page.body.querySelector('ion-input > input').getAttribute('aria-labelledby');
-        expect(ariaLabelledBy).toBe(null);
-    });
+  it('set a default aria-labelledby when a neighboring ion-label exists', async () => {
+    const page = await newSpecPage({
+      components: [Input, Item, Label],
+      html: `<ion-item>
+        <ion-label>A11y Test</ion-label>
+        <ion-input></ion-input>
+      </ion-item>`
+    })
 
-    it('set a default aria-labelledby when a neighboring ion-label exists', async () => {
-        const page = await newSpecPage({
-            components: [Input, Item, Label],
-            html: `<ion-item>
-                <ion-label>A11y Test</ion-label>
-                <ion-input></ion-input>
-            </ion-item>`
-        })
-
-        const ariaLabelledBy = page.body.querySelector('ion-input > input').getAttribute('aria-labelledby');
-        expect(ariaLabelledBy).toBe('ion-input-1-lbl');
-    });
+    const label = page.body.querySelector('ion-label'); 
+    const ariaLabelledBy = page.body.querySelector('ion-input > input').getAttribute('aria-labelledby');
+    expect(ariaLabelledBy).toBe(label.id);
+  });
 });
