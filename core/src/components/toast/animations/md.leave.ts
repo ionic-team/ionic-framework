ab262a9df1d2cbfb674c9ba9a1648f67a9d1af7a
Liam DeBeasi <liamdebeasi@icloud.com>
2021-04-29 14:27:10 -0400

    improve overlay animations with shadow dom

--- core/src/components/toast/animations/md.leave.ts
+++ core/src/components/toast/animations/md.leave.ts
@@ -1,22 +1,22 @@
 import { Animation } from '../../../interface';
 import { createAnimation } from '../../../utils/animation/animation';
+import { getElementRoot } from '../../../utils/helpers';
 
 /**
  * md Toast Leave Animation
  */
-export const mdLeaveAnimation = (baseEl: ShadowRoot): Animation => {
+export const mdLeaveAnimation = (baseEl: HTMLElement): Animation => {
   const baseAnimation = createAnimation();
   const wrapperAnimation = createAnimation();
 
-  const hostEl = baseEl.host || baseEl;
-  const wrapperEl = baseEl.querySelector('.toast-wrapper') as HTMLElement;
+  const root = getElementRoot(baseEl);
+  const wrapperEl = root.querySelector('.toast-wrapper') as HTMLElement;
 
   wrapperAnimation
     .addElement(wrapperEl)
     .fromTo('opacity', 0.99, 0);
 
   return baseAnimation
-    .addElement(hostEl)
     .easing('cubic-bezier(.36,.66,.04,1)')
     .duration(300)
     .addAnimation(wrapperAnimation);
