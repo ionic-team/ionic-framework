fd5dd01887edea2b2e217c776d45d02b97ca45aa
willmartindev <contact@willmartin.dev>
2021-05-24 17:07:33 -0400

    check for undefined element in counter

--- core/src/components/item/item.tsx
+++ core/src/components/item/item.tsx
@@ -283,7 +283,7 @@ export class Item implements ComponentInterface, AnchorInterface, ButtonInterfac
   }
 
   private updateCounterOutput(inputEl: HTMLIonInputElement | HTMLIonTextareaElement) {
-    if (this.counter && !this.multipleInputs && inputEl.maxlength !== undefined) {
+    if (this.counter && !this.multipleInputs && inputEl?.maxlength !== undefined) {
       const length = inputEl?.value?.toString().length ?? '0';
       this.counterString = `${length}/${inputEl.maxlength}`;
     }
