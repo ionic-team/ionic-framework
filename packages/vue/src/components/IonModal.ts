dc9bd788bc042e79d86bab9cf46f5605ae445486
Liam DeBeasi <liamdebeasi@icloud.com>
2021-06-17 17:24:48 +0000

    fix modal

--- packages/vue/src/components/IonModal.ts
+++ packages/vue/src/components/IonModal.ts
@@ -1,22 +1,23 @@
 import { defineComponent, h, ref, onMounted } from 'vue';
+import { defineCustomElement } from '../utils';
+import { IonModal as IonModalCmp } from '@ionic/core/components/ion-modal.js';
 
-export const IonModal = defineComponent({
-  name: 'IonModal',
-  setup(_, { attrs, slots }) {
-    const isOpen = ref(false);
-    const elementRef = ref();
+export const IonModal = /*@__PURE__*/ defineComponent((_, { attrs, slots }) => {
+  defineCustomElement('ion-modal', IonModalCmp);
 
-    onMounted(() => {
-      elementRef.value.addEventListener('will-present', () => isOpen.value = true);
-      elementRef.value.addEventListener('did-dismiss', () => isOpen.value = false);
-    });
+  const isOpen = ref(false);
+  const elementRef = ref();
 
-    return () => {
-      return h(
-        'ion-modal',
-        { ...attrs, ref: elementRef },
-        (isOpen.value) ? slots : undefined
-      )
-    }
+  onMounted(() => {
+    elementRef.value.addEventListener('will-present', () => isOpen.value = true);
+    elementRef.value.addEventListener('did-dismiss', () => isOpen.value = false);
+  });
+
+  return () => {
+    return h(
+      'ion-modal',
+      { ...attrs, ref: elementRef },
+      (isOpen.value) ? slots : undefined
+    )
   }
 });
