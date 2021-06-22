cd1ce296baefe44a9b5840cc8e56b908ca25fd53
Liam DeBeasi <liamdebeasi@icloud.com>
2021-06-22 18:15:38 +0000

    add initial hook update

--- packages/vue/src/hooks.ts
+++ packages/vue/src/hooks.ts
@@ -7,11 +7,26 @@ import {
   getCurrentInstance
 } from 'vue';
 import { LifecycleHooks } from './utils';
+import { AnimationBuilder } from './';
+import { Location } from './types';
 
 type Handler = (processNextHandler: () => void) => Promise<any> | void | null;
 
+export type RouteAction = 'push' | 'pop' | 'replace';
+export type RouteDirection = 'forward' | 'back' | 'root' | 'none';
+
 export interface IonRouter {
   canGoBack: (deep?: number) => boolean;
+  push: (location: string | Location, routerAnimation?: AnimationBuilder) => void;
+  replace: (location: string | Location, routerAnimation?: AnimationBuilder) => void;
+  back: (routerAnimation?: AnimationBuilder) => void;
+  forward: (routerAnimation?: AnimationBuilder) => void;
+  navigate: (
+    location: string | Location,
+    routerDirection?: RouteDirection,
+    routerAction?: RouteAction,
+    routerAnimation?: AnimationBuilder
+  ) => void;
 }
 
 export interface IonKeyboardRef {
@@ -30,10 +45,45 @@ export const useBackButton = (priority: number, handler: Handler) => {
 }
 
 export const useIonRouter = (): IonRouter => {
-  const { canGoBack } = inject('navManager') as any;
+  const {
+    canGoBack,
+    goBack,
+    goForward,
+    handleNavigate
+  } = inject('navManager') as any;
+
+  const navigate = (
+    location: string | Location,
+    routerDirection?: RouteDirection,
+    routerAction?: RouteAction,
+    routerAnimation?: AnimationBuilder
+  ) => handleNavigate(location, routerAction, routerDirection, routerAnimation);
+
+  const push = (
+    location: string | Location,
+    routerAnimation?: AnimationBuilder
+  ) => navigate(location, 'forward', 'push', routerAnimation);
+
+  const replace = (
+    location: string | Location,
+    routerAnimation?: AnimationBuilder
+  ) => navigate(location, 'root', 'replace', routerAnimation);
+
+  const back = (
+    routerAnimation?: AnimationBuilder
+  ) => goBack(routerAnimation);
+
+  const forward = (
+    routerAnimation?: AnimationBuilder
+  ) => goForward(routerAnimation);
 
   return {
-    canGoBack
+    canGoBack,
+    push,
+    replace,
+    back,
+    forward,
+    navigate
   } as IonRouter
 }
 
---
+++ packages/vue/src/types.ts
@@ -0,0 +1,11 @@
+type Dictionary<T> = { [key: string]: T }
+
+export interface Location {
+  name?: string
+  path?: string
+  hash?: string
+  query?: Dictionary<string | (string | null)[] | null | undefined>
+  params?: Dictionary<string>
+  append?: boolean
+  replace?: boolean
+}
