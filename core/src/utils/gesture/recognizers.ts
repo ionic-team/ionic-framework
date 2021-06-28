7b222187f123af10d56f5f778fd53661dc4837a2
Mac <harlow44@gmail.com>
2021-06-28 03:03:56 -0500

    Support undefined as a direciton.
    After digging into the code I don't think it was built with the whole 2 directional gestures in mind.
    The work around being create an X gesture and a Y gesture. (however the docs elude to 'undefined' as a type option)
    
    This is the best I can imagine implementing without a very large dive into what things are doing.
    Pan could calculated off the larger delta in still giving the pan some use?
    
    ```
              (WARNING): Pan the way it's used does not make sense since it does not have an X or Y element and just represents the chosen direction.
              This change has the potential to be breaking for users who actually un-ironically use undefined for 'y', or if the base of
              Ionic made that mistake anywhere.
              (DOCS): undefined is now actually an option just like in the docs.
              (PERF): Instead of creating 2 `createGesture`'s you can use one. (for people who need an X&Y draggable item) 
    ```
    
    #23523 as it also relates to #21704

--- core/src/utils/gesture/recognizers.ts
+++ core/src/utils/gesture/recognizers.ts
@@ -9,6 +9,7 @@ export interface PanRecognizer {
 export const createPanRecognizer = (direction: string, thresh: number, maxAngle: number): PanRecognizer => {
   const radians = maxAngle * (Math.PI / 180);
   const isDirX = direction === 'x';
+  const isDirUndefined = direction === undefined;
   const maxCosine = Math.cos(radians);
   const threshold = thresh * thresh;
 
@@ -39,6 +40,20 @@ export const createPanRecognizer = (direction: string, thresh: number, maxAngle:
         return false;
       }
       const hypotenuse = Math.sqrt(distance);
+      
+      if(isDirUndefined) {
+        pan = distance > 0 ? 1 : 0; // Could do the calculation with the bigger Dx or Dy
+        /* 
+          (WARNING): Pan the way it's used does not make sense since it does not have an X or Y element and just represents the chosen direction.
+          This change has the potential to be breaking for users who actually un-ironically use undefined for 'y', or if the base of
+          Ionic made that mistake anywhere.
+          (DOCS): undefined is now actually an option just like in the docs.
+          (PERF): Instead of creating 2 `createGesture`'s you can use one. (for people who need an X&Y draggable item) 
+        */
+        dirty = false;
+        return true;
+      }
+      
       const cosine = (isDirX ? deltaX : deltaY) / hypotenuse;
 
       if (cosine > maxCosine) {
