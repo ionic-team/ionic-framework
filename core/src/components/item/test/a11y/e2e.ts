9021e7cc4b48a69ccc94faa7d2ddcb10a2afa340
William Martin <contact@willmartin.dev>
2021-07-09 09:19:48 -0400

    fix(item): mirror disabled prop to aria attribute (#23544)
    resolves #23513

---
+++ core/src/components/item/test/a11y/e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from '@stencil/core/testing';
+import { AxePuppeteer } from '@axe-core/puppeteer';
+
+test('item: axe', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/item/test/a11y?ionic:_testing=true'
+  });
+
+  const results = await new AxePuppeteer(page).analyze();
+  expect(results.violations.length).toEqual(0);
+});
