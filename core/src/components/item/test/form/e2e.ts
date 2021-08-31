140542f7ed999bd4f7f7bb0fe7f5204d458e6ce5
Liam DeBeasi <liamdebeasi@icloud.com>
2021-08-31 14:06:06 -0400

    update e2e

--- core/src/components/item/test/form/e2e.ts
+++ core/src/components/item/test/form/e2e.ts
@@ -1,17 +1,8 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-test('item: basic', async () => {
+test('item: form', async () => {
   const page = await newE2EPage({
-    url: '/src/components/item/test/basic?ionic:_testing=true'
-  });
-
-  const compare = await page.compareScreenshot();
-  expect(compare).toMatchScreenshot();
-});
-
-test('item: basic-rtl', async () => {
-  const page = await newE2EPage({
-    url: '/src/components/item/test/basic?ionic:_testing=true&rtl=true'
+    url: '/src/components/item/test/form?ionic:_testing=true'
   });
 
   const compare = await page.compareScreenshot();
