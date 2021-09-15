e47933e05047d46ceb0970752239d9b0671d7213
alexbleggi <alexbleggi@gmail.com>
2021-09-15 20:00:09 -0300

    button v666

--- core/src/stories/components/global/button/button-secondary.stories.js
+++ core/src/stories/components/global/button/button-secondary.stories.js
@@ -50,7 +50,7 @@ export const Secondary = Template.bind({});
 Secondary.parameters = {
   design: {
     type: 'figma',
-    url: 'https://www.figma.com/file/2j9jNt3PmQXpzD3IQJkyZe/Componentes?node-id=729%3A189',
+    url: 'https://www.figma.com/file/2j9jNt3PmQXpzD3IQJkyZe/Componentes?node-id=6232%3A46685',
   },
 }
 Secondary.argTypes = {
@@ -73,11 +73,11 @@ Secondary.argTypes = {
     },
   },
   dsSize: {
-    options: [undefined, 'xxs', 'xs', 'sm'],
+    options: [undefined, 'lg', 'md', 'sm', 'xs', 'xxs', 'xxxs'],
     control: { type: 'radio'},
     description: "Define a variação de tamanho componente.",
     table: {
-      type:  { summary: 'xs | sm | md | lg' },
+      type:  { summary: 'lg' | 'md' | 'sm' | 'xs' | 'xxs' | 'xxxs' },
       defaultValue: { summary: 'undefined' },
     },
   },
