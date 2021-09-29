3104bfc3f77cea9c0490f84b2c138c117cab1918
Liam DeBeasi <liamdebeasi@icloud.com>
2021-09-29 16:18:59 +0000

    chore(): a few fixes, add tests

--- core/src/components/picker-column-internal/picker-column-internal.tsx
+++ core/src/components/picker-column-internal/picker-column-internal.tsx
@@ -42,7 +42,7 @@ export class PickerColumnInternal implements ComponentInterface {
    * Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`.
    * For more information on colors, see [theming](/docs/theming/basics).
    */
-  @Prop({ reflect: true }) color?: Color;
+  @Prop({ reflect: true }) color?: Color = 'primary';
 
   /**
    * If `true`, tapping the picker will
