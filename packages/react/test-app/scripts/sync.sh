3e043e4952ff75c3059d582fe02af0d78155f408
Liam DeBeasi <liamdebeasi@icloud.com>
2021-10-22 20:35:29 +0000

    test(react): ensure test app is getting latest CE build files

--- packages/react/test-app/scripts/sync.sh
+++ packages/react/test-app/scripts/sync.sh
@@ -10,10 +10,10 @@ cp -a ../../react-router/dist node_modules/@ionic/react-router/dist
 cp -a ../../react-router/package.json node_modules/@ionic/react-router/package.json
 
 # Copy core dist
-rm -rf node_modules/@ionic/core/dist node_modules/@ionic/core/loader
+rm -rf node_modules/@ionic/core/dist node_modules/@ionic/core/components
 cp -a ../../../core/dist node_modules/@ionic/core/dist
-cp -a ../../../core/loader node_modules/@ionic/core/loader
 cp -a ../../../core/package.json node_modules/@ionic/core/package.json
+cp -a ../../../core/components node_modules/@ionic/core/components
 
 # Copy ionicons
 rm -rf node_modules/ionicons
