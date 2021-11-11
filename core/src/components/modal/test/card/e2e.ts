df9215649559738f9304d1ba192e54c5f4a520ec
Liam DeBeasi <liamdebeasi@icloud.com>
2021-11-11 14:04:20 +0000

    test(modal): add stacked modal visual regression test

--- core/src/components/modal/test/card/e2e.ts
+++ core/src/components/modal/test/card/e2e.ts
@@ -1,3 +1,4 @@
+import { newE2EPage } from '@stencil/core/testing';
 import { testModal } from '../test.utils';
 
 const DIRECTORY = 'card';
@@ -5,6 +6,52 @@ const DIRECTORY = 'card';
 test('modal: card', async () => {
   await testModal(DIRECTORY, '#card', true);
 });
+
 test('modal: card - custom', async () => {
   await testModal(DIRECTORY, '#card-custom', true);
+});
+
+test('modal: card - stacked - tablet', async () => {
+  const page = await newE2EPage({ url: '/src/components/modal/test/card?ionic:_testing=true&ionic:mode=ios' });
+
+  // Ensure that we get tablet styles
+  await page.setViewport({ width: 768, height: 500 });
+
+  const screenshotCompares = [];
+  const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+
+  await page.click('#card');
+  await page.waitForSelector('#card');
+
+  await ionModalDidPresent.next();
+
+  screenshotCompares.push(await page.compareScreenshot());
+
+  await page.click('.add');
+  await page.waitForSelector('.add');
+
+  await ionModalDidPresent.next();
+
+  screenshotCompares.push(await page.compareScreenshot());
+});
+
+test('modal: card - stacked - mobile', async () => {
+  const page = await newE2EPage({ url: '/src/components/modal/test/card?ionic:_testing=true&ionic:mode=ios' });
+
+  const screenshotCompares = [];
+  const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+
+  await page.click('#card');
+  await page.waitForSelector('#card');
+
+  await ionModalDidPresent.next();
+
+  screenshotCompares.push(await page.compareScreenshot());
+
+  await page.click('.add');
+  await page.waitForSelector('.add');
+
+  await ionModalDidPresent.next();
+
+  screenshotCompares.push(await page.compareScreenshot());
 });
