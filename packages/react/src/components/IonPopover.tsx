535fa53f3c8cd1c063ed3fa6be8aa91fe7b2a865
Sean Perkins <sean@ionic.io>
2022-01-04 14:20:57 -0500

    chore(): lint issue

--- packages/react/src/components/IonPopover.tsx
+++ packages/react/src/components/IonPopover.tsx
@@ -1,5 +1,6 @@
 import { JSX } from '@ionic/core/components';
 import { defineCustomElement } from '@ionic/core/components/ion-popover.js';
+
 import { createInlineOverlayComponent } from './createInlineOverlayComponent'
 
 export const IonPopover = /*@__PURE__*/ () => {
