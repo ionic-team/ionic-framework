8c442059b5a1208d49383e1b897ddddc4b0296ff
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-01-12 11:44:52 -0500

    docs(input): clarify intended placeholder behavior for certain input types (#24567)
    resolves #24557

--- core/src/components/input/input.tsx
+++ core/src/components/input/input.tsx
@@ -151,6 +151,8 @@ export class Input implements ComponentInterface {
 
   /**
    * Instructional text that shows before the input has a value.
+   * This property applies only when the `type` property is set to `"email"`,
+   * `"number"`, `"password"`, `"search"`, `"tel"`, `"text"`, or `"url"`, otherwise it is ignored.
    */
   @Prop() placeholder?: string;
 
