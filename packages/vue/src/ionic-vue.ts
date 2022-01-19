42729ed7be8ffb87c1483ec612f3765b8f569514
Liam DeBeasi <liamdebeasi@icloud.com>
2022-01-19 20:16:59 +0000

    fix(react): setupIonicReact no longer crashes in SSR environment

--- packages/vue/src/ionic-vue.ts
+++ packages/vue/src/ionic-vue.ts
@@ -21,24 +21,23 @@ const getHelperFunctions = () => {
 export const IonicVue: Plugin = {
 
   async install(_: App, config: IonicConfig = {}) {
-    if (typeof (window as any) !== 'undefined') {
-
-      /**
-       * By default Ionic Framework hides elements that
-       * are not hydrated, but in the CE build there is no
-       * hydration.
-       * TODO: Remove when all integrations have been
-       * migrated to CE build.
-       */
+    /**
+     * By default Ionic Framework hides elements that
+     * are not hydrated, but in the CE build there is no
+     * hydration.
+     * TODO: Remove when all integrations have been
+     * migrated to CE build.
+     */
+    if (typeof (document as any) !== 'undefined') {
       document.documentElement.classList.add('ion-ce');
-
-      const { ael, rel, ce } = getHelperFunctions();
-      initialize({
-        ...config,
-        _ael: ael,
-        _rel: rel,
-        _ce: ce
-      });
     }
+
+    const { ael, rel, ce } = getHelperFunctions();
+    initialize({
+      ...config,
+      _ael: ael,
+      _rel: rel,
+      _ce: ce
+    });
   }
 };
