39f84b21cf6e0cc5d1e0b4fc415ab954442d687b
Liam DeBeasi <liamdebeasi@icloud.com>
2022-01-20 22:35:01 +0000

    test(): add additional test

--- core/src/components/menu/test/focus-trap/e2e.ts
+++ core/src/components/menu/test/focus-trap/e2e.ts
@@ -32,3 +32,28 @@ test('menu: focus trap with overlays', async () => {
 
   expect(await getActiveElementID(page)).toEqual('open-modal-button');
 });
+
+test('menu: focus trap with content inside overlays', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/menu/test/focus-trap?ionic:_testing=true'
+  });
+
+  const ionDidOpen = await page.spyOnEvent('ionDidOpen');
+  const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+  const ionModalDidDismiss= await page.spyOnEvent('ionModalDidDismiss');
+
+  const menu = await page.find('ion-menu');
+  await menu.callMethod('open');
+  await ionDidOpen.next();
+
+  expect(await getActiveElementID(page)).toEqual('open-modal-button');
+
+  const openModal = await page.find('#open-modal-button');
+  await openModal.click();
+  await ionModalDidPresent.next();
+
+  const button = await page.find('#other-button');
+  await button.click();
+
+  expect(await getActiveElementID(page)).toEqual('other-button');
+});
