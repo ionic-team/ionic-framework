88ce010418eaca38786b51720c696860b417ab6d
Amanda Smith <90629384+amandaesmith3@users.noreply.github.com>
2022-01-24 15:25:53 -0600

    fix(spinner): ensure transform doesn't overwrite circular animation (#24643)

---
+++ core/src/components/spinner/test/transform/e2e.ts
@@ -0,0 +1,18 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+// made to catch animation issues when transform is applied to host
+// example: https://github.com/ionic-team/ionic-framework/issues/19247
+test('spinner: transform', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/spinner/test/transform?ionic:_testing=true'
+  });
+
+  const compare = await page.compareScreenshot();
+  expect(compare).toMatchScreenshot();
+
+  // wait for a bit less than the spinner's duration (to avoid looping before screenshot is taken)
+  await new Promise(resolve => setTimeout(resolve, 1500));
+
+  const compare2 = await page.compareScreenshot();
+  expect(compare).toMatchScreenshot();
+});
