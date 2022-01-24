034d04920952b081d643e69bdbc66cf24e73a7f4
Sean Perkins <sean@ionic.io>
2022-01-24 11:50:27 -0500

    chore(angular): upgrade test app to Angular 13 (#24621)

--- angular/test/test-app/src/polyfills.ts
+++ angular/test/test-app/src/polyfills.ts
@@ -18,16 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
