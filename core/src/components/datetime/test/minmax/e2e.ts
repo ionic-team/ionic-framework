7b3838cc670de7845bb5937d204e86cdba93b6e6
Sean Perkins <sean@ionic.io>
2022-02-09 13:57:55 -0500

    fix(datetime): navigate to month within min range (#24759)
    Resolves #24757

--- core/src/components/datetime/test/minmax/e2e.ts
+++ core/src/components/datetime/test/minmax/e2e.ts
@@ -47,3 +47,17 @@ test('datetime: minmax navigation disabled', async () => {
   expect(navButtons[1]).toHaveAttribute('disabled');
 
 });
+
+test('datetime: min including day should not disable month', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/datetime/test/minmax?ionic:_testing=true'
+  });
+
+  const calendarMonths = await page.findAll('ion-datetime#min-with-day >>> .calendar-month');
+
+  await page.waitForChanges();
+
+  expect(calendarMonths[0]).toHaveClass('calendar-month-disabled');
+  expect(calendarMonths[1]).not.toHaveClass('calendar-month-disabled');
+  expect(calendarMonths[2]).not.toHaveClass('calendar-month-disabled');
+})
