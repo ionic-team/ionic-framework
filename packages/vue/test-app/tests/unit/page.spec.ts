1d3793d0b949a769d912372401aa31c8eee794ad
Liam DeBeasi <liamdebeasi@icloud.com>
2022-02-11 14:50:19 +0000

    test(vue): add additional test

--- packages/vue/test-app/tests/unit/page.spec.ts
+++ packages/vue/test-app/tests/unit/page.spec.ts
@@ -11,6 +11,30 @@ describe('IonPage', () => {
   beforeAll(() => {
     (HTMLElement.prototype as HTMLIonRouterOutletElement).commit = jest.fn();
   });
+  it('should add ion-page class', async () => {
+    const Page1 = {
+      template: '<ion-page></ion-page>',
+      components: { IonPage }
+    };
+
+    const router = createRouter({
+      history: createWebHistory(process.env.BASE_URL),
+      routes: [
+        { path: '/', component: Page1 }
+      ]
+    });
+
+    router.push('/');
+    await router.isReady();
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router, IonicVue]
+      }
+    });
+
+    const cmp = wrapper.findComponent(Page1);
+    expect(cmp.classes('ion-page')).toBe(true);
+  });
   it('should preserve custom classes', async () => {
     const Page1 = {
       template: '<ion-page class="custom-class"></ion-page>',
