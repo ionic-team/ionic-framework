abc36ae80b060a659f7557ad90efe98b78f5ead9
Victor Berchet <victor@suumit.com>
2022-02-11 07:08:53 -0800

    fix(router-outlet): navigating to same route with different params now activates component (#24760)
    resolves #24653

--- core/src/components/router-outlet/test/basic/e2e.ts
+++ core/src/components/router-outlet/test/basic/e2e.ts
@@ -27,3 +27,17 @@ test('getRouteId() should return the route parameters', async () => {
   expect(routeId.id).toEqual('PAGE-THREE');
   expect(routeId.params).toEqual({ param: 'route' });
 });
+
+test('it should be possible to activate the same component provided parameters are different', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/router-outlet/test/basic?ionic:_testing=true'
+  });  
+
+  await page.$eval('ion-item[href="#/page-4.1/foo"] ion-label', (el: any) => el.click());
+  await page.waitForChanges();
+  expect(await page.$eval('ion-router-outlet', (el) => el.textContent)).toMatch(/text = foo/);
+
+  await page.$eval('ion-item[href="#/page-4.2/bar"] ion-label', (el: any) => el.click());
+  await page.waitForChanges();
+  expect(await page.$eval('ion-router-outlet', (el) => el.textContent)).toMatch(/text = bar/);
+});
