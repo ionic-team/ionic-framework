ea74aad90ea99a5594c3907cfa4e2d42fa18305d
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2022-02-13 12:54:48 +0100

    test BackButton: add rtl e2e test

--- core/src/components/back-button/test/basic/e2e.ts
+++ core/src/components/back-button/test/basic/e2e.ts
@@ -8,3 +8,12 @@ test('back-button: basic', async () => {
   const compare = await page.compareScreenshot();
   expect(compare).toMatchScreenshot();
 });
+
+test('back-button: basic-rtl', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/back-button/test/basic?ionic:_testing=true&rtl=true'
+  });
+
+  const compare = await page.compareScreenshot();
+  expect(compare).toMatchScreenshot();
+});
