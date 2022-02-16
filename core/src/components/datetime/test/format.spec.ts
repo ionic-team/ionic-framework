e866032021db4d813258cbdb477304410283a7ad
Liam DeBeasi <liamdebeasi@icloud.com>
2022-02-16 19:31:11 +0000

    test(): add more tests

--- core/src/components/datetime/test/format.spec.ts
+++ core/src/components/datetime/test/format.spec.ts
@@ -37,6 +37,18 @@ describe('getMonthAndDay()', () => {
   it('should return mar, 11 may', () => {
     expect(getMonthAndDay('es-ES', { month: 5, day: 11, year: 2021 })).toEqual('mar, 11 may');
   });
+
+  it('should return Sat, Apr 1', () => {
+    expect(getMonthAndDay('en-US', { month: 4, day: 1, year: 2006 })).toEqual('Sat, Apr 1');
+  });
+
+  it('should return mar, 11 may', () => {
+    expect(getMonthAndDay('es-ES', { month: 5, day: 11, year: 2021 })).toEqual('mar, 11 may');
+  });
+
+  it('should return sáb, 1 abr', () => {
+    expect(getMonthAndDay('es-ES', { month: 4, day: 1, year: 2006 })).toEqual('sáb, 1 abr');
+  });
 })
 
 describe('getFormattedHour()', () => {
