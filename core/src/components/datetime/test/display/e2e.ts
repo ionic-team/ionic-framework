4a4c83fa12dd9fee4917b3fb6c34ab6f3245116f
amandaesmith3 <amanda.s@ionic.io>
2022-02-22 14:49:25 -0600

    test(datetime): add e2e for presentation change

--- core/src/components/datetime/test/display/e2e.ts
+++ core/src/components/datetime/test/display/e2e.ts
@@ -52,3 +52,22 @@ test('display', async () => {
     expect(screenshotCompare).toMatchScreenshot();
   }
 });
+
+test('month selection should work after changing presentation', async () => {
+  const page = await newE2EPage({
+    url: '/src/components/datetime/test/display?ionic:_testing=true'
+  });
+
+  await page.select('#presentation', 'date-time');
+  await page.waitForChanges();
+
+  await page.select('#presentation', 'time-date');
+  await page.waitForChanges();
+
+  const nextMonthButton = await page.find('ion-datetime >>> .calendar-next-prev ion-button + ion-button');
+  await nextMonthButton.click();
+  await page.waitForChanges();
+
+  const monthYear = await page.find('ion-datetime >>> .calendar-month-year');
+  expect(monthYear.innerText).toEqual('March 2022');
+});
