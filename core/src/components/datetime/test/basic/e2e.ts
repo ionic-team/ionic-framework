ff8a0c2d8376b4f1f1940ba2f141d2394ccca299
Sean Perkins <sean@ionic.io>
2022-02-23 15:01:32 -0500

    fix(): remove waitForTimeout for test

--- core/src/components/datetime/test/basic/e2e.ts
+++ core/src/components/datetime/test/basic/e2e.ts
@@ -117,14 +117,26 @@ describe('datetime: confirm date', () => {
   test('should set the date value based on the selected date', async () => {
 
     const page = await newE2EPage({
-      html: `<ion-datetime value="2021-12-25T12:40:00.000Z"></ion-datetime>`
+      html: `
+        <button>Bind datetimeMonthDidChange event</button>
+        <ion-datetime value="2021-12-25T12:40:00.000Z"></ion-datetime>
+        <script type="module">
+          import { InitMonthDidChangeEvent } from '/src/components/datetime/test/utils/month-did-change-event.js';
+          document.querySelector('button').addEventListener('click', function() {
+            InitMonthDidChangeEvent();
+          });
+        </script>
+      `
     });
+
+    const eventButton = await page.find('button');
+    await eventButton.click();
 
     const buttons = await page.findAll('ion-datetime >>> .calendar-next-prev ion-button');
 
     await buttons[1].click();
 
-    await page.waitForTimeout(350);
+    await page.waitForEvent('datetimeMonthDidChange');
 
     await page.$eval('ion-datetime', async (el: any) => {
       await el.confirm();
