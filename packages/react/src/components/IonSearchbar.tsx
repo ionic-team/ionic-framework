3240b216fc47857f154a00ce1e4afcccb516e0b4
Sean Perkins <sean@ionic.io>
2022-03-01 16:56:18 -0500

    chore(): lint react package

--- packages/react/src/components/IonSearchbar.tsx
+++ packages/react/src/components/IonSearchbar.tsx
@@ -1,6 +1,6 @@
-import { createValueTrackedComponent } from './createValueTrackedComponent';
-
 import type { JSX } from '@ionic/core/components';
 import { defineCustomElement } from '@ionic/core/components/ion-searchbar.js';
+
+import { createValueTrackedComponent } from './createValueTrackedComponent';
 
 export const IonSearchbar = /*@__PURE__*/createValueTrackedComponent<JSX.IonSearchbar, HTMLIonSearchbarElement>('ion-searchbar', undefined, undefined, defineCustomElement);
