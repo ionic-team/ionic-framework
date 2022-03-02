8faff8b8350a0d9e14e59d8d32c92917ba3fa25d
amandaesmith3 <amanda.s@ionic.io>
2022-03-02 13:42:05 -0600

    chore(): remove debug logs

--- core/src/components/datetime/test/utils/month-did-change-event.js
+++ core/src/components/datetime/test/utils/month-did-change-event.js
@@ -6,15 +6,11 @@
  * @param {*} datetimeSelector The element selector for the `ion-datetime` component.
  */
 export function InitMonthDidChangeEvent(datetimeSelector = 'ion-datetime') {
-  console.log("initing event");
   const observer = new MutationObserver(mutationRecords => {
     if (mutationRecords[0].type === 'characterData') {
-      console.log("dispatching event");
       document.dispatchEvent(new CustomEvent('datetimeMonthDidChange'));
     }
   });
-
-  console.log(document.querySelector(datetimeSelector));
 
   observer.observe(document.querySelector(datetimeSelector).shadowRoot.querySelector('.calendar-month-year'), {
     characterData: true,
