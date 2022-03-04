65e46967072af0e52962f76ef5eba2fdb882ec67
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-04 20:22:34 +0000

    chore(): take fullsize screenshot

--- core/src/components/button/test/basic/button.e2e.p.ts
+++ core/src/components/button/test/basic/button.e2e.p.ts
@@ -5,6 +5,6 @@ test.describe('button: basic', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto(`/src/components/button/test/basic`);
 
-    expect(await page.screenshot()).toMatchSnapshot(`buton-diff-${page.getSnapshotSettings()}.png`);
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`buton-diff-${page.getSnapshotSettings()}.png`);
   });
 })
