5c3f442de39931989a65e2818e2813d63ddebaac
Sean Perkins <sean@ionic.io>
2022-03-09 12:20:06 -0500

    test(refresher): legacy refresher pull to refresh

--- core/src/components/refresher/test/basic/e2e.ts
+++ core/src/components/refresher/test/basic/e2e.ts
@@ -28,4 +28,26 @@ describe('refresher: basic', () => {
     expect(items.length).toBe(60);
   });
 
+  describe('legacy refresher', () => {
+
+    it('should load more items when performing a pull-to-refresh', async () => {
+      const refresherContent = await page.$('ion-refresher-content');
+      refresherContent.evaluate((el: any) => {
+        // Resets the pullingIcon to enable the legacy refresher
+        el.pullingIcon = undefined;
+      });
+
+      await page.waitForChanges();
+
+      const initialItems = await page.findAll('ion-item');
+      expect(initialItems.length).toBe(30);
+
+      await pullToRefresh(page);
+
+      const items = await page.findAll('ion-item');
+      expect(items.length).toBe(60);
+    });
+
+  });
+
 });
