c2b99b594b29a08d3827c2e4ca4d5b0aac53076b
Sean Perkins <sean@ionic.io>
2022-03-15 10:53:32 -0400

    test(header): screenshot test for large title

--- core/src/components/header/test/scroll-target/e2e.ts
+++ core/src/components/header/test/scroll-target/e2e.ts
@@ -31,6 +31,10 @@ describe('ion-header: custom scroll target', () => {
     describe('when the scroll container has overflow', () => {
 
       it('should display the collapsed title on scroll', async () => {
+        const screenshotCompares = [];
+
+        screenshotCompares.push(await page.compareScreenshot('large title expanded'));
+
         const largeHeader = await page.find('ion-header[collapse="condense"]');
         const collapseHeader = await page.find('ion-header[collapse="fade"]');
 
@@ -39,6 +43,13 @@ describe('ion-header: custom scroll target', () => {
 
         expect(largeHeader.className).toContain('header-collapse-condense-inactive');
         expect(collapseHeader.className).not.toContain('header-collapse-condense-inactive');
+
+        screenshotCompares.push(await page.compareScreenshot('large title collapsed'));
+
+        for (const screenshotCompare of screenshotCompares) {
+          expect(screenshotCompare).toMatchScreenshot();
+        }
+
       });
 
     });
