29a5804e6a9f6aa0018efdf238a0e339f0c1be35
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-17 14:19:25 +0000

    chore(): update @vue/cli-plugin-typescript, @vue/cli-service, and typescript

--- packages/vue/test-app/tests/unit/page.spec.ts
+++ packages/vue/test-app/tests/unit/page.spec.ts
@@ -14,6 +14,7 @@ describe('IonPage', () => {
   it('should add ion-page class', async () => {
     const Page1 = {
       template: '<ion-page></ion-page>',
+      name: 'Page1',
       components: { IonPage }
     };
 
@@ -38,6 +39,7 @@ describe('IonPage', () => {
   it('should preserve custom classes', async () => {
     const Page1 = {
       template: '<ion-page class="custom-class"></ion-page>',
+      name: 'Page1',
       components: { IonPage }
     };
 
