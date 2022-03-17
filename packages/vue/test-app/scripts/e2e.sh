e323afb3ea0c4eae2731dc5523e404158a1be6e9
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-17 18:58:29 +0000

    chore(): fix typo

--- packages/vue/test-app/scripts/e2e.sh
+++ packages/vue/test-app/scripts/e2e.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 TESTS_PATH=$1
-CURRENT_SHARD=$2
-TOTAL_SHARDS=$3
+FILE_EXTENSION=$2
+CURRENT_SHARD=$3
+TOTAL_SHARDS=$4
 
-node_modules/.bin/concurrently "npm run start -- --mode production" "node_modules/.bin/wait-on http-get://localhost:8080 && npm run cypress -- --spec $(./scripts/shard.sh ${TESTS_PATH} ${CURRENT_SHARD} ${TOTAL_SHARDS})"  --kill-others --success first
+node_modules/.bin/concurrently "npm run start -- --mode production" "node_modules/.bin/wait-on http-get://localhost:8080 && npm run cypress -- --spec $(./scripts/shard.sh ${TESTS_PATH} ${FILE_EXTENSION} ${CURRENT_SHARD} ${TOTAL_SHARDS})"  --kill-others --success first
