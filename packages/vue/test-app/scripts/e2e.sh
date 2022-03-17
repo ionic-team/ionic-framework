a6ea8b66858a80f2221ce2f68ad969b850845a8b
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-17 17:25:21 +0000

    test(): pass shard flags correctly

--- packages/vue/test-app/scripts/e2e.sh
+++ packages/vue/test-app/scripts/e2e.sh
@@ -1,1 +1,4 @@
-node_modules/.bin/concurrently "npm run start -- --mode production" "node_modules/.bin/wait-on http-get://localhost:8080 && npm run cypress -- --spec $(./scripts/shard.sh ${1} ${2})"  --kill-others --success first
+CURRENT_SHARD=$1
+TOTAL_SHARDS=$2
+
+node_modules/.bin/concurrently "npm run start -- --mode production" "node_modules/.bin/wait-on http-get://localhost:8080 && npm run cypress -- --spec $(./scripts/shard.sh ${CURRENT_SHARD} ${TOTAL_SHARDS})"  --kill-others --success first
--- packages/vue/test-app/scripts/shard.sh
+++ packages/vue/test-app/scripts/shard.sh
@@ -1,5 +1,12 @@
-CURRENT_SHARD=$1
-TOTAL_SHARDS=$2
+# Splits test files into shards
+# to be run in parallel
+# Usage: ./scripts/shard.sh [Current Shard] [Total Number of Shards]
+# Example: ./scripts/shard.sh 1 2
+# If shards are not provided it will
+# assume 1 total shard.
+
+CURRENT_SHARD="${1:-1}"
+TOTAL_SHARDS="${2:-1}"
 
 # Find all spec files and order alphabetically
 paths=($(find ./tests/e2e/specs -name '*.js' | sort))
