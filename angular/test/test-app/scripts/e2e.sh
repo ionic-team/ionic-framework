291d8bc6ceacc615c4c10497ff6e7a35eb1198e0
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-17 20:17:40 +0000

    test(angular): parallelize angular e2e tests

---
+++ angular/test/test-app/scripts/e2e.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TESTS_PATH=$1
+FILE_EXTENSION=$2
+CURRENT_SHARD=$3
+TOTAL_SHARDS=$4
+
+node_modules/.bin/concurrently "npm run start -- --configuration production" "node_modules/.bin/wait-on http-get://localhost:4200 && npm run cypress -- --spec $(./scripts/shard.sh ${TESTS_PATH} ${FILE_EXTENSION} ${CURRENT_SHARD} ${TOTAL_SHARDS})"  --kill-others --success first
