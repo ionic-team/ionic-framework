6de2865e32dbfff07cd730398c58e50524dbd1e2
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-17 20:35:04 +0000

    chore(): fix cypress command

--- angular/test/test-app/scripts/e2e.sh
+++ angular/test/test-app/scripts/e2e.sh
@@ -5,4 +5,4 @@ FILE_EXTENSION=$2
 CURRENT_SHARD=$3
 TOTAL_SHARDS=$4
 
-node_modules/.bin/concurrently "npm run start -- --configuration production" "node_modules/.bin/wait-on http-get://localhost:4200 && node_modules/.bin/cypress --spec $(./scripts/shard.sh ${TESTS_PATH} ${FILE_EXTENSION} ${CURRENT_SHARD} ${TOTAL_SHARDS})"  --kill-others --success first
+node_modules/.bin/concurrently "npm run start -- --configuration production" "node_modules/.bin/wait-on http-get://localhost:4200 && npm run cypress -- --spec $(./scripts/shard.sh ${TESTS_PATH} ${FILE_EXTENSION} ${CURRENT_SHARD} ${TOTAL_SHARDS})"  --kill-others --success first
