26b6c483280b58a5e6d20f9a3ac67c0b56b8606d
Liam DeBeasi <liamdebeasi@icloud.com>
2022-03-30 16:32:17 +0000

    chore(lint): install eslint

---
+++ core/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@ionic/eslint-config/recommended",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ]
+};
