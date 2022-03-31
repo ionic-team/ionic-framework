91530107841e162d6b6bd01bea88d93b9dac726b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-03-31 10:57:33 -0400

    test(playwright): add stencil utils, migrate first test (#24979)

--- core/src/components/button/test/basic/button.e2e.ts
+++ core/src/components/button/test/basic/button.e2e.ts
@@ -1,5 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { expect, describe } from '@playwright/test';
+import { test } from '@utils/test/playwright';
 
-test('this is a temporary test', () => {
-  expect(true).toEqual(true);
+test.describe('button: basic', () => {
+  test('should not have visual regressions', async ({ page }) => {
+    await page.goto(`/src/components/button/test/basic`);
+
+    await page.setIonViewport();
+
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`button-diff-${page.getSnapshotSettings()}.png`);
+  });
 });
