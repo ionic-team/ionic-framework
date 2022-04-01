75dffa0e1466710dbeba95f454bcb36e592aef9f
amandaesmith3 <amanda.s@ionic.io>
2022-04-01 10:46:11 -0500

    test(menu): add scroll position test

--- core/src/components/menu/test/basic/e2e.ts
+++ core/src/components/menu/test/basic/e2e.ts
@@ -34,6 +34,30 @@ test('menu: focus trap', async () => {
   expect(activeElID).toEqual('start-menu-button');
 });
 
+test('menu: preserve scroll position', async () => {
+  const page = await newE2EPage({ url: '/src/components/menu/test/basic?ionic:_testing=true' });
+
+  await page.click('#open-first');
+  const menu = await page.find('#start-menu');
+  await menu.waitForVisible();
+
+  await page.$eval('#start-menu ion-content', (menuContentEl: any) => {
+    return menuContentEl.scrollToPoint(0, 200);
+  });
+
+  await menu.callMethod('close');
+
+  await page.click('#open-first');
+  await menu.waitForVisible();
+  
+  const scrollTop = await page.$eval('#start-menu ion-content', async (menuContentEl: any) => {
+    const contentScrollEl = await menuContentEl.getScrollElement();
+    return contentScrollEl.scrollTop;
+  });
+
+  expect(scrollTop).toEqual(200);
+});
+
 /**
  * RTL Tests
  */
