e4c16ef8b832fb9b4fc169db437271c78e4e7f0f
Sean Perkins <sean@ionic.io>
2022-04-01 15:30:44 -0400

    test(): waitForSelector datetime ready

--- core/src/components/datetime/test/presentation/datetime.e2e.ts
+++ core/src/components/datetime/test/presentation/datetime.e2e.ts
@@ -8,8 +8,19 @@ test.describe('datetime: presentation', () => {
 
     await page.setIonViewport();
 
-    // Intersection observer delay for the .calendar-body element
-    await page.waitForTimeout(100);
+    /**
+     * Datetime will wait a frame to display the calendar body. Wait for all
+     * the test datetime components to be ready before taking a screenshot.
+     */
+    await Promise.all([
+      page.waitForSelector('ion-datetime[presentation="date-time"].datetime-ready'),
+      page.waitForSelector('ion-datetime[presentation="time-date"].datetime-ready'),
+      page.waitForSelector('ion-datetime[presentation="time"].datetime-ready'),
+      page.waitForSelector('ion-datetime[presentation="date"].datetime-ready'),
+      page.waitForSelector('ion-datetime[presentation="month-year"].datetime-ready'),
+      page.waitForSelector('ion-datetime[presentation="month"].datetime-ready'),
+      page.waitForSelector('ion-datetime[presentation="year"].datetime-ready')
+    ]);
 
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`datetime-presentation-diff-${page.getSnapshotSettings()}.png`);
   });
