e1c633b04092dbe82965ed91ecd5d3dc6e595484
Sean Perkins <sean@ionic.io>
2022-04-07 14:41:29 -0400

    chore(): expand playwright page viewport by mobile offset

--- core/src/utils/test/playwright/page/utils/set-ion-viewport.ts
+++ core/src/utils/test/playwright/page/utils/set-ion-viewport.ts
@@ -1,4 +1,43 @@
 import type { Page } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+const getDeviceForPage = (
+  page: Page
+): typeof devices[0] & {
+  screen: {
+    width: number;
+    height: number;
+  };
+} => {
+  const { userAgent } = (page.context() as any)._options;
+  const deviceName = Object.keys(devices).find((key) => devices[key].userAgent.includes(userAgent));
+  if (!deviceName) {
+    throw new Error(`Could not find device for user agent: ${userAgent}`);
+  }
+  return devices[deviceName] as any;
+};
+
+/**
+ * Playwright mobile devices have a smaller viewport size than their screen size.
+ * When taking screenshots, this can cause the screenshot to be cropped.
+ *
+ * https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/deviceDescriptorsSource.json#L578-L585
+ *
+ * We access Playwright's internal options to offset by the difference between
+ * the viewport size and the screen size.
+ */
+const getScreenshotDeviceOffset = (page: Page) => {
+  let viewportOffset = 0;
+
+  const pageContextOptions = (page.context() as any)._options;
+
+  if (pageContextOptions.isMobile) {
+    const device = getDeviceForPage(page);
+    viewportOffset = device.screen.height - pageContextOptions.viewport.height;
+  }
+
+  return viewportOffset;
+};
 
 /**
  * Taking fullpage screenshots in Playwright
@@ -24,7 +63,7 @@ export const setIonViewport = async (page: Page) => {
   });
 
   const width = currentViewport?.width ?? 640;
-  const height = (currentViewport?.height ?? 480) + pixelAmountRenderedOffscreen;
+  const height = (currentViewport?.height ?? 480) + pixelAmountRenderedOffscreen + getScreenshotDeviceOffset(page);
 
   await page.setViewportSize({
     width,
