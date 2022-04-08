da60b3fee13a3fd044070a7995441a7fb2f516ce
Liam DeBeasi <liamdebeasi@icloud.com>
2022-04-08 18:25:08 +0000

    test(): add parseDate tests

--- core/src/components/datetime/test/parse.spec.ts
+++ core/src/components/datetime/test/parse.spec.ts
@@ -1,4 +1,4 @@
-import { getPartsFromCalendarDay } from '../utils/parse';
+import { getPartsFromCalendarDay, parseDate } from '../utils/parse';
 
 describe('getPartsFromCalendarDay()', () => {
   it('should extract DatetimeParts from a calendar day element', () => {
@@ -17,4 +17,35 @@ describe('getPartsFromCalendarDay()', () => {
   });
 });
 
-// TODO: parseDate()
+describe('parseDate()', () => {
+  it('should correctly parse a valid ISO 8601 string', () => {
+    expect(parseDate('2022-01-01T00:00:00-00:00')).toEqual({
+      year: 2022,
+      month: 1,
+      day: 1,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: undefined,
+      tzOffset: -0,
+      ampm: 'am'
+    });
+  });
+  it('should correctly parse a custom timezone offset', () => {
+    expect(parseDate('2022-01-01T00:00:00-04:00')).toEqual({
+      year: 2022,
+      month: 1,
+      day: 1,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: undefined,
+      tzOffset: -240,
+      ampm: 'am'
+    });
+  });
+  it('should correctly determine AM/PM', () => {
+    expect(parseDate('2022-01-01T03:00:00').ampm).toEqual('am');
+    expect(parseDate('2022-01-01T13:00:00').ampm).toEqual('pm');
+  });
+});
