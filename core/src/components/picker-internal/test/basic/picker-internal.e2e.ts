7a7c2617e747aa80bae8ff8cf34472cc68297b93
Sean Perkins <sean@ionic.io>
2022-04-22 15:38:11 -0400

    chore(): skip based on user agent

--- core/src/components/picker-internal/test/basic/picker-internal.e2e.ts
+++ core/src/components/picker-internal/test/basic/picker-internal.e2e.ts
@@ -1,6 +1,9 @@
-import type { TestInfo } from '@playwright/test';
 import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
+
+const isMobileSafariLinux = (userAgent?: string) => {
+  return userAgent?.includes('Linux') === true && userAgent?.includes('Mobile Safari') === true;
+}
 
 test.describe('picker-internal', () => {
   test('inline pickers should not have visual regression', async ({ page }) => {
@@ -13,37 +16,38 @@ test.describe('picker-internal', () => {
     );
   });
 
-  test('popover picker should not have visual regression', async ({ page, userAgent }, testInfo: TestInfo) => {
-    test.skip(testInfo.project.name === 'Mobile Safari' && userAgent?.includes('Linux') === true, 'Mobile Safari on Linux renders the selected option incorrectly');
+  test.describe('within overlay:', () => {
+    test.skip(({ userAgent }) => isMobileSafariLinux(userAgent), 'Mobile Safari on Linux renders the selected option incorrectly');
 
-    await page.goto(`/src/components/picker-internal/test/basic`);
+    test('popover: should not have visual regression', async ({ page }) => {
+      await page.goto(`/src/components/picker-internal/test/basic`);
 
-    await page.setIonViewport();
+      await page.setIonViewport();
 
-    await page.click('#popover');
+      await page.click('#popover');
 
-    await page.spyOnEvent('ionPopoverDidPresent');
-    // Accounts for slight delay in showing the popover
-    await page.waitForTimeout(100);
+      await page.spyOnEvent('ionPopoverDidPresent');
+      // Accounts for slight delay in showing the popover
+      await page.waitForTimeout(100);
 
-    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
-      `picker-internal-popover-diff-${page.getSnapshotSettings()}.png`
-    );
-  });
+      expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
+        `picker-internal-popover-diff-${page.getSnapshotSettings()}.png`
+      );
+    });
 
-  test('modal picker should not have visual regression', async ({ page, userAgent }, testInfo: TestInfo) => {
-    test.skip(testInfo.project.name === 'Mobile Safari' && userAgent?.includes('Linux') === true, 'Mobile Safari on Linux renders the selected option incorrectly');
+    test('modal: should not have visual regression', async ({ page }) => {
+      await page.goto(`/src/components/picker-internal/test/basic`);
 
-    await page.goto(`/src/components/picker-internal/test/basic`);
+      await page.setIonViewport();
 
-    await page.setIonViewport();
+      await page.click('#modal');
 
-    await page.click('#modal');
+      await page.spyOnEvent('ionModalDidPresent');
 
-    await page.spyOnEvent('ionModalDidPresent');
+      expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
+        `picker-internal-modal-diff-${page.getSnapshotSettings()}.png`
+      );
+    });
+  })
 
-    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
-      `picker-internal-modal-diff-${page.getSnapshotSettings()}.png`
-    );
-  });
 });
