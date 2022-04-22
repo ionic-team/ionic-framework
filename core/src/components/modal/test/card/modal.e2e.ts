0053e0b42d2f25c9a0df599a5cf409eb6b6dcd4d
Liam DeBeasi <liamdebeasi@icloud.com>
2022-04-22 17:04:11 +0000

    test(modal): add card swiping tests

--- core/src/components/modal/test/card/modal.e2e.ts
+++ core/src/components/modal/test/card/modal.e2e.ts
@@ -1,51 +1,97 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { dragElementBy, test } from '@utils/test/playwright';
 
-test.describe('card modal: rendering', () => {
-  test('should not have visual regressions', async ({ page }) => {
+test.describe('card modal', () => {
+  test.beforeEach(async ({ page }, testInfo) => {
     await page.goto('/src/components/modal/test/card');
-    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
 
-    await page.click('#card');
+    test.skip(testInfo.project.metadata.mode !== 'ios', 'Card style modal is only available on iOS');
+  })
+  test.describe('card modal: rendering', () => {
+    test('should not have visual regressions', async ({ page }) => {
+      const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
 
-    await ionModalDidPresent.next();
+      await page.click('#card');
 
-    expect(await page.screenshot()).toMatchSnapshot(`modal-card-present-${page.getSnapshotSettings()}.png`);
+      await ionModalDidPresent.next();
+
+      expect(await page.screenshot()).toMatchSnapshot(`modal-card-present-${page.getSnapshotSettings()}.png`);
+    });
+    test('should not have visual regressions with custom modal', async ({ page }) => {
+      const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+
+      await page.click('#card-custom');
+
+      await ionModalDidPresent.next();
+
+      expect(await page.screenshot()).toMatchSnapshot(`modal-card-custom-present-${page.getSnapshotSettings()}.png`);
+    });
+    test('should not have visual regressions with stacked cards', async ({ page }) => {
+      const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+
+      await page.click('#card');
+      await ionModalDidPresent.next();
+
+      await page.click('.add');
+      await ionModalDidPresent.next();
+
+      expect(await page.screenshot()).toMatchSnapshot(`modal-card-stacked-present-${page.getSnapshotSettings()}.png`);
+    });
+    test('should not have visual regressions with stacked custom cards', async ({ page }) => {
+      const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+
+      await page.click('#card-custom');
+      await ionModalDidPresent.next();
+
+      await page.click('.add');
+      await ionModalDidPresent.next();
+
+      expect(await page.screenshot()).toMatchSnapshot(
+        `modal-card-custom-stacked-present-${page.getSnapshotSettings()}.png`
+      );
+    });
   });
-  test('should not have visual regressions with custom modal', async ({ page }) => {
-    await page.goto('/src/components/modal/test/card');
-    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+  test.describe.only('card modal: swipe to close', () => {
+    test('it should swipe to close when swiped on the header', async ({ page }) => {
+      const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+      const ionModalDidDismiss = await page.spyOnEvent('ionModalDidDismiss');
 
-    await page.click('#card-custom');
+      await page.click('#card');
+      await ionModalDidPresent.next();
 
-    await ionModalDidPresent.next();
+      const header = await page.locator('ion-modal ion-header');
+      await dragElementBy(header, page, 0, 500);
 
-    expect(await page.screenshot()).toMatchSnapshot(`modal-card-custom-present-${page.getSnapshotSettings()}.png`);
-  });
-  test('should not have visual regressions with stacked cards', async ({ page }) => {
-    await page.goto('/src/components/modal/test/card');
-    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+      await ionModalDidDismiss.next();
+    });
+    test('it should swipe to close when swiped on the content', async ({ page }) => {
+      const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+      const ionModalDidDismiss = await page.spyOnEvent('ionModalDidDismiss');
 
-    await page.click('#card');
-    await ionModalDidPresent.next();
+      await page.click('#card');
+      await ionModalDidPresent.next();
 
-    await page.click('.add');
-    await ionModalDidPresent.next();
+      const content = await page.locator('ion-modal ion-content');
+      await dragElementBy(content, page, 0, 500);
 
-    expect(await page.screenshot()).toMatchSnapshot(`modal-card-stacked-present-${page.getSnapshotSettings()}.png`);
-  });
-  test('should not have visual regressions with stacked custom cards', async ({ page }) => {
-    await page.goto('/src/components/modal/test/card');
-    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+      await ionModalDidDismiss.next();
+    });
+    test('it should not swipe to close when swiped on the content but the content is scrolled', async ({ page }) => {
+      const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
 
-    await page.click('#card-custom');
-    await ionModalDidPresent.next();
+      await page.click('#card');
+      await ionModalDidPresent.next();
 
-    await page.click('.add');
-    await ionModalDidPresent.next();
+      const modal = await page.locator('ion-modal');
+      const content = (await page.$('ion-modal ion-content'))!;
 
-    expect(await page.screenshot()).toMatchSnapshot(
-      `modal-card-custom-stacked-present-${page.getSnapshotSettings()}.png`
-    );
+      await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom(0));
+
+      await dragElementBy(content, page, 0, 500);
+
+      await content.waitForElementState('stable');
+
+      expect(modal).toBeVisible();
+    });
   });
 });
