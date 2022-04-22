dde6bc76954ad8f017bc860bac87323e7ef2ecde
Sean Perkins <sean@ionic.io>
2022-04-22 14:30:14 -0400

    chore(): prettier

--- core/src/components/select/test/basic/select.e2e.ts
+++ core/src/components/select/test/basic/select.e2e.ts
@@ -1,29 +1,21 @@
-
 import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('select: basic', () => {
-
   test('should not open multiple alert windows when clicked multiple times', async ({ page }) => {
-
     test.info().annotations.push({
       type: 'issue',
-      description: 'https://github.com/ionic-team/ionic-framework/issues/25126'
+      description: 'https://github.com/ionic-team/ionic-framework/issues/25126',
     });
 
     await page.goto(`/src/components/select/test/basic`);
 
     const genderSelect = page.locator('#gender');
 
-    await Promise.all([
-      genderSelect.click(),
-      genderSelect.click(),
-      genderSelect.click()
-    ]);
+    await Promise.all([genderSelect.click(), genderSelect.click(), genderSelect.click()]);
 
     const alerts = await page.$$('ion-alert');
 
     expect(alerts.length).toBe(1);
   });
-
 });
