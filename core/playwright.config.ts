f671195757bd6b578853537e4650afde753a096e
Liam DeBeasi <liamdebeasi@icloud.com>
2022-04-26 17:31:12 +0000

    chore(): testing parallel tests on CI

--- core/playwright.config.ts
+++ core/playwright.config.ts
@@ -87,7 +87,7 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
