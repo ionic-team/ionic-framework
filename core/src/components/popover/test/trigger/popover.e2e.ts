b8939faddcdd95bf0f0e25b9e6a44faf0fea8edd
amandaesmith3 <amanda.s@ionic.io>
2022-04-26 11:11:55 -0500

    test(popover): replace unnecessary screenshots with visibility checks

--- core/src/components/popover/test/trigger/popover.e2e.ts
+++ core/src/components/popover/test/trigger/popover.e2e.ts
@@ -1,43 +1,40 @@
 import { expect } from '@playwright/test';
+import type { E2EPage } from '@utils/test/playwright';
 import { test } from '@utils/test/playwright';
 
-import { openPopover, screenshotPopover } from '../test.utils';
+import { openPopover } from '../test.utils';
 
 test.describe('popover: trigger', async () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/src/components/popover/test/trigger');
+  });
+
   test('should open popover by left clicking on trigger', async ({ page }) => {
-    await screenshotPopover(page, 'left-click-trigger', 'trigger');
+    await openPopover(page, 'left-click-trigger');
+    await checkPopover(page, '.left-click-popover');
   });
 
   test('should open popover by right clicking on trigger', async ({ page }) => {
-    await page.goto('/src/components/popover/test/trigger');
     const ionPopoverDidPresent = await page.spyOnEvent('ionPopoverDidPresent');
 
     await page.click('#right-click-trigger', { button: 'right' });
     await ionPopoverDidPresent.next();
 
-    await page.setIonViewport();
-    expect(await page.screenshot()).toMatchSnapshot(`popover-trigger-right-click-trigger-${page.getSnapshotSettings()}.png`);
+    await checkPopover(page, '.right-click-popover');
   });
 
   test('should open popover by hovering over trigger', async ({ page }) => {
-    await page.goto('/src/components/popover/test/trigger');
     const ionPopoverDidPresent = await page.spyOnEvent('ionPopoverDidPresent');
 
     const button = page.locator('#hover-trigger');
     await button.hover();
     await ionPopoverDidPresent.next();
 
-    await page.setIonViewport();
-    expect(await page.screenshot()).toMatchSnapshot(`popover-trigger-hover-trigger-${page.getSnapshotSettings()}.png`);
-  });
-
-  test('should not close main popover with dismiss-on-select when clicking a trigger', async ({ page }) => {
-    await page.goto('/src/components/popover/test/trigger');
-
-    await openPopover(page, 'nested-click-trigger');
-    await openPopover(page, 'nested-click-trigger-two');
-
-    await page.setIonViewport();
-    expect(await page.screenshot()).toMatchSnapshot(`popover-trigger-nested-click-trigger-${page.getSnapshotSettings()}.png`);
+    await checkPopover(page, '.hover-popover');
   });
 });
+
+const checkPopover = async (page: E2EPage, popoverSelector: string) => {
+  const popover = page.locator(popoverSelector);
+  expect(popover).toBeVisible();
+};
