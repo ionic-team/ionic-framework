67ddf56ad70b82941a2f2c5cc0966d13dc6ef981
Liam DeBeasi <liamdebeasi@icloud.com>
2022-04-27 17:24:30 +0000

    test(vue): add more tests

--- packages/vue/test-app/tests/e2e/specs/hbb.js
+++ packages/vue/test-app/tests/e2e/specs/hbb.js
@@ -28,4 +28,69 @@ describe('Hardware Back Button', () => {
     cy.ionPageHidden('tab2');
     cy.ionPageVisible('tab1');
   });
+
+  it('should correctly go back to the root tab from child pages', () => {
+    cy.visit('http://localhost:8080');
+
+    cy.routerPush('/tabs');
+    cy.ionPageHidden('home');
+    cy.ionPageVisible('tab1');
+
+    cy.routerPush('/tabs/tab1/childone');
+    cy.ionPageHidden('tab1');
+    cy.ionPageVisible('tab1childone');
+
+    cy.routerPush('/tabs/tab1/childtwo');
+    cy.ionPageHidden('tab1childone');
+    cy.ionPageVisible('tab1childtwo');
+
+    cy.hardwareBackButton();
+    cy.ionPageDoesNotExist('tab1childtwo');
+    cy.ionPageVisible('tab1childone');
+
+    cy.hardwareBackButton();
+    cy.ionPageDoesNotExist('tab1childone');
+    cy.ionPageVisible('tab1');
+
+    cy.hardwareBackButton();
+    cy.ionPageDoesNotExist('tab1');
+    cy.ionPageVisible('home');
+  });
+
+  // TODO FW-1389
+  it.skip('should correctly go back to the root tab after switching pages', () => {
+    cy.visit('http://localhost:8080');
+
+    cy.routerPush('/tabs');
+    cy.ionPageHidden('home');
+    cy.ionPageVisible('tab1');
+
+    cy.routerPush('/tabs/tab1/childone');
+    cy.ionPageHidden('tab1');
+    cy.ionPageVisible('tab1childone');
+
+    cy.routerPush('/tabs/tab1/childtwo');
+    cy.ionPageHidden('tab1childone');
+    cy.ionPageVisible('tab1childtwo');
+
+    cy.get('ion-tab-button#tab-button-tab2').click();
+    cy.ionPageHidden('tab1childtwo');
+    cy.ionPageVisible('tab2');
+
+    cy.get('ion-tab-button#tab-button-tab1').click();
+    cy.ionPageHidden('tab2');
+    cy.ionPageVisible('tab1childtwo');
+
+    cy.hardwareBackButton();
+    cy.ionPageDoesNotExist('tab1childtwo');
+    cy.ionPageVisible('tab1childone');
+
+    cy.hardwareBackButton();
+    cy.ionPageDoesNotExist('tab1childone');
+    cy.ionPageVisible('tab1');
+
+    cy.hardwareBackButton();
+    cy.ionPageDoesNotExist('tab1');
+    cy.ionPageVisible('home');
+  });
 })
