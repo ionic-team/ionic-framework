ce2f8121be840fa4db60d467c05e4628ec4b2760
Liam DeBeasi <liamdebeasi@icloud.com>
2022-05-02 14:35:47 +0000

    test(menu): add standalone test

---
+++ core/src/components/menu/test/standalone/menu.e2e.ts
@@ -0,0 +1,18 @@
+import { test } from '@utils/test/playwright';
+
+test.describe('menu - ensure component works without ion-app', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/src/components/menu/test/standalone');
+  });
+  test('it should correctly open a menu', async ({ page }) => {
+    const ionDidOpen = await page.spyOnEvent('ionDidOpen');
+    const ionDidClose = await page.spyOnEvent('ionDidClose');
+
+    const menu = await page.locator('#start-menu');
+    await menu.evaluate((el: HTMLIonMenuElement) => el.open());
+    await ionDidOpen.next();
+
+    await menu.evaluate((el: HTMLIonMenuElement) => el.close());
+    await ionDidClose.next();
+  });
+})
