d10f94805772eb45ee89f5d57749e692bd3d32d6
Liam DeBeasi <liamdebeasi@icloud.com>
2022-05-03 16:42:53 +0000

    test(modal): simplify custom modal e2e test

--- core/src/components/modal/test/custom/modal.e2e.ts
+++ core/src/components/modal/test/custom/modal.e2e.ts
@@ -1,8 +1,9 @@
 import { expect } from '@playwright/test';
 import { test, Viewports } from '@utils/test/playwright';
+import type { E2EPage } from '@utils/test/playwright';
 
 test.describe('modal: custom rendering', () => {
-  test('should not have visual regressions', async ({ page }) => {
+  const runVisualTests = async (page: E2EPage, screenshotModifier: string = '') => {
     await page.goto('/src/components/modal/test/custom');
     const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
 
@@ -12,20 +13,14 @@ test.describe('modal: custom rendering', () => {
 
     await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`modal-custom-present-${page.getSnapshotSettings()}.png`);
+    expect(await page.screenshot()).toMatchSnapshot(`modal-custom-present-${screenshotModifier}${page.getSnapshotSettings()}.png`);
+  }
+  test('should not have visual regressions', async ({ page }) => {
+    await runVisualTests(page);
   });
 
   test('should not have visual regressions with tablet viewport', async ({ page }) => {
     await page.setViewportSize(Viewports.tablet.portrait);
-    await page.goto('/src/components/modal/test/custom');
-    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
-
-    await page.click('#custom-modal');
-
-    await ionModalDidPresent.next();
-
-    await page.setIonViewport();
-
-    expect(await page.screenshot()).toMatchSnapshot(`modal-custom-present-tablet-${page.getSnapshotSettings()}.png`);
+    await runVisualTests(page, 'tablet-');
   });
 });
