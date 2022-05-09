2196bab99ad6242614f7124394c416077f9998a9
Liam DeBeasi <liamdebeasi@icloud.com>
2022-05-09 13:10:02 +0000

    test(loading): migrate standalone tests

---
+++ core/src/components/loading/test/standalone/loading.e2e.ts
@@ -0,0 +1,27 @@
+import { test } from '@utils/test/playwright';
+
+import { expect } from '@playwright/test';
+
+
+test.describe('loading: standalone', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/src/components/loading/test/standalone');
+  });
+  test('should open a basic loader', async ({ page }) => {
+    const ionLoadingDidPresent = await page.spyOnEvent('ionLoadingDidPresent');
+    const ionLoadingDidDismiss = await page.spyOnEvent('ionLoadingDidPresent');
+
+    await page.click('#basic-loading');
+
+    await ionLoadingDidPresent.next();
+
+    expect(await page.screenshot()).toMatchSnapshot(`loading-standalone-diff-${page.getSnapshotSettings()}.png`);
+
+    const loading = await page.locator('ion-loading');
+    await loading.evaluate((el: HTMLIonLoadingElement) => el.dismiss());
+
+    await ionLoadingDidDismiss.next();
+
+    expect(loading).toBeHidden();
+  });
+});
