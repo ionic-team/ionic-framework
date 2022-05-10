b6d72ed670f30d5484e8e10a557d3660f95ef672
Liam DeBeasi <liamdebeasi@icloud.com>
2022-05-10 16:26:02 +0000

    test(sekect): skip async rtl test

--- core/src/components/select/test/compare-with/select.e2e.ts
+++ core/src/components/select/test/compare-with/select.e2e.ts
@@ -2,7 +2,9 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('select: compare-with', () => {
-  test('should correctly set value when using compareWith property', async ({ page }) => {
+  test('should correctly set value when using compareWith property', async ({ page }, testInfo) => {
+    test.skip(testInfo.project.metadata.rtl === true, 'This is checking internal logic. RTL tests are not needed');
+
     await page.goto('/src/components/select/test/compare-with');
 
     const multipleSelect = await page.locator('#multiple');
