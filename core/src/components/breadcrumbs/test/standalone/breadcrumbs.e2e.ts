127baf61c17acb190ac80e7547be0b4b00fe1e6d
amandaesmith3 <amanda.s@ionic.io>
2022-05-10 12:42:01 -0500

    test(breadcrumbs): run standalone test at tablet size

--- core/src/components/breadcrumbs/test/standalone/breadcrumbs.e2e.ts
+++ core/src/components/breadcrumbs/test/standalone/breadcrumbs.e2e.ts
@@ -1,11 +1,12 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, Viewports } from '@utils/test/playwright';
 
 test.describe('breadcrumbs: standalone', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto(`/src/components/breadcrumbs/test/standalone`);
 
-    await page.setIonViewport();
+    // avoid contents getting cut off
+    await page.setViewportSize(Viewports.tablet.portrait);
 
     expect(await page.screenshot()).toMatchSnapshot(`breadcrumb-standalone-diff-${page.getSnapshotSettings()}.png`);
   });
