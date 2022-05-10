4afb07526265416d8fa933182c3d2a7a959ae7d9
Liam DeBeasi <liamdebeasi@icloud.com>
2022-05-10 16:16:52 +0000

    test(select): migrate compareWith test

---
+++ core/src/components/select/test/compare-with/select.e2e.ts
@@ -0,0 +1,20 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('select: compare-with', () => {
+  test('should correctly set value when using compareWith property', async ({ page }) => {
+    await page.goto('/src/components/select/test/compare-with');
+
+    const multipleSelect = await page.locator('#multiple');
+    const singleSelect = await page.locator('#single');
+
+    expect(multipleSelect).toHaveJSProperty('value', [{
+      label: 'selected by default',
+      value: '1'
+    }]);
+    expect(singleSelect).toHaveJSProperty('value', {
+      label: 'selected by default',
+      value: '1'
+    });
+  });
+});
