b6bedd29c60a2b29ad031a6cca61d5a8200e5ca4
amandaesmith3 <amanda.s@ionic.io>
2022-05-11 10:48:46 -0500

    test(breadcrumbs): force fullscreen on standalone instead of using tablet size

--- core/src/components/breadcrumbs/test/standalone/breadcrumbs.e2e.ts
+++ core/src/components/breadcrumbs/test/standalone/breadcrumbs.e2e.ts
@@ -1,13 +1,10 @@
 import { expect } from '@playwright/test';
-import { test, Viewports } from '@utils/test/playwright';
+import { test } from '@utils/test/playwright';
 
 test.describe('breadcrumbs: standalone', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto(`/src/components/breadcrumbs/test/standalone`);
 
-    // avoid contents getting cut off
-    await page.setViewportSize(Viewports.tablet.portrait);
-
-    expect(await page.screenshot()).toMatchSnapshot(`breadcrumb-standalone-diff-${page.getSnapshotSettings()}.png`);
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`breadcrumb-standalone-diff-${page.getSnapshotSettings()}.png`);
   });
 });
