4ea2e47bd26c321e933dcdb348412b283779ae09
amandaesmith3 <amanda.s@ionic.io>
2022-05-11 11:24:51 -0500

    chore(): lint

--- core/src/components/breadcrumbs/test/standalone/breadcrumbs.e2e.ts
+++ core/src/components/breadcrumbs/test/standalone/breadcrumbs.e2e.ts
@@ -5,6 +5,8 @@ test.describe('breadcrumbs: standalone', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto(`/src/components/breadcrumbs/test/standalone`);
 
-    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`breadcrumb-standalone-diff-${page.getSnapshotSettings()}.png`);
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
+      `breadcrumb-standalone-diff-${page.getSnapshotSettings()}.png`
+    );
   });
 });
