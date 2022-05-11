8a39b6f06a1b1ec60aa47706c076007961db8c7d
Liam DeBeasi <liamdebeasi@icloud.com>
2022-05-11 15:32:43 +0000

    test(select): add standalone test

---
+++ core/src/components/select/test/standalone/select.e2e.ts
@@ -0,0 +1,19 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('select: standalone', () => {
+  test('should open an overlay without ion-app', async ({ page }) => {
+    await page.goto(`/src/components/select/test/standalone`);
+    const ionAlertDidPresent = await page.spyOnEvent('ionAlertDidPresent');
+    const ionAlertDidDismiss = await page.spyOnEvent('ionAlertDidDismiss');
+
+    await page.click('#gender');
+
+    await ionAlertDidPresent.next();
+
+    const alert = await page.locator('ion-alert');
+    await alert.evaluate((el: HTMLIonAlertElement) => alert.dismiss());
+
+    await ionAlertDidDismiss.next();
+  });
+});
