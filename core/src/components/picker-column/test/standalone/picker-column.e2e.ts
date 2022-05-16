8796d3375331838a6b559ded3824d15b01919b56
Sean Perkins <sean@ionic.io>
2022-05-16 15:03:18 -0400

    chore(): clean-up

--- core/src/components/picker-column/test/standalone/picker-column.e2e.ts
+++ core/src/components/picker-column/test/standalone/picker-column.e2e.ts
@@ -3,25 +3,20 @@ import { test } from '@utils/test/playwright';
 import { testPickerColumn } from '../test.utils';
 
 test.describe('picker-column', () => {
-
   test.beforeEach(async ({ page }) => {
     await page.goto('/src/components/picker-column/test/standalone');
   });
 
   test.describe('single column', () => {
-
     test('should not have any visual regressions', async ({ page }) => {
       testPickerColumn(page, '#single-column-button', 'single');
     });
-
   });
 
   test.describe('multiple columns', () => {
-
     test('should not have any visual regressions', async ({ page }) => {
       testPickerColumn(page, '#multiple-column-button', 'multiple');
     });
-
   });
 
 });
--- core/src/components/picker-column/test/test.utils.ts
+++ core/src/components/picker-column/test/test.utils.ts
@@ -2,10 +2,16 @@ import { expect } from '@playwright/test';
 import type { E2EPage } from '@utils/test/playwright';
 import { dragElementBy } from '@utils/test/playwright';
 
-export async function testPickerColumn(page: E2EPage, selector: string, description: string) {
+/**
+ * Visual regression tests for picker-column.
+ * @param page - The page to run the tests on.
+ * @param buttonSelector - The selector for the button that opens the picker.
+ * @param description - The description to amend to the screenshot names (typically 'single' or 'multiple').
+ */
+export async function testPickerColumn(page: E2EPage, buttonSelector: string, description: string) {
   const ionPickerDidPresentSpy = await page.spyOnEvent('ionPickerDidPresent');
 
-  await page.click(selector);
+  await page.click(buttonSelector);
   await ionPickerDidPresentSpy.next();
 
   await page.waitForTimeout(100);
