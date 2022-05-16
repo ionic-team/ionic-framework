764da2394acfed3b067e7454a38fca134e82c14b
Sean Perkins <sean@ionic.io>
2022-05-16 15:40:07 -0400

    chore(): adjust tests

--- core/src/components/picker-column/test/standalone/picker-column.e2e.ts
+++ core/src/components/picker-column/test/standalone/picker-column.e2e.ts
@@ -3,18 +3,16 @@ import { test } from '@utils/test/playwright';
 import { testPickerColumn } from '../test.utils';
 
 test.describe('picker-column', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/picker-column/test/standalone');
-  });
-
   test.describe('single column', () => {
     test('should not have any visual regressions', async ({ page }) => {
+      await page.goto('/src/components/picker-column/test/standalone');
       testPickerColumn(page, '#single-column-button', 'single');
     });
   });
 
   test.describe('multiple columns', () => {
     test('should not have any visual regressions', async ({ page }) => {
+      await page.goto('/src/components/picker-column/test/standalone');
       testPickerColumn(page, '#multiple-column-button', 'multiple');
     });
   });
