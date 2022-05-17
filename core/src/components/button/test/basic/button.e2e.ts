a2602c9bc21be6bca5b92b3afe382a8a1d22ff89
Sean Perkins <sean@ionic.io>
2022-05-16 22:28:45 -0400

    test(button): ripple effect screenshot tests

--- core/src/components/button/test/basic/button.e2e.ts
+++ core/src/components/button/test/basic/button.e2e.ts
@@ -10,3 +10,28 @@ test.describe('button: basic', () => {
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`button-diff-${page.getSnapshotSettings()}.png`);
   });
 });
+
+test.describe('button: ripple effect', () => {
+  test('should not have visual regressions', async ({ page }, testInfo) => {
+    test.skip(testInfo.project.metadata.mode !== 'md', 'Ripple effect is only available in md mode');
+
+    await page.setContent(`
+      <ion-button>Button</ion-button>
+      <ion-button id="rippleBtn">Button with Ripple</ion-button>
+    `);
+
+    const button = page.locator('#rippleBtn');
+    const boundingBox = await button.boundingBox();
+
+    if (boundingBox) {
+      await page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2);
+    }
+
+    await page.mouse.down();
+    await page.waitForTimeout(100);
+
+    expect(await page.screenshot({ animations: 'disabled' })).toMatchSnapshot(
+      `button-ripple-effect-${page.getSnapshotSettings()}.png`
+    );
+  });
+});
