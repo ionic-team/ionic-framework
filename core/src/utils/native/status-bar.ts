2aae26850f594063e3029204131fd2ce0f266938
Liam DeBeasi <liamdebeasi@icloud.com>
2022-05-26 19:16:25 +0000

    chore(): use ssr safe window

--- core/src/utils/native/status-bar.ts
+++ core/src/utils/native/status-bar.ts
@@ -1,3 +1,5 @@
+import { win } from '../window';
+
 interface StyleOptions {
   style: Style;
 }
@@ -10,19 +12,16 @@ export enum Style {
 
 export const StatusBar = {
   getEngine() {
-    const win = window as any;
-    return win.Capacitor?.isPluginAvailable('StatusBar') && win.Capacitor.Plugins.StatusBar;
+    return (win as any)?.Capacitor?.isPluginAvailable('StatusBar') && (win as any)?.Capacitor.Plugins.StatusBar;
   },
   supportsDefaultStatusBarStyle() {
-    const win = window as any;
-
     /**
      * The 'DEFAULT' status bar style was added
      * to the @capacitor/status-bar plugin in Capacitor 3.
      * PluginHeaders is only supported in Capacitor 3+,
      * so we can use this to detect Capacitor 3.
      */
-    return !!win.Capacitor?.PluginHeaders;
+    return !!(win as any)?.Capacitor?.PluginHeaders;
   },
   setStyle(options: StyleOptions) {
     const engine = this.getEngine();
