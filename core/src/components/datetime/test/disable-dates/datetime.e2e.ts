16a8d4dc024afccf6d523372e6241b40a6d6cda4
Liam DeBeasi <liamdebeasi@icloud.com>
2022-06-01 17:06:41 +0000

    chore(): lint

--- core/src/components/datetime/test/disable-dates/datetime.e2e.ts
+++ core/src/components/datetime/test/disable-dates/datetime.e2e.ts
@@ -6,13 +6,18 @@ const DISABLED_CALENDAR_DAY_SELECTOR = '.calendar-day[disabled]:not(.calendar-da
 
 const queryAllDisabledDays = (page: E2EPage, datetimeSelector = 'ion-datetime') => {
   return page.locator(`${datetimeSelector} ${DISABLED_CALENDAR_DAY_SELECTOR}`);
-}
+};
 
 const queryAllWorkingMonthDisabledDays = (page: E2EPage, datetimeSelector = 'ion-datetime') => {
   return page.locator(`${datetimeSelector} .calendar-month:nth-child(2) ${DISABLED_CALENDAR_DAY_SELECTOR}`);
-}
+};
 
 test.describe('datetime: disable dates', () => {
+  /**
+   * We need to access testInfo, but Playwright
+   * requires that we destructure the first parameter.
+   */
+  // eslint-disable-next-line no-empty-pattern
   test.beforeEach(({}, testInfo) => {
     test.skip(testInfo.project.metadata.rtl === true, 'These tests do not check layout rendering functionality.');
   });
@@ -24,23 +29,23 @@ test.describe('datetime: disable dates', () => {
     test.describe('when isDateEnabled returns true', () => {
       test('calendar days should be enabled', async ({ page }) => {
         const datetime = page.locator('ion-datetime');
-        await datetime.evaluate((el: HTMLIonDatetimeElement) => el.isDateEnabled = () => true);
+        await datetime.evaluate((el: HTMLIonDatetimeElement) => (el.isDateEnabled = () => true));
         await page.waitForChanges();
 
         const disabledDays = queryAllDisabledDays(page);
         expect(await disabledDays.count()).toBe(0);
       });
-    })
+    });
     test.describe('when isDateEnabled returns false', () => {
       test('calendar days should be disabled', async ({ page }) => {
         const datetime = page.locator('ion-datetime');
-        await datetime.evaluate((el: HTMLIonDatetimeElement) => el.isDateEnabled = () => false);
+        await datetime.evaluate((el: HTMLIonDatetimeElement) => (el.isDateEnabled = () => false));
         await page.waitForChanges();
 
         const disabledDays = queryAllDisabledDays(page);
         expect(await disabledDays.count()).toBe(91);
       });
-    })
+    });
     test.describe('when isDateEnabled returns undefined', () => {
       test('calendar days should be disabled', async ({ page }) => {
         const datetime = page.locator('ion-datetime');
@@ -51,15 +56,17 @@ test.describe('datetime: disable dates', () => {
            * values which is why we do the ts-ignore.
            */
           // @ts-ignore
-          el.isDateEnabled = () => { undefined }
+          el.isDateEnabled = () => {
+            undefined;
+          };
         });
 
         await page.waitForChanges();
 
         const disabledDays = queryAllDisabledDays(page);
         expect(await disabledDays.count()).toBe(91);
-      })
-    })
+      });
+    });
     test.describe('when isDateEnabled returns null', () => {
       test('calendar days should be disabled', async ({ page }) => {
         const datetime = page.locator('ion-datetime');
@@ -70,15 +77,15 @@ test.describe('datetime: disable dates', () => {
            * values which is why we do the ts-ignore.
            */
           // @ts-ignore
-          el.isDateEnabled = () => null
+          el.isDateEnabled = () => null;
         });
 
         await page.waitForChanges();
 
         const disabledDays = queryAllDisabledDays(page);
         expect(await disabledDays.count()).toBe(91);
-      })
-    })
+      });
+    });
   });
   test.describe('check example usages', () => {
     test.beforeEach(async ({ page }) => {
@@ -110,7 +117,7 @@ test.describe('datetime: disable dates', () => {
 
       expect(await disabledDays.count()).toBe(31);
     });
-  })
+  });
   test.describe('with a min date range', () => {
     test('should not enable already disabled dates', async ({ page }) => {
       await page.setContent(`
@@ -125,7 +132,7 @@ test.describe('datetime: disable dates', () => {
 
       expect(await disabledDays.count()).toBe(14);
     });
-  })
+  });
   test.describe('with a max date range', () => {
     test('should not enable already disabled dates', async ({ page }) => {
       await page.setContent(`
@@ -140,5 +147,5 @@ test.describe('datetime: disable dates', () => {
 
       expect(await disabledDays.count()).toBe(16);
     });
-  })
+  });
 });
