4d16314f9fb51239ee8232b24d9b1aeec4f93828
Liam DeBeasi <liamdebeasi@icloud.com>
2022-06-03 17:47:09 +0000

    chore(): improve reliability of month-year-picker tests

--- core/src/components/datetime/test/month-year-picker/datetime.e2e.ts
+++ core/src/components/datetime/test/month-year-picker/datetime.e2e.ts
@@ -4,6 +4,7 @@ import { test } from '@utils/test/playwright';
 test.describe('datetime: month-year picker', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/src/components/datetime/test/month-year-picker');
+    await page.waitForSelector('.datetime-ready');
   });
 
   test('should hide the footer when picker is open', async ({ page }) => {
