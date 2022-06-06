6e599b31b68a7d6af2edd99b7a51241c85c1b467
Liam DeBeasi <liamdebeasi@icloud.com>
2022-06-06 19:45:39 +0000

    chore(): improve display tests

--- core/src/components/datetime/test/display/datetime.e2e.ts
+++ core/src/components/datetime/test/display/datetime.e2e.ts
@@ -3,86 +3,93 @@ import { test } from '@utils/test/playwright';
 
 test.describe('datetime: display', () => {
   test.describe('datetime: rendering', () => {
-    test('fixed size should not have visual regressions', async ({ page }) => {
-      await page.goto('/src/components/datetime/test/display');
-
-      const select = page.locator('select#presentation');
-      const datetime = page.locator('ion-datetime');
-
-      select.selectOption('date-time');
-      await page.waitForChanges();
-
-      expect(await datetime.screenshot()).toMatchSnapshot(
-        `datetime-display-date-time-${page.getSnapshotSettings()}.png`
-      );
-
-      select.selectOption('time-date');
-      await page.waitForChanges();
-
-      expect(await datetime.screenshot()).toMatchSnapshot(
-        `datetime-display-time-date-${page.getSnapshotSettings()}.png`
-      );
-
-      select.selectOption('time');
-      await page.waitForChanges();
-
-      expect(await datetime.screenshot()).toMatchSnapshot(`datetime-display-time-${page.getSnapshotSettings()}.png`);
-
-      select.selectOption('date');
-      await page.waitForChanges();
-
-      expect(await datetime.screenshot()).toMatchSnapshot(`datetime-display-date-${page.getSnapshotSettings()}.png`);
-    });
-
-    test('cover size should not have visual regressions', async ({ page }) => {
-      /**
-       * We need to take a screenshot of the entire page
-       * here as we want to test that the datetime fills
-       * the entire screen.
-       */
-      await page.setViewportSize({ width: 500, height: 500 });
-      await page.goto('/src/components/datetime/test/display');
-      await page.waitForSelector('.datetime-ready');
-      const changeEvent = await page.spyOnEvent('change');
-
-      const select = page.locator('select#presentation');
-      const sizeSelect = page.locator('select#size');
-
-      sizeSelect.selectOption('cover');
-      await changeEvent.next();
-
-      select.selectOption('date-time');
-      await changeEvent.next();
-
-      await page.waitForChanges();
-
-      expect(await page.screenshot()).toMatchSnapshot(
-        `datetime-display-cover-date-time-${page.getSnapshotSettings()}.png`
-      );
-
-      select.selectOption('time-date');
-      await changeEvent.next();
-      await page.waitForChanges();
-
-      expect(await page.screenshot()).toMatchSnapshot(
-        `datetime-display-cover-time-date-${page.getSnapshotSettings()}.png`
-      );
-
-      select.selectOption('time');
-      await changeEvent.next();
-      await page.waitForChanges();
-
-      expect(await page.screenshot()).toMatchSnapshot(`datetime-display-cover-time-${page.getSnapshotSettings()}.png`);
-
-      select.selectOption('date');
-      await changeEvent.next();
-      await page.waitForChanges();
-
-      expect(await page.screenshot()).toMatchSnapshot(`datetime-display-cover-date-${page.getSnapshotSettings()}.png`);
-    });
+    test.describe('fixed sizes', () => {
+      test('date-time should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime value="2022-02-22T16:30:00" presentation="date-time"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-date-time-${page.getSnapshotSettings()}.png`
+        );
+      })
+      test('time-date should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime value="2022-02-22T16:30:00" presentation="time-date"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-time-date-${page.getSnapshotSettings()}.png`
+        );
+      })
+      test('time should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime value="2022-02-22T16:30:00" presentation="time"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-time-${page.getSnapshotSettings()}.png`
+        );
+      })
+      test('date should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime value="2022-02-22T16:30:00" presentation="date"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-date-${page.getSnapshotSettings()}.png`
+        );
+      })
+    })
+    test.describe('cover sizes', () => {
+      test.beforeEach(async ({ page }) => {
+        /**
+         * We need to take a screenshot of the entire page
+         * here as we want to test that the datetime fills
+         * the entire screen.
+         */
+        await page.setViewportSize({ width: 500, height: 500 });
+      });
+      test('date-time should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime size="cover" value="2022-02-22T16:30:00" presentation="date-time"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-cover-date-time-${page.getSnapshotSettings()}.png`
+        );
+      })
+      test('time-date should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime size="cover" value="2022-02-22T16:30:00" presentation="time-date"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-cover-time-date-${page.getSnapshotSettings()}.png`
+        );
+      })
+      test('time should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime size="cover" value="2022-02-22T16:30:00" presentation="time"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-cover-time-${page.getSnapshotSettings()}.png`
+        );
+      })
+      test('date should not have any visual regressions', async ({ page }) => {
+        await page.setContent(`
+          <ion-datetime size="cover" value="2022-02-22T16:30:00" presentation="date"></ion-datetime>
+        `);
+        const datetime = page.locator('ion-datetime');
+        expect(await datetime.screenshot()).toMatchSnapshot(
+          `datetime-display-cover-date-${page.getSnapshotSettings()}.png`
+        );
+      })
+    })
   });
   test.describe('datetime: switch presentations', () => {
-    test('month selection should work after changing presentation', async ({ page, browserName }) => {
+    test('month selection should work after changing presentation', async ({ page }) => {
       await page.goto('/src/components/datetime/test/display');
       const ionWorkingPartsDidChange = await page.spyOnEvent('ionWorkingPartsDidChange');
       await page.waitForSelector('.datetime-ready');
