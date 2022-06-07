2094f5f822786f45d8863cff85a698a0978b2a24
Liam DeBeasi <liamdebeasi@icloud.com>
2022-06-07 20:31:26 +0000

    test(datetime): add isDateEnabled and values tests

--- core/src/components/datetime/test/prefer-wheel/datetime.e2e.ts
+++ core/src/components/datetime/test/prefer-wheel/datetime.e2e.ts
@@ -31,4 +31,51 @@ test.describe('datetime: date wheel rendering', () => {
     const dayValues = page.locator('ion-datetime .day-column .picker-item[data-value]');
     expect(await dayValues.count()).toEqual(1);
   });
+  test('should respect isDateEnabled preference', async ({ page }) => {
+    await page.setContent(`
+      <ion-datetime presentation="date" prefer-wheel="true" value="2022-01-01"></ion-datetime>
+      <script>
+        const datetime = document.querySelector('ion-datetime');
+        datetime.isDateEnabled = (dateIsoString) => {
+          const date = new Date(dateIsoString);
+          if (date.getUTCDate() % 2 === 0) {
+            return false;
+          }
+          return true;
+        }
+      </script>
+    `);
+
+    await page.waitForSelector('.datetime-ready');
+
+    const disabledMonths = page.locator('.month-column .picker-item[disabled]');
+    const disabledYears = page.locator('.year-column .picker-item[disabled]');
+    const disabledDays = page.locator('.day-column .picker-item[disabled]');
+
+    expect(await disabledMonths.count()).toBe(0);
+    expect(await disabledYears.count()).toBe(0);
+    expect(await disabledDays.count()).toBe(15);
+  });
+  test('should respect month, day, and year preferences', async ({ page }) => {
+    await page.setContent(`
+      <ion-datetime
+        presentation="date"
+        prefer-wheel="true"
+        value="2022-01-01"
+        month-values="4,6"
+        day-values="1,2,3,4,5"
+        year-values="2022,2020,2019"
+      ></ion-datetime>
+    `);
+
+    await page.waitForSelector('.datetime-ready');
+
+    const monthValues = page.locator('.month-column .picker-item:not(.picker-item-empty)');
+    const yearValues = page.locator('.year-column .picker-item:not(.picker-item-empty)');
+    const dayValues = page.locator('.day-column .picker-item:not(.picker-item-empty)');
+
+    expect(await monthValues.count()).toBe(2);
+    expect(await yearValues.count()).toBe(3);
+    expect(await dayValues.count()).toBe(5);
+  });
 });
