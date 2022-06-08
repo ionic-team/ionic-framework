393861369767e3157cbed408540bb8d9a8b1631c
Sean Perkins <sean@ionic.io>
2022-06-08 11:44:26 -0400

    test(modal): getBackdropValueForSheet returns valid integer

---
+++ core/src/components/modal/utils.spec.ts
@@ -0,0 +1,15 @@
+import { getBackdropValueForSheet } from './utils';
+
+describe('getBackdropValueForSheet()', () => {
+
+  it('should return a valid integer when backdropBreakpoint is 1', () => {
+    /**
+     * Issue: https://github.com/ionic-team/ionic-framework/issues/25402
+     */
+    const backdropBreakpoint = 1;
+    expect(getBackdropValueForSheet(1, backdropBreakpoint)).toBe(0);
+    expect(getBackdropValueForSheet(5, backdropBreakpoint)).toBe(4);
+    expect(getBackdropValueForSheet(10, backdropBreakpoint)).toBe(9);
+  });
+
+});
