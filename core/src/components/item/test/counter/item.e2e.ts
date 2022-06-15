092c94ac8468d7d6646c3db583f69093ef6b1f7e
Sean Perkins <sean@ionic.io>
2022-06-15 13:06:52 -0400

    chore(): remove flaky test

--- core/src/components/item/test/counter/item.e2e.ts
+++ core/src/components/item/test/counter/item.e2e.ts
@@ -49,48 +49,5 @@ test.describe('item: counter', () => {
       expect(await itemCounter.textContent()).toBe('test label');
     });
 
-    test.describe('when an exception occurs', () => {
-      const logs: string[] = [];
-
-      test.beforeEach(async ({ page }) => {
-        await page.setContent(`
-          <ion-item counter="true"">
-            <ion-input maxlength="20" value=""></ion-input>
-          </ion-item>`);
-
-        page.on('console', (ev) => {
-          if (ev.type() === 'error') {
-            logs.push(ev.text());
-          }
-        });
-
-        const itemCounter = page.locator('ion-item .item-counter');
-
-        expect(await itemCounter.textContent()).toBe('0 / 20');
-
-        await page.$eval('ion-item', (el: any) => {
-          el.counterFormatter = () => {
-            throw new Error('This is an expected error');
-          };
-        });
-        await page.waitForChanges();
-      });
-
-      test('should default the formatting to length / maxlength', async ({ page }) => {
-        const input = page.locator('ion-input');
-
-        await input.click();
-        await input.type('abcde');
-
-        const itemCounter = page.locator('ion-item .item-counter');
-
-        expect(await itemCounter.textContent()).toBe('5 / 20');
-      });
-
-      test('should log an error', () => {
-        expect(logs.length).toBeGreaterThan(0);
-        expect(logs[0]).toMatch('[Ionic Error]: Exception in provided `counterFormatter`.');
-      });
-    });
   });
 });
