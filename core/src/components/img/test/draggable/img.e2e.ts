4c9157188a131a220245e38c0025871703000ae9
Liam DeBeasi <liamdebeasi@icloud.com>
2022-06-16 16:14:11 +0000

    chore(): fix test

--- core/src/components/img/test/draggable/img.e2e.ts
+++ core/src/components/img/test/draggable/img.e2e.ts
@@ -6,12 +6,12 @@ test.describe('img: rendering', () => {
     await page.goto('/src/components/img/test/draggable');
 
     const imgDraggableTrue = page.locator('#img-draggable-true img');
-    expect(imgDraggableTrue).toHaveJSProperty('draggable', true);
+    expect(imgDraggableTrue).toHaveAttribute('draggable', 'true');
 
     const imgDraggableFalse = page.locator('#img-draggable-false img');
-    expect(imgDraggableFalse).toHaveJSProperty('draggable', false);
+    expect(imgDraggableFalse).toHaveAttribute('draggable', 'false');
 
     const imgDraggableUnset = page.locator('#img-draggable-unset img');
-    expect(imgDraggableUnset).toHaveJSProperty('draggable', true);
+    expect(imgDraggableUnset).toHaveAttribute('draggable', '');
   });
 });
