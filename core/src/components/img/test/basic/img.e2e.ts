d2d1278b1275a4c7a187fb3117ee30d7d2d40966
Sean Perkins <sean@ionic.io>
2022-06-21 13:22:12 -0400

    chore(): check image completion state and document test

--- core/src/components/img/test/basic/img.e2e.ts
+++ core/src/components/img/test/basic/img.e2e.ts
@@ -5,8 +5,22 @@ test.describe('img: basic', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto('/src/components/img/test/basic');
 
-    const ionImgDidLoad = await page.spyOnEvent('ionImgDidLoad');
+    /**
+     * The first `ion-img` resolves before the spy can be created,
+     * since the image is initially visible and the IO fires.
+     *
+     * We manually look at the image's completion state to determine
+     * that it loaded, before creating a spy for the second image.
+     */
+    const img = await page.locator('.img-part img');
+    await img.evaluate((el: HTMLImageElement) => el.complete);
 
+    const ionImgDidLoad = await page.spyOnEvent('ionImgDidLoad');
+    /**
+     * Resizing the viewport will cause the intersection observers of
+     * the remaining images to fire, which will cause the `ionImgDidLoad`
+     * event to fire.
+     */
     await page.setIonViewport();
 
     await ionImgDidLoad.next();
