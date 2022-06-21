e57493b0472e5c09716abf49b804c6f3a40f406f
Sean Perkins <sean@ionic.io>
2022-06-21 12:49:21 -0400

    chore(): wait for images to load

--- core/src/components/img/test/basic/img.e2e.ts
+++ core/src/components/img/test/basic/img.e2e.ts
@@ -5,7 +5,11 @@ test.describe('img: basic', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto('/src/components/img/test/basic');
 
+    const ionImgDidLoad = await page.spyOnEvent('ionImgDidLoad');
+
     await page.setIonViewport();
+
+    await ionImgDidLoad.next();
 
     expect(await page.screenshot()).toMatchSnapshot(`img-basic-${page.getSnapshotSettings()}.png`);
   });
