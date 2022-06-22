21354f06383308ce7043e4dfe92fb7c70f17b9ce
Sean Perkins <sean@ionic.io>
2022-06-22 15:18:40 -0400

    test(textarea): floating label after value is set

--- core/src/components/textarea/test/basic/textarea.e2e.ts
+++ core/src/components/textarea/test/basic/textarea.e2e.ts
@@ -13,17 +13,28 @@ test.describe('textarea: basic', () => {
   test.describe('with floating labels', () => {
     /**
      * Verifies the display of a floating label above an `ion-textarea`.
+     * Captures a screenshot of the initial state (without a value) and verifies
+     * that the label translates correctly after the value is set.
      */
     test('should not have visual regressions', async ({ page }) => {
       await page.setContent(`
       <ion-item>
         <ion-label color="primary" position="floating">Floating Label</ion-label>
-        <ion-textarea value="Default value"></ion-textarea>
+        <ion-textarea value=""></ion-textarea>
       </ion-item>`);
 
-      await page.setIonViewport();
-
       const item = page.locator('ion-item');
+
+      expect(await item.screenshot()).toMatchSnapshot(`textarea-floating-label-initial-${page.getSnapshotSettings()}.png`);
+
+      const textarea = page.locator('textarea');
+      await textarea.evaluate((el: HTMLIonTextareaElement) => {
+        el.value = 'Updated value';
+      });
+
+      await page.waitForChanges();
+
+      await page.setIonViewport();
 
       expect(await item.screenshot()).toMatchSnapshot(`textarea-floating-label-diff-${page.getSnapshotSettings()}.png`);
     });
