4ba3e60a2f3dfde66e5ebb7cdc36306406619c8e
amandaesmith3 <amanda.s@ionic.io>
2022-06-23 10:30:20 -0500

    test(accordion): fix focus race conditions and Webkit behavior

--- core/src/components/accordion/test/a11y/accordion.e2e.ts
+++ core/src/components/accordion/test/a11y/accordion.e2e.ts
@@ -2,36 +2,38 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('accordion: a11y', () => {
-  test('accordions should be keyboard navigable', async ({ page }) => {
+  test('accordions should be keyboard navigable', async ({ page, browserName }) => {
     await page.goto(`/src/components/accordion/test/a11y`);
+    const tabKey = browserName === 'webkit' ? 'Alt+Tab' : 'Tab';
+
     const personalInfoHeader = page.locator('ion-accordion:first-child > ion-item');
     const billingAddressHeader = page.locator('ion-accordion:nth-child(2) > ion-item');
     const shippingAddressHeader = page.locator('ion-accordion:nth-child(3) > ion-item');
     const addressInput = page.locator('#address1 input');
 
-    await page.keyboard.press('Tab');
-    expect(personalInfoHeader).toBeFocused();
+    await page.keyboard.press(tabKey);
+    await expect(personalInfoHeader).toBeFocused();
 
     await page.keyboard.press('ArrowDown');
-    expect(billingAddressHeader).toBeFocused();
+    await expect(billingAddressHeader).toBeFocused();
 
     await page.keyboard.press('ArrowDown');
-    expect(shippingAddressHeader).toBeFocused();
+    await expect(shippingAddressHeader).toBeFocused();
 
     await page.keyboard.press('ArrowDown');
-    expect(personalInfoHeader).toBeFocused();
+    await expect(personalInfoHeader).toBeFocused();
 
     await page.keyboard.press('ArrowUp');
-    expect(shippingAddressHeader).toBeFocused();
+    await expect(shippingAddressHeader).toBeFocused();
 
     // open Shipping Address accordion and move focus to the input inside it
     await page.keyboard.press('Enter');
     await page.waitForChanges();
-    await page.keyboard.press('Tab');
-    expect(addressInput).toBeFocused();
+    await page.keyboard.press(tabKey);
+    await expect(addressInput).toBeFocused();
 
     // ensure keyboard interaction doesn't move focus from body
     await page.keyboard.press('ArrowDown');
-    expect(addressInput).toBeFocused();
+    await expect(addressInput).toBeFocused();
   });
 });
