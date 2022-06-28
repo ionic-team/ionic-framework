059582d3b53b89fed96b2661a04f288921a9aa16
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-06-28 11:12:43 -0400

    Update overlay-auto-mount.component.ts

--- angular/test/test-app/src/app/overlay-auto-mount/overlay-auto-mount.component.ts
+++ angular/test/test-app/src/app/overlay-auto-mount/overlay-auto-mount.component.ts
@@ -1,27 +1,13 @@
-import { AfterViewInit, Component } from "@angular/core";
+import { Component } from "@angular/core";
 
 /**
- * Validates that inline modals will correctly display
- * dynamic contents that are updated after the modal is
- * display.
+ * Validates that inline modals correctly mount
+ * inner components when autoMountComponent is
+ * enabled.
  */
 @Component({
   selector: 'app-overlay-auto-mount',
   templateUrl: 'overlay-auto-mount.component.html'
 })
-export class OverlayAutoMountComponent implements AfterViewInit {
-
-  items: string[] = [];
-
-  breakpointDidChangeCounter = 0;
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.items = ['A', 'B', 'C', 'D'];
-    }, 1000);
-  }
-
-  onBreakpointDidChange() {
-    this.breakpointDidChangeCounter++;
-  }
+export class OverlayAutoMountComponent {
 }
