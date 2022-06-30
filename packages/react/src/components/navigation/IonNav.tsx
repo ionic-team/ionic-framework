2cdf05496e289c6daf23b8aea8f7c33fdc123833
Sean Perkins <sean@ionic.io>
2022-06-30 16:00:19 -0400

    chore(): look-up views, logging, comments

--- packages/react/src/components/navigation/IonNav.tsx
+++ packages/react/src/components/navigation/IonNav.tsx
@@ -1,20 +1,31 @@
 import { defineCustomElement } from '@ionic/core/components/ion-nav.js';
 import React, { useState } from 'react';
+import type { FrameworkDelegate, JSX as IonicJSX } from '@ionic/core/components';
 import { ReactDelegate } from '../../framework-delegate';
 import { createReactComponent } from '../react-component-lib';
 
-const IonNavInner = createReactComponent('ion-nav', undefined, undefined, defineCustomElement);
+const IonNavInner = createReactComponent<
+  IonicJSX.IonNav & { delegate: FrameworkDelegate },
+  HTMLIonNavElement
+>('ion-nav', undefined, undefined, defineCustomElement);
 
-export const IonNav = (props: any) => {
-  const { children, ...restOfProps } = props;
+export const IonNav: React.FC<IonicJSX.IonNav> = ({ children, ...restOfProps }) => {
   const [views, setViews] = useState<JSX.Element[]>([]);
 
-  const addView = (view: JSX.Element) => {
-    setViews([...views, view]);
+  /**
+   * Allows us to create React components that are rendered within
+   * the context of the IonNav component.
+   */
+  const addView = (view: JSX.Element) => setViews([...views, view]);
+  const removeView = (view: JSX.Element) => {
+    const filteredViews = views.filter((v) => v !== view);
+    console.log('setting views to', filteredViews);
+    setViews(filteredViews);
   };
-  const removeView = (view: JSX.Element) => setViews(views.filter((v) => v !== view));
 
   const delegate = ReactDelegate(addView, removeView);
+
+  console.log('rendering views', views);
 
   return (
     <IonNavInner delegate={delegate} {...restOfProps}>
--- packages/react/src/framework-delegate.tsx
+++ packages/react/src/framework-delegate.tsx
@@ -4,6 +4,20 @@ import { FrameworkDelegate } from '@ionic/core/components';
  * The React Framework Delegate is an implementation of the FrameworkDelegate.
  * Responsible for managing how the framework creates, attaches, and removes components.
  */
+let attachViewCounter = 0;
+
+const delegateViews: {
+  view: JSX.Element;
+  el: HTMLElement | HTMLDivElement;
+}[] = [];
+
+const addViewToMemory = (view: JSX.Element, el: HTMLElement | HTMLDivElement) => {
+  delegateViews.push({
+    view,
+    el,
+  });
+};
+
 export const ReactDelegate = (
   addView: (view: JSX.Element) => void,
   removeView: (view: JSX.Element) => void
@@ -16,30 +30,42 @@ export const ReactDelegate = (
     _propsOrDataObj?: any,
     cssClasses?: string[]
   ): Promise<any> => {
+    // Creates a ref that will resolve the HTML Element of the component.
     const ref = React.createRef<HTMLDivElement>();
+    // Unique key for the component.
+    const key = ++attachViewCounter;
 
     view = (
-      // TODO - We could swap this out with IonPage (doesn't really change anything)
-      <div ref={ref} className={cssClasses && cssClasses.join(' ')}>
+      <div key={key} ref={ref} className={cssClasses && cssClasses.join(' ')}>
         {component()}
       </div>
     );
 
     /**
-     * Callback to add the view to the React component instance.
-     * This allows us to render components within React's context.
+     * Callback to allow the parent React component to render
+     * the new component within its context.
      */
     addView(view);
 
+    addViewToMemory(view, ref.current!);
+
+    // Return the HTML Element value of the component.
     return ref.current;
   };
 
   const removeViewFromDom = (_container: HTMLElement, component: HTMLElement): Promise<void> => {
-    // Remove view still needs work, back navigation will throw an assert error
-    if (view) {
-      removeView(view);
+    console.log('removeViewFromDom', {
+      container: _container,
+      component,
+    });
+
+    const viewIndex = delegateViews.findIndex((v) => v.el === component);
+
+    if (viewIndex > -1) {
+      console.log('removing view', delegateViews[viewIndex]);
+      removeView(delegateViews[viewIndex].view);
+      delegateViews.splice(viewIndex, 1);
     }
-    component.remove();
 
     return Promise.resolve();
   };
