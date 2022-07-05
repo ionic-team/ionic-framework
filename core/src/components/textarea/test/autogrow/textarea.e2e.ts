d01aef13cd39ff889181d0ddde87a1b8eb65bc59
Sean Perkins <sean@ionic.io>
2022-07-05 16:15:51 -0400

    test(): skip autogrow diff screenshots

--- core/src/components/textarea/test/autogrow/textarea.e2e.ts
+++ core/src/components/textarea/test/autogrow/textarea.e2e.ts
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('textarea: autogrow', () => {
-  test('should not have visual regressions', async ({ page }) => {
+  test.skip('should not have visual regressions', async ({ page }) => {
     await page.goto(`/src/components/textarea/test/autogrow`);
 
     await page.waitForChanges();
