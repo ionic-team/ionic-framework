13d6eb85ae96022ba76479fd6b6cf4751a424192
Sean Perkins <sean@ionic.io>
2022-07-08 14:06:41 -0400

    test(input): migrate to playwright

---
+++ core/src/components/input/test/basic/input.e2e.ts
@@ -0,0 +1,56 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('input: basic', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/src/components/input/test/basic');
+  });
+
+  test('should not have visual regressions', async ({ page }) => {
+    const compares = [];
+
+    await page.setIonViewport();
+
+    compares.push({
+      name: 'initial',
+      screenshot: await page.screenshot()
+    });
+
+    await page.click('#fullInput');
+
+    const fullItem = page.locator('#fullItem');
+    await expect(fullItem).toHaveClass(/item-has-focus/);
+
+    compares.push({
+      name: 'full input focused',
+      screenshot: await page.screenshot()
+    });
+
+    await page.click('#insetInput');
+
+    const insetItem = page.locator('#insetItem');
+    await expect(insetItem).toHaveClass(/item-has-focus/);
+
+    compares.push({
+      name: 'inset input focused',
+      screenshot: await page.screenshot()
+    });
+
+    await page.click('#noneInput');
+
+    const noneItem = page.locator('#noneItem');
+    await expect(noneItem).toHaveClass(/item-has-focus/);
+
+    compares.push({
+      name: 'no lines input focused',
+      screenshot: await page.screenshot()
+    });
+
+    for (const compare of compares) {
+      expect(compare.screenshot).toMatchSnapshot(`input-diff-${compare.name}-${page.getSnapshotSettings()}.png`);
+    }
+  });
+
+
+});
---
+++ core/src/components/input/test/masking/input.e2e.ts
@@ -0,0 +1,24 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('input: masking', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/src/components/input/test/masking');
+  });
+
+  test('should filter out spaces', async ({ page }) => {
+    const input = page.locator('#inputTrimmed');
+
+    await input.click();
+
+    await page.keyboard.type('S p a c e s');
+
+    const currentValue = await input.evaluate((el: HTMLInputElement) => {
+      return el.value;
+    });
+
+    await expect(currentValue).toEqual('Spaces');
+  });
+});
+
