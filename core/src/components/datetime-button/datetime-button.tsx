c2a36698a9cdcdb23b2b377ca67784d792aaa5e2
Liam DeBeasi <liamdebeasi@icloud.com>
2022-07-12 15:07:27 +0000

    chore(): prettier

--- core/src/components/datetime-button/datetime-button.tsx
+++ core/src/components/datetime-button/datetime-button.tsx
@@ -31,8 +31,6 @@ export class DatetimeButton implements ComponentInterface {
   @Prop() datetime?: string;
 
   render() {
-    return (
-      <Host></Host>
-    );
+    return <Host></Host>;
   }
 }
