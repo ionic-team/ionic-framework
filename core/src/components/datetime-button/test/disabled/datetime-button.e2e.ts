2174cd378495f54d465162faec414f6e63bc91cc
Liam DeBeasi <liamdebeasi@icloud.com>
2022-07-19 13:23:50 +0000

    test(datetime-button): move skip to non-screenshot test

--- core/src/components/datetime-button/test/disabled/datetime-button.e2e.ts
+++ core/src/components/datetime-button/test/disabled/datetime-button.e2e.ts
@@ -2,12 +2,10 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('datetime-button: disabled buttons', () => {
-  // eslint-disable-next-line no-empty-pattern
-  test.beforeEach(async ({}, testInfo) => {
+  test('buttons should not be enabled when component is disabled', async ({ page }, testInfo) => {
     test.skip(testInfo.project.metadata.rtl === 'rtl', 'No layout tests');
     test.skip(testInfo.project.metadata.mode === 'ios', 'No mode-specific logic');
-  });
-  test('buttons should not be enabled when component is disabled', async ({ page }) => {
+
     await page.setContent(`
       <ion-datetime-button datetime="datetime" disabled="true"></ion-datetime-button>
       <ion-datetime id="datetime" presentation="date-time"></ion-datetime>
