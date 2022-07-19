80d551e17495df60a5d67f2864c2765631c714a3
Sean Perkins <sean@ionic.io>
2022-07-19 11:51:11 -0400

    refactor(): use toHaveJSProperty

--- core/src/components/input/test/masking/input.e2e.ts
+++ core/src/components/input/test/masking/input.e2e.ts
@@ -15,10 +15,6 @@ test.describe('input: masking', () => {
 
     await page.keyboard.type('S p a c e s');
 
-    const currentValue = await input.evaluate((el: HTMLInputElement) => {
-      return el.value;
-    });
-
-    await expect(currentValue).toEqual('Spaces');
+    await expect(input).toHaveJSProperty('value', 'Spaces');
   });
 });
