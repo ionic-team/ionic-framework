d6aaef46648ba465764d558f91a6522807054aa6
Liam DeBeasi <liamdebeasi@icloud.com>
2022-07-19 13:24:13 +0000

    test(datetime-button): add default value

--- core/src/components/datetime-button/test/disabled/datetime-button.e2e.ts
+++ core/src/components/datetime-button/test/disabled/datetime-button.e2e.ts
@@ -18,7 +18,7 @@ test.describe('datetime-button: disabled buttons', () => {
   test('buttons should visually be disabled', async ({ page }) => {
     await page.setContent(`
       <ion-datetime-button datetime="datetime" disabled="true"></ion-datetime-button>
-      <ion-datetime id="datetime" presentation="date-time"></ion-datetime>
+      <ion-datetime id="datetime" presentation="date-time" value="2022-01-01T16:30:00></ion-datetime>
     `);
     await page.waitForSelector('.datetime-ready');
 
