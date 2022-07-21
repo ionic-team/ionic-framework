726c00ff1cb583d000a5324070b4b4d960602b8d
Liam DeBeasi <liamdebeasi@icloud.com>
2022-07-21 15:55:07 +0000

    test(angular): add build script

---
+++ angular/test/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# The directory where the source
+# for each specific application is.
+APPS_DIR="apps"
+
+# The directory where the
+# base application logic is
+BASE_DIR="base"
+BUILD_DIR="build"
+
+# The specific application
+# we are building
+APP_DIR="${1}"
+
+# The full path to the specific application.
+FULL_APP_DIR="${APPS_DIR}/${APP_DIR}/"
+
+# The full path to the base application.
+FULL_BASE_DIR="${BASE_DIR}/"
+
+# The full path to the built application.
+BUILD_APP_DIR="${BUILD_DIR}/${APP_DIR}/"
+
+# First we need to copy the base application
+cp -r $FULL_BASE_DIR $BUILD_APP_DIR
+
+# Then we can copy the specific app which
+# will override any files in the base application.
+cp -r $FULL_APP_DIR $BUILD_APP_DIR
