fd27284b39e943a6d7a2ad6a8741570ee0260a68
Liam DeBeasi <liamdebeasi@icloud.com>
2022-07-26 21:28:17 +0000

    chore(): skip correct test

--- core/src/components/accordion/test/basic/accordion.e2e.ts
+++ core/src/components/accordion/test/basic/accordion.e2e.ts
@@ -2,8 +2,7 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('accordion: basic', () => {
-  // TODO(FW-1842) Re-enable when flakiness has been addressed.
-  test.skip('should not have visual regressions', async ({ page }) => {
+  test('should not have visual regressions', async ({ page }) => {
     await page.goto(`/src/components/accordion/test/basic`);
 
     await page.setIonViewport();
