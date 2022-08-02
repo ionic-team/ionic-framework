b9c84c4d852a3de270e69495a9dad40e0ce17ac7
amandaesmith3 <amanda.s@ionic.io>
2022-08-02 13:51:13 -0500

    test(searchbar): migrate tests to Playwright

---
+++ core/src/components/searchbar/test/basic/searchbar.e2e.ts
@@ -0,0 +1,33 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('searchbar: basic', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/src/components/searchbar/test/basic`);
+  });
+
+  test('should not have visual regressions', async ({ page }) => {
+    await page.setIonViewport();
+    expect(await page.screenshot()).toMatchSnapshot(`searchbar-diff-${page.getSnapshotSettings()}.png`);
+  });
+
+  test('should show cancel button on focus if show-cancel-button=focus', async ({ page }) => {
+    const searchbar = page.locator('#basic');
+
+    await searchbar.evaluate((el: HTMLIonSearchbarElement) => el.setFocus());
+    await page.waitForChanges();
+
+    expect(searchbar).toHaveClass(/searchbar-has-focus/);
+    expect(await searchbar.screenshot()).toMatchSnapshot(`searchbar-show-cancel-focus-${page.getSnapshotSettings()}.png`);
+  });
+
+  test('should not show cancel button on focus if show-cancel-button=never', async ({ page }) => {
+    const searchbar = page.locator('#noCancel');
+
+    await searchbar.evaluate((el: HTMLIonSearchbarElement) => el.setFocus());
+    await page.waitForChanges();
+
+    expect(searchbar).toHaveClass(/searchbar-has-focus/);
+    expect(await searchbar.screenshot()).toMatchSnapshot(`searchbar-show-cancel-never-${page.getSnapshotSettings()}.png`);
+  });
+});
