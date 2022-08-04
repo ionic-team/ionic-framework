9b50ab20077db53fb43d183aa62ede3a47030b94
Sean Perkins <sean@ionic.io>
2022-08-04 12:12:53 -0400

    test(): use toHaveReceivedEventDetail

--- core/src/components/radio-group/test/form/radio-group.e2e.ts
+++ core/src/components/radio-group/test/form/radio-group.e2e.ts
@@ -7,14 +7,16 @@ test.describe('radio-group: form', () => {
   });
 
   test('selecting an option should update the value', async ({ page }) => {
-    const value = page.locator('#value');
+    const radioGroup = page.locator('ion-radio-group');
+    const ionChange = await page.spyOnEvent('ionChange');
     const griffRadio = page.locator('ion-radio[value="griff"]');
 
-    await expect(value).toHaveText('');
+    await expect(radioGroup).toHaveAttribute('value', 'biff');
 
     await griffRadio.click();
+    await page.waitForChanges();
 
-    await expect(value).toHaveText('griff');
+    await expect(ionChange).toHaveReceivedEventDetail({ value: 'griff' });
   });
 
   test('selecting a disabled option should not update the value', async ({ page }) => {
