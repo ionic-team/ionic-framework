826cd054adcacd16de44d4233f05e23352573a2e
amandaesmith3 <amanda.s@ionic.io>
2022-08-09 13:05:00 -0500

    fix(header): hiden from screen readers when collapsed

---
+++ core/src/components/header/test/fade/header.e2e.ts
@@ -0,0 +1,29 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('header: fade', () => {
+  test('should be hidden from screen readers when collapsed', async ({ page }, testInfo) => {
+    test.skip(testInfo.project.metadata.mode === 'md', 'Logic only applies to iOS mode');
+
+    await page.goto('/src/components/header/test/fade');
+    const header = page.locator('#collapsibleHeader');
+    const content = page.locator('ion-content');
+
+    await expect(header).toHaveAttribute('aria-hidden', 'true');
+
+    await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom());
+    await page.waitForChanges();
+
+    /**
+     * Playwright can't do .not.toHaveAttribute() because a value is expected,
+     * and toHaveAttribute can't accept a value of type null.
+     */
+    const ariaHidden = await header.getAttribute('aria-hidden');
+    expect(ariaHidden).toBeNull();
+
+    await content.evaluate((el: HTMLIonContentElement) => el.scrollToTop());
+    await page.waitForChanges();
+
+    await expect(header).toHaveAttribute('aria-hidden', 'true');
+  });
+});
