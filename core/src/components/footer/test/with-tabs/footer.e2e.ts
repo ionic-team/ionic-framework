466bf4d84f44a4042401ed8371c4c3c683f9a29a
amandaesmith3 <amanda.s@ionic.io>
2022-08-15 13:20:05 -0500

    chore(): lint

--- core/src/components/footer/test/with-tabs/footer.e2e.ts
+++ core/src/components/footer/test/with-tabs/footer.e2e.ts
@@ -4,7 +4,7 @@ import { test } from '@utils/test/playwright';
 test.describe('footer: with tabs', () => {
   test('should not have extra padding when near a tab bar', async ({ page }, testInfo) => {
     test.skip(testInfo.project.metadata.rtl === true, 'This does not test LTR vs. RTL layout.');
-    
+
     await page.goto('/src/components/footer/test/with-tabs');
 
     const footer = page.locator('[tab="tab-one"] ion-footer');
