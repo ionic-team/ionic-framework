157ee48273af0c10ba0846971ebf44e568abd98b
Sean Perkins <sean@ionic.io>
2022-08-23 14:01:30 -0400

    chore(): lint

--- core/src/components/progress-bar/test/standalone/progress-bar.e2e.ts
+++ core/src/components/progress-bar/test/standalone/progress-bar.e2e.ts
@@ -5,6 +5,8 @@ test.describe('progress-bar: standalone', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto('/src/components/progress-bar/test/standalone');
 
-    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`progress-bar-standalone-${page.getSnapshotSettings()}.png`);
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
+      `progress-bar-standalone-${page.getSnapshotSettings()}.png`
+    );
   });
 });
