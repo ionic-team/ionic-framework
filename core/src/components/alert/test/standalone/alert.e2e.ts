2a7b0e9bb2a4e78d6bbb5b928bf9dd2e2b4887fa
amandaesmith3 <amanda.s@ionic.io>
2022-08-25 13:19:10 -0500

    test(alert): migrate to Playwright

---
+++ core/src/components/alert/test/standalone/alert.e2e.ts
@@ -0,0 +1,14 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('alert: standalone', () => {
+  test('should not have visual regressions', async ({ page }) => {
+    const alert = page.locator('ion-alert');
+    const didPresent = await page.spyOnEvent('ionAlertDidPresent');
+
+    await page.click('#basic');
+    await didPresent.next();
+
+    expect(await alert.screenshot()).toMatchSnapshot(`alert-standalone-${page.getSnapshotSettings()}.png`);
+  });
+});
