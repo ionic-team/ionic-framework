5146188d4aa3c78dd17396920c7a5f3f2e91f23a
Sean Perkins <sean@ionic.io>
2022-08-25 16:06:28 -0400

    fix(): md implementation against spec

--- core/src/components/range/test/a11y/range.e2e.ts
+++ core/src/components/range/test/a11y/range.e2e.ts
@@ -17,20 +17,37 @@ test.describe('range: a11y', () => {
         `
       );
 
-      const rangeWithPin = page.locator('ion-range#withPin');
-      const rangeWithoutPin = page.locator('ion-range#withoutPin');
       const tabKey = browserName === 'webkit' ? 'Alt+Tab' : 'Tab';
+
+      const rangeWithPin = page.locator('ion-range#withPin');
+
+      const range = page.locator('ion-range#withoutPin');
+      const rangeHandle = page.locator('ion-range#withoutPin .range-knob-handle');
 
       await page.keyboard.press(tabKey);
       await page.waitForChanges();
 
       expect(await rangeWithPin.screenshot()).toMatchSnapshot(`range-focus-with-pin-${page.getSnapshotSettings()}.png`);
 
+      await rangeHandle.hover();
+      await page.waitForChanges();
+
+      expect(await range.screenshot()).toMatchSnapshot(
+        `range-focus-without-pin-hover-${page.getSnapshotSettings()}.png`
+      );
+
       await page.keyboard.press(tabKey);
       await page.waitForChanges();
 
-      expect(await rangeWithoutPin.screenshot()).toMatchSnapshot(
-        `range-focus-without-pin-${page.getSnapshotSettings()}.png`
+      expect(await range.screenshot()).toMatchSnapshot(
+        `range-focus-without-pin-focus-${page.getSnapshotSettings()}.png`
+      );
+
+      await rangeHandle.click();
+      await page.waitForChanges();
+
+      expect(await range.screenshot()).toMatchSnapshot(
+        `range-focus-without-pin-active-${page.getSnapshotSettings()}.png`
       );
     });
   });
