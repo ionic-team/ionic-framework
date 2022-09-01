9b753799552bc246f1299f7b9b0a8517448f6605
Sean Perkins <sean@ionic.io>
2022-09-01 15:23:45 -0400

    test(range): use mouse down instead of tab focus

--- core/src/components/range/test/a11y/range.e2e.ts
+++ core/src/components/range/test/a11y/range.e2e.ts
@@ -1,5 +1,16 @@
+import type { Locator } from '@playwright/test';
 import { expect } from '@playwright/test';
+import type { E2EPage } from '@utils/test/playwright';
 import { test } from '@utils/test/playwright';
+
+const mouseDownRangeHandle = async (page: E2EPage, rangeHandle: Locator) => {
+  const box = (await rangeHandle.boundingBox())!;
+  const centerX = box.x + box.width / 2;
+  const centerY = box.y + box.height / 2;
+
+  await page.mouse.move(centerX, centerY);
+  await page.mouse.down();
+};
 
 test.describe('range: a11y', () => {
   test('should not have visual regressions', async ({ page, skip, browserName }) => {
@@ -18,48 +29,36 @@ test.describe('range: a11y', () => {
     const tabKey = browserName === 'webkit' ? 'Alt+Tab' : 'Tab';
 
     const range = page.locator('ion-range');
-    const rangeHandle = page.locator('ion-range .range-knob-handle');
+    const rangeHandle = range.locator('.range-knob-handle');
 
     await page.keyboard.press(tabKey);
     await page.waitForChanges();
 
     expect(await range.screenshot()).toMatchSnapshot(`range-focus-${page.getSnapshotSettings()}.png`);
 
-    const box = (await rangeHandle.boundingBox())!;
-    const centerX = box.x + box.width / 2;
-    const centerY = box.y + box.height / 2;
-
-    await page.mouse.move(centerX, centerY);
-    await page.mouse.down();
-
+    await mouseDownRangeHandle(page, rangeHandle);
     await page.waitForChanges();
 
     expect(await range.screenshot()).toMatchSnapshot(`range-active-${page.getSnapshotSettings()}.png`);
   });
 
   test.describe('with pin', () => {
-    test('should not have visual regressions', async ({ page, skip, browserName }) => {
+    test('should not have visual regressions', async ({ page, skip }) => {
       skip.rtl();
 
       await page.setContent(
         `<ion-app>
           <ion-content>
-            <ion-range min="0" max="100" value="49" pin="true"></ion-range>
+            <ion-range min="0" max="100" value="50" pin="true"></ion-range>
           </ion-content>
         </ion-app>
         `
       );
 
-      const tabKey = browserName === 'webkit' ? 'Alt+Tab' : 'Tab';
-
       const range = page.locator('ion-range');
+      const rangeHandle = range.locator('.range-knob-handle');
 
-      await page.keyboard.press(tabKey);
-
-      await expect(range).toBeFocused();
-
-      await page.keyboard.press('ArrowRight');
-
+      await mouseDownRangeHandle(page, rangeHandle);
       await page.waitForChanges();
 
       expect(await range.screenshot()).toMatchSnapshot(`range-focus-with-pin-${page.getSnapshotSettings()}.png`);
