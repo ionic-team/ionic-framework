5be5026e972b1b2dcedb9c3ca743b3fe10fc3505
Brandy Carney <brandy@ionic.io>
2022-09-09 10:31:20 -0400

    fix(item): properly show the highlight on iOS when highlight-height is set

---
+++ core/src/components/item/test/fill/item.e2e.ts
@@ -0,0 +1,12 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('item: fill', () => {
+  test('should not have visual regressions', async ({ page }) => {
+    await page.goto(`/src/components/item/test/fill`);
+
+    await page.setIonViewport({ resizeViewportWidth: true });
+
+    expect(await page.screenshot()).toMatchSnapshot(`item-fill-diff-${page.getSnapshotSettings()}.png`);
+  });
+});
