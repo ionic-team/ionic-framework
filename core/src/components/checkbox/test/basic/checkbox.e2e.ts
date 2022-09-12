c5cb2f319bc25bc06b84c33351a74a20d76d07ed
Liam DeBeasi <liamdebeasi@icloud.com>
2022-09-12 18:04:20 +0000

    test(checkbox): add failing tests

--- core/src/components/checkbox/test/basic/checkbox.e2e.ts
+++ core/src/components/checkbox/test/basic/checkbox.e2e.ts
@@ -10,3 +10,52 @@ test.describe('checkbox: basic', () => {
     expect(await page.screenshot()).toMatchSnapshot(`checkbox-basic-${page.getSnapshotSettings()}.png`);
   });
 });
+
+test.describe.only('checkbox: ionChange', () => {
+  test.beforeEach(({ skip }) => {
+    skip.rtl();
+  });
+  test('should fire ionChange when interacting with checkbox', async ({ page }) => {
+    await page.setContent(`
+      <ion-checkbox value="my-checkbox"></ion-checkbox>
+    `);
+
+    const ionChange = await page.spyOnEvent('ionChange');
+    const checkbox = page.locator('ion-checkbox');
+
+    await checkbox.click();
+    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: true });
+
+    await checkbox.click();
+    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: false });
+  });
+
+  test('should fire ionChange when interacting with checkbox in item', async ({ page }) => {
+    await page.setContent(`
+      <ion-item>
+        <ion-checkbox value="my-checkbox"></ion-checkbox>
+      </ion-item>
+    `);
+
+    const ionChange = await page.spyOnEvent('ionChange');
+    const item = page.locator('ion-item');
+
+    await item.click();
+    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: true });
+
+    await item.click();
+    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: false });
+  });
+
+  test('should not fire when programmatically setting a value', async ({ page }) => {
+    await page.setContent(`
+      <ion-checkbox value="my-checkbox"></ion-checkbox>
+    `);
+
+    const ionChange = await page.spyOnEvent('ionChange');
+    const checkbox = page.locator('ion-checkbox');
+
+    await checkbox.evaluate((el: HTMLIonCheckboxElement) => (el.checked = true));
+    await expect(ionChange).not.toHaveReceivedEvent();
+  });
+});
