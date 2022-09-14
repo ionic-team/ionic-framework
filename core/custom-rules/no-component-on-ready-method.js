0c8539a1114315b40477bcdecb3bb78f089b1fa8
Liam DeBeasi <liamdebeasi@icloud.com>
2022-09-14 18:25:34 +0000

    only check for calling method

--- core/custom-rules/no-component-on-ready-method.js
+++ core/custom-rules/no-component-on-ready-method.js
@@ -6,8 +6,15 @@ module.exports = {
     },
     create(context) {
       return {
-        MemberExpression(node) {
-          if (node.property.name === 'componentOnReady') {
+        CallExpression(node) {
+          /**
+           * We only want to exclude usages of componentOnReady().
+           * Checking for the existence of the componentOnReady method
+           * is a way of determining if we are in a lazy loaded build
+           * or custom elements build, so we want to allow that.
+           */
+          const callee = node.callee;
+          if (callee.type === 'MemberExpression' && callee.property.name === 'componentOnReady') {
             context.report({ node: node, messageId: 'noComponentOnReadyMethod' });
           }
       }
