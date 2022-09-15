5c10f98ceb3ae42d3363b38ba786b9122676a59c
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-09-15 15:36:45 -0500

    fix(overlays): focus trapping no longer includes hidden elements (#25948)

--- core/src/utils/test/overlays/overlays.e2e.ts
+++ core/src/utils/test/overlays/overlays.e2e.ts
@@ -2,9 +2,10 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('overlays: focus', () => {
-  test('should not focus the overlay container if element inside of overlay is focused', async ({ page, skip }) => {
+  test.beforeEach(({ skip }) => {
     skip.rtl();
-
+  });
+  test('should not focus the overlay container if element inside of overlay is focused', async ({ page }) => {
     await page.setContent(`
       <ion-button id="open-modal">Show Modal</ion-button>
       <ion-modal trigger="open-modal">
@@ -25,5 +26,31 @@ test.describe('overlays: focus', () => {
     await page.waitForChanges();
 
     await expect(page.locator('ion-input input')).toBeFocused();
+  });
+
+  test('should not select a hidden focusable element', async ({ page, browserName }) => {
+    await page.setContent(`
+      <ion-button id="open-modal">Show Modal</ion-button>
+      <ion-modal trigger="open-modal">
+        <ion-content>
+          <ion-button hidden id="hidden">Hidden Button</ion-button>
+          <ion-button id="visible">Visible Button</ion-button>
+        </ion-content>
+      </ion-modal>
+    `);
+
+    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+    const presentButton = page.locator('ion-button#open-modal');
+    const visibleButton = page.locator('ion-button#visible');
+    const tabKey = browserName === 'webkit' ? 'Alt+Tab' : 'Tab';
+
+    await presentButton.click();
+    await ionModalDidPresent.next();
+
+    await page.keyboard.press(tabKey);
+    await expect(visibleButton).toBeFocused();
+
+    await page.keyboard.press(tabKey);
+    await expect(visibleButton).toBeFocused();
   });
 });
