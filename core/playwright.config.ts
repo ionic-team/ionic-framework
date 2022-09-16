f8beafc74ba73e28c8acdd77f034081db56a0e21
Liam DeBeasi <liamdebeasi@icloud.com>
2022-09-16 15:15:04 -0400

    test(playwright): run parallel tests

--- core/playwright.config.ts
+++ core/playwright.config.ts
@@ -97,8 +97,6 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
