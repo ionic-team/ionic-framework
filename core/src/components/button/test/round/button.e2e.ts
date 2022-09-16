b67939a26f35c6980ea1a96d5884e638189a186d
amandaesmith3 <amanda.s@ionic.io>
2022-09-16 09:20:43 -0500

    test(button): remove RTL from expand and round tests

--- core/src/components/button/test/round/button.e2e.ts
+++ core/src/components/button/test/round/button.e2e.ts
@@ -2,7 +2,8 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('button: round', () => {
-  test('should not have visual regressions', async ({ page }) => {
+  test('should not have visual regressions', async ({ page, skip }) => {
+    skip.rtl('All content takes up the full width, so RTL has no effect.');
     await page.goto(`/src/components/button/test/round`);
 
     await page.setIonViewport();
