ba57aff4a37d127dad68477499ee364a038db984
amandaesmith3 <amanda.s@ionic.io>
2022-09-20 12:56:09 -0500

    test(content): remove RTL and iOS where redundant

--- core/src/components/content/test/fullscreen/content.e2e.ts
+++ core/src/components/content/test/fullscreen/content.e2e.ts
@@ -2,7 +2,10 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('content: fullscreen', () => {
-  test('should not have visual regressions', async ({ page }) => {
+  test('should not have visual regressions', async ({ page, skip }) => {
+    skip.rtl();
+    skip.mode('ios', 'ion-content does not have mode-specific styling');
+    
     await page.goto(`/src/components/content/test/fullscreen`);
 
     await page.setIonViewport();
