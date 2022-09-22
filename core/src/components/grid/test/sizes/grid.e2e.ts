284ebe774efd6d72e8bdeda0b1b21a569602857f
amandaesmith3 <amanda.s@ionic.io>
2022-09-22 12:53:40 -0500

    test(grid): migrate to Playwright

---
+++ core/src/components/grid/test/sizes/grid.e2e.ts
@@ -0,0 +1,11 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test('should not have visual regressions', async ({ page, skip }) => {
+  skip.mode('ios', 'ion-grid does not have different styling per-mode');
+  await page.goto(`/src/components/grid/test/sizes`);
+
+  await page.setIonViewport();
+
+  expect(await page.screenshot()).toMatchSnapshot(`grid-sizes-${page.getSnapshotSettings()}.png`);
+});
