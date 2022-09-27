a4023ba113b67ab56f6136a21729e5cc7374226e
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-09-27 19:25:54 +0000

    chore(): testing

--- core/src/components/picker-column-internal/picker-column-internal.tsx
+++ core/src/components/picker-column-internal/picker-column-internal.tsx
@@ -271,13 +271,20 @@ export class PickerColumnInternal implements ComponentInterface {
        * pixel scrolls which can be inconsistent across
        * browsers.
        */
-      const top = Math.floor(targetOffsetFromTop - midpoint);
+      const top = Math.ceil(targetOffsetFromTop - midpoint);
 
-      el.scroll({
-        top,
-        left: 0,
-        behavior: smooth ? 'smooth' : undefined,
-      });
+      /**
+       * This avoids scroll inconsistency
+       * issues across browsers where the
+       * scroll position is off by 1px.
+       */
+      if (el.scrollTop !== top) {
+        el.scroll({
+          top,
+          left: 0,
+          behavior: smooth ? 'smooth' : undefined,
+        });
+      }
     }
   };
 
