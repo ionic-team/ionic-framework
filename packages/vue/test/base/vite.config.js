c13f574fde7c19bc025ea545527eb07e67e6fd8f
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-09-29 15:49:44 +0000

    chore(): add vite config

---
+++ packages/vue/test/base/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
