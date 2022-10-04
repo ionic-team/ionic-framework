00b94a5d00879faeecda4d6e94f03577e4071b83
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-10-04 14:26:00 +0000

    test(datetime): add failing test

--- core/src/components/datetime/test/set-value/datetime.e2e.ts
+++ core/src/components/datetime/test/set-value/datetime.e2e.ts
@@ -2,11 +2,13 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('datetime: set-value', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ skip }) => {
+    skip.rtl();
+  });
+  test('should update the active date when value is initially set', async ({ page }) => {
     await page.goto('/src/components/datetime/test/set-value');
     await page.waitForSelector('.datetime-ready');
-  });
-  test('should update the active date', async ({ page }) => {
+
     const datetime = page.locator('ion-datetime');
     await datetime.evaluate((el: HTMLIonDatetimeElement) => (el.value = '2021-11-25T12:40:00.000Z'));
 
@@ -15,7 +17,10 @@ test.describe('datetime: set-value', () => {
     const activeDate = page.locator('ion-datetime .calendar-day-active');
     await expect(activeDate).toHaveText('25');
   });
-  test('should update the active time', async ({ page }) => {
+  test('should update the active time when value is initially set', async ({ page }) => {
+    await page.goto('/src/components/datetime/test/set-value');
+    await page.waitForSelector('.datetime-ready');
+
     const datetime = page.locator('ion-datetime');
     await datetime.evaluate((el: HTMLIonDatetimeElement) => (el.value = '2021-11-25T12:40:00.000Z'));
 
@@ -23,5 +28,35 @@ test.describe('datetime: set-value', () => {
 
     const activeDate = page.locator('ion-datetime .time-body');
     await expect(activeDate).toHaveText('12:40 PM');
+  });
+  test.only('should update active item when value is not initially set', async ({ page }) => {
+    await page.setContent(`
+      <ion-datetime presentation="date" locale="en-US"></ion-datetime>
+    `);
+    await page.waitForSelector('.datetime-ready');
+
+    const datetime = page.locator('ion-datetime');
+    const activeDayButton = page.locator('.calendar-day-active');
+    const monthYearButton = page.locator('.calendar-month-year');
+    const monthColumn = page.locator('.month-column');
+    const yearColumn = page.locator('.year-column');
+
+    await datetime.evaluate((el: HTMLIonDatetimeElement) => (el.value = '2021-10-05'));
+
+    await monthYearButton.click();
+    await page.waitForChanges();
+
+    await monthColumn.locator('.picker-item[data-value="10"]').click();
+    await page.waitForChanges();
+
+    await yearColumn.locator('.picker-item[data-value="2021"]').click();
+    await page.waitForChanges();
+
+    await monthYearButton.click();
+    await page.waitForChanges();
+
+    await expect(activeDayButton).toHaveAttribute('data-day', '5');
+    await expect(activeDayButton).toHaveAttribute('data-month', '10');
+    await expect(activeDayButton).toHaveAttribute('data-year', '2021');
   });
 });
