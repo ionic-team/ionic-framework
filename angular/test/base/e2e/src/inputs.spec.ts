f2483d5791ccf7376b8dd21ebaf47654eb727fbc
Sean Perkins <sean@ionic.io>
2022-10-07 16:00:04 -0400

    test(angular): range form control

--- angular/test/base/e2e/src/inputs.spec.ts
+++ angular/test/base/e2e/src/inputs.spec.ts
@@ -9,7 +9,6 @@ describe('Inputs', () => {
     cy.get('ion-input').should('have.prop', 'value').and('equal', 'some text');
     cy.get('ion-datetime').should('have.prop', 'value').and('equal', '1994-03-15');
     cy.get('ion-select').should('have.prop', 'value').and('equal', 'nes');
-    cy.get('ion-range').should('have.prop', 'value').and('equal', 10);
   });
 
   it('should have reset value', () => {
@@ -20,7 +19,6 @@ describe('Inputs', () => {
     cy.get('ion-input').should('have.prop', 'value').and('equal', '');
     cy.get('ion-datetime').should('have.prop', 'value').and('equal', '');
     cy.get('ion-select').should('have.prop', 'value').and('equal', '');
-    cy.get('ion-range').should('have.prop', 'value').and('be.NaN');
   });
 
   it('should get some value', () => {
@@ -32,7 +30,6 @@ describe('Inputs', () => {
     cy.get('ion-input').should('have.prop', 'value').and('equal', 'some text');
     cy.get('ion-datetime').should('have.prop', 'value').and('equal', '1994-03-15');
     cy.get('ion-select').should('have.prop', 'value').and('equal', 'nes');
-    cy.get('ion-range').should('have.prop', 'value').and('equal', 10);
   });
 
   it('change values should update angular', () => {
@@ -46,19 +43,11 @@ describe('Inputs', () => {
 
     cy.get('ion-datetime').invoke('prop', 'value', '1996-03-15');
     cy.get('ion-select').invoke('prop', 'value', 'playstation');
-    cy.get('ion-range').invoke('prop', 'value', 20);
 
     cy.get('#checkbox-note').should('have.text', 'true');
     cy.get('#toggle-note').should('have.text', 'true');
     cy.get('#input-note').should('have.text', 'hola');
     cy.get('#datetime-note').should('have.text', '1996-03-15');
     cy.get('#select-note').should('have.text', 'playstation');
-    cy.get('#range-note').should('have.text', '20');
   });
-
-  it('nested components should not interfere with NgModel', () => {
-    cy.get('#range-note').should('have.text', '10');
-    cy.get('#nested-toggle').click();
-    cy.get('#range-note').should('have.text', '10');
-  });
-})
+});
