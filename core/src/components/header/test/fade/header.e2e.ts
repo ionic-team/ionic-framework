368533d0248fee7e667e37f75944e9ac65bb01a5
Brandy Carney <brandy@ionic.io>
2022-10-12 16:49:06 -0400

    test(header): migrate to playwright

---
+++ core/src/components/header/test/fade/header.e2e.ts
@@ -0,0 +1,23 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('header: fade', () => {
+  test.beforeEach(({ skip }) => {
+    skip.rtl();
+  });
+
+  test('should not have visual regressions with fade header', async ({ page, skip }) => {
+    skip.mode('md', 'Translucent effect is only available in iOS mode.');
+    skip.browser('firefox', 'Firefox has some issues rendering translucent effects on Linux.');
+
+    await page.goto('/src/components/header/test/fade');
+
+    const header = page.locator('ion-header');
+    expect(await header.screenshot()).toMatchSnapshot(`header-fade-not-blurred-diff-${page.getSnapshotSettings()}.png`);
+
+    const content = page.locator('ion-content');
+    await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom(0));
+
+    expect(await header.screenshot()).toMatchSnapshot(`header-fade-blurred-diff-${page.getSnapshotSettings()}.png`);
+  });
+});
---
+++ core/src/components/header/test/scroll-target/header.e2e.ts
@@ -0,0 +1,28 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('header: scroll-target', () => {
+  test.beforeEach(({ skip }) => {
+    skip.rtl();
+  });
+
+  /**
+   * This test suite verifies that the fade effect for iOS is working correctly
+   * when the `ion-header` is using a custom scroll target with the `.ion-content-scroll-host`
+   * selector.
+   */
+  test('should not have visual regressions with custom scroll target header', async ({ page, skip }) => {
+    skip.mode('md', 'Translucent effect is only available in iOS mode.');
+    skip.browser('firefox', 'Firefox has some issues rendering translucent effects on Linux.');
+
+    await page.goto('/src/components/header/test/scroll-target');
+
+    const header = page.locator('ion-header');
+    expect(await header.screenshot()).toMatchSnapshot(`header-scroll-target-not-blurred-diff-${page.getSnapshotSettings()}.png`);
+
+    const scrollTarget = page.locator('#scroll-target');
+    await scrollTarget.evaluate((el: HTMLDivElement) => el.scrollTop = el.scrollHeight);
+
+    expect(await header.screenshot()).toMatchSnapshot(`header-scroll-target-blurred-diff-${page.getSnapshotSettings()}.png`);
+  });
+});
