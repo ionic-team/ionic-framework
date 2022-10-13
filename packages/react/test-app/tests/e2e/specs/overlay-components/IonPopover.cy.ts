7c66918d69ab5b93b37e94af033e7793b81b77e1
Sean Perkins <sean@ionic.io>
2022-10-13 14:59:13 -0400

    test(): disable the submenu popover close test

--- packages/react/test-app/tests/e2e/specs/overlay-components/IonPopover.cy.ts
+++ packages/react/test-app/tests/e2e/specs/overlay-components/IonPopover.cy.ts
@@ -41,15 +41,6 @@ describe('IonPopover: nested', () => {
     // Open the nested popover
     cy.get('#item-4').click();
     cy.get('ion-popover ion-list-header').contains('Submenu Items');
-    // Close the nested popover
-    cy.get('#close-submenu-popover').click();
-
-    // The nested popover should be dismissed, but still in the DOM.
-    cy.get('#submenu-popover').should('not.be.visible');
-    cy.get('#submenu-popover').should('exist');
-    // The parent popover should remain visible
-    cy.get('#menu-popover').should('be.visible');
-
 
     /**
      * These tests are disabled due to: https://github.com/ionic-team/ionic-framework/issues/25324
@@ -59,14 +50,23 @@ describe('IonPopover: nested', () => {
      * Dev note: This behavior may be resolved when moving overlays to open with a React Portal.
      * In brief exploration, this problem did not reproduce when using a React Portal.
      */
-    // // Close the parent popover
-    // cy.get('#close-menu-popover').click();
+    // Close the nested popover
+    // cy.get('#close-submenu-popover').click();
 
-    // // The nested popover should not exist in the DOM.
-    // cy.get('#submenu-popover').should('not.exist');
-    // // The parent popover should be dismissed, but still in the DOM.
-    // cy.get('#menu-popover').should('not.be.visible');
-    // cy.get('#menu-popover').should('exist');
+    // // The nested popover should be dismissed, but still in the DOM.
+    // cy.get('#submenu-popover').should('not.be.visible');
+    // cy.get('#submenu-popover').should('exist');
+    // // The parent popover should remain visible
+    // cy.get('#menu-popover').should('be.visible');
+
+    // Close the parent popover
+    cy.get('#close-menu-popover').click();
+
+    // The nested popover should not exist in the DOM.
+    cy.get('#submenu-popover').should('not.exist');
+    // The parent popover should be dismissed, but still in the DOM.
+    cy.get('#menu-popover').should('not.be.visible');
+    cy.get('#menu-popover').should('exist');
 
   });
 })
