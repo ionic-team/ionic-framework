c4c8c540123bb4f0f4d67a9c4a45fe6108770f69
Brandy Carney <brandy@ionic.io>
2022-10-27 16:06:49 -0400

    test(menu): add working focus-trap e2e

---
+++ core/src/components/menu/test/focus-trap/menu.e2e.ts
@@ -0,0 +1,88 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('menu: focus trap', () => {
+  test.beforeEach(async ({ page, skip }) => {
+    await page.goto(`/src/components/menu/test/focus-trap`);
+
+    skip.rtl('Trapping focus is not dependent on document direction');
+    skip.browser('firefox', 'Firefox incorrectly allows keyboard focus to move to ion-content');
+  });
+
+  test('should trap focus with overlays', async ({ page }) => {
+    const ionDidOpen = await page.spyOnEvent('ionDidOpen');
+    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+    const ionModalDidDismiss = await page.spyOnEvent('ionModalDidDismiss');
+
+    await page.click('#open-menu-button');
+    await ionDidOpen.next();
+
+    const menu = await page.locator('#menu');
+    await expect(menu).toBeFocused();
+
+    const openModalButton = page.locator('#open-modal-button');
+    await openModalButton.click();
+    await ionModalDidPresent.next();
+
+    const modal = await page.locator('#modal');
+    await expect(modal).toBeFocused();
+
+    await modal.evaluate(async (el: HTMLIonModalElement) => {
+      await el.dismiss();
+    });
+    await ionModalDidDismiss.next();
+
+    // await expect(openModalButton).toBeFocused();
+  });
+
+  test('should trap focus with content inside overlays', async ({ page }) => {
+    const ionDidOpen = await page.spyOnEvent('ionDidOpen');
+    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+
+    await page.click('#open-menu-button');
+    await ionDidOpen.next();
+
+    const menu = await page.locator('#menu');
+    await expect(menu).toBeFocused();
+
+    await page.click('#open-modal-button');
+    await ionModalDidPresent.next();
+
+    const otherButton = page.locator('#other-button');
+    await otherButton.click();
+
+    // await expect(otherButton).toBeFocused();
+  });
+
+  test('should work with swipe gestures after modal is dismissed', async ({ page }) => {
+    const ionDidOpen = await page.spyOnEvent('ionDidOpen');
+    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+    const ionModalDidDismiss = await page.spyOnEvent('ionModalDidDismiss');
+
+    await page.click('#open-menu-button');
+    await ionDidOpen.next();
+
+    const menu = await page.locator('#menu');
+    await expect(menu).toBeFocused();
+
+    await page.click('#open-modal-button');
+    await ionModalDidPresent.next();
+
+    const modal = await page.locator('#modal');
+
+    await modal.evaluate(async (el: HTMLIonModalElement) => {
+      await el.dismiss();
+    });
+    await ionModalDidDismiss.next();
+
+    await page.mouse.move(30, 168);
+    await page.mouse.down();
+
+    await page.mouse.move(384, 168);
+    await page.mouse.up();
+
+    await page.waitForChanges();
+
+    await expect(menu).toHaveClass(/show-menu/);
+  });
+});
