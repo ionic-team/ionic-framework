03c16bca0421ad6bc899968c6b19cd82d2bfe485
Sean Perkins <sean@ionic.io>
2022-10-27 12:39:20 -0400

    test(modal): presenting only creates a single root element with ion-page

--- core/src/components/modal/test/inline/modal.e2e.ts
+++ core/src/components/modal/test/inline/modal.e2e.ts
@@ -21,4 +21,44 @@ test.describe('modal: inline', () => {
 
     expect(await page.screenshot()).toMatchSnapshot(`modal-inline-dismiss-${page.getSnapshotSettings()}.png`);
   });
+
+  test('presenting should create a single root element with the ion-page class', async ({ page, skip }, testInfo) => {
+    skip.mode('md');
+    skip.rtl();
+
+    testInfo.annotations.push({
+      type: 'issue',
+      description: 'https://github.com/ionic-team/ionic-framework/issues/26117',
+    });
+
+    await page.setContent(`
+    <ion-datetime-button datetime="datetime"></ion-datetime-button>
+
+    <ion-modal>
+      <ion-datetime id="datetime" presentation="date-time"></ion-datetime>
+    </ion-modal>
+    `);
+
+    const ionModalDidPresent = await page.spyOnEvent('ionModalDidPresent');
+    const ionModalDidDismiss = await page.spyOnEvent('ionModalDidDismiss');
+    const modal = page.locator('ion-modal');
+
+    await page.locator('#date-button').click();
+    await ionModalDidPresent.next();
+
+    // Verifies that the host element exists with the .ion-page class
+    expect(await modal.evaluate((el: HTMLIonModalElement) => el.firstElementChild!.className)).toBe('ion-page');
+
+    await modal.evaluate((el: HTMLIonModalElement) => el.dismiss());
+    await ionModalDidDismiss.next();
+
+    await page.locator('#date-button').click();
+    await ionModalDidPresent.next();
+
+    // Verifies that presenting the overlay again does not create a new host element
+    expect(await modal.evaluate((el: HTMLIonModalElement) => el.firstElementChild!.className)).toBe('ion-page');
+    expect(
+      await modal.evaluate((el: HTMLIonModalElement) => el.firstElementChild!.firstElementChild!.className)
+    ).not.toBe('ion-page');
+  });
 });
