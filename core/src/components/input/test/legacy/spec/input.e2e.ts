2a80a5f59c406358a8aa8415e2e1b3d42eada8ec
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-10-31 17:31:08 +0000

    chore(): update tests

--- core/src/components/input/test/legacy/spec/input.e2e.ts
+++ core/src/components/input/test/legacy/spec/input.e2e.ts
@@ -3,7 +3,7 @@ import { test } from '@utils/test/playwright';
 
 test.describe('input: spec', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/input/test/spec');
+    await page.goto('/src/components/input/test/legacy/spec');
   });
 
   test('should not have visual regressions', async ({ page }) => {
