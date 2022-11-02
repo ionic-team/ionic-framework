613bdcf7ccb8321191948cec5c89f1c480969b62
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-02 19:28:37 +0000

    refactor(input): clean up counter logic

--- core/src/components/input/input.tsx
+++ core/src/components/input/input.tsx
@@ -524,12 +524,12 @@ export class Input implements ComponentInterface {
   }
 
   private renderCounter() {
-    const { counter, maxlength, el, counterFormatter } = this;
+    const { counter, maxlength, counterFormatter, value } = this;
     if (counter !== true || maxlength === undefined) {
       return;
     }
 
-    return <div class="counter">{getCounterText(el, counterFormatter)}</div>;
+    return <div class="counter">{getCounterText(value, maxlength, counterFormatter)}</div>;
   }
 
   /**
