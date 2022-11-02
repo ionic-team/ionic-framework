fe20efa92c4c1f500f795a48596880cb77ad4506
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-02 18:27:19 +0000

    feat(input): add base solid + shape implementation

--- core/src/components/input/input.tsx
+++ core/src/components/input/input.tsx
@@ -555,7 +555,7 @@ export class Input implements ComponentInterface {
   }
 
   private renderInput() {
-    const { disabled, readonly, inputId, labelPlacement } = this;
+    const { disabled, fill, readonly, shape, inputId, labelPlacement } = this;
     const mode = getIonMode(this);
     const value = this.getValue();
 
@@ -566,6 +566,8 @@ export class Input implements ComponentInterface {
           [mode]: true,
           'has-value': this.hasValue(),
           'has-focus': this.hasFocus,
+          [`input-fill-${fill}`]: fill !== undefined,
+          [`input-shape-${shape}`]: shape !== undefined,
         })}
       >
         <div
