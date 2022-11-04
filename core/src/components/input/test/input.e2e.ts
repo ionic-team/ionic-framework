1c76776c8fbf715f21a27c8e97bf4d4587980325
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-04 16:12:33 -0400

    Update core/src/components/input/test/input.e2e.ts
    Co-authored-by: Amanda Johnston <90629384+amandaejohnston@users.noreply.github.com>

--- core/src/components/input/test/input.e2e.ts
+++ core/src/components/input/test/input.e2e.ts
@@ -7,7 +7,7 @@ test.describe('input: rendering', () => {
     skip.mode('ios', 'Rendering is the same across platforms');
   });
 
-  test('should not rendering the label element if no label was passed', async ({ page }) => {
+  test('should not render the label element if no label was passed', async ({ page }) => {
     await page.setContent(`<ion-input aria-label="my hidden label"></ion-label>`);
 
     const label = page.locator('ion-input label');
