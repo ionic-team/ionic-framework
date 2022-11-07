eaecb89ac477cc4d0ded2bb0927be3c2d9afc0ed
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-07 21:14:06 +0000

    test(input): add item tests

--- core/src/components/input/test/item/input.e2e.ts
+++ core/src/components/input/test/item/input.e2e.ts
@@ -1,75 +1,79 @@
 import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
-test.describe('input: fill', () => {
-  test.beforeEach(({ skip }) => {
-    skip.mode('ios', 'Fill is only available in MD mode');
+test.describe('input: item', () => {
+  test('should render correctly in list with no fill', async ({ page }) => {
+    await page.setContent(`
+      <ion-list>
+        <ion-item>
+          <ion-input
+            label="Email"
+            value="hi@ionic.io"
+            helper-text="Enter your email"
+            maxlength="20"
+            counter="true"
+          ></ion-input>
+        </ion-item>
+      </ion-list>
+    `);
+    const list = page.locator('ion-list');
+    expect(await list.screenshot()).toMatchSnapshot(`input-list-no-fill-${page.getSnapshotSettings()}.png`);
+
   });
+  test('should render correctly in list with solid fill', async ({ page }) => {
+    await page.setContent(`
+      <ion-list>
+        <ion-item>
+          <ion-input
+            fill="solid"
+            label="Email"
+            value="hi@ionic.io"
+            helper-text="Enter your email"
+            maxlength="20"
+            counter="true"
+          ></ion-input>
+        </ion-item>
+      </ion-list>
+    `);
+    const list = page.locator('ion-list');
+    expect(await list.screenshot()).toMatchSnapshot(`input-list-solid-${page.getSnapshotSettings()}.png`);
 
-  test.describe('input: fill solid', () => {
-    test('should not have visual regressions', async ({ page }) => {
-      await page.setContent(`
-        <ion-input
-          fill="solid"
-          label="Email"
-          value="hi@ionic.io"
-          helper-text="Enter your email"
-          maxlength="20"
-          counter="true"
-        ></ion-input>
-      `);
+  })
+  test('should render correctly in list with outline fill', async ({ page }) => {
+    await page.setContent(`
+      <ion-list>
+        <ion-item>
+          <ion-input
+            fill="outline"
+            label="Email"
+            value="hi@ionic.io"
+            helper-text="Enter your email"
+            maxlength="20"
+            counter="true"
+          ></ion-input>
+        </ion-item>
+      </ion-list>
+    `);
+    const list = page.locator('ion-list');
+    expect(await list.screenshot()).toMatchSnapshot(`input-list-outline-${page.getSnapshotSettings()}.png`);
 
-      const input = page.locator('ion-input');
-      expect(await input.screenshot()).toMatchSnapshot(`input-fill-solid-${page.getSnapshotSettings()}.png`);
-    });
-    test('should not have visual regressions with shaped solid', async ({ page }) => {
-      await page.setContent(`
-        <ion-input
-          shape="rounded"
-          fill="solid"
-          label="Email"
-          value="hi@ionic.io"
-          helper-text="Enter your email"
-          maxlength="20"
-          counter="true"
-        ></ion-input>
-      `);
+  })
+  test('should render correctly in inset list with no fill', async ({ page }) => {
+    await page.setContent(`
+      <ion-list inset="true">
+        <ion-item>
+          <ion-input
+            label="Email"
+            value="hi@ionic.io"
+            helper-text="Enter your email"
+            maxlength="20"
+            counter="true"
+          ></ion-input>
+        </ion-item>
+      </ion-list>
+    `);
+    const list = page.locator('ion-list');
+    expect(await list.screenshot()).toMatchSnapshot(`input-inset-list-no-fill-${page.getSnapshotSettings()}.png`);
 
-      const input = page.locator('ion-input');
-      expect(await input.screenshot()).toMatchSnapshot(`input-fill-shaped-solid-${page.getSnapshotSettings()}.png`);
-    });
-  });
-  test.describe('input: fill outline', () => {
-    test('should not have visual regressions', async ({ page }) => {
-      await page.setContent(`
-        <ion-input
-          fill="outline"
-          label="Email"
-          value="hi@ionic.io"
-          helper-text="Enter your email"
-          maxlength="20"
-          counter="true"
-        ></ion-input>
-      `);
-
-      const input = page.locator('ion-input');
-      expect(await input.screenshot()).toMatchSnapshot(`input-fill-outline-${page.getSnapshotSettings()}.png`);
-    });
-    test('should not have visual regressions with shaped outline', async ({ page }) => {
-      await page.setContent(`
-        <ion-input
-          shape="rounded"
-          fill="outline"
-          label="Email"
-          value="hi@ionic.io"
-          helper-text="Enter your email"
-          maxlength="20"
-          counter="true"
-        ></ion-input>
-      `);
-
-      const input = page.locator('ion-input');
-      expect(await input.screenshot()).toMatchSnapshot(`input-fill-shaped-outline-${page.getSnapshotSettings()}.png`);
-    });
-  });
+  })
 });
