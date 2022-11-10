59de4c6c7a4a7d4ef976fabb4c3ad32cdf744d66
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-10 19:28:03 +0000

    refactor(react): ionicons is a dependency

--- packages/react-router/scripts/sync.sh
+++ packages/react-router/scripts/sync.sh
@@ -9,7 +9,3 @@ rm -rf node_modules/@ionic/core/dist node_modules/@ionic/core/components
 cp -a ../../core/dist node_modules/@ionic/core/dist
 cp -a ../../core/components node_modules/@ionic/core/components
 cp -a ../../core/package.json node_modules/@ionic/core/package.json
-
-# Copy ionicons
-rm -rf node_modules/ionicons
-cp -a ../../core/node_modules/ionicons node_modules/ionicons
--- packages/react/test-app/scripts/sync.sh
+++ packages/react/test-app/scripts/sync.sh
@@ -14,11 +14,3 @@ rm -rf node_modules/@ionic/core/dist node_modules/@ionic/core/components
 cp -a ../../../core/dist node_modules/@ionic/core/dist
 cp -a ../../../core/package.json node_modules/@ionic/core/package.json
 cp -a ../../../core/components node_modules/@ionic/core/components
-
-# Copy stencil
-rm -rf node_modules/@stencil/core
-cp -a ../../../core/node_modules/@stencil/core node_modules/@stencil/core
-
-# Copy ionicons
-rm -rf node_modules/ionicons
-cp -a ../../../core/node_modules/ionicons node_modules/ionicons
