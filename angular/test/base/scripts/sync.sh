2cb90c70f29d190f05e69611402b2a67c506f8d2
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-10 19:36:59 +0000

    refactor(angular): ionicons is a dependency

--- angular/test/base/scripts/sync.sh
+++ angular/test/base/scripts/sync.sh
@@ -14,7 +14,3 @@ cp -a ../../../../core/dist node_modules/@ionic/core/dist
 cp -a ../../../../core/hydrate node_modules/@ionic/core/hydrate
 cp -a ../../../../core/loader node_modules/@ionic/core/loader
 cp -a ../../../../core/package.json node_modules/@ionic/core/package.json
-
-# # Copy ionicons
-rm -rf node_modules/ionicons
-cp -a ../../../../core/node_modules/ionicons node_modules/ionicons
