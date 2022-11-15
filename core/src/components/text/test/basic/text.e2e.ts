02623eb8187c9fba55aa8692bd95e9fc68c0ff89
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-15 14:45:22 +0000

    test(text): migrate tests to playwright

---
+++ core/src/components/text/test/basic/text.e2e.ts
@@ -0,0 +1,29 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('text: basic', () => {
+  test.beforeEach(({ skip }) => {
+    skip.rtl();
+    skip.mode('ios', 'Text does not have per-mode styles');
+  });
+  test('should render default text', async ({ page }) => {
+    await page.setContent(`
+      <ion-text>
+        <h1>H1: The quick brown fox jumps over the lazy dog</h1>
+        <p>This is <sub>subscript</sub>. <ion-text>This is nested text</ion-text>
+      </ion-text>
+    `);
+
+    expect(await page.screenshot()).toMatchSnapshot(`text-${page.getSnapshotSettings()}.png`);
+  });
+  test('should render text with color prop', async ({ page }) => {
+    await page.setContent(`
+      <ion-text color="primary">
+        <h1>H1: The quick brown fox jumps over the lazy dog</h1>
+        <p>This is <sub>subscript</sub>. <ion-text color="danger">This is nested text</ion-text>
+      </ion-text>
+    `);
+
+    expect(await page.screenshot()).toMatchSnapshot(`text-color-${page.getSnapshotSettings()}.png`);
+  });
+});
