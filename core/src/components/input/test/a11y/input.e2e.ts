665e7dbb0028696e54feace09b540c66cba48b86
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-16 19:52:43 +0000

    chore(): typo

--- core/src/components/input/test/a11y/input.e2e.ts
+++ core/src/components/input/test/a11y/input.e2e.ts
@@ -2,7 +2,7 @@ import AxeBuilder from '@axe-core/playwright';
 import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
-test.describe('header: a11y', () => {
+test.describe('input: a11y', () => {
   test.beforeEach(async ({ skip }) => {
     skip.rtl();
     skip.mode('md');
