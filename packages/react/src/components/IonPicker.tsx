8f9d9f575478b417347154c9cc6678bc21e7bf20
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-22 16:09:16 +0000

    fix(react): fix import

--- packages/react/src/components/IonPicker.tsx
+++ packages/react/src/components/IonPicker.tsx
@@ -1,7 +1,7 @@
 import { JSX } from '@ionic/core/components';
 import { defineCustomElement } from '@ionic/core/components/ion-picker.js';
 
-import { createInlineOverlayComponent } from './createControllerComponent';
+import { createInlineOverlayComponent } from './createInlineOverlayComponent';
 
 export const IonPicker = /*@__PURE__*/ createInlineOverlayComponent<
   JSX.IonPicker,
