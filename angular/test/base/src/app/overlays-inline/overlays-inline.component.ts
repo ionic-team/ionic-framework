f23fb342b279cd4646b432dd420b457f6c41ac12
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-22 14:14:04 -0500

    fix(angular): inline overlays are exported (#26333)

---
+++ angular/test/base/src/app/overlays-inline/overlays-inline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+/**
+ * Validates that inline overlays will will display correctly.
+ */
+@Component({
+  selector: 'app-overlays-inline',
+  templateUrl: 'overlays-inline.component.html'
+})
+export class OverlaysInlineComponent {}
