68c64a288e5bb04cb8519c090e0270fe8015c599
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-11-23 15:18:48 +0000

    test(segment): add failing test

--- core/src/components/segment/test/toolbar/segment.e2e.ts
+++ core/src/components/segment/test/toolbar/segment.e2e.ts
@@ -39,6 +39,24 @@ test.describe('segment: toolbar', () => {
       expect(await header.screenshot()).toMatchSnapshot(`segment-toolbar-color-${page.getSnapshotSettings()}.png`);
     });
 
+    test.only('should not have visual regressions when used in a toolbar with light color', async ({ page }) => {
+      await page.setContent(`
+        <ion-header>
+          <ion-toolbar color="light">
+            <ion-segment value="a">
+              <ion-segment-button value="a">First</ion-segment-button>
+              <ion-segment-button value="b">Second</ion-segment-button>
+              <ion-segment-button value="c" disabled="true">Third</ion-segment-button>
+            </ion-segment>
+          </ion-toolbar>
+        </ion-header>
+      `);
+
+      const header = page.locator('ion-header');
+
+      expect(await header.screenshot()).toMatchSnapshot(`segment-toolbar-light-color-${page.getSnapshotSettings()}.png`);
+    });
+
     test('should not inherit height when segment is MD and toolbar is iOS', async ({ page, skip }) => {
       skip.rtl();
       skip.mode('ios', 'We manually set the mode in this test, so the automatic mode switching is not needed');
