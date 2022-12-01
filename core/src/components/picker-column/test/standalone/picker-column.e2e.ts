a3d19a3833440d1bb4873f8044317fb0da5a44bf
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 22:29:47 +0000

    test(picker-column): use generators

--- core/src/components/picker-column/test/standalone/picker-column.e2e.ts
+++ core/src/components/picker-column/test/standalone/picker-column.e2e.ts
@@ -1,19 +1,21 @@
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
 import { testPickerColumn } from '../test.utils';
 
-test.describe('picker-column', () => {
-  test.describe('single column', () => {
-    test('should not have any visual regressions', async ({ page }) => {
-      await page.goto('/src/components/picker-column/test/standalone');
-      await testPickerColumn(page, '#single-column-button', 'single');
+configs().forEach(({ title, config }) => {
+  test.describe('picker-column', () => {
+    test.describe('single column', () => {
+      test(title('should not have any visual regressions'), async ({ page }) => {
+        await page.goto('/src/components/picker-column/test/standalone', config);
+        await testPickerColumn(page, '#single-column-button', 'single');
+      });
     });
-  });
 
-  test.describe('multiple columns', () => {
-    test('should not have any visual regressions', async ({ page }) => {
-      await page.goto('/src/components/picker-column/test/standalone');
-      await testPickerColumn(page, '#multiple-column-button', 'multiple');
+    test.describe('multiple columns', () => {
+      test(title('should not have any visual regressions'), async ({ page }) => {
+        await page.goto('/src/components/picker-column/test/standalone', config);
+        await testPickerColumn(page, '#multiple-column-button', 'multiple');
+      });
     });
   });
 });
