5069ac5b1569c5ecf4e3bfc0e603fee75cb09f4d
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 20:43:11 +0000

    test(avatar): use configs

--- core/src/components/avatar/test/basic/avatar.e2e.ts
+++ core/src/components/avatar/test/basic/avatar.e2e.ts
@@ -1,22 +1,24 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('avatar: basic', () => {
-  test('should not have visual regressions', async ({ page, skip }) => {
-    skip.rtl('Avatar does not test RTL behaviors. Usages of Avatar in slots are tested in components that use Avatar.');
+configs({ directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('avatar: basic', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto(`/src/components/avatar/test/basic`, config);
 
-    await page.goto(`/src/components/avatar/test/basic`);
+      const avatar = page.locator('#avatar');
+      const avatarChip = page.locator('#avatar-chip');
+      const avatarItemStart = page.locator('#avatar-item-start');
+      const avatarItemEnd = page.locator('#avatar-item-end');
 
-    const avatar = page.locator('#avatar');
-    const avatarChip = page.locator('#avatar-chip');
-    const avatarItemStart = page.locator('#avatar-item-start');
-    const avatarItemEnd = page.locator('#avatar-item-end');
-
-    expect(await avatar.screenshot()).toMatchSnapshot(`avatar-diff-${page.getSnapshotSettings()}.png`);
-    expect(await avatarChip.screenshot()).toMatchSnapshot(`avatar-chip-diff-${page.getSnapshotSettings()}.png`);
-    expect(await avatarItemStart.screenshot()).toMatchSnapshot(
-      `avatar-item-start-diff-${page.getSnapshotSettings()}.png`
-    );
-    expect(await avatarItemEnd.screenshot()).toMatchSnapshot(`avatar-item-end-diff-${page.getSnapshotSettings()}.png`);
+      expect(await avatar.screenshot()).toMatchSnapshot(`avatar-diff-${page.getSnapshotSettings()}.png`);
+      expect(await avatarChip.screenshot()).toMatchSnapshot(`avatar-chip-diff-${page.getSnapshotSettings()}.png`);
+      expect(await avatarItemStart.screenshot()).toMatchSnapshot(
+        `avatar-item-start-diff-${page.getSnapshotSettings()}.png`
+      );
+      expect(await avatarItemEnd.screenshot()).toMatchSnapshot(
+        `avatar-item-end-diff-${page.getSnapshotSettings()}.png`
+      );
+    });
   });
 });
