db31c80ec511107555292869d541207fffe461c4
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:50:07 +0000

    test(item-divider): use generators

--- core/src/components/item-divider/test/spec/item-divider.e2e.ts
+++ core/src/components/item-divider/test/spec/item-divider.e2e.ts
@@ -1,14 +1,16 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('item-divider: spec', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/item-divider/test/spec');
-  });
+configs().forEach(({ title, config }) => {
+  test.describe('item-divider: spec', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/src/components/item-divider/test/spec', config);
+    });
 
-  test('should not have visual regressions', async ({ page }) => {
-    await page.setIonViewport();
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`list-item-divider-${page.getSnapshotSettings()}.png`);
+      expect(await page.screenshot()).toMatchSnapshot(`list-item-divider-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
