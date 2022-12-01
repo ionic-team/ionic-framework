53a0bf48c9103d19f87b5c20382eb264ba1c8c0e
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:20:07 +0000

    test(fab-button): use generators

--- core/src/components/fab-button/test/a11y/fab-button.e2e.ts
+++ core/src/components/fab-button/test/a11y/fab-button.e2e.ts
@@ -1,16 +1,18 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('fab-button: aria attributes', () => {
-  test('should inherit aria attributes to inner button', async ({ page, skip }) => {
-    skip.rtl();
-    skip.mode('ios');
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('fab-button: aria attributes', () => {
+    test(title('should inherit aria attributes to inner button'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-fab-button aria-label="Hello World">My Button</ion-fab-button>
+      `,
+        config
+      );
 
-    await page.setContent(`
-      <ion-fab-button aria-label="Hello World">My Button</ion-fab-button>
-    `);
-
-    const nativeButton = page.locator('ion-fab-button button');
-    await expect(nativeButton).toHaveAttribute('aria-label', 'Hello World');
+      const nativeButton = page.locator('ion-fab-button button');
+      await expect(nativeButton).toHaveAttribute('aria-label', 'Hello World');
+    });
   });
 });
