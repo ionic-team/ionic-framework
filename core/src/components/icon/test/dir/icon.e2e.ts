bf06d1d581226da2ec975293605344e2aa75e09f
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:32:19 +0000

    test(icon): use generators

--- core/src/components/icon/test/dir/icon.e2e.ts
+++ core/src/components/icon/test/dir/icon.e2e.ts
@@ -1,25 +1,29 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('icon: rtl', () => {
-  test('should flip icon when rtl is active', async ({ page, skip }) => {
-    skip.mode('ios');
+configs({ modes: ['ios'] }).forEach(({ title, config }) => {
+  test.describe('icon: rtl', () => {
+    test(title('should flip icon when rtl is active'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-icon name="cut" flip-rtl="true"></ion-icon>
+      `,
+        config
+      );
 
-    await page.setContent(`
-      <ion-icon name="cut" flip-rtl="true"></ion-icon>
-    `);
+      const icon = page.locator('ion-icon');
+      expect(await icon.screenshot()).toMatchSnapshot(`icon-flip-${page.getSnapshotSettings()}.png`);
+    });
+    test(title('should not flip icon when rtl is active'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-icon name="cut" flip-rtl="false"></ion-icon>
+      `,
+        config
+      );
 
-    const icon = page.locator('ion-icon');
-    expect(await icon.screenshot()).toMatchSnapshot(`icon-flip-${page.getSnapshotSettings()}.png`);
-  });
-  test('should not flip icon when rtl is active', async ({ page, skip }) => {
-    skip.mode('ios');
-
-    await page.setContent(`
-      <ion-icon name="cut" flip-rtl="false"></ion-icon>
-    `);
-
-    const icon = page.locator('ion-icon');
-    expect(await icon.screenshot()).toMatchSnapshot(`icon-no-flip-${page.getSnapshotSettings()}.png`);
+      const icon = page.locator('ion-icon');
+      expect(await icon.screenshot()).toMatchSnapshot(`icon-no-flip-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
