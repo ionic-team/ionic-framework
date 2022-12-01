1337d4890d3d9bf11544f3c9bda87826c7146a4a
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 22:12:23 +0000

    test(list-header): use generators

--- core/src/components/list-header/test/basic/list-header.e2e.ts
+++ core/src/components/list-header/test/basic/list-header.e2e.ts
@@ -1,12 +1,14 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('list-header: basic', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto(`/src/components/list-header/test/basic`);
+configs().forEach(({ title, config }) => {
+  test.describe('list-header: basic', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto(`/src/components/list-header/test/basic`, config);
 
-    await page.setIonViewport();
+      await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`list-header-${page.getSnapshotSettings()}.png`);
+      expect(await page.screenshot()).toMatchSnapshot(`list-header-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
