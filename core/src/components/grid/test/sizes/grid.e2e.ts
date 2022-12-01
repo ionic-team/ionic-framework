41e4584b095e16f81a5757e9e32766798f62b946
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:25:05 +0000

    test(grid): use generators

--- core/src/components/grid/test/sizes/grid.e2e.ts
+++ core/src/components/grid/test/sizes/grid.e2e.ts
@@ -1,11 +1,12 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test('should not have visual regressions', async ({ page, skip }) => {
-  skip.mode('ios', 'ion-grid does not have different styling per-mode');
-  await page.goto(`/src/components/grid/test/sizes`);
+configs({ modes: ['ios'] }).forEach(({ title, config }) => {
+  test(title('should not have visual regressions'), async ({ page }) => {
+    await page.goto(`/src/components/grid/test/sizes`, config);
 
-  await page.setIonViewport();
+    await page.setIonViewport();
 
-  expect(await page.screenshot()).toMatchSnapshot(`grid-sizes-${page.getSnapshotSettings()}.png`);
+    expect(await page.screenshot()).toMatchSnapshot(`grid-sizes-${page.getSnapshotSettings()}.png`);
+  });
 });
