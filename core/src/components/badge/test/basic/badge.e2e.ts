42f64c221b548bef01c426ccd6ad2f00d16465f9
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 20:46:59 +0000

    test(badge): use configs

--- core/src/components/badge/test/basic/badge.e2e.ts
+++ core/src/components/badge/test/basic/badge.e2e.ts
@@ -1,12 +1,14 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('badge: rendering', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto('/src/components/badge/test/basic');
+configs().forEach(({ title, config }) => {
+  test.describe('badge: rendering', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto('/src/components/badge/test/basic', config);
 
-    await page.setIonViewport();
+      await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`badge-basic-${page.getSnapshotSettings()}.png`);
+      expect(await page.screenshot()).toMatchSnapshot(`badge-basic-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
