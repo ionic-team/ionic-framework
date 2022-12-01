6b0e1d6111ffc00da7a0bebab0017b585bd08abd
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 22:35:35 +0000

    test(progress-bar): use generators

--- core/src/components/progress-bar/test/basic/progress-bar.e2e.ts
+++ core/src/components/progress-bar/test/basic/progress-bar.e2e.ts
@@ -1,12 +1,14 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('progress-bar: basic', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto('/src/components/progress-bar/test/basic');
+configs().forEach(({ title, config }) => {
+  test.describe('progress-bar: basic', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto('/src/components/progress-bar/test/basic', config);
 
-    await page.setIonViewport();
+      await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`progress-bar-basic-${page.getSnapshotSettings()}.png`);
+      expect(await page.screenshot()).toMatchSnapshot(`progress-bar-basic-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
