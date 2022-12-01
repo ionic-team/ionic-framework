b32179f3a9a10c27461ca0a0c112a6795d2bac55
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:33:19 +0000

    test(img): use generators

--- core/src/components/img/test/basic/img.e2e.ts
+++ core/src/components/img/test/basic/img.e2e.ts
@@ -1,30 +1,32 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('img: basic', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto('/src/components/img/test/basic');
+configs().forEach(({ title, config }) => {
+  test.describe('img: basic', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto('/src/components/img/test/basic', config);
 
-    /**
-     * The first `ion-img` resolves before the spy can be created,
-     * since the image is initially visible and the IO fires.
-     *
-     * We manually look at the image's completion state to determine
-     * that it loaded, before creating a spy for the second image.
-     */
-    const img = await page.locator('.img-part img');
-    await img.evaluate((el: HTMLImageElement) => el.complete);
+      /**
+       * The first `ion-img` resolves before the spy can be created,
+       * since the image is initially visible and the IO fires.
+       *
+       * We manually look at the image's completion state to determine
+       * that it loaded, before creating a spy for the second image.
+       */
+      const img = await page.locator('.img-part img');
+      await img.evaluate((el: HTMLImageElement) => el.complete);
 
-    const ionImgDidLoad = await page.spyOnEvent('ionImgDidLoad');
-    /**
-     * Resizing the viewport will cause the intersection observers of
-     * the remaining images to fire, which will cause the `ionImgDidLoad`
-     * event to fire.
-     */
-    await page.setIonViewport();
+      const ionImgDidLoad = await page.spyOnEvent('ionImgDidLoad');
+      /**
+       * Resizing the viewport will cause the intersection observers of
+       * the remaining images to fire, which will cause the `ionImgDidLoad`
+       * event to fire.
+       */
+      await page.setIonViewport();
 
-    await ionImgDidLoad.next();
+      await ionImgDidLoad.next();
 
-    expect(await page.screenshot()).toMatchSnapshot(`img-basic-${page.getSnapshotSettings()}.png`);
+      expect(await page.screenshot()).toMatchSnapshot(`img-basic-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
--- core/src/components/img/test/draggable/img.e2e.ts
+++ core/src/components/img/test/draggable/img.e2e.ts
@@ -1,17 +1,19 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('img: draggable', () => {
-  test('should correctly set draggable attribute on inner img element', async ({ page }) => {
-    await page.goto('/src/components/img/test/draggable');
+configs().forEach(({ title, config }) => {
+  test.describe('img: draggable', () => {
+    test(title('should correctly set draggable attribute on inner img element'), async ({ page }) => {
+      await page.goto('/src/components/img/test/draggable', config);
 
-    const imgDraggableTrue = page.locator('#img-draggable-true img');
-    await expect(imgDraggableTrue).toHaveAttribute('draggable', 'true');
+      const imgDraggableTrue = page.locator('#img-draggable-true img');
+      await expect(imgDraggableTrue).toHaveAttribute('draggable', 'true');
 
-    const imgDraggableFalse = page.locator('#img-draggable-false img');
-    await expect(imgDraggableFalse).toHaveAttribute('draggable', 'false');
+      const imgDraggableFalse = page.locator('#img-draggable-false img');
+      await expect(imgDraggableFalse).toHaveAttribute('draggable', 'false');
 
-    const imgDraggableUnset = page.locator('#img-draggable-unset img');
-    await expect(imgDraggableUnset).toHaveAttribute('draggable', '');
+      const imgDraggableUnset = page.locator('#img-draggable-unset img');
+      await expect(imgDraggableUnset).toHaveAttribute('draggable', '');
+    });
   });
 });
