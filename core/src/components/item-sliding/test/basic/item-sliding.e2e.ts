3dee64cecdfcbbb05e1ac6f2c97c1448a606e3e8
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 22:03:08 +0000

    test(item-sliding): use generators

--- core/src/components/item-sliding/test/basic/item-sliding.e2e.ts
+++ core/src/components/item-sliding/test/basic/item-sliding.e2e.ts
@@ -1,73 +1,75 @@
 import { expect } from '@playwright/test';
-import { dragElementBy, test } from '@utils/test/playwright';
+import { dragElementBy, test, configs } from '@utils/test/playwright';
 
 import { testSlidingItem } from '../test.utils';
 
 test.describe('item-sliding: basic', () => {
-  test.beforeEach(async ({ skip }) => {
-    skip.mode('ios', "item-sliding doesn't have mode-specific styling");
+  configs({ modes: ['md'] }).forEach(({ title, config }) => {
+    test.describe('item-sliding: rendering', () => {
+      test(title('should not have visual regressions'), async ({ page, browserName }) => {
+        // TODO(FW-2608)
+        test.fixme(
+          browserName === 'firefox' || browserName === 'webkit',
+          'https://github.com/ionic-team/ionic-framework/issues/26103'
+        );
+
+        await page.goto(`/src/components/item-sliding/test/basic`, config);
+        const item = page.locator('#item2');
+
+        await testSlidingItem(page, item, 'start', true);
+        await testSlidingItem(page, item, 'end');
+      });
+    });
   });
 
-  test('should not have visual regressions', async ({ page, browserName }, testInfo) => {
-    // TODO(FW-2608)
-    test.fixme(
-      testInfo.project.metadata.rtl === true && (browserName === 'firefox' || browserName === 'webkit'),
-      'https://github.com/ionic-team/ionic-framework/issues/26103'
-    );
+  configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
+    test.describe('item-sliding: functionality', () => {
+      // mouse gesture is flaky on CI, skip for now
+      test.fixme(title('should open when swiped'), async ({ page, skip }) => {
+        skip.browser(
+          (browserName: string) => browserName !== 'chromium',
+          'dragElementBy is flaky outside of Chrome browsers.'
+        );
 
-    await page.goto(`/src/components/item-sliding/test/basic`);
-    const item = page.locator('#item2');
+        await page.goto(`/src/components/item-sliding/test/basic`, config);
+        const item = page.locator('#item2');
 
-    await testSlidingItem(page, item, 'start', true);
-    await testSlidingItem(page, item, 'end');
-  });
+        await dragElementBy(item, page, -150);
+        await page.waitForChanges();
 
-  // mouse gesture is flaky on CI, skip for now
-  test.fixme('should open when swiped', async ({ page, skip }) => {
-    skip.rtl();
-    skip.browser(
-      (browserName: string) => browserName !== 'chromium',
-      'dragElementBy is flaky outside of Chrome browsers.'
-    );
+        // item-sliding doesn't have an easy way to tell whether it's fully open so just screenshot it
+        expect(await item.screenshot()).toMatchSnapshot(`item-sliding-gesture-${page.getSnapshotSettings()}.png`);
+      });
 
-    await page.goto(`/src/components/item-sliding/test/basic`);
-    const item = page.locator('#item2');
+      test(title('should not scroll when the item-sliding is swiped'), async ({ page, skip }) => {
+        skip.browser('webkit', 'mouse.wheel is not available in WebKit');
 
-    await dragElementBy(item, page, -150);
-    await page.waitForChanges();
+        await page.goto(`/src/components/item-sliding/test/basic`, config);
 
-    // item-sliding doesn't have an easy way to tell whether it's fully open so just screenshot it
-    expect(await item.screenshot()).toMatchSnapshot(`item-sliding-gesture-${page.getSnapshotSettings()}.png`);
-  });
+        const itemSlidingEl = page.locator('#item2');
+        const scrollEl = page.locator('ion-content .inner-scroll');
 
-  test('should not scroll when the item-sliding is swiped', async ({ page, skip }) => {
-    skip.browser('webkit', 'mouse.wheel is not available in WebKit');
-    skip.rtl();
+        expect(await scrollEl.evaluate((el: HTMLElement) => el.scrollTop)).toEqual(0);
 
-    await page.goto(`/src/components/item-sliding/test/basic`);
+        const box = (await itemSlidingEl.boundingBox())!;
+        const centerX = box.x + box.width / 2;
+        const centerY = box.y + box.height / 2;
 
-    const itemSlidingEl = page.locator('#item2');
-    const scrollEl = page.locator('ion-content .inner-scroll');
+        await page.mouse.move(centerX, centerY);
+        await page.mouse.down();
+        await page.mouse.move(centerX - 30, centerY);
 
-    expect(await scrollEl.evaluate((el: HTMLElement) => el.scrollTop)).toEqual(0);
+        /**
+         * Do not use scrollToBottom() or other scrolling methods
+         * on ion-content as those will update the scroll position.
+         * Setting scrollTop still works even with overflow-y: hidden.
+         * However, simulating a user gesture should not scroll the content.
+         */
+        await page.mouse.wheel(0, 100);
+        await page.waitForChanges();
 
-    const box = (await itemSlidingEl.boundingBox())!;
-    const centerX = box.x + box.width / 2;
-    const centerY = box.y + box.height / 2;
-
-    await page.mouse.move(centerX, centerY);
-    await page.mouse.down();
-    await page.mouse.move(centerX - 30, centerY);
-
-    /**
-     * Do not use scrollToBottom() or other scrolling methods
-     * on ion-content as those will update the scroll position.
-     * Setting scrollTop still works even with overflow-y: hidden.
-     * However, simulating a user gesture should not scroll the content.
-     */
-    await page.mouse.wheel(0, 100);
-    await page.waitForChanges();
-
-    expect(await scrollEl.evaluate((el: HTMLElement) => el.scrollTop)).toEqual(0);
+        expect(await scrollEl.evaluate((el: HTMLElement) => el.scrollTop)).toEqual(0);
+      });
+    });
   });
 });
