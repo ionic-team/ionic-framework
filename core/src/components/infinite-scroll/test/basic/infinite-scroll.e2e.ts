3c69e0fdc57a95d6ecf17ee8abcdc9840cbf80c2
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:34:31 +0000

    test(infinite-scroll): use generators

--- core/src/components/infinite-scroll/test/basic/infinite-scroll.e2e.ts
+++ core/src/components/infinite-scroll/test/basic/infinite-scroll.e2e.ts
@@ -1,22 +1,20 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('infinite-scroll: basic', () => {
-  test.beforeEach(({ skip }) => {
-    skip.rtl();
-    skip.mode('ios');
-  });
-  test('should load more items when scrolled to the bottom', async ({ page }) => {
-    await page.goto('/src/components/infinite-scroll/test/basic');
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('infinite-scroll: basic', () => {
+    test(title('should load more items when scrolled to the bottom'), async ({ page }) => {
+      await page.goto('/src/components/infinite-scroll/test/basic', config);
 
-    const ionInfiniteComplete = await page.spyOnEvent('ionInfiniteComplete');
-    const content = page.locator('ion-content');
-    const items = page.locator('ion-item');
-    expect(await items.count()).toBe(30);
+      const ionInfiniteComplete = await page.spyOnEvent('ionInfiniteComplete');
+      const content = page.locator('ion-content');
+      const items = page.locator('ion-item');
+      expect(await items.count()).toBe(30);
 
-    await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom(0));
-    await ionInfiniteComplete.next();
+      await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom(0));
+      await ionInfiniteComplete.next();
 
-    expect(await items.count()).toBe(60);
+      expect(await items.count()).toBe(60);
+    });
   });
 });
--- core/src/components/infinite-scroll/test/scroll-target/infinite-scroll.e2e.ts
+++ core/src/components/infinite-scroll/test/scroll-target/infinite-scroll.e2e.ts
@@ -1,22 +1,20 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('infinite-scroll: scroll-target', () => {
-  test.beforeEach(({ skip }) => {
-    skip.rtl();
-    skip.mode('ios');
-  });
-  test('should load more items when scroll target is scrolled to the bottom', async ({ page }) => {
-    await page.goto('/src/components/infinite-scroll/test/scroll-target');
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('infinite-scroll: scroll-target', () => {
+    test(title('should load more items when scroll target is scrolled to the bottom'), async ({ page }) => {
+      await page.goto('/src/components/infinite-scroll/test/scroll-target', config);
 
-    const ionInfiniteComplete = await page.spyOnEvent('ionInfiniteComplete');
-    const content = page.locator('#scroll-target');
-    const items = page.locator('ion-item');
-    expect(await items.count()).toBe(30);
+      const ionInfiniteComplete = await page.spyOnEvent('ionInfiniteComplete');
+      const content = page.locator('#scroll-target');
+      const items = page.locator('ion-item');
+      expect(await items.count()).toBe(30);
 
-    await content.evaluate((el: HTMLElement) => (el.scrollTop = el.scrollHeight));
-    await ionInfiniteComplete.next();
+      await content.evaluate((el: HTMLElement) => (el.scrollTop = el.scrollHeight));
+      await ionInfiniteComplete.next();
 
-    expect(await items.count()).toBe(60);
+      expect(await items.count()).toBe(60);
+    });
   });
 });
