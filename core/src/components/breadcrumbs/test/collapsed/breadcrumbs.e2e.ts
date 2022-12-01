915b11d75eb5108dc8773f62e3032b09eb550c60
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 20:52:01 +0000

    test(breadcrumbs): use generators

--- core/src/components/breadcrumbs/test/collapsed/breadcrumbs.e2e.ts
+++ core/src/components/breadcrumbs/test/collapsed/breadcrumbs.e2e.ts
@@ -1,12 +1,14 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('breadcrumbs: collapsed', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto(`/src/components/breadcrumbs/test/collapsed`);
+configs().forEach(({ title, config }) => {
+  test.describe('breadcrumbs: collapsed', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto(`/src/components/breadcrumbs/test/collapsed`, config);
 
-    await page.setIonViewport();
+      await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`breadcrumb-collapsed-diff-${page.getSnapshotSettings()}.png`);
+      expect(await page.screenshot()).toMatchSnapshot(`breadcrumb-collapsed-diff-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
