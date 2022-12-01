cb168b95d99378d0870b717589eddaf9c6ae1da0
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:19:11 +0000

    test(fab): use generators

--- core/src/components/fab/test/basic/fab.e2e.ts
+++ core/src/components/fab/test/basic/fab.e2e.ts
@@ -1,72 +1,73 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('fab: basic (visual checks)', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(`/src/components/fab/test/basic`);
-  });
-
-  test('should not have visual regressions', async ({ page }) => {
-    await page.setIonViewport();
-
-    expect(await page.screenshot()).toMatchSnapshot(`fab-basic-${page.getSnapshotSettings()}.png`);
-  });
-
-  test('should not have visual regressions when open', async ({ page }) => {
-    // this fab has multiple buttons on each side, so it covers all the bases
-    const fab = page.locator('#fab5');
-
-    await fab.click();
-    await page.waitForChanges();
-
-    /**
-     * fab.screenshot doesn't work since ion-fab's bounding box only covers the
-     * central button. This viewport size crops extra white space while also
-     * containing all the buttons in the open fab.
-     */
-    await page.setViewportSize({
-      width: 320,
-      height: 415,
+configs().forEach(({ title, config }) => {
+  test.describe('fab: basic (visual checks)', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(`/src/components/fab/test/basic`, config);
     });
 
-    expect(await page.screenshot()).toMatchSnapshot(`fab-open-${page.getSnapshotSettings()}.png`);
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.setIonViewport();
+
+      expect(await page.screenshot()).toMatchSnapshot(`fab-basic-${page.getSnapshotSettings()}.png`);
+    });
+
+    test(title('should not have visual regressions when open'), async ({ page }) => {
+      // this fab has multiple buttons on each side, so it covers all the bases
+      const fab = page.locator('#fab5');
+
+      await fab.click();
+      await page.waitForChanges();
+
+      /**
+       * fab.screenshot doesn't work since ion-fab's bounding box only covers the
+       * central button. This viewport size crops extra white space while also
+       * containing all the buttons in the open fab.
+       */
+      await page.setViewportSize({
+        width: 320,
+        height: 415,
+      });
+
+      expect(await page.screenshot()).toMatchSnapshot(`fab-open-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
 
-test.describe('fab: basic (functionality checks)', () => {
-  test.beforeEach(async ({ page, skip }) => {
-    skip.rtl();
-    skip.mode('ios');
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('fab: basic (functionality checks)', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(`/src/components/fab/test/basic`, config);
+    });
 
-    await page.goto(`/src/components/fab/test/basic`);
-  });
+    test(title('should toggle active state when clicked'), async ({ page }) => {
+      const fab = page.locator('#fab1');
+      const fabList = fab.locator('ion-fab-list');
 
-  test('should toggle active state when clicked', async ({ page }) => {
-    const fab = page.locator('#fab1');
-    const fabList = fab.locator('ion-fab-list');
+      expect(fabList).not.toHaveClass(/fab-list-active/);
 
-    expect(fabList).not.toHaveClass(/fab-list-active/);
+      // open fab
+      await fab.click();
+      await page.waitForChanges();
+      expect(fabList).toHaveClass(/fab-list-active/);
 
-    // open fab
-    await fab.click();
-    await page.waitForChanges();
-    expect(fabList).toHaveClass(/fab-list-active/);
+      // close fab
+      await fab.click();
+      await page.waitForChanges();
+      expect(fabList).not.toHaveClass(/fab-list-active/);
+    });
 
-    // close fab
-    await fab.click();
-    await page.waitForChanges();
-    expect(fabList).not.toHaveClass(/fab-list-active/);
-  });
+    test(title('should not open when disabled'), async ({ page }) => {
+      const fab = page.locator('#fab2');
+      const fabList = fab.locator('ion-fab-list');
 
-  test('should not open when disabled', async ({ page }) => {
-    const fab = page.locator('#fab2');
-    const fabList = fab.locator('ion-fab-list');
+      expect(fabList).not.toHaveClass(/fab-list-active/);
 
-    expect(fabList).not.toHaveClass(/fab-list-active/);
-
-    // attempt to open fab
-    await fab.click();
-    await page.waitForChanges();
-    expect(fabList).not.toHaveClass(/fab-list-active/);
+      // attempt to open fab
+      await fab.click();
+      await page.waitForChanges();
+      expect(fabList).not.toHaveClass(/fab-list-active/);
+    });
   });
 });
--- core/src/components/fab/test/states/fab.e2e.ts
+++ core/src/components/fab/test/states/fab.e2e.ts
@@ -1,11 +1,12 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test('should not have visual regressions', async ({ page, skip }) => {
-  skip.rtl();
-  await page.goto(`/src/components/fab/test/states`);
+configs({ directions: ['ltr'] }).forEach(({ title, config }) => {
+  test(title('should not have visual regressions'), async ({ page }) => {
+    await page.goto(`/src/components/fab/test/states`, config);
 
-  await page.setIonViewport();
+    await page.setIonViewport();
 
-  expect(await page.screenshot()).toMatchSnapshot(`fab-states-${page.getSnapshotSettings()}.png`);
+    expect(await page.screenshot()).toMatchSnapshot(`fab-states-${page.getSnapshotSettings()}.png`);
+  });
 });
