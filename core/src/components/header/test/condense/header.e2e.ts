4057c9c168592209870cc5a73ce1eb42ff762403
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-01 21:30:49 +0000

    test(header): use config

--- core/src/components/header/test/condense/header.e2e.ts
+++ core/src/components/header/test/condense/header.e2e.ts
@@ -1,43 +1,42 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('header: condense', () => {
-  test('should be hidden from screen readers when collapsed', async ({ page, skip }) => {
-    skip.mode('md');
-    skip.rtl();
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('header: condense', () => {
+    test(title('should be hidden from screen readers when collapsed'), async ({ page }) => {
+      await page.goto('/src/components/header/test/condense', config);
+      const largeTitleHeader = page.locator('#largeTitleHeader');
+      const smallTitleHeader = page.locator('#smallTitleHeader');
+      const content = page.locator('ion-content');
 
-    await page.goto('/src/components/header/test/condense');
-    const largeTitleHeader = page.locator('#largeTitleHeader');
-    const smallTitleHeader = page.locator('#smallTitleHeader');
-    const content = page.locator('ion-content');
+      await expect(smallTitleHeader).toHaveAttribute('aria-hidden', 'true');
 
-    await expect(smallTitleHeader).toHaveAttribute('aria-hidden', 'true');
+      expect(await largeTitleHeader.screenshot({ animations: 'disabled' })).toMatchSnapshot(
+        `header-condense-large-title-initial-diff-${page.getSnapshotSettings()}.png`
+      );
 
-    expect(await largeTitleHeader.screenshot({ animations: 'disabled' })).toMatchSnapshot(
-      `header-condense-large-title-initial-diff-${page.getSnapshotSettings()}.png`
-    );
+      await content.evaluate(async (el: HTMLIonContentElement) => {
+        await el.scrollToBottom();
+      });
+      await page.waitForSelector('#largeTitleHeader.header-collapse-condense-inactive');
 
-    await content.evaluate(async (el: HTMLIonContentElement) => {
-      await el.scrollToBottom();
+      expect(await smallTitleHeader.screenshot({ animations: 'disabled' })).toMatchSnapshot(
+        `header-condense-large-title-collapsed-diff-${page.getSnapshotSettings()}.png`
+      );
+
+      /**
+       * Playwright can't do .not.toHaveAttribute() because a value is expected,
+       * and toHaveAttribute can't accept a value of type null.
+       */
+      const ariaHidden = await smallTitleHeader.getAttribute('aria-hidden');
+      expect(ariaHidden).toBeNull();
+
+      await content.evaluate(async (el: HTMLIonContentElement) => {
+        await el.scrollToTop();
+      });
+      await page.waitForSelector('#smallTitleHeader.header-collapse-condense-inactive');
+
+      await expect(smallTitleHeader).toHaveAttribute('aria-hidden', 'true');
     });
-    await page.waitForSelector('#largeTitleHeader.header-collapse-condense-inactive');
-
-    expect(await smallTitleHeader.screenshot({ animations: 'disabled' })).toMatchSnapshot(
-      `header-condense-large-title-collapsed-diff-${page.getSnapshotSettings()}.png`
-    );
-
-    /**
-     * Playwright can't do .not.toHaveAttribute() because a value is expected,
-     * and toHaveAttribute can't accept a value of type null.
-     */
-    const ariaHidden = await smallTitleHeader.getAttribute('aria-hidden');
-    expect(ariaHidden).toBeNull();
-
-    await content.evaluate(async (el: HTMLIonContentElement) => {
-      await el.scrollToTop();
-    });
-    await page.waitForSelector('#smallTitleHeader.header-collapse-condense-inactive');
-
-    await expect(smallTitleHeader).toHaveAttribute('aria-hidden', 'true');
   });
 });
--- core/src/components/header/test/fade/header.e2e.ts
+++ core/src/components/header/test/fade/header.e2e.ts
@@ -1,28 +1,25 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('header: fade', () => {
-  test.beforeEach(({ skip }) => {
-    skip.rtl();
-  });
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('header: fade', () => {
+    test(title('should not have visual regressions with fade header'), async ({ page, skip }) => {
+      skip.browser('firefox', 'Firefox has some issues rendering translucent effects on Linux.');
 
-  test('should not have visual regressions with fade header', async ({ page, skip }) => {
-    skip.mode('md', 'Translucent effect is only available in iOS mode.');
-    skip.browser('firefox', 'Firefox has some issues rendering translucent effects on Linux.');
+      await page.goto('/src/components/header/test/fade', config);
 
-    await page.goto('/src/components/header/test/fade');
+      const header = page.locator('ion-header');
+      expect(await header.screenshot({ animations: 'disabled' })).toMatchSnapshot(
+        `header-fade-not-blurred-diff-${page.getSnapshotSettings()}.png`
+      );
 
-    const header = page.locator('ion-header');
-    expect(await header.screenshot({ animations: 'disabled' })).toMatchSnapshot(
-      `header-fade-not-blurred-diff-${page.getSnapshotSettings()}.png`
-    );
+      const content = page.locator('ion-content');
+      await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom(0));
+      await page.waitForChanges();
 
-    const content = page.locator('ion-content');
-    await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom(0));
-    await page.waitForChanges();
-
-    expect(await header.screenshot({ animations: 'disabled' })).toMatchSnapshot(
-      `header-fade-blurred-diff-${page.getSnapshotSettings()}.png`
-    );
+      expect(await header.screenshot({ animations: 'disabled' })).toMatchSnapshot(
+        `header-fade-blurred-diff-${page.getSnapshotSettings()}.png`
+      );
+    });
   });
 });
