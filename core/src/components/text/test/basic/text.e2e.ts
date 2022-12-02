e93b446de11cce5a5082d670b43e5f4bd7724116
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-02 18:42:23 +0000

    test(text): use generators

--- core/src/components/text/test/basic/text.e2e.ts
+++ core/src/components/text/test/basic/text.e2e.ts
@@ -1,29 +1,33 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('text: basic', () => {
-  test.beforeEach(({ skip }) => {
-    skip.rtl();
-    skip.mode('ios', 'Text does not have per-mode styles');
-  });
-  test('should render default text', async ({ page }) => {
-    await page.setContent(`
-      <ion-text>
-        <strong>The quick brown fox <ion-text><sup>jumps</sup></ion-text> over the <ion-text><sub>lazy dog</sub></ion-text></strong>
-      </ion-text>
-    `);
+configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('text: basic', () => {
+    test(title('should render default text'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-text>
+          <strong>The quick brown fox <ion-text><sup>jumps</sup></ion-text> over the <ion-text><sub>lazy dog</sub></ion-text></strong>
+        </ion-text>
+      `,
+        config
+      );
 
-    const text = page.locator('ion-text');
-    expect(await text.nth(0).screenshot()).toMatchSnapshot(`text-${page.getSnapshotSettings()}.png`);
-  });
-  test('should render text with color prop', async ({ page }) => {
-    await page.setContent(`
-      <ion-text color="primary">
-        <strong>The quick brown fox <ion-text color="success"><sup>jumps</sup></ion-text> over the <ion-text color="danger"><sub>lazy dog</sub></ion-text></strong>
-      </ion-text>
-    `);
+      const text = page.locator('ion-text');
+      expect(await text.nth(0).screenshot()).toMatchSnapshot(`text-${page.getSnapshotSettings()}.png`);
+    });
+    test(title('should render text with color prop'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-text color="primary">
+          <strong>The quick brown fox <ion-text color="success"><sup>jumps</sup></ion-text> over the <ion-text color="danger"><sub>lazy dog</sub></ion-text></strong>
+        </ion-text>
+      `,
+        config
+      );
 
-    const text = page.locator('ion-text');
-    expect(await text.nth(0).screenshot()).toMatchSnapshot(`text-color-${page.getSnapshotSettings()}.png`);
+      const text = page.locator('ion-text');
+      expect(await text.nth(0).screenshot()).toMatchSnapshot(`text-color-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
