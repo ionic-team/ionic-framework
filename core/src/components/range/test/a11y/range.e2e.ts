f2ba4b6c60a2581bdc3f9b998048780465662520
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-02 18:11:51 +0000

    test(range): use generators

--- core/src/components/range/test/a11y/range.e2e.ts
+++ core/src/components/range/test/a11y/range.e2e.ts
@@ -1,50 +1,17 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('range: a11y', () => {
-  test('should not have visual regressions', async ({ page, skip }) => {
-    skip.rtl();
-    skip.mode('ios', 'iOS mode does not display hover/active/focus styles.');
-
-    await page.setContent(
-      `<ion-app>
-        <ion-content>
-          <ion-range min="0" max="100" value="80"></ion-range>
-        </ion-content>
-      </ion-app>
-        `
-    );
-
-    const range = page.locator('ion-range');
-    const rangeHandle = range.locator('.range-knob-handle');
-
-    await rangeHandle.evaluate((el) => el.classList.add('ion-focused'));
-    await page.waitForChanges();
-
-    expect(await range.screenshot()).toMatchSnapshot(`range-focus-${page.getSnapshotSettings()}.png`);
-
-    const box = (await rangeHandle.boundingBox())!;
-    const centerX = box.x + box.width / 2;
-    const centerY = box.y + box.height / 2;
-
-    await page.mouse.move(centerX, centerY);
-    await page.mouse.down();
-    await page.waitForChanges();
-
-    expect(await range.screenshot()).toMatchSnapshot(`range-active-${page.getSnapshotSettings()}.png`);
-  });
-
-  test.describe('with pin', () => {
-    test('should not have visual regressions', async ({ page, skip }) => {
-      skip.rtl();
-
+configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('range: a11y', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
       await page.setContent(
         `<ion-app>
           <ion-content>
-            <ion-range min="0" max="100" value="50" pin="true"></ion-range>
+            <ion-range min="0" max="100" value="80"></ion-range>
           </ion-content>
         </ion-app>
-        `
+          `,
+        config
       );
 
       const range = page.locator('ion-range');
@@ -53,7 +20,39 @@ test.describe('range: a11y', () => {
       await rangeHandle.evaluate((el) => el.classList.add('ion-focused'));
       await page.waitForChanges();
 
-      expect(await range.screenshot()).toMatchSnapshot(`range-focus-with-pin-${page.getSnapshotSettings()}.png`);
+      expect(await range.screenshot()).toMatchSnapshot(`range-focus-${page.getSnapshotSettings()}.png`);
+
+      const box = (await rangeHandle.boundingBox())!;
+      const centerX = box.x + box.width / 2;
+      const centerY = box.y + box.height / 2;
+
+      await page.mouse.move(centerX, centerY);
+      await page.mouse.down();
+      await page.waitForChanges();
+
+      expect(await range.screenshot()).toMatchSnapshot(`range-active-${page.getSnapshotSettings()}.png`);
+    });
+
+    test.describe('with pin', () => {
+      test(title('should not have visual regressions'), async ({ page }) => {
+        await page.setContent(
+          `<ion-app>
+            <ion-content>
+              <ion-range min="0" max="100" value="50" pin="true"></ion-range>
+            </ion-content>
+          </ion-app>
+          `,
+          config
+        );
+
+        const range = page.locator('ion-range');
+        const rangeHandle = range.locator('.range-knob-handle');
+
+        await rangeHandle.evaluate((el) => el.classList.add('ion-focused'));
+        await page.waitForChanges();
+
+        expect(await range.screenshot()).toMatchSnapshot(`range-focus-with-pin-${page.getSnapshotSettings()}.png`);
+      });
     });
   });
 });
