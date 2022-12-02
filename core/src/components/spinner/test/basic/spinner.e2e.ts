0aafa847c19571840806670544f54c361f518480
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-02 18:36:55 +0000

    test(spinner): use generators

--- core/src/components/spinner/test/basic/spinner.e2e.ts
+++ core/src/components/spinner/test/basic/spinner.e2e.ts
@@ -1,15 +1,17 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('spinner: basic', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/spinner/test/basic');
-  });
-  test.describe('spinner: visual regression tests', () => {
-    test('should not have visual regressions', async ({ page }) => {
-      await page.setIonViewport();
+configs().forEach(({ title, config }) => {
+  test.describe('spinner: basic', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/src/components/spinner/test/basic', config);
+    });
+    test.describe('spinner: visual regression tests', () => {
+      test(title('should not have visual regressions'), async ({ page }) => {
+        await page.setIonViewport();
 
-      expect(await page.screenshot()).toMatchSnapshot(`spinner-basic-diff-${page.getSnapshotSettings()}.png`);
+        expect(await page.screenshot()).toMatchSnapshot(`spinner-basic-diff-${page.getSnapshotSettings()}.png`);
+      });
     });
   });
 });
