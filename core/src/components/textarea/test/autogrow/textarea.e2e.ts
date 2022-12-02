25afe617efacde2c6d9fdf6f603777b79d49f6c4
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-02 18:44:40 +0000

    test(textarea): use generators

--- core/src/components/textarea/test/autogrow/textarea.e2e.ts
+++ core/src/components/textarea/test/autogrow/textarea.e2e.ts
@@ -1,72 +1,78 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('textarea: autogrow', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto(`/src/components/textarea/test/autogrow`);
+configs().forEach(({ title, config }) => {
+  test.describe('textarea: autogrow', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto(`/src/components/textarea/test/autogrow`, config);
 
-    await page.setIonViewport();
+      await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`textarea-autogrow-diff-${page.getSnapshotSettings()}.png`);
-  });
-
-  test('should grow when typing', async ({ page }) => {
-    await page.setContent(
-      `
-      <ion-app>
-        <ion-content>
-          <ion-list>
-            <ion-item>
-              <ion-textarea auto-grow="true"></ion-textarea>
-            </ion-item>
-          </ion-list>
-        </ion-content>
-      </ion-app>`
-    );
-
-    const textarea = await page.waitForSelector('ion-textarea');
-
-    await textarea.click();
-
-    await textarea.type('Now, this is a story all about how');
-
-    await page.setIonViewport();
-
-    expect(await textarea.screenshot()).toMatchSnapshot(`textarea-autogrow-initial-${page.getSnapshotSettings()}.png`);
-
-    await textarea.type(
-      [
-        `\nMy life got flipped-turned upside down`,
-        `And I'd like to take a minute`,
-        `Just sit right there`,
-        `I'll tell you how I became the prince of a town called Bel-Air`,
-      ].join('\n')
-    );
-
-    expect(await textarea.screenshot()).toMatchSnapshot(`textarea-autogrow-after-${page.getSnapshotSettings()}.png`);
-  });
-
-  test('should break long lines without white space', async ({ page }) => {
-    test.info().annotations.push({
-      type: 'issue',
-      description: 'https://github.com/ionic-team/ionic-framework/issues/25893',
+      expect(await page.screenshot()).toMatchSnapshot(`textarea-autogrow-diff-${page.getSnapshotSettings()}.png`);
     });
 
-    await page.setContent(
-      `<ion-app>
-        <ion-content>
-          <ion-textarea
-            auto-grow="true"
-            value="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz">
-          </ion-textarea>
-        </ion-content>
-      </ion-app>`
-    );
+    test(title('should grow when typing'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-app>
+          <ion-content>
+            <ion-list>
+              <ion-item>
+                <ion-textarea auto-grow="true"></ion-textarea>
+              </ion-item>
+            </ion-list>
+          </ion-content>
+        </ion-app>`,
+        config
+      );
 
-    const textarea = await page.locator('ion-textarea');
+      const textarea = await page.waitForSelector('ion-textarea');
 
-    expect(await textarea.screenshot()).toMatchSnapshot(
-      `textarea-autogrow-word-break-${page.getSnapshotSettings()}.png`
-    );
+      await textarea.click();
+
+      await textarea.type('Now, this is a story all about how');
+
+      await page.setIonViewport();
+
+      expect(await textarea.screenshot()).toMatchSnapshot(
+        `textarea-autogrow-initial-${page.getSnapshotSettings()}.png`
+      );
+
+      await textarea.type(
+        [
+          `\nMy life got flipped-turned upside down`,
+          `And I'd like to take a minute`,
+          `Just sit right there`,
+          `I'll tell you how I became the prince of a town called Bel-Air`,
+        ].join('\n')
+      );
+
+      expect(await textarea.screenshot()).toMatchSnapshot(`textarea-autogrow-after-${page.getSnapshotSettings()}.png`);
+    });
+
+    test(title('should break long lines without white space'), async ({ page }) => {
+      test.info().annotations.push({
+        type: 'issue',
+        description: 'https://github.com/ionic-team/ionic-framework/issues/25893',
+      });
+
+      await page.setContent(
+        `<ion-app>
+          <ion-content>
+            <ion-textarea
+              auto-grow="true"
+              value="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz">
+            </ion-textarea>
+          </ion-content>
+        </ion-app>`,
+        config
+      );
+
+      const textarea = await page.locator('ion-textarea');
+
+      expect(await textarea.screenshot()).toMatchSnapshot(
+        `textarea-autogrow-word-break-${page.getSnapshotSettings()}.png`
+      );
+    });
   });
 });
