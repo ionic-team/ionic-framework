a0cc4eb28424d99db8573602ff082bd379b722d4
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-02 18:05:57 +0000

    test(radio-group): use generators

--- core/src/components/radio-group/test/basic/radio-group.e2e.ts
+++ core/src/components/radio-group/test/basic/radio-group.e2e.ts
@@ -1,80 +1,95 @@
 import { expect } from '@playwright/test';
 import type { Locator } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 import type { E2EPage } from '@utils/test/playwright';
 
-test.describe('radio-group: basic', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto(`/src/components/radio-group/test/basic`);
+configs().forEach(({ title, config }) => {
+  test.describe('radio-group: basic', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto(`/src/components/radio-group/test/basic`, config);
 
-    const list = page.locator('ion-list');
+      const list = page.locator('ion-list');
 
-    expect(await list.screenshot()).toMatchSnapshot(`radio-group-diff-${page.getSnapshotSettings()}.png`);
+      expect(await list.screenshot()).toMatchSnapshot(`radio-group-diff-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
 
-test.describe('radio-group: interaction', () => {
-  let radioFixture: RadioFixture;
+configs({ directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe('radio-group: interaction', () => {
+    let radioFixture: RadioFixture;
 
-  test.beforeEach(({ page, skip }) => {
-    skip.rtl();
-    radioFixture = new RadioFixture(page);
-  });
+    test.beforeEach(({ page }) => {
+      radioFixture = new RadioFixture(page);
+    });
 
-  test('spacebar should not deselect without allowEmptySelection', async ({ page }) => {
-    await page.setContent(`
-      <ion-radio-group value="one" allow-empty-selection="false">
-        <ion-item>
-          <ion-label>One</ion-label>
-          <ion-radio id="one" value="one"></ion-radio>
-        </ion-item>
-      </ion-radio-group>
-    `);
+    test(title('spacebar should not deselect without allowEmptySelection'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-radio-group value="one" allow-empty-selection="false">
+          <ion-item>
+            <ion-label>One</ion-label>
+            <ion-radio id="one" value="one"></ion-radio>
+          </ion-item>
+        </ion-radio-group>
+      `,
+        config
+      );
 
-    await radioFixture.checkRadio('keyboard');
-    await radioFixture.expectChecked(true);
-  });
+      await radioFixture.checkRadio('keyboard');
+      await radioFixture.expectChecked(true);
+    });
 
-  test('spacebar should deselect with allowEmptySelection', async ({ page }) => {
-    await page.setContent(`
-      <ion-radio-group value="one" allow-empty-selection="true">
-        <ion-item>
-          <ion-label>One</ion-label>
-          <ion-radio id="one" value="one"></ion-radio>
-        </ion-item>
-      </ion-radio-group>
-    `);
+    test(title('spacebar should deselect with allowEmptySelection'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-radio-group value="one" allow-empty-selection="true">
+          <ion-item>
+            <ion-label>One</ion-label>
+            <ion-radio id="one" value="one"></ion-radio>
+          </ion-item>
+        </ion-radio-group>
+      `,
+        config
+      );
 
-    await radioFixture.checkRadio('keyboard');
-    await radioFixture.expectChecked(false);
-  });
+      await radioFixture.checkRadio('keyboard');
+      await radioFixture.expectChecked(false);
+    });
 
-  test('click should not deselect without allowEmptySelection', async ({ page }) => {
-    await page.setContent(`
-      <ion-radio-group value="one" allow-empty-selection="false">
-        <ion-item>
-          <ion-label>One</ion-label>
-          <ion-radio id="one" value="one"></ion-radio>
-        </ion-item>
-      </ion-radio-group>
-    `);
+    test(title('click should not deselect without allowEmptySelection'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-radio-group value="one" allow-empty-selection="false">
+          <ion-item>
+            <ion-label>One</ion-label>
+            <ion-radio id="one" value="one"></ion-radio>
+          </ion-item>
+        </ion-radio-group>
+      `,
+        config
+      );
 
-    await radioFixture.checkRadio('mouse');
-    await radioFixture.expectChecked(true);
-  });
+      await radioFixture.checkRadio('mouse');
+      await radioFixture.expectChecked(true);
+    });
 
-  test('click should deselect with allowEmptySelection', async ({ page }) => {
-    await page.setContent(`
-      <ion-radio-group value="one" allow-empty-selection="true">
-        <ion-item>
-          <ion-label>One</ion-label>
-          <ion-radio id="one" value="one"></ion-radio>
-        </ion-item>
-      </ion-radio-group>
-    `);
+    test(title('click should deselect with allowEmptySelection'), async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-radio-group value="one" allow-empty-selection="true">
+          <ion-item>
+            <ion-label>One</ion-label>
+            <ion-radio id="one" value="one"></ion-radio>
+          </ion-item>
+        </ion-radio-group>
+      `,
+        config
+      );
 
-    await radioFixture.checkRadio('mouse');
-    await radioFixture.expectChecked(false);
+      await radioFixture.checkRadio('mouse');
+      await radioFixture.expectChecked(false);
+    });
   });
 });
 
--- core/src/components/radio-group/test/form/radio-group.e2e.ts
+++ core/src/components/radio-group/test/form/radio-group.e2e.ts
@@ -1,34 +1,36 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('radio-group: form', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/radio-group/test/form');
-  });
+configs().forEach(({ title, config }) => {
+  test.describe('radio-group: form', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/src/components/radio-group/test/form', config);
+    });
 
-  test('selecting an option should update the value', async ({ page }) => {
-    const radioGroup = page.locator('ion-radio-group');
-    const ionChange = await page.spyOnEvent('ionChange');
-    const griffRadio = page.locator('ion-radio[value="griff"]');
+    test(title('selecting an option should update the value'), async ({ page }) => {
+      const radioGroup = page.locator('ion-radio-group');
+      const ionChange = await page.spyOnEvent('ionChange');
+      const griffRadio = page.locator('ion-radio[value="griff"]');
 
-    await expect(radioGroup).toHaveAttribute('value', 'biff');
+      await expect(radioGroup).toHaveAttribute('value', 'biff');
 
-    await griffRadio.click();
-    await page.waitForChanges();
+      await griffRadio.click();
+      await page.waitForChanges();
 
-    await expect(ionChange).toHaveReceivedEventDetail({ value: 'griff' });
-  });
+      await expect(ionChange).toHaveReceivedEventDetail({ value: 'griff' });
+    });
 
-  test('selecting a disabled option should not update the value', async ({ page }) => {
-    const value = page.locator('#value');
-    const disabledRadio = page.locator('ion-radio[value="george"]');
+    test(title('selecting a disabled option should not update the value'), async ({ page }) => {
+      const value = page.locator('#value');
+      const disabledRadio = page.locator('ion-radio[value="george"]');
 
-    await expect(value).toHaveText('');
-    await expect(disabledRadio).toHaveAttribute('disabled', '');
+      await expect(value).toHaveText('');
+      await expect(disabledRadio).toHaveAttribute('disabled', '');
 
-    await disabledRadio.click({ force: true });
-    await page.waitForChanges();
+      await disabledRadio.click({ force: true });
+      await page.waitForChanges();
 
-    await expect(value).toHaveText('');
+      await expect(value).toHaveText('');
+    });
   });
 });
