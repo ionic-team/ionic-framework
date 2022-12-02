747db962ba5f285b1c04ba0547275ef0a7077501
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-02 18:35:44 +0000

    test(skeleton-text): use generators

--- core/src/components/skeleton-text/test/custom/skeleton-text.e2e.ts
+++ core/src/components/skeleton-text/test/custom/skeleton-text.e2e.ts
@@ -1,12 +1,14 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('skeleton-text: custom', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto('/src/components/skeleton-text/test/custom');
+configs().forEach(({ title, config }) => {
+  test.describe('skeleton-text: custom', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto('/src/components/skeleton-text/test/custom', config);
 
-    await page.setIonViewport();
+      await page.setIonViewport();
 
-    expect(await page.screenshot()).toMatchSnapshot(`skeleton-text-custom-${page.getSnapshotSettings()}.png`);
+      expect(await page.screenshot()).toMatchSnapshot(`skeleton-text-custom-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
