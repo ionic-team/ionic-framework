27440aad1f19dab47c7c7161d85839f5b2c6faef
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-02 18:46:22 +0000

    test(title): use generators

--- core/src/components/title/test/basic/title.e2e.ts
+++ core/src/components/title/test/basic/title.e2e.ts
@@ -1,12 +1,14 @@
 import { expect } from '@playwright/test';
-import { test } from '@utils/test/playwright';
+import { test, configs } from '@utils/test/playwright';
 
-test.describe('title: basic', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto('/src/components/title/test/basic');
-    const wrapper = page.locator('#header-wrapper');
+configs().forEach(({ title, config }) => {
+  test.describe('title: basic', () => {
+    test(title('should not have visual regressions'), async ({ page }) => {
+      await page.goto('/src/components/title/test/basic', config);
+      const wrapper = page.locator('#header-wrapper');
 
-    // only screenshot the headers to avoid unnecessary blank space from ion-content
-    expect(await wrapper.screenshot()).toMatchSnapshot(`title-basic-${page.getSnapshotSettings()}.png`);
+      // only screenshot the headers to avoid unnecessary blank space from ion-content
+      expect(await wrapper.screenshot()).toMatchSnapshot(`title-basic-${page.getSnapshotSettings()}.png`);
+    });
   });
 });
