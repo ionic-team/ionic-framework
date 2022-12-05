a836ed6a8af8df4fcba6b7808020cf230a9572ec
Sean Perkins <sean@ionic.io>
2022-12-05 16:52:14 -0500

    fix: add legacy flag to legacy html form control element type

--- core/src/utils/forms/form-controller.ts
+++ core/src/utils/forms/form-controller.ts
@@ -1,4 +1,4 @@
-type HTMLLegacyFormControlElement = HTMLElement & { label?: string };
+type HTMLLegacyFormControlElement = HTMLElement & { label?: string; legacy?: boolean };
 
 /**
  * Creates a controller that tracks whether a form control is using the legacy or modern syntax. This should be removed when the legacy form control syntax is removed.
