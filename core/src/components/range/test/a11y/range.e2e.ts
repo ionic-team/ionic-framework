de92c9ba21e9966f548db939d26852672377df8a
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-07 19:26:09 +0000

    test(range): update a11y test

--- core/src/components/range/test/a11y/range.e2e.ts
+++ core/src/components/range/test/a11y/range.e2e.ts
@@ -11,7 +11,12 @@ test.describe('range: a11y', () => {
   test('should not have accessibility violations', async ({ page }) => {
     await page.goto(`/src/components/range/test/a11y`);
 
-    const results = await new AxeBuilder({ page }).analyze();
+    /**
+     * Axe does not take <slot> elements into account
+     * when checking color-contrast. As a result, it will
+     * incorrectly report color-contrast issues: https://github.com/dequelabs/axe-core/issues/3329
+     */
+    const results = await new AxeBuilder({ page }).disableRules('color-contrast').analyze();
     expect(results.violations).toEqual([]);
   });
 });
