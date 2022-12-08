b98bfb5f28efbd301c8cda47acca4c00ca9851dc
amandaesmith3 <amanda.s@ionic.io>
2022-12-08 14:59:10 -0600

    test(checkbox): re-add tests for new markup

--- core/src/components/checkbox/test/basic/checkbox.e2e.ts
+++ core/src/components/checkbox/test/basic/checkbox.e2e.ts
@@ -1,13 +1,12 @@
 import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
-test.describe('checkbox: basic (legacy)', () => {
+test.describe('checkbox: basic', () => {
   test('should not have visual regressions', async ({ page }) => {
-    await page.goto(`/src/components/checkbox/test/legacy/basic`);
+    await page.goto(`/src/components/checkbox/test/basic`);
 
-    await page.setIonViewport();
-
-    expect(await page.screenshot()).toMatchSnapshot(`checkbox-legacy-basic-${page.getSnapshotSettings()}.png`);
+    const content = page.locator('#checkboxes');
+    expect(await content.screenshot()).toMatchSnapshot(`checkbox-basic-${page.getSnapshotSettings()}.png`);
   });
 });
 
@@ -15,25 +14,26 @@ test.describe('checkbox: ionChange', () => {
   test.beforeEach(({ skip }) => {
     skip.rtl();
   });
+
   test('should fire ionChange when interacting with checkbox', async ({ page }) => {
     await page.setContent(`
-      <ion-checkbox value="my-checkbox"></ion-checkbox>
+      <ion-checkbox aria-label="checkbox" value="my-checkbox"></ion-checkbox>
     `);
 
     const ionChange = await page.spyOnEvent('ionChange');
     const checkbox = page.locator('ion-checkbox');
 
     await checkbox.click();
-    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: true });
+    expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: true });
 
     await checkbox.click();
-    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: false });
+    expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: false });
   });
 
   test('should fire ionChange when interacting with checkbox in item', async ({ page }) => {
     await page.setContent(`
       <ion-item>
-        <ion-checkbox value="my-checkbox"></ion-checkbox>
+        <ion-checkbox aria-label="checkbox" value="my-checkbox"></ion-checkbox>
       </ion-item>
     `);
 
@@ -41,21 +41,21 @@ test.describe('checkbox: ionChange', () => {
     const item = page.locator('ion-item');
 
     await item.click();
-    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: true });
+    expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: true });
 
     await item.click();
-    await expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: false });
+    expect(ionChange).toHaveReceivedEventDetail({ value: 'my-checkbox', checked: false });
   });
 
   test('should not fire when programmatically setting a value', async ({ page }) => {
     await page.setContent(`
-      <ion-checkbox value="my-checkbox"></ion-checkbox>
+      <ion-checkbox aria-label="checkbox" value="my-checkbox"></ion-checkbox>
     `);
 
     const ionChange = await page.spyOnEvent('ionChange');
     const checkbox = page.locator('ion-checkbox');
 
     await checkbox.evaluate((el: HTMLIonCheckboxElement) => (el.checked = true));
-    await expect(ionChange).not.toHaveReceivedEvent();
+    expect(ionChange).not.toHaveReceivedEvent();
   });
 });
