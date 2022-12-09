51b955ffa256a94e3dd59d7b46f4b8273401337b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-09 16:10:18 +0000

    chore(): use light background to see border radius

--- core/src/components/list/test/inset/list.e2e.ts
+++ core/src/components/list/test/inset/list.e2e.ts
@@ -5,13 +5,15 @@ test.describe('list: inset', () => {
   test.describe('list: rendering', () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.setContent(`
-        <ion-list inset="true">
-          <ion-item>Pokémon Yellow</ion-item>
-          <ion-item>Super Metroid</ion-item>
-          <ion-item>Mega Man X</ion-item>
-          <ion-item>The Legend of Zelda</ion-item>
-          <ion-item lines="full">Halo</ion-item>
-        </ion-list>
+        <ion-content color="primary">
+          <ion-list inset="true">
+            <ion-item>Pokémon Yellow</ion-item>
+            <ion-item>Super Metroid</ion-item>
+            <ion-item>Mega Man X</ion-item>
+            <ion-item>The Legend of Zelda</ion-item>
+            <ion-item lines="full">Halo</ion-item>
+          </ion-list>
+        </ion-content>
       `);
 
       const list = page.locator('ion-list');
