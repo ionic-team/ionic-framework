b773d2a849aaf0e1d086b5b673a4c9f3c6220ccd
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-12 21:54:49 +0000

    test(modal): add missing page param

--- core/src/components/modal/test/canDismiss/modal.e2e.ts
+++ core/src/components/modal/test/canDismiss/modal.e2e.ts
@@ -386,7 +386,7 @@ test.describe('modal: canDismiss', () => {
   });
 
   test.describe('function params', () => {
-    test.beforeEach(async ({ skip }) => {
+    test.beforeEach(async ({ page, skip }) => {
       skip.mode('md');
       await page.goto('/src/components/modal/test/canDismiss');
     });
