ae5ddd8d170d7efce1e78b4690704526589c9915
amandaesmith3 <amanda.s@ionic.io>
2022-12-13 12:17:38 -0600

    chore(): lint

--- core/src/components/checkbox/test/indeterminate/checkbox.e2e.ts
+++ core/src/components/checkbox/test/indeterminate/checkbox.e2e.ts
@@ -4,7 +4,7 @@ import { test } from '@utils/test/playwright';
 test.describe('checkbox: indeterminate', () => {
   test('should not have visual regressions', async ({ page, skip }) => {
     skip.rtl();
-    
+
     await page.goto(`/src/components/checkbox/test/indeterminate`);
 
     const content = page.locator('#checkboxes');
