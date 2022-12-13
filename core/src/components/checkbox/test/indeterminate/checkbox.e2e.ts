3d6f45f4052703372776df41df4cf1d732bbc4ad
amandaesmith3 <amanda.s@ionic.io>
2022-12-13 12:15:34 -0600

    test(checkbox): add skips

--- core/src/components/checkbox/test/indeterminate/checkbox.e2e.ts
+++ core/src/components/checkbox/test/indeterminate/checkbox.e2e.ts
@@ -2,7 +2,9 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('checkbox: indeterminate', () => {
-  test('should not have visual regressions', async ({ page }) => {
+  test('should not have visual regressions', async ({ page, skip }) => {
+    skip.rtl();
+    
     await page.goto(`/src/components/checkbox/test/indeterminate`);
 
     const content = page.locator('#checkboxes');
