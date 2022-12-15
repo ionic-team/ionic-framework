06ed058ebc608a19d75a98cd3c30c12c9da98950
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-15 14:51:10 +0000

    test(radio-group): add await to async asset

--- core/src/components/radio-group/test/search/radio-group.e2e.ts
+++ core/src/components/radio-group/test/search/radio-group.e2e.ts
@@ -19,7 +19,7 @@ test.describe('radio-group', () => {
       // filter radio so it is not in DOM
       await page.fill('ion-searchbar input', 'zero');
       await page.waitForChanges();
-      expect(radio).toBeHidden();
+      await expect(radio).toBeHidden();
 
       // ensure radio group has the same value
       await expect(radioGroup).toHaveJSProperty('value', 'two');
