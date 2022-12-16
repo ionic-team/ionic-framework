de87d3a4d2dbc56ac1df7d7a44db2bf38d9b3fea
amandaesmith3 <amanda.s@ionic.io>
2022-12-16 10:10:39 -0600

    test(checkbox): add test for long label

--- core/src/components/checkbox/test/label/checkbox.e2e.ts
+++ core/src/components/checkbox/test/label/checkbox.e2e.ts
@@ -21,6 +21,7 @@ test.describe('checkbox: label', () => {
         `checkbox-label-start-justify-start-${page.getSnapshotSettings()}.png`
       );
     });
+
     test('should render an end justification with label in the start position', async ({ page }) => {
       await page.setContent(`
         <ion-checkbox label-placement="start" justify="end" style="width: 200px">Label</ion-checkbox>
@@ -31,6 +32,7 @@ test.describe('checkbox: label', () => {
         `checkbox-label-start-justify-end-${page.getSnapshotSettings()}.png`
       );
     });
+
     test('should render a space between justification with label in the start position', async ({ page }) => {
       await page.setContent(`
         <ion-checkbox label-placement="start" justify="space-between" style="width: 200px">Label</ion-checkbox>
@@ -40,6 +42,17 @@ test.describe('checkbox: label', () => {
       expect(await checkbox.screenshot()).toMatchSnapshot(
         `checkbox-label-start-justify-space-between-${page.getSnapshotSettings()}.png`
       );
+    });
+
+    test('should truncate long labels with ellipses', async ({ page }) => {
+      await page.setContent(`
+        <ion-checkbox label-placement="start" justify="start" style="width: 200px">
+          Long Label Long Label Long Label Long Label Long Label Long Label
+        </ion-checkbox>
+      `);
+
+      const checkbox = page.locator('ion-checkbox');
+      expect(await checkbox.screenshot()).toMatchSnapshot(`checkbox-long-label-${page.getSnapshotSettings()}.png`);
     });
   });
 
@@ -54,6 +67,7 @@ test.describe('checkbox: label', () => {
         `checkbox-label-end-justify-start-${page.getSnapshotSettings()}.png`
       );
     });
+
     test('should render an end justification with label in the end position', async ({ page }) => {
       await page.setContent(`
         <ion-checkbox label-placement="end" justify="end" style="width: 200px">Label</ion-checkbox>
@@ -64,6 +78,7 @@ test.describe('checkbox: label', () => {
         `checkbox-label-end-justify-end-${page.getSnapshotSettings()}.png`
       );
     });
+
     test('should render a space between justification with label in the end position', async ({ page }) => {
       await page.setContent(`
         <ion-checkbox label-placement="end" justify="space-between" style="width: 200px">Label</ion-checkbox>
@@ -87,6 +102,7 @@ test.describe('checkbox: label', () => {
         `checkbox-label-fixed-justify-start-${page.getSnapshotSettings()}.png`
       );
     });
+
     test('should render an end justification with label in the fixed position', async ({ page }) => {
       await page.setContent(`
         <ion-checkbox label-placement="fixed" justify="end" style="width: 200px">This is a long label</ion-checkbox>
@@ -97,6 +113,7 @@ test.describe('checkbox: label', () => {
         `checkbox-label-fixed-justify-end-${page.getSnapshotSettings()}.png`
       );
     });
+
     test('should render a space between justification with label in the fixed position', async ({ page }) => {
       await page.setContent(`
         <ion-checkbox label-placement="fixed" justify="space-between" style="width: 200px">This is a long label</ion-checkbox>
