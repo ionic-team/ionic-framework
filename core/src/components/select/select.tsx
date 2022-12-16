5d2bd62487d6eea3455a6eb253cbda8e0eb7c30b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-16 17:21:51 +0000

    refactor(select): add fill class

--- core/src/components/select/select.tsx
+++ core/src/components/select/select.tsx
@@ -678,7 +678,7 @@ export class Select implements ComponentInterface {
   }
 
   private renderSelect() {
-    const { disabled, el, isExpanded, labelPlacement, justify, placeholder } = this;
+    const { disabled, el, isExpanded, labelPlacement, justify, placeholder, fill } = this;
     const mode = getIonMode(this);
     const hasFloatingOrStackedLabel = labelPlacement === 'floating' || labelPlacement === 'stacked';
     const justifyEnabled = !hasFloatingOrStackedLabel;
@@ -695,6 +695,7 @@ export class Select implements ComponentInterface {
           'has-value': this.hasValue(),
           'has-placeholder': placeholder !== undefined,
           [`select-${rtl}`]: true,
+          [`select-fill-${fill}`]: fill !== undefined,
           [`select-justify-${justify}`]: justifyEnabled,
           [`select-label-placement-${labelPlacement}`]: true,
         })}
