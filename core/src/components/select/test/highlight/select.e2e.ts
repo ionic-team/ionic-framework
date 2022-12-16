38418079ddc0cfee4c018367985a9c4be441e3bc
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-16 14:38:32 -0500

    test(select): add base tests files for fill, shape, color, highlight (#26497)

---
+++ core/src/components/select/test/highlight/select.e2e.ts
@@ -0,0 +1,14 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('input: highlights', () => {
+  test.beforeEach(({ skip }) => {
+    skip.rtl();
+  });
+  test.describe('input: no fill', () => {
+  });
+  test.describe('input: solid', () => {
+  });
+  test.describe('input: outline', () => {
+  });
+});
