450b2b2f99e9b688b737604ce443e23f4e648e54
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2022-12-20 21:39:16 +0000

    chore(): update input test

--- core/src/components/input/test/item/input.e2e.ts
+++ core/src/components/input/test/item/input.e2e.ts
@@ -4,14 +4,11 @@ import { test } from '@utils/test/playwright';
 test.describe('input: item', () => {
   test('should render correctly in list with no fill', async ({ page }) => {
     await page.setContent(`
-      <ion-list>
+      <ion-list lines="none">
         <ion-item>
           <ion-input
             label="Email"
             value="hi@ionic.io"
-            helper-text="Enter your email"
-            maxlength="20"
-            counter="true"
           ></ion-input>
         </ion-item>
       </ion-list>
@@ -26,9 +23,6 @@ test.describe('input: item', () => {
           <ion-input
             label="Email"
             value="hi@ionic.io"
-            helper-text="Enter your email"
-            maxlength="20"
-            counter="true"
           ></ion-input>
         </ion-item>
       </ion-list>
