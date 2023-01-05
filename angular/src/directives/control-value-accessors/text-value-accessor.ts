afb4d88bf86e4ed65feaf453bce39a7b582b6f6c
amandaesmith3 <amanda.s@ionic.io>
2023-01-05 10:40:06 -0600

    fix(angular): add types to TextValueAccessor

--- angular/src/directives/control-value-accessors/text-value-accessor.ts
+++ angular/src/directives/control-value-accessors/text-value-accessor.ts
@@ -20,7 +20,7 @@ export class TextValueAccessorDirective extends ValueAccessor {
   }
 
   @HostListener('ionChange', ['$event.target'])
-  _handleInputEvent(el: HTMLInputElement): void {
+  _handleInputEvent(el: HTMLIonInputElement | HTMLIonTextareaElement | HTMLIonSearchbarElement): void {
     this.handleChangeEvent(el, el.value);
   }
 }
