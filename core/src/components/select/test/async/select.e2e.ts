8cb5f501ecd3fca88ec939af9d0695fdedf33caf
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-01-09 17:46:07 +0000

    test(select): add async test

---
+++ core/src/components/select/test/async/select.e2e.ts
@@ -0,0 +1,17 @@
+import { expect } from '@playwright/test';
+import { test } from '@utils/test/playwright';
+
+test.describe('select: async', () => {
+  test('should correctly set the value after a delay', async ({ page, skip }) => {
+    skip.rtl('This is checking internal logic. RTL tests are not needed');
+
+    await page.goto(`/src/components/select/test/async`);
+    const selectValueSet = await page.spyOnEvent('selectValueSet');
+
+    const select = await page.locator('#default');
+
+    await selectValueSet.next();
+
+    await expect(select).toHaveJSProperty('value', 'bird');
+  });
+});
