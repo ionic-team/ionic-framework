2eba84d3db8fcfb71b28c37b6fc721d05f49fdeb
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-01-09 21:47:28 +0000

    test(loading): add test for aria-label

--- core/src/components/loading/test/a11y/loading.e2e.ts
+++ core/src/components/loading/test/a11y/loading.e2e.ts
@@ -15,4 +15,16 @@ test.describe('loading: a11y', () => {
     const results = await new AxeBuilder({ page }).analyze();
     expect(results.violations).toEqual([]);
   });
+  test('should set aria-labelledby with a label', async ({ page }) => {
+    await page.goto('/src/components/loading/test/a11y');
+
+    const ionLoadingDidPresent = await page.spyOnEvent('ionLoadingDidPresent');
+
+    await page.click('#open-label-loading');
+
+    await ionLoadingDidPresent.next();
+
+    const results = await new AxeBuilder({ page }).analyze();
+    expect(results.violations).toEqual([]);
+  });
 });
