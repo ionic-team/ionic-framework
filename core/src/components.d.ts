77ce9e066e1ee71438ceac35479ea04e8df021f5
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-01-09 13:08:01 -0500

    fix(segment): nested interactive is not rendered (#26575)

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -2484,6 +2484,7 @@ export namespace Components {
           * The mode determines which platform styles to use.
          */
         "mode"?: "ios" | "md";
+        "setFocus": () => Promise<void>;
         /**
           * The type of the button.
          */
