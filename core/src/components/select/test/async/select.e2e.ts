f6795b40b362ae043a02091bccbfbe8d7ac97dcb
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-01-24 13:59:48 +0000

    lint

--- core/src/components/select/test/async/select.e2e.ts
+++ core/src/components/select/test/async/select.e2e.ts
@@ -5,7 +5,7 @@ test.describe('select: async', () => {
   test.beforeEach(async ({ skip }) => {
     skip.rtl('This is checking internal logic. RTL tests are not needed');
     skip.mode('md');
-  })
+  });
   test('should correctly set the value after a delay', async ({ page }) => {
     await page.goto(`/src/components/select/test/async`);
     const selectValueSet = await page.spyOnEvent('selectValueSet');
