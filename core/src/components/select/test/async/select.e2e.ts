1f3c7ef52cc64849f02c242383d5c57167f72faf
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-01-24 13:58:09 +0000

    test(select): add failing test

--- core/src/components/select/test/async/select.e2e.ts
+++ core/src/components/select/test/async/select.e2e.ts
@@ -2,9 +2,11 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('select: async', () => {
-  test('should correctly set the value after a delay', async ({ page, skip }) => {
+  test.beforeEach(async ({ skip }) => {
     skip.rtl('This is checking internal logic. RTL tests are not needed');
-
+    skip.mode('md');
+  })
+  test('should correctly set the value after a delay', async ({ page }) => {
     await page.goto(`/src/components/select/test/async`);
     const selectValueSet = await page.spyOnEvent('selectValueSet');
 
@@ -13,5 +15,17 @@ test.describe('select: async', () => {
     await selectValueSet.next();
 
     await expect(select).toHaveJSProperty('value', 'bird');
+  });
+
+  test('should re-render when options update but value is already set', async ({ page }) => {
+    await page.goto(`/src/components/select/test/async`);
+    const selectValueSet = await page.spyOnEvent('selectValueSet');
+
+    const select = await page.locator('#with-value');
+
+    await selectValueSet.next();
+
+    await expect(select).toHaveJSProperty('value', 'bird');
+    await expect(select.locator('.select-text')).toHaveText('bird');
   });
 });
