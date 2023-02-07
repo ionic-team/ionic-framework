9385ed388e2efbc7eaba2c9321af38a12c812d70
Sean Perkins <sean@ionic.io>
2023-02-06 22:42:05 -0500

    feat(picker-column): assign custom aria-labels to column options

--- core/src/components/picker-column/test/picker-column.spec.tsx
+++ core/src/components/picker-column/test/picker-column.spec.tsx
@@ -1,5 +1,6 @@
 import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
+
 import { PickerColumnCmp } from '../picker-column';
 
 describe('picker-column', () => {
@@ -13,5 +14,30 @@ describe('picker-column', () => {
 
     const pickerCol = page.body.querySelector('ion-picker-column');
     expect(pickerCol.classList.contains('test-class')).toBe(true);
+  });
+
+  it('should add aria-label to the picker column option', async () => {
+    const col = {
+      options: [
+        {
+          text: 'C#',
+          ariaLabel: 'C Sharp',
+        },
+        {
+          text: 'Java',
+        },
+      ],
+    };
+
+    const page = await newSpecPage({
+      components: [PickerColumnCmp],
+      template: () => <ion-picker-column col={col}></ion-picker-column>,
+    });
+
+    const firstOption = page.body.querySelector('ion-picker-column .picker-opt:nth-child(1)');
+    const secondOption = page.body.querySelector('ion-picker-column .picker-opt:nth-child(2)');
+
+    expect(firstOption.getAttribute('aria-label')).toBe('C Sharp');
+    expect(secondOption.getAttribute('aria-label')).toBe(null);
   });
 });
