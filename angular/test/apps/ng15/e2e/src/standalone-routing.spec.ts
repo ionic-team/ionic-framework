1dee16f3a25dedf831afc09f9c400a42e861beea
Sean Perkins <sean@ionic.io>
2023-02-22 12:33:49 -0500

    feat(angular,angular-server): angular v14 minimum support (#26822)
    BREAKING CHANGE:
    
    Angular v14 is now required to use `@ionic/angular` and `@ionic/angular-server`. Upgrade your project to Angular v14 by following the [Angular v14 update guide](https://update.angular.io/?l=3&v=13.0-14.0).
    
    The dev-preview `environmentInjector` property has been removed from `ion-tabs` and `ion-router-outlet`. Standalone component routing is now available without additional custom configuration. Remove the `environmentInjector` property from your `ion-tabs` and `ion-router-outlet` components.

--- angular/test/apps/ng15/e2e/src/standalone-routing.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Routing with Standalone Components', () => {
-  beforeEach(() => {
-    cy.visit('/version-test/standalone');
-  });
-
-  it('should render the component', () => {
-    cy.get('ion-content').contains('This is a standalone component rendered from a route.');
-  });
-});
