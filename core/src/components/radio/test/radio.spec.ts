653515ebd099257f53542a631608ba0e94fd3683
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-03-08 20:06:19 +0000

    test(radio): add failing test

--- core/src/components/radio/test/radio.spec.ts
+++ core/src/components/radio/test/radio.spec.ts
@@ -1,4 +1,6 @@
 import { Radio } from '../radio.tsx';
+import { RadioGroup } from '../../radio-group/radio-group.tsx';
+import { newSpecPage } from '@stencil/core/testing';
 
 describe('ion-radio', () => {
   it('should set a default value', async () => {
@@ -7,5 +9,25 @@ describe('ion-radio', () => {
     await radio.connectedCallback();
 
     expect(radio.value).toEqual('ion-rb-0');
+  });
+
+  it('should update the checked state when updating the value', async () => {
+    const page = await newSpecPage({
+      components: [Radio, RadioGroup],
+      html: `
+        <ion-radio-group value="a">
+          <ion-radio value="other-value"></ion-radio>
+        </ion-radio-group>
+      `,
+    });
+
+    const radio = page.root.querySelector('ion-radio');
+    expect(radio.classList.contains('radio-checked')).toBe(false);
+
+    radio.value = 'a';
+
+    await page.waitForChanges();
+
+    expect(radio.classList.contains('radio-checked')).toBe(true);
   });
 });
