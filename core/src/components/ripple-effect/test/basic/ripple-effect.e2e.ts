aab2f5efb51099ae5e7a7692223e95fcae78e1e0
Sean Perkins <sean@ionic.io>
2023-03-07 21:39:58 -0500

    test(ripple-effect): enable ripple effect test

--- core/src/components/ripple-effect/test/basic/ripple-effect.e2e.ts
+++ core/src/components/ripple-effect/test/basic/ripple-effect.e2e.ts
@@ -2,8 +2,12 @@ import { expect } from '@playwright/test';
 import type { E2EPage } from '@utils/test/playwright';
 import { test } from '@utils/test/playwright';
 
-// TODO FW-3081
-test.describe.skip('ripple-effect: basic', () => {
+test.describe('ripple-effect: basic', () => {
+  test.beforeEach(async ({ skip }) => {
+    skip.rtl();
+    skip.mode('ios');
+  });
+
   test('should add .ion-activated when pressed', async ({ page }) => {
     await verifyRippleEffect(page, '#small-btn');
     await verifyRippleEffect(page, '#large-btn');
@@ -14,7 +18,7 @@ test.describe.skip('ripple-effect: basic', () => {
 
   test.describe('ripple effect with nested ion-button', () => {
     test('should add .ion-activated when the block is pressed', async ({ page }) => {
-      await page.goto(`/src/components/ripple-effect/test/basic?ionic:_testing=false&ionic:mode=md`);
+      await page.goto('/src/components/ripple-effect/test/basic?ionic:_testing=false');
 
       const el = page.locator('#ripple-with-button');
 
@@ -30,9 +34,7 @@ test.describe.skip('ripple-effect: basic', () => {
       // Waits for the ripple effect to be added
       await page.waitForSelector('.ion-activated');
 
-      const elHandle = await el.elementHandle();
-      const classes = await elHandle?.evaluate((el) => el.classList.value);
-      expect(classes).toMatch(/ion-activated/);
+      await expect(el).toHaveClass(/ion-activated/);
     });
 
     test('should add .ion-activated when the button is pressed', async ({ page }) => {
@@ -42,23 +44,20 @@ test.describe.skip('ripple-effect: basic', () => {
 });
 
 const verifyRippleEffect = async (page: E2EPage, selector: string) => {
-  await page.goto(`/src/components/ripple-effect/test/basic?ionic:_testing=false&ionic:mode=md`);
+  await page.goto('/src/components/ripple-effect/test/basic?ionic:_testing=false');
 
   const el = page.locator(selector);
 
   await el.scrollIntoViewIfNeeded();
 
-  const boundingBox = await el.boundingBox();
+  const boundingBox = await el.boundingBox()!;
 
   if (boundingBox) {
     await page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2);
     await page.mouse.down();
   }
 
-  // Waits for the ripple effect to be added
-  await page.waitForSelector(`${selector}.ion-activated`);
+  await page.waitForSelector('.ion-activated');
 
-  const elHandle = await el.elementHandle();
-  const classes = await elHandle?.evaluate((el) => el.classList.value);
-  expect(classes).toMatch(/ion-activated/);
+  await expect(el).toHaveClass(/ion-activated/);
 };
