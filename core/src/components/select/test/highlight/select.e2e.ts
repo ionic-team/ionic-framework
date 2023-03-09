09f293363a8263e5576ed64212c76c2ec957116b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-03-09 14:29:50 +0000

    add validity tests

--- core/src/components/select/test/highlight/select.e2e.ts
+++ core/src/components/select/test/highlight/select.e2e.ts
@@ -6,7 +6,112 @@ test.describe('input: highlights', () => {
     skip.rtl();
     skip.mode('ios');
   });
+
   test.describe('input: no fill', () => {
+    test('should render valid state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select value="apple" class="ion-touched ion-valid ion-focused" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-no-fill-valid-${page.getSnapshotSettings()}.png`);
+    });
+    test('should render invalid state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select value="apple" class="ion-touched ion-invalid" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-no-fill-invalid-${page.getSnapshotSettings()}.png`);
+    });
+    test('should render focused state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select value="apple" class="ion-focused" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-no-fill-focus-${page.getSnapshotSettings()}.png`);
+    });
+  });
+  test.describe('input: solid', () => {
+    test('should render valid state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select fill="solid" value="apple" class="ion-touched ion-valid ion-focused" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-solid-valid-${page.getSnapshotSettings()}.png`);
+    });
+    test('should render invalid state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select fill="solid" value="apple" class="ion-touched ion-invalid" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-solid-invalid-${page.getSnapshotSettings()}.png`);
+    });
+    test('should render focused state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select fill="solid" value="apple" class="ion-focused" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-solid-focus-${page.getSnapshotSettings()}.png`);
+    });
+  });
+  test.describe('input: outline', () => {
+    test('should render valid state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select fill="outline" value="apple" class="ion-touched ion-valid ion-focused" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-outline-valid-${page.getSnapshotSettings()}.png`);
+    });
+    test('should render invalid state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select fill="outline" value="apple" class="ion-touched ion-invalid ion-focused" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-outline-invalid-${page.getSnapshotSettings()}.png`);
+    });
+    test('should render focused state correctly', async ({ page }) => {
+      await page.setContent(`
+        <ion-select fill="outline" value="apple" class="ion-focused" label="Favorite Fruit">
+          <ion-select-option value="apple">Apple</ion-select-option>
+        </ion-select>
+      `);
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(`select-outline-focus-${page.getSnapshotSettings()}.png`);
+    });
+  });
+});
+
+test.describe('select: expanded highlight', () => {
+  test.beforeEach(({ skip }) => {
+    skip.rtl();
+    skip.mode('ios');
+  });
+
+  test.describe('select: no fill', () => {
     test('should render bottom highlight', async ({ page }) => {
       await page.setContent(`
         <ion-select label="Label" class="select-expanded"></ion-select>
@@ -18,7 +123,7 @@ test.describe('input: highlights', () => {
       );
     });
   });
-  test.describe('input: solid', () => {
+  test.describe('select: solid', () => {
     test('should render bottom highlight', async ({ page }) => {
       await page.setContent(`
         <ion-select fill="solid" label="Label" class="select-expanded"></ion-select>
@@ -30,7 +135,7 @@ test.describe('input: highlights', () => {
       );
     });
   });
-  test.describe('input: outline', () => {
+  test.describe('select: outline', () => {
     test('should render bottom highlight', async ({ page }) => {
       await page.setContent(`
         <ion-select fill="outline" label="Label" class="select-expanded"></ion-select>
