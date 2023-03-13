3e139a40f99e4412505b55d3a417a5c8354f8cc3
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-03-13 13:17:04 +0000

    chore(): lint

--- core/src/components/textarea/test/textarea.spec.ts
+++ core/src/components/textarea/test/textarea.spec.ts
@@ -4,11 +4,11 @@ import { Textarea } from '../textarea';
 it('should inherit attributes', async () => {
   const page = await newSpecPage({
     components: [Textarea],
-    html: '<ion-textarea title="my title" tabindex="-1" data-form-type="password"></ion-textarea>'
+    html: '<ion-textarea title="my title" tabindex="-1" data-form-type="password"></ion-textarea>',
   });
 
   const nativeEl = page.body.querySelector('ion-textarea textarea');
   expect(nativeEl.getAttribute('title')).toBe('my title');
   expect(nativeEl.getAttribute('tabindex')).toBe('-1');
   expect(nativeEl.getAttribute('data-form-type')).toBe('password');
-})
+});
