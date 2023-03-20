e4f490cf7899ff9c4fa3c6c33e07c5c4fb4c6b31
Brandy Carney <brandy@ionic.io>
2023-03-20 16:52:45 -0400

    test(select): not enough text in the test to make it wrap

--- core/src/components/select/test/wrapping/select.e2e.ts
+++ core/src/components/select/test/wrapping/select.e2e.ts
@@ -7,7 +7,7 @@ test.describe('select: wrapping', () => {
 
     await page.setContent(`
       <ion-select value="brown" aria-label="Hair Color">
-        <ion-select-option value="brown">Brown Hair Is The Best At Wrapping</ion-select-option>
+        <ion-select-option value="brown">Brown Hair Is The Best At Wrapping When No Label Exists And Really Long Text Exists To Make It Wrap The Text</ion-select-option>
       </ion-select>
     `);
 
@@ -20,7 +20,20 @@ test.describe('select: wrapping', () => {
 
     await page.setContent(`
       <ion-select value="brown" aria-label="Hair Color" class="ion-text-wrap">
-        <ion-select-option value="brown">Brown Hair Is The Best At Wrapping</ion-select-option>
+        <ion-select-option value="brown">Brown Hair Is The Best At Wrapping When No Label Exists And Really Long Text Exists To Make It Wrap The Text</ion-select-option>
+      </ion-select>
+    `);
+
+    const select = page.locator('ion-select');
+    await expect(select).toHaveScreenshot(`select-wrap-${page.getSnapshotSettings()}.png`);
+  });
+
+  test('should not wrap label while wrapping text with class', async ({ page, skip }) => {
+    skip.rtl();
+
+    await page.setContent(`
+      <ion-select label="Hair Color" value="brown" aria-label="Hair Color" class="ion-text-wrap">
+        <ion-select-option value="brown">Brown Hair Is The Best At Wrapping When No Label Exists And Really Long Text Exists To Make It Wrap The Text</ion-select-option>
       </ion-select>
     `);
 
