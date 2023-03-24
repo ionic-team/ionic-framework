317f4eefecddfb1392ca71d551840f446feee302
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-03-24 19:37:27 +0000

    test(select): update wrapping tests for v7

--- core/src/components/select/test/legacy/wrapping/select.e2e.ts
+++ core/src/components/select/test/legacy/wrapping/select.e2e.ts
@@ -6,7 +6,7 @@ test.describe('select: wrapping', () => {
     skip.rtl();
 
     await page.setContent(`
-      <ion-select value="nowrap" aria-label="Should Not Wrap">
+      <ion-select value="nowrap">
         <ion-select-option value="nowrap">Should not wrap when no label exists and no class is added to make the text wrap</ion-select-option>
       </ion-select>
     `);
@@ -19,7 +19,7 @@ test.describe('select: wrapping', () => {
     skip.rtl();
 
     await page.setContent(`
-      <ion-select value="wrap" aria-label="Should Wrap" class="ion-text-wrap">
+      <ion-select value="wrap" class="ion-text-wrap">
         <ion-select-option value="wrap">Should wrap when no label exists and really long text exists to make it wrap the text</ion-select-option>
       </ion-select>
     `);
@@ -32,12 +32,15 @@ test.describe('select: wrapping', () => {
     skip.rtl();
 
     await page.setContent(`
-      <ion-select value="wrap" label="Really long label should not wrap" class="ion-text-wrap">
-        <ion-select-option value="wrap">Should wrap value only when label exists and really long text exists to make it wrap the text</ion-select-option>
-      </ion-select>
+      <ion-item>
+        <ion-label>Really long label should not wrap</ion-label>
+        <ion-select value="wrap" class="ion-text-wrap">
+          <ion-select-option value="wrap">Should wrap value only when label exists and really long text exists to make it wrap the text</ion-select-option>
+        </ion-select>
+      </ion-item>
     `);
 
-    const select = page.locator('ion-select');
+    const select = page.locator('ion-item');
     await expect(select).toHaveScreenshot(`select-wrap-with-label-${page.getSnapshotSettings()}.png`);
   });
 });
