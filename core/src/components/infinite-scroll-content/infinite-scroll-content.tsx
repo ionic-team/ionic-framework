5c0ead37bf73b678b08742826e19d7a8395ce01e
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-03-27 15:14:02 -0400

    docs(config): update innerHTML comments (#27032)

--- core/src/components/infinite-scroll-content/infinite-scroll-content.tsx
+++ core/src/components/infinite-scroll-content/infinite-scroll-content.tsx
@@ -33,9 +33,9 @@ export class InfiniteScrollContent implements ComponentInterface {
    * For more information: [Security Documentation](https://ionicframework.com/docs/faq/security)
    *
    * This property accepts custom HTML as a string.
-   * Developers who only want to pass plain text
-   * can disable the custom HTML functionality
-   * by setting `innerHTMLTemplatesEnabled: false` in the Ionic config.
+   * Content is parsed as plaintext by default.
+   * `innerHTMLTemplatesEnabled` must be set to `true` in the Ionic config
+   * before custom HTML can be used.
    */
   @Prop() loadingText?: string | IonicSafeString;
 
