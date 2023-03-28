eab389ba2d58b4cbe8f286b09d9cba9b1efa196f
Maria Hutt <maria@ionic.io>
2023-03-28 12:10:15 -0700

    chore(breadcrumbs): spacing

--- packages/vue/test/base/src/views/Breadcrumbs.vue
+++ packages/vue/test/base/src/views/Breadcrumbs.vue
@@ -7,24 +7,24 @@
     </ion-header>
     <ion-content>
       <h1>Default</h1>
-			<ion-breadcrumbs id="default">
-				<ion-breadcrumb href="#"> Home </ion-breadcrumb>
-				<ion-breadcrumb href="#electronics"> Electronics </ion-breadcrumb>
-				<ion-breadcrumb href="#photography"> Photography </ion-breadcrumb>
-				<ion-breadcrumb href="#cameras"> Cameras </ion-breadcrumb>
-				<ion-breadcrumb href="#film"> Film </ion-breadcrumb>
-				<ion-breadcrumb> 35 mm </ion-breadcrumb>
-			</ion-breadcrumbs>
-			<hr />
-			<h1>Color: Danger</h1>
-			<ion-breadcrumbs id="color" color="danger">
-				<ion-breadcrumb href="#"> Home </ion-breadcrumb>
-				<ion-breadcrumb href="#electronics"> Electronics </ion-breadcrumb>
-				<ion-breadcrumb href="#photography"> Photography </ion-breadcrumb>
-				<ion-breadcrumb href="#cameras"> Cameras </ion-breadcrumb>
-				<ion-breadcrumb href="#film"> Film </ion-breadcrumb>
-				<ion-breadcrumb> 35 mm </ion-breadcrumb>
-			</ion-breadcrumbs>
+      <ion-breadcrumbs id="default">
+        <ion-breadcrumb href="#"> Home </ion-breadcrumb>
+        <ion-breadcrumb href="#electronics"> Electronics </ion-breadcrumb>
+        <ion-breadcrumb href="#photography"> Photography </ion-breadcrumb>
+        <ion-breadcrumb href="#cameras"> Cameras </ion-breadcrumb>
+        <ion-breadcrumb href="#film"> Film </ion-breadcrumb>
+        <ion-breadcrumb> 35 mm </ion-breadcrumb>
+      </ion-breadcrumbs>
+      <hr />
+      <h1>Color: Danger</h1>
+      <ion-breadcrumbs id="color" color="danger">
+        <ion-breadcrumb href="#"> Home </ion-breadcrumb>
+        <ion-breadcrumb href="#electronics"> Electronics </ion-breadcrumb>
+        <ion-breadcrumb href="#photography"> Photography </ion-breadcrumb>
+        <ion-breadcrumb href="#cameras"> Cameras </ion-breadcrumb>
+        <ion-breadcrumb href="#film"> Film </ion-breadcrumb>
+        <ion-breadcrumb> 35 mm </ion-breadcrumb>
+      </ion-breadcrumbs>
     </ion-content>
   </ion-page>
 </template>
