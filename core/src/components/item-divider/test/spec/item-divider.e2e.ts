455c79c6e5fecd1cf138e22de3d97c462dadc94a
Maria Hutt <maria@ionic.io>
2023-03-28 15:32:04 -0700

    test(item-divider): verify that margin-end is added

--- core/src/components/item-divider/test/spec/item-divider.e2e.ts
+++ core/src/components/item-divider/test/spec/item-divider.e2e.ts
@@ -11,4 +11,16 @@ test.describe('item-divider: spec', () => {
 
     await expect(page).toHaveScreenshot(`list-item-divider-${page.getSnapshotSettings()}.png`);
   });
+
+  test('should have margin-end on button in end slot', async ({ page, skip }) => {
+    skip.mode('ios', 'This behavior is only available in MD mode.');
+
+    const button = page.locator('ion-button');
+    const buttonMarginEnd = await button.evaluate((el) => {
+      const styles = window.getComputedStyle(el);
+      return styles.getPropertyValue('margin-end');
+    });
+
+    expect(buttonMarginEnd).not.toBe('0px');
+  });
 });
