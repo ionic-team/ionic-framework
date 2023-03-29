c79af9b1a9110e3bd9b6bdc843aaa48aeb808016
Shawn Taylor <shawn@ionic.io>
2023-03-29 10:42:12 -0400

    Use smaller snapshots

--- core/src/components/item-divider/test/spec/item-divider.e2e.ts
+++ core/src/components/item-divider/test/spec/item-divider.e2e.ts
@@ -1,14 +1,39 @@
 import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
-test.describe('item-divider: spec', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/item-divider/test/spec');
+test.describe('item-divider: basic', () => {
+  test('should display an item divider with text', async ({ page }) => {
+    await page.setContent(`
+      <ion-item-divider>
+        <ion-label>Item Divider</ion-label>
+      </ion-item-divider>
+    `);
+
+    const divider = page.locator('ion-item-divider');
+    await expect(divider).toHaveScreenshot(`item-divider-text-${page.getSnapshotSettings()}.png`);
   });
 
-  test('should not have visual regressions', async ({ page }) => {
-    await page.setIonViewport();
+  test('should display an item divider with a button in the end slot', async ({ page }) => {
+    await page.setContent(`
+      <ion-item-divider>
+        <ion-label>Item Divider</ion-label>
+        <ion-button slot="end">Button</ion-button>
+      </ion-item-divider>
+    `);
 
-    await expect(page).toHaveScreenshot(`list-item-divider-${page.getSnapshotSettings()}.png`);
+    const divider = page.locator('ion-item-divider');
+    await expect(divider).toHaveScreenshot(`item-divider-button-end-${page.getSnapshotSettings()}.png`);
+  });
+
+  test('should display an item divider with an icon in the start slot', async ({ page }) => {
+    await page.setContent(`
+      <ion-item-divider>
+        <ion-icon slot="start" name="star"></ion-icon>
+        <ion-label>Item Divider</ion-label>
+      </ion-item-divider>
+    `);
+
+    const divider = page.locator('ion-item-divider');
+    await expect(divider).toHaveScreenshot(`item-divider-icon-start-${page.getSnapshotSettings()}.png`);
   });
 });
