b4078149ac1890abff06ad9b334db5cdbb93cc85
Maria Hutt <maria@ionic.io>
2023-03-31 14:32:25 -0700

    test(breadcrumbs): check color attribute renders in the DOM

--- core/src/components/breadcrumbs/test/basic/breadcrumbs.e2e.ts
+++ core/src/components/breadcrumbs/test/basic/breadcrumbs.e2e.ts
@@ -2,11 +2,24 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('breadcrumbs: basic', () => {
-  test('should not have visual regressions', async ({ page }) => {
-    await page.goto(`/src/components/breadcrumbs/test/basic`);
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/src/components/breadcrumbs/test/basic');
+  });
 
+  test('should not have visual regressions', async ({ page }) => {
     await page.setIonViewport();
 
     await expect(page).toHaveScreenshot(`breadcrumb-diff-${page.getSnapshotSettings()}.png`);
+  });
+
+  test('should have color attribute', async ({ page }) => {
+    test.info().annotations.push({
+      type: 'issue',
+      description: 'https://github.com/ionic-team/ionic-framework/issues/25446',
+    });
+
+    const breadcrumbs = page.locator('#color');
+
+    expect(await breadcrumbs.getAttribute('color')).not.toBe(null);
   });
 });
