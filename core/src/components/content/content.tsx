75186d3f83d052906189abc29f3256c4cfc9c903
Amanda Johnston <90629384+amandaejohnston@users.noreply.github.com>
2023-03-31 10:17:36 -0500

    docs(content): fix typo in forceOverscroll description (#27077)

--- core/src/components/content/content.tsx
+++ core/src/components/content/content.tsx
@@ -75,7 +75,7 @@ export class Content implements ComponentInterface {
   /**
    * If `true` and the content does not cause an overflow scroll, the scroll interaction will cause a bounce.
    * If the content exceeds the bounds of ionContent, nothing will change.
-   * Note, the does not disable the system bounce on iOS. That is an OS level setting.
+   * Note, this does not disable the system bounce on iOS. That is an OS level setting.
    */
   @Prop({ mutable: true }) forceOverscroll?: boolean;
 
