5ab09111de4c6423745612a26f4f0728cff992e6
Maria Hutt <maria@ionic.io>
2023-04-03 10:48:10 -0700

    test(breadcrumbs): revert e2e

--- core/src/components/breadcrumbs/test/basic/breadcrumbs.e2e.ts
+++ core/src/components/breadcrumbs/test/basic/breadcrumbs.e2e.ts
@@ -2,11 +2,9 @@ import { expect } from '@playwright/test';
 import { test } from '@utils/test/playwright';
 
 test.describe('breadcrumbs: basic', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/breadcrumbs/test/basic');
-  });
-
   test('should not have visual regressions', async ({ page }) => {
+    await page.goto(`/src/components/breadcrumbs/test/basic`);
+
     await page.setIonViewport();
 
     await expect(page).toHaveScreenshot(`breadcrumb-diff-${page.getSnapshotSettings()}.png`);
