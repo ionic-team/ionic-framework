f8667830dc98a26bd0e5303a346379ca1bd46f98
Maria Hutt <thetaPC@users.noreply.github.com>
2023-04-03 11:35:14 -0700

    test(breadcrumbs): check color attribute renders in the DOM (#27080)
    Co-authored-by: liamdebeasi <liamdebeasi@users.noreply.github.com>

--- packages/vue/test/base/tests/e2e/specs/breadcrumbs.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Components: Breadcrumbs", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:8080/components/breadcrumbs");
-  });
-
-  it("should have color attribute", () => {
-    cy.get('ion-breadcrumbs#color').should('have.prop', 'color');
-  });
-});
