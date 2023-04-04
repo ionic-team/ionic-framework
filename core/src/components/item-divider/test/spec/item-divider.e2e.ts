313a323b0af740d52bec51554e622484cb67de0d
Maria Hutt <maria@ionic.io>
2023-04-04 09:44:14 -0700

    test(item-divider): removed duplicate

--- core/src/components/item-divider/test/spec/item-divider.e2e.ts
+++ core/src/components/item-divider/test/spec/item-divider.e2e.ts
@@ -11,21 +11,4 @@ test.describe('item-divider: spec', () => {
 
     await expect(page).toHaveScreenshot(`list-item-divider-${page.getSnapshotSettings()}.png`);
   });
-
-  test('should have margin-end on button in end slot', async ({ page, skip }) => {
-    skip.mode('ios', 'This behavior is only available in MD mode.');
-
-    test.info().annotations.push({
-      type: 'issue',
-      description: 'https://github.com/ionic-team/ionic-framework/issues/17012',
-    });
-
-    const button = page.locator('ion-button');
-    const buttonMarginEnd = await button.evaluate((el) => {
-      const styles = window.getComputedStyle(el);
-      return styles.getPropertyValue('margin-end');
-    });
-
-    expect(buttonMarginEnd).not.toBe('0px');
-  });
 });
