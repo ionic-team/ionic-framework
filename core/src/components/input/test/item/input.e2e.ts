9b5eae4a435b5f683a2d690e624a6ef2364ca422
Sean Perkins <sean@ionic.io>
2023-04-10 16:12:09 -0400

    test(input): modern syntax supports async labels

--- core/src/components/input/test/item/input.e2e.ts
+++ core/src/components/input/test/item/input.e2e.ts
@@ -36,4 +36,35 @@ test.describe('input: item', () => {
     const list = page.locator('ion-list');
     expect(await list.screenshot()).toMatchSnapshot(`input-inset-list-no-fill-${page.getSnapshotSettings()}.png`);
   });
+  test('input renders as modern when label is set asynchronously', async ({ page }, testInfo) => {
+    testInfo.annotations.push({
+      type: 'issue',
+      description: 'https://github.com/ionic-team/ionic-framework/issues/27085',
+    });
+
+    await page.setContent(
+      `<ion-item>
+        <ion-input></ion-input>
+      </ion-item>
+      <ion-button onclick="setLabel()">Set label</ion-button>
+
+      <script>
+        function setLabel() {
+          const input = document.querySelector('ion-input');
+          input.label = 'New label';
+        }
+      </script>
+      `
+    );
+    const input = page.locator('ion-input');
+    // Initial template should be modern
+    await expect(input).not.toHaveClass(/legacy-input/);
+
+    // Click button to set label
+    await page.click('ion-button');
+    await page.waitForChanges();
+
+    // Template should still be modern
+    await expect(input).not.toHaveClass(/legacy-input/);
+  });
 });
