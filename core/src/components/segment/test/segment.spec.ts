23de0b70a377a8dd9cce298fe13579cbdeebecfa
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-10 13:49:42 +0000

    refactor(segment): use spec instead of e2e

---
+++ core/src/components/segment/test/segment.spec.ts
@@ -0,0 +1,21 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { Segment } from '../segment';
+import { SegmentButton } from '../../segment-button/segment-button';
+
+it('should set checked state when value is set asynchronously', async () => {
+  const page = await newSpecPage({
+    components: [Segment, SegmentButton],
+    html: `
+      <ion-segment value="first">
+        <ion-segment-button>First</ion-segment-button>
+      </ion-segment>
+    `,
+  });
+
+  const segmentButton = page.root.querySelector('ion-segment-button');
+
+  segmentButton.value = 'first';
+  await page.waitForChanges();
+
+  expect(segmentButton.classList.contains('segment-button-checked')).toBe(true);
+});
