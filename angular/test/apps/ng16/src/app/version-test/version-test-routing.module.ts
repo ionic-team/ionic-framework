a26d402c5ce9b53a1c5e6bd6fc79630488bf3028
Sean Perkins <sean@ionic.io>
2023-04-14 12:27:00 -0400

    chore: clone ng15 test app for ng16

---
+++ angular/test/apps/ng16/src/app/version-test/version-test-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([])
+  ],
+  exports: [RouterModule]
+})
+export class VersionTestRoutingModule { }
