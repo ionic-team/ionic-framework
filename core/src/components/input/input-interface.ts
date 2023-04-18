a7e7b4fc3263d422d8289d8554eab094036cbf55
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-18 12:46:05 +0000

    add comment

--- core/src/components/input/input-interface.ts
+++ core/src/components/input/input-interface.ts
@@ -1,3 +1,8 @@
+/**
+ * Values are converted to strings when emitted which is
+ * why we do not have a `number` type here even though the
+ * `value` prop accepts a `number` type.
+ */
 export interface InputChangeEventDetail {
   value?: string | null;
   event?: Event;
