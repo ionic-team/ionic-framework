ea6ebbfed4edd471678994def84da32fc0ba9ade
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-20 17:48:42 +0000

    test(alert): simplify trigger test

--- core/src/components/alert/test/trigger/alert.e2e.ts
+++ core/src/components/alert/test/trigger/alert.e2e.ts
@@ -1,10 +1,12 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-test.describe('alert: trigger', () => {
-  configs({ directions: ['ltr'], modes: ['ios'] }).forEach(({ config, title }) => {
-    test(title('should open the alert'), async ({ page }) => {
+configs({ directions: ['ltr'], modes: ['ios'] }).forEach(({ config, title }) => {
+  test.describe(title('alert: trigger'), () => {
+    test.beforeEach(async ({ page }) => {
       await page.goto('/src/components/alert/test/trigger', config);
+    });
+    test('should open the alert', async ({ page }) => {
       const ionAlertDidPresent = await page.spyOnEvent('ionAlertDidPresent');
       const alert = page.locator('#default-alert');
 
@@ -14,8 +16,7 @@ test.describe('alert: trigger', () => {
       await expect(alert).toBeVisible();
     });
 
-    test(title('should present a previously presented alert'), async ({ page }) => {
-      await page.goto('/src/components/alert/test/trigger', config);
+    test('should present a previously presented alert', async ({ page }) => {
       const ionAlertDidPresent = await page.spyOnEvent('ionAlertDidPresent');
       const ionAlertDidDismiss = await page.spyOnEvent('ionAlertDidDismiss');
       const alert = page.locator('#timeout-alert');
