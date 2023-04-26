88ee51e6dd50a85d2327bf35166f835b9e860c68
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-26 16:40:37 -0400

    test(button): migrate round

---
+++ core/src/components/button/test/round/button.e2e.ts
@@ -0,0 +1,14 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('button: round'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto(`/src/components/button/test/round`, config);
+
+      await page.setIonViewport();
+
+      await expect(page).toHaveScreenshot(screenshot(`button-round`));
+    });
+  });
+});
