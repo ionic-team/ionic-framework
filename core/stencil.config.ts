0851bbe9341dbd4211fa08b755dddd93ae515018
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-26 17:01:45 -0400

    Update stencil.config.ts

--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -136,13 +136,13 @@ export const config: Config = {
           externalEvent: 'ionChange'
         },
         {
-          elements: ['ion-datetime', 'ion-radio-group', 'ion-radio', 'ion-range', 'ion-segment', 'ion-segment-button', 'ion-select', 'ion-accordion-group'],
+          elements: ['ion-datetime', 'ion-radio-group', 'ion-radio', 'ion-segment', 'ion-segment-button', 'ion-select', 'ion-accordion-group'],
           targetAttr: 'value',
           event: 'v-ion-change',
           externalEvent: 'ionChange'
         },
         {
-          elements: ['ion-input', 'ion-searchbar', 'ion-textarea'],
+          elements: ['ion-input', 'ion-range', 'ion-searchbar', 'ion-textarea'],
           targetAttr: 'value',
           event: 'v-ion-input',
           externalEvent: 'ionInput'
