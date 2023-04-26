f8d06beebbc343fa7cf4c60c2e54700dd8f45f15
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-26 16:04:11 -0400

    test(back-button): migrate toolbar

---
+++ core/src/components/back-button/test/toolbar/back-button.e2e.ts
@@ -0,0 +1,15 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs().forEach(({ config, screenshot, title }) => {
+  test.describe(title('back-button: toolbar'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto(`/src/components/back-button/test/toolbar`, config);
+
+      await page.setIonViewport();
+
+      await expect(page).toHaveScreenshot(screenshot(`back-button-toolbar`));
+    });
+  });
+
+})
