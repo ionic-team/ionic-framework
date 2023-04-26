ea4c24a8af08306ffef11d412555a7dbbc22b44c
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-26 14:54:22 -0400

    test(utils): add support for generators (#27241)
    Issue number: N/A
    
    ---------
    
    This PR adds support for writing tests with generators.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27241/commits/8febaddee916ecc1797327b352797ed1b7859647
    
    - Adds the underlying generator code necessary to create multiple
    variants of a single test
    - Note: This PR does not add support for dark mode generators. This will
    be added in a separate PR.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27241/commits/5c498d8daf82ead64310a12184e97da0b4be2b13
    
    - Adds the type declarations on `page.goto` and `page.setContent`
    necessary to accept the config result provided by the `configs`
    generator function.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27241/commits/df8c44b563160851c55c08176362388a29bb2fc1
    
    - Updates the `goto` and `setContent` functionality to support the
    generator config with backwards compatibility for the legacy tests.

---
+++ core/src/utils/test/playwright/generator.ts
@@ -0,0 +1,81 @@
+export type Mode = 'ios' | 'md';
+export type Direction = 'ltr' | 'rtl';
+
+export type TitleFn = (title: string) => string;
+export type ScreenshotFn = (fileName: string) => string;
+
+export interface TestConfig {
+  mode: Mode;
+  direction: Direction;
+}
+
+interface TestUtilities {
+  title: TitleFn;
+  screenshot: ScreenshotFn;
+  config: TestConfig;
+}
+
+interface TestConfigOption {
+  modes?: Mode[];
+  directions?: Direction[];
+}
+
+/**
+ * Generates a unique test title based on a base title
+ * and a test config. Playwright uses test titles to generate
+ * test IDs for the test reports, so it's important that
+ * each test title is unique.
+ */
+const generateTitle = (title: string, config: TestConfig): string => {
+  const { mode, direction } = config;
+
+  return `${title} - ${mode}/${direction}`;
+};
+
+/**
+ * Generates a unique filename based on a base filename
+ * and a test config.
+ */
+const generateScreenshotName = (fileName: string, config: TestConfig): string => {
+  const { mode, direction } = config;
+
+  return `${fileName}-${mode}-${direction}.png`;
+};
+
+/**
+ * Given a config generate an array of test variants.
+ */
+export const configs = (testConfig: TestConfigOption = DEFAULT_TEST_CONFIG_OPTION): TestUtilities[] => {
+  const { modes, directions } = testConfig;
+
+  const configs: TestConfig[] = [];
+
+  /**
+   * If certain options are not provided,
+   * fall back to the defaults.
+   */
+  const processedMode: Mode[] = modes ?? DEFAULT_MODES;
+  const processedDirection: Direction[] = directions ?? DEFAULT_DIRECTIONS;
+
+  processedMode.forEach((mode: Mode) => {
+    processedDirection.forEach((direction: Direction) => {
+      configs.push({ mode, direction });
+    });
+  });
+
+  return configs.map((config) => {
+    return {
+      config,
+      title: (title: string) => generateTitle(title, config),
+      screenshot: (fileName: string) => generateScreenshotName(fileName, config),
+    };
+  });
+};
+
+const DEFAULT_MODES: Mode[] = ['ios', 'md'];
+const DEFAULT_DIRECTIONS: Direction[] = ['ltr', 'rtl'];
+
+const DEFAULT_TEST_CONFIG_OPTION = {
+  modes: DEFAULT_MODES,
+  directions: DEFAULT_DIRECTIONS,
+};
