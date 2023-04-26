a667b3e5b35dc0aee476b787a2489cc3d3566d4e
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-26 16:38:45 -0400

    test(action-sheet): migrate to generator (#27246)
    Issue number: N/A
    
    ---------
    
    This PR migrates the action sheet tests to use the generator
    infrastructure.

---
+++ core/src/components/action-sheet/test/is-open/action-sheet.e2e.ts
@@ -0,0 +1,34 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'], modes: ['ios'] }).forEach(({ config, title }) => {
+  test.describe(title('action sheet: isOpen'), () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/src/components/action-sheet/test/is-open', config);
+    });
+    test('should open the action sheet', async ({ page }) => {
+      const ionActionSheetDidPresent = await page.spyOnEvent('ionActionSheetDidPresent');
+      const actionSheet = page.locator('ion-action-sheet');
+
+      await page.click('#default');
+
+      await ionActionSheetDidPresent.next();
+      await expect(actionSheet).toBeVisible();
+    });
+
+    test('should open the action sheet then close after a timeout', async ({ page }) => {
+      const ionActionSheetDidPresent = await page.spyOnEvent('ionActionSheetDidPresent');
+      const ionActionSheetDidDismiss = await page.spyOnEvent('ionActionSheetDidDismiss');
+      const actionSheet = page.locator('ion-action-sheet');
+
+      await page.click('#timeout');
+
+      await ionActionSheetDidPresent.next();
+      await expect(actionSheet).toBeVisible();
+
+      await ionActionSheetDidDismiss.next();
+
+      await expect(actionSheet).toBeHidden();
+    });
+  });
+});
