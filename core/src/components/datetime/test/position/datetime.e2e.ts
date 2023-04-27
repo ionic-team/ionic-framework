8c041cec36922e7a0e9481837a318e0a29b056de
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-27 11:16:36 -0400

    test(datetime): migrate position

---
+++ core/src/components/datetime/test/position/datetime.e2e.ts
@@ -0,0 +1,26 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs().forEach(({ title, screenshot, config }) => {
+  test.describe(title('datetime: position'), () => {
+    test('should position the time picker relative to the click target', async ({ page }) => {
+      await page.goto('/src/components/datetime/test/position', config);
+      const ionPopoverDidPresent = await page.spyOnEvent('ionPopoverDidPresent');
+
+      const openDateTimeBtn = page.locator('ion-button#open-datetime');
+      await openDateTimeBtn.click();
+
+      await ionPopoverDidPresent.next();
+      await page.waitForSelector('.datetime-ready');
+
+      await expect(page).toHaveScreenshot(screenshot(`datetime-position-base`));
+
+      const timepickerBtn = page.locator('ion-datetime .time-body');
+      await timepickerBtn.click();
+
+      await ionPopoverDidPresent.next();
+
+      await expect(page).toHaveScreenshot(screenshot(`datetime-position-popover`));
+    });
+  });
+});
