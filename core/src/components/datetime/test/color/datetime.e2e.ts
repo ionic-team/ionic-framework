22635d7d6232fdb7caeec48968561885949c349b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-27 13:35:47 -0400

    another comment

--- core/src/components/datetime/test/color/datetime.e2e.ts
+++ core/src/components/datetime/test/color/datetime.e2e.ts
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
+/**
+ * This behavior does not vary across directions
+ */
 configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('datetime: color'), () => {
     test('should not have visual regressions', async ({ page }) => {
