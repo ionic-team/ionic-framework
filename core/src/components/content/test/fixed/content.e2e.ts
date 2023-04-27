0cc96ab5deb2f235e6b01058d2feaef3e9503513
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-27 10:41:09 -0400

    test(content): migrate fixed

---
+++ core/src/components/content/test/fixed/content.e2e.ts
@@ -0,0 +1,14 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('content: fixed'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto(`/src/components/content/test/fixed`, config);
+
+      await page.setIonViewport();
+
+      await expect(page).toHaveScreenshot(screenshot(`content-fixed`));
+    });
+  });
+});
