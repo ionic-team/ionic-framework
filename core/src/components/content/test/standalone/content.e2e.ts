8d6e7a89124d66c90f38b9231328aa0ea240bb83
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-27 10:49:57 -0400

    Update content.e2e.ts

--- core/src/components/content/test/standalone/content.e2e.ts
+++ core/src/components/content/test/standalone/content.e2e.ts
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
+/**
+ * ion-content does not have mode-specific styling
+ */
 configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('content: standalone'), () => {
     test('should not have visual regressions', async ({ page }) => {
