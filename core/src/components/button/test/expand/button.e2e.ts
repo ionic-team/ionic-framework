ae90e418fae449aa76ffc173040a697561b83240
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-27 10:53:20 -0400

    Update button.e2e.ts

--- core/src/components/button/test/expand/button.e2e.ts
+++ core/src/components/button/test/expand/button.e2e.ts
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
+/**
+ * All content takes up the full width, so RTL has no effect.
+ */
 configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('button: expand'), () => {
     test('should not have visual regressions', async ({ page }) => {
