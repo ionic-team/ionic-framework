f08f58fed512a29acb29902df1c90cf752d14ce0
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-27 10:42:26 -0400

    test(content): migrate fullscreen

---
+++ core/src/components/content/test/fullscreen/content.e2e.ts
@@ -0,0 +1,14 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('content: fullscreen'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto(`/src/components/content/test/fullscreen`, config);
+
+      await page.setIonViewport();
+
+      await expect(page).toHaveScreenshot(screenshot(`content-fullscreen`));
+    });
+  });
+});
