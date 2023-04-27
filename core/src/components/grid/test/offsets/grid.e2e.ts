9ccb824086d592be0af789cb394a80500a1fa96e
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-27 15:29:56 -0400

    test(grid): migrate offsets

--- core/src/components/grid/test/offsets/grid.e2e.ts
+++ core/src/components/grid/test/offsets/grid.e2e.ts
@@ -5,13 +5,13 @@ import { configs, test } from '@utils/test/playwright';
  * ion-grid does not have different styling per-mode
  */
 configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
-  test.describe(title('grid: basic'), () => {
+  test.describe(title('grid: offsets'), () => {
     test('should not have visual regressions', async ({ page }) => {
-      await page.goto(`/src/components/grid/test/basic`, config);
+      await page.goto(`/src/components/grid/test/offsets`, config);
 
       await page.setIonViewport();
 
-      await expect(page).toHaveScreenshot(screenshot(`grid-basic`));
+      await expect(page).toHaveScreenshot(screenshot(`grid-offsets`));
     });
   });
 });
