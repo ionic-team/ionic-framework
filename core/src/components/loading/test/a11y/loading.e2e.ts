4acf843d3e2b2500d678addd6453b299cb0a6350
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-28 11:04:11 -0400

    test(loading): migrate a11y

---
+++ core/src/components/loading/test/a11y/loading.e2e.ts
@@ -0,0 +1,32 @@
+import AxeBuilder from '@axe-core/playwright';
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe(title('loading: a11y'), () => {
+    test('should set aria-labelledby with a message', async ({ page }) => {
+      await page.goto('/src/components/loading/test/a11y', config);
+
+      const ionLoadingDidPresent = await page.spyOnEvent('ionLoadingDidPresent');
+
+      await page.click('#open-message-loading');
+
+      await ionLoadingDidPresent.next();
+
+      const results = await new AxeBuilder({ page }).analyze();
+      expect(results.violations).toEqual([]);
+    });
+    test('should set aria-labelledby with a label', async ({ page }) => {
+      await page.goto('/src/components/loading/test/a11y', config);
+
+      const ionLoadingDidPresent = await page.spyOnEvent('ionLoadingDidPresent');
+
+      await page.click('#open-label-loading');
+
+      await ionLoadingDidPresent.next();
+
+      const results = await new AxeBuilder({ page }).analyze();
+      expect(results.violations).toEqual([]);
+    });
+  });
+});
