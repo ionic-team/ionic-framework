32ab63ac7d87348484162941dbca34f33a4dc07d
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-28 11:56:43 -0400

    test(datetime): migrate to generators (#27304)
    Issue number: N/A
    
    ---------
    
    <!-- Please refer to our contributing documentation for any questions on
    submitting a pull request, or let us know here if you need any help:
    https://ionicframework.com/docs/building/contributing -->
    
    <!-- Some docs updates need to be made in the `ionic-docs` repo, in a
    separate PR. See
    https://github.com/ionic-team/ionic-framework/blob/main/.github/CONTRIBUTING.md#modifying-documentation
    for details. -->
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    Datetime tests use the legacy syntax
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Datetime tests use the generator syntax
    
    **Note to reviewers:**
    
    I recommend reviewing this commit by commit because datetime has a lot
    of tests. A few commits I'd like to call out:
    
    
    https://github.com/ionic-team/ionic-framework/pull/27304/commits/77d9ab0ff24d23aafad053ed08a7ba8be3669ac7
    
    - I removed several screenshots that were not being tested anymore. It
    seems the actual test was modified/removed in the past, but the
    screenshot ground truths remained.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27304/commits/ed45c7fa057024bdbf79a41b2e3ab6a822e2685f
    
    - This test checks that the datetime can properly expand in width based
    on the `cover` property. As a result, no RTL tests are needed because
    the behavior does not vary across directions. I narrowed the config of
    this test to only check LTR and removed extraneous screenshots.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27304/commits/96c08410f21d1edca02c7975e9c3e9287b845815
    
    - This test only checks that the correct text content is used within the
    component. As a result, I narrowed the config to only check LTR
    direction and removed extraneous screenshots.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ core/src/components/datetime/test/time-label/datetime.e2e.ts
@@ -0,0 +1,31 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe(title('datetime: time label'), () => {
+    test('should render default time label', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-datetime></ion-datetime>
+      `,
+        config
+      );
+      await page.waitForSelector('.datetime-ready');
+
+      const timeLabel = page.locator('ion-datetime .time-header');
+      await expect(timeLabel).toHaveText('Time');
+    });
+    test('should not render a custom time label', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-datetime show-default-time-label="false"></ion-datetime>
+      `,
+        config
+      );
+      await page.waitForSelector('.datetime-ready');
+
+      const timeLabel = page.locator('ion-datetime .time-header');
+      await expect(timeLabel).toHaveText('');
+    });
+  });
+});
