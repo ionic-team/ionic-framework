dc4165874a37c9f5da2e86e0fd91750801f06a20
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-28 11:50:03 -0400

    test(label): migrate to generators (#27325)
    Issue number: N/A
    
    ---------
    
    <!-- Please refer to our contributing documentation for any questions on
    submitting a pull request, or let us know here if you need any help:
    https://ionicframework.com/docs/building/contributing -->
    
    <!-- Some docs updates need to be made in the `ionic-docs` repo, in a
    separate PR. See
    https://github.com/ionic-team/ionic-framework/blob/main/.github/CONTRIBUTING.md#modifying-documentation
    for details. -->
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    Label sliding tests use legacy syntax
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Label sliding tests use modern syntax
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ core/src/components/label/test/basic/label.e2e.ts
@@ -0,0 +1,63 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs().forEach(({ title, screenshot, config }) => {
+  test.describe(title('label: rendering'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-label>My Label</ion-label>
+      `,
+        config
+      );
+
+      const labelEl = page.locator('ion-label');
+
+      await expect(labelEl).toHaveScreenshot(screenshot(`label-basic`));
+    });
+    test('should not have visual regressions with fixed label', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-item>
+          <ion-label>My Label</ion-label>
+        </ion-item>
+      `,
+        config
+      );
+
+      const itemEl = page.locator('ion-item');
+
+      await expect(itemEl).toHaveScreenshot(screenshot(`label-fixed`));
+    });
+    test('should not have visual regressions with stacked label', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-item>
+          <ion-label position="stacked">My Label</ion-label>
+          <ion-input></ion-input>
+        </ion-item>
+      `,
+        config
+      );
+
+      const itemEl = page.locator('ion-item');
+
+      await expect(itemEl).toHaveScreenshot(screenshot(`label-stacked`));
+    });
+    test('should not have visual regressions with floating label', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-item>
+          <ion-label position="floating">My Label</ion-label>
+          <ion-input></ion-input>
+        </ion-item>
+      `,
+        config
+      );
+
+      const itemEl = page.locator('ion-item');
+
+      await expect(itemEl).toHaveScreenshot(screenshot(`label-floating`));
+    });
+  });
+});
