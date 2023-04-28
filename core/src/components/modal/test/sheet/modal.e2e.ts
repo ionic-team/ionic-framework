e1e7571c7a6ac1c656d255a68fe5a27e1f8be34b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-28 22:24:18 +0000

    remove unused import

--- core/src/components/modal/test/sheet/modal.e2e.ts
+++ core/src/components/modal/test/sheet/modal.e2e.ts
@@ -1,5 +1,5 @@
 import { expect } from '@playwright/test';
-import { configs, test, dragElementBy } from '@utils/test/playwright';
+import { configs, test } from '@utils/test/playwright';
 
 /**
  * Sheet does not have any additional RTL behaviors
