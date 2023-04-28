eaf1fd8a65348c0b8f9284257ee751bb08553291
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-28 10:26:41 -0400

    test(item): migrate slotted-inputs

---
+++ core/src/components/item/test/slotted-inputs/item.e2e.ts
@@ -0,0 +1,14 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs().forEach(({ title, screenshot, config }) => {
+  test.describe(title('item: slotted inputs'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto(`/src/components/item/test/slotted-inputs`, config);
+
+      await page.setIonViewport();
+
+      await expect(page).toHaveScreenshot(screenshot(`item-slotted-inputs`));
+    });
+  });
+});
