c3a39614785494bd5747f1ca81c224c77e9cc746
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-28 12:04:12 -0400

    test(item): migrate to generators (#27322)
    Issue number: N/A
    
    ---------
    
    <!-- Please refer to our contributing documentation for any questions on
    submitting a pull request, or let us know here if you need any help:
    https://ionicframework.com/docs/building/contributing -->
    
    <!-- Some docs updates need to be made in the `ionic-docs` repo, in a
    separate PR. See
    https://github.com/ionic-team/ionic-framework/blob/main/.github/CONTRIBUTING.md#modifying-documentation
    for details. -->
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    Item tests use legacy syntax
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Item tests use modern syntax
    
    
    https://github.com/ionic-team/ionic-framework/pull/27322/commits/414494a56d4445ac428d1d3c992e4c8c626aff77
    
    - This behavior does not vary across directions, so I removed the RTL
    screenshots.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27322/commits/8e9d0d5cbdb9ec93b03fe9a996f7286bd5651500
    
    - This behavior does not vary across directions, so I removed the RTL
    screenshots.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27322/commits/0d46d00eb0a259841d8e877de53dd7c1aa3bddae
    
    - This behavior does not vary across directions, so I removed the RTL
    screenshots.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27322/commits/505c054bd8e8936220c97fe21de0fa520810866c
    
    - This behavior does not vary across directions, so I removed the RTL
    screenshots.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ core/src/components/item/test/disabled/item.e2e.ts
@@ -0,0 +1,18 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+/**
+ * This behavior does not vary across directions
+ */
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('item: disabled state'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto(`/src/components/item/test/disabled`, config);
+
+      await page.setIonViewport();
+
+      // TODO: FW-4037 - Fix label color inconsistency between disabled controls
+      await expect(page).toHaveScreenshot(screenshot(`item-disabled-diff`));
+    });
+  });
+});
