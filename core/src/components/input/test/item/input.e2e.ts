b02846ee79ad36bf86a0ccdd1dcd74274fb266d3
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-01 17:08:31 -0400

    convert async label test to spec

--- core/src/components/input/test/item/input.e2e.ts
+++ core/src/components/input/test/item/input.e2e.ts
@@ -43,29 +43,5 @@ configs().forEach(({ title, screenshot, config }) => {
       const list = page.locator('ion-list');
       expect(await list.screenshot()).toMatchSnapshot(screenshot(`input-inset-list-no-fill`));
     });
-    test('input renders as modern when label is set asynchronously', async ({ page }, testInfo) => {
-      testInfo.annotations.push({
-        type: 'issue',
-        description: 'https://github.com/ionic-team/ionic-framework/issues/27085',
-      });
-
-      await page.setContent(
-        `<ion-item>
-          <ion-input></ion-input>
-        </ion-item>
-        `,
-        config
-      );
-      const input = page.locator('ion-input');
-      // Initial template should be modern
-      await expect(input).not.toHaveClass(/legacy-input/);
-
-      // Update the input label
-      await input.evaluate((el: HTMLIonInputElement) => (el.label = 'New label'));
-      await page.waitForChanges();
-
-      // Template should still be modern
-      await expect(input).not.toHaveClass(/legacy-input/);
-    });
   });
 });
