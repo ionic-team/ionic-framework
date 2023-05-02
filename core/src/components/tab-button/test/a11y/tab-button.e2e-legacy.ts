317298702426d6b55da5d25ca4a88478d6d79814
Brandy Carney <brandy@ionic.io>
2023-05-02 16:35:36 -0400

    fix(tab-button): use darker text to pass a11y

--- core/src/components/tab-button/test/a11y/tab-button.e2e-legacy.ts
+++ core/src/components/tab-button/test/a11y/tab-button.e2e-legacy.ts
@@ -6,8 +6,7 @@ test.describe('tab-button: a11y', () => {
   test('should not have any axe violations', async ({ page }) => {
     await page.goto('/src/components/tab-button/test/a11y');
 
-    // TODO FW-3604
-    const results = await new AxeBuilder({ page }).disableRules('color-contrast').analyze();
+    const results = await new AxeBuilder({ page }).analyze();
     expect(results.violations).toEqual([]);
   });
 });
