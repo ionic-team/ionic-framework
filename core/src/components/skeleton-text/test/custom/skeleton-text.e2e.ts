3e12b2e7347702486f464d0fd23eb2d0bac02584
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-03 16:59:22 -0400

    move screenshots for custom

--- core/src/components/skeleton-text/test/custom/skeleton-text.e2e.ts
+++ core/src/components/skeleton-text/test/custom/skeleton-text.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs().forEach(({ title, config }) => {
+configs().forEach(({ title, screenshot, config }) => {
   test.describe(title('skeleton-text: custom'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto('/src/components/skeleton-text/test/custom', config);
