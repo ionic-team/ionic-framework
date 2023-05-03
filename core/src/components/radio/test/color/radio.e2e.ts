35256d70ecd158bb323856911e4cb36f886daf88
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-03 11:53:35 -0400

    test(radio, radio-group): migrate to generators (#27365)
    Issue number: N/A
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    Radio and radio group tests are using legacy syntax
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Radio and radio group tests are using generator syntax 
    
    
    https://github.com/ionic-team/ionic-framework/pull/27365/commits/c0256f3ce1d6661a7ed6b23225c6e34d201067df
    
    - The radio group basic interaction tests do not vary across modes, so I
    removed those extra checks.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27365/commits/579bcacab2e3fa058da8263605feee68d199b0da
    
    - The basic radio directory had some old screenshots that were not being
    used, so I removed them.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27365/commits/997d652864df97d7c98bf8ca0c75634be940709d
    
    - The legacy radio keyboard behavior does not vary across modes, so I
    removed that extra check
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ core/src/components/radio/test/color/radio.e2e.ts
@@ -0,0 +1,34 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('radio: color'), () => {
+    test('should apply color when checked', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-radio-group value="1">
+          <ion-radio color="danger" value="1">Label</ion-radio>
+        </ion-radio-group>
+      `,
+        config
+      );
+
+      const radio = page.locator('ion-radio');
+      expect(await radio.screenshot()).toMatchSnapshot(screenshot(`radio-color-checked`));
+    });
+
+    test('should not apply color when unchecked', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-radio-group>
+          <ion-radio color="danger" value="1">Label</ion-radio>
+        </ion-radio-group>
+      `,
+        config
+      );
+
+      const radio = page.locator('ion-radio');
+      expect(await radio.screenshot()).toMatchSnapshot(screenshot(`radio-color-unchecked`));
+    });
+  });
+});
