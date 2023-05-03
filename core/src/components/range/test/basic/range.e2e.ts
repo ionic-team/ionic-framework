a2420ee69db8ecde774001990ed7a0ba77c0c664
amandaesmith3 <amanda.s@ionic.io>
2023-05-03 15:16:44 -0500

    lint

--- core/src/components/range/test/basic/range.e2e.ts
+++ core/src/components/range/test/basic/range.e2e.ts
@@ -9,9 +9,10 @@ configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
         description: 'https://github.com/ionic-team/ionic-framework/issues/21968',
       });
 
-      await page.setContent(`
-        <ion-range aria-label="range" step="0.05" min="0.1" max="1" value="0.1"></ion-range>
-      `, config);
+      await page.setContent(
+        `<ion-range aria-label="range" step="0.05" min="0.1" max="1" value="0.1"></ion-range>`,
+        config
+      );
 
       const range = page.locator('ion-range');
       await pageUtils.pressKeys('Tab'); // focus range
