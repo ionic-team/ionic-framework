e25c76977d642241b36ab6ca3f6eaf4240829770
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-03 09:40:59 -0400

    test(popover): migrate reference to fixture

--- core/src/components/popover/test/reference/popover.e2e.ts
+++ core/src/components/popover/test/reference/popover.e2e.ts
@@ -1,19 +1,23 @@
 import { configs, test } from '@utils/test/playwright';
 
-import { screenshotPopover } from '../test.utils';
+import { PopoverFixture } from '../fixture';
 
 configs().forEach(({ title, screenshot, config }) => {
   test.describe(title('popover: reference'), async () => {
+    let popoverFixture!: PopoverFixture;
     test.beforeEach(async ({ page }) => {
-      await page.goto(`src/components/popover/test/reference`, config);
+      popoverFixture = new PopoverFixture(page);
+      await popoverFixture.goto(`src/components/popover/test/reference`, config);
     });
 
-    test('should position popover relative to mouse click', async ({ page }) => {
-      await screenshotPopover(page, screenshot, 'event-trigger', 'reference');
+    test('should position popover relative to mouse click', async () => {
+      await popoverFixture.open('#event-trigger');
+      await popoverFixture.screenshot('reference-event-trigger', screenshot);
     });
 
-    test('should position popover relative to trigger', async ({ page }) => {
-      await screenshotPopover(page, screenshot, 'trigger-trigger', 'reference');
+    test('should position popover relative to trigger', async () => {
+      await popoverFixture.open('#trigger-trigger');
+      await popoverFixture.screenshot('reference-trigger-trigger', screenshot);
     });
   });
 });
