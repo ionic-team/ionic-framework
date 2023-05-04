14e000db76f26539e0ab929b4eee9146fea79c1c
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-04 11:49:46 -0400

    test(utils): migrate to generators (#27383)
    Issue number: N/A
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    Animation, framework delegate, and overlay tests are using legacy syntax
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Animation, framework delegate, and overlay tests are using generator
    syntax
    
    
    https://github.com/ionic-team/ionic-framework/pull/27383/commits/513e8508422460ca435b487dc541be269f86192d
    
    The overlay focus tests do not vary across modes, so I removed the extra
    checks.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ core/src/utils/animation/test/basic/animation.e2e.ts
@@ -0,0 +1,24 @@
+import { configs, test } from '@utils/test/playwright';
+import type { E2EPage } from '@utils/test/playwright';
+
+configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe(title('animation: basic'), async () => {
+    test(`should resolve using web animations`, async ({ page }) => {
+      await page.goto('/src/utils/animation/test/basic', config);
+      await testPage(page);
+    });
+
+    test(`should resolve using css animations`, async ({ page }) => {
+      await page.goto('/src/utils/animation/test/basic?ionic:_forceCSSAnimations=true', config);
+      await testPage(page);
+    });
+  });
+});
+
+const testPage = async (page: E2EPage) => {
+  const ionAnimationFinished = await page.spyOnEvent('ionAnimationFinished');
+
+  await page.click('.play');
+
+  await ionAnimationFinished.next();
+};
