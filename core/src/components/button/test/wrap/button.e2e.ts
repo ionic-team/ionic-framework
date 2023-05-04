966ebaeae7e952a781d60f72d0849dfacb32ee8a
Brandy Carney <brandy@ionic.io>
2023-05-04 17:24:19 -0400

    test(button): add tests for button text wrapping

---
+++ core/src/components/button/test/wrap/button.e2e.ts
@@ -0,0 +1,97 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('button: wrap'), () => {
+    test('should render button with long text', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-button>This is the button that never ends it just goes on and on my friends</ion-button>
+      `,
+        config
+      );
+
+      const wrapper = page.locator('ion-button');
+
+      await expect(wrapper).toHaveScreenshot(screenshot(`button-wrap`));
+    });
+
+    test('should render button with long text and icons', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-button>
+          <ion-icon slot="start" name="heart"></ion-icon>
+          This is the button that never ends it just goes on and on my friends
+          <ion-icon slot="end" name="star"></ion-icon>
+        </ion-button>
+      `,
+        config
+      );
+
+      const wrapper = page.locator('ion-button');
+
+      await expect(wrapper).toHaveScreenshot(screenshot(`button-wrap-icons`));
+    });
+
+    test('should render block button with long text', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-button expand="block">This is the button that never ends it just goes on and on my friends</ion-button>
+      `,
+        config
+      );
+
+      const wrapper = page.locator('ion-button');
+
+      await expect(wrapper).toHaveScreenshot(screenshot(`button-wrap-block`));
+    });
+
+    test('should render block button with long text and icons', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-button expand="block">
+          <ion-icon slot="start" name="heart"></ion-icon>
+          This is the button that never ends it just goes on and on my friends
+          <ion-icon slot="end" name="star"></ion-icon>
+        </ion-button>
+      `,
+        config
+      );
+
+      const wrapper = page.locator('ion-button');
+
+      await expect(wrapper).toHaveScreenshot(screenshot(`button-wrap-block-icons`));
+    });
+
+    test('should render full button with long text', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-button expand="full">This is the button that never ends it just goes on and on my friends</ion-button>
+      `,
+        config
+      );
+
+      const wrapper = page.locator('ion-button');
+
+      await expect(wrapper).toHaveScreenshot(screenshot(`button-wrap-full`));
+    });
+
+    test('should render full button with long text and icons', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-button expand="full">
+          <ion-icon slot="start" name="heart"></ion-icon>
+          This is the button that never ends it just goes on and on my friends
+          <ion-icon slot="end" name="star"></ion-icon>
+        </ion-button>
+      `,
+        config
+      );
+
+      const wrapper = page.locator('ion-button');
+
+      await expect(wrapper).toHaveScreenshot(screenshot(`button-wrap-full-icons`));
+    });
+
+  });
+});
