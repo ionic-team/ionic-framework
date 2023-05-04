75c382a4d15554bafb0c7e3b14e320b0d555ea4b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-04 10:49:15 -0400

    add screenshot fn

--- core/src/components/select/test/legacy/single-value/select.e2e.ts
+++ core/src/components/select/test/legacy/single-value/select.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ directions: ['ltr'] }).forEach(({ title, config }) => {
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('select: single-value'), () => {
     test('should open single value select', async ({ page }) => {
       await page.goto(`/src/components/select/test/legacy/single-value`, config);
