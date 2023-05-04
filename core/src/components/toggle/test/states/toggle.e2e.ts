c1fe9402a6707618b721d008e0d5b76129537255
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-04 12:40:05 -0400

    test(toggle): migrate to generators (#27390)
    Issue number: N/A
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    Toggle tests are using legacy syntax
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Toggle tests are using modern syntax
    
    
    https://github.com/ionic-team/ionic-framework/pull/27390/commits/16bb695631035cf35128480941ffec1a310b0357
    
    - The basic directory had unused screenshots, so I removed them.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27390/commits/8b2480c391beb1fbbe3342bbc20611fdc6f22498
    
    - The on/off dark mode tests do not vary across directions, so I removed
    the RTL checks
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ core/src/components/toggle/test/states/toggle.e2e.ts
@@ -0,0 +1,42 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('toggle: states'), () => {
+    test('should render disabled toggle correctly', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-toggle disabled="true">Label</ion-toggle>
+      `,
+        config
+      );
+
+      const toggle = page.locator('ion-toggle');
+      expect(await toggle.screenshot()).toMatchSnapshot(screenshot(`toggle-disabled`));
+    });
+
+    test('should render checked toggle correctly', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-toggle checked="true">Label</ion-toggle>
+      `,
+        config
+      );
+
+      const toggle = page.locator('ion-toggle');
+      expect(await toggle.screenshot()).toMatchSnapshot(screenshot(`toggle-checked`));
+    });
+
+    test('should render unchecked toggle correctly', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-toggle>Label</ion-toggle>
+      `,
+        config
+      );
+
+      const toggle = page.locator('ion-toggle');
+      expect(await toggle.screenshot()).toMatchSnapshot(screenshot(`toggle-unchecked`));
+    });
+  });
+});
