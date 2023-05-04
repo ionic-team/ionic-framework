625c872d933444e8727b737eacd4799cef01da03
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-04 11:26:40 -0400

    test(range): migrate to generators (#27369)
    Issue number: N/A
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    Range tests are using legacy syntax
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Range tests are using generator syntax
    
    
    https://github.com/ionic-team/ionic-framework/pull/27369/commits/849e12797426d293be0424bd9c8ed0df03817e1f
    
    - The scroll target tests do not vary across modes so I removed those
    additional checks.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27369/commits/0b8c15838a5f834a3b9ed3fb667b10ccd682eee9
    
    - The standalone directory had some screenshots that were not in use, so
    I deleted them
    
    
    https://github.com/ionic-team/ionic-framework/pull/27369/commits/5b306f28b7add5645cbe0adef6ab6cde17dd98d8
    
    - The modern range event tests do not vary across modes/directions, so I
    removed the additional checks.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27369/commits/699fb62789dfbc0a6325ff18138ba554ae10d1d6
    
    - The legacy scroll target tests do not vary across modes so I removed
    those additional checks.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27369/commits/c7dbb2d38049d057072d0fadc6429cd949af7b07
    
    - The legacy range functional tests do not vary across modes/directions,
    so I removed the additional checks.
    
    
    https://github.com/ionic-team/ionic-framework/pull/27369/commits/aa06ba8ebf7ab56c9f240252faf9a0485fe55b45
    
    - The legacy range event tests do not vary across modes/directions, so I
    removed the additional checks.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

---
+++ core/src/components/range/test/color/range.e2e.ts
@@ -0,0 +1,22 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('range: color'), () => {
+    test('should apply color', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-range color="danger" value="50">
+          <ion-icon name="volume-off" slot="start"></ion-icon>
+          <ion-icon name="volume-high" slot="end"></ion-icon>
+          <span slot="label">Volume</span>
+        </ion-range>
+      `,
+        config
+      );
+
+      const range = page.locator('ion-range');
+      expect(await range.screenshot()).toMatchSnapshot(screenshot(`range-color`));
+    });
+  });
+});
