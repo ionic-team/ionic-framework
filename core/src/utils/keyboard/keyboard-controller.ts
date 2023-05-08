caaaac8b2f6253a50d8a4ff81bc7a32361294cdb
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-08 13:28:28 -0400

    fix(footer, tab-bar): wait for resize before showing

--- core/src/utils/keyboard/keyboard-controller.ts
+++ core/src/utils/keyboard/keyboard-controller.ts
@@ -1,3 +1,4 @@
+import { KeyboardResize, Keyboard } from '../native/keyboard';
 import { win } from '../window';
 
 /**
@@ -6,26 +7,72 @@ import { win } from '../window';
  * @internal
  * @param keyboardChangeCallback A function to call when the keyboard opens or closes.
  */
-export const createKeyboardController = (
-  keyboardChangeCallback?: (keyboardOpen: boolean) => void
-): KeyboardController => {
+export const createKeyboardController = async (
+  keyboardChangeCallback?: (keyboardOpen: boolean, resizePromise?: Promise<void>) => void
+): Promise<KeyboardController> => {
   let keyboardWillShowHandler: (() => void) | undefined;
   let keyboardWillHideHandler: (() => void) | undefined;
   let keyboardVisible: boolean;
 
-  const init = () => {
+  const init = async () => {
+    const resizeOptions = await Keyboard.getResizeMode();
+    const resizeMode = resizeOptions == undefined ? undefined : resizeOptions.mode;
+
     keyboardWillShowHandler = () => {
       keyboardVisible = true;
-      if (keyboardChangeCallback) keyboardChangeCallback(true);
+      fireChangeCallback(keyboardVisible, resizeMode);
     };
 
     keyboardWillHideHandler = () => {
       keyboardVisible = false;
-      if (keyboardChangeCallback) keyboardChangeCallback(false);
+      fireChangeCallback(keyboardVisible, resizeMode);
     };
 
     win?.addEventListener('keyboardWillShow', keyboardWillShowHandler);
     win?.addEventListener('keyboardWillHide', keyboardWillHideHandler);
+  };
+
+  const fireChangeCallback = (state: boolean, resizeMode: KeyboardResize | undefined) => {
+    if (keyboardChangeCallback) {
+      /**
+       * If the resize mode is undefined then we cannot safely
+       * assume that the web content will resize. This will
+       * be the case if an app is deployed to a mobile browser/PWA
+       * as the native Capacitor keyboard plugin will not be available.
+       */
+      const resizePromise =
+        resizeMode === undefined || resizeMode === KeyboardResize.None ? undefined : createResizePromise();
+
+      keyboardChangeCallback(state, resizePromise);
+    }
+  };
+
+  /**
+   * Code responding to keyboard lifecycles may need
+   * to show/hide content once the webview has
+   * resized as a result of the keyboard showing/hiding.
+   * resizePromise provides a way for code to wait for the
+   * resize event that was triggered as a result of the keyboard.
+   */
+  const createResizePromise = (): Promise<void> => {
+    return new Promise((resolve) => {
+      let timeout: ReturnType<typeof setTimeout> | undefined;
+      const callback = () => {
+        if (timeout !== undefined) {
+          clearTimeout(timeout);
+          timeout = undefined;
+        }
+        resolve();
+      };
+      win?.addEventListener('resize', callback, { once: true });
+
+      /**
+       * In the event that the `resize` event never fires,
+       * create a fallback of 1000ms so code does not get stuck
+       * in an inconsistent state.
+       */
+      timeout = setTimeout(callback, 1000);
+    });
   };
 
   const destroy = () => {
@@ -37,7 +84,7 @@ export const createKeyboardController = (
 
   const isKeyboardVisible = () => keyboardVisible;
 
-  init();
+  await init();
   return { init, destroy, isKeyboardVisible };
 };
 
