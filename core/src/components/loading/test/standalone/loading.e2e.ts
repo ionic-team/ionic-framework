f6dac8b47c93765085bc32cbbba9a5f78d156ea6
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-09 09:35:16 -0400

    test(loading): standalone test checks visibility

--- core/src/components/loading/test/standalone/loading.e2e.ts
+++ core/src/components/loading/test/standalone/loading.e2e.ts
@@ -3,24 +3,21 @@ import { configs, test } from '@utils/test/playwright';
 
 configs().forEach(({ title, screenshot, config }) => {
   test.describe(title('loading: standalone'), () => {
-    test.beforeEach(async ({ page }) => {
-      await page.goto('/src/components/loading/test/standalone', config);
-    });
     test('should open a basic loader', async ({ page }) => {
+      await page.goto('/src/components/loading/test/standalone', config);
+
       const ionLoadingDidPresent = await page.spyOnEvent('ionLoadingDidPresent');
       const ionLoadingDidDismiss = await page.spyOnEvent('ionLoadingDidPresent');
+      const loading = page.locator('ion-loading');
 
       await page.click('#basic-loading');
 
       await ionLoadingDidPresent.next();
+      await expect(loading).toBeVisible();
 
-      await expect(page).toHaveScreenshot(screenshot(`loading-standalone-diff`));
-
-      const loading = page.locator('ion-loading');
       await loading.evaluate((el: HTMLIonLoadingElement) => el.dismiss());
 
       await ionLoadingDidDismiss.next();
-
       await expect(loading).toBeHidden();
     });
   });
