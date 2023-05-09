fc7c166e0e715f6976b0c137debd4b3d82752569
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-09 09:30:39 -0400

    test(loading): remove redundant variant tests

--- core/src/components/loading/test/basic/loading.e2e.ts
+++ core/src/components/loading/test/basic/loading.e2e.ts
@@ -1,53 +1,68 @@
 import { expect } from '@playwright/test';
-import type { E2EPage } from '@utils/test/playwright';
+import type { E2EPage, ScreenshotFn } from '@utils/test/playwright';
 import { configs, test } from '@utils/test/playwright';
+
+const runVisualTest = async (page: E2EPage, selector: string, screenshot: ScreenshotFn, screenshotModifier: string) => {
+  const ionLoadingDidPresent = await page.spyOnEvent('ionLoadingDidPresent');
+  const ionLoadingDidDismiss = await page.spyOnEvent('ionLoadingDidPresent');
+
+  await page.click(selector);
+
+  await ionLoadingDidPresent.next();
+
+  await expect(page).toHaveScreenshot(screenshot(`loading-${screenshotModifier}-diff`));
+
+  const loading = page.locator('ion-loading');
+  await loading.evaluate((el: HTMLIonLoadingElement) => el.dismiss());
+
+  await ionLoadingDidDismiss.next();
+
+  await expect(loading).toBeHidden();
+};
 
 configs().forEach(({ title, screenshot, config }) => {
   test.describe(title('loading: basic'), () => {
-    test.beforeEach(async ({ page }) => {
+    test('should open a basic loader', async ({ page }) => {
       await page.goto('/src/components/loading/test/basic', config);
-    });
-    test.describe('loading: visual regression tests', () => {
-      const runVisualTest = async (page: E2EPage, selector: string, screenshotModifier: string) => {
-        const ionLoadingDidPresent = await page.spyOnEvent('ionLoadingDidPresent');
-        const ionLoadingDidDismiss = await page.spyOnEvent('ionLoadingDidPresent');
-
-        await page.click(selector);
-
-        await ionLoadingDidPresent.next();
-
-        await expect(page).toHaveScreenshot(screenshot(`loading-${screenshotModifier}-diff`));
-
-        const loading = page.locator('ion-loading');
-        await loading.evaluate((el: HTMLIonLoadingElement) => el.dismiss());
-
-        await ionLoadingDidDismiss.next();
-
-        await expect(loading).toBeHidden();
-      };
-      test('should open a basic loader', async ({ page }) => {
-        await runVisualTest(page, '#basic-loading', 'basic');
-      });
-      test('should open a loader with long text', async ({ page }) => {
-        await runVisualTest(page, '#long-content-loading', 'long-content');
-      });
-      test('should open a loader with no spinner', async ({ page }) => {
-        await runVisualTest(page, '#no-spinner-loading', 'no-spinner');
-      });
-      test('should open a translucent loader', async ({ page }) => {
-        await runVisualTest(page, '#translucent-loading', 'translucent');
-      });
-      test('should open a loader with a custom class', async ({ page }) => {
-        await runVisualTest(page, '#custom-class-loading', 'custom-class');
-      });
+      await runVisualTest(page, '#basic-loading', screenshot, 'basic');
     });
   });
 });
 
 /**
- * These behaviors do not vary across modes/directions
+ * These behaviors do not vary across directions.
  */
-configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('loading: variants rendering'), () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/src/components/loading/test/basic', config);
+    });
+    test('should open a loader with long text', async ({ page }) => {
+      await runVisualTest(page, '#long-content-loading', screenshot, 'long-content');
+    });
+    test('should open a loader with no spinner', async ({ page }) => {
+      await runVisualTest(page, '#no-spinner-loading', screenshot, 'no-spinner');
+    });
+    test('should open a loader with a custom class', async ({ page }) => {
+      await runVisualTest(page, '#custom-class-loading', screenshot, 'custom-class');
+    });
+  });
+});
+
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  /**
+   * Translucency is only available on iOS.
+   */
+  test.describe(title('loading: translucent rendering'), () => {
+    test('should open a translucent loader', async ({ page }) => {
+      await page.goto('/src/components/loading/test/basic', config);
+      await runVisualTest(page, '#translucent-loading', screenshot, 'translucent');
+    });
+  });
+
+  /**
+   * These behaviors do not vary across modes/directions
+   */
   test.describe(title('loading: focus trapping'), () => {
     test('it should trap focus in the loader', async ({ page, browserName }) => {
       await page.goto('/src/components/loading/test/basic', config);
