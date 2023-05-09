ec529cfe1292e3a09f071f98a0220e3ba4cfeada
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-09 12:06:15 -0400

    test(toast): isOpen tests open and close behavior

--- core/src/components/toast/test/is-open/toast.e2e.ts
+++ core/src/components/toast/test/is-open/toast.e2e.ts
@@ -6,29 +6,20 @@ import { configs, test } from '@utils/test/playwright';
  */
 configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
   test.describe(title('toast: isOpen'), () => {
-    test.beforeEach(async ({ page }) => {
+    test('should open and close the toast', async ({ page }) => {
       await page.goto('/src/components/toast/test/is-open', config);
-    });
 
-    test('should open the toast', async ({ page }) => {
       const ionToastDidPresent = await page.spyOnEvent('ionToastDidPresent');
+      const ionToastDidDismiss = await page.spyOnEvent('ionToastDidDismiss');
+
       const toast = page.locator('ion-toast');
 
       await page.click('#default');
 
       await ionToastDidPresent.next();
       await expect(toast).toBeVisible();
-    });
 
-    test('should open the toast then close after a timeout', async ({ page }) => {
-      const ionToastDidPresent = await page.spyOnEvent('ionToastDidPresent');
-      const ionToastDidDismiss = await page.spyOnEvent('ionToastDidDismiss');
-      const toast = page.locator('ion-toast');
-
-      await page.click('#timeout');
-
-      await ionToastDidPresent.next();
-      await expect(toast).toBeVisible();
+      await toast.evalutae((el: HTMLIonToastElement) => el.isOpen = false);
 
       await ionToastDidDismiss.next();
       await expect(toast).toBeHidden();
