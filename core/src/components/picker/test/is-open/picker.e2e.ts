feb1f6c1388c234d194d31bb7ef99c82c8642878
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-09 11:59:36 -0400

    test(picker): isOpen tests open and close behavior

--- core/src/components/picker/test/is-open/picker.e2e.ts
+++ core/src/components/picker/test/is-open/picker.e2e.ts
@@ -6,29 +6,19 @@ import { configs, test } from '@utils/test/playwright';
  */
 configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
   test.describe(title('picker: isOpen'), () => {
-    test.beforeEach(async ({ page }) => {
-      await page.goto('/src/components/picker/test/is-open', config);
-    });
-
     test('should open the picker', async ({ page }) => {
+      await page.goto('/src/components/picker/test/is-open', config);
+
       const ionPickerDidPresent = await page.spyOnEvent('ionPickerDidPresent');
+      const ionPickerDidDismiss = await page.spyOnEvent('ionPickerDidDismiss');
       const picker = page.locator('ion-picker');
 
       await page.click('#default');
 
       await ionPickerDidPresent.next();
       await expect(picker).toBeVisible();
-    });
 
-    test('should open the picker then close after a timeout', async ({ page }) => {
-      const ionPickerDidPresent = await page.spyOnEvent('ionPickerDidPresent');
-      const ionPickerDidDismiss = await page.spyOnEvent('ionPickerDidDismiss');
-      const picker = page.locator('ion-picker');
-
-      await page.click('#timeout');
-
-      await ionPickerDidPresent.next();
-      await expect(picker).toBeVisible();
+      await picker.evaluate((el: HTMLIonPickerElement) => (el.isOpen = false));
 
       await ionPickerDidDismiss.next();
       await expect(picker).toBeHidden();
