f6f2dc4d970288af1b0c85a2587d82c727d6e93f
Maria Hutt <maria@ionic.io>
2023-05-11 14:26:23 -0700

    fix(picker-column): correct RTL direction

--- core/src/components/picker-column/test/standalone/picker-column.e2e.ts
+++ core/src/components/picker-column/test/standalone/picker-column.e2e.ts
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
 configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
-  test.describe(title('picker-column'), () => {
+  test.describe(title('picker-column: standalone'), () => {
     test('should present picker without ion-app', async ({ page }) => {
       await page.goto('/src/components/picker-column/test/standalone', config);
 
@@ -15,6 +15,30 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
       await ionPickerDidPresent.next();
 
       await expect(picker).toBeVisible();
+    });
+  });
+});
+
+/**
+ * ion-picker-column does not have mode-specific styling for document direction
+ */
+configs({ modes: ['ios'] }).forEach(({ title, config, screenshot }) => {
+  test.describe(title('picker-column: document direction'), () => {
+    test('should not have visual regressions', async ({ page }, testInfo) => {
+      testInfo.annotations.push({
+        type: 'issue',
+        description: 'https://github.com/ionic-team/ionic-framework/issues/21205',
+      });
+
+      await page.goto('/src/components/picker-column/test/standalone', config);
+
+      const ionPickerDidPresent = await page.spyOnEvent('ionPickerDidPresent');
+
+      await page.click('#multiple-column-button');
+
+      await ionPickerDidPresent.next();
+
+      await expect(page).toHaveScreenshot(screenshot(`picker-column-document-direction`));
     });
   });
 });
