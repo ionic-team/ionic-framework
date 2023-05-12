821d1327de5e2a93517fba716f7942e3382418a6
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-05-12 10:00:37 -0400

    test(chip): break tests up

--- core/src/components/chip/test/states/chip.e2e.ts
+++ core/src/components/chip/test/states/chip.e2e.ts
@@ -1,14 +1,43 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs().forEach(({ title, screenshot, config }) => {
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  /**
+   * This behavior does not vary across modes/directions.
+   */
   test.describe(title('chip: states'), () => {
-    test('should not have visual regressions', async ({ page }) => {
-      await page.goto('/src/components/chip/test/states', config);
+    test('should render disabled state', async ({ page }) => {
+      await page.setContent(
+        `<ion-chip disabled="true">
+          <ion-label>Disabled</ion-label>
+        </ion-chip>`,
+        config
+      );
 
-      await page.setIonViewport();
+      const chip = page.locator('ion-chip');
 
-      await expect(page).toHaveScreenshot(screenshot(`chip-states`));
+      await expect(chip).toHaveScreenshot(screenshot(`chip-disabled`));
+    });
+    test('should custom chip', async ({ page }) => {
+      await page.setContent(
+        `
+        <style>
+          ion-chip {
+            --background: green;
+            --color: black;
+
+            opacity: 1;
+          }
+        </style>
+        <ion-chip disabled="true">
+          <ion-label>Custom</ion-label>
+        </ion-chip>`,
+        config
+      );
+
+      const chip = page.locator('ion-chip');
+
+      await expect(chip).toHaveScreenshot(screenshot(`chip-custom`));
     });
   });
 });
