b40cacf24ee22d91c7f36d26c53672f675e98118
amandaesmith3 <amanda.s@ionic.io>
2023-05-16 13:35:16 -0500

    remove extra log

--- angular/src/providers/modal-controller.ts
+++ angular/src/providers/modal-controller.ts
@@ -16,7 +16,6 @@ export class ModalController extends OverlayBaseController<ModalOptions, HTMLIon
   }
 
   create(opts: ModalOptions): Promise<HTMLIonModalElement> {
-    console.log("opts:", opts);
     return super.create({
       ...opts,
       delegate: this.angularDelegate.create(this.environmentInjector, this.injector, 'modal'),
