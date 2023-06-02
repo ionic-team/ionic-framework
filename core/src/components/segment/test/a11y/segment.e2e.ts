cd71093a0705a7d5fcd164bea7f26958d67e002d
Maria Hutt <maria@ionic.io>
2023-06-02 12:47:49 -0700

    test(segment): wait for visibility

--- core/src/components/segment/test/a11y/segment.e2e.ts
+++ core/src/components/segment/test/a11y/segment.e2e.ts
@@ -11,33 +11,23 @@ configs().forEach(({ title, config }) => {
       expect(results.violations).toEqual([]);
     });
 
-    test('segment buttons should be keyboard navigable', async ({ page, pageUtils }) => {
+    test.only('segment buttons should be keyboard navigable', async ({ page, pageUtils }) => {
       const isRTL = config.direction === 'rtl';
       const nextKey = isRTL ? 'ArrowLeft' : 'ArrowRight';
       const previousKey = isRTL ? 'ArrowRight' : 'ArrowLeft';
 
-      // A flaky test only occurs on "Mobile Chrome" when the page has
-      // another segment group.
-      // If it occurs, the focus state gets stuck in the middle of transition.
-      // This appears to happen due to the page still loading in JS while the
-      // test is running, which can be fixed by using `waitForChanges`.
-      // https://github.com/microsoft/playwright/issues/14422
-      await page.setContent(
-        `<ion-segment aria-label="Tab Options" color="dark" select-on-focus>
-      <ion-segment-button value="bookmarks">
-        <ion-label>Bookmarks</ion-label>
-      </ion-segment-button>
-      <ion-segment-button value="reading-list">
-        <ion-label>Reading List</ion-label>
-      </ion-segment-button>
-      <ion-segment-button value="shared-links">
-        <ion-label>Shared Links</ion-label>
-      </ion-segment-button>
-    </ion-segment>`,
-        config
-      );
+      await page.goto('/src/components/segment/test/a11y', config);
 
       const segmentButtons = page.locator('ion-segment-button');
+
+      // A flaky test only occurs on "Mobile Chrome"
+      // If it occurs, the elements are not visible yet
+      // when the test presses the any key on the keyboard
+      // This may be due to the fact that the elements are
+      // still updating their state before the render method
+      // The workaround is to wait for the first element to be visible
+      // If the first element is visible, then the rest of the elements are visible
+      await segmentButtons.nth(0).waitFor();
 
       await pageUtils.pressKeys('Tab');
       await expect(segmentButtons.nth(0)).toBeFocused();
