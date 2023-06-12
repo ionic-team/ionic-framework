d48a950e020e1189841fb267870e4d9bd3047cc6
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-06-12 16:45:49 -0400

    listen for changes to slotted content itself

--- core/src/utils/forms/slot-mutation-controller.ts
+++ core/src/utils/forms/slot-mutation-controller.ts
@@ -2,8 +2,7 @@ import { win } from '@utils/browser';
 
 /**
  * Used to update a scoped component that uses emulated slots. This fires when
- * content is passed into the slot. It does not fire when existing
- * slotted content changes.
+ * content is passed into the slot or when the content inside of a slot changes.
  * This is not needed for components using native slots in the Shadow DOM.
  * @internal
  * @param el The host element to observe
@@ -15,12 +14,24 @@ export const createSlotMutationController = (
   slotName: string,
   mutationCallback: () => void
 ): SlotMutationController => {
-  let mutationObserver: MutationObserver | undefined;
+  let hostMutationObserver: MutationObserver | undefined;
+  let slottedContentMutationObserver: MutationObserver | undefined;
+
   if (win !== undefined && 'IntersectionObserver' in win) {
-    mutationObserver = new MutationObserver((entries) => {
+    hostMutationObserver = new MutationObserver((entries) => {
       for (const entry of entries) {
         for (const node of entry.addedNodes) {
-          if (node.nodeType === Node.ELEMENT_NODE && (node as Element).slot === slotName) {
+          /**
+           * Check to see if the added node
+           *  is our slotted content.
+           */
+          if (node.nodeType === Node.ELEMENT_NODE && (node as HTMLElement).slot === slotName) {
+            /**
+             * If so, we want to watch the slotted
+             * content itself for changes. This lets us
+             * detect when content inside of the slot changes.
+             */
+            watchForSlotChange(node as HTMLElement);
             mutationCallback();
             return;
           }
@@ -28,15 +39,46 @@ export const createSlotMutationController = (
       }
     });
 
-    mutationObserver.observe(el, {
+    hostMutationObserver.observe(el, {
       childList: true,
     });
   }
 
+  /**
+   * Listen for changes inside of the slotted content.
+   * We can listen for subtree changes here to be
+   * informed of text within the slotted content
+   * changing. Doing this on the host is possible
+   * but it is much more expensive to do because
+   * it also listens for changes to the internals
+   * of the component.
+   */
+  const watchForSlotChange = (slottedEl: HTMLElement) => {
+    if (slottedContentMutationObserver) {
+      slottedContentMutationObserver.disconnect();
+      slottedContentMutationObserver = undefined;
+    }
+
+    slottedContentMutationObserver = new MutationObserver(() => {
+      mutationCallback();
+    });
+
+    /**
+     * Listen for changes inside of the element
+     * as well as anything deep in the tree.
+     */
+    slottedContentMutationObserver.observe(slottedEl, { subtree: true, childList: true });
+  };
+
   const destroy = () => {
-    if (mutationObserver) {
-      mutationObserver.disconnect();
-      mutationObserver = undefined;
+    if (hostMutationObserver) {
+      hostMutationObserver.disconnect();
+      hostMutationObserver = undefined;
+    }
+
+    if (slottedContentMutationObserver) {
+      slottedContentMutationObserver.disconnect();
+      slottedContentMutationObserver = undefined;
     }
   };
 
