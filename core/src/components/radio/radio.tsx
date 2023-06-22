5d6f519eeba62d4c78d3d97439eaf10087e06510
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-06-22 12:50:43 -0400

    fix(radio): avoid nested interactive

--- core/src/components/radio/radio.tsx
+++ core/src/components/radio/radio.tsx
@@ -249,18 +249,6 @@ export class Radio implements ComponentInterface {
         tabindex={buttonTabindex}
       >
         <label class="radio-wrapper">
-          {/*
-            The native control must be rendered
-            before the visible label text due to https://bugs.webkit.org/show_bug.cgi?id=251951
-          */}
-          <input
-            type="radio"
-            checked={checked}
-            disabled={disabled}
-            tabindex={-1}
-            ref={(nativeEl) => (this.nativeInput = nativeEl as HTMLInputElement)}
-            aria-hidden="true"
-          />
           <div
             class={{
               'label-text-wrapper': true,
