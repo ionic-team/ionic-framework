b418632450ffddb139b8a6837a3d99ad57cfc9a1
Sean Perkins <sean@ionic.io>
2023-06-29 10:17:07 -0400

    chore: remove IonReactHashRouter
    React Router v6 exposes hooks to listen for history/location change. This implementation doesn't provide anything extra and should be removed completely AFAIK.

--- packages/react-router/src/index.ts
+++ packages/react-router/src/index.ts
@@ -1,3 +1,2 @@
 export { IonReactRouter } from './ReactRouter/IonReactRouter';
 export { IonReactMemoryRouter } from './ReactRouter/IonReactMemoryRouter';
-export { IonReactHashRouter } from './ReactRouter/IonReactHashRouter';
