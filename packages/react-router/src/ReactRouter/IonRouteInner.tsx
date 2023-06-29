0936639f819ad3ea233c5180079ebd4d6ca4421d
Sean Perkins <sean@ionic.io>
2023-06-29 01:26:02 -0400

    chore: migrate IonRouteInner to react-router v6

--- packages/react-router/src/ReactRouter/IonRouteInner.tsx
+++ packages/react-router/src/ReactRouter/IonRouteInner.tsx
@@ -2,29 +2,6 @@ import type { IonRouteProps } from '@ionic/react';
 import React from 'react';
 import { Route } from 'react-router';
 
-export class IonRouteInner extends React.PureComponent<IonRouteProps> {
-  render() {
-    return (
-      <Route
-        path={this.props.path}
-        exact={this.props.exact}
-        render={this.props.render}
-        {
-          /**
-           * `computedMatch` is a private API in react-router v5 that
-           * has been removed in v6.
-           *
-           * This needs to be removed when we support v6.
-           *
-           * TODO: FW-647
-           */
-          ...((this.props as any).computedMatch !== undefined
-            ? {
-                computedMatch: (this.props as any).computedMatch,
-              }
-            : {})
-        }
-      />
-    );
-  }
+export function IonRouteInner(props: IonRouteProps) {
+  return <Route path={props.path} children={props.render} />;
 }
