6bbab525d047d7475dc9a902a4bc574a6ba78cc7
Sean Perkins <sean@ionic.io>
2023-07-03 12:33:24 -0400

    chore: remove previous IonRouterOutlet implementation

--- packages/react/src/components/IonRouterOutlet.tsx
+++ packages/react/src/components/IonRouterOutlet.tsx
@@ -49,41 +49,6 @@ const IonRouterOutletContainer = (props: PropsWithChildren<InternalProps>) => {
   );
 };
 
-// class IonRouterOutletContainer extends React.Component<InternalProps, InternalState> {
-//   context!: React.ContextType<typeof NavContext>;
-
-//   constructor(props: InternalProps) {
-//     super(props);
-//   }
-
-//   render() {
-//     const StackManager = this.context.getStackManager();
-//     const { children, forwardedRef, ...props } = this.props;
-
-//     return this.context.hasIonicRouter() ? (
-//       props.ionPage ? (
-//         <OutletPageManager StackManager={StackManager} routeInfo={this.context.routeInfo} {...props}>
-//           {children}
-//         </OutletPageManager>
-//       ) : (
-//         <StackManager routeInfo={this.context.routeInfo}>
-//           <IonRouterOutletInner {...props} forwardedRef={forwardedRef}>
-//             {children}
-//           </IonRouterOutletInner>
-//         </StackManager>
-//       )
-//     ) : (
-//       <IonRouterOutletInner ref={forwardedRef} {...this.props}>
-//         {this.props.children}
-//       </IonRouterOutletInner>
-//     );
-//   }
-
-//   static get contextType() {
-//     return NavContext;
-//   }
-// }
-
 export const IonRouterOutlet = createForwardRef<Props & IonicReactProps, HTMLIonRouterOutletElement>(
   IonRouterOutletContainer,
   'IonRouterOutlet'
