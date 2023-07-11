ad7c02c2d5e5e91e5cb957b213c0b166c34fc6b9
Sean Perkins <sean@ionic.io>
2023-07-11 13:01:40 -0400

    chore: remove cached ref to capacitor

--- core/src/utils/native/capacitor.ts
+++ core/src/utils/native/capacitor.ts
@@ -1,12 +1,7 @@
 import type { CapacitorGlobal } from '@capacitor/core';
 import { win } from '@utils/browser';
 
-let capacitor: CapacitorGlobal | undefined;
-
 export const getCapacitor = () => {
-  if (capacitor !== undefined) {
-    return capacitor;
-  }
   if (win !== undefined) {
     return (win as any).Capacitor as CapacitorGlobal;
   }
