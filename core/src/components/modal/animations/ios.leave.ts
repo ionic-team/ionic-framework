4e286347125dc553fe6e87a9eb9596528e07c377
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-20 11:21:53 -0400

    fix(modal): body background is reset with inline card modals

--- core/src/components/modal/animations/ios.leave.ts
+++ core/src/components/modal/animations/ios.leave.ts
@@ -51,9 +51,9 @@ export const iosLeaveAnimation = (baseEl: HTMLElement, opts: ModalAnimationOptio
 
         presentingEl.style.setProperty('overflow', '');
 
-        const numModals = Array.from(bodyEl.querySelectorAll('ion-modal')).filter(
-          (m) => m.presentingElement !== undefined
-        ).length;
+        const numModals = (
+          Array.from(bodyEl.querySelectorAll('ion-modal:not(.overlay-hidden)')) as HTMLIonModalElement[]
+        ).filter((m) => m.presentingElement !== undefined).length;
         if (numModals <= 1) {
           bodyEl.style.setProperty('background-color', '');
         }
