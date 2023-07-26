cbb3d1e5760b792004dc854e3a8d866df68dd681
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-26 14:20:40 -0400

    clean up

--- packages/angular/test/base/src/app/app.routes.ts
+++ packages/angular/test/base/src/app/app.routes.ts
@@ -4,4 +4,3 @@ export const routes: Routes = [
   { path: 'lazy', loadChildren: () => import('./app-lazy/app.module').then(m => m.AppModule) },
   { path: 'standalone', loadChildren: () => import('./app-standalone/app.routes').then(m => m.routes) }
 ];
-
