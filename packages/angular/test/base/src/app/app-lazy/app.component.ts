17f420dc3e6f6f769e9c8236e5bd36d04611c8ae
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-26 13:07:36 -0400

    refactor: create lazy subroute for lazy loaded components

--- packages/angular/test/base/src/app/app-lazy/app.component.ts
+++ packages/angular/test/base/src/app/app-lazy/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
