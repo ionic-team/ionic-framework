68b927c0ca7a6b159d17c3141e52b7ca409d4270
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-26 14:19:52 -0400

    clean up

--- packages/angular/test/base/src/app/app-standalone/app.component.ts
+++ packages/angular/test/base/src/app/app-standalone/app.component.ts
@@ -3,7 +3,6 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root-standalone',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   standalone: true,
   imports: [RouterModule]
 })
