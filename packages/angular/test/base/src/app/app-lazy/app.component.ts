9a352a003440cb0f8007d675c8ab41e7f2d2d1c9
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-26 14:20:22 -0400

    clean up

--- packages/angular/test/base/src/app/app-lazy/app.component.ts
+++ packages/angular/test/base/src/app/app-lazy/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 }
