eceb73e0dd826250e3e337cd63483660e1b9bd45
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-26 13:31:32 -0400

    remove bootstrap

--- packages/angular/test/base/src/app/app-lazy/app.component.ts
+++ packages/angular/test/base/src/app/app-lazy/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root-lazy',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
