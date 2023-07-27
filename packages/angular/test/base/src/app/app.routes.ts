85cac110653bbb6170e5f93143f6f6338a368266
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-27 13:40:40 -0400

    test: redirect to lazy route

--- packages/angular/test/base/src/app/app.routes.ts
+++ packages/angular/test/base/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/lazy'
+  },
   { path: 'lazy', loadChildren: () => import('./app-lazy/app.module').then(m => m.AppModule) },
   { path: 'standalone', loadChildren: () => import('./app-standalone/app.routes').then(m => m.routes) }
 ];
