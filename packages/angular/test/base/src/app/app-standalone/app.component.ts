6465128d25bbc3dbfed42acd9b80a731471b7db9
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-27 16:53:26 -0400

    initialize ionic temporarily

--- packages/angular/test/base/src/app/app-standalone/app.component.ts
+++ packages/angular/test/base/src/app/app-standalone/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+/**
+ * This temporary code initialized Ionic and ensures components are visible.
+ * TODO FW-4766 Can be removed when ticket is implemented
+ */
+import { initialize } from '@ionic/core/components';
+initialize();
+
+document.querySelector('html')!.classList.add('ion-ce')
+
+
 @Component({
   selector: 'app-root-standalone',
   templateUrl: './app.component.html',
