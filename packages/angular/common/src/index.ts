58ad4a8b5cbeb8dd31c0a9db15a6436f9986c633
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-28 10:49:28 -0400

    feat(angular): add base popover implementation

--- packages/angular/common/src/index.ts
+++ packages/angular/common/src/index.ts
@@ -1,3 +1,2 @@
-// This is required to get ng-packagr to build.
-// Remove this when you actually have something to export
-export const placeholder = true;
+export { IonPopover, POPOVER_INPUTS, POPOVER_METHODS } from './overlays/popover';
+export { ProxyCmp } from './utils';
