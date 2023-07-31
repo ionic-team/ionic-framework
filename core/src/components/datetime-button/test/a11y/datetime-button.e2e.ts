e2e7f972882a63bbfde4fb93e50993a340b02348
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-31 10:07:57 -0400

    fix(datetime-button): buttons wrap instead of truncate (#27872)

--- core/src/components/datetime-button/test/a11y/datetime-button.e2e.ts
+++ core/src/components/datetime-button/test/a11y/datetime-button.e2e.ts
@@ -25,7 +25,7 @@ configs({ directions: ['ltr'], modes: ['ios'] }).forEach(({ title, screenshot, c
       await expect(datetimeButton).toHaveScreenshot(screenshot(`datetime-button-scale`));
     });
 
-    test('should truncate text on larger font sizes', async ({ page }) => {
+    test('should wrap text on larger font sizes', async ({ page }) => {
       await page.setContent(
         `
         <style>
@@ -41,29 +41,7 @@ configs({ directions: ['ltr'], modes: ['ios'] }).forEach(({ title, screenshot, c
 
       const datetimeButton = page.locator('ion-datetime-button');
 
-      await expect(datetimeButton).toHaveScreenshot(screenshot(`datetime-button-scale-truncate`));
-    });
-
-    test('should truncate text on larger font sizes inside of an item', async ({ page }) => {
-      await page.setContent(
-        `
-        <style>
-          html {
-            font-size: 48px;
-          }
-        </style>
-        <ion-item>
-          <ion-label>Start Date</ion-label>
-          <ion-datetime-button locale="en-US" datetime="datetime"></ion-datetime-button>
-        </ion-item>
-        <ion-datetime id="datetime" value="2022-01-01T06:30:00" presentation="date-time"></ion-datetime>
-      `,
-        config
-      );
-
-      const datetimeButton = page.locator('ion-datetime-button');
-
-      await expect(datetimeButton).toHaveScreenshot(screenshot(`datetime-button-scale-truncate-item`));
+      await expect(datetimeButton).toHaveScreenshot(screenshot(`datetime-button-scale-wrap`));
     });
   });
 });
