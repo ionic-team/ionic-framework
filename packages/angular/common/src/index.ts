e8bdbdb95a1bf2c84d4bbf3e7867c10a3335f022
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-31 15:37:22 -0400

    chore: add exports from common

--- packages/angular/common/src/index.ts
+++ packages/angular/common/src/index.ts
@@ -1,3 +1,8 @@
-// This is required to get ng-packagr to build.
-// Remove this when you actually have something to export
-export const placeholder = true;
+export { ActionSheetController } from './providers/action-sheet-controller';
+export { AlertController } from './providers/alert-controller';
+export { LoadingController } from './providers/loading-controller';
+export { MenuController } from './providers/menu-controller';
+export { ModalController } from './providers/modal-controller';
+export { PickerController } from './providers/picker-controller';
+export { PopoverController } from './providers/popover-controller';
+export { ToastController } from './providers/toast-controller';
