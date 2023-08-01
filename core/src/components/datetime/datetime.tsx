cf4128fba02205ac210bb8cf040c65505892d875
amandaesmith3 <amanda.s@ionic.io>
2023-08-01 13:41:02 -0500

    try adding animate param back in but also animating when calling reset

--- core/src/components/datetime/datetime.tsx
+++ core/src/components/datetime/datetime.tsx
@@ -368,7 +368,7 @@ export class Datetime implements ComponentInterface {
     const { value } = this;
 
     if (this.hasValue()) {
-      await this.processValue(value);
+      await this.processValue(value, true);
     }
 
     this.emitStyle();
@@ -511,7 +511,7 @@ export class Datetime implements ComponentInterface {
    */
   @Method()
   async reset(startDate?: string) {
-    await this.processValue(startDate);
+    await this.processValue(startDate, true);
   }
 
   /**
@@ -1196,7 +1196,7 @@ export class Datetime implements ComponentInterface {
     });
   }
 
-  private processValue = async (value?: string | string[] | null) => {
+  private processValue = async (value?: string | string[] | null, animate = false) => {
     const hasValue = value !== null && value !== undefined && (!Array.isArray(value) || value.length > 0);
     const valueToProcess = hasValue ? parseDate(value) : this.defaultParts;
 
@@ -1273,7 +1273,7 @@ export class Datetime implements ComponentInterface {
     const didChangeMonth = month !== workingParts.month || year !== workingParts.year;
     const elIsVisible = el.offsetParent !== null;
     const { isGridStyle, showMonthAndYear } = this;
-    if (isGridStyle && didChangeMonth && elIsVisible && !showMonthAndYear) {
+    if (animate && isGridStyle && didChangeMonth && elIsVisible && !showMonthAndYear) {
       /**
        * Tell other render functions that we need to force the
        * target month to appear in place of the actual next/prev month.
