109c37216775405818dea541bcb02afc4562b237
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-01 17:04:41 -0400

    fix(angular): back button receives router outlet

--- packages/angular/src/directives/navigation/ion-back-button.ts
+++ packages/angular/src/directives/navigation/ion-back-button.ts
@@ -1,8 +1,17 @@
-import { Directive } from '@angular/core';
-import { IonBackButton as IonBackButtonBase } from '@ionic/angular/common';
+import { Directive, Optional } from '@angular/core';
+import { IonBackButton as IonBackButtonBase, NavController, Config } from '@ionic/angular/common';
+import { IonRouterOutlet } from './ion-router-outlet';
 
 @Directive({
   selector: 'ion-back-button',
 })
 // eslint-disable-next-line @angular-eslint/directive-class-suffix
-export class IonBackButtonDelegateDirective extends IonBackButtonBase {}
+export class IonBackButtonDelegateDirective extends IonBackButtonBase {
+  constructor(
+    @Optional() routerOutlet: IonRouterOutlet,
+    navCtrl: NavController,
+    config: Config
+  ) {
+    super(routerOutlet, navCtrl, config);
+  }
+}
