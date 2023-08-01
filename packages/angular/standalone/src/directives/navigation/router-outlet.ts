168101d34d4bf4e7fe9a48fcf446800db87982d9
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-01 15:26:12 -0400

    feat(angular): add standalone router outlet

---
+++ packages/angular/standalone/src/directives/navigation/router-outlet.ts
@@ -0,0 +1,9 @@
+import { Directive } from '@angular/core';
+import { IonRouterOutlet as IonRouterOutletBase } from '@ionic/angular/common';
+
+@Directive({
+  selector: 'ion-router-outlet',
+  standalone: true
+})
+// eslint-disable-next-line @angular-eslint/directive-class-suffix
+export class IonRouterOutlet extends IonRouterOutletBase {}
