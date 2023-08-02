705c31dc34e4b7f69118bcab02a4defb4c8f44ff
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-02 14:45:12 -0400

    test: fix flaky test

--- core/src/components/datetime/test/set-value/datetime.e2e.ts
+++ core/src/components/datetime/test/set-value/datetime.e2e.ts
@@ -27,20 +27,21 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
       const activeDate = page.locator('ion-datetime .time-body');
       await expect(activeDate).toHaveText('12:40 PM');
     });
-    test('should update active item when value is not initially set', async ({ page }) => {
+    test.only('should update active item when value is not initially set', async ({ page }) => {
       await page.setContent(
         `
         <ion-datetime presentation="date" locale="en-US"></ion-datetime>
       `,
         config
       );
+
       await page.waitForSelector('.datetime-ready');
 
       const datetime = page.locator('ion-datetime');
       const activeDayButton = page.locator('.calendar-day-active');
       const monthYearButton = page.locator('.calendar-month-year');
       const monthColumn = page.locator('.month-column');
-      const yearColumn = page.locator('.year-column');
+      const ionChange = await page.spyOnEvent('ionChange');
 
       await datetime.evaluate((el: HTMLIonDatetimeElement) => (el.value = '2021-10-05'));
 
@@ -49,11 +50,9 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
       await page.waitForChanges();
 
       // Select October 2021
+      // The year will automatically switch to 2021 when selecting 10
       await monthColumn.locator('.picker-item[data-value="10"]').click();
-      await page.waitForChanges();
-
-      await yearColumn.locator('.picker-item[data-value="2021"]').click();
-      await page.waitForChanges();
+      await ionChange.next();
 
       // Close month/year picker
       await monthYearButton.click();
