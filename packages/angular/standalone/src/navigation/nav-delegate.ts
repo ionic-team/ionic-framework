db3d782e4abb98f401f8d862d9b3a26f91159e7f
Maria Hutt <maria@ionic.io>
2023-08-03 08:23:06 -0700

    feat(angular): more lint

--- packages/angular/standalone/src/navigation/nav-delegate.ts
+++ packages/angular/standalone/src/navigation/nav-delegate.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NavDelegate as NavDelegateBase, ProxyCmp, NAV_DELEGATE_SELECTOR, NAV_DELEGATE_TEMPLATE } from '@ionic/angular/common';
+import {
+  NavDelegate as NavDelegateBase,
+  ProxyCmp,
+  NAV_DELEGATE_SELECTOR,
+  NAV_DELEGATE_TEMPLATE,
+} from '@ionic/angular/common';
 import { defineCustomElement } from '@ionic/core/components/ion-nav.js';
 
 @ProxyCmp({
