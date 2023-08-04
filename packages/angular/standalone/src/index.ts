c52a0972b952e987a7c9fe4d10c3122de53d9df0
Maria Hutt <thetaPC@users.noreply.github.com>
2023-08-04 16:20:39 -0700

    feat(angular): add standalone nav (#27876)

--- packages/angular/standalone/src/index.ts
+++ packages/angular/standalone/src/index.ts
@@ -2,3 +2,4 @@ export { IonBackButton } from './navigation/back-button';
 export { IonModal } from './overlays/modal';
 export { IonPopover } from './overlays/popover';
 export { IonRouterOutlet } from './navigation/router-outlet';
+export { IonNav } from './navigation/nav-delegate';
