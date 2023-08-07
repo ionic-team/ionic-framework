b6dcbde05364ef2c1ec287aed18d0a47fd18f732
Brandy Carney <brandy@ionic.io>
2023-08-07 12:31:01 -0400

    test(note): add test for font scaling with dynamic type

---
+++ core/src/components/note/test/a11y/note.e2e.ts
@@ -0,0 +1,65 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('note: font scaling'), () => {
+    test('should scale text on larger font sizes', async ({ page }) => {
+      await page.setContent(
+        `
+        <style>
+          html {
+            font-size: 310%;
+          }
+        </style>
+
+        <ion-note>Default</ion-note>
+      `,
+        config
+      );
+
+      const note = page.locator('ion-note');
+
+      await expect(note).toHaveScreenshot(screenshot(`note-scale`));
+    });
+    test('should scale text in an item on larger font sizes', async ({ page }) => {
+      await page.setContent(
+        `
+        <style>
+          html {
+            font-size: 310%;
+          }
+        </style>
+
+        <ion-item>
+          <ion-note slot="end">Default</ion-note>
+        </ion-item>
+      `,
+        config
+      );
+
+      const note = page.locator('ion-note');
+
+      await expect(note).toHaveScreenshot(screenshot(`note-item-scale`));
+    });
+    test('should scale text in an item divider on larger font sizes', async ({ page }) => {
+      await page.setContent(
+        `
+        <style>
+          html {
+            font-size: 310%;
+          }
+        </style>
+
+        <ion-item-divider>
+          <ion-note slot="end">Default</ion-note>
+        </ion-item-divider>
+      `,
+        config
+      );
+
+      const note = page.locator('ion-note');
+
+      await expect(note).toHaveScreenshot(screenshot(`note-item-divider-scale`));
+    });
+  });
+});
