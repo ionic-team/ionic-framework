ea74059bd3e894aa3a823e8d0f6839cc893a23db
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-09 09:50:17 -0500

    add TODO

--- core/src/components/toast/toast-interface.ts
+++ core/src/components/toast/toast-interface.ts
@@ -25,6 +25,8 @@ export interface ToastOptions {
 
 export type ToastLayout = 'baseline' | 'stacked';
 
+// TODO FW-4923 remove cssClass property
+
 export interface ToastButton {
   text?: string;
   icon?: string;
