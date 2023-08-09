e9fa30002bd5dec4f2f56a15c84eec1b3e794942
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-09 09:35:11 -0500

    fix(button): hidden button is added when form is set async (#27955)
    Issue number: resolves #27952
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    The hidden button in `ion-button` that is responsible for submitting the
    form does not get added when the `form` property is set async.
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - `ion-button` now checks to see if it needs to render a hidden button
    whenever it re-renders. This allows it to account for changes to the
    `type` property, `form` property, etc.
    
    Since this code can potentially run multiple times I added an extra
    check so we don't add multiple buttons to the form.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!-- If this introduces a breaking change, please describe the impact
    and migration path for existing applications below. -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->
    
    Dev build: `7.2.3-dev.11691523847.1760ab58`

--- core/src/components/button/test/form-reference/button.e2e.ts
+++ core/src/components/button/test/form-reference/button.e2e.ts
@@ -130,6 +130,29 @@ configs({ directions: ['ltr'], modes: ['ios'] }).forEach(({ title, config }) => 
 
       expect(submitEvent).not.toHaveReceivedEvent();
     });
+
+    test('should submit the form by id when form is set async', async ({ page }, testInfo) => {
+      testInfo.annotations.push({
+        type: 'issue',
+        description: 'https://github.com/ionic-team/ionic-framework/issues/27952',
+      });
+      await page.setContent(
+        `
+        <form id="myForm"></form>
+        <ion-button type="submit">Submit</ion-button>
+      `,
+        config
+      );
+
+      const submitEvent = await page.spyOnEvent('submit');
+      const button = page.locator('ion-button');
+
+      await button.evaluate((el: HTMLIonButtonElement) => (el.form = 'myForm'));
+
+      await page.click('ion-button');
+
+      expect(submitEvent).toHaveReceivedEvent();
+    });
   });
 
   test.describe(title('should throw a warning if the form cannot be found'), () => {
