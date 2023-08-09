0947ec87de69291dcc99022b87ba14ccdf5cc00d
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-09 10:07:16 -0500

    test github reporter

--- core/playwright.config.ts
+++ core/playwright.config.ts
@@ -69,7 +69,10 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['github']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
