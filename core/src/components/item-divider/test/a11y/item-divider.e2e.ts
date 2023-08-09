f0adf894742b8056e0ca7f94cae3c6614aae8be1
Brandy Carney <brandy@ionic.io>
2023-08-09 10:18:01 -0400

    fix(item-divider): remove unused styles and support dynamic type with headings

--- core/src/components/item-divider/test/a11y/item-divider.e2e.ts
+++ core/src/components/item-divider/test/a11y/item-divider.e2e.ts
@@ -23,5 +23,30 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
 
       await expect(itemDivider).toHaveScreenshot(screenshot(`item-divider-scale`));
     });
+    test('should scale text on larger font sizes when item divider contains headings and paragraphs', async ({ page }) => {
+      await page.setContent(
+        `
+        <style>
+          html {
+            font-size: 310%;
+          }
+        </style>
+
+        <ion-item-divider>
+          <ion-label>
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+            <p>Paragraph</p>
+          </ion-label>
+        </ion-item-divider>
+      `,
+        config
+      );
+
+      const itemDivider = page.locator('ion-item-divider');
+
+      await expect(itemDivider).toHaveScreenshot(screenshot(`item-divider-headings-scale`));
+    });
   });
 });
