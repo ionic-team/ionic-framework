123697cfcf5417027f88140f111ba1a69a724394
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-10 09:28:55 -0500

    test(title): add failing test

--- core/src/components/title/test/basic/title.e2e.ts
+++ core/src/components/title/test/basic/title.e2e.ts
@@ -12,3 +12,19 @@ configs().forEach(({ title, screenshot, config }) => {
     });
   });
 });
+
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('title: special characters'), () => {
+    test('should not cut off characters', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-title size="large">ÔÔÔ</ion-title>
+      `,
+        config
+      );
+
+      const title = page.locator('ion-title');
+      await expect(title).toHaveScreenshot(screenshot('title-characters'));
+    });
+  });
+});
