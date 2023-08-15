cedcc1374e109a717bd28ecf9688309efa1700f6
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-15 10:51:30 -0400

    whitespace

--- packages/angular/test/base/src/main-standalone.ts
+++ packages/angular/test/base/src/main-standalone.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { provideIonicAngular, IonicRouteStrategy } from '@ionic/angular/standalone';
+
 import { AppComponentStandalone } from './app/app-standalone.component';
 
 import { routes } from './app/app.routes';
-
 
 export const bootstrapStandalone = () => {
   bootstrapApplication(AppComponentStandalone, {
