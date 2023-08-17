98ef699a54ec92f4899603a01dc6f0394feaabfa
Sean Perkins <sean@ionic.io>
2023-08-17 16:13:01 -0400

    chore(react): update sync script to use tgz

--- packages/react/scripts/sync.sh
+++ packages/react/scripts/sync.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-# Copy core dist
-rm -rf node_modules/@ionic/core/dist node_modules/@ionic/core/components
-cp -a ../../core/dist node_modules/@ionic/core/dist
-cp -a ../../core/components node_modules/@ionic/core/components
-cp -a ../../core/package.json node_modules/@ionic/core/package.json
+# Delete old packages
+rm -f *.tgz
+
+# Pack @ionic/core
+npm pack ../../core
+
+# Install Dependencies
+npm install *.tgz --no-save
