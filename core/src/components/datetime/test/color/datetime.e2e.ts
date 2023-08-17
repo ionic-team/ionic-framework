6ca4cdadc8890dfa9050fa2a4a03c775dfb0c04a
Brandy Carney <brandy@ionic.io>
2023-08-17 14:01:08 -0400

    test(datetime): add test for customizing the css parts

--- core/src/components/datetime/test/color/datetime.e2e.ts
+++ core/src/components/datetime/test/color/datetime.e2e.ts
@@ -9,7 +9,7 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto('/src/components/datetime/test/color', config);
 
-      const datetime = page.locator('ion-datetime');
+      const datetime = page.locator('#colorDatetime');
 
       await expect(datetime).toHaveScreenshot(screenshot(`datetime-color`));
 
@@ -17,6 +17,13 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
       await page.waitForChanges();
 
       await expect(datetime).toHaveScreenshot(screenshot(`datetime-color-dark`));
+    });
+    test('should be able to customize datetime using css parts', async ({ page }) => {
+      await page.goto('/src/components/datetime/test/color', config);
+
+      const datetime = page.locator('#customDatetime');
+
+      await expect(datetime).toHaveScreenshot(screenshot(`datetime-custom-css-parts`));
     });
   });
 });
