f9f839384067feb87a4b5f216b3e1fb5e37ea682
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-24 15:47:52 -0400

    test(searchbar): add tests for text cut off on ios

--- core/src/components/searchbar/test/basic/searchbar.e2e.ts
+++ core/src/components/searchbar/test/basic/searchbar.e2e.ts
@@ -122,6 +122,30 @@ configs().forEach(({ title, screenshot, config }) => {
 
       await expect(searchbar).toHaveScreenshot(screenshot(`searchbar-buttons`));
     });
+
+    test('text should not be cut off when clear button is hidden', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-searchbar show-clear-button="focus" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non metus vel velit sollicitudin suscipit quis sed lectus. "></ion-searchbar>
+      `,
+        config
+      );
+
+      const searchbar = page.locator('ion-searchbar');
+      await expect(searchbar).toHaveScreenshot(screenshot(`searchbar-text-clear-hidden`));
+    });
+
+    test('text should be cut off when clear button is visible', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-searchbar show-clear-button="always" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non metus vel velit sollicitudin suscipit quis sed lectus. "></ion-searchbar>
+      `,
+        config
+      );
+
+      const searchbar = page.locator('ion-searchbar');
+      await expect(searchbar).toHaveScreenshot(screenshot(`searchbar-text-clear-visible`));
+    });
   });
 });
 
