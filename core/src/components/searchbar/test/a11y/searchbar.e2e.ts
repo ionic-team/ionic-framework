2a963cd4c51d735defdc9de34e49f22acd5dad65
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-29 12:19:30 -0400

    test(searchbar): add test

--- core/src/components/searchbar/test/a11y/searchbar.e2e.ts
+++ core/src/components/searchbar/test/a11y/searchbar.e2e.ts
@@ -20,5 +20,26 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
 
       await expect(searchbar).toHaveScreenshot(screenshot(`searchbar-scale`));
     });
+    test('should scale text on larger font sizes in a toolbar', async ({ page }) => {
+      await page.setContent(
+        `
+        <style>
+          html {
+            font-size: 36px;
+          }
+        </style>
+        <ion-header>
+          <ion-toolbar>
+            <ion-searchbar value="My Text" show-cancel-button="always" show-clear-button="always"></ion-searchbar>
+          </ion-toolbar>
+        </ion-header>
+      `,
+        config
+      );
+
+      const searchbar = page.locator('ion-searchbar');
+
+      await expect(searchbar).toHaveScreenshot(screenshot(`searchbar-scale-toolbar`));
+    });
   });
 });
