6ac655e9102c2bc12f243967fbc06547178fe431
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-29 12:05:45 -0400

    add test for fallback

--- core/src/components/popover/test/async/popover.e2e.ts
+++ core/src/components/popover/test/async/popover.e2e.ts
@@ -18,5 +18,37 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, c
 
       await expect(page).toHaveScreenshot(screenshot(`popover-async`));
     });
+    /**
+     * Framework delegate should fall back to returning the host
+     * component when no child content is passed otherwise
+     * the overlay will get stuck when trying to re-present.
+     */
+    test('should open popover even if nothing was passed', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-popover></ion-popover>
+      `,
+        config
+      );
+
+      const popover = page.locator('ion-popover');
+      const ionPopoverDidPresent = await page.spyOnEvent('ionPopoverDidPresent');
+      const ionPopoverDidDismiss = await page.spyOnEvent('ionPopoverDidDismiss');
+
+      await popover.evaluate((el: HTMLIonPopoverElement) => el.present());
+
+      await ionPopoverDidPresent.next();
+      await expect(popover).toBeVisible();
+
+      await popover.evaluate((el: HTMLIonPopoverElement) => el.dismiss());
+
+      await ionPopoverDidDismiss.next();
+      await expect(popover).toBeHidden();
+
+      await popover.evaluate((el: HTMLIonPopoverElement) => el.present());
+
+      await ionPopoverDidPresent.next();
+      await expect(popover).toBeVisible();
+    });
   });
 });
