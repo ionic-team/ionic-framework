e1a2471486670851f6ea46497898527956a20bb1
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-30 10:19:00 -0400

    test(datetime): un-flake presentation test

--- core/src/components/datetime/test/presentation/datetime.e2e.ts
+++ core/src/components/datetime/test/presentation/datetime.e2e.ts
@@ -1,33 +1,49 @@
 import type { Locator } from '@playwright/test';
 import { expect } from '@playwright/test';
-import type { E2EPage, E2EPageOptions } from '@utils/test/playwright';
+import type { E2EPage, E2EPageOptions, ScreenshotFn } from '@utils/test/playwright';
 import { configs, test } from '@utils/test/playwright';
 
 configs().forEach(({ title, screenshot, config }) => {
-  test(title('should not have visual regressions'), async ({ page }) => {
-    await page.goto(`/src/components/datetime/test/presentation`, config);
+  test.describe(title('datetime: presentation rendering'), () => {
+    let presentationFixture!: DatetimePresentationFixture;
+    test.beforeEach(({ page }) => {
+      presentationFixture = new DatetimePresentationFixture(page);
+    });
 
-    await page.waitForSelector('.datetime-ready');
+    test('should not have visual regressions with date-time presentation', async () => {
+      await presentationFixture.goto('date-time', config);
+      await presentationFixture.screenshot('datetime-presentation-date-time-diff', screenshot);
+    });
 
-    const datetime = page.locator('ion-datetime');
+    test('should not have visual regressions with time-date presentation', async () => {
+      await presentationFixture.goto('time-date', config);
+      await presentationFixture.screenshot('datetime-presentation-time-date-diff', screenshot);
+    });
 
-    const compares = [];
-    const presentations = ['date-time', 'time-date', 'time', 'date', 'month-year', 'month', 'year'];
+    test('should not have visual regressions with time presentation', async () => {
+      await presentationFixture.goto('time', config);
+      await presentationFixture.screenshot('datetime-presentation-time-diff', screenshot);
+    });
 
-    for (const presentation of presentations) {
-      await page.locator('select').selectOption(presentation);
-      await page.waitForChanges();
-      compares.push({
-        presentation,
-        screenshot: datetime,
-      });
-    }
+    test('should not have visual regressions with date presentation', async () => {
+      await presentationFixture.goto('date', config);
+      await presentationFixture.screenshot('datetime-presentation-date-diff', screenshot);
+    });
 
-    for (const compare of compares) {
-      await expect(compare.screenshot).toHaveScreenshot(
-        screenshot(`datetime-presentation-${compare.presentation}-diff`)
-      );
-    }
+    test('should not have visual regressions with month-year presentation', async () => {
+      await presentationFixture.goto('month-year', config);
+      await presentationFixture.screenshot('datetime-presentation-month-year-diff', screenshot);
+    });
+
+    test('should not have visual regressions with month presentation', async () => {
+      await presentationFixture.goto('month', config);
+      await presentationFixture.screenshot('datetime-presentation-month-diff', screenshot);
+    });
+
+    test('should not have visual regressions with year presentation', async () => {
+      await presentationFixture.goto('year', config);
+      await presentationFixture.screenshot('datetime-presentation-year-diff', screenshot);
+    });
   });
 });
 
@@ -156,6 +172,31 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
     });
   });
 });
+
+class DatetimePresentationFixture {
+  readonly page: E2EPage;
+
+  private datetime!: Locator;
+
+  constructor(page: E2EPage) {
+    this.page = page;
+  }
+
+  async goto(presentation: string, config: E2EPageOptions) {
+    await this.page.setContent(
+      `
+      <ion-datetime presentation="${presentation}" value="2022-03-10T13:00:00"></ion-datetime>
+    `,
+      config
+    );
+    await this.page.waitForSelector('.datetime-ready');
+    this.datetime = this.page.locator('ion-datetime');
+  }
+
+  async screenshot(name: string, screenshotFn: ScreenshotFn) {
+    await expect(this.datetime).toHaveScreenshot(screenshotFn(name));
+  }
+}
 
 class TimePickerFixture {
   readonly page: E2EPage;
