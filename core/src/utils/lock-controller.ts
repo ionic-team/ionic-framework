5604c57fbb6d501325e0f86e9706c3f597e9e250
Shawn Taylor <shawn@ionic.io>
2023-08-30 17:02:45 -0400

    Update core/src/utils/lock-controller.ts
    Co-authored-by: Liam DeBeasi <liamdebeasi@users.noreply.github.com>

--- core/src/utils/lock-controller.ts
+++ core/src/utils/lock-controller.ts
@@ -1,7 +1,7 @@
 /**
  * Creates a lock controller.
  *
- * Claiming a lock means that nothing else can aquire the lock until it is released.
+ * Claiming a lock means that nothing else can acquire the lock until it is released.
  * This can momentarily prevent execution of code that needs to wait for the earlier code to finish.
  * For example, this can be used to prevent multiple transitions from occurring at the same time.
  */
