e6c7bb60e7e61c965f45e2bf3e3bd16f5125ad56
Maria Hutt <thetaPC@users.noreply.github.com>
2023-09-01 09:30:59 -0700

    feat(checkbox, radio, toggle, range): stacked labels for form controls (#28075)
    Co-authored-by: Liam DeBeasi <liamdebeasi@users.noreply.github.com>
    Co-authored-by: ionitron <hi@ionicframework.com>

--- core/src/components/toggle/test/item/toggle.e2e.ts
+++ core/src/components/toggle/test/item/toggle.e2e.ts
@@ -70,4 +70,23 @@ configs({ directions: ['ltr'], modes: ['md'] }).forEach(({ title, screenshot, co
       await expect(list).toHaveScreenshot(screenshot(`toggle-long-label-in-item`));
     });
   });
+
+  test.describe(title('toggle: stacked label in item'), () => {
+    test('should render margins correctly when using stacked label in item', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-list>
+            <ion-radio-group>
+              <ion-item>
+                <ion-toggle label-placement="stacked">Enable Notifications</ion-toggle>
+              </ion-item>
+            </ion-radio-group>
+          </ion-list>
+        `,
+        config
+      );
+      const list = page.locator('ion-list');
+      await expect(list).toHaveScreenshot(screenshot(`toggle-stacked-label-in-item`));
+    });
+  });
 });
