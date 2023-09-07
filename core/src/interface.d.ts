6e7dc924913bec4b32a0e3893ac00ba96d02c74e
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-09-07 11:38:59 -0400

    Update interface.d.ts

--- core/src/interface.d.ts
+++ core/src/interface.d.ts
@@ -43,7 +43,7 @@ export {
   AnimationKeyFrames,
   AnimationLifecycle,
 } from './utils/animation/animation-interface';
-export { getIonPageElement, TransitionOptions } from './utils/transition'
+export { getIonPageElement, TransitionOptions } from './utils/transition';
 export { HTMLIonOverlayElement, OverlayController, OverlayInterface } from './utils/overlays-interface';
 export { Config, config } from './global/config';
 export { Gesture, GestureConfig, GestureDetail } from './utils/gesture';
