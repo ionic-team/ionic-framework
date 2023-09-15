0787e3aa411ef7bbce2f8d31f23da2d119a16eb7
Maria Hutt <maria@ionic.io>
2023-09-15 09:43:09 -0700

    test(menu): requested changes

--- core/src/components/menu/test/safe-area/menu.e2e.ts
+++ core/src/components/menu/test/safe-area/menu.e2e.ts
@@ -5,112 +5,96 @@ import { configs, test } from '@utils/test/playwright';
  * This behavior does not vary across modes.
  */
 configs({ modes: ['md'] }).forEach(({ title, config, screenshot }) => {
-  test.describe(title('menu: safe area'), () => {
+  test.describe.only(title('menu: safe area'), () => {
     test.beforeEach(async ({ page }) => {
       await page.goto(`/src/components/menu/test/safe-area`, config);
     });
     test.describe('side: start', () => {
       test('should render with safe area when notch is on the left', async ({ page }) => {
         const ionDidOpen = await page.spyOnEvent('ionDidOpen');
-        const ionDidClose = await page.spyOnEvent('ionDidClose');
 
         await page.evaluate(() => {
-          const style = document.createElement('style');
-          style.innerHTML = `
+          const style = document.querySelector('style');
+          style!.innerHTML = `
             :root {
-              --ion-safe-area-left: 50px;
-              --ion-safe-area-right: 10px;
+              --ion-safe-area-left: 50px !important;
+              --ion-safe-area-right: 10px !important;
             }
           `;
-          document.head.appendChild(style);
         });
 
         await page.click('#open-start');
         await ionDidOpen.next();
 
-        await expect(page).toHaveScreenshot(screenshot(`menu-start-safe-area-left-notch`));
+        const startMenu = page.locator('[menu-id="start-menu"]');
+        await expect(startMenu).toHaveClass(/show-menu/);
 
-        await page.locator('[menu-id="start-menu"]').evaluate(async (el: HTMLIonMenuElement) => {
-          await el.close();
-        });
-        await ionDidClose.next();
+        await expect(page).toHaveScreenshot(screenshot(`menu-start-safe-area-left-notch`));
       });
       test('should render with safe area when notch is on the right', async ({ page }) => {
         const ionDidOpen = await page.spyOnEvent('ionDidOpen');
-        const ionDidClose = await page.spyOnEvent('ionDidClose');
 
         await page.evaluate(() => {
-          const style = document.createElement('style');
-          style.innerHTML = `
+          const style = document.querySelector('style');
+          style!.innerHTML = `
             :root {
-              --ion-safe-area-left: 10px;
-              --ion-safe-area-right: 50px;
+              --ion-safe-area-left: 10px !important;
+              --ion-safe-area-right: 50px !important;
             }
           `;
-          document.head.appendChild(style);
         });
 
         await page.click('#open-start');
         await ionDidOpen.next();
 
-        await expect(page).toHaveScreenshot(screenshot(`menu-start-safe-area-right-notch`));
+        const startMenu = page.locator('[menu-id="start-menu"]');
+        await expect(startMenu).toHaveClass(/show-menu/);
 
-        await page.locator('[menu-id="start-menu"]').evaluate(async (el: HTMLIonMenuElement) => {
-          await el.close();
-        });
-        await ionDidClose.next();
+        await expect(page).toHaveScreenshot(screenshot(`menu-start-safe-area-right-notch`));
       });
     });
     test.describe('side: end', () => {
       test('should render with safe area when notch is on the left', async ({ page }) => {
         const ionDidOpen = await page.spyOnEvent('ionDidOpen');
-        const ionDidClose = await page.spyOnEvent('ionDidClose');
 
         await page.evaluate(() => {
-          const style = document.createElement('style');
-          style.innerHTML = `
+          const style = document.querySelector('style');
+          style!.innerHTML = `
             :root {
-              --ion-safe-area-left: 50px;
-              --ion-safe-area-right: 10px;
+              --ion-safe-area-left: 50px !important;
+              --ion-safe-area-right: 10px !important;
             }
           `;
-          document.head.appendChild(style);
         });
 
         await page.click('#open-end');
         await ionDidOpen.next();
 
-        await expect(page).toHaveScreenshot(screenshot(`menu-end-safe-area-left-notch`));
+        const endMenu = page.locator('[menu-id="end-menu"]');
+        await expect(endMenu).toHaveClass(/show-menu/);
 
-        await page.locator('[menu-id="end-menu"]').evaluate(async (el: HTMLIonMenuElement) => {
-          await el.close();
-        });
-        await ionDidClose.next();
+        await expect(page).toHaveScreenshot(screenshot(`menu-end-safe-area-left-notch`));
       });
       test('should render with safe area when notch is on the right', async ({ page }) => {
         const ionDidOpen = await page.spyOnEvent('ionDidOpen');
-        const ionDidClose = await page.spyOnEvent('ionDidClose');
 
         await page.evaluate(() => {
-          const style = document.createElement('style');
-          style.innerHTML = `
+          const style = document.querySelector('style');
+          style!.innerHTML = `
             :root {
-              --ion-safe-area-left: 10px;
-              --ion-safe-area-right: 50px;
+              --ion-safe-area-left: 10px !important;
+              --ion-safe-area-right: 50px !important;
             }
           `;
-          document.head.appendChild(style);
         });
 
         await page.click('#open-end');
         await ionDidOpen.next();
 
-        await expect(page).toHaveScreenshot(screenshot(`menu-end-safe-area-right-notch`));
+        const endMenu = page.locator('[menu-id="end-menu"]');
+        await expect(endMenu).toHaveClass(/show-menu/);
 
-        await page.locator('[menu-id="end-menu"]').evaluate(async (el: HTMLIonMenuElement) => {
-          await el.close();
-        });
-        await ionDidClose.next();
+        await expect(page).toHaveScreenshot(screenshot(`menu-end-safe-area-right-notch`));
       });
     });
   });
