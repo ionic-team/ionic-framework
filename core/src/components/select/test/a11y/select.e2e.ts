bd3918305ffa3c91149c3cadae269c5029e9b97a
Maria Hutt <maria@ionic.io>
2023-09-26 15:01:36 -0700

    test(select): larger font

--- core/src/components/select/test/a11y/select.e2e.ts
+++ core/src/components/select/test/a11y/select.e2e.ts
@@ -2,13 +2,36 @@ import AxeBuilder from '@axe-core/playwright';
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config, screenshot }) => {
   test.describe(title('select: a11y'), () => {
     test('should not have accessibility violations', async ({ page }) => {
       await page.goto(`/src/components/select/test/a11y`, config);
 
       const results = await new AxeBuilder({ page }).analyze();
       expect(results.violations).toEqual([]);
+    });
+
+    test.describe(title('select: font scaling'), () => {
+      test('should scale text on larger font sizes', async ({ page }) => {
+        await page.setContent(
+          `
+            <style>
+              html {
+                font-size: 310%;
+              }
+            </style>
+            <ion-select label="My Label" value="oranges" placeholder="Placeholder">
+              <ion-select-option value="apples">Apples</ion-select-option>
+              <ion-select-option value="bananas">Bananas</ion-select-option>
+              <ion-select-option value="oranges">Oranges</ion-select-option>
+            </ion-select>
+          `,
+          config
+        );
+
+        const select = page.locator('ion-select');
+        await expect(select).toHaveScreenshot(screenshot('select-scale'));
+      });
     });
   });
 });
