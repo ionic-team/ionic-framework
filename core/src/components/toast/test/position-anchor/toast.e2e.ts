42a343f791461f8a7c1f901e981795393b14864f
amandaesmith3 <amanda.s@ionic.io>
2023-09-28 10:54:13 -0500

    move beforeEach inside describe

--- core/src/components/toast/test/position-anchor/toast.e2e.ts
+++ core/src/components/toast/test/position-anchor/toast.e2e.ts
@@ -2,21 +2,21 @@ import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
 configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/src/components/toast/test/position-anchor', config);
-
-    /**
-     * We need to screenshot the whole page to ensure the toasts are positioned
-     * correctly, but we don't need much extra white space between the header
-     * and footer.
-     */
-    await page.setViewportSize({
-      width: 425,
-      height: 425,
-    });
-  });
-
   test.describe(title('toast: positionAnchor'), () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/src/components/toast/test/position-anchor', config);
+
+      /**
+       * We need to screenshot the whole page to ensure the toasts are positioned
+       * correctly, but we don't need much extra white space between the header
+       * and footer.
+       */
+      await page.setViewportSize({
+        width: 425,
+        height: 425,
+      });
+    });
+    
     test('should place top-position toast underneath anchor', async ({ page }) => {
       const ionToastDidPresent = await page.spyOnEvent('ionToastDidPresent');
 
