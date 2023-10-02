9bfa69c8eca5d410c7ebcde5c43b29ced4fb6b76
amandaesmith3 <amanda.s@ionic.io>
2023-10-02 14:48:39 -0500

    pull animation position logic into separate util

--- core/src/components/toast/animations/md.enter.ts
+++ core/src/components/toast/animations/md.enter.ts
@@ -1,11 +1,10 @@
 import { createAnimation } from '@utils/animation/animation';
-import { win } from '@utils/browser';
 import { getElementRoot } from '@utils/helpers';
 
 import type { Animation } from '../../../interface';
 import type { ToastPresentOptions } from '../toast-interface';
 
-import { warnIfAnchorIsHidden } from './utils';
+import { getAnimationPosition } from './utils';
 
 /**
  * MD Toast Enter Animation
@@ -18,46 +17,7 @@ export const mdEnterAnimation = (baseEl: HTMLElement, opts: ToastPresentOptions)
   const root = getElementRoot(baseEl);
   const wrapperEl = root.querySelector('.toast-wrapper') as HTMLElement;
 
-  /**
-   * Start with a predefined offset from the edge the toast will be
-   * positioned relative to. By default, this will be the top edge
-   * of the screen for position="top", or the bottom edge for
-   * position="bottom".
-   */
-  let offset = 8;
-  let top, bottom;
-
-  /**
-   * If positionAnchor is defined, add in the distance from the target
-   * screen edge to the target anchor edge. For position="top", the
-   * bottom anchor edge is targeted. For position="bottom", the top
-   * anchor edge is targeted.
-   */
-  if (positionAnchor && win) {
-    warnIfAnchorIsHidden(positionAnchor, baseEl);
-
-    const box = positionAnchor.getBoundingClientRect();
-    if (position === 'top') {
-      offset += box.bottom;
-    } else if (position === 'bottom') {
-      /**
-       * Just box.top is the distance from the top edge of the screen
-       * to the top edge of the anchor. We want to calculate from the
-       * bottom edge of the screen instead.
-       */
-      offset += win.innerHeight - box.top;
-    }
-
-    /**
-     * We don't include safe area here because that should already be
-     * accounted for when checking the position of the anchor.
-     */
-    top = `${offset}px`;
-    bottom = `${offset}px`;
-  } else {
-    top = `calc(${offset}px + var(--ion-safe-area-top, 0px))`;
-    bottom = `calc(${offset}px + var(--ion-safe-area-bottom, 0px))`;
-  }
+  const { top, bottom } = getAnimationPosition(position, positionAnchor, 'md', baseEl);
 
   wrapperAnimation.addElement(wrapperEl);
 
--- core/src/components/toast/animations/utils.ts
+++ core/src/components/toast/animations/utils.ts
@@ -1,6 +1,75 @@
+import { win } from '@utils/browser';
 import { printIonWarning } from '@utils/logging';
+import type { Mode } from 'src/interface';
 
-export function warnIfAnchorIsHidden(positionAnchor: HTMLElement, toast: HTMLElement) {
+import type { ToastPosition } from '../toast-interface';
+
+/**
+ * Calculate the CSS top and bottom position of the toast, to be used
+ * as starting points for the animation keyframes.
+ * 
+ * Note that MD animates bottom-positioned toasts using style.bottom,
+ * which calculates from the bottom edge of the screen, while iOS uses
+ * translateY, which calculates from the top edge of the screen. This
+ * is why the bottom calculates differ slightly between modes.
+ */
+export function getAnimationPosition(position: ToastPosition, positionAnchor: HTMLElement | undefined, mode: Mode, toast: HTMLElement): {
+  top: string,
+  bottom: string
+} {
+  /**
+   * Start with a predefined offset from the edge the toast will be
+   * positioned relative to, whether on the screen or anchor element.
+   */
+  let offset: number;
+  if (mode === 'md') {
+    offset = 8;
+  } else {
+    offset = position === 'top' ? 10 : -10;
+  }
+
+  /**
+   * If positionAnchor is defined, add in the distance from the target
+   * screen edge to the target anchor edge. For position="top", the
+   * bottom anchor edge is targeted. For position="bottom", the top
+   * anchor edge is targeted.
+   */
+  if (positionAnchor && win) {
+    warnIfAnchorIsHidden(positionAnchor, toast);
+
+    const box = positionAnchor.getBoundingClientRect();
+    if (position === 'top') {
+      offset += box.bottom;
+    } else if (position === 'bottom') {
+      /**
+       * Just box.top is the distance from the top edge of the screen
+       * to the top edge of the anchor. We want to calculate from the
+       * bottom edge of the screen instead.
+       */
+      if (mode === 'md') {
+        offset += win.innerHeight - box.top;
+      } else {
+        offset -= win.innerHeight - box.top;
+      }
+    }
+
+    /**
+     * We don't include safe area here because that should already be
+     * accounted for when checking the position of the anchor.
+     */
+    return {
+      top: `${offset}px`,
+      bottom: `${offset}px`
+    };
+  } else {
+    return {
+      top: `calc(${offset}px + var(--ion-safe-area-top, 0px))`,
+      bottom: mode === 'md' ? `calc(${offset}px + var(--ion-safe-area-bottom, 0px))` : `calc(${offset}px - var(--ion-safe-area-bottom, 0px))`
+    };
+  }
+}
+
+function warnIfAnchorIsHidden(positionAnchor: HTMLElement, toast: HTMLElement) {
   if (positionAnchor.offsetParent === null) {
     printIonWarning(
       'The positionAnchor element for ion-toast was found in the DOM, but appears to be hidden. This may lead to unexpected positioning of the toast.',
