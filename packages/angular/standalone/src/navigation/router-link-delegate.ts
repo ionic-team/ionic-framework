a6e4fc4feb5259f4444bc02d8c0dac096b29a1a0
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-10-04 14:30:25 -0400

    fix(angular): routerLink directives can be used (#28282)

--- packages/angular/standalone/src/navigation/router-link-delegate.ts
+++ packages/angular/standalone/src/navigation/router-link-delegate.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
+import { Directive } from '@angular/core';
 import {
   RouterLinkDelegateDirective as RouterLinkDelegateBase,
   RouterLinkWithHrefDelegateDirective as RouterLinkHrefDelegateBase,
 } from '@ionic/angular/common';
 
-@Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
+@Directive({
   selector: ':not(a):not(area)[routerLink]',
-  template: '<ng-content></ng-content>',
   standalone: true,
 })
+// eslint-disable-next-line @angular-eslint/directive-class-suffix
 export class IonRouterLink extends RouterLinkDelegateBase {}
 
-@Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
+@Directive({
   selector: 'a[routerLink],area[routerLink]',
-  template: '<ng-content></ng-content>',
   standalone: true,
 })
+// eslint-disable-next-line @angular-eslint/directive-class-suffix
 export class IonRouterLinkWithHref extends RouterLinkHrefDelegateBase {}
