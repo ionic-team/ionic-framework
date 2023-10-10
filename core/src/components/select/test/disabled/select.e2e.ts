ea8db8affaa1b8ebae1fe040944569ada87e5a41
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-10-10 14:46:20 -0400

    Update core/src/components/select/test/disabled/select.e2e.ts
    Co-authored-by: Amanda Johnston <90629384+amandaejohnston@users.noreply.github.com>

--- core/src/components/select/test/disabled/select.e2e.ts
+++ core/src/components/select/test/disabled/select.e2e.ts
@@ -3,7 +3,7 @@ import { configs, test } from '@utils/test/playwright';
 
 configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
   test.describe(title('select: disabled options'), () => {
-    test('should not open focus a disabled option when no value is set', async ({ page, skip }) => {
+    test('should not focus a disabled option when no value is set', async ({ page, skip }) => {
       // TODO (FW-2979)
       skip.browser('webkit', 'Safari 16 only allows text fields and pop-up menus to be focused.');
 
