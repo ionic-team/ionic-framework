2e59f5bea081c83461f1ea3aaf0b7c5d7bb392a9
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-10-12 12:02:18 -0400

    Update menu-controller.spec.ts

--- packages/angular/test/base/e2e/src/standalone/menu-controller.spec.ts
+++ packages/angular/test/base/e2e/src/standalone/menu-controller.spec.ts
@@ -3,6 +3,7 @@ describe('Menu Controller', () => {
     cy.visit('/standalone/menu-controller');
   })
 
+  // https://github.com/ionic-team/ionic-framework/issues/28337
   it('should register menus correctly', () => {
     cy.get('#set-menu-count').click();
     cy.get('#registered-menu-count').should('have.text', '1');
