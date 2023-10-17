d4377c21499c9e544a931cd057159081d9096bbd
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-10-17 14:49:35 -0400

    add more menus

--- core/src/components/split-pane/test/multiple/split-pane.e2e.ts
+++ core/src/components/split-pane/test/multiple/split-pane.e2e.ts
@@ -12,21 +12,27 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
       await page.setViewportSize(Viewports.large);
       await page.goto(`/src/components/split-pane/test/multiple`, config);
 
-      const menuOne = page.locator('ion-menu#menu-one');
-      const menuTwo = page.locator('ion-menu#menu-two');
+      const paneOneMenuOne = page.locator('ion-menu#pane-one-menu-one');
+      const paneOneMenuTwo = page.locator('ion-menu#pane-one-menu-two');
+
+      const paneTwoMenuOne = page.locator('ion-menu#pane-two-menu-one');
+      const paneTwoMenuTwo = page.locator('ion-menu#pane-two-menu-two');
 
       const showPaneOne = page.locator('button#show-pane-one');
       const showPaneTwo = page.locator('button#show-pane-two');
 
-      await expect(menuOne).toBeVisible();
+      await expect(paneOneMenuOne).toBeVisible();
+      await expect(paneOneMenuTwo).toBeVisible();
 
       await showPaneTwo.click();
 
-      await expect(menuTwo).toBeVisible();
+      await expect(paneTwoMenuOne).toBeVisible();
+      await expect(paneTwoMenuTwo).toBeVisible();
 
       await showPaneOne.click();
 
-      await expect(menuOne).toBeVisible();
+      await expect(paneOneMenuOne).toBeVisible();
+      await expect(paneOneMenuTwo).toBeVisible();
     });
   });
 });
