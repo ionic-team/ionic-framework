2de1153cb03828664f6d9e72ae959849470a4e2c
Brandy Carney <brandy@ionic.io>
2023-10-24 17:47:26 -0400

    test(tab-bar): combine safe area tests

--- core/src/components/tab-bar/test/basic/tab-bar.e2e.ts
+++ core/src/components/tab-bar/test/basic/tab-bar.e2e.ts
@@ -37,13 +37,14 @@ configs().forEach(({ title, screenshot, config }) => {
  */
 configs().forEach(({ title, screenshot, config }) => {
   test.describe(title('tab-bar: basic'), () => {
-    test.describe('safe area left', () => {
-      test('should have padding on the left only', async ({ page }) => {
+    test.describe('safe area', () => {
+      test('should have padding added by the safe area', async ({ page }) => {
         await page.setContent(
           `
           <style>
             :root {
               --ion-safe-area-left: 40px;
+              --ion-safe-area-right: 20px;
             }
           </style>
 
@@ -67,41 +68,7 @@ configs().forEach(({ title, screenshot, config }) => {
 
         const tabBar = page.locator('ion-tab-bar');
 
-        await expect(tabBar).toHaveScreenshot(screenshot(`tab-bar-safe-area-left`));
-      });
-    });
-
-    test.describe('safe area right', () => {
-      test('should have padding on the right only', async ({ page }) => {
-        await page.setContent(
-          `
-          <style>
-            :root {
-              --ion-safe-area-right: 40px;
-            }
-          </style>
-
-          <ion-tab-bar selected-tab="1">
-            <ion-tab-button tab="1">
-              <ion-label>Recents</ion-label>
-            </ion-tab-button>
-
-            <ion-tab-button tab="2">
-              <ion-label>Favorites</ion-label>
-              <ion-badge>23</ion-badge>
-            </ion-tab-button>
-
-            <ion-tab-button tab="3">
-              <ion-label>Settings</ion-label>
-            </ion-tab-button>
-          </ion-tab-bar>
-        `,
-          config
-        );
-
-        const tabBar = page.locator('ion-tab-bar');
-
-        await expect(tabBar).toHaveScreenshot(screenshot(`tab-bar-safe-area-right`));
+        await expect(tabBar).toHaveScreenshot(screenshot(`tab-bar-safe-area`));
       });
     });
   });
