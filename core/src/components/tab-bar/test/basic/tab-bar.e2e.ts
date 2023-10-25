fd2fdc68e1120b9aadc3b33ab7e5ee8051b105e9
Brandy Carney <brandy@ionic.io>
2023-10-25 10:56:18 -0400

    test(tab-bar): remove basic test as coverage exists already

--- core/src/components/tab-bar/test/basic/tab-bar.e2e.ts
+++ core/src/components/tab-bar/test/basic/tab-bar.e2e.ts
@@ -1,35 +1,6 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs().forEach(({ title, screenshot, config }) => {
-  test.describe(title('tab-bar: basic'), () => {
-    test('should not have visual regressions', async ({ page }) => {
-      await page.setContent(
-        `
-        <ion-tab-bar selected-tab="1">
-          <ion-tab-button tab="1">
-            <ion-label>Recents</ion-label>
-          </ion-tab-button>
-
-          <ion-tab-button tab="2">
-            <ion-label>Favorites</ion-label>
-            <ion-badge>23</ion-badge>
-          </ion-tab-button>
-
-          <ion-tab-button tab="3">
-            <ion-label>Settings</ion-label>
-          </ion-tab-button>
-        </ion-tab-bar>
-      `,
-        config
-      );
-
-      const tabBar = page.locator('ion-tab-bar');
-      await expect(tabBar).toHaveScreenshot(screenshot(`tab-bar-diff`));
-    });
-  });
-});
-
 /**
  * This behavior needs to be tested in both modes and directions to
  * make sure the safe area padding is applied only to that side
