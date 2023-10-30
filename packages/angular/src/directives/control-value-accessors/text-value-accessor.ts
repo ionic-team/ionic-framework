a8df389f37968312cdc4fdd2815600a88f9fb703
amandaesmith3 <amanda.s@ionic.io>
2023-10-30 09:46:35 -0500

    add tech debt ticket

--- packages/angular/src/directives/control-value-accessors/text-value-accessor.ts
+++ packages/angular/src/directives/control-value-accessors/text-value-accessor.ts
@@ -2,6 +2,7 @@ import { ElementRef, Injector, Directive, HostListener } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ValueAccessor } from '@ionic/angular/common';
 
+// TODO(FW-5495): rename class since range isn't a text component
 @Directive({
   selector: 'ion-input:not([type=number]),ion-textarea,ion-searchbar,ion-range',
   providers: [
