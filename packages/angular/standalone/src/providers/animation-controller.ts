859afda371b6783ad74fa61d5ec99b9706fde7b3
Sean Perkins <sean@ionic.io>
2023-11-06 13:00:58 -0500

    chore:  lint

--- packages/angular/standalone/src/providers/animation-controller.ts
+++ packages/angular/standalone/src/providers/animation-controller.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { AnimationController as AnimationControllerBase } from '@ionic/angular/common';
 import { createAnimation, getTimeGivenProgression } from '@ionic/core/components';
 
