63b5de8207b853a6c73b7a4b693d183dd8343ff1
Shawn Taylor <shawn@ionic.io>
2023-11-10 13:45:26 -0500

    chore: adds angular test app for v17

---
+++ packages/angular/test/apps/ng17/src/app/lazy/version-test/modal-nav-params/nav-root.component.ts
@@ -0,0 +1,38 @@
+import { JsonPipe } from "@angular/common";
+import { Component } from "@angular/core";
+
+import { IonicModule } from "@ionic/angular";
+
+/**
+ * This is used to track if any occurences of
+ * the ion-nav root component being attached to
+ * the DOM result in the rootParams not being
+ * assigned to the component instance.
+ *
+ * https://github.com/ionic-team/ionic-framework/issues/27146
+ */
+let rootParamsException = false;
+
+@Component({
+  selector: 'app-modal-content',
+  template: `
+    {{ hasException ? 'ERROR' : 'OK' }}
+  `,
+  standalone: true,
+  imports: [IonicModule, JsonPipe]
+})
+export class NavRootComponent {
+
+  params: any;
+
+  ngOnInit() {
+    if (this.params === undefined) {
+      rootParamsException = true;
+    }
+  }
+
+  get hasException() {
+    return rootParamsException;
+  }
+
+}
