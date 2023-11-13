985a9697e89baa0ac291cf70a0876cc1df82c9c4
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-11-13 12:50:57 -0500

    fix: fix some controllers

--- packages/angular/standalone/src/providers/toast-controller.ts
+++ packages/angular/standalone/src/providers/toast-controller.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { OverlayBaseController } from '@ionic/angular/common';
 import type { ToastOptions } from '@ionic/core/components';
 import { toastController } from '@ionic/core/components';
+import { defineCustomElement } from '@ionic/core/components/ion-toast.js';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,6 @@ import { toastController } from '@ionic/core/components';
 export class ToastController extends OverlayBaseController<ToastOptions, HTMLIonToastElement> {
   constructor() {
     super(toastController);
+    defineCustomElement();
   }
 }
