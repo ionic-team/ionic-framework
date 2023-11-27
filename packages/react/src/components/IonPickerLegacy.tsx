343581523ea39e23a8cb0d7c0a8e64c35b1796a8
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-11-27 12:57:04 -0500

    refactor(react): fix build error

---
+++ packages/react/src/components/IonPickerLegacy.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from '@ionic/core/components';
+import { defineCustomElement } from '@ionic/core/components/ion-picker-legacy.js';
+
+import { createInlineOverlayComponent } from './createInlineOverlayComponent';
+
+export const IonPickerLegacy = /*@__PURE__*/ createInlineOverlayComponent<JSX.IonPickerLegacy, HTMLIonPickerLegacyElement>(
+  'ion-picker',
+  defineCustomElement
+);
