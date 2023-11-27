b54009bac9223bdb6ad916ddfdfd6f6021912bee
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-11-27 12:57:09 -0500

    chore: add missing file

--- packages/react/src/components/IonPicker.tsx
+++
@@ -1,9 +0,0 @@
-import type { JSX } from '@ionic/core/components';
-import { defineCustomElement } from '@ionic/core/components/ion-picker-legacy.js';
-
-import { createInlineOverlayComponent } from './createInlineOverlayComponent';
-
-export const IonPickerLegacy = /*@__PURE__*/ createInlineOverlayComponent<JSX.IonPickerLegacy, HTMLIonPickerLegacyElement>(
-  'ion-picker',
-  defineCustomElement
-);
