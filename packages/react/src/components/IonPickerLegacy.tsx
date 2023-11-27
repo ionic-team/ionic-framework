0894e02b51a52cc1c3ed632626a98245a3283b59
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-11-27 13:42:37 -0500

    chore(react): lint

--- packages/react/src/components/IonPickerLegacy.tsx
+++ packages/react/src/components/IonPickerLegacy.tsx
@@ -3,7 +3,7 @@ import { defineCustomElement } from '@ionic/core/components/ion-picker-legacy.js
 
 import { createInlineOverlayComponent } from './createInlineOverlayComponent';
 
-export const IonPickerLegacy = /*@__PURE__*/ createInlineOverlayComponent<JSX.IonPickerLegacy, HTMLIonPickerLegacyElement>(
-  'ion-picker',
-  defineCustomElement
-);
+export const IonPickerLegacy = /*@__PURE__*/ createInlineOverlayComponent<
+  JSX.IonPickerLegacy,
+  HTMLIonPickerLegacyElement
+>('ion-picker', defineCustomElement);
