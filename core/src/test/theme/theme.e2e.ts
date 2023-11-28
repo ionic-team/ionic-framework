6d38fadbcc5ca348433e1a17ba6170ba229f6000
Sean Perkins <sean@ionic.io>
2023-11-28 14:28:35 -0500

    chore: split light and dark theme testing

--- core/src/test/theme/theme.e2e.ts
+++ core/src/test/theme/theme.e2e.ts
@@ -2,14 +2,17 @@ import AxeBuilder from '@axe-core/playwright';
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-const colors = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger', 'light', 'medium', 'dark'];
-
-configs({ modes: ['md'], directions: ['ltr'], themes: ['light', 'dark'] }).forEach(({ config, title }) => {
+/**
+ * Ionic `light` color should not be tested against a white background (light mode).
+ */
+configs({ modes: ['md', 'ios'], directions: ['ltr'], themes: ['light'] }).forEach(({ config, title }) => {
   test.describe(title('theme'), () => {
-    test.beforeAll(async ({ skip }) => {
+    test.beforeAll(({ skip }) => {
       skip.browser('firefox');
       skip.browser('webkit');
     });
+
+    const colors = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger', 'medium', 'dark'];
 
     for (const color of colors) {
       test(`color "${color}" should pass AA guidelines`, async ({ page }) => {
@@ -22,6 +25,36 @@ configs({ modes: ['md'], directions: ['ltr'], themes: ['light', 'dark'] }).forEa
           </style>
           <p style="color: var(--ion-color-${color});">Hello World</p>
         `,
+          config
+        );
+
+        const results = await new AxeBuilder({ page }).analyze();
+        expect(results.violations).toEqual([]);
+      });
+    }
+  });
+});
+
+configs({ modes: ['md', 'ios'], directions: ['ltr'], themes: ['dark'] }).forEach(({ config, title }) => {
+  test.describe(title('theme'), () => {
+    test.beforeAll(({ skip }) => {
+      skip.browser('firefox');
+      skip.browser('webkit');
+    });
+
+    const colors = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger', 'light', 'medium', 'dark'];
+
+    for (const color of colors) {
+      test(`color "${color}" should pass AA guidelines`, async ({ page }) => {
+        await page.setContent(
+          `
+        <style>
+          body {
+            background-color: var(--ion-background-color);
+          }
+        </style>
+        <p style="color: var(--ion-color-${color});">Hello World</p>
+      `,
           config
         );
 
