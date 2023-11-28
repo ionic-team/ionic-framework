2dee0b857dcd963fa6dc694519090c916e6a4709
Sean Perkins <sean@ionic.io>
2023-11-28 13:31:01 -0500

    chore: add theme support for playwright

--- core/src/test/theme/theme.e2e.ts
+++ core/src/test/theme/theme.e2e.ts
@@ -2,83 +2,32 @@ import AxeBuilder from '@axe-core/playwright';
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-const ionicColors: {
-  [key: string]: {
-    hex: string;
-    lightBackground: string;
-    darkBackground: string;
-  };
-} = {
-  primary: {
-    hex: '#428cff',
-    lightBackground: '#fff',
-    darkBackground: '#121212',
-  },
-  secondary: {
-    hex: '#50c8ff',
-    lightBackground: '#fff',
-    darkBackground: '#121212',
-  },
-  tertiary: {
-    hex: '#6a64ff',
-    lightBackground: '#fff',
-    darkBackground: '#121212',
-  },
-};
+const colors = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger', 'light', 'medium', 'dark'];
 
-configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ config, title }) => {
-  test.describe(title('theme: dark'), () => {
+configs({ modes: ['md'], directions: ['ltr'], themes: ['light', 'dark'] }).forEach(({ config, title }) => {
+  test.describe(title('theme'), () => {
     test.beforeAll(async ({ skip }) => {
       skip.browser('firefox');
       skip.browser('webkit');
     });
 
-    test.describe('on a light background', () => {
-      for (const color of Object.keys(ionicColors)) {
-        test(`color "${color}" should pass AA guidelines`, async ({ page }) => {
-          await page.setContent(
-            `
+    for (const color of colors) {
+      test(`color "${color}" should pass AA guidelines`, async ({ page }) => {
+        await page.setContent(
+          `
           <style>
-            :root {
-              --ion-color-${color}: ${ionicColors[color].hex};
-            }
             body {
-              background-color: ${ionicColors[color].lightBackground};
+              background-color: var(--ion-background-color);
             }
           </style>
           <p style="color: var(--ion-color-${color});">Hello World</p>
         `,
-            config
-          );
+          config
+        );
 
-          const results = await new AxeBuilder({ page }).analyze();
-          expect(results.violations).toEqual([]);
-        });
-      }
-    });
-
-    test.describe('on a dark background', () => {
-      for (const color of Object.keys(ionicColors)) {
-        test(`color "${color}" should pass AA guidelines`, async ({ page }) => {
-          await page.setContent(
-            `
-          <style>
-            :root {
-              --ion-color-${color}: ${ionicColors[color].hex};
-            }
-            body {
-              background-color: ${ionicColors[color].darkBackground};
-            }
-          </style>
-          <p style="color: var(--ion-color-${color});">Hello World</p>
-        `,
-            config
-          );
-
-          const results = await new AxeBuilder({ page }).analyze();
-          expect(results.violations).toEqual([]);
-        });
-      }
-    });
+        const results = await new AxeBuilder({ page }).analyze();
+        expect(results.violations).toEqual([]);
+      });
+    }
   });
 });
