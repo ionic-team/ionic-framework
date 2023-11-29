4c6249d4701eec14e7cf14e088dcda5f86b032aa
Shawn Taylor <shawn@ionic.io>
2023-11-29 15:12:55 -0500

    feat(checkbox): add shadow part for label

--- core/src/components/checkbox/test/checkbox.spec.ts
+++ core/src/components/checkbox/test/checkbox.spec.ts
@@ -2,6 +2,23 @@ import { newSpecPage } from '@stencil/core/testing';
 
 import { Checkbox } from '../checkbox';
 
+describe('ion-checkbox: shadow parts', () => {
+  it('should render the checkbox with shadow parts', async () => {
+    const page = await newSpecPage({
+      components: [Checkbox],
+      html: `
+        <ion-checkbox>Checkbox</ion-checkbox>
+      `,
+    });
+
+    const checkbox = page.body.querySelector('ion-checkbox')!;
+
+    expect(checkbox.shadowRoot!.querySelector('[part="container"]')).not.toBe(null);
+    expect(checkbox.shadowRoot!.querySelector('[part="label"]')).not.toBe(null);
+    expect(checkbox.shadowRoot!.querySelector('[part="mark"]')).not.toBe(null);
+  });
+});
+
 describe('ion-checkbox: disabled', () => {
   it('clicking disabled checkbox should not toggle checked state', async () => {
     const page = await newSpecPage({
