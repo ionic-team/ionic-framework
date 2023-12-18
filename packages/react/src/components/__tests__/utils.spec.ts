0c9472ad177ff5492c6b0aa02071a0984f63c7c0
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-12-18 17:42:33 -0500

    fix react tests

--- packages/react/src/components/__tests__/utils.spec.ts
+++ packages/react/src/components/__tests__/utils.spec.ts
@@ -1,5 +1,6 @@
 import * as utils from '../react-component-lib/utils';
-import '@testing-library/jest-dom';
+// https://github.com/testing-library/jest-native/issues/72
+import '@testing-library/jest-dom/extend-expect';
 
 describe('isCoveredByReact', () => {
   it('should identify standard events as covered by React', () => {
