e840083aa0a8a168ffbf16b59adc1a79b9bcc2e7
Sean Perkins <sean@ionic.io>
2023-12-19 18:16:09 -0500

    feat(modal): update styles to iOS 17 specs

--- core/src/components/modal/gestures/swipe-to-close.ts
+++ core/src/components/modal/gestures/swipe-to-close.ts
@@ -13,7 +13,7 @@ import { calculateSpringStep, handleCanDismiss } from './utils';
 
 // Defaults for the card swipe animation
 export const SwipeToCloseDefaults = {
-  MIN_PRESENTING_SCALE: 0.93,
+  MIN_PRESENTING_SCALE: 0.91,
 };
 
 export const createSwipeToCloseGesture = (
