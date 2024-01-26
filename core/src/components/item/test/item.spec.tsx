49b4ed77cc26daf1e61a76215fe49045a1e3fdf6
Sean Perkins <sean@ionic.io>
2024-01-26 11:56:46 -0500

    chore: move spec test into describe block

--- core/src/components/item/test/item.spec.tsx
+++ core/src/components/item/test/item.spec.tsx
@@ -3,18 +3,20 @@ import { newSpecPage } from '@stencil/core/testing';
 
 import { Item } from '../item';
 
-it('should change focusable option after switching button option status', async () => {
-  const page = await newSpecPage({
-    components: [Item],
-    template: () => <ion-item button={false}></ion-item>,
+describe('item', () => {
+  it('should change focusable option after switching button option status', async () => {
+    const page = await newSpecPage({
+      components: [Item],
+      template: () => <ion-item button={false}></ion-item>,
+    });
+
+    const item = page.body.querySelector('ion-item')!;
+    // Change button attribute to true
+    item.setAttribute('button', 'true');
+
+    await page.waitForChanges();
+
+    // Check if it has the expected class that gives the highlight style to .item-highlight element
+    expect(item).toHaveClass('ion-focusable');
   });
-
-  const item = page.body.querySelector('ion-item')!;
-  // Change button attribute to true
-  item.setAttribute('button', 'true');
-
-  await page.waitForChanges();
-
-  // Check if it has the expected class that gives the highlight style to .item-highlight element
-  expect(item).toHaveClass('ion-focusable');
 });
