331e44cdcbc40ecc995a4388b51832be2eb1320b
Sean Perkins <sean@ionic.io>
2024-01-29 23:08:30 -0500

    chore: attempt to exclude stencil/core from optimization

--- packages/react/test/apps/react18/vite.config.ts
+++ packages/react/test/apps/react18/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 3000
   },
   optimizeDeps: {
-    exclude: ['@ionic/react', '@ionic/react-router'],
+    exclude: ['@ionic/react', '@ionic/react-router', '@stencil/core'],
     /**
      * ESM dependencies that have a CJS dependency need to 
      * include that dependency for optimization.
