fb484898034d06af94b20200b982be36d18d18d4
Sean Perkins <13732623+sean-perkins@users.noreply.github.com>
2024-01-31 11:34:57 -0500

    chore(test-apps): improve developer message with test apps (#28917)
    Issue number: N/A
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Improves the developer message when generating a test app to include
    the next steps to running the test app.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!--
      If this introduces a breaking change:
    1. Describe the impact and migration path for existing applications
    below.
      2. Update the BREAKING.md file with the breaking change.
    3. Add "BREAKING CHANGE: [...]" to the commit description when merging.
    See
    https://github.com/ionic-team/ionic-framework/blob/main/.github/CONTRIBUTING.md#footer
    for more information.
    -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->

--- packages/angular/test/build.sh
+++ packages/angular/test/build.sh
@@ -32,4 +32,12 @@ cp -R $FULL_BASE_DIR $BUILD_APP_DIR
 # will override any files in the base application.
 cp -R $FULL_APP_DIR $BUILD_APP_DIR
 
-echo "Copied test app files for ${APP_DIR}"
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Print a message with instructions for developers.
+echo -e "\n${GREEN}Successfully generated test app at: $BUILD_APP_DIR \n${NC}"
+echo -e "Please follow these steps:"
+echo -e "  1. cd $BUILD_APP_DIR"
+echo -e "  2. Install dependencies with: pnpm install"
+echo -e "  3. Run the dev server with: pnpm start\n"
