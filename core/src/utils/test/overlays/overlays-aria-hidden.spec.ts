900ec0f147bc7401b3f5343cd3dbf4e1ba43e263
amandaesmith3 <amanda.s@ionic.io>
2024-02-08 10:15:11 -0600

    add comment

--- core/src/utils/test/overlays/overlays-aria-hidden.spec.ts
+++ core/src/utils/test/overlays/overlays-aria-hidden.spec.ts
@@ -2,6 +2,12 @@ import { newSpecPage } from '@stencil/core/testing';
 
 import { Modal } from '../../../components/modal/modal';
 
+/**
+ * These tests are put in a separate file from overlays.spec.ts
+ * as a workaround to a Stencil issue where setTimeouts in other
+ * components can hang around and execute after their relevant
+ * tests have finished, causing errors.
+ */
 describe('aria-hidden on individual overlays', () => {
   it('should hide non-topmost overlays from screen readers', async () => {
     const page = await newSpecPage({
