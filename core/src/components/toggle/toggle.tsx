6ca3829fb2e23c71ca32b4ceb6902e9d4897333b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-02-12 15:17:08 -0500

    Update toggle.tsx

--- core/src/components/toggle/toggle.tsx
+++ core/src/components/toggle/toggle.tsx
@@ -9,7 +9,7 @@ import { checkmarkOutline, removeOutline, ellipseOutline } from 'ionicons/icons'
 
 import { config } from '../../global/config';
 import { getIonMode } from '../../global/ionic-global';
-import type { Color, Gesture, GestureDetail, Mode, StyleEventDetail } from '../../interface';
+import type { Color, Gesture, GestureDetail, Mode } from '../../interface';
 
 import type { ToggleChangeEventDetail } from './toggle-interface';
 
