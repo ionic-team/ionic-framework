d53fb297e7dddd6127b028e66e03367b8db3a80d
Brandy Carney <brandyscarney@users.noreply.github.com>
2024-03-11 17:29:47 -0400

    refactor(item): remove deprecated highlight variables used with legacy form controls (#29055)
    Issue number: internal
    
    ---------
    
    ## What is the current behavior?
    
    In Ionic Framework v7, we [simplified the form control
    syntax](https://ionic.io/blog/ionic-7-is-here#simplified-form-control-syntax),
    eliminating the requirement to place form controls inside of an
    `ion-item`. We ensured backwards compatibility by introducing a `legacy`
    property for the form controls and keeping, but deprecating, the
    following CSS variables on item:
    
    ```css
    --highlight-color-focused
    --highlight-color-invalid
    --highlight-color-valid
    --highlight-height
    ```
    
    While this was supported in v7, console warnings were logged to notify
    developers that they needed to update to the modern syntax if they were
    using form controls in an item for the best accessibility experience.
    
    ## What is the new behavior?
    
    Removes the `--highlight-color-focused`, `--highlight-color-invalid`,
    `--highlight-color-valid`, and `--highlight-height` variables from item.
    
    ## Does this introduce a breaking change?
    
    - [x] Yes
    - [ ] No
    
    Steps taken to mitigate this breaking change: 
    1. Developers have had console warnings when using the legacy syntax
    since the v7 release and the variables were marked as `deprecated`.
    2. The removal of these CSS variables has been documented in the
    Breaking Changes document with a link to the migration guides for the
    affected form controls.
    
    BREAKING CHANGE:
    
    The following CSS variables have been removed from item:
    `--highlight-height`, `--highlight-color-focused`,
    `--highlight-color-valid`, and `--highlight-color-invalid`. These
    variables were used on the bottom border highlight of an item when the
    form control inside of that item was focused. The form control syntax
    was [simplified in
    v7](https://ionic.io/blog/ionic-7-is-here#simplified-form-control-syntax)
    so that inputs, selects, and textareas would no longer be required to be
    used inside of an item.
    
    If you have not yet migrated to the modern form control syntax,
    migration guides for each of the form controls that added a highlight to
    item can be found below:
    - [Input migration
    documentation](https://ionicframework.com/docs/api/input#migrating-from-legacy-input-syntax)
    - [Select migration
    documentation](https://ionicframework.com/docs/api/select#migrating-from-legacy-select-syntax)
    - [Textarea migration
    documentation](https://ionicframework.com/docs/api/textarea#migrating-from-legacy-textarea-syntax)
    
    The highlight variables should then be moved from the item to the form
    control:
    
    ```diff
    - ion-item {
    + ion-input,
    + ion-textarea,
    + ion-select {
      --highlight-color-focused: purple;
      --highlight-color-valid: blue;
      --highlight-color-invalid: orange;
      --highlight-height: 6px;
    }
    ```
    
    > [!NOTE]
    > The input and textarea components are scoped, which means they will
    automatically scope their CSS by appending each of the styles with an
    additional class at runtime. Overriding scoped selectors in CSS requires
    a [higher
    specificity](https://developer.mozilla.org/en-US/docs/Web/CSS/Specificity)
    selector. Targeting the `ion-input` or `ion-textarea` for customization
    will not work; therefore we recommend adding a class and customizing it
    that way.
    
    ---------
    
    Co-authored-by: Sean Perkins <sean@ionic.io>

--- core/src/components/item/item.tsx
+++ core/src/components/item/item.tsx
@@ -374,10 +374,8 @@ export class Item implements ComponentInterface, AnchorInterface, ButtonInterfac
                 flip-rtl={detailIcon === chevronForward}
               ></ion-icon>
             )}
-            <div class="item-inner-highlight"></div>
           </div>
           {canActivate && mode === 'md' && <ion-ripple-effect></ion-ripple-effect>}
-          <div class="item-highlight"></div>
         </TagType>
       </Host>
     );
