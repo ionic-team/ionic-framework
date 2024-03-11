1630cb3dddfb6b565802f343e077f2d87e40da29
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-03-11 14:03:07 -0400

    add ability to run in headed mode

---
+++ core/scripts/run-playwright-docker.sh
@@ -0,0 +1,12 @@
+# Build docker image
+npm run docker.build
+
+# Run docker container and execute playwright tests
+# -e and -v allow us to run playwright in a headed environment
+# if --headed is passed
+
+# Set "$@" to pass an arguments to the playwright command
+docker run -it --rm \
+-e DISPLAY=host.docker.internal:0 \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+--ipc=host --mount=type=bind,source=./,target=/ionic ionic-playwright npm run test.e2e -- "$@"
