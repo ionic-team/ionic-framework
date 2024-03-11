64647cfc1f7f5174ddfb443fc728888daa90d2be
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-03-11 15:19:43 -0400

    add txt file instead for config

--- core/scripts/run-playwright-docker-ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run docker container and execute playwright tests
-# Do not pass -it since we do not need this session to be interactive (since it will run on CI)
-# We also do not set -e and -v to run in headed mode.
-
-# Set "$@" to pass an arguments to the playwright command
-docker run -e CI='true' --rm --ipc=host --mount=type=bind,source=./,target=/ionic ionic-playwright npm run test.e2e -- "$@"
--- core/scripts/run-playwright-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run docker container and execute playwright tests
-# -e and -v allow us to run playwright in a headed environment
-# if --headed is passed
-
-# Set "$@" to pass an arguments to the playwright command
-docker run -it --rm -e DISPLAY=host.docker.internal:0 -v /tmp/.X11-unix:/tmp/.X11-unix --ipc=host --mount=type=bind,source=./,target=/ionic ionic-playwright npm run test.e2e -- "$@"
