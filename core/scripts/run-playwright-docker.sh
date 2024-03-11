e049cefc8398fc58a975a1865ed81de90b6123c5
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-03-11 14:59:51 -0400

    remove linebreak

--- core/scripts/run-playwright-docker.sh
+++ core/scripts/run-playwright-docker.sh
@@ -5,7 +5,4 @@
 # if --headed is passed
 
 # Set "$@" to pass an arguments to the playwright command
-docker run -it --rm \
--e DISPLAY=host.docker.internal:0 \
--v /tmp/.X11-unix:/tmp/.X11-unix \
---ipc=host --mount=type=bind,source=./,target=/ionic ionic-playwright npm run test.e2e -- "$@"
+docker run -it --rm -e DISPLAY=host.docker.internal:0 -v /tmp/.X11-unix:/tmp/.X11-unix --ipc=host --mount=type=bind,source=./,target=/ionic ionic-playwright npm run test.e2e -- "$@"
