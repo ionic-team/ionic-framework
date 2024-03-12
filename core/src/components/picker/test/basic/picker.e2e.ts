610e27572d9892027afe8be2caeae214cec2cbe2
José Rio <jose.rio@hotmail.com>
2024-03-12 19:40:07 +0000

    [fix](picker): Tests to ensure proper handler is triggered.

--- core/src/components/picker/test/basic/picker.e2e.ts
+++ core/src/components/picker/test/basic/picker.e2e.ts
@@ -25,4 +25,44 @@ configs().forEach(({ title, screenshot, config }) => {
       await expect(page).toHaveScreenshot(screenshot(`picker-value-selected`));
     });
   });
+
+  test.describe(title('Test cancel buttons'), () => {
+    test('cancel', async ({ page }) => {
+      await page.goto('/src/components/picker/test/basic', config);
+      
+      await page.click('#basicMultipleCancelBtns');
+      const optBtn1 = page.locator('ion-picker button.cancel1-btn');
+      await optBtn1.click();
+      const confirmOptBtn1Alert = page.locator('ion-alert[data-testid=cancel1-btn-clicked]');
+      const optBtn1AlertInfo = confirmOptBtn1Alert.locator('.alert-message').innerText();
+      const optBtn1AlertOkBtn = confirmOptBtn1Alert.locator('.alert-button-group button');
+      expect(await optBtn1AlertInfo).toBe('cancel1-btn-clicked');
+      await optBtn1AlertOkBtn.click();
+
+      await page.click('#basicMultipleCancelBtns');
+      const optBtn2 = page.locator('ion-picker button.cancel2-btn');
+      await optBtn2.click();
+      const confirmOptBtn2Alert = page.locator('ion-alert[data-testid=cancel2-btn-clicked]');
+      const optBtn2AlertInfo = confirmOptBtn2Alert.locator('.alert-message').innerText();
+      const optBtn2AlertOkBtn = confirmOptBtn2Alert.locator('.alert-button-group button');
+      expect(await optBtn2AlertInfo).toBe('cancel2-btn-clicked');
+      await optBtn2AlertOkBtn.click();
+      
+      await page.click('#basicMultipleCancelBtns');
+      const ionPickerDidDismiss = await page.spyOnEvent('ionPickerDidDismiss');
+      const optBtn3 = page.locator('ion-picker button.cancel3-btn');
+      await optBtn3.click();
+      await ionPickerDidDismiss.next();
+      expect(ionPickerDidDismiss).toHaveReceivedEventDetail({
+        data: {
+          hours: {
+            text: '1',
+            value: '01',
+            columnIndex: 0
+          }
+        },
+        role: 'cancel'
+      });
+    });
+  });
 });
