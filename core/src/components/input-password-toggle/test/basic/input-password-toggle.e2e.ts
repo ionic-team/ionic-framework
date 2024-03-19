7e66c47f476cda99763476705b2d979b2a9d1f18
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-03-19 14:22:27 -0400

    test(input): add screenshot test

--- core/src/components/input-password-toggle/test/basic/input-password-toggle.e2e.ts
+++ core/src/components/input-password-toggle/test/basic/input-password-toggle.e2e.ts
@@ -1,8 +1,8 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => {
-  test.describe(title('input password toggle'), () => {
+configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('input password toggle: states'), () => {
     test('should be hidden when inside of a readonly input', async ({ page }) => {
       await page.setContent(
         `
@@ -28,6 +28,23 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
 
       const inputPasswordToggle = page.locator('ion-input-password-toggle');
       await expect(inputPasswordToggle).toBeHidden();
+    });
+  });
+
+  test.describe(title('input password toggle: rendering'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-input label="input" type="password">
+            <ion-input-password-toggle slot="end"></ion-input-password-toggle>
+          </ion-input>
+        `,
+        config
+      );
+
+      const inputPasswordToggle = page.locator('ion-input-password-toggle');
+
+      await expect(inputPasswordToggle).toHaveScreenshot(screenshot('input-password-toggle'));
     });
   });
 });
