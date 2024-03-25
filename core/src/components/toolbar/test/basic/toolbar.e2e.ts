7cd0d9b4946f45968540a998129ae30974d375e4
amandaesmith3 <amanda.s@ionic.io>
2024-03-25 13:09:58 -0500

    add dark mode to existing toolbar test

--- core/src/components/toolbar/test/basic/toolbar.e2e.ts
+++ core/src/components/toolbar/test/basic/toolbar.e2e.ts
@@ -1,12 +1,12 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs().forEach(({ title, screenshot, config }) => {
+configs({ themes: ['light', 'dark'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('toolbar: basic'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto(`/src/components/toolbar/test/basic`, config);
 
-      // capture both header toolbars at once, but don't include all the white space in the ion-content
+      // capture all header toolbars at once, but don't include all the white space in the ion-content
       const header = page.locator('ion-header');
       await expect(header).toHaveScreenshot(screenshot(`toolbar-basic`));
     });
