f79871a014ab4693915e23582edf7b1db914acf3
João Ferreira <60441552+JoaoFerreira-FrontEnd@users.noreply.github.com>
2024-04-02 20:07:02 +0100

    feat(checkbox): add small size (#29253)
    - here are the changes to have the checkbox small;
    
    Issue number: resolves N/A
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    - Adds the new small size to ionic theme checkbox
    -- The small size of the checkbox is 16px.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ---------
    
    Co-authored-by: Sean Perkins <13732623+sean-perkins@users.noreply.github.com>
    Co-authored-by: Sean Perkins <sean@ionic.io>

--- core/src/components/checkbox/test/basic/checkbox.e2e.ts
+++ core/src/components/checkbox/test/basic/checkbox.e2e.ts
@@ -123,3 +123,22 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
     });
   });
 });
+
+configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('checkbox: basic visual tests'), () => {
+    test('should have a small size applied correctly', async ({ page }) => {
+      await page.setContent(
+        `
+        <div id="checkboxes">
+          <ion-checkbox size="small">Small</ion-checkbox>
+          <ion-checkbox size="small" checked="true">Small - Checked</ion-checkbox>
+        </div>
+      `,
+        config
+      );
+
+      const checkboxes = page.locator('#checkboxes');
+      await expect(checkboxes).toHaveScreenshot(screenshot(`checkbox-small`));
+    });
+  });
+});
