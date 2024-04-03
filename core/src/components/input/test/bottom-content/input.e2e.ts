48a4e98ba9be34793d43ee6f31d64d5925864b95
Maria Hutt <maria@ionic.io>
2024-04-03 13:02:24 -0700

    refactor(input): update helper text color for ionic theme

--- core/src/components/input/test/bottom-content/input.e2e.ts
+++ core/src/components/input/test/bottom-content/input.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+configs({ directions: ['ltr'], modes: ['ionic-md', 'md', 'ios'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('input: bottom content'), () => {
     test('entire input component should render correctly with no fill', async ({ page }) => {
       await page.setContent(
