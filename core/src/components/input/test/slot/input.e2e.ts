d17c86536c4f99faafbfec4aa8875f02af598939
amandaesmith3 <amanda.s@ionic.io>
2024-04-05 12:48:22 -0500

    adjust margins for start/end slots

--- core/src/components/input/test/slot/input.e2e.ts
+++ core/src/components/input/test/slot/input.e2e.ts
@@ -47,6 +47,25 @@ configs().forEach(({ title, screenshot, config }) => {
   });
 });
 
+configs({ modes: ['ionic-md'] }).forEach(({ title, config, screenshot }) => {
+  test.describe(title('input: start and end slots (visual checks for ionic theme)'), () => {
+    test('should not have visual regressions with a stacked label and outline fill', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-input label-placement="start" fill="outline" value="hi@ionic.io" label="Email">
+            <ion-icon slot="start" name="lock" aria-hidden="true"></ion-icon>
+            <ion-icon slot="end" name="lock" aria-hidden="true"></ion-icon>
+          </ion-input>
+        `,
+        config
+      );
+
+      const input = page.locator('ion-input');
+      await expect(input).toHaveScreenshot(screenshot(`input-slots-label-stacked-fill-outline`));
+    });
+  });
+});
+
 configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
   test.describe(title('input: start and end slots (functionality checks)'), () => {
     test('should raise floating label when there is content in the start slot', async ({ page }) => {
