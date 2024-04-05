40c45f125a53e0e286c947bed325d82699ff0cb5
João Ferreira <joao.manuel.ferreira123@gmail.com>
2024-04-05 18:13:50 +0100

    feat(checkbox): add states and tests

--- core/src/components/checkbox/test/states/checkbox.e2e.ts
+++ core/src/components/checkbox/test/states/checkbox.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+configs({ modes: ['ios', 'md', 'ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('checkbox: states'), () => {
     test('should render disabled checkbox correctly', async ({ page }) => {
       await page.setContent(
@@ -49,6 +49,47 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
 
       const checkbox = page.locator('ion-checkbox');
       await expect(checkbox).toHaveScreenshot(screenshot(`checkbox-unchecked`));
+    });
+
+    test('should render focus checkbox correctly', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-checkbox class="ion-focused">Label</ion-checkbox>
+          <ion-checkbox class="ion-focused" checked>Label</ion-checkbox>
+      `,
+        config
+      );
+
+      const checkbox = page.locator('ion-checkbox');
+      await expect(checkbox).toHaveScreenshot(screenshot(`checkbox-focused`));
+    });
+
+    test('should render checkbox hover correctly', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-checkbox>Label</ion-checkbox>
+          <ion-checkbox checked>Label</ion-checkbox>
+      `,
+        config
+      );
+
+      const checkbox = page.locator('ion-checkbox');
+      checkbox.hover();
+      await expect(checkbox).toHaveScreenshot(screenshot(`checkbox-hover`));
+    });
+
+    test('should render checkbox active correctly', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-checkbox>Label</ion-checkbox>
+          <ion-checkbox checked>Label</ion-checkbox>
+      `,
+        config
+      );
+
+      const checkbox = page.locator('ion-checkbox');
+      checkbox.click();
+      await expect(checkbox).toHaveScreenshot(screenshot(`checkbox-active`));
     });
   });
 });
