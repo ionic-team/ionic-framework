7a1be6ec0207d990f435f8bb2d45e7d2b5e62ac2
Brandy Carney <brandy@ionic.io>
2024-04-09 15:50:17 -0400

    test(card): update the test to match button shape + UX requirements design

--- core/src/components/card/test/shape/card.e2e.ts
+++ core/src/components/card/test/shape/card.e2e.ts
@@ -4,40 +4,42 @@ import { configs, test } from '@utils/test/playwright';
 /**
  * Shape is only available in the Ionic theme.
  */
-
 configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
-  test.describe(title('card: Shape'), () => {
-    test('Shape Default: should not have visual regressions with basic card', async ({ page }) => {
+  test.describe(title('card: shape'), () => {
+    test.describe('default', () => {
+      test('should not have visual regressions', async ({ page }) => {
+        await page.goto(`/src/components/card/test/shape`, config);
 
-      await page.goto(`/src/components/card/test/shape`, config);
+        await page.setIonViewport();
 
-      await page.setIonViewport();
+        const container = page.locator('#default');
 
-      const container = page.locator('#default');
-
-      await expect(container).toHaveScreenshot(screenshot(`card-default`));
+        await expect(container).toHaveScreenshot(screenshot(`card-default`));
+      });
     });
 
-    test('Shape round: should not have visual regressions with basic card', async ({ page }) => {
+    test.describe('round', () => {
+      test('should not have visual regressions', async ({ page }) => {
+        await page.goto(`/src/components/card/test/shape`, config);
 
-      await page.goto(`/src/components/card/test/shape`, config);
+        await page.setIonViewport();
 
-      await page.setIonViewport();
+        const container = page.locator('#round');
 
-      const container = page.locator('#round');
-
-      await expect(container).toHaveScreenshot(screenshot(`card-round`));
+        await expect(container).toHaveScreenshot(screenshot(`card-round`));
+      });
     });
 
-    test('Shape rectangular: should not have visual regressions with basic card', async ({ page }) => {
+    test.describe('rectangular', () => {
+      test('should not have visual regressions', async ({ page }) => {
+        await page.goto(`/src/components/card/test/shape`, config);
 
-      await page.goto(`/src/components/card/test/shape`, config);
+        await page.setIonViewport();
 
-      await page.setIonViewport();
+        const container = page.locator('#rectangular');
 
-      const container = page.locator('#rectangular');
-
-      await expect(container).toHaveScreenshot(screenshot(`card-rectangular`));
+        await expect(container).toHaveScreenshot(screenshot(`card-rectangular`));
+      });
     });
   });
 });
