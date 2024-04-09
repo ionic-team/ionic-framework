70d0377ccf71bdf80d7ac608e19bd18cb0c4a7f6
João Ferreira <60441552+JoaoFerreira-FrontEnd@users.noreply.github.com>
2024-04-09 18:23:22 +0100

    Update core/src/components/checkbox/test/basic/checkbox.e2e.ts
    Co-authored-by: Sean Perkins <13732623+sean-perkins@users.noreply.github.com>

--- core/src/components/checkbox/test/basic/checkbox.e2e.ts
+++ core/src/components/checkbox/test/basic/checkbox.e2e.ts
@@ -172,7 +172,7 @@ configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screensh
 
       const checkbox = page.locator('#checkboxes');
       const box = await checkbox.boundingBox();
-      if(box !== null){
+      if(box !== null) {
         await page.mouse.click(box.x + box.width / 2, box.y + 47 );
       }
     });
