0a71b061fa99a35e804aafe3906be757f7a9668e
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2024-04-10 16:24:16 +0100

    Added TODO

--- core/src/components/chip/chip.tsx
+++ core/src/components/chip/chip.tsx
@@ -50,6 +50,7 @@ export class Chip implements ComponentInterface {
         aria-disabled={this.disabled ? 'true' : null}
         class={createColorClasses(this.color, {
           [theme]: true,
+          // TODO(FW-6120): remove the theme==='ionic' when we add support for the `ios` and `md` modes.
           [`chip-${shape}`]: theme === 'ionic' && shape !== undefined,
           'chip-outline': this.outline,
           'chip-disabled': this.disabled,
