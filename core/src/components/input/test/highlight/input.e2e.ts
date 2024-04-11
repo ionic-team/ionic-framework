96e5b2be57303f371c99d703a1b925fda83dde01
Maria Hutt <thetaPC@users.noreply.github.com>
2024-04-11 09:51:42 -0700

    fix(input) match invalid state to ionic design (#29291)
    Co-authored-by: Sean Perkins <13732623+sean-perkins@users.noreply.github.com>

--- core/src/components/input/test/highlight/input.e2e.ts
+++ core/src/components/input/test/highlight/input.e2e.ts
@@ -344,3 +344,51 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
     });
   });
 });
+
+configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('input: highlights'), () => {
+    test.describe('input: no fill', () => {
+      test('should render invalid state correctly', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            value="hi@ionic.io"
+            class="ion-touched ion-invalid"
+            label="Email"
+            error-text="Please enter a valid email"
+            helper-text="Enter an email"
+            counter="true"
+            maxlength="20"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-no-fill-invalid`));
+      });
+    });
+    test.describe('input: outline', () => {
+      test('should render invalid state correctly', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            fill="outline"
+            value="hi@ionic.io"
+            class="ion-touched ion-invalid"
+            label="Email"
+            error-text="Please enter a valid email"
+            helper-text="Enter an email"
+            counter="true"
+            maxlength="20"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-outline-invalid`));
+      });
+    });
+  });
+});
