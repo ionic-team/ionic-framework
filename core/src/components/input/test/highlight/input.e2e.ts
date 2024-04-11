67ec7455a8d07cdfd5d2d36e726eb276cca40fec
Maria Hutt <thetaPC@users.noreply.github.com>
2024-04-11 12:22:03 -0700

    fix(input): match valid state to ionic design (#29277)
    Co-authored-by: Sean Perkins <13732623+sean-perkins@users.noreply.github.com>
    Co-authored-by: ionitron <hi@ionicframework.com>

--- core/src/components/input/test/highlight/input.e2e.ts
+++ core/src/components/input/test/highlight/input.e2e.ts
@@ -348,6 +348,25 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
 configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('input: highlights'), () => {
     test.describe('input: no fill', () => {
+      test('should render valid state correctly', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            value="hi@ionic.io"
+            class="ion-valid has-focus"
+            label="Email"
+            error-text="Please enter a valid email"
+            helper-text="Enter an email"
+            counter="true"
+            maxlength="20"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-no-fill-valid`));
+      });
       test('should render invalid state correctly', async ({ page }) => {
         await page.setContent(
           `
@@ -369,6 +388,26 @@ configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ title, screensh
       });
     });
     test.describe('input: outline', () => {
+      test('should render valid state correctly', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            fill="outline"
+            value="hi@ionic.io"
+            class="ion-valid has-focus"
+            label="Email"
+            error-text="Please enter a valid email"
+            helper-text="Enter an email"
+            counter="true"
+            maxlength="20"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-outline-valid`));
+      });
       test('should render invalid state correctly', async ({ page }) => {
         await page.setContent(
           `
@@ -388,6 +427,34 @@ configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ title, screensh
 
         const input = page.locator('ion-input');
         await expect(input).toHaveScreenshot(screenshot(`input-outline-invalid`));
+      });
+      test('should render custom highlight correctly', async ({ page }) => {
+        await page.setContent(
+          `
+          <style>
+            ion-input.custom {
+              --highlight-color-valid: purple;
+            }
+          </style>
+
+          <div class="container">
+            <ion-input
+              fill="outline"
+              value="hi@ionic.io"
+              class="custom has-focus ion-valid"
+              label="Email"
+              error-text="Please enter a valid email"
+              helper-text="Enter an email"
+              counter="true"
+              maxlength="20"
+            ></ion-input>
+          </div>
+        `,
+          config
+        );
+
+        const container = page.locator('.container');
+        await expect(container).toHaveScreenshot(screenshot(`input-outline-custom-highlight`));
       });
     });
   });
