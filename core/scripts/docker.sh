42e66c68e9d83a8e205766fbc9caf861093040db
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-04-12 11:44:16 -0400

    chore: use shell script for pwd

---
+++ core/scripts/docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --rm -e DISPLAY=$(cat docker-display.txt) -v $(cat docker-display-volume.txt) --ipc=host --mount=type=bind,source=$(pwd),target=/ionic ionic-playwright npm run test.e2e -- "$@"
