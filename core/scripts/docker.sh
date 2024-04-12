c4bd68fc19ad22b2df4744e7c64c4ed404ef93e8
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-04-12 11:54:58 -0400

    remove newline

--- core/scripts/docker.sh
+++ core/scripts/docker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 docker run -it --rm -e DISPLAY=$(cat docker-display.txt) -v $(cat docker-display-volume.txt) --ipc=host --mount=type=bind,source=$(pwd),target=/ionic ionic-playwright npm run test.e2e -- "$@"
