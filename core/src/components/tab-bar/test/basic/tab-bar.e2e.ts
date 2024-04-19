fa2fabdb0a4bd745140e81bf88d4dc5783fc9094
Brandy Carney <brandyscarney@users.noreply.github.com>
2024-04-19 14:26:15 -0400

    feat(tabs): add ionic theme for tab bar and tab button (#29359)
    Issue number: internal
    
    ---------
    
    ## What is the current behavior?
    Tab bar & tab button use the `md` styles for the `ionic` theme
    
    ## What is the new behavior?
    Adds the following styles for the `ionic` theme:
    - Rectangular, fixed tab bar
    - Box shadow with elevation z2
    - Selected, activated, and focused states
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ## Other information
    - A ticket (FW-6186) has been created to track updating the hardcoded
    hex values to design tokens
    - A ticket (FW-6189) has been created to add badge styles

--- core/src/components/tab-bar/test/basic/tab-bar.e2e.ts
+++ core/src/components/tab-bar/test/basic/tab-bar.e2e.ts
@@ -2,6 +2,46 @@ import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
 /**
+ * This behavior needs to be tested in all modes and directions
+ */
+configs({ modes: ['ionic-md', 'md', 'ios'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('tab-bar: basic'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-tab-bar selected-tab="2">
+          <ion-tab-button tab="1">
+            <ion-icon name="home-outline"></ion-icon>
+            <ion-label>Label</ion-label>
+          </ion-tab-button>
+
+          <ion-tab-button tab="2">
+            <ion-icon name="home-outline"></ion-icon>
+            <ion-label>Label</ion-label>
+          </ion-tab-button>
+
+          <ion-tab-button tab="3" class="ion-focused">
+            <ion-icon name="home-outline"></ion-icon>
+            <ion-label>Label</ion-label>
+          </ion-tab-button>
+
+          <ion-tab-button tab="4" class="ion-activated">
+            <ion-icon name="home-outline"></ion-icon>
+            <ion-label>Label</ion-label>
+          </ion-tab-button>
+        </ion-tab-bar>
+      `,
+        config
+      );
+
+      const tabBar = page.locator('ion-tab-bar');
+
+      await expect(tabBar).toHaveScreenshot(screenshot(`tab-bar-default`));
+    });
+  });
+});
+
+/**
  * This behavior needs to be tested in both modes and directions to
  * make sure the safe area padding is applied only to that side
  * regardless of direction
