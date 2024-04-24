f81f6c241ec1b318f0a0e1d1f5eb40f36735f333
Sean Perkins <sean@ionic.io>
2024-04-24 17:07:00 -0400

    fix: outline focus styles for ionic theme

--- core/src/components/input/test/fill/input.e2e.ts
+++ core/src/components/input/test/fill/input.e2e.ts
@@ -111,6 +111,26 @@ configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
         const input = page.locator('ion-input');
         await expect(input).toHaveScreenshot(screenshot(`input-fill-outline`));
       });
+
+      test('focus state should not have visual regressions', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input class="has-focus"
+            fill="outline"
+            label="Email"
+            value="hi@ionic.io"
+            helper-text="Enter your email"
+            maxlength="20"
+            counter="true"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-fill-outline-focus`));
+      });
+
       test('should render correctly with floating label', async ({ page }) => {
         await page.setContent(
           `
