0873dc2ffbc623439a74926937a75f54a28b9709
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-04-25 10:40:56 -0400

    test(item): re-enable nested interactive check (#29399)
    The ticket was marked as done, but this one was missed.

--- core/src/components/item/test/a11y/item.e2e.ts
+++ core/src/components/item/test/a11y/item.e2e.ts
@@ -7,10 +7,7 @@ configs({ directions: ['ltr'] }).forEach(({ config, screenshot, title }) => {
     test('should not have accessibility violations', async ({ page }) => {
       await page.goto(`/src/components/item/test/a11y`, config);
 
-      const results = await new AxeBuilder({ page })
-        // TODO(FW-404): Re-enable rule once select is updated to avoid nested-interactive
-        .disableRules('nested-interactive')
-        .analyze();
+      const results = await new AxeBuilder({ page }).analyze();
       expect(results.violations).toEqual([]);
     });
 
