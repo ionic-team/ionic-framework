4a4d447e9df26222434cb69c82557fd6a9c0c533
Maria Hutt <thetaPC@users.noreply.github.com>
2024-04-30 08:27:34 -0700

    fix(input): match medium size for ionic theme (#29403)

--- core/src/components/input/test/size/input.e2e.ts
+++ core/src/components/input/test/size/input.e2e.ts
@@ -6,6 +6,71 @@ import { configs, test } from '@utils/test/playwright';
  */
 configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('input: size'), () => {
+    test.describe('input: size medium', () => {
+      test('should not have visual regressions', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            label="Email"
+            value="hi@ionic.io"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-size-medium`));
+      });
+      test('should render correctly with stacked label', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            label="Email"
+            label-placement="stacked"
+            value="hi@ionic.io"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-size-medium-label-stacked`));
+      });
+      test('should not have visual regressions with fill outline', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            fill="outline"
+            label="Email"
+            label-placement="stacked"
+            value="hi@ionic.io"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-size-medium-outline`));
+      });
+      test('should not have visual regressions with fill outline and round shape', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-input
+            fill="outline"
+            shape="round"
+            label="Email"
+            label-placement="stacked"
+            value="hi@ionic.io"
+          ></ion-input>
+        `,
+          config
+        );
+
+        const input = page.locator('ion-input');
+        await expect(input).toHaveScreenshot(screenshot(`input-size-medium-outline-round`));
+      });
+    });
+
     test.describe('input: size large', () => {
       test('should not have visual regressions', async ({ page }) => {
         await page.setContent(
