3dfcc41b99049a8cfed9e57422bd78358d0ad507
Maria Hutt <maria@ionic.io>
2024-05-08 16:30:20 -0700

    test(input): revert as fix was open in another PR

--- core/src/components/input/test/fill/input.e2e.ts
+++ core/src/components/input/test/fill/input.e2e.ts
@@ -109,7 +109,7 @@ configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
         const input = page.locator('ion-input');
         await expect(input).toHaveScreenshot(screenshot(`input-fill-outline-label-floating`));
       });
-      test('padding should be customizable', async ({ page }) => {
+      test.only('padding should be customizable', async ({ page }) => {
         await page.setContent(
           `
           <style>
