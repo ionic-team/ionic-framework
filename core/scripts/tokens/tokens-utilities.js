ebfb13449283241d26496fbe6c4dca04c790ed85
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2024-05-21 09:07:31 +0100

    revert rgb lint

--- core/scripts/tokens/tokens-utilities.js
+++ core/scripts/tokens/tokens-utilities.js
@@ -2,7 +2,13 @@ const variablesPrefix = 'ionic';
 
 function hexToRgb(hex) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? { r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16) } : null;
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
 }
 
 function generateShadowValue(shadow) {
