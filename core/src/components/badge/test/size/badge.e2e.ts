ec161bf7651f4a307cde67b38904d1ce1ac54605
Maria Hutt <maria@ionic.io>
2024-05-21 13:37:53 -0700

    test(badge): add medium snapshots

--- core/src/components/badge/test/size/badge.e2e.ts
+++ core/src/components/badge/test/size/badge.e2e.ts
@@ -19,7 +19,7 @@ configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ config, screens
       await expect(badge).toHaveScreenshot(screenshot(`badge-size-small`));
     });
 
-    test('should render medium badges', async ({ page }) => {
+    test.only('should render medium badges', async ({ page }) => {
       await page.setContent(
         `
         <ion-badge size="medium">00</ion-badge>
