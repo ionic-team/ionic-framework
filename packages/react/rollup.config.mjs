66fb35604ce4fd3a4c144832077ca611b9ae61bf
Sean Perkins <sean.perkins@outsystems.com>
2024-05-21 09:25:38 -0400

    chore: upgrade to latest react output target

--- packages/react/rollup.config.mjs
+++ packages/react/rollup.config.mjs
@@ -1,4 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
+
 const external = ['react', 'react-dom', 'react-router', 'react-router-dom', 'history', 'tslib'];
 
 export default {
@@ -10,10 +11,12 @@ export default {
       chunkFileNames: '[name]-[hash].js',
       format: 'es',
       sourcemap: true,
-    }
+    },
   ],
-  plugins: [
-    typescript(),
-  ],
-  external: id => external.includes(id) || id.startsWith('@ionic/core') || id.startsWith('ionicons'),
+  plugins: [typescript()],
+  external: (id) =>
+    external.includes(id) ||
+    id.startsWith('@ionic/core') ||
+    id.startsWith('ionicons') ||
+    id.startsWith('@stencil/react-output-target'),
 };
--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -88,7 +88,7 @@ export {
   TransitionOptions,
 } from '@ionic/core/components';
 
-export * from './proxies';
+export * from './components';
 export * from './routing-proxies';
 
 // createControllerComponent
