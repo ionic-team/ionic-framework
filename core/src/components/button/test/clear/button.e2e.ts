72bfe63211267788a52d1655edbbd9ed336e11da
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2024-05-27 20:56:38 +0100

    revert tests

--- core/src/components/button/test/clear/button.e2e.ts
+++ core/src/components/button/test/clear/button.e2e.ts
@@ -4,7 +4,7 @@ import { configs, test } from '@utils/test/playwright';
 /**
  * Fill="clear" does not render differently based on the direction.
  */
-configs({ directions: ['ltr'], modes: ['ios', 'md'] }).forEach(({ title, config, screenshot }) => {
+configs({ directions: ['ltr'], modes: ['ios', 'md', 'ionic-md'] }).forEach(({ title, config, screenshot }) => {
   test.describe(title('button: fill: clear'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto(`/src/components/button/test/clear`, config);
@@ -12,23 +12,6 @@ configs({ directions: ['ltr'], modes: ['ios', 'md'] }).forEach(({ title, config,
       await page.setIonViewport();
 
       await expect(page).toHaveScreenshot(screenshot(`button-fill-clear`));
-    });
-  });
-});
-
-configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
-  test.describe(title('button: fill: clear'), () => {
-    test('should not have visual regressions', async ({ page }) => {
-      await page.setContent(
-        `<div id="container" class="ion-padding">
-          <ion-button fill="clear">Default</ion-button>
-        </div>`,
-        config
-      );
-
-      const chip = page.locator('#container');
-
-      await expect(chip).toHaveScreenshot(screenshot(`ionic-button-fill-clear`));
     });
   });
 });
