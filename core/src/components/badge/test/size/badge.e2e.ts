7a3acff7f8b4a14c55308225c8feb746bde8c0b4
Maria Hutt <maria@ionic.io>
2024-05-29 17:04:39 -0700

    test(badge): mimic designs

--- core/src/components/badge/test/size/badge.e2e.ts
+++ core/src/components/badge/test/size/badge.e2e.ts
@@ -5,11 +5,11 @@ import { configs, test } from '@utils/test/playwright';
  * This behavior does not vary across directions.
  */
 configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ config, screenshot, title }) => {
-  test.describe(title('badge: size'), () => {
+  test.describe.only(title('badge: size'), () => {
     test('should render xsmall badges', async ({ page }) => {
       await page.setContent(
         `
-        <ion-badge size="xsmall">00</ion-badge>
+        <ion-badge size="xsmall">1</ion-badge>
       `,
         config
       );
@@ -17,6 +17,19 @@ configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ config, screens
       const badge = page.locator('ion-badge');
 
       await expect(badge).toHaveScreenshot(screenshot(`badge-size-xsmall`));
+    });
+
+    test('should render xsmall badges with long text', async ({ page }) => {
+      await page.setContent(
+        `
+        <ion-badge size="xsmall">99+</ion-badge>
+      `,
+        config
+      );
+
+      const badge = page.locator('ion-badge');
+
+      await expect(badge).toHaveScreenshot(screenshot(`badge-size-xsmall-long-text`));
     });
 
     test('should render small badges', async ({ page }) => {
