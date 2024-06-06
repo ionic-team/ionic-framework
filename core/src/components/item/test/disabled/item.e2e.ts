72d6d4b550692bcac8a502f8bbdfe9b1d144f402
rug <rmfgoncalves@gmail.com>
2024-06-06 18:35:57 +0200

    Adding test to ionic theme

--- core/src/components/item/test/disabled/item.e2e.ts
+++ core/src/components/item/test/disabled/item.e2e.ts
@@ -4,7 +4,7 @@ import { configs, test } from '@utils/test/playwright';
 /**
  * This behavior does not vary across directions
  */
-configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+configs({ modes: ['ionic-md', 'md', 'ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('item: disabled state'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto(`/src/components/item/test/disabled`, config);
