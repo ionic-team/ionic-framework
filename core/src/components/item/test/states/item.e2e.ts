00c8d5ec557aac8fe03fbe0d454b62699c2b6f04
rug <rmfgoncalves@gmail.com>
2024-06-07 16:12:21 +0200

    feat(item): add styles for focus state in ionic theme (#29595)
    Issue number: internal
    
    ---------
    
    ## What is the new behavior?
    - Added scss for the focus state of `ion-item` for the Ionic theme
    - Added item with class `ion-focused` to test page
    - Added the ionic theme to the test page of item `states`
    
    ## Does this introduce a breaking change?
    - [ ] Yes
    - [x] No
    
    ## Other Information
    
    
    [Preview](https://ionic-framework-git-rou-10782-ionic1.vercel.app/src/components/item/test/theme-ionic?ionic:theme=ionic)
    
    ---------
    
    Co-authored-by: ionitron <hi@ionicframework.com>
    Co-authored-by: Brandy Carney <brandyscarney@users.noreply.github.com>

--- core/src/components/item/test/states/item.e2e.ts
+++ core/src/components/item/test/states/item.e2e.ts
@@ -4,7 +4,7 @@ import { configs, test } from '@utils/test/playwright';
 /**
  * This behavior does not vary across directions
  */
-configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+configs({ modes: ['ionic-md', 'ios', 'md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('item: states'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto(`/src/components/item/test/states`, config);
