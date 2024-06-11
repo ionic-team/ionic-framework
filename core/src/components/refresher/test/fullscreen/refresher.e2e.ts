771938cfcfa99dfc2c0ba54201656883f46b6d3b
Maria Hutt <maria@ionic.io>
2024-06-11 11:39:33 -0700

    test(refresher): add comment

--- core/src/components/refresher/test/fullscreen/refresher.e2e.ts
+++ core/src/components/refresher/test/fullscreen/refresher.e2e.ts
@@ -52,6 +52,7 @@ configs({ directions: ['ltr'] }).forEach(({ title, config, screenshot }) => {
       );
     });
 
+    // Bug only occurs with the legacy refresher.
     test.describe('legacy refresher', () => {
       test.only('should display when content is fullscreen', async ({ page, browserName }) => {
         test.info().annotations.push({
