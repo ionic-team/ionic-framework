ca67c292c7700d6679037523262b4193c2865eb4
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2024-06-24 17:27:32 +0100

    added ionic snapshots

--- core/src/components/title/test/theme-ionic/title.e2e.ts
+++ core/src/components/title/test/theme-ionic/title.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs().forEach(({ title, screenshot, config }) => {
+configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('title: basic'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto('/src/components/title/test/theme-ionic', config);
