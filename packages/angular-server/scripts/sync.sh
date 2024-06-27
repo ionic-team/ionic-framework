24b1aa7b1a21094340ddc61d64ad0606de2c1039
Brandy Carney <brandyscarney@gmail.com>
2024-06-27 10:37:16 -0400

    chore(angular-server): add a sync command

---
+++ packages/angular-server/scripts/sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Copy core dist
+rm -rf node_modules/@ionic/core/dist node_modules/@ionic/core/components
+cp -a ../../core/dist node_modules/@ionic/core/dist
+cp -a ../../core/hydrate node_modules/@ionic/core/
+cp -a ../../core/components node_modules/@ionic/core/components
+cp -a ../../core/package.json node_modules/@ionic/core/package.json
