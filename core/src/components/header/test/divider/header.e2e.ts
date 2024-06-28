b2f573fce74e903d58af2f284300a333e6caa9a1
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2024-06-28 17:38:29 +0100

    change test to setContent

--- core/src/components/header/test/divider/header.e2e.ts
+++ core/src/components/header/test/divider/header.e2e.ts
@@ -4,11 +4,28 @@ import { configs, test } from '@utils/test/playwright';
 configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('header: divider'), () => {
     test('should not have visual regressions with divider header', async ({ page }) => {
-      await page.goto(`/src/components/header/test/divider`, config);
+      await page.setContent(
+        `
+        <style>
+          .container {
+            padding: 10px;
+          }
+        </style>
+      
+        <div class="container">
+          <ion-header divider="true">
+            <ion-toolbar>
+              <ion-title>Header - Divider</ion-title>
+            </ion-toolbar>
+          </ion-header>
+        </div>
+      `,
+        config
+      );
 
-      await page.setIonViewport();
+      const container = page.locator('.container');
 
-      await expect(page).toHaveScreenshot(screenshot(`ionic-header-divider`));
+      await expect(container).toHaveScreenshot(screenshot(`ionic-header-divider`));
     });
   });
 });
