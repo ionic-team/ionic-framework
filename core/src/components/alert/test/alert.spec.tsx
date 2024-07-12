70c237ce00de08c46d204a51252ab9e7dbcb2f17
Mikel Hamer <mikelhamer@gmail.com>
2024-07-12 19:32:13 -0400

    fix(alert): dont overwrite id set in htmlAttributes
    closes #29704

--- core/src/components/alert/test/alert.spec.tsx
+++ core/src/components/alert/test/alert.spec.tsx
@@ -2,6 +2,7 @@ import { newSpecPage } from '@stencil/core/testing';
 
 import { config } from '../../../global/config';
 import { Alert } from '../alert';
+import { h } from '@stencil/core';
 
 describe('alert: custom html', () => {
   it('should not allow for custom html by default', async () => {
@@ -38,4 +39,16 @@ describe('alert: custom html', () => {
     expect(content.textContent).toContain('Custom Text');
     expect(content.querySelector('button.custom-html')).toBe(null);
   });
+
+  it('should not overwrite the id set in htmlAttributes', async () => {
+    const id = 'custom-id'
+    const page = await newSpecPage({
+      components: [Alert],
+      template: () => <ion-alert htmlAttributes={{id}} overlayIndex={-1}></ion-alert>
+    });
+
+    const alert = page.body.querySelector('ion-alert')!;
+    expect(alert.id).toBe(id);
+  });
+
 });
