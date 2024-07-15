6bf510d71e42ecdf6074fb27ac26190d78755198
Brandy Carney <brandyscarney@gmail.com>
2024-07-15 15:29:44 -0400

    style: code cleanup

--- core/src/components/segment-content/segment-content.tsx
+++ core/src/components/segment-content/segment-content.tsx
@@ -1,5 +1,5 @@
 import type { ComponentInterface } from '@stencil/core';
-import { Component, Element, Host, h } from '@stencil/core';
+import { Component, Host, h } from '@stencil/core';
 
 @Component({
   tag: 'ion-segment-content',
@@ -7,8 +7,6 @@ import { Component, Element, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class SegmentContent implements ComponentInterface {
-  @Element() el!: HTMLElement;
-
   render() {
     return (
       <Host>
