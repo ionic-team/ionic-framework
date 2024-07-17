0ba5219c09fc491255cc0d282581c77b98ca80de
Sean Perkins <sean.perkins@outsystems.com>
2024-07-17 17:08:45 -0400

    feat(IonReactRouter): migrate to a functional component and react router 6

--- packages/react-router/src/ReactRouter/IonReactRouter.tsx
+++ packages/react-router/src/ReactRouter/IonReactRouter.tsx
@@ -1,8 +1,8 @@
 import type { Action as HistoryAction, History, Location as HistoryLocation } from 'history';
-import { createBrowserHistory as createHistory } from 'history';
-import React from 'react';
+import type { PropsWithChildren } from 'react';
+import React, { useEffect, useRef } from 'react';
 import type { BrowserRouterProps } from 'react-router-dom';
-import { Router } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 
 import { IonRouter } from './IonRouter';
 
@@ -10,44 +10,30 @@ interface IonReactRouterProps extends BrowserRouterProps {
   history?: History;
 }
 
-export class IonReactRouter extends React.Component<IonReactRouterProps> {
-  historyListenHandler?: (location: HistoryLocation, action: HistoryAction) => void;
-  history: History;
+export const IonReactRouter = ({ children }: PropsWithChildren<IonReactRouterProps>) => {
+  const location = useLocation();
 
-  constructor(props: IonReactRouterProps) {
-    super(props);
-    const { history, ...rest } = props;
-    this.history = history || createHistory(rest);
-    this.history.listen(this.handleHistoryChange.bind(this));
-    this.registerHistoryListener = this.registerHistoryListener.bind(this);
-  }
+  const historyListenHandler = useRef<(location: HistoryLocation, action: HistoryAction) => void>();
 
-  /**
-   * history@4.x passes separate location and action
-   * params. history@5.x passes location and action
-   * together as a single object.
-   * TODO: If support for React Router <=5 is dropped
-   * this logic is no longer needed. We can just assume
-   * a single object with both location and action.
-   */
-  handleHistoryChange(location: HistoryLocation, action: HistoryAction) {
+  const registerHistoryListener = (cb: (location: HistoryLocation, action: HistoryAction) => void) => {
+    historyListenHandler.current = cb;
+  };
+
+  const handleHistoryChange = (location: HistoryLocation, action: HistoryAction) => {
     const locationValue = (location as any).location || location;
     const actionValue = (location as any).action || action;
-    if (this.historyListenHandler) {
-      this.historyListenHandler(locationValue, actionValue);
+    if (historyListenHandler.current) {
+      historyListenHandler.current(locationValue, actionValue);
     }
-  }
+  };
 
-  registerHistoryListener(cb: (location: HistoryLocation, action: HistoryAction) => void) {
-    this.historyListenHandler = cb;
-  }
+  useEffect(() => {
+    handleHistoryChange(location, 'POP'); // TODO @sean unsure about this
+  }, [location]);
 
-  render() {
-    const { children, ...props } = this.props;
-    return (
-      <Router history={this.history} {...props}>
-        <IonRouter registerHistoryListener={this.registerHistoryListener}>{children}</IonRouter>
-      </Router>
-    );
-  }
-}
+  return (
+    <BrowserRouter>
+      <IonRouter registerHistoryListener={registerHistoryListener}>{children}</IonRouter>
+    </BrowserRouter>
+  );
+};
