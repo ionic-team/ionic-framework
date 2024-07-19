2c4c100354465e9898db40310c2a6d15f790b5b2
Mikel Hamer <mikelhamer@gmail.com>
2024-07-19 00:51:07 -0400

    test(alert): move htmlAttributes id test to alert-id.spec.tsx

--- core/src/components/alert/test/alert.spec.ts
+++ core/src/components/alert/test/alert.spec.ts
@@ -2,7 +2,6 @@ import { newSpecPage } from '@stencil/core/testing';
 
 import { config } from '../../../global/config';
 import { Alert } from '../alert';
-import { h } from '@stencil/core';
 
 describe('alert: custom html', () => {
   it('should not allow for custom html by default', async () => {
@@ -40,14 +39,4 @@ describe('alert: custom html', () => {
     expect(content.querySelector('button.custom-html')).toBe(null);
   });
 
-  it('should not overwrite the id set in htmlAttributes', async () => {
-    const id = 'custom-id';
-    const page = await newSpecPage({
-      components: [Alert],
-      template: () => <ion-alert htmlAttributes={{ id }} overlayIndex={-1}></ion-alert>,
-    });
-
-    const alert = page.body.querySelector('ion-alert')!;
-    expect(alert.id).toBe(id);
-  });
 });
