fe0c02fa663eb451f87337130f77ec85fbf07bb1
José Rio <jose.rio@hotmail.com>
2024-08-21 15:30:47 +0100

    feat(input-password-toggle): switch margin into margin mixin.

--- core/src/components/input-password-toggle/test/basic/input-password-toggle.e2e.ts
+++ core/src/components/input-password-toggle/test/basic/input-password-toggle.e2e.ts
@@ -1,7 +1,6 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-
 configs({ modes: ['md', 'ios', 'ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('input password toggle: states'), () => {
     test('should be hidden when inside of a readonly input', async ({ page }) => {
