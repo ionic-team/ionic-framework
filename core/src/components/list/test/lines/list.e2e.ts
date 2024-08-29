e703f7fbdb6912e3f1642651cb40a48b36d92cfa
Brandy Carney <brandyscarney@gmail.com>
2024-08-29 15:33:22 -0400

    test(list): add ionic theme to lines e2e test

--- core/src/components/list/test/lines/list.e2e.ts
+++ core/src/components/list/test/lines/list.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs().forEach(({ title, screenshot, config }) => {
+configs({ modes: ['ios', 'md', 'ionic-md'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('list: lines'), () => {
     test('lines="full" should render correctly', async ({ page }) => {
       await page.goto(`/src/components/list/test/lines`, config);
