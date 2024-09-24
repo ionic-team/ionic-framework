8e177978624c083a98f2b00fbdbfcd3b29e40201
Brandy Carney <brandyscarney@gmail.com>
2024-09-24 11:26:49 -0400

    test(segment-view): remove toolbars

--- core/src/components/segment-view/test/basic/segment-view.e2e.ts
+++ core/src/components/segment-view/test/basic/segment-view.e2e.ts
@@ -9,18 +9,17 @@ configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
     test('should show the first content with no initial value', async ({ page }) => {
       await page.setContent(
         `
-          <ion-segment>
-            <ion-segment-button content-id="paid" value="paid">
-              <ion-label>Paid</ion-label>
-            </ion-segment-button>
-            <ion-segment-button content-id="free" value="free">
-              <ion-label>Free</ion-label>
-            </ion-segment-button>
-            <ion-segment-button content-id="top" value="top">
-              <ion-label>Top</ion-label>
-            </ion-segment-button>
-          </ion-segment>
-        </ion-toolbar>
+        <ion-segment>
+          <ion-segment-button content-id="paid" value="paid">
+            <ion-label>Paid</ion-label>
+          </ion-segment-button>
+          <ion-segment-button content-id="free" value="free">
+            <ion-label>Free</ion-label>
+          </ion-segment-button>
+          <ion-segment-button content-id="top" value="top">
+            <ion-label>Top</ion-label>
+          </ion-segment-button>
+        </ion-segment>
         <ion-segment-view>
           <ion-segment-content id="paid">Paid</ion-segment-content>
           <ion-segment-content id="free">Free</ion-segment-content>
@@ -37,18 +36,17 @@ configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
     test('should show the content matching the initial value', async ({ page }) => {
       await page.setContent(
         `
-          <ion-segment value="free">
-            <ion-segment-button content-id="paid" value="paid">
-              <ion-label>Paid</ion-label>
-            </ion-segment-button>
-            <ion-segment-button content-id="free" value="free">
-              <ion-label>Free</ion-label>
-            </ion-segment-button>
-            <ion-segment-button content-id="top" value="top">
-              <ion-label>Top</ion-label>
-            </ion-segment-button>
-          </ion-segment>
-        </ion-toolbar>
+        <ion-segment value="free">
+          <ion-segment-button content-id="paid" value="paid">
+            <ion-label>Paid</ion-label>
+          </ion-segment-button>
+          <ion-segment-button content-id="free" value="free">
+            <ion-label>Free</ion-label>
+          </ion-segment-button>
+          <ion-segment-button content-id="top" value="top">
+            <ion-label>Top</ion-label>
+          </ion-segment-button>
+        </ion-segment>
         <ion-segment-view>
           <ion-segment-content id="paid">Paid</ion-segment-content>
           <ion-segment-content id="free">Free</ion-segment-content>
@@ -65,18 +63,17 @@ configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
     test('should update the content when changing the value by clicking a segment button', async ({ page }) => {
       await page.setContent(
         `
-          <ion-segment value="free">
-            <ion-segment-button content-id="paid" value="paid">
-              <ion-label>Paid</ion-label>
-            </ion-segment-button>
-            <ion-segment-button content-id="free" value="free">
-              <ion-label>Free</ion-label>
-            </ion-segment-button>
-            <ion-segment-button content-id="top" value="top">
-              <ion-label>Top</ion-label>
-            </ion-segment-button>
-          </ion-segment>
-        </ion-toolbar>
+        <ion-segment value="free">
+          <ion-segment-button content-id="paid" value="paid">
+            <ion-label>Paid</ion-label>
+          </ion-segment-button>
+          <ion-segment-button content-id="free" value="free">
+            <ion-label>Free</ion-label>
+          </ion-segment-button>
+          <ion-segment-button content-id="top" value="top">
+            <ion-label>Top</ion-label>
+          </ion-segment-button>
+        </ion-segment>
         <ion-segment-view>
           <ion-segment-content id="paid">Paid</ion-segment-content>
           <ion-segment-content id="free">Free</ion-segment-content>
