bc8b59f2ab16547c5e28e68dc891de5f1c883bd4
Maria Hutt <maria@ionic.io>
2024-09-24 11:27:39 -0700

    test(tab-bar): add expand tests

---
+++ core/src/components/tab-bar/test/expand/tab-bar.e2e.ts
@@ -0,0 +1,39 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+/**
+ * This behavior only applies to the `ionic` theme.
+ */
+configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('tab-bar: expand'), () => {
+    test.describe(title('compact'), () => {
+      test('should not have visual regressions', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-tab-bar expand="compact">
+            <ion-tab-button tab="1">
+              <ion-icon name="triangle-outline"></ion-icon>
+              <ion-label>Label</ion-label>
+            </ion-tab-button>
+  
+            <ion-tab-button tab="2">
+              <ion-icon name="triangle-outline"></ion-icon>
+              <ion-label>Label</ion-label>
+            </ion-tab-button>
+  
+            <ion-tab-button tab="3">
+              <ion-icon name="triangle-outline"></ion-icon>
+              <ion-label>Label</ion-label>
+            </ion-tab-button>
+          </ion-tab-bar>
+        `,
+          config
+        );
+
+        const tabBar = page.locator('ion-tab-bar');
+
+        await expect(tabBar).toHaveScreenshot(screenshot(`tab-bar-expand-compact`));
+      });
+    });
+  });
+});
