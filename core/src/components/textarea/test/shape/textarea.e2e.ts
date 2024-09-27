70029332691288550814d5ce55bd5f0cd808cf95
Maria Hutt <thetaPC@users.noreply.github.com>
2024-09-27 15:16:28 -0700

    fix(textarea): update background placement for ionic (#29910)

--- core/src/components/textarea/test/shape/textarea.e2e.ts
+++ core/src/components/textarea/test/shape/textarea.e2e.ts
@@ -2,6 +2,38 @@ import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
 /**
+ * This behavior does not vary across directions.
+ * The round shape is only available in the Ionic and Material Design themes.
+ * Shapes with different fill types are only available in the Material Design theme.
+ */
+configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('textarea: shape'), () => {
+    test.describe('fill: solid', () => {
+      test.describe('round', () => {
+        test('should not have visual regressions', async ({ page }) => {
+          await page.setContent(
+            `
+            <ion-textarea
+              shape="round"
+              fill="solid"
+              label="Email"
+              label-placement="stacked"
+              value="hi@ionic.io"
+            ></ion-textarea>
+          `,
+            config
+          );
+
+          const textarea = page.locator('ion-textarea');
+
+          await expect(textarea).toHaveScreenshot(screenshot(`textarea-fill-solid-shape-round`));
+        });
+      });
+    });
+  });
+});
+
+/**
  * This behavior does not vary across directions
  * The round shape is only available in the Ionic and Material Design themes
  */
