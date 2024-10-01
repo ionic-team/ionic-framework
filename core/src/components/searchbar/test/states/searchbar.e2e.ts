4579aa8701af3fd64bbab07a53bcfbb9426eeaaf
Maria Hutt <maria@ionic.io>
2024-10-01 13:49:35 -0700

    test(searchbar): add snapshots for focus ring buttons

---
+++ core/src/components/searchbar/test/states/searchbar.e2e.ts
@@ -0,0 +1,44 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+/**
+ * This behavior is only applicable to the `ionic-md` mode.
+ * This behavior does not vary across directions.
+ */
+configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('searchbar: focused'), () => {
+    test('should render focus ring on the cancel button', async ({ page, pageUtils }) => {
+      await page.setContent(
+        `
+        <ion-searchbar show-cancel-button="always"></ion-searchbar>
+      `,
+        config
+      );
+
+      await pageUtils.pressKeys('Tab'); // Focused on the input
+      await pageUtils.pressKeys('Tab'); // Focused on the cancel button
+
+      const searchbar = page.locator('ion-searchbar');
+
+      await expect(searchbar).toHaveScreenshot(screenshot(`searchbar-state-focused-cancel-button`));
+    });
+  });
+
+  test.describe(title('searchbar: focused'), () => {
+    test('should render focus ring on the clear button', async ({ page, pageUtils }) => {
+      await page.setContent(
+        `
+        <ion-searchbar show-clear-button="always" value="Filled text"></ion-searchbar>
+      `,
+        config
+      );
+
+      await pageUtils.pressKeys('Tab'); // Focused on the input
+      await pageUtils.pressKeys('Tab'); // Focused on the clear button
+
+      const searchbar = page.locator('ion-searchbar');
+
+      await expect(searchbar).toHaveScreenshot(screenshot(`searchbar-state-focused-clear-button`));
+    });
+  });
+});
