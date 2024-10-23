b866eca5405512d88fd9e08236eb6ffa36ef3d44
Brandy Carney <brandyscarney@gmail.com>
2024-10-23 17:44:07 -0400

    test(alert): update to move spec test logic to existing e2e test

--- core/src/components/alert/test/a11y/alert.e2e.ts
+++ core/src/components/alert/test/a11y/alert.e2e.ts
@@ -17,6 +17,27 @@ const testAria = async (
 
   const alert = page.locator('ion-alert');
 
+  const header = alert.locator('.alert-title');
+  const subHeader = alert.locator('.alert-sub-title');
+
+  // If a header exists, it should be an h2 element
+  if (await header.count() > 0) {
+    const headerTagName = await header.evaluate((el) => el.tagName);
+    expect(headerTagName).toBe('H2');
+  }
+
+  // If a header and subHeader exist, the subHeader should be an h3 element
+  if (await header.count() > 0 && await subHeader.count() > 0) {
+    const subHeaderTagName = await subHeader.evaluate((el) => el.tagName);
+    expect(subHeaderTagName).toBe('H3');
+  }
+
+  // If a subHeader exists without a header, the subHeader should be an h2 element
+  if (await header.count() === 0 && await subHeader.count() > 0) {
+    const subHeaderTagName = await subHeader.evaluate((el) => el.tagName);
+    expect(subHeaderTagName).toBe('H2');
+  }
+
   /**
    * expect().toHaveAttribute() can't check for a null value, so grab and check
    * the values manually instead.
