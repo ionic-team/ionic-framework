d4ff124737f894302a3aa558219e9d1d7820b6be
Brandy Carney <brandyscarney@users.noreply.github.com>
2024-10-31 11:35:35 -0400

    feat(accordion): add shape styles for the ionic theme (#29973)
    - Applies border radius based on the `shape` defined on the parent accordion group
    - Adds an e2e test for shape with focused items to show the border radius
    - Adds screenshots for the e2e test

---
+++ core/src/components/accordion/test/shape/accordion.e2e.ts
@@ -0,0 +1,48 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ directions: ['ltr'], modes: ['ionic-md'] }).forEach(({ config, screenshot, title }) => {
+  test.describe(title('accordion: shape'), () => {
+    ['round', 'soft', 'rectangular'].forEach((shape) => {
+      test(`${shape} - should not have visual regressions`, async ({ page }) => {
+        await page.setContent(
+          `
+          <style>
+            /* Background styles to show the border radius */
+            :root {
+              background: #222;
+            }
+          </style>
+
+          <!-- Focused accordion to show the border radius -->
+          <ion-accordion-group value="first" expand="inset" shape="${shape}">
+            <ion-accordion value="first">
+              <ion-item slot="header" class="ion-focused">
+                <ion-label>Accordion title</ion-label>
+              </ion-item>
+              <div slot="content">This is the body of the accordion.</div>
+            </ion-accordion>
+            <ion-accordion value="second">
+              <ion-item slot="header">
+                <ion-label>Accordion title</ion-label>
+              </ion-item>
+              <div slot="content">This is the body of the accordion.</div>
+            </ion-accordion>
+            <ion-accordion value="third">
+              <ion-item slot="header">
+                <ion-label>Accordion title</ion-label>
+              </ion-item>
+              <div slot="content">This is the body of the accordion.</div>
+            </ion-accordion>
+          </ion-accordion-group>
+      `,
+          config
+        );
+
+        const accordionGroup = page.locator('ion-accordion-group');
+
+        await expect(accordionGroup).toHaveScreenshot(screenshot(`accordion-shape-${shape}`));
+      });
+    });
+  });
+});
