20267bdc133eddd32bf85ca6d90be448e815442c
Maria Hutt <maria@ionic.io>
2024-11-05 11:35:21 -0800

    test(select): add color snapshots

--- core/src/components/select/test/color/select.e2e.ts
+++ core/src/components/select/test/color/select.e2e.ts
@@ -1,6 +1,26 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
+configs({ modes: ['md', 'ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('input: color'), () => {
+    test.describe('input: fill outline', () => {
+      test('should set label and highlight color on expand', async ({ page }) => {
+        await page.setContent(
+          `
+          <ion-select fill="outline" label="Label" class="select-expanded" value="apple" class="ion-focused" color="danger">
+            <ion-select-option value="apple">Apple</ion-select-option>
+          </ion-select>
+        `,
+          config
+        );
+
+        const select = page.locator('ion-select');
+        await expect(select).toHaveScreenshot(screenshot(`select-outline-color`));
+      });
+    });
+  });
+});
+
 configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('input: color'), () => {
     test.describe('input: fill none', () => {
@@ -31,21 +51,6 @@ configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, screenshot, co
 
         const select = page.locator('ion-select');
         await expect(select).toHaveScreenshot(screenshot(`select-solid-color`));
-      });
-    });
-    test.describe('input: fill outline', () => {
-      test('should set label and highlight color on expand', async ({ page }) => {
-        await page.setContent(
-          `
-          <ion-select fill="outline" label="Label" class="select-expanded" value="apple" class="ion-focused" color="danger">
-            <ion-select-option value="apple">Apple</ion-select-option>
-          </ion-select>
-        `,
-          config
-        );
-
-        const select = page.locator('ion-select');
-        await expect(select).toHaveScreenshot(screenshot(`select-outline-color`));
       });
     });
   });
