47b69c9ccba8b7f52dac19b0a8eba98dd58d325c
Brandy Carney <brandyscarney@gmail.com>
2024-11-26 14:01:00 -0500

    test(angular): move common e2e flies

--- packages/angular/test/apps/ng19/e2e/src/standalone/router-outlet.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Router Outlet', () => {
-  beforeEach(() => {
-    cy.visit('/standalone/router-outlet');
-  })
-
-  it('should render a as a child page of the router outlet', () => {
-    cy.ionPageVisible('ion-router-outlet app-router-outlet');
-  });
-})
