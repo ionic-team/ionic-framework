b2695f6827a420fc952bf1ead837019d949cddf5
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-01-22 14:34:19 -0500

    test(e2e): update e2e test for parts

--- core/src/components/select/test/bottom-content/select.e2e.ts
+++ core/src/components/select/test/bottom-content/select.e2e.ts
@@ -24,16 +24,16 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
       await expect(helperText).toHaveText('Helper text');
       await expect(errorText).toBeHidden();
     });
-    test('input should have an aria-describedby attribute when helper text is present', async ({ page }) => {
+    test('select should have an aria-describedby attribute when helper text is present', async ({ page }) => {
       await page.setContent(
         `<ion-select label="Label" helper-text="Helper text" error-text="Error text"></ion-select>`,
         config
       );
 
-      const input = page.locator('ion-select button');
+      const select = page.locator('ion-select button');
       const helperText = page.locator('ion-select .helper-text');
       const helperTextId = await helperText.getAttribute('id');
-      const ariaDescribedBy = await input.getAttribute('aria-describedby');
+      const ariaDescribedBy = await select.getAttribute('aria-describedby');
 
       expect(ariaDescribedBy).toBe(helperTextId);
     });
@@ -50,45 +50,47 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
       await expect(errorText).toHaveText('Error text');
     });
 
-    test('input should have an aria-describedby attribute when error text is present', async ({ page }) => {
+    test('select should have an aria-describedby attribute when error text is present', async ({ page }) => {
       await page.setContent(
         `<ion-select label="Label" class="ion-invalid ion-touched" helper-text="Helper text" error-text="Error text"></ion-select>`,
         config
       );
 
-      const input = page.locator('ion-select button');
+      const select = page.locator('ion-select button');
       const errorText = page.locator('ion-select .error-text');
       const errorTextId = await errorText.getAttribute('id');
-      const ariaDescribedBy = await input.getAttribute('aria-describedby');
+      const ariaDescribedBy = await select.getAttribute('aria-describedby');
 
       expect(ariaDescribedBy).toBe(errorTextId);
     });
-    test('input should have aria-invalid attribute when input is invalid', async ({ page }) => {
+    test('select should have aria-invalid attribute when select is invalid', async ({ page }) => {
       await page.setContent(
         `<ion-select label="Label" class="ion-invalid ion-touched" helper-text="Helper text" error-text="Error text"></ion-select>`,
         config
       );
 
-      const input = page.locator('ion-select button');
+      const select = page.locator('ion-select button');
 
-      await expect(input).toHaveAttribute('aria-invalid');
+      await expect(select).toHaveAttribute('aria-invalid');
     });
-    test('input should not have aria-invalid attribute when input is valid', async ({ page }) => {
+    test('select should not have aria-invalid attribute when select is valid', async ({ page }) => {
       await page.setContent(
         `<ion-select label="Label" helper-text="Helper text" error-text="Error text"></ion-select>`,
         config
       );
 
-      const input = page.locator('ion-select button');
+      const select = page.locator('ion-select button');
 
-      await expect(input).not.toHaveAttribute('aria-invalid');
+      await expect(select).not.toHaveAttribute('aria-invalid');
     });
-    test('input should not have aria-describedby attribute when no hint or error text is present', async ({ page }) => {
+    test('select should not have aria-describedby attribute when no hint or error text is present', async ({
+      page,
+    }) => {
       await page.setContent(`<ion-select label="Label"></ion-select>`, config);
 
-      const input = page.locator('ion-select button');
+      const select = page.locator('ion-select button');
 
-      await expect(input).not.toHaveAttribute('aria-describedby');
+      await expect(select).not.toHaveAttribute('aria-describedby');
     });
   });
 });
@@ -149,105 +151,69 @@ configs({ modes: ['ios', 'md'], directions: ['ltr'] }).forEach(({ title, screens
 /**
  * Customizing supporting text is the same across modes and directions
  */
-configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('select: supporting text customization'), () => {
-    test('should not have visual regressions when rendering helper text with a custom color via css parts', async ({
+    test('should not have visual regressions when rendering helper text with custom css', async ({
       page,
     }) => {
       await page.setContent(
         `
         <style>
-          ion-select.custom-select::part(supporting-text) {
+          ion-select::part(supporting-text) {
             font-size: 20px;
           }
 
-          ion-select.custom-select::part(helper-text) {
+          ion-select::part(helper-text) {
             color: green;
           }
         </style>
-        <ion-select label="Label" class="custom-select" helper-text="Helper text"></ion-select>
-      `,
-        config
-      );
-
-      const errorText = page.locator('ion-select');
-      await expect(errorText).toHaveScreenshot(screenshot(`select-helper-text-custom-parts`));
-    });
-    test('should not have visual regressions when rendering helper text with a custom color via css var', async ({
-      page,
-    }) => {
-      await page.setContent(
-        `
-        <style>
-          ion-select.custom-select {
-            --helper-text-color: green;
-          }
-        </style>
-        <ion-select class="custom-select" label="Label" helper-text="Helper text"></ion-select>
+        <ion-select label="Label" helper-text="Helper text"></ion-select>
       `,
         config
       );
 
       const helperText = page.locator('ion-select');
-      await expect(helperText).toHaveScreenshot(screenshot(`select-helper-text-custom-color-var`));
+      await expect(helperText).toHaveScreenshot(screenshot(`select-helper-text-custom-css`));
     });
-    test('should not have visual regressions when rendering error text with a custom color via css parts', async ({
+    test('should not have visual regressions when rendering error text with custom css', async ({
       page,
     }) => {
       await page.setContent(
         `
         <style>
-          ion-select.custom-select::part(supporting-text) {
+          ion-select::part(supporting-text) {
             font-size: 20px;
           }
 
-          ion-select.custom-select::part(error-text) {
+          ion-select::part(error-text) {
             color: purple;
           }
         </style>
-        <ion-select label="Label" class="ion-invalid ion-touched custom-select" error-text="Error text"></ion-select>
+        <ion-select class="ion-invalid ion-touched" label="Label" error-text="Error text"></ion-select>
       `,
         config
       );
 
       const errorText = page.locator('ion-select');
-      await expect(errorText).toHaveScreenshot(screenshot(`select-error-text-custom-parts`));
+      await expect(errorText).toHaveScreenshot(screenshot(`select-error-text-custom-css`));
     });
-    test('should not have visual regressions when rendering error text with a custom color via css var', async ({
+    test('should not have visual regressions when rendering error text with a custom css variable', async ({
       page,
     }) => {
       await page.setContent(
         `
         <style>
-          ion-select.custom-select {
-            --error-text-color: purple;
-          }
-        </style>
-        <ion-select class="ion-invalid ion-touched custom-select" label="Label" error-text="Error text"></ion-select>
-      `,
-        config
-      );
-
-      const errorText = page.locator('ion-select');
-      await expect(errorText).toHaveScreenshot(screenshot(`select-error-text-custom-color-var`));
-    });
-    test('should not have visual regressions when rendering error text with a custom color via css highlight var', async ({
-      page,
-    }) => {
-      await page.setContent(
-        `
-        <style>
-          ion-select.custom-select {
+          ion-select {
             --highlight-color-invalid: purple;
           }
         </style>
-        <ion-select class="ion-invalid ion-touched custom-select" label="Label" error-text="Error text"></ion-select>
+        <ion-select class="ion-invalid ion-touched" label="Label" error-text="Error text"></ion-select>
       `,
         config
       );
 
       const errorText = page.locator('ion-select');
-      await expect(errorText).toHaveScreenshot(screenshot(`select-error-text-custom-highlight-var`));
+      await expect(errorText).toHaveScreenshot(screenshot(`select-error-text-custom-css-var`));
     });
   });
 });
