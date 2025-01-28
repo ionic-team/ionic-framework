6ce34d12c540b71bcb522a852be22771173172c5
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-01-28 18:06:33 -0500

    fix(radio): justify start when label placement is end

--- core/src/components/radio/test/item/radio.e2e.ts
+++ core/src/components/radio/test/item/radio.e2e.ts
@@ -40,7 +40,7 @@ configs().forEach(({ title, screenshot, config }) => {
 
 configs({ directions: ['ltr'], modes: ['md'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('radio: long label in item'), () => {
-    test('should render margins correctly when using long label in item', async ({ page }) => {
+    test('should not have visual regressions when using long label in item', async ({ page }) => {
       await page.setContent(
         `
           <ion-list>
@@ -60,8 +60,27 @@ configs({ directions: ['ltr'], modes: ['md'] }).forEach(({ title, screenshot, co
     });
   });
 
+  test.describe(title('radio: end label in item'), () => {
+    test('should not have visual regressions when using end label in item', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-list>
+            <ion-radio-group>
+              <ion-item>
+                <ion-radio label-placement="end">Enable Notifications</ion-radio>
+              </ion-item>
+            </ion-radio-group>
+          </ion-list>
+        `,
+        config
+      );
+      const list = page.locator('ion-list');
+      await expect(list).toHaveScreenshot(screenshot(`radio-end-label-in-item`));
+    });
+  });
+
   test.describe(title('radio: stacked label in item'), () => {
-    test('should render margins correctly when using stacked label in item', async ({ page }) => {
+    test('should not have visual regressions when using stacked label in item', async ({ page }) => {
       await page.setContent(
         `
           <ion-list>
