45637ec46a52102c4b5a503b10b07c729f2e2b35
BenOsodrac <bernardo.cardoso@outsystems.com>
2025-02-25 14:43:56 +0000

    add tests

---
+++ core/src/components/badge/test/hint/badge.e2e.ts
@@ -0,0 +1,14 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+configs({ modes: ['md', 'ios', 'ionic-md'] }).forEach(({ config, screenshot, title }) => {
+  test.describe(title('badge: hint'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto('/src/components/badge/test/hint', config);
+
+      await page.setIonViewport();
+
+      await expect(page).toHaveScreenshot(screenshot(`badge-hint`));
+    });
+  });
+});
