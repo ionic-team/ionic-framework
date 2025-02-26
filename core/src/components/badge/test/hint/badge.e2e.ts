a6c2643a2162585d141c07a06aee6975e2a6bd36
Bernardo Cardoso <32780808+BenOsodrac@users.noreply.github.com>
2025-02-26 10:09:31 +0000

    Update core/src/components/badge/test/hint/badge.e2e.ts
    Co-authored-by: Brandy Smith <brandyscarney@users.noreply.github.com>

--- core/src/components/badge/test/hint/badge.e2e.ts
+++ core/src/components/badge/test/hint/badge.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ modes: ['md', 'ios', 'ionic-md'] }).forEach(({ config, screenshot, title }) => {
+configs({ directions: ['ltr'], modes: ['md', 'ios', 'ionic-md'] }).forEach(({ config, screenshot, title }) => {
   test.describe(title('badge: hint'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto('/src/components/badge/test/hint', config);
