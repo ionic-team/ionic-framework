2edbdaffdcc5f80c162a52b0687a2861acf9ba6c
Maria Hutt <maria@ionic.io>
2025-02-27 08:07:04 -0800

    fix(app): revert removing of setFocus comment

--- core/src/components/app/app.tsx
+++ core/src/components/app/app.tsx
@@ -17,7 +17,13 @@ export class App implements ComponentInterface {
   @Element() el!: HTMLElement;
 
   /**
-   * Sets focus on elements that use `ion-focusable`.
+   * Used to set focus on an element that uses `ion-focusable`.
+   * Do not use this if focusing the element as a result of a keyboard
+   * event as the focus utility should handle this for us. This method
+   * should be used when we want to programmatically focus an element as
+   * a result of another user action. (Ex: We focus the first element
+   * inside of a popover when the user presents it, but the popover is not always
+   * presented as a result of keyboard action.)
    *
    * @param elements - The elements to set focus on.
    */
