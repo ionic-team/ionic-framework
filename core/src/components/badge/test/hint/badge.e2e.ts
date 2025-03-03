2db15bb351c305cbe547552d965d61c20d17bc86
Bernardo Cardoso <32780808+BenOsodrac@users.noreply.github.com>
2025-03-03 18:02:05 +0000

    feat(avatar): support new badge hint feature (#30223)
    Issue number: internal
    
    ---------
    
    <!-- Please do not submit updates to dependencies unless it fixes an
    issue. -->
    
    <!-- Please try to limit your pull request to one type (bugfix, feature,
    etc). Submit multiple pull requests if needed. -->
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - Added css on avatar to support the new badge vertical prop on md, ios
    and ionic theme
    - Added support on ionic theme for different position, according to the
    Avatar size.
    - There's no figma for md/iOS implementation, so just added for now a
    best effort on how it looks.
    - re-organized tests and added new screenshots for the avatar use-case.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    <!--
      If this introduces a breaking change:
    1. Describe the impact and migration path for existing applications
    below.
      2. Update the BREAKING.md file with the breaking change.
    3. Add "BREAKING CHANGE: [...]" to the commit description when merging.
    See
    https://github.com/ionic-team/ionic-framework/blob/main/docs/CONTRIBUTING.md#footer
    for more information.
    -->
    
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->
    
    - [Badge
    hint](https://ionic-framework-git-rou-11628-ionic1.vercel.app/src/components/badge/test/hint?ionic:theme=ionic)

--- core/src/components/badge/test/hint/badge.e2e.ts
+++ core/src/components/badge/test/hint/badge.e2e.ts
@@ -2,13 +2,23 @@ import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
 configs({ directions: ['ltr'], modes: ['md', 'ios', 'ionic-md'] }).forEach(({ config, screenshot, title }) => {
-  test.describe(title('badge: hint'), () => {
+  test.describe(title('badge: hint empty'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto('/src/components/badge/test/hint', config);
 
-      const container = page.locator('ion-list');
+      const container = page.locator('#empty');
 
-      await expect(container).toHaveScreenshot(screenshot(`badge-hint`));
+      await expect(container).toHaveScreenshot(screenshot(`badge-hint-empty`));
+    });
+  });
+
+  test.describe(title('badge: hint inside avatar'), () => {
+    test('should not have visual regressions', async ({ page }) => {
+      await page.goto('/src/components/badge/test/hint', config);
+
+      const container = page.locator('#avatar');
+
+      await expect(container).toHaveScreenshot(screenshot(`badge-hint-avatar`));
     });
   });
 });
