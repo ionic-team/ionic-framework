0865a33a9405d31555ad72ba6edd7a7d174ade94
Marta Carlos <marta.carlos@outsystems.com>
2025-03-10 11:31:05 +0000

    fix(button): fix spinner margins
    also update e2e tests spanshots

--- core/src/components/button/test/spinner/button.e2e.ts
+++ core/src/components/button/test/spinner/button.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ modes: ['ios', 'md', 'ionic-md', 'ionic-ios'] }).forEach(({ config, screenshot, title }) => {
+configs({ modes: ['ionic-md', 'ionic-ios'] }).forEach(({ config, screenshot, title }) => {
   test.describe(title('button: spinner'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto(`/src/components/button/test/spinner`, config);
