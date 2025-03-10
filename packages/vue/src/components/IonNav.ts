944de97198841e201a5b2ecf46e9e3bc6e57411a
Christian Bromann <git@bromann.dev>
2025-03-10 11:58:25 -0700

    tweak

--- packages/vue/src/components/IonNav.ts
+++ packages/vue/src/components/IonNav.ts
@@ -1,27 +1,12 @@
+import type { JSX, AnimationBuilder } from "@ionic/core/components";
 import { defineCustomElement } from "@ionic/core/components/ion-nav.js";
-import type { VNode } from "vue";
+import type { VNode , PropType } from "vue";
 import { defineComponent, h, shallowRef } from "vue";
 
 import { VueDelegate } from "../framework-delegate";
 
-export const IonNav = /*@__PURE__*/ defineComponent({
-  name: "IonNav",
-  /**
-   * The default values follow what is defined at
-   * https://ionicframework.com/docs/api/nav#properties
-   * otherwise the default values on the Web Component
-   * may be overridden. For example, if the default animated value
-   * is not `true` below, then Vue would default the prop to `false`
-   * which would override the Web Component default of `true`.
-   */
-  props: {
-    animated: Boolean,
-    animation: Function,
-    root: [Function, Object, String],
-    rootParams: Object,
-    swipeGesture: Boolean,
-  },
-  setup(props) {
+export const IonNav = /*@__PURE__*/ defineComponent<JSX.IonNav>(
+  (props: JSX.IonNav) => {
     defineCustomElement();
     const views = shallowRef([]);
 
@@ -39,4 +24,37 @@ export const IonNav = /*@__PURE__*/ defineComponent({
       return h("ion-nav", { ...props, delegate }, views.value);
     };
   },
-});
+  {
+    name: "IonNav",
+    /**
+     * The default values follow what is defined at
+     * https://ionicframework.com/docs/api/nav#properties
+     * otherwise the default values on the Web Component
+     * may be overridden. For example, if the default animated value
+     * is not `true` below, then Vue would default the prop to `false`
+     * which would override the Web Component default of `true`.
+     */
+    props: {
+      animated: {
+        type: Boolean,
+        default: true,
+      },
+      animation: {
+        type: Function as unknown as PropType<AnimationBuilder>,
+        default: undefined,
+      },
+      root: {
+        type: [Function, Object, String],
+        default: undefined,
+      },
+      rootParams: {
+        type: Object,
+        default: undefined,
+      },
+      swipeGesture: {
+        type: Boolean,
+        default: undefined,
+      },
+    },
+  }
+);
