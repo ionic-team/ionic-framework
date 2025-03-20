bba7dbf946ec3799b38e146e9ca36775bd37865c
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-03-20 10:50:16 -0400

    feat(select): add helper and error text to the ionic theme

--- core/src/components/select/test/bottom-content/select.e2e.ts
+++ core/src/components/select/test/bottom-content/select.e2e.ts
@@ -98,7 +98,7 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
 /**
  * Rendering is different across modes
  */
-configs({ modes: ['ios', 'md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+configs({ modes: ['ios', 'md', 'ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('select: helper text rendering'), () => {
     test('should not have visual regressions when rendering helper text', async ({ page }) => {
       await page.setContent(`<ion-select label="Label" helper-text="Helper text"></ion-select>`, config);
