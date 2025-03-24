3002234a62a40b937547519403b990962ddf2f62
Marta Carlos <101343976+OS-martacarlos@users.noreply.github.com>
2025-03-24 15:33:00 +0000

    Apply suggestions from code review
    Co-authored-by: Brandy Smith <brandyscarney@users.noreply.github.com>

--- core/src/components/divider/test/basic/divider.e2e.ts
+++ core/src/components/divider/test/basic/divider.e2e.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ directions: ['ltr', 'rtl'], modes: ['ios', 'md', 'ionic-md'] }).forEach(({ config, screenshot, title }) => {
+configs({ directions: ['ltr'], modes: ['md', 'ionic-md'] }).forEach(({ config, screenshot, title }) => {
   test.describe(title('divider: rendering'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.setContent(
