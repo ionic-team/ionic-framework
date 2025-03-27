01424c13b8cd0eb9c4ada704c8aa34bb5ba11cef
João Ferreira <joao.manuel.ferreira@outsystems.com>
2025-03-27 11:26:10 +0000

    - lint.fix;

--- core/src/components/datetime/datetime.tsx
+++ core/src/components/datetime/datetime.tsx
@@ -2383,7 +2383,7 @@ export class Datetime implements ComponentInterface {
                     if (isAdjacentDay) {
                       // The user selected a day outside the current month. Ignore this button, as the month will be re-rendered.
                       this.el.blur();
-                      this.activeParts = {...activePart, ...referenceParts};
+                      this.activeParts = { ...activePart, ...referenceParts };
                       this.animateToDate(referenceParts);
                       this.confirm();
                     } else {
