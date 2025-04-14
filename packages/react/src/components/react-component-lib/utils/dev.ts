75f8e84bde89b9413f3e848a421b19bc2b68f16c
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-04-14 14:10:36 -0400

    chore: build

--- packages/react/src/components/react-component-lib/utils/dev.ts
+++ packages/react/src/components/react-component-lib/utils/dev.ts
@@ -1,5 +1,3 @@
-import { printIonWarning } from '@ionic/core';
-
 export const isDevMode = () => {
   return process && process.env && process.env.NODE_ENV === 'development';
 };
@@ -9,7 +7,7 @@ const warnings: { [key: string]: boolean } = {};
 export const deprecationWarning = (key: string, message: string) => {
   if (isDevMode()) {
     if (!warnings[key]) {
-      printIonWarning(message);
+      console.warn(message);
       warnings[key] = true;
     }
   }
