d5a097111b82418ddab3301caecb2184c71e5e13
Shane <shane@shanessite.net>
2025-05-01 12:08:44 -0700

    Update core/src/components/select/test/basic/select.e2e.ts
    Co-authored-by: Maria Hutt <thetaPC@users.noreply.github.com>

--- core/src/components/select/test/basic/select.e2e.ts
+++ core/src/components/select/test/basic/select.e2e.ts
@@ -153,6 +153,10 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
 
   test.describe(title('select: click'), () => {
     test('clicking a select label should only trigger onclick once', async ({ page }) => {
+      testInfo.annotations.push({
+        type: 'issue',
+        description: 'https://github.com/ionic-team/ionic-framework/issues/30165',
+      });
       // Create a spy function in page context
       await page.setContent(
         `
