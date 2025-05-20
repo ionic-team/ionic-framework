ca27ed618ba9b6daf017f3ac132d258fb5898665
Maria Hutt <thetaPC@users.noreply.github.com>
2025-05-19 17:01:48 -0700

    chore(IonRouteInner): upgrade to rr6

--- packages/react-router/src/ReactRouter/IonRouteInner.tsx
+++ packages/react-router/src/ReactRouter/IonRouteInner.tsx
@@ -4,27 +4,6 @@ import { Route } from 'react-router';
 
 export class IonRouteInner extends React.PureComponent<IonRouteProps> {
   render() {
-    return (
-      <Route
-        path={this.props.path}
-        exact={this.props.exact}
-        render={this.props.render}
-        {
-          /**
-           * `computedMatch` is a private API in react-router v5 that
-           * has been removed in v6.
-           *
-           * This needs to be removed when we support v6.
-           *
-           * TODO: FW-647
-           */
-          ...((this.props as any).computedMatch !== undefined
-            ? {
-                computedMatch: (this.props as any).computedMatch,
-              }
-            : {})
-        }
-      />
-    );
+    return <Route path={this.props.path} end={this.props.end} element={this.props.element} />;
   }
 }
