0461608ed4880bd82743313f50d69e46c5ba0a52
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-05-27 14:51:25 -0400

    chore(scripts): update build scripts for test app to remove deleted files

--- packages/angular/test/build.sh
+++ packages/angular/test/build.sh
@@ -35,4 +35,13 @@ rsync -a --exclude='.git' --exclude='node_modules' $FULL_BASE_DIR $BUILD_APP_DIR
 echo "Copying application version..."
 rsync -a --exclude='.git' --exclude='node_modules' $FULL_APP_DIR $BUILD_APP_DIR
 
+# Remove files that don't exist in either source directory
+echo "Cleaning up removed files..."
+find $BUILD_APP_DIR -type f -not -path "*/node_modules/*" -not -path "*/.git/*" | while read file; do
+    rel_path=${file#$BUILD_APP_DIR}
+    if [ ! -f "${FULL_BASE_DIR}${rel_path}" ] && [ ! -f "${FULL_APP_DIR}${rel_path}" ]; then
+        rm "$file"
+    fi
+done
+
 echo "Copied test app files for ${APP_DIR}"
