70dbcc977d563a9d5cb00d4599f6468895bf1d9c
Brandy Smith <brandyscarney@users.noreply.github.com>
2025-06-23 09:51:43 -0400

    feat(reorder-group): add ionReorderStart, ionReorderMove, ionReorderEnd events (#30471)
    Issue number: resolves #23148 resolves #27614
    
    ---------
    
    ## What is the current behavior?
    The `ion-reorder-group` only emits an `ionItemReorder` event when the reorder gesture ends AND the item position has changed. There is no way to listen for when the gesture starts, is actively moving, or ends without the item changing position.
    
    ## What is the new behavior?
    - Adds an `ionReorderStart` event that is fired without any details on the start of the gesture.
    - Adds an `ionReorderMove` event that is fired continuously during gesture move and includes the `from` and `to` detail.
    - Adds an `ionReorderEnd` event that is fired at the end of the gesture and always includes the `from` and `to` detail, even if they are the same.
    - Deprecates the `ionItemReorder` event, recommending to use the `ionReorderEnd` instead.
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    While this does not introduce a breaking change, it does deprecate the `ionItemReorder` event in favor of the `ionReorderEnd` event. This event behaves a bit differently since it is always emitted on end. If the `from` and `to` are the same, it will still emit them, so it's possible to check if they are the same to determine if `ionReorderEnd` fired without moving item positions.
    
    ----
    
    Co-authored-by: sfinktah <sfinktah@github.spamtrak.org>
    Co-authored-by: Brandy Smith <6577830+brandyscarney@users.noreply.github.com>

--- packages/angular/src/index.ts
+++ packages/angular/src/index.ts
@@ -90,6 +90,7 @@ export {
   InputOtpChangeEventDetail,
   InputOtpCompleteEventDetail,
   InputOtpInputEventDetail,
+  // TODO(FW-6590): Remove the next two lines once the deprecated event is removed
   ItemReorderEventDetail,
   ItemReorderCustomEvent,
   ItemSlidingCustomEvent,
@@ -112,6 +113,10 @@ export {
   RangeKnobMoveEndEventDetail,
   RefresherCustomEvent,
   RefresherEventDetail,
+  ReorderMoveCustomEvent,
+  ReorderMoveEventDetail,
+  ReorderEndCustomEvent,
+  ReorderEndEventDetail,
   RouterEventDetail,
   RouterCustomEvent,
   ScrollBaseCustomEvent,
