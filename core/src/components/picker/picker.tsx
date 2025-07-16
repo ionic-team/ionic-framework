c2b40d8808280ccc08006682e076ee0ec5916f59
José Rio <jose.rio@hotmail.com>
2025-07-16 12:23:37 +0100

    fix(piker): remove aria-hidden attribute

--- core/src/components/picker/picker.tsx
+++ core/src/components/picker/picker.tsx
@@ -537,7 +537,6 @@ export class Picker implements ComponentInterface {
     return (
       <Host onPointerDown={(ev: PointerEvent) => this.onPointerDown(ev)} onClick={() => this.onClick()}>
         <input
-          aria-hidden="true"
           tabindex={-1}
           inputmode="numeric"
           type="number"
