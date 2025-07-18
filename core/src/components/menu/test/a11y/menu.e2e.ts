ba6ca9b6215b578a58fca89637307dee04863415
Christian Bromann <git@bromann.dev>
2025-07-18 10:43:23 -0700

    fix linter

--- core/src/components/menu/test/a11y/menu.e2e.ts
+++ core/src/components/menu/test/a11y/menu.e2e.ts
@@ -1,4 +1,4 @@
-import AxeBuilder from '@axe-core/playwright';
+// import AxeBuilder from '@axe-core/playwright';
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
