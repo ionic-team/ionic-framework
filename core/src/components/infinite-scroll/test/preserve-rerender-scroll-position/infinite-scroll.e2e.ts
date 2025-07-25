7a569259842dc16367d115c590324d953dd451f6
ShaneK <shane@shanessite.net>
2025-07-25 06:23:59 -0700

    fix(infinite-scroll): prevent locking sibling min height while it's already locked, prevent useless setTimeout from being set up while preserve rerender scroll property isn't set

--- core/src/components/infinite-scroll/test/preserve-rerender-scroll-position/infinite-scroll.e2e.ts
+++ core/src/components/infinite-scroll/test/preserve-rerender-scroll-position/infinite-scroll.e2e.ts
@@ -1,6 +1,8 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
+test.setTimeout(100000);
+
 configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
   test.describe(title('infinite-scroll: preserve rerender scroll position'), () => {
     test('should load more items when scrolled to the bottom', async ({ page }) => {
@@ -13,7 +15,7 @@ configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
       expect(await items.count()).toBe(30);
 
       let previousScrollTop = 0;
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 20; i++) {
         await content.evaluate((el: HTMLIonContentElement) => el.scrollToBottom(0));
         const currentScrollTop = await innerScroll.evaluate((el: HTMLIonContentElement) => el.scrollTop);
         expect(currentScrollTop).toBeGreaterThan(previousScrollTop);
@@ -28,7 +30,9 @@ configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
         // Timeout to allow the browser to catch up.
         // For some reason, without this, the scroll top gets reset to 0. Adding this
         // prevents that, which implies it's an issue with Playwright, not the feature.
-        await new Promise((resolve) => setTimeout(resolve, 100));
+        // For some reason, this delay needs to be longer than the time required to
+        // reset the minimum height in infinite scroll.
+        await new Promise((resolve) => setTimeout(resolve, 1001));
       }
     });
   });
