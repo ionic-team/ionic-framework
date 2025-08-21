8240d60a453e39c05cc6e59d5ba837a1b1f5afc8
José Rio <jose.rio@hotmail.com>
2025-08-21 13:05:22 +0100

    (chore): Add script to create testing local packages

---
+++ packages/vue/scripts/sync-and-pack.sh
@@ -0,0 +1,34 @@
+set -e
+
+# Delete old packages
+rm -f *.tgz
+
+# Delete vite cache
+rm -rf node_modules/.vite
+
+# Pack @ionic/core
+echo "\n📦 Packing @ionic/core..."
+npm pack ../../core
+
+# Pack @ionic/vue-router
+echo "\n📦 Packing @ionic/vue-router..."
+npm pack ../vue-router
+
+# Update package.json with global path for the @ionic/core package
+echo "\n⚙️ Updating package.json with global path for @ionic/core..."
+CORE_PACKAGE=$(ls ionic-core-*.tgz | head -1)
+sed -i "" "s|\"@ionic/core\": \".*\"|\"@ionic/core\": \"file:$(pwd)/$CORE_PACKAGE\"|" package.json
+
+# Install Dependencies
+echo "\n🔧 Installing dependencies..."
+npm install
+
+# Build the project
+echo "\n🔨 Building the project..."
+npm run build
+
+# Pack @ionic/vue
+echo "\n📦 Packing @ionic/vue..."
+npm pack
+
+echo "\n✅ Packed @ionic/vue package!\n   $(pwd)/$(ls ionic-vue-*.tgz | head -1)\n"
