3282d870eaf1115689fbad06acf9a5d9756aa8e7
BenOsodrac <bernardo.cardoso@outsystems.com>
2025-09-02 19:08:10 +0100

    migrate tokens scripts to esmodules logic

--- core/scripts/tokens/index.js
+++ core/scripts/tokens/index.js
@@ -4,177 +4,168 @@
 // - It also works very well out of the box with any kind of Design Tokens formats, like Figma Tokens, as well as APIs to adjust to more custom ones.
 // - It is probably the most well-known and widely used Design Tokens tool. It has also been regularly maintained for a long time.
 // - It can easily scale to different necessities we might have in the future.
-(async () => {
-  const {
-    generateShadowValue,
-    generateFontSizeValue,
-    generateFontFamilyValue,
-    generateTypographyOutput,
-    generateValue,
-    generateColorUtilityClasses,
-    generateDefaultSpaceUtilityClasses,
-    generateSpaceUtilityClasses,
-    removeConsecutiveRepeatedWords,
-    setPrefixValue,
-    generateRadiusUtilityClasses,
-    generateBorderUtilityClasses,
-    generateFontUtilityClasses,
-    generateShadowUtilityClasses,
-    generateUtilityClasses
-  } = require('./utils.js');
+import * as utils from './utils.js';
+const {
+  generateShadowValue,
+  generateFontSizeValue,
+  generateFontFamilyValue,
+  generateTypographyOutput,
+  generateValue,
+  generateColorUtilityClasses,
+  generateDefaultSpaceUtilityClasses,
+  generateSpaceUtilityClasses,
+  removeConsecutiveRepeatedWords,
+  setPrefixValue,
+  generateRadiusUtilityClasses,
+  generateBorderUtilityClasses,
+  generateFontUtilityClasses,
+  generateShadowUtilityClasses,
+  generateUtilityClasses
+} = utils;
+import StyleDictionary from 'style-dictionary';
 
-  const StyleDictionary = (await import('style-dictionary')).default;
+// Set the prefix for variables and classes
+setPrefixValue('token');
 
-  // Set the prefix for variables and classes
-  setPrefixValue('ion');
+// Register a custom file header
+StyleDictionary.registerFileHeader({
+  name: 'custom-header',
+  fileHeader: (defaultMessages = []) => {
+    return [...defaultMessages, 'Do not edit directly, this file was auto-generated.'];
+  },
+});
 
-  // Register a custom file header
-  StyleDictionary.registerFileHeader({
-    name: 'custom-header',
-    fileHeader: async (defaultMessages = []) => {
-      return [...defaultMessages, 'Do not edit directly, this file was auto-generated.'];
-    },
-  });
+// SCSS variables format
+StyleDictionary.registerFormat({
+  name: 'scssVariablesFormat',
+  format: function ({ dictionary }) { // Use 'format' for Style Dictionary v3
+    console.log('Generating SCSS variables...');
 
-  // SCSS variables format
-  StyleDictionary.registerFormat({
-    name: 'scssVariablesFormat',
-    format: async function ({ dictionary, file }) {
+    const primitiveProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'primitives');
+    const scaleProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'scale');
+    const borderProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'border');
+    const semanticsProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'semantics');
+    const nonPrimitiveScaleBorderSemanticsProperties = dictionary.allTokens.filter(
+      (prop) => !['primitives', 'scale', 'border', 'semantics'].includes(prop.path[0])
+    );
+    const typographyProperties = nonPrimitiveScaleBorderSemanticsProperties.filter((prop) => prop.$type === 'typography');
+    const otherProperties = nonPrimitiveScaleBorderSemanticsProperties.filter((prop) => prop.$type !== 'typography');
+    
+    // Order: primitives → semantics → scale → border → other → typography
+    const sortedProperties = [
+      ...primitiveProperties,
+      ...semanticsProperties,
+      ...scaleProperties,
+      ...borderProperties,
+      ...otherProperties,
+      ...typographyProperties
+    ];
 
-      console.log('Generating SCSS variables...');
+    const prefixedVariables = sortedProperties.map((prop) => {
+      // Remove consecutive repeated words from the token name, like border-border-color
+      const propName = removeConsecutiveRepeatedWords(prop.name);
 
-      const primitiveProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'primitives');
-      const scaleProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'scale');
-      const borderProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'border');
-      const semanticsProperties = dictionary.allTokens.filter((prop) => prop.path[0] === 'semantics');
-      const nonPrimitiveScaleBorderSemanticsProperties = dictionary.allTokens.filter(
-        (prop) => !['primitives', 'scale', 'border', 'semantics'].includes(prop.path[0])
-      );
-      const typographyProperties = nonPrimitiveScaleBorderSemanticsProperties.filter((prop) => prop.$type === 'typography');
-      const otherProperties = nonPrimitiveScaleBorderSemanticsProperties.filter((prop) => prop.$type !== 'typography');
-      
-      // Order: primitives → semantics → scale → border → other → typography
-      const sortedProperties = [
-        ...primitiveProperties,
-        ...semanticsProperties,
-        ...scaleProperties,
-        ...borderProperties,
-        ...otherProperties,
-        ...typographyProperties
-      ];
+      switch (prop.$type) {
+        case 'boxShadow':
+          return generateShadowValue(prop, propName);
+        case 'fontFamilies':
+          return generateFontFamilyValue(prop, propName, 'scss');
+        case 'fontSizes':
+          return generateFontSizeValue(prop, propName, 'scss');
+        case 'typography':
+          return generateTypographyOutput(prop, propName, true);
+        default:
+          return generateValue(prop, propName);
+      }
+    });
 
-      const prefixedVariables = sortedProperties.map((prop) => {
-        // Remove consecutive repeated words from the token name, like border-border-color
-        const propName = removeConsecutiveRepeatedWords(prop.name);
+    // In v3, the header is added automatically by Style Dictionary.
+    // The format function should only return the file content.
+    return [
+      '@use "../themes/functions.sizes" as font;\n',
+      prefixedVariables.join('\n') + '\n',
+    ].join('');
+  },
+});
 
-        switch (prop.$type) {
-          case 'boxShadow':
-            return generateShadowValue(prop, propName);
-          case 'fontFamilies':
-            return generateFontFamilyValue(prop, propName, 'scss');
-          case 'fontSizes':
-            return generateFontSizeValue(prop, propName, 'scss');
-          case 'typography':
-            return generateTypographyOutput(prop, propName, true);
-          default:
-            return generateValue(prop, propName);
-        }
-      });
+// Create utility-classes
+StyleDictionary.registerFormat({
+  name: 'cssUtilityClassesFormat',
+  format: function ({ dictionary }) { // Use 'format' for Style Dictionary v3
+    console.log('Generating Utility-Classes...');
 
-      const fileHeader = await file.options.fileHeader();
+    // Arrays to store specific utility classes
+    const typographyUtilityClasses = [];
+    const otherUtilityClasses = [];
 
-      return [
-        `/*\n${fileHeader.join('\n')}\n*/`,
-        '@use "../themes/functions.sizes" as font;\n',
-        prefixedVariables.join('\n') + '\n',
-      ].join('\n');
-    },
-  });
+    // Generate utility classes for each token
+    dictionary.allTokens.map((prop) => {
+      const tokenCategory = prop.attributes.category;
 
-  // Create utility-classes
-  StyleDictionary.registerFormat({
-    name: 'cssUtilityClassesFormat',
-    format: async function ({ dictionary, file }) {
+      if (prop.$type === 'fontFamilies' || tokenCategory === 'scale' || tokenCategory === 'backdrop') {
+        // Not creating for the tokens below, as they make no sense to exist as utility-classes.
+        return;
+      }
 
-      console.log('Generating Utility-Classes...');
+      // Remove consecutive repeated words from the token name, like border-border-color
+      const propName = removeConsecutiveRepeatedWords(prop.name);
 
-      // Arrays to store specific utility classes
-      const typographyUtilityClasses = [];
-      const otherUtilityClasses = [];
+      if (prop.$type === 'typography') {
+        // Typography tokens are handled differently, as each might have a different tokenType
+        return typographyUtilityClasses.push(generateTypographyOutput(prop, propName, false));
+      } else if (tokenCategory.startsWith('round') || tokenCategory.startsWith('rectangular') || tokenCategory.startsWith('soft')) {
+        // Generate utility classes for border-radius shape tokens, as they have their own token json file, based on primitive tokens
+        return otherUtilityClasses.push(generateRadiusUtilityClasses(propName));
+      }
 
-      // Generate utility classes for each token
-      dictionary.allTokens.map((prop) => {
+      let utilityClass = '';
+      switch (tokenCategory) {
+        case 'color':
+        case 'primitives':
+        case 'semantics':
+        case 'text':
+        case 'bg':
+        case 'icon':
+        case 'state':
+          utilityClass = generateColorUtilityClasses(prop, propName);
+          break;
+        case 'border':
+          utilityClass = generateBorderUtilityClasses(prop, propName);
+          break;
+        case 'font':
+          utilityClass = generateFontUtilityClasses(prop, propName);
+          break;
+        case 'space':
+          utilityClass = generateSpaceUtilityClasses(prop, propName);
+          break;
+        case 'shadow':
+        case 'elevation':
+          utilityClass = generateShadowUtilityClasses(propName);
+          break;
+        default:
+          utilityClass = generateUtilityClasses(tokenCategory, propName);
+      }
 
-        const tokenCategory = prop.attributes.category;
+      return otherUtilityClasses.push(utilityClass);
+    });
 
-        if (prop.$type === 'fontFamilies' || tokenCategory === 'scale' || tokenCategory === 'backdrop') {
-          // Not creating for the tokens below, as they make no sense to exist as utility-classes.
-          return;
-        }
+    const defaultSpaceUtilityClasses = generateDefaultSpaceUtilityClasses();
+    otherUtilityClasses.push(defaultSpaceUtilityClasses);
 
-        // Remove consecutive repeated words from the token name, like border-border-color
-        const propName = removeConsecutiveRepeatedWords(prop.name);
+    // Concatenate typography utility classes at the beginning
+    const finalOutput = typographyUtilityClasses.concat(otherUtilityClasses).join('\n');
 
-        if (prop.$type === 'typography') {
-          // Typography tokens are handled differently, as each might have a different tokenType
-          return typographyUtilityClasses.push(generateTypographyOutput(prop, propName, false));
-
-        } else if (tokenCategory.startsWith('round') || tokenCategory.startsWith('rectangular') || tokenCategory.startsWith('soft')) {
-          // Generate utility classes for border-radius shape tokens, as they have their own token json file, based on primitive tokens
-          return otherUtilityClasses.push(generateRadiusUtilityClasses(propName));
-        }
-
-        let utilityClass = '';
-        switch (tokenCategory) {
-          case 'color':
-          case 'primitives':
-          case 'semantics':
-          case 'text':
-          case 'bg':
-          case 'icon':
-          case 'state':
-            utilityClass = generateColorUtilityClasses(prop, propName);
-            break;
-          case 'border':
-            utilityClass = generateBorderUtilityClasses(prop, propName);
-            break;
-          case 'font':
-            utilityClass = generateFontUtilityClasses(prop, propName);
-            break;
-          case 'space':
-            utilityClass = generateSpaceUtilityClasses(prop, propName);
-            break;
-          case 'shadow':
-          case 'elevation':
-            utilityClass = generateShadowUtilityClasses(propName);
-            break;
-          default:
-            utilityClass = generateUtilityClasses(tokenCategory, propName);
-        }
-
-        return otherUtilityClasses.push(utilityClass);
-      });
-
-      const defaultSpaceUtilityClasses = generateDefaultSpaceUtilityClasses();
-      otherUtilityClasses.push(defaultSpaceUtilityClasses);
-
-      // Concatenate typography utility classes at the beginning
-      const finalOutput = typographyUtilityClasses.concat(otherUtilityClasses).join('\n');
-
-      const fileHeader = await file.options.fileHeader();
-
-      return [
-        `/*\n${fileHeader.join('\n')}\n*/`,
-        '@import "./ionic.vars";\n@import "../themes/mixins";\n',
-        finalOutput,
-      ].join('\n');
-    },
-  });
-
-})();
+    // In v3, the header is added automatically by Style Dictionary.
+    // The format function should only return the file content.
+    return [
+      '@import "./ionic.vars";\n@import "../themes/mixins";\n',
+      finalOutput,
+    ].join('');
+  },
+});
 
 // APPLY THE CONFIGURATION
-module.exports = {
+const config = {
   source: ["node_modules/outsystems-design-tokens/tokens/**/*.json"],
   platforms: {
     scss: {
@@ -199,3 +190,5 @@ module.exports = {
     }
   }
 };
+
+export default config;
