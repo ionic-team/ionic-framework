652cbf35c43a3bb4aed3d30ea42c8e5a3676ba39
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2025-10-07 17:23:22 -0400

    style: lint

--- core/src/themes/ios/dark.tokens.ts
+++ core/src/themes/ios/dark.tokens.ts
@@ -2,5 +2,5 @@ import { darkTheme as baseDarkTheme } from '../base/dark.tokens';
 import type { DarkTheme } from '../themes.interfaces';
 
 export const darkTheme: DarkTheme = {
-  ...baseDarkTheme
+  ...baseDarkTheme,
 };
--- core/src/themes/ios/light.tokens.ts
+++ core/src/themes/ios/light.tokens.ts
@@ -1,3 +1,3 @@
 import type { LightTheme } from '../themes.interfaces';
 
-export const lightTheme: LightTheme = { };
+export const lightTheme: LightTheme = {};
