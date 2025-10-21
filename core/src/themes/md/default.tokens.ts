a7699eabbe1241423f9fc43027aa116a15061492
Brandy Smith <brandyscarney@users.noreply.github.com>
2025-10-21 15:56:18 -0400

    feat(themes): add md theme tokens and update ionic theme to include semantic tokens (#30734)
    - Updates the theme tokens interface to add more numeric tokens & semantic tokens
    - Moves the `config` values into a separate `config` option which uses the `IonicConfig` interface
      - Adds a config option for `formHighlight` to `IonicConfig`
    - Defines default & dark tokens for the `md` theme
    - Removes the numeric tokens from the `ionic` theme and adds semantic tokens
    - Remove the numerous `--ion-font-family` overrides in favor of the tokens
    
    ---------
    
    Co-authored-by: Brandy Smith <6577830+brandyscarney@users.noreply.github.com>

--- core/src/themes/md/default.tokens.ts
+++ core/src/themes/md/default.tokens.ts
@@ -13,4 +13,67 @@ export const defaultTheme: DefaultTheme = {
     light: lightTheme,
     dark: darkTheme,
   },
+
+  config: {
+    formHighlight: true,
+    rippleEffect: true,
+  },
+
+  fontFamily: '"Roboto", "Helvetica Neue", sans-serif',
+
+  spacing: {
+    xxxxs: 'var(--ion-spacing-25)',
+    xxxs: 'var(--ion-spacing-50)',
+    xxs: 'var(--ion-spacing-100)',
+    xs: 'var(--ion-spacing-150)',
+    sm: 'var(--ion-spacing-200)',
+    md: 'var(--ion-spacing-300)',
+    lg: 'var(--ion-spacing-400)',
+    xl: 'var(--ion-spacing-500)',
+    xxl: 'var(--ion-spacing-600)',
+    xxxl: 'var(--ion-spacing-700)',
+    xxxxl: 'var(--ion-spacing-800)',
+  },
+
+  scaling: {
+    xxxxs: 'var(--ion-scaling-300)',
+    xxxs: 'var(--ion-scaling-400)',
+    xxs: 'var(--ion-scaling-500)',
+    xs: 'var(--ion-scaling-600)',
+    sm: 'var(--ion-scaling-700)',
+    md: 'var(--ion-scaling-800)',
+    lg: 'var(--ion-scaling-900)',
+    xl: 'var(--ion-scaling-1000)',
+    xxl: 'var(--ion-scaling-1100)',
+    xxxl: 'var(--ion-scaling-1200)',
+    xxxxl: 'var(--ion-scaling-1600)',
+  },
+
+  borderWidth: {
+    xxxxs: 'var(--ion-border-width-0)',
+    xxxs: 'var(--ion-border-width-25)',
+    xxs: 'var(--ion-border-width-50)',
+    xs: 'var(--ion-border-width-75)',
+    sm: 'var(--ion-border-width-100)',
+    md: 'var(--ion-border-width-150)',
+    lg: 'var(--ion-border-width-200)',
+    xl: 'var(--ion-border-width-250)',
+    xxl: 'var(--ion-border-width-300)',
+    xxxl: 'var(--ion-border-width-350)',
+    xxxxl: 'var(--ion-border-width-400)',
+  },
+
+  radii: {
+    xxxxs: 'var(--ion-radii-0)',
+    xxxs: 'var(--ion-radii-25)',
+    xxs: 'var(--ion-radii-50)',
+    xs: 'var(--ion-radii-75)',
+    sm: 'var(--ion-radii-100)',
+    md: 'var(--ion-radii-200)',
+    lg: 'var(--ion-radii-300)',
+    xl: 'var(--ion-radii-400)',
+    xxl: 'var(--ion-radii-700)',
+    xxxl: 'var(--ion-radii-900)',
+    xxxxl: 'var(--ion-radii-full)',
+  },
 };
