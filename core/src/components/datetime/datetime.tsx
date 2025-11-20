2699f54b85b86847c823323fb715b5b4ad424dd8
ShaneK <shane@shanessite.net>
2025-11-20 11:43:56 -0800

    chore(lint): fix lint

--- core/src/components/datetime/datetime.tsx
+++ core/src/components/datetime/datetime.tsx
@@ -1168,8 +1168,8 @@ export class Datetime implements ComponentInterface {
     raf(() => visibleIO?.observe(intersectionTrackerRef!));
 
     /**
-   * TODO(FW-6931): Remove this fallback upon solving the root cause
-   * Fallback: If IntersectionObserver never reports that the
+     * TODO(FW-6931): Remove this fallback upon solving the root cause
+     * Fallback: If IntersectionObserver never reports that the
      * datetime is visible but the host clearly has layout, ensure
      * we still initialize listeners and mark the component as ready.
      *
