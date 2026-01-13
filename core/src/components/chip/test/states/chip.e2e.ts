d830d48156118f518c6d8003fadca3b2ab33c16d
Maria Hutt <maria.hutt@outsystems.com>
2026-01-13 15:23:12 -0800

    test(chip): update states snapshots

--- core/src/components/chip/test/states/chip.e2e.ts
+++ core/src/components/chip/test/states/chip.e2e.ts
@@ -1,22 +1,64 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
-  /**
-   * This behavior does not vary across modes/directions.
-   */
+/**
+ * This behavior does not vary across directions.
+ * 
+ * `md` does not differ from `ios`.
+ */
+configs({ modes: ['ios', 'ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('chip: states'), () => {
     test('should render disabled state', async ({ page }) => {
       await page.setContent(
-        `<ion-chip disabled="true">
-          <ion-label>Disabled</ion-label>
-        </ion-chip>`,
+        `
+          // Adding margin so chips aren't overlapping
+          <style>
+            ion-chip {
+              margin: 4px 0;
+            }
+          </style>
+
+          <div id="container">
+            <ion-chip disabled="true" hue="subtle">
+              <ion-label>Disabled</ion-label>
+            </ion-chip>
+            <ion-chip disabled="true" hue="bold">
+              <ion-label>Disabled</ion-label>
+            </ion-chip>
+          </div>
+        `,
         config
       );
 
-      const chip = page.locator('ion-chip');
+      const container = page.locator('#container');
 
-      await expect(chip).toHaveScreenshot(screenshot(`chip-disabled`));
+      await expect(container).toHaveScreenshot(screenshot(`chip-disabled`));
+    });
+    test('should render focus state', async ({ page }) => {
+      await page.setContent(
+        `
+          // Adding margin so chips aren't overlapping
+          <style>
+            ion-chip {
+              margin: 4px 0;
+            }
+          </style>
+
+          <div id="container">
+            <ion-chip class="ion-focused" hue="subtle">
+              <ion-label>Focused</ion-label>
+            </ion-chip>
+            <ion-chip class="ion-focused" hue="bold">
+              <ion-label>Focused</ion-label>
+            </ion-chip>
+          </div>
+        `,
+        config
+      );
+
+      const container = page.locator('#container');
+
+      await expect(container).toHaveScreenshot(screenshot(`chip-focused`));
     });
     test('should custom chip', async ({ page }) => {
       await page.setContent(
@@ -38,28 +80,6 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, c
       const chip = page.locator('ion-chip');
 
       await expect(chip).toHaveScreenshot(screenshot(`chip-custom`));
-    });
-  });
-});
-
-configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
-  /**
-   * This behavior only applies to Ionic Theme.
-   */
-  test.describe(title('chip: focus state'), () => {
-    test('should render focus state', async ({ page }) => {
-      await page.setContent(
-        `<div id="container" class="ion-padding">
-          <ion-chip class="ion-focused">
-            <ion-label>Focused</ion-label>
-          </ion-chip>
-        </div>`,
-        config
-      );
-
-      const chip = page.locator('#container');
-
-      await expect(chip).toHaveScreenshot(screenshot(`chip-focused`));
     });
   });
 });
