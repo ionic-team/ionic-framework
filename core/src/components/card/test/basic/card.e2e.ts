28a4a34ec565621a1b18b8d1aef04991257805b1
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2026-01-16 16:25:41 -0500

    test(card): remove headings test and screenshots

--- core/src/components/card/test/basic/card.e2e.ts
+++ core/src/components/card/test/basic/card.e2e.ts
@@ -91,27 +91,6 @@ configs({ directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
       const card = page.locator('ion-card');
       await expect(card).toHaveScreenshot(screenshot(`card-color`));
     });
-    test('headings should have correct size in card', async ({ page }) => {
-      await page.setContent(
-        `
-        <ion-card>
-          <ion-card-content>
-            <h1>Heading 1</h1>
-            <h2>Heading 2</h2>
-            <h3>Heading 3</h3>
-            <h4>Heading 4</h4>
-            <h5>Heading 5</h5>
-            <h6>Heading 6</h6>
-            <p>Paragraph</p>
-          </ion-card-content>
-        </ion-card>
-      `,
-        config
-      );
-
-      const card = page.locator('ion-card');
-      await expect(card).toHaveScreenshot(screenshot(`card-headings`));
-    });
     test('should render even without header or content elements', async ({ page }) => {
       await page.setContent(
         `
