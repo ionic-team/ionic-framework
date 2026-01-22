25a7adf7ccac505072e95c73721dc849c33a0650
Maria Hutt <maria.hutt@outsystems.com>
2026-01-21 16:55:44 -0800

    test(chip): add to the states

--- core/src/components/chip/test/states/chip.e2e.ts
+++ core/src/components/chip/test/states/chip.e2e.ts
@@ -1,23 +1,30 @@
 import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
-configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, screenshot, config }) => {
+/**
+ * This behavior does not vary across modes/directions
+ */
+configs({ modes: ['ios'], directions: ['ltr'], palettes: ['dark', 'light'] }).forEach(({ title, screenshot, config }) => {
   /**
    * This behavior does not vary across modes/directions.
    */
   test.describe(title('chip: states'), () => {
     test('should render disabled state', async ({ page }) => {
-      await page.setContent(
-        `<ion-chip disabled="true">
-          <ion-label>Disabled</ion-label>
-        </ion-chip>`,
-        config
-      );
+      await page.goto(`/src/components/chip/test/states`, config);
 
-      const chip = page.locator('ion-chip');
+      const container = page.locator('#disabled');
 
-      await expect(chip).toHaveScreenshot(screenshot(`chip-disabled`));
+      await expect(container).toHaveScreenshot(screenshot(`chip-disabled`));
     });
+
+    test('should render activated state', async ({ page }) => {
+      await page.goto(`/src/components/chip/test/states`, config);
+
+      const container = page.locator('#activated');
+
+      await expect(container).toHaveScreenshot(screenshot(`chip-activated`));
+    });
+
     test('should custom chip', async ({ page }) => {
       await page.setContent(
         `
