b0d82fc398194c3f1703c6ad4bc2fc915c2c917c
Maria Hutt <maria.hutt@outsystems.com>
2026-01-23 10:29:35 -0800

    test(select-modal): update new tests to be "custom"

---
+++ core/src/components/select-modal/test/custom/select-modal.e2e.ts
@@ -0,0 +1,37 @@
+import { expect } from '@playwright/test';
+import { configs, test } from '@utils/test/playwright';
+
+import type { SelectModalOption } from '../../select-modal-interface';
+import { SelectModalPage } from '../fixtures';
+
+const options: SelectModalOption[] = [
+  { value: 'apple', text: 'Apple', disabled: false, checked: false },
+  { value: 'banana', text: 'Banana', disabled: false, checked: false },
+];
+
+/**
+ * This behavior does not vary across modes/directions.
+ */
+configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
+  test.describe(title('select-modal: custom'), () => {
+    let selectModalPage: SelectModalPage;
+
+    test.beforeEach(async ({ page }) => {
+      selectModalPage = new SelectModalPage(page);
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    test('should render custom cancel text when prop is provided', async ({ page: _page }, testInfo) => {
+      testInfo.annotations.push({
+        type: 'issue',
+        description: 'https://github.com/ionic-team/ionic-framework/issues/30295',
+      });
+
+      await selectModalPage.setup(config, options, false);
+
+      const cancelButton = selectModalPage.selectModal.locator('ion-button');
+
+      await expect(cancelButton).toHaveText('Close me');
+    });
+  });
+});
