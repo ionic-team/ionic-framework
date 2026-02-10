46806bd6e2af90a0b31fca68f508c06d3d281ec0
Brandy Smith <brandyscarney@users.noreply.github.com>
2026-02-10 09:59:24 -0500

    feat(segment-view): add swipeGesture property to disable swiping (#30948)
    Issue number: resolves #30290
    
    ---------
    
    ## What is the current behavior?
    The segment view swipe gesture can only be disabled by adding the `disabled` property and setting `opacity: 1`.
    
    ## What is the new behavior?
    - Adds a new property, `swipeGesture`, to disable swiping on the segment view
    - Adds an e2e test which verifies the styles blocking the swipe are correctly applied when `swipeGesture` is `false`
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No
    
    ---------
    
    Co-authored-by: Brandy Smith <6577830+brandyscarney@users.noreply.github.com>

--- packages/angular/src/directives/proxies.ts
+++ packages/angular/src/directives/proxies.ts
@@ -2113,14 +2113,14 @@ export declare interface IonSegmentContent extends Components.IonSegmentContent 
 
 
 @ProxyCmp({
-  inputs: ['disabled']
+  inputs: ['disabled', 'swipeGesture']
 })
 @Component({
   selector: 'ion-segment-view',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: '<ng-content></ng-content>',
   // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
-  inputs: ['disabled'],
+  inputs: ['disabled', 'swipeGesture'],
 })
 export class IonSegmentView {
   protected el: HTMLIonSegmentViewElement;
