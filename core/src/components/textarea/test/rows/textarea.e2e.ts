787dadf03d93ed96d9dc4d40244daf1ce156ba9b
Maria Hutt <thetaPC@users.noreply.github.com>
2026-02-18 13:25:42 -0800

    test(textarea): change text from small

--- core/src/components/textarea/test/rows/textarea.e2e.ts
+++ core/src/components/textarea/test/rows/textarea.e2e.ts
@@ -13,7 +13,7 @@ configs({ modes: ['ionic-md'], directions: ['ltr'] }).forEach(({ title, screensh
         <div id="container" style="display: flex; flex-direction: column; gap: 20px;">
           <ion-textarea
             rows="1"
-            label="Small"
+            label="Comments"
             value="1&#10;2&#10;3&#10;4&#10;5&#10;6&#10;7&#10;8&#10;9&#10;0"
           ></ion-textarea>
           <ion-textarea
