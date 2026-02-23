524469f96583e80b498fc13f8841536587aea136
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2026-02-23 15:22:00 -0500

    test(range): add tests for the dual knob lower and upper parts, restructure

--- core/src/components/range/test/custom/range.e2e.ts
+++ core/src/components/range/test/custom/range.e2e.ts
@@ -41,40 +41,6 @@ configs({ directions: ['ltr'], modes: ['md'] }).forEach(({ title, config }) => {
         expect(activeBarBackgroundColor).toBe('rgb(0, 128, 0)');
       });
 
-      test('should be able to customize tick parts', async ({ page }) => {
-        await page.setContent(
-          `
-          <style>
-            ion-range::part(tick) {
-              background-color: red;
-            }
-            ion-range::part(tick-active) {
-              background-color: green;
-            }
-          </style>
-
-          <ion-range label="Label" value="50" snaps="true" ticks="true"></ion-range>
-        `,
-          config
-        );
-
-        const range = page.locator('ion-range');
-
-        const tickBackgroundColor = await range.evaluate((el) => {
-          const shadowRoot = el.shadowRoot;
-          const tick = shadowRoot?.querySelector('.range-tick:not(.range-tick-active)');
-          return tick ? window.getComputedStyle(tick).backgroundColor : '';
-        });
-        expect(tickBackgroundColor).toBe('rgb(255, 0, 0)');
-
-        const activeTickBackgroundColor = await range.evaluate((el) => {
-          const shadowRoot = el.shadowRoot;
-          const activeTick = shadowRoot?.querySelector('.range-tick-active');
-          return activeTick ? window.getComputedStyle(activeTick).backgroundColor : '';
-        });
-        expect(activeTickBackgroundColor).toBe('rgb(0, 128, 0)');
-      });
-
       test('should be able to customize pin parts', async ({ page }) => {
         await page.setContent(
           `
@@ -110,6 +76,40 @@ configs({ directions: ['ltr'], modes: ['md'] }).forEach(({ title, config }) => {
         expect(pinBeforeBackgroundColor).toBe('rgb(0, 128, 0)');
       });
 
+      test('should be able to customize tick parts', async ({ page }) => {
+        await page.setContent(
+          `
+          <style>
+            ion-range::part(tick) {
+              background-color: red;
+            }
+            ion-range::part(tick-active) {
+              background-color: green;
+            }
+          </style>
+
+          <ion-range label="Label" value="50" snaps="true" ticks="true"></ion-range>
+        `,
+          config
+        );
+
+        const range = page.locator('ion-range');
+
+        const tickBackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const tick = shadowRoot?.querySelector('.range-tick:not(.range-tick-active)');
+          return tick ? window.getComputedStyle(tick).backgroundColor : '';
+        });
+        expect(tickBackgroundColor).toBe('rgb(255, 0, 0)');
+
+        const activeTickBackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const activeTick = shadowRoot?.querySelector('.range-tick-active');
+          return activeTick ? window.getComputedStyle(activeTick).backgroundColor : '';
+        });
+        expect(activeTickBackgroundColor).toBe('rgb(0, 128, 0)');
+      });
+
       test('should be able to customize knob parts', async ({ page }) => {
         await page.setContent(
           `
@@ -143,87 +143,174 @@ configs({ directions: ['ltr'], modes: ['md'] }).forEach(({ title, config }) => {
         });
         expect(knobBackgroundColor).toBe('rgb(0, 128, 0)');
       });
-    });
 
-    test('should be able to customize dual knob parts', async ({ page }) => {
-      await page.setContent(
-        `
-        <style>
-          ion-range::part(knob-handle-a) {
-            background-color: red;
-          }
-          ion-range::part(knob-handle-b) {
-            background-color: green;
-          }
-          ion-range::part(knob-a) {
-            background-color: blue;
-          }
-          ion-range::part(knob-b) {
-            background-color: yellow;
-          }
-          ion-range::part(pin-a) {
-            background-color: orange;
-          }
-          ion-range::part(pin-b) {
-            background-color: purple;
-          }
-        </style>
+      test('should be able to customize dual knob a & b parts', async ({ page }) => {
+        await page.setContent(
+          `
+          <style>
+            ion-range::part(knob-handle-a) {
+              background-color: red;
+            }
+            ion-range::part(knob-handle-b) {
+              background-color: green;
+            }
+            ion-range::part(knob-a) {
+              background-color: blue;
+            }
+            ion-range::part(knob-b) {
+              background-color: yellow;
+            }
+            ion-range::part(pin-a) {
+              background-color: orange;
+            }
+            ion-range::part(pin-b) {
+              background-color: purple;
+            }
+          </style>
 
-        <ion-range label="Label" dual-knobs="true" pin="true"></ion-range>
-      `,
-        config
-      );
+          <ion-range label="Label" dual-knobs="true" pin="true"></ion-range>
+        `,
+          config
+        );
 
-      const range = page.locator('ion-range');
-      await range.evaluate((el) => {
-        (el as any).value = {
-          lower: 25,
-          upper: 75,
-        };
+        const range = page.locator('ion-range');
+        await range.evaluate((el) => {
+          (el as any).value = {
+            lower: 25,
+            upper: 75,
+          };
+        });
+        await page.waitForChanges();
+
+        const knobHandleABackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const knobHandleA = shadowRoot?.querySelector('.range-knob-handle-a');
+          return knobHandleA ? window.getComputedStyle(knobHandleA).backgroundColor : '';
+        });
+        expect(knobHandleABackgroundColor).toBe('rgb(255, 0, 0)');
+
+        const knobHandleBBackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const knobHandleB = shadowRoot?.querySelector('.range-knob-handle-b');
+          return knobHandleB ? window.getComputedStyle(knobHandleB).backgroundColor : '';
+        });
+        expect(knobHandleBBackgroundColor).toBe('rgb(0, 128, 0)');
+
+        // We query for the knob inside knob-handle-a because the knob
+        // does not get a class for the knob name, only a part.
+        const knobABackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const knobA = shadowRoot?.querySelector('.range-knob-handle-a .range-knob');
+          return knobA ? window.getComputedStyle(knobA).backgroundColor : '';
+        });
+        expect(knobABackgroundColor).toBe('rgb(0, 0, 255)');
+
+        // We query for the knob inside knob-handle-b because the knob
+        // does not get a class for the knob name, only a part.
+        const knobBBackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const knobB = shadowRoot?.querySelector('.range-knob-handle-b .range-knob');
+          return knobB ? window.getComputedStyle(knobB).backgroundColor : '';
+        });
+        expect(knobBBackgroundColor).toBe('rgb(255, 255, 0)');
+
+        // We query for the pin inside knob-handle-a because the pin
+        // does not get a class for the knob name, only a part.
+        const pinABackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const pinA = shadowRoot?.querySelector('.range-knob-handle-a .range-pin');
+          return pinA ? window.getComputedStyle(pinA).backgroundColor : '';
+        });
+        expect(pinABackgroundColor).toBe('rgb(255, 165, 0)');
+
+        // We query for the pin inside knob-handle-b because the pin
+        // does not get a class for the knob name, only a part.
+        const pinBBackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const pinB = shadowRoot?.querySelector('.range-knob-handle-b .range-pin');
+          return pinB ? window.getComputedStyle(pinB).backgroundColor : '';
+        });
+        expect(pinBBackgroundColor).toBe('rgb(128, 0, 128)');
       });
-      await page.waitForChanges();
 
-      const knobHandleABackgroundColor = await range.evaluate((el) => {
-        const shadowRoot = el.shadowRoot;
-        const knobHandleA = shadowRoot?.querySelector('.range-knob-handle-a');
-        return knobHandleA ? window.getComputedStyle(knobHandleA).backgroundColor : '';
-      });
-      expect(knobHandleABackgroundColor).toBe('rgb(255, 0, 0)');
+      test('should be able to customize dual knob lower & upper parts', async ({ page }) => {
+        await page.setContent(
+          `
+          <style>
+            ion-range::part(knob-handle-lower) {
+              background-color: red;
+            }
+            ion-range::part(knob-handle-upper) {
+              background-color: green;
+            }
+            ion-range::part(knob-lower) {
+              background-color: blue;
+            }
+            ion-range::part(knob-upper) {
+              background-color: yellow;
+            }
+            ion-range::part(pin-lower) {
+              background-color: orange;
+            }
+            ion-range::part(pin-upper) {
+              background-color: purple;
+            }
+          </style>
 
-      const knobHandleBBackgroundColor = await range.evaluate((el) => {
-        const shadowRoot = el.shadowRoot;
-        const knobHandleB = shadowRoot?.querySelector('.range-knob-handle-b');
-        return knobHandleB ? window.getComputedStyle(knobHandleB).backgroundColor : '';
-      });
-      expect(knobHandleBBackgroundColor).toBe('rgb(0, 128, 0)');
+          <ion-range label="Label" dual-knobs="true" pin="true"></ion-range>
+        `,
+          config
+        );
 
-      const knobABackgroundColor = await range.evaluate((el) => {
-        const shadowRoot = el.shadowRoot;
-        const knobA = shadowRoot?.querySelector('.range-knob-handle-a .range-knob');
-        return knobA ? window.getComputedStyle(knobA).backgroundColor : '';
-      });
-      expect(knobABackgroundColor).toBe('rgb(0, 0, 255)');
+        const range = page.locator('ion-range');
+        await range.evaluate((el) => {
+          (el as any).value = {
+            lower: 25,
+            upper: 75,
+          };
+        });
+        await page.waitForChanges();
 
-      const knobBBackgroundColor = await range.evaluate((el) => {
-        const shadowRoot = el.shadowRoot;
-        const knobB = shadowRoot?.querySelector('.range-knob-handle-b .range-knob');
-        return knobB ? window.getComputedStyle(knobB).backgroundColor : '';
-      });
-      expect(knobBBackgroundColor).toBe('rgb(255, 255, 0)');
+        // Lower & upper are added as shadow parts but not CSS classes, so we
+        // have to query by their shadow parts.
+        const knobHandleLowerBackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const knobHandleLower = shadowRoot?.querySelector('[part~="knob-handle-lower"]');
+          return knobHandleLower ? window.getComputedStyle(knobHandleLower).backgroundColor : '';
+        });
+        expect(knobHandleLowerBackgroundColor).toBe('rgb(255, 0, 0)');
 
-      const pinABackgroundColor = await range.evaluate((el) => {
-        const shadowRoot = el.shadowRoot;
-        const pinA = shadowRoot?.querySelector('.range-knob-handle-a .range-pin');
-        return pinA ? window.getComputedStyle(pinA).backgroundColor : '';
-      });
-      expect(pinABackgroundColor).toBe('rgb(255, 165, 0)');
+        const knobHandleUpperBackgroundColor = await range.evaluate((el) => {
+          const shadowRoot = el.shadowRoot;
+          const knobHandleUpper = shadowRoot?.querySelector('[part~="knob-handle-upper"]');
+          return knobHandleUpper ? window.getComputedStyle(knobHandleUpper).backgroundColor : '';
+        });
+        expect(knobHandleUpperBackgroundColor).toBe('rgb(0, 128, 0)');
 
-      const pinBBackgroundColor = await range.evaluate((el) => {
-        const shadowRoot = el.shadowRoot;
-        const pinB = shadowRoot?.querySelector('.range-knob-handle-b .range-pin');
-        return pinB ? window.getComputedStyle(pinB).backgroundColor : '';
+        const knobLowerBackgroundColor = await range.evaluate((el) => {
+          const knobLower = el.shadowRoot?.querySelector('[part~="knob-lower"]');
+          return knobLower ? window.getComputedStyle(knobLower).backgroundColor : '';
+        });
+        expect(knobLowerBackgroundColor).toBe('rgb(0, 0, 255)');
+
+        const knobUpperBackgroundColor = await range.evaluate((el) => {
+          const knobUpper = el.shadowRoot?.querySelector('[part~="knob-upper"]');
+          return knobUpper ? window.getComputedStyle(knobUpper).backgroundColor : '';
+        });
+        expect(knobUpperBackgroundColor).toBe('rgb(255, 255, 0)');
+
+        const pinLowerBackgroundColor = await range.evaluate((el) => {
+          const pinLower = el.shadowRoot?.querySelector('[part~="pin-lower"]');
+          return pinLower ? window.getComputedStyle(pinLower).backgroundColor : '';
+        });
+        expect(pinLowerBackgroundColor).toBe('rgb(255, 165, 0)');
+
+        const pinUpperBackgroundColor = await range.evaluate((el) => {
+          const pinUpper = el.shadowRoot?.querySelector('[part~="pin-upper"]');
+          return pinUpper ? window.getComputedStyle(pinUpper).backgroundColor : '';
+        });
+        expect(pinUpperBackgroundColor).toBe('rgb(128, 0, 128)');
       });
-      expect(pinBBackgroundColor).toBe('rgb(128, 0, 128)');
     });
   });
 });
