0b73f03000365f094591e21bb88caf8efca878e3
Maria Hutt <maria.hutt@outsystems.com>
2026-02-27 14:40:58 -0800

    feat(modal): add drag events to Angular common

--- packages/angular/common/src/overlays/modal.ts
+++ packages/angular/common/src/overlays/modal.ts
@@ -7,7 +7,7 @@ import {
   NgZone,
   TemplateRef,
 } from '@angular/core';
-import type { Components, ModalBreakpointChangeEventDetail } from '@ionic/core/components';
+import type { Components, ModalBreakpointChangeEventDetail, ModalDragEventDetail } from '@ionic/core/components';
 
 import { ProxyCmp, proxyOutputs } from '../utils/proxy';
 
@@ -32,6 +32,18 @@ export declare interface IonModal extends Components.IonModal {
    * Emitted after the modal breakpoint has changed.
    */
   ionBreakpointDidChange: EventEmitter<CustomEvent<ModalBreakpointChangeEventDetail>>;
+  /**
+   * Emitted when the sheet or card modal has started being dragged.
+   */
+  ionDragStart: EventEmitter<CustomEvent<ModalDragEventDetail>>;
+  /**
+   * Emitted while the sheet or card modal is being dragged.
+   */
+  ionDragMove: EventEmitter<CustomEvent<ModalDragEventDetail>>;
+  /**
+   * Emitted when the sheet or card modal has finished being dragged.
+   */
+  ionDragEnd: EventEmitter<CustomEvent<ModalDragEventDetail>>;
   /**
    * Emitted after the modal has presented. Shorthand for ionModalDidPresent.
    */
