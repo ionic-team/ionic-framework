78cfbba78cf8d929694277d3ba32370f50fdddea
Maria Hutt <maria.hutt@outsystems.com>
2026-03-17 15:41:20 -0700

    test(spinner): update resize

--- core/src/components/spinner/test/resize/spinner.e2e.ts
+++ core/src/components/spinner/test/resize/spinner.e2e.ts
@@ -9,25 +9,31 @@ configs({ directions: ['ltr'], modes: ['md'] }).forEach(({ title, screenshot, co
     test.beforeEach(async ({ page }) => {
       await page.setViewportSize({ width: 320, height: 340 });
     });
-    test('should not have visual regressions', async ({ page }) => {
+    test('should not have visual regressions', async ({ page }, testInfo) => {
+      testInfo.annotations.push({
+        type: 'issue',
+        description: 'https://github.com/ionic-team/ionic-framework/issues/18115',
+      });
+
       await page.setContent(
         `
-        <style>
-          ion-spinner {
-            width: 100px;
-            height: 100px;
-          }
-        </style>
-        <ion-spinner name="lines"></ion-spinner>
-        <ion-spinner name="lines-small"></ion-spinner>
-        <ion-spinner name="lines-sharp"></ion-spinner>
-        <ion-spinner name="lines-sharp-small"></ion-spinner>
-        <ion-spinner name="circular"></ion-spinner>
-        <ion-spinner name="dots"></ion-spinner>
-        <ion-spinner name="bubbles"></ion-spinner>
-        <ion-spinner name="circles"></ion-spinner>
-        <ion-spinner name="crescent"></ion-spinner>
-      `,
+          <style>
+            ion-spinner.spinner-medium {
+              --ion-spinner-size-medium-width: 100px;
+              --ion-spinner-size-medium-height: 100px;
+            }
+          </style>
+
+          <ion-spinner size="medium" name="lines"></ion-spinner>
+          <ion-spinner size="medium" name="lines-small"></ion-spinner>
+          <ion-spinner size="medium" name="lines-sharp"></ion-spinner>
+          <ion-spinner size="medium" name="lines-sharp-small"></ion-spinner>
+          <ion-spinner size="medium" name="circular"></ion-spinner>
+          <ion-spinner size="medium" name="dots"></ion-spinner>
+          <ion-spinner size="medium" name="bubbles"></ion-spinner>
+          <ion-spinner size="medium" name="circles"></ion-spinner>
+          <ion-spinner size="medium" name="crescent"></ion-spinner>
+        `,
         config
       );
 
