ec6412469f5fac76203ed4b87ffe1ea8e822cc9d
Maria Hutt <maria.hutt@outsystems.com>
2026-04-21 09:25:15 -0700

    test(button): update layout

--- core/src/components/button/test/badge/badge.e2e.ts
+++ core/src/components/button/test/badge/badge.e2e.ts
@@ -11,26 +11,32 @@ configs({ directions: ['ltr'], modes: ['md', 'ios', 'ionic-md'] }).forEach(({ co
 
         const iconButtons = positions
           .flatMap((position) =>
-            badgeSizes.flatMap((badgeSize) =>
-              contents.map(
-                (html) => `<ion-button size="${buttonSize}" shape="round">
-                <ion-icon slot="icon-only" name="add"></ion-icon>
-                <ion-badge hue="bold" shape="round" color="danger" size="${badgeSize}" vertical="${position}">${html}</ion-badge>
-              </ion-button>`
-              )
+            contents.map(
+              (html) =>
+                `<div class="row">${badgeSizes
+                  .map(
+                    (badgeSize) => `<ion-button size="${buttonSize}" shape="round">
+                  <ion-icon slot="icon-only" name="add"></ion-icon>
+                  <ion-badge hue="bold" shape="round" color="danger" size="${badgeSize}" vertical="${position}">${html}</ion-badge>
+                </ion-button>`
+                  )
+                  .join('\n')}</div>`
             )
           )
           .join('\n');
 
         const textButtons = positions
           .flatMap((position) =>
-            badgeSizes.flatMap((badgeSize) =>
-              contents.map(
-                (html) => `<ion-button size="${buttonSize}" shape="round">
-                Button
-                <ion-badge hue="bold" shape="round" color="danger" size="${badgeSize}" vertical="${position}">${html}</ion-badge>
-              </ion-button>`
-              )
+            contents.map(
+              (html) =>
+                `<div class="row">${badgeSizes
+                  .map(
+                    (badgeSize) => `<ion-button size="${buttonSize}" shape="round">
+                  Button
+                  <ion-badge hue="bold" shape="round" color="danger" size="${badgeSize}" vertical="${position}">${html}</ion-badge>
+                </ion-button>`
+                  )
+                  .join('\n')}</div>`
             )
           )
           .join('\n');
@@ -38,19 +44,19 @@ configs({ directions: ['ltr'], modes: ['md', 'ios', 'ionic-md'] }).forEach(({ co
         await page.setContent(
           `
             <style>
-              /* Prevent badges from overlapping */
-              ion-button {
-                margin-inline-end: 40px;
-              }
-
-              #container > div {
+              .row {
+                display: flex;
+                flex-wrap: wrap;
+                align-items: flex-start;
                 padding-bottom: 10px;
+                row-gap: 5px;
+                column-gap: 30px;
               }
             </style>
 
             <div id="container">
-              <div>${iconButtons}</div>
-              <div>${textButtons}</div>
+              ${iconButtons}
+              ${textButtons}
             </div>
           `,
           config
@@ -59,15 +65,12 @@ configs({ directions: ['ltr'], modes: ['md', 'ios', 'ionic-md'] }).forEach(({ co
         const container = page.locator('#container');
 
         /**
-         * Large buttons overflow the default viewport, causing
+         * Buttons overflow the default viewport, causing
          * unrendered areas to appear black in the screenshot.
          * Resizing to fit content.
          */
-        if (buttonSize === 'large' || buttonSize === 'medium') {
-          const box = await container.boundingBox();
-
-          await page.setViewportSize({ width: Math.ceil(box!.width) + 32, height: Math.ceil(box!.height) + 32 });
-        }
+        const box = await container.boundingBox();
+        await page.setViewportSize({ width: Math.ceil(box!.width) + 40, height: Math.ceil(box!.height) + 32 });
 
         await expect(container).toHaveScreenshot(screenshot(`button-${buttonSize}-badge`));
       });
