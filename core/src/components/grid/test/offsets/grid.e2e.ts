b215bb30849162512378c81ce9a054ecea5639b4
Maria Hutt <thetaPC@users.noreply.github.com>
2026-06-04 16:26:03 -0700

    test(grid): split offsets

--- core/src/components/grid/test/offsets/grid.e2e.ts
+++ core/src/components/grid/test/offsets/grid.e2e.ts
@@ -6,14 +6,34 @@ import { configs, test } from '@utils/test/playwright';
  */
 configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('grid: offsets'), () => {
-    test('should not have visual regressions', async ({ page }) => {
+    test.beforeEach(async ({ page }) => {
       await page.goto(`/src/components/grid/test/offsets`, config);
 
       await page.setIonViewport();
+    });
 
-      const grids = page.locator('#grids');
+    test('should not have visual regressions for offset', async ({ page }) => {
+      const offset = page.locator('#offset-1');
 
-      await expect(grids).toHaveScreenshot(screenshot('grid-offsets'));
+      await expect(offset).toHaveScreenshot(screenshot('grid-offsets-offset'));
+    });
+
+    test('should not have visual regressions for order', async ({ page }) => {
+      const order = page.locator('#order');
+
+      await expect(order).toHaveScreenshot(screenshot('grid-offsets-order'));
+    });
+
+    test('should not have visual regressions for a larger offset', async ({ page }) => {
+      const offset = page.locator('#offset-2');
+
+      await expect(offset).toHaveScreenshot(screenshot('grid-offsets-larger-offset'));
+    });
+
+    test('should not have visual regressions for dynamic offset', async ({ page }) => {
+      const dynamicOffset = page.locator('#dynamic-offset');
+
+      await expect(dynamicOffset).toHaveScreenshot(screenshot('grid-offsets-dynamic-offset'));
     });
   });
 });
