789fb43d42016abbea1daf0a1fca2d2aa92f6d6e
Maria Hutt <thetaPC@users.noreply.github.com>
2026-06-04 16:16:33 -0700

    test(grid): split sizes

--- core/src/components/grid/test/sizes/grid.e2e.ts
+++ core/src/components/grid/test/sizes/grid.e2e.ts
@@ -6,14 +6,34 @@ import { configs, test } from '@utils/test/playwright';
  */
 configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
   test.describe(title('grid: sizes'), () => {
-    test('should not have visual regressions', async ({ page }) => {
+    test.beforeEach(async ({ page }) => {
       await page.goto(`/src/components/grid/test/sizes`, config);
 
       await page.setIonViewport();
+    });
 
-      const content = page.locator('ion-content');
+    test('should not have visual regressions for a 10 column layout', async ({ page }) => {
+      const grid = page.locator('#ten-column-layout');
 
-      await expect(content).toHaveScreenshot(screenshot('grid-sizes'));
+      await expect(grid).toHaveScreenshot(screenshot('grid-sizes-ten-column-layout'));
+    });
+
+    test('should not have visual regressions for responsive sm columns', async ({ page }) => {
+      const grid = page.locator('#responsive-sm');
+
+      await expect(grid).toHaveScreenshot(screenshot('grid-sizes-responsive-sm'));
+    });
+
+    test('should not have visual regressions for auto sized columns', async ({ page }) => {
+      const grid = page.locator('#size-auto');
+
+      await expect(grid).toHaveScreenshot(screenshot('grid-sizes-size-auto'));
+    });
+
+    test('should not have visual regressions for breakpoint sizes', async ({ page }) => {
+      const breakpointSizes = page.locator('#breakpoint-sizes');
+
+      await expect(breakpointSizes).toHaveScreenshot(screenshot('grid-sizes-breakpoint-sizes'));
     });
   });
 });
