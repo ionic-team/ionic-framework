378fb7614c82bae2b4ae8ec9eb343aaaf9f80349
Maria Hutt <thetaPC@users.noreply.github.com>
2026-06-04 15:55:35 -0700

    test(grid): rename test

--- core/src/components/grid/test/offsets-pull-push/grid.e2e.ts
+++ core/src/components/grid/test/offsets-pull-push/grid.e2e.ts
@@ -6,13 +6,13 @@ import { configs, test } from '@utils/test/playwright';
  * ion-grid does not have different styling per-mode
  */
 configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
-  test.describe(title('grid: offsets'), () => {
+  test.describe(title('grid: pull/push'), () => {
     test('should not have visual regressions', async ({ page }) => {
       await page.goto(`/src/components/grid/test/offsets-pull-push`, config);
 
       await page.setIonViewport();
 
-      await expect(page).toHaveScreenshot(screenshot('grid-offsets'));
+      await expect(page).toHaveScreenshot(screenshot('grid-pull-push'));
     });
   });
 });
