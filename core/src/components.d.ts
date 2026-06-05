1df0280a20f1986f68a7b66806177c9b470a784f
Maria Hutt <thetaPC@users.noreply.github.com>
2026-06-05 16:00:14 -0700

    fix(select): set modal interface cancel button to "Okay" by default

--- core/src/components.d.ts
+++ core/src/components.d.ts
@@ -3022,10 +3022,9 @@ export namespace Components {
     }
     interface IonSelect {
         /**
-          * The text to display on the cancel button.
-          * @default 'Cancel'
+          * The text to display on the cancel button. Defaults to `'Cancel'` for the `alert` and `action-sheet` interfaces, and `'Okay'` for the `modal` interface.
          */
-        "cancelText": string;
+        "cancelText"?: string;
         /**
           * The color to use from your application's color palette. Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`. For more information on colors, see [theming](/docs/theming/basics).  This property is only available when using the modern select syntax.
          */
@@ -8205,8 +8204,7 @@ declare namespace LocalJSX {
     }
     interface IonSelect {
         /**
-          * The text to display on the cancel button.
-          * @default 'Cancel'
+          * The text to display on the cancel button. Defaults to `'Cancel'` for the `alert` and `action-sheet` interfaces, and `'Okay'` for the `modal` interface.
          */
         "cancelText"?: string;
         /**
