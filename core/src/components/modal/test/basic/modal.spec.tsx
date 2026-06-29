4d29b27fcd8a81466a9329066d950ba324ae2a0e
Austin-Spriggs <aspriggs@madisoncollege.edu>
2026-06-29 06:57:20 -0500

    fix: Test for the recalculated content dimensions on a Modal

--- core/src/components/modal/test/basic/modal.spec.tsx
+++ core/src/components/modal/test/basic/modal.spec.tsx
@@ -2,6 +2,7 @@ import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
 
 import { Modal } from '../../modal';
+import { Content } from '../../../content/content'
 import { FOCUS_TRAP_DISABLE_CLASS } from '@utils/overlays';
 
 describe('modal: htmlAttributes inheritance', () => {
@@ -37,5 +38,29 @@ describe('modal: focus trap', () => {
     const modal = page.body.querySelector('ion-modal')!;
 
     expect(modal.classList.contains(FOCUS_TRAP_DISABLE_CLASS)).toBe(false);
+  });
+});
+
+describe('modal: content dimensions', () => {
+  it('should recalculate ion-content dimensions before presenting', async () => {
+    const page = await newSpecPage({
+      components: [Content, Modal],
+      template: () => (
+        <ion-modal overlayIndex={1} animated={false}>
+          <ion-content color="dark">Test Content</ion-content>
+        </ion-modal>
+      ),
+    });
+
+    const modal = page.body.querySelector('ion-modal')!;
+
+    const recalculateSpy = jest
+      .spyOn(Content.prototype, 'recalculateDimensions')
+      .mockResolvedValue(undefined);
+
+    await modal.present();
+    await page.waitForChanges();
+
+    expect(recalculateSpy).toHaveBeenCalledTimes(1);
   });
 });
