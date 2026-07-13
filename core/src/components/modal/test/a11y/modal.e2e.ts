2d3cb96969e20d46aa932a18c51d1b314f94a8ec
Gonçalo M. <goncalo.martins@outsystems.com>
2026-07-13 22:26:13 +0100

    Update core/src/components/modal/test/a11y/modal.e2e.ts
    Co-authored-by: Brandy Smith <brandyscarney@users.noreply.github.com>

--- core/src/components/modal/test/a11y/modal.e2e.ts
+++ core/src/components/modal/test/a11y/modal.e2e.ts
@@ -59,7 +59,7 @@ configs({ modes: ['ios'], directions: ['ltr'] }).forEach(({ title, config }) => 
     test('should focus the sheet modal wrapper on present', async ({ page }, testInfo) => {
       testInfo.annotations.push({
         type: 'issue',
-        description: 'modal content is inaccessible when TalkBack is enabled',
+        description: 'FW-7611',
       });
       await page.setContent(
         `
