9ef4d4b94fda29564da707eac02c64d6b12a9a1e
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2026-07-30 17:18:58 -0400

    test(input): capture slot behavior when value is not present

--- core/src/components/input/test/slot/input.e2e.ts
+++ core/src/components/input/test/slot/input.e2e.ts
@@ -6,7 +6,7 @@ configs().forEach(({ title, screenshot, config }) => {
     test('should not have visual regressions with a start-positioned label', async ({ page }) => {
       await page.setContent(
         `
-          <ion-input label-placement="start" value="100" label="Weight" clear-input="true">
+          <ion-input label-placement="start" label="Weight" clear-input="true">
             <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
             <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
             <ion-button slot="end" aria-label="Show/hide password">
@@ -24,7 +24,49 @@ configs().forEach(({ title, screenshot, config }) => {
       await expect(input).toHaveScreenshot(screenshot(`input-slot-label-start`));
     });
 
+    test('should not have visual regressions with a start-positioned label when value is present', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-input label-placement="start" value="100" label="Weight" clear-input="true">
+            <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+            <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+            <ion-button slot="end" aria-label="Show/hide password">
+              <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+            </ion-button>
+            <ion-button slot="end">
+              <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+            </ion-button>
+          </ion-input>
+        `,
+        config
+      );
+
+      const input = page.locator('ion-input');
+      await expect(input).toHaveScreenshot(screenshot(`input-slot-label-start-value`));
+    });
+
     test('should not have visual regressions with a floating label', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-input label-placement="floating" label="Weight" clear-input="true">
+            <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+            <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+            <ion-button slot="end" aria-label="Show/hide password">
+              <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+            </ion-button>
+            <ion-button slot="end">
+              <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+            </ion-button>
+          </ion-input>
+        `,
+        config
+      );
+
+      const input = page.locator('ion-input');
+      await expect(input).toHaveScreenshot(screenshot(`input-slot-label-floating`));
+    });
+
+    test('should not have visual regressions with a floating label when value is present', async ({ page }) => {
       await page.setContent(
         `
           <ion-input label-placement="floating" value="100" label="Weight" clear-input="true">
@@ -42,7 +84,7 @@ configs().forEach(({ title, screenshot, config }) => {
       );
 
       const input = page.locator('ion-input');
-      await expect(input).toHaveScreenshot(screenshot(`input-slot-label-floating`));
+      await expect(input).toHaveScreenshot(screenshot(`input-slot-label-floating-value`));
     });
   });
 });
@@ -55,6 +97,36 @@ configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
     ['solid', 'outline'].forEach((fill) => {
       test.describe(`fill: ${fill}`, () => {
         test('should not have visual regressions with a start-positioned label', async ({ page }) => {
+          await page.setContent(
+            `
+              <!-- Apply container styles to capture the entire input -->
+              <style>
+                .container {
+                  padding: 8px;
+                }
+              </style>
+
+              <div class="container">
+                <ion-input label-placement="start" fill="${fill}" label="Weight" clear-input="true">
+                  <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+                  <ion-button slot="end" aria-label="Show/hide password">
+                    <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                  <ion-button slot="end">
+                    <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                </ion-input>
+              </div>
+            `,
+            config
+          );
+
+          const container = page.locator('.container');
+          await expect(container).toHaveScreenshot(screenshot(`input-slot-fill-${fill}-label-start`));
+        });
+
+        test('should not have visual regressions with a start-positioned label when value is present', async ({ page }) => {
           await page.setContent(
             `
               <!-- Apply container styles to capture the entire input -->
@@ -81,10 +153,40 @@ configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
           );
 
           const container = page.locator('.container');
-          await expect(container).toHaveScreenshot(screenshot(`input-slot-fill-${fill}-label-start`));
+          await expect(container).toHaveScreenshot(screenshot(`input-slot-fill-${fill}-label-start-value`));
         });
 
         test('should not have visual regressions with a floating label', async ({ page }) => {
+          await page.setContent(
+            `
+              <!-- Apply container styles to capture the entire input -->
+              <style>
+                .container {
+                  padding: 8px;
+                }
+              </style>
+
+              <div class="container">
+                <ion-input label-placement="floating" fill="${fill}" label="Weight" clear-input="true">
+                  <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+                  <ion-button slot="end" aria-label="Show/hide password">
+                    <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                  <ion-button slot="end">
+                    <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                </ion-input>
+              </div>
+            `,
+            config
+          );
+
+          const container = page.locator('.container');
+          await expect(container).toHaveScreenshot(screenshot(`input-slot-fill-${fill}-label-floating`));
+        });
+
+        test('should not have visual regressions with a floating label when value is present', async ({ page }) => {
           await page.setContent(
             `
               <!-- Apply container styles to capture the entire input -->
@@ -111,7 +213,7 @@ configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
           );
 
           const container = page.locator('.container');
-          await expect(container).toHaveScreenshot(screenshot(`input-slot-fill-${fill}-label-floating`));
+          await expect(container).toHaveScreenshot(screenshot(`input-slot-fill-${fill}-label-floating-value`));
         });
       });
     });
