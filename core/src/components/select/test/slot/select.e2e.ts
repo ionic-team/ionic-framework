657ba03d0dda0df4bf7595a7d70e698a1b19acbd
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2026-08-05 13:37:31 -0400

    test(select): rename tests and add fill and rtl coverage

--- core/src/components/select/test/slot/select.e2e.ts
+++ core/src/components/select/test/slot/select.e2e.ts
@@ -2,13 +2,16 @@ import { expect } from '@playwright/test';
 import { configs, test } from '@utils/test/playwright';
 
 configs().forEach(({ title, screenshot, config }) => {
-  test.describe(title('select: start and end slots (visual checks)'), () => {
+  test.describe(title('select: slot'), () => {
     test('should not have visual regressions with a start-positioned label', async ({ page }) => {
       await page.setContent(
         `
-          <ion-select label-placement="start" fill="solid" placeholder="Select weight" label="Weight">
+          <ion-select label-placement="start" placeholder="Select weight" label="Weight">
             <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
             <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+            <ion-select-option value="100">100</ion-select-option>
+            <ion-select-option value="200">200</ion-select-option>
+            <ion-select-option value="300">300</ion-select-option>
             <ion-button slot="end" aria-label="Show/hide password">
               <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
             </ion-button>
@@ -21,15 +24,42 @@ configs().forEach(({ title, screenshot, config }) => {
       );
 
       const select = page.locator('ion-select');
-      await expect(select).toHaveScreenshot(screenshot(`select-slots-label-start`));
+      await expect(select).toHaveScreenshot(screenshot(`select-slot-label-start`));
+    });
+
+    test('should not have visual regressions with a start-positioned label when value is present', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-select label-placement="start" placeholder="Select weight" value="100" label="Weight">
+            <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+            <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+            <ion-select-option value="100">100</ion-select-option>
+            <ion-select-option value="200">200</ion-select-option>
+            <ion-select-option value="300">300</ion-select-option>
+            <ion-button slot="end" aria-label="Show/hide password">
+              <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+            </ion-button>
+            <ion-button slot="end">
+              <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+            </ion-button>
+          </ion-select>
+        `,
+        config
+      );
+
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(screenshot(`select-slot-label-start-value`));
     });
 
     test('should not have visual regressions with a floating label', async ({ page }) => {
       await page.setContent(
         `
-          <ion-select label-placement="floating" fill="solid" placeholder="Select weight" label="Weight">
+          <ion-select label-placement="floating" placeholder="Select weight" label="Weight">
             <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
             <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+            <ion-select-option value="100">100</ion-select-option>
+            <ion-select-option value="200">200</ion-select-option>
+            <ion-select-option value="300">300</ion-select-option>
             <ion-button slot="end" aria-label="Show/hide password">
               <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
             </ion-button>
@@ -42,61 +72,253 @@ configs().forEach(({ title, screenshot, config }) => {
       );
 
       const select = page.locator('ion-select');
-      await expect(select).toHaveScreenshot(screenshot(`select-slots-label-floating`));
+      await expect(select).toHaveScreenshot(screenshot(`select-slot-label-floating`));
     });
-  });
-});
 
-configs({ modes: ['md'], directions: ['ltr'] }).forEach(({ title, config }) => {
-  test.describe(title('select: start and end slots (functionality checks)'), () => {
-    test('should raise floating label when there is content in the start slot', async ({ page }) => {
+    /**
+     * The placeholder is not displayed for floating labels unless
+     * the select is expanded. This captures the expanded state
+     * where the placeholder is visible.
+     */
+    test('should not have visual regressions with a floating label when expanded', async ({ page }) => {
+      test.info().annotations.push({
+        type: 'issue',
+        description: 'https://github.com/ionic-team/ionic-framework/issues/30402',
+      });
+
       await page.setContent(
         `
-          <ion-select label-placement="floating" fill="solid" label="Weight">
+          <ion-select label-placement="floating" placeholder="Select weight" label="Weight" class="select-expanded label-floating">
             <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
-          </ion-select>
-        `,
-        config
-      );
-
-      const select = page.locator('ion-select');
-      await expect(select).toHaveClass(/label-floating/);
-    });
-
-    test('should raise floating label when there is content in the end slot', async ({ page }) => {
-      await page.setContent(
-        `
-          <ion-select label-placement="floating" fill="solid" label="Weight">
-            <ion-icon slot="end" name="barbell" aria-hidden="true"></ion-icon>
-          </ion-select>
-        `,
-        config
-      );
-
-      const select = page.locator('ion-select');
-      await expect(select).toHaveClass(/label-floating/);
-    });
-
-    test('should not open select when slotted buttons are clicked', async ({ page }) => {
-      await page.setContent(
-        `
-          <ion-select label="Favorite Pizza" placeholder="Select a pizza">
-            <ion-select-option value="pepperoni">Pepperoni</ion-select-option>
-            <ion-select-option value="supreme">Supreme</ion-select-option>
-            <ion-select-option value="chicken">Chicken</ion-select-option>
-            <ion-button fill="clear" slot="end" aria-label="Show/hide password">
-              <ion-icon slot="icon-only" name="eye" aria-hidden="true"></ion-icon>
+            <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+            <ion-select-option value="100">100</ion-select-option>
+            <ion-select-option value="200">200</ion-select-option>
+            <ion-select-option value="300">300</ion-select-option>
+            <ion-button slot="end" aria-label="Show/hide password">
+              <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+            </ion-button>
+            <ion-button slot="end">
+              <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
             </ion-button>
           </ion-select>
         `,
         config
       );
 
-      await page.click('ion-select ion-button[slot="end"]');
-      await page.waitForChanges();
+      const select = page.locator('ion-select');
+      await expect(select).toHaveScreenshot(screenshot(`select-slot-label-floating-expanded`));
+    });
+
+    test('should not have visual regressions with a floating label when value is present', async ({ page }) => {
+      await page.setContent(
+        `
+          <ion-select label-placement="floating" placeholder="Select weight" value="100" label="Weight">
+            <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+            <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+            <ion-select-option value="100">100</ion-select-option>
+            <ion-select-option value="200">200</ion-select-option>
+            <ion-select-option value="300">300</ion-select-option>
+            <ion-button slot="end" aria-label="Show/hide password">
+              <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+            </ion-button>
+            <ion-button slot="end">
+              <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+            </ion-button>
+          </ion-select>
+        `,
+        config
+      );
 
       const select = page.locator('ion-select');
-      await expect(select).not.toHaveClass(/select-expanded/);
+      await expect(select).toHaveScreenshot(screenshot(`select-slot-label-floating`));
+    });
+  });
+});
+
+/**
+ * The solid and outline fills are only supported by `md` mode.
+ */
+configs({ modes: ['md'] }).forEach(({ title, screenshot, config }) => {
+  test.describe(title('select: slot'), () => {
+    ['solid', 'outline'].forEach((fill) => {
+      test.describe(`fill: ${fill}`, () => {
+        test('should not have visual regressions with a start-positioned label', async ({ page }) => {
+          await page.setContent(
+            `
+              <!-- Apply container styles to capture the entire select -->
+              <style>
+                .container {
+                  padding: 8px;
+                }
+              </style>
+
+              <div class="container">
+                <ion-select label-placement="start" fill="${fill}" placeholder="Select weight" label="Weight">
+                  <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+                  <ion-select-option value="100">100</ion-select-option>
+                  <ion-select-option value="200">200</ion-select-option>
+                  <ion-select-option value="300">300</ion-select-option>
+                  <ion-button slot="end" aria-label="Show/hide password">
+                    <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                  <ion-button slot="end">
+                    <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                </ion-select>
+              </div>
+            `,
+            config
+          );
+
+          const container = page.locator('.container');
+          await expect(container).toHaveScreenshot(screenshot(`select-slot-fill-${fill}-label-start`));
+        });
+
+        test('should not have visual regressions with a start-positioned label when value is present', async ({
+          page,
+        }) => {
+          await page.setContent(
+            `
+              <!-- Apply container styles to capture the entire select -->
+              <style>
+                .container {
+                  padding: 8px;
+                }
+              </style>
+
+              <div class="container">
+                <ion-select label-placement="start" fill="${fill}" placeholder="Select weight" value="100" label="Weight">
+                  <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+                  <ion-select-option value="100">100</ion-select-option>
+                  <ion-select-option value="200">200</ion-select-option>
+                  <ion-select-option value="300">300</ion-select-option>
+                  <ion-button slot="end" aria-label="Show/hide password">
+                    <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                  <ion-button slot="end">
+                    <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                </ion-select>
+              </div>
+            `,
+            config
+          );
+
+          const container = page.locator('.container');
+          await expect(container).toHaveScreenshot(screenshot(`select-slot-fill-${fill}-label-start-value`));
+        });
+
+        test('should not have visual regressions with a floating label', async ({ page }) => {
+          await page.setContent(
+            `
+              <!-- Apply container styles to capture the entire select -->
+              <style>
+                .container {
+                  padding: 8px;
+                }
+              </style>
+
+              <div class="container">
+                <ion-select label-placement="floating" fill="${fill}" placeholder="Select weight" label="Weight">
+                  <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+                  <ion-select-option value="100">100</ion-select-option>
+                  <ion-select-option value="200">200</ion-select-option>
+                  <ion-select-option value="300">300</ion-select-option>
+                  <ion-button slot="end" aria-label="Show/hide password">
+                    <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                  <ion-button slot="end">
+                    <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                </ion-select>
+              </div>
+            `,
+            config
+          );
+
+          const container = page.locator('.container');
+          await expect(container).toHaveScreenshot(screenshot(`select-slot-fill-${fill}-label-floating`));
+        });
+
+        /**
+         * The placeholder is not displayed for floating labels unless
+         * the select is expanded. This captures the expanded state
+         * where the placeholder is visible.
+         */
+        test('should not have visual regressions with a floating label when expanded', async ({ page }) => {
+          test.info().annotations.push({
+            type: 'issue',
+            description: 'https://github.com/ionic-team/ionic-framework/issues/30402',
+          });
+
+          await page.setContent(
+            `
+              <!-- Apply container styles to capture the entire select -->
+              <style>
+                .container {
+                  padding: 8px;
+                }
+              </style>
+
+              <div class="container">
+                <ion-select label-placement="floating" fill="${fill}" placeholder="Select weight" label="Weight" class="select-expanded label-floating">
+                  <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+                  <ion-select-option value="100">100</ion-select-option>
+                  <ion-select-option value="200">200</ion-select-option>
+                  <ion-select-option value="300">300</ion-select-option>
+                  <ion-button slot="end" aria-label="Show/hide password">
+                    <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                  <ion-button slot="end">
+                    <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                </ion-select>
+              </div>
+            `,
+            config
+          );
+
+          const container = page.locator('.container');
+          await expect(container).toHaveScreenshot(screenshot(`select-slot-fill-${fill}-label-floating-expanded`));
+        });
+
+        test('should not have visual regressions with a floating label when value is present', async ({ page }) => {
+          await page.setContent(
+            `
+              <!-- Apply container styles to capture the entire select -->
+              <style>
+                .container {
+                  padding: 8px;
+                }
+              </style>
+
+              <div class="container">
+                <ion-select label-placement="floating" fill="${fill}" placeholder="Select weight" value="100" label="Weight">
+                  <ion-icon slot="start" name="barbell" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" name="heart" aria-hidden="true"></ion-icon>
+                  <ion-select-option value="100">100</ion-select-option>
+                  <ion-select-option value="200">200</ion-select-option>
+                  <ion-select-option value="300">300</ion-select-option>
+                  <ion-button slot="end" aria-label="Show/hide password">
+                    <ion-icon slot="icon-only" name="lock-closed" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                  <ion-button slot="end">
+                    <ion-icon slot="icon-only" name="trash" aria-hidden="true"></ion-icon>
+                  </ion-button>
+                </ion-select>
+              </div>
+            `,
+            config
+          );
+
+          const container = page.locator('.container');
+          await expect(container).toHaveScreenshot(screenshot(`select-slot-fill-${fill}-label-floating-value`));
+        });
+      });
     });
   });
 });
