037d55fe0a2d3a81475cc6654fda7388f218fb12
OS-jacobbell <228905018+OS-jacobbell@users.noreply.github.com>
2026-08-17 14:55:55 -0600

    chore(angular): test schematics for ng add

---
+++ packages/angular/scripts/verify-schematics.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { execSync } = require('node:child_process');
+
+const testName = 'schematics-test';
+const packageRootDir = path.join(__dirname, '..');
+const testDir = path.join(packageRootDir, testName);
+
+try {
+  // Pack ionic-angular
+  execSync(`npm pack`, {cwd: packageRootDir});
+
+  // Create new Angular project
+  execSync(`npx ng new ${testName} --style css --ssr false --ai-config none`, {cwd: packageRootDir});
+
+  // Install ionic-angular package
+  execSync(`npx ng add --skip-confirmation ../ionic-angular-*`, {cwd: testDir});
+} catch(error) {
+  console.log(error);
+  process.exitCode = 1;
+}
+
+fs.removeSync(testDir);
