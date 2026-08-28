157028438437683371358cf0884f19e0eb61d8bb
OS-jacobbell <228905018+OS-jacobbell@users.noreply.github.com>
2026-08-28 09:29:27 -0600

    remove unused testing infrastructure from the code-split test app

--- packages/angular/test/code-split/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'code-split',
-  webDir: 'www'
-};
-
-export default config;
