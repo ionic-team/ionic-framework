3a6a7e1643c95c73aefe7a289fbb9d3bcdf41f33
OS-jacobbell <228905018+OS-jacobbell@users.noreply.github.com>
2026-09-02 14:29:32 -0600

    add comment

--- packages/vue/scripts/build-web-types.js
+++ packages/vue/scripts/build-web-types.js
@@ -74,7 +74,7 @@ for (const component of filteredComponents) {
     name: componentName,
     "doc-url": docUrl,
     description: component.docs,
-    priority: "highest",
+    priority: "highest", // Fixes attribute autocomplete (FW-7636)
     source: {
       module:
         "@ionic/core/" +
